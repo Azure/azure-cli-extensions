3c318e1237707c31917dce90d28374f1bca66bde
Derek Bekoe <debekoe@microsoft.com>
2017-11-01 16:23:55 -0700

    Some refactoring

--- scripts/ci/test_index.py
+++ scripts/ci/test_index.py
@@ -34,14 +34,19 @@ def get_index_data():
         raise AssertionError("Invalid JSON in {}: {}".format(INDEX_PATH, err))
 
 
-def whl_download_from_url(url, ext_file):
+def get_whl_from_url(url, filename, tmp_dir, whl_cache):
+    if url in whl_cache:
+        return whl_cache[url]
     import requests
     r = requests.get(url, stream=True)
     assert r.status_code == 200, "Request to {} failed with {}".format(url, r.status_code)
+    ext_file = os.path.join(tmp_dir, filename)
     with open(ext_file, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:  # ignore keep-alive new chunks
                 f.write(chunk)
+    whl_cache[url] = ext_file
+    return ext_file
 
 
 def get_sha256sum(a_file):
@@ -57,6 +62,12 @@ class TestIndex(unittest.TestCase):
     def setUpClass(cls):
         cls.longMessage = True
         cls.index = get_index_data()
+        cls.whl_cache_dir = tempfile.mkdtemp()
+        cls.whl_cache = {}
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.whl_cache_dir)
 
     def test_format_version(self):
         self.assertEqual(self.index['formatVersion'], '1')
@@ -76,7 +87,8 @@ class TestIndex(unittest.TestCase):
                                 "Filename {} must start with {}".format(item['filename'], ext_name))
                 parsed_filename = WHEEL_INFO_RE(item['filename'])
                 p = parsed_filename.groupdict()
-                universal_wheel = p.get('pyver') == 'py2.py3' and p.get('abi') == 'none' and p.get('plat') == 'any'
+                universal_wheel = p.get('name') and p.get('pyver') == 'py2.py3' and p.get('abi') == 'none' \
+                                  and p.get('plat') == 'any'
                 self.assertTrue(universal_wheel,
                                 "{} of {} not universal (platform independent) wheel. "
                                 "It should end in py2.py3-none-any.whl".format(item['filename'], ext_name))
@@ -86,20 +98,6 @@ class TestIndex(unittest.TestCase):
             for item in exts:
                 self.assertEqual(os.path.basename(item['downloadUrl']), item['filename'],
                                  "Filename must match last segment of downloadUrl")
-
-    @unittest.skipUnless(os.getenv('CI'), 'Skipped as not running on CI')
-    def test_checksums(self):
-        tmp_dir = tempfile.mkdtemp()
-        for exts in self.index['extensions'].values():
-            for item in exts:
-                ext_file = os.path.join(tmp_dir, item['filename'])
-                whl_download_from_url(item['downloadUrl'], ext_file)
-                computed_hash = get_sha256sum(ext_file)
-                self.assertEqual(computed_hash, item['sha256Digest'],
-                                 "Computed {} but found {} in index for {}".format(computed_hash,
-                                                                                   item['sha256Digest'],
-                                                                                   item['filename']))
-        shutil.rmtree(tmp_dir)
 
     def test_filename_duplicates(self):
         filenames = []
@@ -113,6 +111,26 @@ class TestIndex(unittest.TestCase):
                 dups.append(f)
             filename_seen.add(f)
         self.assertFalse(dups, "Duplicate filenames found {}".format(dups))
+
+    @unittest.skipUnless(os.getenv('CI'), 'Skipped as not running on CI')
+    def test_checksums(self):
+        for exts in self.index['extensions'].values():
+            for item in exts:
+                ext_file = get_whl_from_url(item['downloadUrl'], item['filename'],
+                                            self.whl_cache_dir, self.whl_cache)
+                computed_hash = get_sha256sum(ext_file)
+                self.assertEqual(computed_hash, item['sha256Digest'],
+                                 "Computed {} but found {} in index for {}".format(computed_hash,
+                                                                                   item['sha256Digest'],
+                                                                                   item['filename']))
+
+    # @unittest.skipUnless(os.getenv('CI'), 'Skipped as not running on CI')
+    # def test_metadata(self):
+    #     tmp_dir = tempfile.mkdtemp()
+    #     for exts in self.index['extensions'].values():
+    #         for item in exts:
+    #             ext_file = get_whl_from_url(item['downloadUrl'], item['filename'],
+    #                                         self.whl_cache_dir, self.whl_cache)
 
 
 if __name__ == '__main__':
