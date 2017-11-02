2a1d6b463cf9822b7787dc69985f1adef290533a
Derek Bekoe <debekoe@microsoft.com>
2017-11-02 15:04:53 -0700

    Minor change

--- scripts/ci/test_source.sh
+++ scripts/ci/test_source.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if compgen -G "src/*/azext_*/tests" > /dev/null; then
-    for d in src/*/azext_*/tests;
-        do echo "Running tests for $d";
-        python -m unittest discover -v $d;
-    done;
-fi
+for d in src/*/azext_*/tests;
+    do echo "Running tests for $d";
+    python -m unittest discover -v $d;
+done;
