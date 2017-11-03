dc94a3bd618f74acdb3167141b26db31e0d06bee
Derek Bekoe <debekoe@microsoft.com>
2017-11-03 14:02:23 -0700

    CI for extension tests (#13)
    * Fix tests
    
    * No more allow failures
    
    * Allow failures for now

--- scripts/ci/test_source.sh
+++ scripts/ci/test_source.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+# Install CLI & CLI testsdk
+echo "Installing azure-cli-testsdk and azure-cli..."
+pip install "git+https://github.com/Azure/azure-cli@dev#egg=azure-cli-testsdk&subdirectory=src/azure-cli-testsdk" -q
+echo "Installed."
+
 for d in src/*/azext_*/tests;
     do echo "Running tests for $d";
-    python -m unittest discover -v $d;
+    if [ -d $d ]; then
+        python -m unittest discover -v $d;
+    else
+        echo "Skipped $d as not a directory."
+    fi
 done;
