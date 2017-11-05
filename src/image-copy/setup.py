3eb6784659388df4340d67ce8a18eef9f73e5727
Tamir Kamara <tamirkamara@gmail.com>
2017-11-05 16:14:51 +0200

    fix package names to allow index ci to pass

--- src/image-copy/setup.py
+++ src/image-copy/setup.py
@@ -8,7 +8,7 @@
 from codecs import open
 from setuptools import setup, find_packages
 
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
@@ -27,7 +27,7 @@ CLASSIFIERS = [
 DEPENDENCIES = []
 
 setup(
-    name='imagecopyextension',
+    name='image-copy-extension',
     version=VERSION,
     description='An Azure CLI Extension that copies images from region to region.',
     long_description='An Azure CLI Extension that copies images from region to region.',
