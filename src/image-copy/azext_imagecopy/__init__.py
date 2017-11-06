af5eec745341f46d6f9ebbee9f0564350fae8f64
Tamir Kamara <tamirkamara@gmail.com>
2017-11-06 19:15:23 +0200

    add image copy extension to the index file (#17)
    * image-copy: first commit
    
    * fixes after @derekbekoe reviewed
    
    * remove dead code
    
    * round of small fixes
    
    * remove test file
    
    * merg commit
    
    * add image-copy to index.json
    
    * Add examples to help
    
    * fix package names to allow index ci to pass
    
    * fix whitespace
    
    * image-copy: remove mincli from index.json

--- src/image-copy/azext_imagecopy/__init__.py
+++ src/image-copy/azext_imagecopy/__init__.py
@@ -12,6 +12,15 @@ helps['image copy'] = """
     long-summary: >
         Allows to copy a managed image (or vm) to other regions.
         Keep in mind that it requires the source disk to be available.
+    examples:
+        - name: Copy an image to several regions and cleanup at the end.
+          text: >
+            az image copy --source-resource-group mySources-rg --source-object-name myImage \\
+                --target-location uksouth northeurope --target-resource-group "images-repo-rg" --cleanup
+        - name: Use an already generalized vm to create images in other regions.
+          text: >
+            az image copy --source-resource-group mySources-rg --source-object-name myVm \\
+                --source-type vm --target-location uksouth northeurope --target-resource-group "images-repo-rg"
 """
 
 
