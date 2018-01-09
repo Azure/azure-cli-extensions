2b28bb0aa2dfcaf463fb924c01a2d85344b9abf7
Derek Bekoe <debekoe@microsoft.com>
2018-01-09 09:34:59 -0800

    Update deps. for CI (#30)

--- scripts/ci/test_source.sh
+++ scripts/ci/test_source.sh
@@ -3,9 +3,10 @@ set -ex
 
 # Install CLI & CLI testsdk
 echo "Installing azure-cli-testsdk and azure-cli..."
-# TODO Update the git commit or branch when we need a new version of azure-cli-testsdk
-pip install "git+https://github.com/Azure/azure-cli@KnackConversion#egg=azure-cli-testsdk&subdirectory=src/azure-cli-testsdk" -q
-pip install knack==0.3.0 -q
+
+# Update the git commit or branch when we need a new version of azure-cli-testsdk
+pip install "git+https://github.com/Azure/azure-cli@dev#egg=azure-cli-testsdk&subdirectory=src/azure-cli-testsdk" -q
+pip install knack==0.3.1 -q
 echo "Installed."
 
 
