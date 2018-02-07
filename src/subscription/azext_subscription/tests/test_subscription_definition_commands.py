4acc2fa3ce0a3e46677f2bc59551569d7c23a776
wilco <wilcob@microsoft.com>
2018-02-06 18:25:42 -0800

    Style fixes

--- src/subscription/azext_subscription/tests/test_subscription_definition_commands.py
+++ src/subscription/azext_subscription/tests/test_subscription_definition_commands.py
@@ -3,6 +3,8 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# pylint: disable=line-too-long
+
 from azure.cli.testsdk import ScenarioTest
 
 
