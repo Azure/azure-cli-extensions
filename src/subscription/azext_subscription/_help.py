12b77adee41d3452e4a084d0ecaef8f49c118b23
wilco <31971584+wilcobmsft@users.noreply.github.com>
2018-02-07 14:22:57 -0800

    Add Subscription RP extension. (#49)
    * Add Subscription Definition commands.
    
    * Add client arg parameter.
    
    * Update tests
    
    * Re-generate Python SDK.
    
    * Revert "Re-generate Python SDK."
    
    This reverts commit a6d7711d3861111d7b35a47ec6b26b69dadde9fc.
    
    * Ignore line-too-long errors.
    
    * Ignore subscription sdk pylint errors
    
    * Update ignore list to reflect broader range of types typically coming from generated code.
    
    * Style fixes
    
    * Add codeowner entry
    
    * PR feedback:
    1. "subscriptiondefinition" -> "subscription-definition"
    2. Remove "-sdn" parameter
    
    * Addressing PR feedback:
    Update help

---
+++ src/subscription/azext_subscription/_help.py
@@ -0,0 +1,17 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from knack.help_files import helps
+
+
+helps['account subscription-definition'] = """
+    type: group
+    short-summary: Manage Azure Subscription Definitions.
+"""
+
+helps['account subscription-definition create'] = """
+    type: command
+    short-summary: Create a subscription definition.
+"""
