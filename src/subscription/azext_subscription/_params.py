a05fdae002b1d5a71cc3565d017e8f6785528efc
wilco <wilcob@microsoft.com>
2018-02-07 10:45:59 -0800

    PR feedback: 1. "subscriptiondefinition" -> "subscription-definition" 2. Remove "-sdn" parameter

--- src/subscription/azext_subscription/_params.py
+++ src/subscription/azext_subscription/_params.py
@@ -8,10 +8,10 @@ from azure.cli.core.commands.parameters import get_enum_type
 
 # pylint: disable=line-too-long
 def load_arguments(self, _):
-    with self.argument_context('account subscriptiondefinition create') as c:
+    with self.argument_context('account subscription-definition create') as c:
         c.argument('offer_type', required=True, help='The subscription\'s offer type.', arg_type=get_enum_type(['MS-AZR-0017P', 'MS-AZR-0148P']))
-        c.argument('subscription_display_name', options_list=['--subscription-display-name', '-sdn'], help='The subscription display name of the subscription definition.')
+        c.argument('subscription_display_name', help='The subscription display name of the subscription definition.')
 
-    for scope in ['account subscriptiondefinition create', 'account subscriptiondefinition show']:
+    for scope in ['account subscription-definition create', 'account subscription-definition show']:
         with self.argument_context(scope) as c:
             c.argument('subscription_definition_name', options_list=['--name', '-n'], help='Name of the subscription definition.')
