2a3b692b3417aa99d59eb41e6a2fdd88202c3df2
Sisira Panchagnula <sisirap@microsoft.com>
2018-02-07 18:55:22 -0800

    Adding support for .NETCORE
    Adding support for dotnetcore with windows as default
    
    Fixing parsing node version
    
    Fixing -l codepath that regressed plus updating index.json
    
    Fixing build errors & upating index.json

---
+++ src/webapp/azext_webapp/_constants.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+NODE_VERSION_DEFAULT = "8.1"
+NETCORE_VERSION_DEFAULT = "2.0"
+# TODO: Remove this once we have the api returning the versions
+NODE_VERSIONS = ['4.4', '4.5', '6.2', '6.6', '6.9', '6.11', '8.0', '8.1']
+NETCORE_VERSIONS = ['1.0', '1.1', '2.0']
+NODE_RUNTIME_NAME = "node"
+NETCORE_RUNTIME_NAME = "dotnetcore"
+OS_DEFAULT = "Windows"
