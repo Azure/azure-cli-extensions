6ee639074b59233b86b9b616c2c99c414ce56c9a
Derek Bekoe <debekoe@microsoft.com>
2018-02-11 20:42:27 -0800

    Fix “[CI] Tests aren't being mocked correctly on CI reruns”

--- scripts/ci/test_source.sh
+++ scripts/ci/test_source.sh
@@ -9,25 +9,6 @@ pip install "git+https://github.com/Azure/azure-cli@dev#egg=azure-cli-testsdk&su
 pip install knack==0.3.1 -q
 echo "Installed."
 
-
-_AZURE_EXTENSION_DIR="$AZURE_EXTENSION_DIR"
-
-for d in src/*/azext_*/tests;
-    do echo "Running tests for $d";
-    if [ -d $d ]; then
-        export AZURE_EXTENSION_DIR=$(mktemp -d);
-        pip install --upgrade --target $AZURE_EXTENSION_DIR/ext $d/../..;
-        python -m unittest discover -v $d/../..;
-        rm -rf $AZURE_EXTENSION_DIR;
-    else
-        echo "Skipped $d as not a directory."
-    fi
-done;
-
-if ! [ -z "${_AZURE_EXTENSION_DIR+_}" ] ; then
-    AZURE_EXTENSION_DIR="$_AZURE_EXTENSION_DIR"
-    export AZURE_EXTENSION_DIR
-    unset _AZURE_EXTENSION_DIR
-fi
+python ./scripts/ci/test_source.py -v
 
 echo "OK. Completed tests."
