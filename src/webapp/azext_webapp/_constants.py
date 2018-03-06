4e37afae80ceb2cb6aee31648f3259dd6b88161c
Sisira Panchagnula <sisirap@microsoft.com>
2018-03-06 13:31:26 -0800

    Fixes#69: Add url after create in json, also changing to return json object instead of string
    Fixes#95: Updating the default version of Node
    
    Fixes:#75:Remove generated zip file after the creating and deploying the app
    
    Updating index.json

--- src/webapp/azext_webapp/_constants.py
+++ src/webapp/azext_webapp/_constants.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-NODE_VERSION_DEFAULT = "8.1"
+NODE_VERSION_DEFAULT = "8.9"
 NETCORE_VERSION_DEFAULT = "2.0"
 # TODO: Remove this once we have the api returning the versions
 NODE_VERSIONS = ['4.4', '4.5', '6.2', '6.6', '6.9', '6.11', '8.0', '8.1']
