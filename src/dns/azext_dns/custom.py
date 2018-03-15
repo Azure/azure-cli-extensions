3b335a19141bd70194d372547cc79494ee46ce7e
Muhammad Waqar <waqar.hameed08@gmail.com>
2018-03-15 05:22:09 +0500

    Adding CLI extension for Private DNS Public Preview.

---
+++ src/dns/azext_dns/custom.py
@@ -0,0 +1,43 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from __future__ import print_function
+from knack.log import get_logger
+from azext_dns.dns.models import (Zone)
+
+logger = get_logger(__name__)
+
+
+def create_dns_zone(client, resource_group_name, zone_name, location='global', tags=None,
+                    if_none_match=False, zone_type='Public', resolution_vnets=None, registration_vnets=None):
+    zone = Zone(location=location, tags=tags)
+
+    if hasattr(zone, 'zone_type'):
+        zone.zone_type = zone_type
+        zone.registration_virtual_networks = registration_vnets
+        zone.resolution_virtual_networks = resolution_vnets
+
+    return client.create_or_update(resource_group_name, zone_name, zone, if_none_match='*' if if_none_match else None)
+
+
+def update_dns_zone(instance, tags=None, zone_type=None, resolution_vnets=None, registration_vnets=None):
+
+    if tags is not None:
+        instance.tags = tags
+
+    if zone_type:
+        instance.zone_type = zone_type
+
+    if resolution_vnets == ['']:
+        instance.resolution_virtual_networks = None
+    elif resolution_vnets:
+        instance.resolution_virtual_networks = resolution_vnets
+
+    if registration_vnets == ['']:
+        instance.registration_virtual_networks = None
+    elif registration_vnets:
+        instance.registration_virtual_networks = registration_vnets
+
+    return instance
