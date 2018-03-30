7cafdd55668f56c12e0642c32e6f850af8c3ee4e
Rohit Joy <rohitjoy@microsoft.com>
2018-03-29 19:23:11 -0700

    Update help.

--- src/rdbms/azext_rdbms/_help.py
+++ src/rdbms/azext_rdbms/_help.py
@@ -9,6 +9,19 @@ from knack.help_files import helps
 
 
 def add_helps(command_group, server_type):
+    helps['{} server create'.format(command_group)] = """
+                short-summary: Create a server.
+                examples:
+                    - name: Create a {0} server with only required paramaters in North Europe.
+                      text: az {1} server create -l northeurope -g testgroup -n testsvr -u username -p password
+                    - name: Create a {0} server with a Standard performance tier and 2 vcore in North Europe.
+                      text: az {1} server create -l northeurope -g testgroup -n testsvr -u username -p password \\
+                            --sku-name GP_Gen4_2
+                    - name: Create a {0} server with all paramaters set.
+                      text: az {1} server create -l northeurope -g testgroup -n testsvr -u username -p password \\
+                            --sku-name B_Gen4_2 --ssl-enforcement Disabled \\
+                            --storage-size 51200 --tags "key=value" --version {{server-version}}
+                """.format(server_type, command_group)
     helps['{} server georestore'.format(command_group)] = """
                 type: command
                 short-summary: Georestore a server from backup.
