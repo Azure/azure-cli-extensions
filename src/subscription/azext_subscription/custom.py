74c797965ca9e3c3f5034285e2e08e70f82974e1
wilco <wilcob@microsoft.com>
2018-03-30 16:58:36 -0700

    Improve parameter names.

--- src/subscription/azext_subscription/custom.py
+++ src/subscription/azext_subscription/custom.py
@@ -75,11 +75,12 @@ def _object_id_args_helper(cli_ctx, object_id=None, spn=None, upn=None):
 
 
 def cli_subscription_create(cmd, client, enrollment_account_name, offer_type,
-                            display_name=None, object_id="", spn="", upn=""):
+                            display_name=None, owner_object_id="", owner_spn="",
+                            owner_upn=""):
     owners = [_object_id_args_helper(cmd.cli_ctx, object_id=x) for x
-              in object_id.split(',') if object_id] + \
-             [_object_id_args_helper(cmd.cli_ctx, spn=x) for x in spn.split(',') if spn] + \
-             [_object_id_args_helper(cmd.cli_ctx, upn=x) for x in upn.split(',') if upn]
+              in owner_object_id.split(',') if owner_object_id] + \
+             [_object_id_args_helper(cmd.cli_ctx, spn=x) for x in owner_spn.split(',') if owner_spn] + \
+             [_object_id_args_helper(cmd.cli_ctx, upn=x) for x in owner_upn.split(',') if owner_upn]
     creation_parameters = SubscriptionCreationParameters(
         display_name=display_name,
         offer_type=offer_type,
