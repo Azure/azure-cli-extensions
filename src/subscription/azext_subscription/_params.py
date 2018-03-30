b04b4048244573c70a845f7de793d592962f4e86
wilco <wilcob@microsoft.com>
2018-03-29 22:53:04 -0700

    Upgrade to latest Python subscription SDK

--- src/subscription/azext_subscription/_params.py
+++ src/subscription/azext_subscription/_params.py
@@ -8,10 +8,9 @@ from azure.cli.core.commands.parameters import get_enum_type
 
 # pylint: disable=line-too-long
 def load_arguments(self, _):
-    with self.argument_context('account subscription-definition create') as c:
+    with self.argument_context('account create') as c:
         c.argument('offer_type', required=True, help='The offer type of the subscription. For example, MS-AZR-0017P (EnterpriseAgreement) and MS-AZR-0148P (EnterpriseAgreement devTest) are available.', arg_type=get_enum_type(['MS-AZR-0017P', 'MS-AZR-0148P']))
-        c.argument('subscription_display_name', help='The subscription display name of the subscription definition.')
-
-    for scope in ['account subscription-definition create', 'account subscription-definition show']:
-        with self.argument_context(scope) as c:
-            c.argument('subscription_definition_name', options_list=['--name', '-n'], help='Name of the subscription definition.')
+        c.argument('display_name', help='The display name of the subscription.')
+        c.argument('object_id', help='The object id(s) of owners which should be granted access to the new subscription.')
+        c.argument('spn', help='The service principal names of owners which should be granted access to the new subscription.')
+        c.argument('upn', help='The user principal names of owners who should be granted access to the new subscription.')
