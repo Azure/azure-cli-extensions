8dcda541b251db5b224b89239ec4543db430c764
Rohit Joy <rohitjoy@microsoft.com>
2018-03-29 19:31:45 -0700

    Add georestore.

--- src/rdbms/azext_rdbms/postgresql/models/performance_tier_properties.py
+++ src/rdbms/azext_rdbms/postgresql/models/performance_tier_properties.py
@@ -28,7 +28,7 @@ class PerformanceTierProperties(Model):
         'service_level_objectives': {'key': 'serviceLevelObjectives', 'type': '[PerformanceTierServiceLevelObjectives]'},
     }
 
-    def __init__(self, id=None, service_level_objectives=None):
-        super(PerformanceTierProperties, self).__init__()
-        self.id = id
-        self.service_level_objectives = service_level_objectives
+    def __init__(self, **kwargs):
+        super(PerformanceTierProperties, self).__init__(**kwargs)
+        self.id = kwargs.get('id', None)
+        self.service_level_objectives = kwargs.get('service_level_objectives', None)
