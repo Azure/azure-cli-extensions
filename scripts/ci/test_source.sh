031060766cea96d42ff6d82a8ef2588ea405cac4
Derek Bekoe <debekoe@microsoft.com>
2018-04-02 16:54:08 -0700

    Fix rdbms test

--- scripts/ci/test_source.sh
+++ scripts/ci/test_source.sh
@@ -6,7 +6,7 @@ echo "Installing azure-cli-testsdk and azure-cli..."
 
 # Update the git commit or branch when we need a new version of azure-cli-testsdk
 pip install "git+https://github.com/Azure/azure-cli@dev#egg=azure-cli-testsdk&subdirectory=src/azure-cli-testsdk" -q
-pip install knack==0.3.1 -q
+pip install knack==0.3.2 -q
 echo "Installed."
 
 python ./scripts/ci/test_source.py -v
