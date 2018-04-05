a0b24e846c3a9d74a5deddac5289753d5187869b
Derek Bekoe <debekoe@microsoft.com>
2018-04-05 15:55:56 -0700

    Fix tests: get CLI from edge builds (#129)
    * Get CLI from edge
    
    * Fix recordings
    
    * Fix others
    
    * Test fix
    
    * fix again
    
    * try this

--- scripts/ci/test_source.sh
+++ scripts/ci/test_source.sh
@@ -3,10 +3,8 @@ set -ex
 
 # Install CLI & CLI testsdk
 echo "Installing azure-cli-testsdk and azure-cli..."
-
-# Update the git commit or branch when we need a new version of azure-cli-testsdk
+pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
 pip install "git+https://github.com/Azure/azure-cli@dev#egg=azure-cli-testsdk&subdirectory=src/azure-cli-testsdk" -q
-pip install knack==0.3.2 -q
 echo "Installed."
 
 python ./scripts/ci/test_source.py -v
--- src/subscription/azext_subscription/tests/latest/test_subscription_commands.py
+++ src/subscription/azext_subscription/tests/latest/test_subscription_commands.py
@@ -12,6 +12,6 @@ from azure.cli.testsdk import ScenarioTest
 class AzureSubscriptionScenarioTest(ScenarioTest):
     def test_create_subscription(self):
         enrollment_accounts = self.cmd('billing enrollment-account list').get_output_in_json()
-        result = self.cmd('account create --enrollment-account-name {} --offer-type MS-AZR-0148P'.format(enrollment_accounts[0].name)).get_output_in_json()
+        result = self.cmd('account create --enrollment-account-name {} --offer-type MS-AZR-0148P'.format(enrollment_accounts[0]['name'])).get_output_in_json()
         self.assertIsNotNone(result)
-        self.assertIsNotNone(result.subscription_link)
+        self.assertIsNotNone(result['subscriptionLink'])
