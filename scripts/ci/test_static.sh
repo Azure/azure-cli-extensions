728f66eca9f71c09ebb2ab71ae2404ffa49da40c
Willie Xu <wx44@cornell.edu>
2018-04-16 16:01:33 -0700

    Initial extension for storage preview  (#138)
    * added new storage preview module extension
    
    * changed namespaces to be relative
    
    CLI code
    
    resolved imports
    
    * removed non-latest tests, ignore oylint for test files
    
    * resolved more pylint failures
    
    pylint errors
    
    relative import pylint
    
    * adding willie as codeowner
    
    codeowner
    
    * addressed feedback

--- scripts/ci/test_static.sh
+++ scripts/ci/test_static.sh
@@ -5,7 +5,8 @@ proc_number=`python -c 'import multiprocessing; print(multiprocessing.cpu_count(
 
 # Run pylint/flake8 on extensions
 # - We ignore 'models', 'operations' and files with suffix '_client.py' as they typically come from vendored Azure SDKs
-pylint ./src/*/azext_*/ --ignore=models,operations,service_bus_management_client,subscription_client,managementgroups,managementpartner --ignore-patterns=[a-zA-Z_]+_client.py --rcfile=./pylintrc -j $proc_number
+pylint ./src/*/azext_*/ --ignore=models,operations,service_bus_management_client,subscription_client,managementgroups,\
+managementpartner --ignore-patterns=test_*,[a-zA-Z_]+_client.py --rcfile=./pylintrc -j $proc_number
 flake8 --statistics --exclude=models,operations,*_client.py,managementgroups,managementpartner --append-config=./.flake8 ./src/*/azext_*/
 
 # Run pylint/flake8 on CI files
