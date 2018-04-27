be00046f14a9afd124fdb7b7254bd29cd1a6abea
Nicholas King <nicking@microsoft.com>
2018-04-27 15:16:27 -0700

    Web apps snapshot commands (#145)
    * First draft of webapps snapshot commands
    
    * Fixes for linter errors
    
    * Fix parameter typos in CLI error message when --source-name and --source-resource-group aren't used together

--- src/webapp/azext_webapp/_help.py
+++ src/webapp/azext_webapp/_help.py
@@ -16,3 +16,26 @@ helps['webapp up'] = """
             az webapp up -n MyUniqueAppName --dryrun \n
             az webapp up -n MyUniqueAppName -l locationName
 """
+
+helps['webapp config snapshot list'] = """
+    type: command
+    short-summary: List the snapshots available for a web app.
+                   Snapshots are automatically managed backups of web app content and configuration.
+    examples:
+        - name: List the snapshots available for a web app named MyApp.
+          text: >
+            az webapp config snapshot list -g Default-Web-WestUS -n MyApp
+"""
+
+helps['webapp config snapshot restore'] = """
+    type: command
+    short-summary: Restore a snapshot to a web app.
+                   A snapshot from a different web app or slot can be restored by specifying the source.
+    examples:
+        - name: Overwrite a web app with its own snapshot.
+          text: >
+            az webapp config snapshot restore -g Default-Web-WestUS -n MyApp -t 2018-04-25T00:09:20.8736381Z
+        - name: Overwrite a web app's staging slot with a snapshot from its production slot.
+          text: >
+            az webapp config snapshot restore -g Default-Web-WestUS -n MyApp -s staging -t 2018-04-25T00:09:20.8736381Z --source-resource-group Default-Web-WestUS --source-name MyApp --restore-config
+"""
