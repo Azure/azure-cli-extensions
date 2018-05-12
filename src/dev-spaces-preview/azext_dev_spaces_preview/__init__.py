3978736fc71fd06a49d759e24d66dfd1b977b646
Saurabh Sarkar <saurabsa@microsoft.com>
2018-05-11 17:12:21 -0700

    Adding dev-spaces extension under the aks module (#168)
    * Adding dev-connect extension under the aks module
    
    * Update Windows Install Path
    
    * Update error message
    
    * Added space create/select functionality with --parent-space options
    
    * File Structure updated
    
    * Update
    
    * CR updates
     - Refactor
     - min AZ cli dependency
    
    * Update package name
    
    * Adding Linux support
    
    * CI build fix
    
    * Updating index - including dev-spaces-preview
    CI build fix
    
    * Fix CI
    
    * Reverting index changes
    
    * Updating PermissionError to OSError
    
    * Update
    
    * Updating CodeOwners

---
+++ src/dev-spaces-preview/azext_dev_spaces_preview/__init__.py
@@ -0,0 +1,40 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core import AzCommandsLoader
+
+import azext_dev_spaces_preview._help  # pylint: disable=unused-import
+
+
+class DevspacesExtCommandLoader(AzCommandsLoader):
+
+    def __init__(self, cli_ctx=None):
+        from azure.cli.core.commands import CliCommandType
+        dev_spaces_custom = CliCommandType(
+            operations_tmpl='azext_dev_spaces_preview.custom#{}')
+        super(DevspacesExtCommandLoader, self).__init__(cli_ctx=cli_ctx,
+                                                        custom_command_type=dev_spaces_custom,
+                                                        min_profile='2017-03-10-profile')
+
+    def load_command_table(self, _):
+        with self.command_group('aks') as g:
+            g.custom_command('use-dev-spaces', 'aks_use_dev_spaces')
+            g.custom_command('remove-dev-spaces', 'aks_remove_dev_spaces')
+        return self.command_table
+
+    def load_arguments(self, _):
+        with self.argument_context('aks use-dev-spaces') as c:
+            c.argument('cluster_name', options_list=['--name', '-n'])
+            c.argument('resource_group_name', options_list=['--resource-group', '-g'])
+            c.argument('space_name', options_list=['--space', '-s'])
+            c.argument('parent_space_name', options_list=['--parent-space', '-p'])
+
+        with self.argument_context('aks remove-dev-spaces') as c:
+            c.argument('cluster_name', options_list=['--name', '-n'])
+            c.argument('resource_group_name', options_list=['--resource-group', '-g'])
+            c.argument('prompt', options_list=['--yes', '-y'], action='store_true')
+
+
+COMMAND_LOADER_CLS = DevspacesExtCommandLoader
