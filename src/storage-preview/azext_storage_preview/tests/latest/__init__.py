aa958b3b9303f3d4a0181f9f856c3b8f984d4a83
williexu <wx44@cornell.edu>
2018-05-14 16:54:14 -0700

    updated old recordings for new sdk

--- src/storage-preview/azext_storage_preview/tests/latest/__init__.py
+++ src/storage-preview/azext_storage_preview/tests/latest/__init__.py
@@ -2,3 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+
+from azure.cli.core.profiles import register_resource_type
+from ...profiles import CUSTOM_MGMT_STORAGE
+register_resource_type('latest', CUSTOM_MGMT_STORAGE, '2018-02-01')
