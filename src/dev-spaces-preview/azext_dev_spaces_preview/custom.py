4cb40e61f886dcaa43b5be144c6d3e3cd6658d68
Saurabh Sarkar <saurabsa@microsoft.com>
2018-06-22 15:09:18 -0700

    Updating output messages to be printed in default mode

--- src/dev-spaces-preview/azext_dev_spaces_preview/custom.py
+++ src/dev-spaces-preview/azext_dev_spaces_preview/custom.py
@@ -119,7 +119,11 @@ def _install_dev_spaces_cli(force_install):
 
     if should_install_azds:
         # Install AZDS
-        logger.info('Installing Dev Spaces (Preview) commands...')
+        print('Installing Dev Spaces (Preview) commands...')
+        if system == 'Windows':
+            print('A separate window will open to guide you through the installation process.')
+        else:
+            print('The following dependencies will be installed: curl, unzip, dotnet.')
         urlretrieve(setup_url, setup_file)
         try:
             subprocess.call(
