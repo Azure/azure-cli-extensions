7a1454a4ea2b881aa0d6b50bced4fe77ae27049a
Ace Eldeib <aleldeib@microsoft.com>
2018-07-27 13:52:40 -0700

    fix homepage and bump minor version

--- src/loganalytics/setup.py
+++ src/loganalytics/setup.py
@@ -8,7 +8,7 @@
 from codecs import open
 from setuptools import setup, find_packages
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
@@ -39,7 +39,7 @@ setup(
     license='MIT',
     author='Ace Eldeib',
     author_email='aleldeib@microsoft.com',
-    url='https://github.com/Azure/azure-cli-extensions',
+    url='https://github.com/Azure/azure-cli-extensions/tree/master/src/log-analytics',
     classifiers=CLASSIFIERS,
     packages=find_packages(exclude=["tests"]),
     package_data={'azext_loganalytics': ['azext_metadata.json']},
