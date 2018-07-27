6844dd0fe88fcb0a2964f80515438f58890f9d5e
Ace Eldeib <aleldeib@microsoft.com>
2018-07-26 18:41:22 -0700

    unused imports

--- src/loganalytics/azext_loganalytics/commands.py
+++ src/loganalytics/azext_loganalytics/commands.py
@@ -8,8 +8,5 @@
 
 def load_command_table(self, _):
 
-    from azure.cli.core.commands import CliCommandType
-    from azext_loganalytics._client_factory import loganalytics_data_plane_client
-
     with self.command_group('monitor log-analytics') as g:
         g.custom_command('query', 'execute_query')
