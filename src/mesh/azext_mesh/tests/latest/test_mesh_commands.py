9ae10fa7de160f7afd1b3530790a4e854888ca91
Jeffrey Jarry <jejarry@microsoft.com>
2018-07-30 11:00:02 -0700

    added proper license header

--- src/mesh/azext_mesh/tests/latest/test_mesh_commands.py
+++ src/mesh/azext_mesh/tests/latest/test_mesh_commands.py
@@ -1,3 +1,10 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# pylint: disable=line-too-long,unused-argument
+
 import unittest
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
 import os
