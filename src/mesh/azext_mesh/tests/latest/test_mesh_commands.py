bf20e90739d86957a34977d9e3089d0dd677c38b
Jeffrey Jarry <jejarry@microsoft.com>
2018-08-01 13:36:17 -0700

    added a test for log

--- src/mesh/azext_mesh/tests/latest/test_mesh_commands.py
+++ src/mesh/azext_mesh/tests/latest/test_mesh_commands.py
@@ -8,7 +8,7 @@
 import unittest
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
 import os
-
+import urllib.request
 
 def _get_test_data_file(filename):
     curr_dir = os.path.dirname(os.path.realpath(__file__))
@@ -105,7 +105,6 @@ class AzureMeshServiceScenarioTest(ScenarioTest):
             'service_name': 'helloWorldService',
             'replica_name': 0,
             'template_location': _get_test_data_file('template1.json')
-
         })
 
         self.cmd('az mesh deployment create -g {rg} --template-file {template_location} --name {deployment_name}')
@@ -125,6 +124,34 @@ class AzureMeshServiceScenarioTest(ScenarioTest):
             self.exists('networkRefs'),
             self.exists('osType'),
             self.exists('replicaName'),
+        ])
+
+        self.cmd('az mesh app delete -g {rg} --name {app_name} --yes')
+
+    @ResourceGroupPreparer(random_name_length=20)
+    def test_code_package_log_commands(self, resource_group):
+        app_name = 'helloWorldApp'
+
+        self.kwargs.update({
+            'resource_group': resource_group,
+            'deployment_name': self.create_random_name(prefix='cli', length=24),
+            'app_name': app_name,
+            'service_name': 'helloWorldService',
+            'replica_name': 0,
+            'template_location': _get_test_data_file('template1.json'),
+            'code_package_name': 'helloWorldCode',
+            'network_name': 'helloWorldNetwork'
+        })
+
+        self.cmd('az mesh deployment create -g {rg} --template-file {template_location} --name {deployment_name}')
+
+        network_info = self.cmd('az mesh network show -g {rg} -n {network_name}').get_output_in_json()
+        ip = network_info["ingressConfig"]["publicIpAddress"]
+        urllib.request.urlopen('http://' + ip)
+
+        # Test log
+        self.cmd('az mesh code-package-log get --app-name {app_name} --code-package-name helloWorldCode  --replica-name {replica_name} --resource-group {rg} --service-name {service_name} ', checks=[
+            self.exists('content')
         ])
 
         self.cmd('az mesh app delete -g {rg} --name {app_name} --yes')
