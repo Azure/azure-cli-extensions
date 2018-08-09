23da285003f2f8849307ebee4fb13a30761f6035
Roshanak <roshanak@Roshanaks-MacBook-Pro-2.local>
2018-08-09 11:47:02 -0700

    fixed linter issues

--- src/find/azext_find/test/test_custom.py
+++ src/find/azext_find/test/test_custom.py
@@ -9,11 +9,13 @@ from azext_find.custom import (
     call_aladdin_service,
 )
 
+
 class FindCustomCommandTest(unittest.TestCase):
 
     def test_call_aladdin_service(self):
         response = call_aladdin_service("what is azure cli?")
         self.assertEqual(200, response.status_code)
+
 
 if __name__ == '__main__':
     unittest.main()
