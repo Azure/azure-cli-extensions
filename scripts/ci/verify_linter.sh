ae09fcb4fcc8fead7f4b8e233a55373d45ce8a68
williexu <wx44@cornell.edu>
2018-09-05 11:44:21 -0700

    added az -v

--- scripts/ci/verify_linter.sh
+++ scripts/ci/verify_linter.sh
@@ -6,6 +6,7 @@ echo "Installing azure-cli-dev-tools and azure-cli..."
 pip install --pre azure-cli --extra-index-url https://azurecliprod.blob.core.windows.net/edge
 pip install -e "git+https://github.com/Azure/azure-cli@dev#egg=azure-cli-dev-tools&subdirectory=tools" -q
 echo "Installed."
+az --version
 set +x
 
 # check for index updates
