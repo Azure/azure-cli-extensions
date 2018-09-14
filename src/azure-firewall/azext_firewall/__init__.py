2129c8754e32ae6818e8dac983a32523f4a96b68
Travis Prescott <travisj.prescott@gmail.com>
2018-09-14 10:27:05 -0700

    Azure Firewall commands

---
+++ src/azure-firewall/azext_firewall/__init__.py
@@ -0,0 +1,35 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core import AzCommandsLoader
+from azure.cli.core.profiles import register_resource_type
+
+import azext_firewall._help  # pylint: disable=unused-import
+
+
+class AzureFirewallCommandsLoader(AzCommandsLoader):
+
+    def __init__(self, cli_ctx=None):
+        from azure.cli.core.commands import CliCommandType
+        from .profiles import CUSTOM_FIREWALL
+        register_resource_type('latest', CUSTOM_FIREWALL, '2018-08-01')
+
+        super(AzureFirewallCommandsLoader, self).__init__(
+            cli_ctx=cli_ctx,
+            custom_command_type=CliCommandType(operations_tmpl='azext_firewall.custom#{}'),
+            resource_type=CUSTOM_FIREWALL
+        )
+
+    def load_command_table(self, args):
+        from .commands import load_command_table
+        load_command_table(self, args)
+        return self.command_table
+
+    def load_arguments(self, args):
+        from ._params import load_arguments
+        load_arguments(self, args)
+
+
+COMMAND_LOADER_CLS = AzureFirewallCommandsLoader
---
+++ src/azure-firewall/azext_firewall/_client_factory.py
@@ -0,0 +1,19 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+def network_client_factory(cli_ctx, aux_subscriptions=None, **_):
+    from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    from .profiles import CUSTOM_FIREWALL
+    return get_mgmt_service_client(cli_ctx, CUSTOM_FIREWALL, aux_subscriptions=aux_subscriptions,
+                                   api_version='2018-08-01')
+
+
+def cf_firewalls(cli_ctx, _):
+    return network_client_factory(cli_ctx).azure_firewalls
+
+
+def cf_firewall_fqdn_tags(cli_ctx, _):
+    return network_client_factory(cli_ctx).azure_firewall_fqdn_tags
