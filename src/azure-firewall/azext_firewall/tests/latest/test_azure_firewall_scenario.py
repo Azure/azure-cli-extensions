912cbfb50519f5e4ccecac839d92e5eb2d8c4ea0
Travis Prescott <tjprescott@users.noreply.github.com>
2018-09-22 10:15:32 -0700

    Azure Firewall commands (#301)

---
+++ src/azure-firewall/azext_firewall/tests/latest/test_azure_firewall_scenario.py
@@ -0,0 +1,23 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer, StorageAccountPreparer, JMESPathCheck, NoneCheck,
+                               api_version_constraint)
+from azure.cli.core.profiles import ResourceType
+from ...profiles import CUSTOM_FIREWALL
+
+
+class AzureFirewallScenario(ScenarioTest):
+
+    @ResourceGroupPreparer(name_prefix='cli_test_azure_firewall')
+    def test_azure_firewall(self, resource_group):
+
+        self.kwargs.update({
+            'af': 'af1',
+            'coll': 'rc1',
+            'rule1': 'rule1',
+            'rule2': 'rule2'
+        })
+        pass
