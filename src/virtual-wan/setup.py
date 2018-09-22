654635674691d9e5c4913faab8d928d7fc4d73c2
Travis Prescott <tjprescott@users.noreply.github.com>
2018-09-22 12:11:20 -0700

    [Network] Virtual WAN and related commands. (#305)
    * Virtual WAN and related commands.
    
    * Changes.

---
+++ src/virtual-wan/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from codecs import open
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
+]
+
+DEPENDENCIES = []
+
+setup(
+    name='virtual-wan',
+    version=VERSION,
+    description='Manage virtual WAN, hubs, VPN gateways and VPN sites.',
+    license='MIT',
+    author='Microsoft Corporation',
+    author_email='azpycli@microsoft.com',
+    url='https://github.com/Azure/azure-cli-extensions/tree/master/src/virtual-wan',
+    classifiers=CLASSIFIERS,
+    package_data={'azext_vwan': ['azext_metadata.json']},
+    packages=find_packages(),
+    install_requires=DEPENDENCIES
+)
