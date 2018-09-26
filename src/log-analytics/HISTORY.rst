24f3fcd2c3aa81b8a157339fb998fc09c325fec7
Ace Eldeib <aleldeib@microsoft.com>
2018-09-26 15:17:09 -0700

    remove params to make CI happy, update index, history

--- src/log-analytics/HISTORY.rst
+++ src/log-analytics/HISTORY.rst
@@ -1,12 +1,22 @@
+0.1.4
+++++++++++++++++++
+
+* Fix generation for --analytics-query help text.
+
+0.1.3
+++++++++++++++++++
+
+* Fix help text for command.
+
 0.1.2
 ++++++++++++++++++
 
-*  Change --kql to --analytics-query
+*  Change --kql to --analytics-query.
 
 0.1.1
 ++++++++++++++++++
 
-* Fix homepage
+* Fix homepage.
 
 0.1.0
 ++++++++++++++++++
--- src/log-analytics/azext_loganalytics/_help.py
+++ src/log-analytics/azext_loganalytics/_help.py
@@ -16,20 +16,11 @@ helps['monitor log-analytics query'] = """
     type: command
     short-summary: Query a Log Analytics workspace.
     parameters:
-      - name: --workspace
-        type: string
-        short-summary: GUID of the Log Analytics workspace.
       - name: --analytics-query
         type: string
         short-summary: Query to execute over the Log Analytics data.
-      - name: --timespan
-        type: string
-        short-summary: Timespan over which to query data. Defaults to all data.
-      - name: --workspaces
-        type: array
-        short-summary: Additional workspaces to union data for querying. Specify additional workspace IDs separated by space.
     examples:
-        - name: Execute a simple query over past 3.5 days.
-          text: |
-            az monitor log-analytics query -w b8317023-66e4-4edc-8a5b-7c002b22f92f --analytics-query "AzureActivity | summarize count() by bin(timestamp, 1h)" -t P3DT12H
+      - name: Execute a simple query over past 3.5 days.
+        text: |
+          az monitor log-analytics query -w b8317023-66e4-4edc-8a5b-7c002b22f92f --analytics-query "AzureActivity | summarize count() by bin(timestamp, 1h)" -t P3DT12H
 """
