c36822005606596882e525ae7f15e4b102fac5d3
Ace Eldeib <alexeldeib@gmail.com>
2018-11-02 14:33:40 -0700

    Exclude tests

--- src/application-insights/setup.py
+++ src/application-insights/setup.py
@@ -41,7 +41,7 @@ setup(
     author_email='aleldeib@microsoft.com',
     url='https://github.com/Azure/azure-cli-extensions/tree/master/src/application-insights',
     classifiers=CLASSIFIERS,
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     package_data={'azext_loganalytics': ['azext_metadata.json']},
     install_requires=DEPENDENCIES
 )
