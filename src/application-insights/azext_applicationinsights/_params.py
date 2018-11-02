638e865d7f577096abaa7a3392cd44ead5cf84e2
Ace Eldeib <alexeldeib@gmail.com>
2018-11-02 00:15:57 -0700

    match up params

--- src/application-insights/azext_applicationinsights/_params.py
+++ src/application-insights/azext_applicationinsights/_params.py
@@ -8,7 +8,7 @@
 
 def load_arguments(self, _):
     with self.argument_context('monitor app-insights query') as c:
-        c.argument('application', options_list=['--app', '-a'], help='GUID of the Application Insights applications')
+        c.argument('application', options_list=['--app-id'], help='GUID of the Application Insights applications')
         c.argument('analytics_query', help='Query to execute over Application Insights data.')
         c.argument('timespan', options_list=['--timespan', '-t'], help='Timespan over which to query. Defaults to querying all available data.')
         c.argument('applications', nargs='+', help='Additional applications over which to union data for querying. Specify additional workspace IDs separated by space.')
