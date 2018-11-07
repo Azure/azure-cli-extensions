b80caa9a53f73860255849a3a8178e4f6d3dd0df
Ace Eldeib <alexeldeib@gmail.com>
2018-11-07 00:35:44 -0800

    remove bad disable

--- src/application-insights/azext_applicationinsights/_params.py
+++ src/application-insights/azext_applicationinsights/_params.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-# pylint: disable=line-too-long,no-ids-for-list-commands
+# pylint: disable=line-too-long
 from ._validators import validate_applications
 
 
