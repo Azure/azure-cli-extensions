ef9ee090324297031232613dfd778cdcc85da4fc
Feng Li <afengli@hotmail.com>
2018-12-05 14:19:50 -0800

    Update create_target.py
    change parameter time_out to timeout

--- src/image-copy/azext_imagecopy/create_target.py
+++ src/image-copy/azext_imagecopy/create_target.py
@@ -18,7 +18,7 @@ STORAGE_ACCOUNT_NAME_LENGTH = 24
 def create_target_image(location, transient_resource_group_name, source_type, source_object_name,
                         source_os_disk_snapshot_name, source_os_disk_snapshot_url, source_os_type,
                         target_resource_group_name, azure_pool_frequency, tags, target_name, target_subscription,
-                        time_out):
+                        timeout):
 
     random_string = get_random_string(STORAGE_ACCOUNT_NAME_LENGTH - len(location))
 
@@ -48,8 +48,8 @@ def create_target_image(location, transient_resource_group_name, source_type, so
     logger.debug("storage account key: %s", target_storage_account_key)
 
     expiry_format = "%Y-%m-%dT%H:%MZ"
-    expiry = datetime.datetime.utcnow() + datetime.timedelta(seconds=time_out)
-    logger.debug("create target storage sas using timeout seconds: %d", time_out)
+    expiry = datetime.datetime.utcnow() + datetime.timedelta(seconds=timeout)
+    logger.debug("create target storage sas using timeout seconds: %d", timeout)
 
     cli_cmd = prepare_cli_command(['storage', 'account', 'generate-sas',
                                    '--account-name', target_storage_account_name,
