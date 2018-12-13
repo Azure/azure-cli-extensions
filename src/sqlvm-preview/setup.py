8e7ecbaa2809d92ca2b6a33bec8ef775aef0c23c
Yadi Reyes <yareyes@microsoft.com>
2018-12-12 18:12:05 -0800

    Flipping the azext_metadata and removing 3.7 support

--- src/sqlvm-preview/setup.py
+++ src/sqlvm-preview/setup.py
@@ -19,7 +19,6 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
     'License :: OSI Approved :: MIT License',
 ]
 
