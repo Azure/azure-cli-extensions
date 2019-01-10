8063c94e179d6e55538ae3bfb5c5820625c1e6e2
williexu <wx44@cornell.edu>
2019-01-10 11:55:06 -0800

    finished mysql up logic

--- src/rdbms-up/azext_rdbms_up/custom.py
+++ src/rdbms-up/azext_rdbms_up/custom.py
@@ -7,7 +7,10 @@ import re
 from azext_rdbms_up.vendored_sdks.azure_mgmt_rdbms import mysql
 from azure.cli.core.commands import LongRunningOperation, _is_poller
 from ._client_factory import cf_mysql_firewall_rules
+from knack.log import get_logger
 
+
+logger = get_logger(__name__)
 
 def mysql_up(cmd, client, resource_group_name, server_name, sku_name=None,
              location=None, administrator_login=None, administrator_login_password=None, backup_retention=None,
@@ -34,13 +37,23 @@ def mysql_up(cmd, client, resource_group_name, server_name, sku_name=None,
         server_result = LongRunningOperation(cmd.cli_ctx, 'Starting {}'.format(cmd.name))(server_result)
 
     # check for user's ip address
-    ip_address = '0.0.0.0'
+    ip_address = None
+    # TODO get user, hostname
+    user = '{}@{}'.format(administrator_login, server_name)
+    host = server_result.fully_qualified_domain_name
     try:
-        mysql_connector.connect(user='wilx@wilxmysqlserver', host='wilxmysqlserver.mysql.database.azure.com')
+        mysql_connector.connect(user=user, host=host, password=administrator_login_password)
     except mysql_connector.errors.DatabaseError as ex:
         pattern = re.compile(r'.*IP address \'(?P<ipAddress>.*)\' is not allowed.*')
         ip_address = pattern.match(str(ex)).groupdict().get('ipAddress')
 
-    # create firewall rule
-    firewall_client = cf_mysql_firewall_rules(cmd.cli_ctx, None)
-    print(firewall_client.create_or_update(resource_group_name, server_name, 'devbox', ip_address, ip_address))
+    # create firewall rule is needed
+    if ip_address:
+        logger.warning('Creating firewall rule, \'devbox\', to allow for your ip address: %s', ip_address)
+        firewall_client = cf_mysql_firewall_rules(cmd.cli_ctx, None)
+        firewall_result = firewall_client.create_or_update(
+            resource_group_name, server_name, 'devbox', ip_address, ip_address)
+        if _is_poller(firewall_result):
+            firewall_result = LongRunningOperation(cmd.cli_ctx, 'Starting {}'.format(cmd.name))(firewall_result)
+
+    return server_result
