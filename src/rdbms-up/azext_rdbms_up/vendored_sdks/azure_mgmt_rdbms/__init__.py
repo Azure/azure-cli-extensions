b023da8ba18e903ba6e74c6185cd7ab1a40d2e8d
williexu <wx44@cornell.edu>
2019-01-18 11:43:13 -0800

    license

--- src/rdbms-up/azext_rdbms_up/vendored_sdks/azure_mgmt_rdbms/__init__.py
+++ src/rdbms-up/azext_rdbms_up/vendored_sdks/azure_mgmt_rdbms/__init__.py
@@ -1,1 +1,6 @@
-﻿__version__="0.0.1"
+﻿# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+__version__ = "0.0.1"
