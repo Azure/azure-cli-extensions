93fd20d06e76125f4d7bed71f23c7000856807fa
Ryan Holt <ryan@ryanholt.net>
2019-02-21 09:32:13 -0500

    Update custom.py

--- src/image-copy/azext_imagecopy/custom.py
+++ src/image-copy/azext_imagecopy/custom.py
@@ -96,7 +96,7 @@ def imagecopy(source_resource_group_name, source_object_name, target_location,
 
     # Start processing in the target locations
 
-    transient_resource_group_name = temporary_rg_name
+    transient_resource_group_name = temporary_resource_group_name
     # pick the first location for the temp group
     transient_resource_group_location = target_location[0].strip()
     create_resource_group(transient_resource_group_name,
