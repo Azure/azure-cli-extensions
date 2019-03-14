25eb29216dc8b390938c9ec5390e12db98acdb15
williexu <wx44@cornell.edu>
2019-03-13 17:43:16 -0700

    more progress on sql up just need to run commands using pyodbc

--- src/db-up/azext_db_up/custom.py
+++ src/db-up/azext_db_up/custom.py
@@ -11,19 +11,17 @@ from knack.log import get_logger
 from knack.util import CLIError
 import mysql.connector as mysql_connector
 import psycopg2
+import pyodbc
 from azext_db_up.vendored_sdks.azure_mgmt_rdbms import mysql, postgresql
 from azext_db_up.vendored_sdks.azure_mgmt_sql import sql
 from azext_db_up._client_factory import (
     cf_mysql_firewall_rules, cf_mysql_config, cf_mysql_db,
     cf_postgres_firewall_rules, cf_postgres_config, cf_postgres_db,
+    cf_sql_firewall_rules, cf_sql_db,
     resource_client_factory)
 from azext_db_up.util import update_kwargs, resolve_poller
 
 logger = get_logger(__name__)
-
-
-def sql_up(cmd, client):
-    pass
 
 
 def mysql_up(cmd, client, resource_group_name=None, server_name=None, location=None, backup_retention=None,
@@ -108,13 +106,13 @@ def postgres_up(cmd, client, resource_group_name=None, server_name=None, locatio
             sku_name, geo_redundant_backup, storage_mb, administrator_login, administrator_login_password, version,
             ssl_enforcement, tags)
 
-        # Set timeout configuration
-        logger.warning('Configuring wait timeout to 8 hours...')
-        config_client = cf_postgres_config(cmd.cli_ctx, None)
-        resolve_poller(
-            config_client.create_or_update(
-                resource_group_name, server_name, 'idle_in_transaction_session_timeout', '28800000'),
-            cmd.cli_ctx, 'PostgreSQL Configuration Update')
+        # # Set timeout configuration
+        # logger.warning('Configuring wait timeout to 8 hours...')
+        # config_client = cf_postgres_config(cmd.cli_ctx, None)
+        # resolve_poller(
+        #     config_client.create_or_update(
+        #         resource_group_name, server_name, 'idle_in_transaction_session_timeout', '28800000'),
+        #     cmd.cli_ctx, 'PostgreSQL Configuration Update')
 
         # Create firewall rule to allow for Azure IPs
         _create_azure_firewall_rule(db_context, cmd, resource_group_name, server_name)
@@ -131,6 +129,51 @@ def postgres_up(cmd, client, resource_group_name=None, server_name=None, locatio
     # connect to postgresql and run some commands
     if administrator_login_password is not None:
         _run_postgresql_commands(host, user, administrator_login_password, database_name)
+
+    return _form_response(
+        _create_postgresql_connection_string(host, user, administrator_login_password, database_name),
+        host, user,
+        administrator_login_password if administrator_login_password is not None else '*****'
+    )
+
+
+def sql_up(cmd, client, resource_group_name=None, server_name=None, location=None, administrator_login=None,
+           administrator_login_password=None, version=None, database_name=None, tags=None):
+    db_context = DbContext(
+        azure_sdk=sql, cf_firewall=cf_sql_firewall_rules, cf_db=cf_sql_db,
+        logging_name='SQL', connector=pyodbc, command_group='sql', server_client=client)
+
+    try:
+        server_result = client.get(resource_group_name, server_name)
+        logger.warning('Found existing PostgreSQL Server \'%s\' in group \'%s\'',
+                       server_name, resource_group_name)
+        # update server if needed
+        server_result = _update_sql_server(
+            db_context, cmd, client, server_result, resource_group_name, server_name, administrator_login_password,
+            version, tags)
+    except CloudError:
+        # Create sql server
+        if administrator_login_password is None:
+            administrator_login_password = str(uuid.uuid4())
+        server_result = _create_sql_server(
+            db_context, cmd, resource_group_name, server_name, location, administrator_login,
+            administrator_login_password, version, tags)
+
+        # Create firewall rule to allow for Azure IPs
+        _create_azure_firewall_rule(db_context, cmd, resource_group_name, server_name)
+
+    # Create sql database if it does not exist
+    _create_sql_database(db_context, cmd, resource_group_name, server_name, database_name, location)
+
+    # check ip address(es) of the user and configure firewall rules
+    sql_errors = (pyodbc.ProgrammingError)
+    host, user = _configure_sql_firewall_rules(
+        db_context, sql_errors, cmd, server_result, resource_group_name, server_name, administrator_login,
+        administrator_login_password, database_name)
+
+    # connect to sql server and run some commands
+    if administrator_login_password is not None:
+        _run_sql_commands(host, user, administrator_login_password, database_name)
 
     return _form_response(
         _create_postgresql_connection_string(host, user, administrator_login_password, database_name),
@@ -271,6 +314,26 @@ def _run_postgresql_commands(host, user, password, database):
     logger.warning("Ran Database Query: `GRANT ALL PRIVILEGES ON DATABASE %s TO root`", database)
 
 
+def _run_sql_commands(host, user, password, database):
+    # Connect to sql and get cursor to run sql commands
+    administrator_login, _ = user.split('@')
+    kwargs = {'user': administrator_login, 'server': host, 'database': database, 'password': password}
+    connection_string = ('DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};'
+                         'DATABASE={database};UID={user};PWD={password}').format(**kwargs)
+    connection = pyodbc.connect(connection_string)
+    logger.warning('Successfully Connected to PostgreSQL.')
+    cursor = connection.cursor()
+    # TODO
+    raise Exception
+    try:
+        cursor.execute("CREATE USER root WITH ENCRYPTED PASSWORD '{}'".format(database))
+        logger.warning("Ran Database Query: `CREATE USER root WITH ENCRYPTED PASSWORD '%s'`", database)
+    except psycopg2.ProgrammingError:
+        pass
+    cursor.execute("GRANT ALL PRIVILEGES ON DATABASE {} TO root".format(database))
+    logger.warning("Ran Database Query: `GRANT ALL PRIVILEGES ON DATABASE %s TO root`", database)
+
+
 def _configure_firewall_rules(
         db_context, connector_errors, cmd, server_result, resource_group_name, server_name, administrator_login,
         administrator_login_password, database_name):
@@ -284,12 +347,67 @@ def _configure_firewall_rules(
     kwargs = {'user': user, 'host': host, 'database': database_name}
     if administrator_login_password is not None:
         kwargs['password'] = administrator_login_password
-
     addresses = set()
     logger.warning('Checking your ip address...')
     for _ in range(20):
         try:
             connection = connector.connect(**kwargs)
+            connection.close()
+        except connector_errors as ex:
+            pattern = re.compile(r'.*[\'"](?P<ipAddress>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)[\'"]')
+            try:
+                addresses.add(pattern.match(str(ex)).groupdict().get('ipAddress'))
+            except AttributeError:
+                pass
+
+    # Create firewall rules for devbox if needed
+    firewall_client = cf_firewall(cmd.cli_ctx, None)
+
+    if addresses and len(addresses) == 1:
+        ip_address = addresses.pop()
+        logger.warning('Configuring server firewall rule, \'devbox\', to allow for your ip address: %s', ip_address)
+        resolve_poller(
+            firewall_client.create_or_update(resource_group_name, server_name, 'devbox', ip_address, ip_address),
+            cmd.cli_ctx, '{} Firewall Rule Create/Update'.format(logging_name))
+    elif addresses:
+        logger.warning('Detected dynamic IP address, configuring firewall rules for IP addresses encountered...')
+        logger.warning('IP Addresses: %s', ', '.join(list(addresses)))
+        firewall_results = []
+        for i, ip_address in enumerate(addresses):
+            firewall_results.append(firewall_client.create_or_update(
+                resource_group_name, server_name, 'devbox' + str(i), ip_address, ip_address))
+        for result in firewall_results:
+            resolve_poller(result, cmd.cli_ctx, '{} Firewall Rule Create/Update'.format(logging_name))
+    logger.warning('If %s server declines your IP address, please create a new firewall rule using:', logging_name)
+    logger.warning('    `az %s server firewall-rule create -g %s -s %s -n {rule_name} '
+                   '--start-ip-address {ip_address} --end-ip-address {ip_address}`',
+                   command_group, resource_group_name, server_name)
+
+    return host, user
+
+
+def _configure_sql_firewall_rules(
+        db_context, connector_errors, cmd, server_result, resource_group_name, server_name, administrator_login,
+        administrator_login_password, database_name):
+    # unpack from context
+    connector, cf_firewall, command_group, logging_name = (
+        db_context.connector, db_context.cf_firewall, db_context.command_group, db_context.logging_name)
+
+    # Check for user's ip address(es)
+    user = '{}@{}'.format(administrator_login, server_name)
+    host = server_result.fully_qualified_domain_name
+    kwargs = {'user': administrator_login, 'server': host, 'database': database_name}
+    if administrator_login_password is not None:
+        kwargs['password'] = administrator_login_password
+    else:
+        kwargs['password'] = '*****'
+    addresses = set()
+    logger.warning('Checking your ip address...')
+    for _ in range(20):
+        try:
+            connection_string = ('DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};'
+                                 'DATABASE={database};UID={user};PWD={password}').format(**kwargs)
+            connection = connector.connect(connection_string)
             connection.close()
         except connector_errors as ex:
             pattern = re.compile(r'.*[\'"](?P<ipAddress>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)[\'"]')
@@ -337,6 +455,19 @@ def _create_database(db_context, cmd, resource_group_name, server_name, database
             '{} Database Create/Update'.format(logging_name))
 
 
+def _create_sql_database(db_context, cmd, resource_group_name, server_name, database_name, location):
+    cf_db, logging_name, azure_sdk = db_context.cf_db, db_context.logging_name, db_context.azure_sdk
+    database_client = cf_db(cmd.cli_ctx, None)
+    try:
+        database_client.get(resource_group_name, server_name, database_name)
+    except CloudError:
+        logger.warning('Creating %s database \'%s\'...', logging_name, database_name)
+        params = azure_sdk.models.Database(location=location)
+        resolve_poller(
+            database_client.create_or_update(resource_group_name, server_name, database_name, params), cmd.cli_ctx,
+            '{} Database Create/Update'.format(logging_name))
+
+
 def _create_azure_firewall_rule(db_context, cmd, resource_group_name, server_name):
     # allow access to azure ip addresses
     cf_firewall, logging_name = db_context.cf_firewall, db_context.logging_name
@@ -373,6 +504,23 @@ def _create_server(db_context, cmd, resource_group_name, server_name, location, 
         '{} Server Create'.format(logging_name))
 
 
+def _create_sql_server(db_context, cmd, resource_group_name, server_name, location, administrator_login,
+                       administrator_login_password, version, tags):
+    logging_name, azure_sdk, server_client = db_context.logging_name, db_context.azure_sdk, db_context.server_client
+    logger.warning('Creating %s Server \'%s\' in group \'%s\'...', logging_name, server_name, resource_group_name)
+
+    parameters = azure_sdk.models.Server(
+        administrator_login=administrator_login,
+        administrator_login_password=administrator_login_password,
+        version=version,
+        location=location,
+        tags=tags)
+
+    return resolve_poller(
+        server_client.create_or_update(resource_group_name, server_name, parameters), cmd.cli_ctx,
+        '{} Server Create'.format(logging_name))
+
+
 def _update_server(db_context, cmd, client, server_result, resource_group_name, server_name, backup_retention,
                    geo_redundant_backup, storage_mb, administrator_login_password, version, ssl_enforcement, tags):
     # storage profile params
@@ -399,6 +547,25 @@ def _update_server(db_context, cmd, client, server_result, resource_group_name, 
     if server_update_kwargs:
         logger.warning('Updating existing %s Server \'%s\' with given arguments', logging_name, server_name)
         params = db_sdk.models.ServerUpdateParameters(**server_update_kwargs)
+        return resolve_poller(client.update(
+            resource_group_name, server_name, params), cmd.cli_ctx, '{} Server Update'.format(logging_name))
+    return server_result
+
+
+def _update_sql_server(db_context, cmd, client, server_result, resource_group_name, server_name,
+                       administrator_login_password, version, tags):
+    db_sdk, logging_name = db_context.azure_sdk, db_context.logging_name
+
+    # update params
+    server_update_kwargs = {}
+    update_kwargs(server_update_kwargs, 'administrator_login_password', administrator_login_password)
+    if version != server_result.version:
+        update_kwargs(server_update_kwargs, 'version', version)
+    update_kwargs(server_update_kwargs, 'tags', tags)
+
+    if server_update_kwargs:
+        logger.warning('Updating existing %s Server \'%s\' with given arguments', logging_name, server_name)
+        params = db_sdk.models.ServerUpdate(**server_update_kwargs)
         return resolve_poller(client.update(
             resource_group_name, server_name, params), cmd.cli_ctx, '{} Server Update'.format(logging_name))
     return server_result
