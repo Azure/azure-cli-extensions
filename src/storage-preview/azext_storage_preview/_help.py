f245655a193f7d7b5268fe1c90511121660081e2
williexu <wx44@cornell.edu>
2019-03-21 16:50:55 -0700

    history, readme, docs, help

--- src/storage-preview/azext_storage_preview/_help.py
+++ src/storage-preview/azext_storage_preview/_help.py
@@ -66,11 +66,11 @@ helps['storage azcopy blob upload'] = """
     short-summary: Upload blobs to a storage blob container using AzCopy.
     examples:
         - name: Upload a single blob to a container.
-          text: storage azcopy blob upload -c MyContainer --account-name MyStorageAccount -s "path/to/file" -d NewBlob
+          text: az storage azcopy blob upload -c MyContainer --account-name MyStorageAccount -s "path/to/file" -d NewBlob
         - name: Upload a directory to a container.
-          text: storage azcopy blob upload -c MyContainer --account-name MyStorageAccount -s "path/to/directory" --recursive
+          text: az storage azcopy blob upload -c MyContainer --account-name MyStorageAccount -s "path/to/directory" --recursive
         - name: Upload the contents of a directory to a container.
-          text: storage azcopy blob upload -c MyContainer --account-name MyStorageAccount -s "path/to/directory/*" --recursive
+          text: az storage azcopy blob upload -c MyContainer --account-name MyStorageAccount -s "path/to/directory/*" --recursive
 """
 
 helps['storage azcopy blob download'] = """
@@ -78,11 +78,11 @@ helps['storage azcopy blob download'] = """
     short-summary: Download blobs from a storage blob container using AzCopy.
     examples:
         - name: Download a single blob from a container.
-          text: storage azcopy blob download -c MyContainer --account-name MyStorageAccount -s "path/to/blob" -d "path/to/file"
+          text: az storage azcopy blob download -c MyContainer --account-name MyStorageAccount -s "path/to/blob" -d "path/to/file"
         - name: Download a virtual directory from a container.
-          text: storage azcopy blob download -c MyContainer --account-name MyStorageAccount -s "path/to/virtual_directory" -d "download/path" --recursive
+          text: az storage azcopy blob download -c MyContainer --account-name MyStorageAccount -s "path/to/virtual_directory" -d "download/path" --recursive
         - name: Download the contents of a container onto a local file system.
-          text: storage azcopy blob download -c MyContainer --account-name MyStorageAccount -s * -d "download/path" --recursive
+          text: az storage azcopy blob download -c MyContainer --account-name MyStorageAccount -s * -d "download/path" --recursive
 """
 
 helps['storage azcopy blob delete'] = """
@@ -90,11 +90,11 @@ helps['storage azcopy blob delete'] = """
     short-summary: Delete blobs from a storage blob container using AzCopy.
     examples:
         - name: Delete a single blob from a container.
-          text: storage azcopy blob delete -c MyContainer --account-name MyStorageAccount -t TargetBlob
+          text: az storage azcopy blob delete -c MyContainer --account-name MyStorageAccount -t TargetBlob
         - name: Delete all blobs from a container.
-          text: storage azcopy blob delete -c MyContainer --account-name MyStorageAccount --recursive
+          text: az storage azcopy blob delete -c MyContainer --account-name MyStorageAccount --recursive
         - name: Delete all blobs in a virtual directory.
-          text: storage azcopy blob delete -c MyContainer --account-name MyStorageAccount -t "path/to/virtual_directory" --recursive
+          text: az storage azcopy blob delete -c MyContainer --account-name MyStorageAccount -t "path/to/virtual_directory" --recursive
 """
 
 helps['storage azcopy run-command'] = """
