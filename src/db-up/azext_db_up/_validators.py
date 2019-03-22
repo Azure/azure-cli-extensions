516dd92424cf786344c85359a0bfb89231ea1cb9
Willie Xu <wx44@cornell.edu>
2019-03-22 14:49:36 -0700

    [db-up] Sql commands- up/down/show-connection-string (#594)
    * finish sql commands and testing
    
    * static check

--- src/db-up/azext_db_up/_validators.py
+++ src/db-up/azext_db_up/_validators.py
@@ -8,13 +8,13 @@ import uuid
 from six.moves import configparser
 from azure.cli.core.commands.validators import get_default_location_from_resource_group
 from azure.mgmt.resource.resources.models import ResourceGroup
-from azext_db_up._client_factory import resource_client_factory
-from azext_db_up.random_name.generate import generate_username
-from azext_db_up.util import create_random_resource_name, get_config_value, set_config_value, remove_section
 from knack.log import get_logger
 from knack.util import CLIError
 from msrestazure.azure_exceptions import CloudError
 from msrest.exceptions import ValidationError
+from azext_db_up._client_factory import resource_client_factory
+from azext_db_up.random_name.generate import generate_username
+from azext_db_up.util import create_random_resource_name, get_config_value, set_config_value, remove_section
 
 logger = get_logger(__name__)
 
