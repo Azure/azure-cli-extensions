521ebfdb33eb4742e5e30127e39ecdd29618973a
Tongyao Si <tosi@microsoft.com>
2019-04-09 11:45:21 +0800

    Add new version of aks-preview in index.json

--- src/aks-preview/setup.py
+++ src/aks-preview/setup.py
@@ -8,7 +8,7 @@
 from codecs import open as open1
 from setuptools import setup, find_packages
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
