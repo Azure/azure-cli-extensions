51e7520f9165b781743c35a1d259d30a4fc28ee5
Yugang Wang <yugangw@microsoft.com>
2019-04-17 11:46:04 -0700

    SQL: make "up" command work end to end (#644)

--- src/db-up/azext_db_up/commands.py
+++ src/db-up/azext_db_up/commands.py
@@ -47,4 +47,3 @@ def load_command_table(self, _):  # pylint: disable=too-many-locals, too-many-st
                          table_transformer=table_transform_connection_string)
         g.custom_command('down', 'server_down', validator=db_down_namespace_processor('sql'),
                          supports_no_wait=True, confirmation=True)
-        g.custom_command('show-connection-string', 'create_sql_connection_string')
