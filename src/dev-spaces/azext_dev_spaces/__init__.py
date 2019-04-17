7b6ae4f88bd6668444a66658eacd17808a0beeb6
amsoedal <amsoedal@gmail.com>
2019-04-16 21:10:41 -0700

    Remove 'preview' from Dev Spaces (#638)

--- src/dev-spaces/azext_dev_spaces/__init__.py
+++ src/dev-spaces/azext_dev_spaces/__init__.py
@@ -5,8 +5,8 @@
 
 from azure.cli.core import AzCommandsLoader
 
-import azext_dev_spaces_preview._help  # pylint: disable=unused-import
-import azext_dev_spaces_preview.custom  # pylint: disable=unused-import
+import azext_dev_spaces._help  # pylint: disable=unused-import
+import azext_dev_spaces.custom  # pylint: disable=unused-import
 
 
 class DevspacesExtCommandLoader(AzCommandsLoader):  # pylint:disable=too-few-public-methods
@@ -14,7 +14,7 @@ class DevspacesExtCommandLoader(AzCommandsLoader):  # pylint:disable=too-few-pub
     def __init__(self, cli_ctx=None):
         from azure.cli.core.commands import CliCommandType
         dev_spaces_custom = CliCommandType(
-            operations_tmpl='azext_dev_spaces_preview.custom#{}')
+            operations_tmpl='azext_dev_spaces.custom#{}')
         super(DevspacesExtCommandLoader, self).__init__(cli_ctx=cli_ctx,
                                                         custom_command_type=dev_spaces_custom,
                                                         min_profile='2017-03-10-profile')
--- src/dev-spaces/readme.md
+++ src/dev-spaces/readme.md
@@ -1,10 +1,10 @@
-# Azure CLI Dev Spaces Preview Extension #
+# Azure CLI Dev Spaces Extension #
 This is a extension for dev spaces features.
 
 ## How to use ##
 First, install the extension:
 ```
-az extension add --name dev-spaces-preview
+az extension add --name dev-spaces
 ```
 
 ```
