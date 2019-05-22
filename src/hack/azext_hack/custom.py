e3feda5cc19555433c807c5cd7e226261dd8e382
Christopher Harrison <chrhar@microsoft.com>
2019-05-22 14:47:27 -0700

    Create database, begin adding app service support

--- src/hack/azext_hack/custom.py
+++ src/hack/azext_hack/custom.py
@@ -2,21 +2,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # # --------------------------------------------------------------------------------------------
-from azure.cli.command_modules.sql._util import (
-    get_sql_management_client
-)
-from azure.cli.command_modules.sql.custom import (
-    server_create
-)
-from azure.cli.core.util import (
-    sdk_no_wait,
-)
-
 from knack.log import get_logger
 
 
 from .utils import(
-    create_resource_group
+    create_resource_group,
+    create_database,
+    create_website
 )
 
 logger = get_logger(__name__)
@@ -27,31 +19,23 @@ def hack_up(cmd, name):
     logger.warning("Creating resource group")
     create_resource_group(cmd, name, location)
     logger.warning("Created resource group")
-    # Create database
-    logger.warning("Creating SQL Server")
-    sql_client = get_sql_management_client(cmd.cli_ctx).servers
-    sql_parameters = {
-        'location': location, # "self.region" is 'west-us' by default
-        'version': '12.0',
-        'administrator_login': 'mysecretname',
-        'administrator_login_password': 'HusH_Sec4et'
-    }
+    # Create SQL server and database
+    logger.warning("Starting database creation job...")
+    database_poller = create_database(cmd, name, location)
 
-    poller = server_create(sql_client, name, name, **sql_parameters)
-    if not poller.done():
-        logger.warning('Running operation...')
-    while not poller.done():
-        pass
-    # sql_client.create_or_update(resource_group_name=name,
-    #                             server_name=name,
-    #                             parameters=sql_parameters)
-    logger.warning("Created SQL Server")
     # Create git deployment account or use existing
-    # Create app service plan w/ local git deploy
+
+    # Create app service plan and website
+    logger.warning("Starting website creation job...")
+    web_app = create_website(cmd, name, location)
+
+    while not database_poller.done():
+        pass
+    logger.warning('Done!!!')
+
     # Add app setting with connection string
     # Display information
     return 'Done!'
-
 
 def hack_down(cmd, name, dryrun=False, confirm=False):
     # confirm
