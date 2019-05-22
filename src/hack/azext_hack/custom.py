4dbbad52eb6014c71b222e1c76ba8be054dbb283
Christopher Harrison <chrhar@microsoft.com>
2019-05-21 17:21:47 -0700

    Added support for creating a database

--- src/hack/azext_hack/custom.py
+++ src/hack/azext_hack/custom.py
@@ -1,22 +1,57 @@
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
-# --------------------------------------------------------------------------------------------
+# # --------------------------------------------------------------------------------------------
+from azure.cli.command_modules.sql._util import (
+    get_sql_management_client
+)
+from azure.cli.command_modules.sql.custom import (
+    server_create
+)
+from azure.cli.core.util import (
+    sdk_no_wait,
+)
+
+from knack.log import get_logger
+
+
 from .utils import(
     create_resource_group
 )
-from knack.log import get_logger
+
 logger = get_logger(__name__)
 
-def hack_up(cmd, name, location='westus'):
+def hack_up(cmd, name):
+    location = 'westus'
     # Create RG
     logger.warning("Creating resource group")
     create_resource_group(cmd, name, location)
+    logger.warning("Created resource group")
     # Create database
+    logger.warning("Creating SQL Server")
+    sql_client = get_sql_management_client(cmd.cli_ctx).servers
+    sql_parameters = {
+        'location': location, # "self.region" is 'west-us' by default
+        'version': '12.0',
+        'administrator_login': 'mysecretname',
+        'administrator_login_password': 'HusH_Sec4et'
+    }
+
+    poller = server_create(sql_client, name, name, **sql_parameters)
+    if not poller.done():
+        logger.warning('Running operation...')
+    while not poller.done():
+        pass
+    # sql_client.create_or_update(resource_group_name=name,
+    #                             server_name=name,
+    #                             parameters=sql_parameters)
+    logger.warning("Created SQL Server")
     # Create git deployment account or use existing
     # Create app service plan w/ local git deploy
     # Add app setting with connection string
     # Display information
+    return 'Done!'
+
 
 def hack_down(cmd, name, dryrun=False, confirm=False):
     # confirm
--- src/hack/azext_hack/utils.py
+++ src/hack/azext_hack/utils.py
@@ -5,4 +5,4 @@ from azure.mgmt.resource.resources.models import ResourceGroup
 def create_resource_group(cmd, name, location):
     client = get_mgmt_service_client(cmd.cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES)
     params = ResourceGroup(location=location)
-    return client.resource_groups.create_or_update(name, params)
+    client.resource_groups.create_or_update(name, params)
