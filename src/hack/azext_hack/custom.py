b8d67280392fb49b4ac85770c520f528a1dd98d1
Christopher Harrison <chrhar@microsoft.com>
2019-05-30 17:01:59 -0700

    Working on adding MySQL support

--- src/hack/azext_hack/custom.py
+++ src/hack/azext_hack/custom.py
@@ -2,41 +2,48 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # # --------------------------------------------------------------------------------------------
+from uuid import uuid4
 from knack.log import get_logger
-
 
 from .utils import(
     create_resource_group,
     create_database,
-    create_website
+    create_website,
+    set_website_settings
 )
 
 logger = get_logger(__name__)
 
 def hack_up(cmd, name):
     location = 'westus'
+    runtime = 'Python|3.6'
+    database_admin = name + '_user'
+    database_password = uuid4()
+    output = {}
+    database_provider = 'mysql'
+
     # Create RG
     logger.warning("Creating resource group")
     create_resource_group(cmd, name, location)
     logger.warning("Created resource group")
     # Create SQL server and database
     logger.warning("Starting database creation job...")
-    database_poller = create_database(cmd, name, location)
-
-    # Create git deployment account or use existing
-
+    database_poller = create_database(cmd, database_provider, name, location, database_admin, database_password)
     # Create app service plan and website
     logger.warning("Starting website creation job...")
-    web_app = create_website(cmd, name, location)
+    output = create_website(cmd, name, runtime)
+    output['settings'] = set_website_settings(cmd, name, database_admin, database_password)
+    # Database takes a while. Wait at the end for it to complete
     while True:
         database_poller.result(15)
         if database_poller.done():
             break
-    logger.warning('Done!!!')
 
-    # Add app setting with connection string
-    # Display information
-    return 'Done!'
+    if output['deployment_password'] == '***':
+        logger.warning('Deployment user was already created. To change password use az webapp deployment user set')
+    else:
+        logger.warning('Created password for deployment user. To change use az webapp deployment user set')
+    return output
 
 def hack_down(cmd, name, dryrun=False, confirm=False):
     # confirm
