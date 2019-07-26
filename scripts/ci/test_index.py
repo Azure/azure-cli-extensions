e51daf1d37f8b0985f338f85b75067491aa31d1c
GaneshaThirumurthi <ganesha.ashoka@gmail.com>
2019-07-26 12:27:47 -0700

    fix indentation

--- scripts/ci/test_index.py
+++ scripts/ci/test_index.py
@@ -130,7 +130,7 @@ class TestIndex(unittest.TestCase):
                               "Build the extension with a different version of the 'wheel' package "
                               "(e.g. `pip install wheel==0.30.0`). "
                               "This is due to https://github.com/pypa/wheel/issues/195".format(ext_name,
-                 if metadata.get("azext.isPreview", False):
+                if metadata.get("azext.isPreview", False):
                     if metadata["azext.isPreview"] == item["metadata"]["azext.isPreview"]:
                         print("GANESHA: TWO ARE EQUAL" + metadata["azext.isPreview"] + item["metadata"]["azext.isPreview"])
                     else:
