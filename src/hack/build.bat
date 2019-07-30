4cdec578f52eed3bb2be9144caf5b5eae5f0f690
Christopher Harrison <chrhar@microsoft.com>
2019-07-30 15:38:05 -0700

    Removing personal testing rig

--- src/hack/build.bat
+++
@@ -1,1 +0,0 @@
-az group delete -n geektrainertesting -y && az extension remove -n hack && python setup.py bdist_wheel && az extension add --source .\dist\hack-0.2.17-py2.py3-none-any.whl -y && az hack up -n geektrainertesting -d CosmosDB -r php
