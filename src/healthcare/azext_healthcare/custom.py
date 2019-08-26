cadb790c24dc7677e87cd24de1fbeaafbacf3dd6
zikalino <zikalino@microsoft.com>
2019-08-26 09:24:50 +0800

    healthcare update

--- src/healthcare/azext_healthcare/custom.py
+++ src/healthcare/azext_healthcare/custom.py
@@ -71,7 +71,27 @@ def update_healthcare(cmd, client, body,
                       cors_methods=None,
                       cors_max_age=None,
                       cors_allow_credentials=None):
-    return client.create_or_update(resource_group_name=resource_group, resource_name=name)
+    service_description = client.get(resource_group_name=resource_group, service_name=service_name, api_id=api_id).as_dict()
+    service_description = {}
+    service_description['location'] = location
+    service_description['kind'] = kind
+    service_description['properties'] = {}
+    service_description['properties']['access_policies'] = []
+    for policy in access_policies_object_id.split(','):
+        service_description['properties']['access_policies'].append({'object_id': policy})
+    service_description['properties']['cors_configuration'] = {}
+    service_description['properties']['cors_configuration']['origins'] = None if cors_origins is None else cors_origins.split(',')
+    service_description['properties']['cors_configuration']['headers'] = None if cors_headers is None else cors_headers.split(',')
+    service_description['properties']['cors_configuration']['methods'] = None if cors_methods is None else cors_methods.split(',')
+    service_description['properties']['cors_configuration']['max_age'] = cors_max_age
+    service_description['properties']['cors_configuration']['allow_credentials'] = cors_allow_credentials
+    service_description['properties']['cosmos_db_configuration'] = {}
+    service_description['properties']['cosmos_db_configuration']['offer_throughput'] = cosmos_db_offer_throughput
+    service_description['authentication_configuration'] = {}
+    service_description['authentication_configuration']['authority'] = authentication_authority
+    service_description['authentication_configuration']['audience'] = authentication_audience
+    service_description['authentication_configuration']['smart_proxy_enabled'] = authentication_smart_proxy_enabled
+    return client.create_or_update(resource_group_name=resource_group, resource_name=name, service_description=service_description)
 
 
 # module equivalent: azure_rm_healthcareapisservice
