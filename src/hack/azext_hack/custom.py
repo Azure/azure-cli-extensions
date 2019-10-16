564f5a82b045bd2fe06f939eeee8321dc1428ccd
Christopher Harrison <chrhar@microsoft.com>
2019-10-16 01:04:31 -0500

    Updated functionality for hack up and added to index (#988)
    * Updated functionality for hack up and added to index
    
    * Restructured, fixed linting
    
    * Updated index.json
    
    * Cleaned up hints
    
    * Delete test_hack_scenario.py
    
    * Delete __init__.py
    
    * Delete __init__.py
    
    * fixing linting issues
    
    * Add test
    
    * resolve some index problems
    
    * fix metadata
    
    * change to microsoft just to make sure it's in sync
    
    * removed requires
    
    * another fix
    
    * fix codeowners
    
    * fixed dependencies
    
    * header
    
    * index is not updated

--- src/hack/azext_hack/custom.py
+++ src/hack/azext_hack/custom.py
@@ -3,51 +3,99 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # # --------------------------------------------------------------------------------------------
 from uuid import uuid4
+
+from azure.cli.core.commands.client_factory import get_mgmt_service_client
+from azure.cli.core.profiles import ResourceType
+from azure.mgmt.resource.resources.models import ResourceGroup
 from knack.log import get_logger
 
-from .utils import (
-    create_resource_group,
-    create_database,
-    create_website,
-    set_website_settings,
-    create_cogsvcs_key
+from ._database_utils import Database
+from ._website_utils import Website
+from ._cogsvcs_utils import (
+    create_cogsvcs_key,
+    get_cogsvcs_key
 )
 
 logger = get_logger(__name__)
 
 
-def hack_up(cmd, name, runtime, database, ai=None):
-    location = 'westus'
-    database_admin = name + '_user'
-    database_password = uuid4()
-    output = {}
-
-    # Create RG
-    logger.warning("Creating resource group")
+def create_hack(cmd, name, runtime, database, location, ai=None):
+    # TODO: Update this to use a default location, or prompt??
+    name = name + str(uuid4())[:5]
+    # # Create RG
+    logger.warning("Creating resource group...")
     create_resource_group(cmd, name, location)
-    logger.warning("Created resource group")
 
-    # Create CogSvcs key
-    # TODO: Make this async and move it to the top
     if ai:
         logger.warning('Starting creation of Cognitive Services keys...')
         create_cogsvcs_key(cmd, name, location)
 
-    # Create SQL server and database
     logger.warning("Starting database creation job...")
-    database_poller = create_database(cmd, database, name, location, database_admin, database_password)
-    # Create app service plan and website
+    database = Database(cmd, database, name, location)
+    database_poller = database.create()
+
     logger.warning("Starting website creation job...")
-    output = create_website(cmd, name, runtime)
-    # Database takes a while. Wait at the end for it to complete
+    website = Website(cmd, name, location, runtime)
+    website.create()
+
     while True:
         database_poller.result(15)
         if database_poller.done():
             break
 
-    output['settings'] = set_website_settings(cmd, name, database, database_admin, database_password, ai)
-    if output['deployment_password'] == '***':
-        logger.warning('Deployment user was already created. To change password use `az webapp deployment user set`')
+    app_settings = {
+        'DATABASE_HOST': database.host,
+        'DATABASE_NAME': database.name,
+        'DATABASE_PORT': database.port,
+        'DATABASE_USER': database.admin if database.database_type != 'mysql' else database.admin + '@' + database.name,
+        'DATABASE_PASSWORD': database.password,
+    }
+
+    if ai:
+        app_settings.update({
+            'COGSVCS_KEY': get_cogsvcs_key(cmd, name),
+            'COGSVCS_CLIENTURL': 'https://{}.api.cognitive.microsoft.com/'.format(location)
+        })
+
+    website.update_settings(app_settings)
+    website.finalize_resource_group()
+
+    output = {}
+
+    deployment_info = {
+        'Deployment url': website.deployment_url,
+        'Git login': website.deployment_user_name,
+    }
+
+    if website.deployment_user_password:
+        deployment_info.update(
+            {'Git password': website.deployment_user_password})
     else:
-        logger.warning('Created password for deployment user. To change use `az webapp deployment user set`')
+        password_info = 'Cannot retrieve password. To change, use `az webapp deployment user set --user-name {}`'
+        deployment_info.update(
+            {'Git password info': password_info.format(website.deployment_user_name)})
+
+    deployment_steps = {
+        '1- Create git repository': 'git init',
+        '2- Add all code': 'git add .',
+        '3- Commit code': 'git commit -m \'Initial commit\'',
+        '4- Add remote to git': 'git remote add azure ' + website.deployment_url,
+        '5- Deploy to Azure': 'git push azure'
+    }
+
+    output.update({'Settings and keys': {
+        'Note': 'All values are stored as environmental variables on website',
+        'Values': app_settings
+    }})
+    output.update({'Deployment info': deployment_info})
+    output.update({'Deployment steps': deployment_steps})
+    output.update({'Website url': website.host_name})
+
     return output
+
+
+def create_resource_group(cmd, name, location):
+    client = get_mgmt_service_client(
+        cmd.cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES)
+    params = ResourceGroup(location=location)
+    client.resource_groups.create_or_update(name, params)
