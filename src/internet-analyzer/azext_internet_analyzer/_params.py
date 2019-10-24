1db0384fff3e7eea833380986e0effe6afa7721b
Zim Kalinowski <zikalino@microsoft.com>
2019-10-24 22:23:48 +0800

    Update src/internet-analyzer/azext_internet_analyzer/_params.py
    Co-Authored-By: Diego Perez Botero <dibotero@microsoft.com>

--- src/internet-analyzer/azext_internet_analyzer/_params.py
+++ src/internet-analyzer/azext_internet_analyzer/_params.py
@@ -79,7 +79,7 @@ def load_arguments(self, _):
     with self.argument_context('internet-analyzer test delete') as c:
         c.argument('resource_group', resource_group_name_type)
         c.argument('profile_name', id_part=None, help='The Profile identifier associated with the Tenant and Partner')
-        c.argument('name', id_part=None, help='The Experiment identifier associated with the Experiment')
+        c.argument('name', id_part=None, help='The name of the Internet Analyzer test to delete')
 
     with self.argument_context('internet-analyzer test list') as c:
         c.argument('resource_group', resource_group_name_type)
