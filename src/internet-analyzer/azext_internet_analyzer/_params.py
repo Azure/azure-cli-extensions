33f9586830863ad43c69a177de1d05b514c80e45
Zim Kalinowski <zikalino@microsoft.com>
2019-10-24 14:25:05 +0800

    Update src/internet-analyzer/azext_internet_analyzer/_params.py
    Co-Authored-By: Diego Perez Botero <dibotero@microsoft.com>

--- src/internet-analyzer/azext_internet_analyzer/_params.py
+++ src/internet-analyzer/azext_internet_analyzer/_params.py
@@ -87,5 +87,5 @@ def load_arguments(self, _):
 
     with self.argument_context('internet-analyzer test show') as c:
         c.argument('resource_group', resource_group_name_type)
-        c.argument('profile_name', id_part=None, help='The Profile identifier associated with the Tenant and Partner')
+        c.argument('profile_name', id_part=None, help='The name of the Internet Analyzer profile under which the test exists')
         c.argument('name', id_part=None, help='The name of the Internet Analyzer test to show')
