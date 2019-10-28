5669973eabe088e863be5f1b632cd4c7297f0b86
Jianhui Harold <haroldrandom@gmail.com>
2019-10-28 20:38:01 +0800

    Remove TODO

--- src/ip-group/setup.py
+++ src/ip-group/setup.py
@@ -14,8 +14,6 @@ except ImportError:
     from distutils import log as logger
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
 
-# TODO: Confirm this is the right version number you want and it matches your
-# HISTORY.rst entry.
 VERSION = '0.1.0'
 
 # The full list of classifiers is available at
