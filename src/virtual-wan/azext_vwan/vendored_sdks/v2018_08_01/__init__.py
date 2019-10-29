1784941298318a52e11679fea87a59f02868b4e0
MyronFanQiu <49134743+MyronFanQiu@users.noreply.github.com>
2019-10-29 13:35:46 +0800

    [Network]Support router table v2 in virtual hub (#1020)
    * bump sdk version
    
    * code change
    
    * add test
    
    * fix comments
    
    * update index

--- src/virtual-wan/azext_vwan/vendored_sdks/v2018_08_01/__init__.py
+++ src/virtual-wan/azext_vwan/vendored_sdks/v2018_08_01/__init__.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+﻿# coding=utf-8
 # --------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
@@ -15,4 +15,3 @@ from .version import VERSION
 __all__ = ['NetworkManagementClient']
 
 __version__ = VERSION
-
