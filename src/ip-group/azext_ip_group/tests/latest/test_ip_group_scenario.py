51c2ba7010edf8f4ccdc23d7250dad1fbc0626a3
Jianhui Harold <haroldrandom@gmail.com>
2019-10-29 18:31:21 +0800

    Fix PEP8 error

--- src/ip-group/azext_ip_group/tests/latest/test_ip_group_scenario.py
+++ src/ip-group/azext_ip_group/tests/latest/test_ip_group_scenario.py
@@ -27,13 +27,10 @@ class IpGroupScenarioTest(ScenarioTest):
             '--ip-addresses 10.0.0.1 10.0.0.2 --location "westus"',
             checks=[self.check('tags.foo', 'doo'), self.check('name', '{name}')]
         )
-        self.cmd('network ip-group update -g {rg} -n {name} --tags foo=boo '
-                 '--ip-addresses 10.0.1.0',
-                 checks=[
-                     self.check('tags.foo', 'boo'),
-                     self.check('ipAddresses', "['10.0.1.0']")
-                 ]
-        )
+        self.cmd('network ip-group update -g {rg} -n {name} --tags foo=boo --ip-addresses 10.0.1.0', checks=[
+            self.check('tags.foo', 'boo'),
+            self.check('ipAddresses', "['10.0.1.0']")
+        ])
         count = len(self.cmd('network ip-group list').get_output_in_json())
         self.cmd('network ip-group show -g {rg} -n {name}', checks=[
             self.check('name', '{name}'),
