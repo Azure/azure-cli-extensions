de65ad508a8efaf447c81df77e3127041c3bd082
Zim Kalinowski <zikalino@microsoft.com>
2019-10-29 12:28:47 +0800

    updated versoin

--- src/managed-network/azext_managed_network/vendored_sdks/managednetwork/operations/_managed_network_groups_operations.py
+++ src/managed-network/azext_managed_network/vendored_sdks/managednetwork/operations/_managed_network_groups_operations.py
@@ -26,7 +26,7 @@ class ManagedNetworkGroupsOperations(object):
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
-    :ivar api_version: Client Api Version. Constant value: "2019-06-01-preview".
+    :ivar api_version: Client Api Version. Constant value: "2019-06-01".
     """
 
     models = models
@@ -36,7 +36,7 @@ class ManagedNetworkGroupsOperations(object):
         self._client = client
         self._serialize = serializer
         self._deserialize = deserializer
-        self.api_version = "2019-06-01-preview"
+        self.api_version = "2019-06-01"
 
         self.config = config
 
