b0192f74d9618c9353c0611db6988f1aefec2cfd
Fareha Rashid <farehar@ntdev.microsoft.com>
2019-10-29 20:54:21 -0700

    update name

--- src/connectedmachine/azext_connectedmachine/_params.py
+++ src/connectedmachine/azext_connectedmachine/_params.py
@@ -12,7 +12,7 @@ def load_arguments(self, _):
     from azure.cli.core.commands.parameters import tags_type
     from azure.cli.core.commands.validators import get_default_location_from_resource_group
 
-    name_type = CLIArgumentType(options_list='--name-name', help='Name of the Connected machine.', id_part='name')
+    name_type = CLIArgumentType(options_list='--name', help='Name of the Connected machine.', id_part='name')
 
     with self.argument_context('connectedmachine') as c:
         c.argument('tags', tags_type)
