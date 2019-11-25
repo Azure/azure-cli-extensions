93f389c1530482d1b9cae422af750b67c53ff0b5
Zunli Hu <zuh@microsoft.com>
2019-11-25 23:26:28 +0800

    pass test_storage_adls_blob_move

--- src/storage-preview/azext_storage_preview/tests/latest/test_storage_adls_scenarios.py
+++ src/storage-preview/azext_storage_preview/tests/latest/test_storage_adls_scenarios.py
@@ -39,70 +39,6 @@ class StorageADLSTests(StorageScenarioMixin, LiveScenarioTest):
         self.storage_cmd('storage blob directory show -c {} -d {} ', account_info, container, directory) \
             .assert_with_checks(JMESPathCheck('metadata.hdi_isfolder', "true"))
 
-        # Upload a single blob to the blob directory
-        self.storage_cmd('storage blob directory upload -c {} -d {} -s "{}"', account_info, container, directory,
-                         os.path.join(test_dir, 'readme'))
-        self.storage_cmd('storage blob directory list -c {} -d {}', account_info, container, directory) \
-            .assert_with_checks(JMESPathCheck('length(@)', 1))
-
-        # Upload a local directory to the blob directory
-        self.storage_cmd('storage blob directory upload -c {} -d {} -s "{}" --recursive', account_info, container,
-                         directory, os.path.join(test_dir, 'apple'))
-        self.storage_cmd('storage blob directory list -c {} -d {}', account_info, container, directory) \
-            .assert_with_checks(JMESPathCheck('length(@)', 12))
-
-        # Upload files in a local directory to the blob directory
-        self.storage_cmd('storage blob directory upload -c {} -d {} -s "{}" --recursive', account_info, container,
-                         directory, os.path.join(test_dir, 'butter/file_*'))
-        self.storage_cmd('storage blob directory list -c {} -d {}', account_info, container, directory) \
-            .assert_with_checks(JMESPathCheck('length(@)', 22))
-
-        local_folder = self.create_temp_dir()
-        # Download a single file
-        self.storage_cmd('storage blob directory download -c {} -s "{}" -d "{}" --recursive', account_info, container,
-                         os.path.join(directory, 'readme'), local_folder)
-        self.assertEqual(1, sum(len(f) for r, d, f in os.walk(local_folder)))
-
-        # Download entire directory
-        self.storage_cmd('storage blob directory download -c {} -s {} -d "{}" --recursive', account_info, container,
-                         directory, local_folder)
-        self.assertEqual(2, sum(len(d) for r, d, f in os.walk(local_folder)))
-        self.assertEqual(22, sum(len(f) for r, d, f in os.walk(local_folder)))
-
-        # Download an entire subdirectory of a storage blob directory.
-        self.storage_cmd('storage blob directory download -c {} -s {} -d "{}" --recursive', account_info, container,
-                         '/'.join([directory, 'apple']), local_folder)
-        self.assertEqual(3, sum(len(d) for r, d, f in os.walk(local_folder)))
-        self.assertEqual(32, sum(len(f) for r, d, f in os.walk(local_folder)))
-
-        # Move the blob directory to another directory
-        des_directory = 'desdirectory'
-        self.storage_cmd('storage blob directory exists -c {} -d {} ', account_info, container, des_directory) \
-            .assert_with_checks(JMESPathCheck('exists', False))
-        self.storage_cmd('storage blob directory move -c {} -d {} -s {}', account_info,
-                         container, des_directory, directory)
-        self.storage_cmd('storage blob directory exists -c {} -d {} ', account_info, container, directory) \
-            .assert_with_checks(JMESPathCheck('exists', False))
-        self.storage_cmd('storage blob directory exists -c {} -d {} ', account_info, container, des_directory) \
-            .assert_with_checks(JMESPathCheck('exists', True))
-        self.storage_cmd('storage blob directory list -c {} -d {}', account_info, container, des_directory) \
-            .assert_with_checks(JMESPathCheck('length(@)', 22))
-        with self.assertRaises(ValueError):
-            self.storage_cmd('storage blob directory move -c {} -d {} -s {}', account_info,
-                             container, des_directory, '/'.join([des_directory, 'readme']))
-
-        blob = 'readme'
-        # Move a blob in a container
-        self.storage_cmd('storage blob move -c {} -d {} -s {}', account_info,
-                         container, blob, '/'.join([des_directory, 'readme']))
-        self.storage_cmd('storage blob directory list -c {} -d {}', account_info, container, des_directory) \
-            .assert_with_checks(JMESPathCheck('length(@)', 21))
-        self.storage_cmd('storage blob exists -c {} -n {} ', account_info, container, blob) \
-            .assert_with_checks(JMESPathCheck('exists', True))
-        with self.assertRaises(ValueError):
-            self.storage_cmd('storage blob move -c {} -d {} -s {}', account_info,
-                             container, blob, des_directory)
-
         # Storage blob access control
         acl = "user::rwx,group::r--,other::---"
         self.storage_cmd('storage blob access set -c {} -b {} -a "{}"', account_info, container, blob, acl)
@@ -115,24 +51,24 @@ class StorageADLSTests(StorageScenarioMixin, LiveScenarioTest):
 
         # Storage blob directory access control
         acl = "user::rwx,group::r--,other::---"
-        self.storage_cmd('storage blob directory access set -c {} -d {} -a "{}"', account_info, container, des_directory, acl)
-        self.storage_cmd('storage blob directory access show -c {} -d {}', account_info, container, des_directory) \
+        self.storage_cmd('storage blob directory access set -c {} -d {} -a "{}"', account_info, container, directory, acl)
+        self.storage_cmd('storage blob directory access show -c {} -d {}', account_info, container, directory) \
             .assert_with_checks(JMESPathCheck('acl', acl))
         self.storage_cmd('storage blob directory access update -c {} -d {} --permissions "rwxrwxrwx"', account_info,
-                         container, des_directory, acl)
+                         container, directory, acl)
         self.storage_cmd('storage blob directory access show -c {} -d {}', account_info, container,
-                         des_directory).assert_with_checks(JMESPathCheck('permissions', "rwxrwxrwx"))
+                         directory).assert_with_checks(JMESPathCheck('permissions', "rwxrwxrwx"))
 
         # Storage blob directory metadata
         self.storage_cmd('storage blob directory metadata update -c {} -d {} --metadata "tag1=value1"', account_info,
-                         container, des_directory)
-        self.storage_cmd('storage blob directory metadata show -c {} -d {} ', account_info, container, des_directory) \
+                         container, directory)
+        self.storage_cmd('storage blob directory metadata show -c {} -d {} ', account_info, container, directory) \
             .assert_with_checks(JMESPathCheck('tag1', "value1"))
 
         # Remove blob directory
         self.storage_cmd('storage blob directory delete -c {} -d {} --recursive', account_info,
-                         container, des_directory, directory)
-        self.storage_cmd('storage blob directory exists -c {} -d {}', account_info, container, des_directory) \
+                         container, directory, directory)
+        self.storage_cmd('storage blob directory exists -c {} -d {}', account_info, container, directory) \
             .assert_with_checks(JMESPathCheck('exists', False))
 
     @api_version_constraint(CUSTOM_MGMT_STORAGE, min_api='2018-02-01')
@@ -203,6 +139,46 @@ class StorageADLSTests(StorageScenarioMixin, LiveScenarioTest):
         with self.assertRaises(SystemExit):
             self.cmd('storage blob directory move -c {} -d {} -s {} --account-name {}'.format(
                 container, directory4, '/'.join([directory3, 'readme']), storage_account))
+
+    @api_version_constraint(CUSTOM_MGMT_STORAGE, min_api='2018-02-01')
+    @StorageTestFilesPreparer()
+    @ResourceGroupPreparer()
+    def test_storage_adls_blob_move(self, resource_group, test_dir):
+        storage_account = self.create_random_name(prefix='clitestaldsaccount', length=24)
+        self.kwargs.update({
+            'sc': storage_account,
+            'rg': resource_group
+        })
+        self.cmd('storage account create -n {sc} -g {rg} --kind StorageV2 --hierarchical-namespace true')
+        account_info = self.get_account_info(resource_group, storage_account)
+        container = self.create_container(account_info)
+        directory = 'dir'
+        des_directory = 'dir1'
+        blob = 'readme'
+
+        self.storage_cmd('storage blob directory create -c {} -d {}', account_info, container, directory)
+        self.storage_cmd('storage blob directory upload -c {} -d {} -s "{}"', account_info, container, directory,
+                         os.path.join(test_dir, 'readme'))
+        self.storage_cmd('storage blob directory create -c {} -d {}', account_info, container, des_directory)
+
+        # Move a blob between different directory in a container
+        self.storage_cmd('storage blob move -c {} -d {} -s {}', account_info,
+                         container, '/'.join([des_directory, 'readme']), '/'.join([directory, 'readme']))
+        self.storage_cmd('storage blob directory list -c {} -d {}', account_info, container, des_directory) \
+            .assert_with_checks(JMESPathCheck('length(@)', 1))
+        self.storage_cmd('storage blob directory list -c {} -d {}', account_info, container, directory) \
+            .assert_with_checks(JMESPathCheck('length(@)', 0))
+
+        # Move a blob in a directory
+        new_blob = 'readme2'
+        self.storage_cmd('storage blob move -c {} -d {} -s {}', account_info,
+                         container, '/'.join([des_directory, new_blob]), '/'.join([des_directory, blob]))
+        self.storage_cmd('storage blob directory list -c {} -d {}', account_info, container, des_directory) \
+            .assert_with_checks(JMESPathCheck('[0].name', '/'.join([des_directory,new_blob])))
+
+        with self.assertRaises(SystemExit):
+            self.storage_cmd('storage blob move -c {} -d {} -s {}', account_info,
+                             container, blob, des_directory)
 
     @api_version_constraint(CUSTOM_MGMT_STORAGE, min_api='2018-02-01')
     @StorageTestFilesPreparer()
