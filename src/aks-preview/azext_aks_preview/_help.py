7cc8d5809a4596612d4db3708b5e0a366f3bd18f
Marwan Ahmed <marwanad@microsoft.com>
2020-02-11 15:48:51 -0800

    add help fields for cluster-autoscaler-profile

--- src/aks-preview/azext_aks_preview/_help.py
+++ src/aks-preview/azext_aks_preview/_help.py
@@ -179,6 +179,9 @@ helps['aks create'] = """
         - name: --max-count
           type: int
           short-summary: Maximum nodes count used for autoscaler, when "--enable-cluster-autoscaler" specified. Please specifying the value in the range of [1, 100].
+        - name: --cluster-autoscaler-profile
+          type: list
+          short-summary: Space-separated list of key=value pairs for configuring cluster autoscaler.
         - name: --vm-set-type
           type: string
           short-summary: Agent pool vm set type. VirtualMachineScaleSets or AvailabilitySet.
@@ -296,6 +299,12 @@ helps['aks update'] = """
         - name: --max-count
           type: int
           short-summary: Maximum nodes count used for autoscaler, when "--enable-cluster-autoscaler" specified. Please specifying the value in the range of [1, 100]
+        - name: --cluster-autoscaler-profile
+          type: list
+          short-summary: Space-separated list of key=value pairs for configuring cluster autoscaler.
+        - name: --api-server-authorized-ip-ranges
+          type: str
+          short-summary: List of authorized IP ranges (separated by comma) for apiserver. Set to "" for disabling it.
         - name: --load-balancer-managed-outbound-ip-count
           type: int
           short-summary: Load balancer managed outbound IP count.
