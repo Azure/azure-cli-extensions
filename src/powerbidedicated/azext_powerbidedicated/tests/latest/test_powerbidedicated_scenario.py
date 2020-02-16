94cd37a7c92901dbaba36562fd288ef5cfbcadc8
Zunli Hu <zuh@microsoft.com>
2020-02-16 15:31:51 +0800

    refine test

--- src/powerbidedicated/azext_powerbidedicated/tests/latest/test_powerbidedicated_scenario.py
+++ src/powerbidedicated/azext_powerbidedicated/tests/latest/test_powerbidedicated_scenario.py
@@ -3,65 +3,57 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import os
-import unittest
-
-from azure_devtools.scenario_tests import AllowLargeResponse
 from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer)
-
-
-TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
 
 class PowerBIDedicatedScenarioTest(ScenarioTest):
 
     @ResourceGroupPreparer(name_prefix='cli_test_powerbidedicated')
     def test_powerbidedicated(self, resource_group):
+        self.kwargs.update({
+            'name': self.create_random_name(prefix='cli_powerbi', length=24),
+        })
 
         self.cmd('az powerbi embedded-capacity create '
                  '--resource-group {rg} '
-                 '--name "azsdktest" '
+                 '--name {name} '
                  '--sku-name "A1" '
                  '--sku-tier "PBIE_Azure" '
                  '--administration-members "azsdktest@microsoft.com,azsdktest2@microsoft.com"',
-                 checks=[])
+                 checks=[
+                     self.check('provisioningState', 'Succeeded'),
+                     self.check('name', '{name}'),
+                     self.check('sku.name', 'A1'),
+                     self.check('sku.tier', 'PBIE_Azure'),
+                     self.check('administration.members[0]', 'PBIE_Azure'),
+                 ])
 
-        self.cmd('az powerbi embedded-capacity list-skus-for-capacity '
-                 '--resource-group {rg} '
-                 '--name "azsdktest"',
-                 checks=[])
+        self.cmd('az powerbi embedded-capacity list --resource-group {rg}',
+                 checks=[
+                     self.check('length(@)', 1),
+                 ])
 
-        self.cmd('az powerbi embedded-capacity list',
-                 checks=[])
-
-        self.cmd('az powerbi embedded-capacity list '
-                 '--resource-group {rg}',
-                 checks=[])
-
-        self.cmd('az powerbi embedded-capacity list',
-                 checks=[])
-
-        self.cmd('az powerbi embedded-capacity list-skus',
-                 checks=[])
-
-        self.cmd('az powerbi embedded-capacity suspend '
-                 '--resource-group {rg} '
-                 '--name "azsdktest"',
-                 checks=[])
-
-        self.cmd('az powerbi embedded-capacity list',
-                 checks=[])
+        self.cmd('az powerbi embedded-capacity show',
+                 checks=[
+                     self.check('provisioningState', 'Succeeded'),
+                     self.check('name', '{name}'),
+                     self.check('sku.name', 'A1'),
+                     self.check('sku.tier', 'PBIE_Azure'),
+                     self.check('administration.members[0]', 'PBIE_Azure'),
+                 ])
 
         self.cmd('az powerbi embedded-capacity update '
                  '--resource-group {rg} '
-                 '--name "azsdktest" '
+                 '--name {name} '
                  '--sku-name "A1" '
                  '--sku-tier "PBIE_Azure" '
                  '--administration-members "azsdktest@microsoft.com,azsdktest2@microsoft.com"',
                  checks=[])
 
-        self.cmd('az powerbi embedded-capacity check-name-availability '
-                 '--type "Microsoft.PowerBIDedicated/capacities"',
+        self.cmd('az powerbi embedded-capacity show',
+                 checks=[])
+
+        self.cmd('az powerbi embedded-capacity delete',
                  checks=[])
 
         self.cmd('az powerbi embedded-capacity list',
