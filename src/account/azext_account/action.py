f88cccb7e335eb063e197908e81a3c4ef9bb1d19
navysingla <47647464+navysingla@users.noreply.github.com>
2020-02-20 19:20:07 -0800

    Update action.py

--- src/account/azext_account/action.py
+++ src/account/azext_account/action.py
@@ -1,14 +1,10 @@
-import argparse
-from knack.util import CLIError
-
-
 # pylint: disable=protected-access
 
 
 class AddOwners(argparse._AppendAction):
-    def __call__(self, parser, namespace, values, option_string=None):
+    def __call__():
        return
 
-    def get_action(self, values, option_string):  # pylint: disable=no-self-use
+    def get_action():  # pylint: disable=no-self-use
         d = {}
         return d
