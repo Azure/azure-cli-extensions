e429702e3b7b602b67a1736e0d096bd1e08c3c66
navysingla <47647464+navysingla@users.noreply.github.com>
2020-02-20 19:12:03 -0800

    Update action.py

--- src/account/azext_account/action.py
+++ src/account/azext_account/action.py
@@ -7,12 +7,8 @@ from knack.util import CLIError
 
 class AddOwners(argparse._AppendAction):
     def __call__(self, parser, namespace, values, option_string=None):
-        action = self.get_action(values, option_string)
+       return
 
     def get_action(self, values, option_string):  # pylint: disable=no-self-use
-        try:
-            properties = dict(x.split('=', 1) for x in values)
-        except ValueError:
-            raise CLIError('usage error: {} [KEY=VALUE ...]'.format(option_string))
         d = {}
         return d
