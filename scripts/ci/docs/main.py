8b7ae4383647a96dc6b4bf25e49d617f38bdc186
Jianhui Harold <haroldrandom@gmail.com>
2020-02-24 02:31:19 +0800

    refine logic to do update azure-cli-extensions-list.md only

--- scripts/ci/docs/main.py
+++ scripts/ci/docs/main.py
@@ -4,6 +4,10 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+"""
+This script must be run at the root of repo folder, which is azure-cli-extensions/
+"""
+
 import os
 import sys
 import logging
@@ -14,27 +18,21 @@ from pkg_resources import parse_version
 
 from jinja2 import Template  # pylint: disable=import-error
 
-from git import Repo
-from github import Github
-
-
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-GH_TOKEN = os.environ.get('GH_TOKEN')
-ADO_BUILD_ID = os.environ.get('ADO_BUILD_ID')
-ADO_REPO_SLUG = os.environ.get('ADO_REPO_SLUG')
-ADO_COMMIT = os.environ.get('ADO_COMMIT')
-DOC_REPO_SLUG = os.environ.get('DOC_REPO_SLUG')
-SCRIPTS_LOCATION = os.path.join(REPO_LOCATION, 'scripts')
+AZURE_CLI_DOCS_REPO_PATH = './azure-cli-docs'
 
-REPO_CLI_DOCS = 'https://{}@github.com/{}'.format(GH_TOKEN, DOC_REPO_SLUG)
-CLONED_CLI_DOCS = os.path.join(os.path.sep, 'doc-repo')
+SCRIPTS_LOCATION = os.path.abspath(os.path.join('.', 'scripts'))
+
+CLONED_CLI_DOCS = os.path.join('..', 'azure-docs-cli')
+print('CLONED_CLI_DOCS =', CLONED_CLI_DOCS)
 AVAILABLE_EXTENSIONS_DOC = os.path.join(CLONED_CLI_DOCS, 'docs-ref-conceptual', 'azure-cli-extensions-list.md')
-TEMPLATE_FILE = os.path.join(SCRIPTS_LOCATION, "avail-ext-doc", "list-template.md")
+print('AVAILABLE_EXTENSIONS_DOC =', AVAILABLE_EXTENSIONS_DOC)
+TEMPLATE_FILE = os.path.join(SCRIPTS_LOCATION, "ci", "docs", "list-template.md")
 
 sys.path.insert(0, SCRIPTS_LOCATION)
-from util import get_index_data, INDEX_PATH
+from ci.util import get_index_data, INDEX_PATH
 
 
 def get_extensions():
@@ -54,7 +52,6 @@ def get_extensions():
 
 
 def update_extensions_list(output_file):
-    template = None
     with open(TEMPLATE_FILE, 'r') as doc_template:
         template = Template(doc_template.read())
     if template is None:
@@ -63,47 +60,8 @@ def update_extensions_list(output_file):
         output.write(template.render(extensions=get_extensions(), date=datetime.date.today().strftime("%m/%d/%Y")))
 
 
-def needs_update(doc_repo):
-    date_format = "%Y-%m-%d %H:%M:%S %z"
-    ext_repo = Repo('.')
-    doc_updated = datetime.datetime.strptime(doc_repo.git.log(AVAILABLE_EXTENSIONS_DOC, pretty="format:%ai", n=1), date_format)
-    template_updated = datetime.datetime.strptime(ext_repo.git.log(TEMPLATE_FILE, pretty="format:%ai", n=1), date_format)
-    index_updated = datetime.datetime.strptime(ext_repo.git.log(INDEX_PATH, pretty="format:%ai", n=1), date_format)
-
-    return (doc_updated < index_updated) or (doc_updated < template_updated)
-
-
-def commit_update(doc_repo):
-    doc_repo.git.add(doc_repo.working_tree_dir)
-    github_con = Github(GH_TOKEN)
-    user = github_con.get_user()
-    doc_repo.git.config('user.email', user.email or 'azpycli@microsoft.com')
-    doc_repo.git.config('user.name', user.name)
-    logger.info('Cloned %s', CLONED_CLI_DOCS)
-    branch_name = 'az-ext-list-ci-{}'.format(ADO_BUILD_ID)
-    local_branch = doc_repo.create_head(branch_name)
-    local_branch.checkout()
-    commit_url = 'https://github.com/{}/commit/{}'.format(ADO_REPO_SLUG, ADO_COMMIT)
-    commit_msg = 'Update CLI extensions available doc.\n Triggered by {} - ' \
-                 'ADO_BUILD_ID={}\n{}'.format(ADO_REPO_SLUG, ADO_BUILD_ID, commit_url)
-    doc_repo.index.commit(commit_msg)
-    doc_repo.git.push('origin', local_branch.name, set_upstream=True)
-    gh_repo = github_con.get_repo(DOC_REPO_SLUG)
-    gh_repo.create_pull(
-        title='Update CLI extensions available doc',
-        body=commit_msg,
-        head=local_branch.name,
-        base='master')
-
-
 def main():
-    # doc_repo = Repo.clone_from(REPO_CLI_DOCS, CLONED_CLI_DOCS)
-    # if needs_update(doc_repo):
-    #     update_extensions_list(AVAILABLE_EXTENSIONS_DOC)
-    #     commit_update(doc_repo)
-    # else:
-    #     logger.info("No update to docs index required, skipping publish")
-    pass
+    update_extensions_list(AVAILABLE_EXTENSIONS_DOC)
 
 
 if __name__ == '__main__':
