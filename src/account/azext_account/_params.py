ee2f5c9fcbd36c27d6c282953b4595672cad76c7
Zim Kalinowski <zikalino@microsoft.com>
2020-02-23 09:47:38 +0100

    regenerated

--- src/account/azext_account/_params.py
+++ src/account/azext_account/_params.py
@@ -35,10 +35,10 @@ def load_arguments(self, _):
     with self.argument_context('account subscriptions enable') as c:
         pass
 
-    with self.argument_context('account subscription_operation show') as c:
+    with self.argument_context('account subscription-operation show') as c:
         c.argument('operation_id', id_part=None, help='The operation ID, which can be found from the Location field in the generate recommendation response header.')
 
-    with self.argument_context('account subscription_factory create_csp_subscription') as c:
+    with self.argument_context('account subscription-factory create-csp-subscription') as c:
         c.argument('billing_account_name', id_part=None, help='The name of the Microsoft Customer Agreement billing account for which you want to create the subscription.')
         c.argument('customer_name', id_part=None, help='The name of the customer.')
         c.argument('display_name', id_part=None, help='The friendly name of the subscription.')
@@ -46,20 +46,20 @@ def load_arguments(self, _):
         c.argument('reseller_id', id_part=None, help='Reseller ID, basically MPN Id.')
         c.argument('service_provider_id', id_part=None, help='Service provider ID, basically MPN Id.')
 
-    with self.argument_context('account subscription_factory create_subscription_in_enrollment_account') as c:
+    with self.argument_context('account subscription-factory create-subscription-in-enrollment-account') as c:
         c.argument('enrollment_account_name', id_part=None, help='The name of the enrollment account to which the subscription will be billed.')
         c.argument('display_name', id_part=None, help='The display name of the subscription.')
         c.argument('owners', id_part=None, help='The list of principals that should be granted Owner access on the subscription. Principals should be of type User, Service Principal or Security Group.', action=AddOwners, nargs='+')
         c.argument('offer_type', arg_type=get_enum_type(['MS-AZR-0017P', 'MS-AZR-0148P']), id_part=None, help='The offer type of the subscription. For example, MS-AZR-0017P (EnterpriseAgreement) and MS-AZR-0148P (EnterpriseAgreement devTest) are available. Only valid when creating a subscription in a enrollment account scope.')
         c.argument('additional_parameters', id_part=None, help='Additional, untyped parameters to support custom subscription creation scenarios.', nargs='+')
 
-    with self.argument_context('account subscription_factory create_subscription') as c:
+    with self.argument_context('account subscription-factory create-subscription') as c:
         c.argument('billing_account_name', id_part=None, help='The name of the Microsoft Customer Agreement billing account for which you want to create the subscription.')
         c.argument('billing_profile_name', id_part=None, help='The name of the billing profile in the billing account for which you want to create the subscription.')
         c.argument('invoice_section_name', id_part=None, help='The name of the invoice section in the billing account for which you want to create the subscription.')
         c.argument('body', id_part=None, help='The subscription creation parameters.', action=AddBody, nargs='+')
 
-    with self.argument_context('account subscription_operations list') as c:
+    with self.argument_context('account subscription-operations list') as c:
         pass
 
     with self.argument_context('account operations list') as c:
