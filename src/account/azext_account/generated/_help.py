de5ce4331960b2f4b7be22a76bd18745daaf48b8
Zim Kalinowski <zikalino@microsoft.com>
2020-02-24 20:02:20 +0100

    examples & test fixes

--- src/account/azext_account/generated/_help.py
+++ src/account/azext_account/generated/_help.py
@@ -27,16 +27,28 @@ helps['account subscriptions show'] = """
 helps['account subscriptions rename'] = """
     type: command
     short-summary: The operation to rename a subscription
+    examples:
+      - name: renameSubscription
+        text: |-
+               az account subscriptions rename
 """
 
 helps['account subscriptions cancel'] = """
     type: command
     short-summary: The operation to cancel a subscription
+    examples:
+      - name: cancelSubscription
+        text: |-
+               az account subscriptions cancel
 """
 
 helps['account subscriptions enable'] = """
     type: command
     short-summary: The operation to enable a subscription
+    examples:
+      - name: enableSubscription
+        text: |-
+               az account subscriptions enable
 """
 
 helps['account subscription-operation'] = """
@@ -47,6 +59,11 @@ helps['account subscription-operation'] = """
 helps['account subscription-operation show'] = """
     type: command
     short-summary: Get the status of the pending Microsoft.Subscription API operations.
+    examples:
+      - name: getPendingSubscriptionOperations
+        text: |-
+               az account subscription-operation show --operation-id \\
+               "e4b8d068-f574-462a-a76f-6fa0afc613c9"
 """
 
 helps['account subscription-factory'] = """
@@ -57,16 +74,35 @@ helps['account subscription-factory'] = """
 helps['account subscription-factory create-csp-subscription'] = """
     type: command
     short-summary: The operation to create a new CSP subscription.
+    examples:
+      - name: createSubscription
+        text: |-
+               az account subscription-factory create-csp-subscription --billing-account-name \\
+               "2bc54a6f-8d8a-5be1-5bff-bb4f285f512b:11a72812-d9a4-446e-9a1e-70c8bcadf5c0_2019-05-31" \\
+               --display-name "Contoso MCA subscription" --sku-id "0001" --customer-name \\
+               "e33ba30d-3718-4b15-bfaa-5627a57cda6f"
 """
 
 helps['account subscription-factory create-subscription-in-enrollment-account'] = """
     type: command
     short-summary: Creates an Azure subscription
+    examples:
+      - name: createSubscription
+        text: |-
+               az account subscription-factory create-subscription-in-enrollment-account --display-name \\
+               "Test Ea Azure Sub" --offer-type "MS-AZR-0017P" --enrollment-account-name \\
+               "73f8ab6e-cfa0-42be-b886-be6e77c2980c"
 """
 
 helps['account subscription-factory create-subscription'] = """
     type: command
     short-summary: The operation to create a new WebDirect or EA Azure subscription.
+    examples:
+      - name: createSubscription
+        text: |-
+               az account subscription-factory create-subscription --billing-account-name \\
+               "0aa27f2b-ec7f-5a65-71f6-a5ff0897bd55:ae0dae1e-de9a-41f6-8257-76b055d98372_2019-05-31" \\
+               --billing-profile-name "27VR-HDWX-BG7-TGB" --invoice-section-name "JGF7-NSBG-PJA-TGB"
 """
 
 helps['account subscription-operations'] = """
@@ -77,6 +113,10 @@ helps['account subscription-operations'] = """
 helps['account subscription-operations list'] = """
     type: command
     short-summary: Lists all of the available pending Microsoft.Subscription API operations.
+    examples:
+      - name: getPendingSubscriptionOperations
+        text: |-
+               az account subscription-operations list
 """
 
 helps['account operations'] = """
@@ -87,6 +127,10 @@ helps['account operations'] = """
 helps['account operations list'] = """
     type: command
     short-summary: Lists all of the available Microsoft.Subscription API operations.
+    examples:
+      - name: getOperations
+        text: |-
+               az account operations list
 """
 
 helps['account tenants'] = """
