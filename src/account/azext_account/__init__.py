68a901e4dc8b31e0b4b92274767cb64eada1f4cf
navysingla <47647464+navysingla@users.noreply.github.com>
2020-02-26 15:27:23 -0800

    Update __init__.py

--- src/account/azext_account/__init__.py
+++ src/account/azext_account/__init__.py
@@ -5,7 +5,7 @@
 
 from azure.cli.core import AzCommandsLoader
 
-from generated._help import helps  # pylint: disable=unused-import
+from azext_account.generated._help import helps  # pylint: disable=unused-import
 
 
 class SubscriptionClientCommandsLoader(AzCommandsLoader):
