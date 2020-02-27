865e932b35505ed26dc375fc7b5acb24faf88c79
Zim Kalinowski <zikalino@microsoft.com>
2020-02-27 04:36:00 +0100

    import custom properly

---
+++ src/account/azext_account/custom.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import azext_account.generated.custom
+try:
+    import azext_account.manual.custom
+except ImportError
+    pass
