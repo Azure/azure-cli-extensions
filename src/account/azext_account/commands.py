d4ac0e86209833da47f08049c71e39cb35ed9e8b
navysingla <47647464+navysingla@users.noreply.github.com>
2020-02-28 15:03:30 -0800

    lygj

--- src/account/azext_account/commands.py
+++ src/account/azext_account/commands.py
@@ -3,7 +3,37 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-try:
-    pass
-except ImportError:
-    pass
+# pylint: disable=line-too-long
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-statements
+# pylint: disable=too-many-locals
+from azure.cli.core.commands import CliCommandType
+
+
+def load_command_table(self, _):
+
+    from ._client_factory import cf_subscription
+    account_subscription = CliCommandType(
+        operations_tmpl='azext_account.vendored_sdks.subscription.operations._subscription_operations#SubscriptionOperations.{}',
+        client_factory=cf_subscription)
+    with self.command_group('account subscription', account_subscription, client_factory=cf_subscription) as g:
+        g.custom_command('create-subscription', 'account_subscription_create_subscription')
+        g.custom_command('create-subscription-in-enrollment-account', 'account_subscription_create_subscription_in_enrollment_account')
+        g.custom_command('create-csp-subscription', 'account_subscription_create_csp_subscription')
+        g.custom_command('rename', 'account_subscription_rename')
+        g.custom_command('cancel', 'account_subscription_cancel')
+        g.custom_command('enable', 'account_subscription_enable')
+
+    from ._client_factory import cf_subscription_operation
+    account_subscription_operation = CliCommandType(
+        operations_tmpl='azext_account.vendored_sdks.subscription.operations._subscription_operation_operations#SubscriptionOperationOperations.{}',
+        client_factory=cf_subscription_operation)
+    with self.command_group('account subscription-operation', account_subscription_operation, client_factory=cf_subscription_operation) as g:
+        g.custom_show_command('show', 'account_subscription_operation_show')
+
+    from ._client_factory import cf_operations
+    account_operations = CliCommandType(
+        operations_tmpl='azext_account.vendored_sdks.subscription.operations._operations_operations#OperationsOperations.{}',
+        client_factory=cf_operations)
+    with self.command_group('account operations', account_operations, client_factory=cf_operations) as g:
+        g.custom_command('list', 'account_operations_list')
--- src/account/azext_account/custom.py
+++ src/account/azext_account/custom.py
@@ -2,8 +2,60 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+# pylint: disable=line-too-long
+# pylint: disable=too-many-statements
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=unused-argument
 
-try:
-    pass
-except ImportError:
-    pass
+
+def account_subscription_create_subscription(cmd, client,
+                                             billing_account_name,
+                                             billing_profile_name,
+                                             invoice_section_name,
+                                             body_display_name,
+                                             body_sku_id,
+                                             body_cost_center=None,
+                                             body_owner=None,
+                                             body_management_group_id=None):
+    return client.create_subscription(billing_account_name=billing_account_name, billing_profile_name=billing_profile_name, invoice_section_name=invoice_section_name, display_name=body_display_name, sku_id=body_sku_id, cost_center=body_cost_center, owner=body_owner, management_group_id=body_management_group_id)
+
+
+def account_subscription_create_subscription_in_enrollment_account(cmd, client,
+                                                                   enrollment_account_name,
+                                                                   body_display_name=None,
+                                                                   body_management_group_id=None,
+                                                                   body_owners=None,
+                                                                   body_offer_type=None):
+    return client.create_subscription_in_enrollment_account(enrollment_account_name=enrollment_account_name, display_name=body_display_name, management_group_id=body_management_group_id, owners=body_owners, offer_type=body_offer_type)
+
+
+def account_subscription_create_csp_subscription(cmd, client,
+                                                 billing_account_name,
+                                                 customer_name,
+                                                 body_display_name,
+                                                 body_sku_id,
+                                                 body_reseller_id=None):
+    return client.create_csp_subscription(billing_account_name=billing_account_name, customer_name=customer_name, display_name=body_display_name, sku_id=body_sku_id, reseller_id=body_reseller_id)
+
+
+def account_subscription_rename(cmd, client,
+                                body_subscription_name=None):
+    return client.rename(subscription_name=body_subscription_name)
+
+
+def account_subscription_cancel(cmd, client):
+    return client.cancel()
+
+
+def account_subscription_enable(cmd, client):
+    return client.enable()
+
+
+def account_subscription_operation_show(cmd, client,
+                                        operation_id):
+    return client.get(operation_id=operation_id)
+
+
+def account_operations_list(cmd, client):
+    return client.list()
