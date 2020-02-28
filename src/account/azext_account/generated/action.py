d3038ef601b58e7abb3c9a65fa9ef0f4b009a758
Zim Kalinowski <zikalino@microsoft.com>
2020-02-28 19:29:33 +0100

    action is fixed now

--- src/account/azext_account/generated/action.py
+++ src/account/azext_account/generated/action.py
@@ -10,63 +10,6 @@ from knack.util import CLIError
 # pylint: disable=protected-access
 
 
-class AddBody(argparse._AppendAction):
-    def __call__(self, parser, namespace, values, option_string=None):
-        action = self.get_action(values, option_string)
-        super(AddBody, self).__call__(parser, namespace, action, option_string)
-
-    def get_action(self, values, option_string):  # pylint: disable=no-self-use
-        try:
-            properties = dict(x.split('=', 1) for x in values)
-        except ValueError:
-            raise CLIError('usage error: {} [KEY=VALUE ...]'.format(option_string))
-        d = {}
-        for k in properties:
-            kl = k.lower()
-            v = properties[k]
-            if kl == 'xxx':
-                d['moo'] = v
-        return d
-
-
-class AddOwner(argparse._AppendAction):
-    def __call__(self, parser, namespace, values, option_string=None):
-        action = self.get_action(values, option_string)
-        super(AddOwner, self).__call__(parser, namespace, action, option_string)
-
-    def get_action(self, values, option_string):  # pylint: disable=no-self-use
-        try:
-            properties = dict(x.split('=', 1) for x in values)
-        except ValueError:
-            raise CLIError('usage error: {} [KEY=VALUE ...]'.format(option_string))
-        d = {}
-        for k in properties:
-            kl = k.lower()
-            v = properties[k]
-            if kl == 'xxx':
-                d['moo'] = v
-        return d
-
-
-class AddBody(argparse._AppendAction):
-    def __call__(self, parser, namespace, values, option_string=None):
-        action = self.get_action(values, option_string)
-        super(AddBody, self).__call__(parser, namespace, action, option_string)
-
-    def get_action(self, values, option_string):  # pylint: disable=no-self-use
-        try:
-            properties = dict(x.split('=', 1) for x in values)
-        except ValueError:
-            raise CLIError('usage error: {} [KEY=VALUE ...]'.format(option_string))
-        d = {}
-        for k in properties:
-            kl = k.lower()
-            v = properties[k]
-            if kl == 'xxx':
-                d['moo'] = v
-        return d
-
-
 class AddOwners(argparse._AppendAction):
     def __call__(self, parser, namespace, values, option_string=None):
         action = self.get_action(values, option_string)
@@ -81,42 +24,6 @@ class AddOwners(argparse._AppendAction):
         for k in properties:
             kl = k.lower()
             v = properties[k]
-        return d
-
-
-class AddBody(argparse._AppendAction):
-    def __call__(self, parser, namespace, values, option_string=None):
-        action = self.get_action(values, option_string)
-        super(AddBody, self).__call__(parser, namespace, action, option_string)
-
-    def get_action(self, values, option_string):  # pylint: disable=no-self-use
-        try:
-            properties = dict(x.split('=', 1) for x in values)
-        except ValueError:
-            raise CLIError('usage error: {} [KEY=VALUE ...]'.format(option_string))
-        d = {}
-        for k in properties:
-            kl = k.lower()
-            v = properties[k]
-            if kl == 'xxx':
-                d['moo'] = v
-        return d
-
-
-class AddBody(argparse._AppendAction):
-    def __call__(self, parser, namespace, values, option_string=None):
-        action = self.get_action(values, option_string)
-        super(AddBody, self).__call__(parser, namespace, action, option_string)
-
-    def get_action(self, values, option_string):  # pylint: disable=no-self-use
-        try:
-            properties = dict(x.split('=', 1) for x in values)
-        except ValueError:
-            raise CLIError('usage error: {} [KEY=VALUE ...]'.format(option_string))
-        d = {}
-        for k in properties:
-            kl = k.lower()
-            v = properties[k]
-            if kl == 'xxx':
-                d['moo'] = v
+            if kl == 'object_id':
+                d['object_id'] = v
         return d
