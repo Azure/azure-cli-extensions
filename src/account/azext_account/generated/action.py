aa6b323c7a43fca6cd4443cfaf33b88b3d1c5485
Zim Kalinowski <zikalino@microsoft.com>
2020-02-29 08:57:19 +0100

    fixed sanity

--- src/account/azext_account/generated/action.py
+++ src/account/azext_account/generated/action.py
@@ -22,7 +22,7 @@ class AddOwners(argparse._AppendAction):
             raise CLIError('usage error: {} [KEY=VALUE ...]'.format(option_string))
         d = {}
         for k in properties:
-            k1 = k.lower()
+            kl = k.lower()
             v = properties[k]
             if kl == 'object_id':
                 d['object_id'] = v
