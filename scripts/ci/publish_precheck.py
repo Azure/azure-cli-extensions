aa2c8020a072b1eb03a4f26cdba7e8cea6692036
Jianhui Harold <haroldrandom@gmail.com>
2020-02-29 23:19:03 +0800

    Init

---
+++ scripts/ci/publish_precheck.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+"""
+This script is used to
+"""
+
+from subprocess import check_output, check_call, CalledProcessError
+
+
+def find_modified_files_against_master_branch():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
