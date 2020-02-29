bcc8c2dc548ba584d9bcfc1571e3bd049b1806a8
Zim Kalinowski <zikalino@microsoft.com>
2020-02-29 08:32:20 +0100

    wildcard imports

--- src/account/azext_account/action.py
+++ src/account/azext_account/action.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# pylint: disable=wildcard-import
 # pylint: disable=unused-wildcard-import
 
 from azext_account.generated.action import *
