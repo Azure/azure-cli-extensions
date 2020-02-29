1c0d480447518c0b9a2312bf232c58ddf9c9f942
Zim Kalinowski <zikalino@microsoft.com>
2020-02-29 09:12:36 +0100

    two spaces

--- src/account/azext_account/action.py
+++ src/account/azext_account/action.py
@@ -6,8 +6,8 @@
 # pylint: disable=wildcard-import
 # pylint: disable=unused-wildcard-import
 
-from azext_account.generated.action import * # noqa: F403
+from azext_account.generated.action import *  # noqa: F403
 try:
-    from azext_account.manual.action import * # noqa: F403
+    from azext_account.manual.action import *  # noqa: F403
 except ImportError:
     pass
