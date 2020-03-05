06a49de69aacd36bba87842c3b6fbf85b4b4ee3a
Zim Kalinowski <zikalino@microsoft.com>
2020-03-05 18:29:19 +0100

    fixed client factory

--- src/account/azext_account/generated/_client_factory.py
+++ src/account/azext_account/generated/_client_factory.py
@@ -5,10 +5,11 @@
 
 
 def cf_account(cli_ctx, *_):
-    from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    from azure.cli.core.commands.client_factory import _get_mgmt_service_client
     from ..vendored_sdks.subscription import SubscriptionClient
-    return get_mgmt_service_client(cli_ctx, SubscriptionClient)
-
+    return _get_mgmt_service_client(cli_ctx, SubscriptionClient,
+                                    subscription_bound=False,
+                                    base_url_bound=False)[0]
 
 def cf_subscription(cli_ctx, *_):
     return cf_account(cli_ctx).subscription
