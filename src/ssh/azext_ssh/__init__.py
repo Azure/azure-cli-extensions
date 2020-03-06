20137a358f75d28378f43e4e145980371ba49c98
Ryan Rossiter <ryrossit@microsoft.com>
2020-03-06 09:06:10 -0800

    Updated help docs

--- src/ssh/azext_ssh/__init__.py
+++ src/ssh/azext_ssh/__init__.py
@@ -7,12 +7,32 @@ from azure.cli.core import AzCommandsLoader
 from knack.help_files import helps
 
 helps['ssh'] = """
+    type: group
+    short-summary: SSH into Azure VMs
+"""
+
+helps['ssh vm'] = """
     type: command
     short-summary: SSH into Azure VMs
     examples:
-        - name: Give a resource group and VM to SSH
+        - name: Give a resource group and VM to SSH to
           text: |
-            az ssh --resource-group [group] --name [vmName] : az ssh --resource-group mygroup --name myvm
+            az ssh vm --resource-group [group] --name [vmName] : az ssh vm --resource-group mygroup --name myvm
+        - name: Give the public IP of a VM to SSH to
+          text: |
+            az ssh vm --ip [ip] : az ssh vm --ip 1.2.3.4
+"""
+
+helps['ssh config'] = """
+    type: command
+    short-summary: Create an SSH config for Azure VMs which can then be imported to 3rd party SSH clients
+    examples:
+        - name: Give a resource group and VM for which to create a config, and save in a local file
+          text: |
+            az ssh config --resource-group [group] --name [vmName] --file [filename] : az ssh config --resource-group mygroup --name myvm --file ./sshconfig
+        - name: Give the public IP of a VM for which to create a config
+          text: |
+            az ssh config --ip [ip] --file [filename] : az ssh config --ip 1.2.3.4 --file ./sshconfig
 """
 
 
