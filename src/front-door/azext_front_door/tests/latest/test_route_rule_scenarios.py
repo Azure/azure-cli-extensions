c17b993fffee80fc23b57f6e777a2fdeefb97d71
Myron <fanqiu@microsoft.com>
2020-03-06 14:16:30 +0800

    address comments

--- src/front-door/azext_front_door/tests/latest/test_route_rule_scenarios.py
+++ src/front-door/azext_front_door/tests/latest/test_route_rule_scenarios.py
@@ -2,13 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-from azure.cli.testsdk import (ScenarioTest, JMESPathCheck, ResourceGroupPreparer,
-                               api_version_constraint)
-from knack.cli import CLIError
-try:
-    from azext_front_door.vendored_sdks.models.error_response_py3 import ErrorResponseException
-except (SyntaxError, ImportError):
-    from azext_front_door.vendored_sdks.models.error_response import ErrorResponseException
+from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer)
 
 
 class RouteRuleScenarioTests(ScenarioTest):
