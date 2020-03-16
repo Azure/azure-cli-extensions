df223a6fd0059425a4971a99df77182f8a89a592
qianwens <qianwens@microsoft.com>
2020-03-16 11:56:22 +0800

    [HPC-Cache] test case fix

--- src/hpc-cache/azext_hpc_cache/tests/latest/test_hpc-cache_scenario.py
+++ src/hpc-cache/azext_hpc_cache/tests/latest/test_hpc-cache_scenario.py
@@ -16,36 +16,37 @@ TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 class StorageCacheScenarioTest(ScenarioTest):
 
     @ResourceGroupPreparer(name_prefix='cli_test_hpc_cache')
-    @StorageAccountPreparer(parameter_name='clihpcteststore')
     def test_hpc_cache(self, resource_group):
         vnet_name = self.create_random_name(prefix='cli', length=24)
-        self.cmd('az network vnet create -g {rg} -n {vnet_name}')
-
+        cache_name = self.create_random_name(prefix='cli', length=24)
+        storage_name = self.create_random_name(prefix='cli', length=24)
+        self.cmd('az storage account create -n {} -g {} -l eastus --sku Standard_LRS'.format(storage_name, resource_group))
+        self.cmd('az network vnet create -g {} -n {}'.format(resource_group, vnet_name))
+        subscription_id = self.current_subscription()
         self.cmd('az hpc-cache create '
-                 '--resource-group {rg} '
-                 '--name "sc-cli-test" '
+                 '--resource-group {} '
+                 '--name {} '
                  '--location "eastus" '
                  '--cache-size-gb "3072" '
-                 '--subnet "/subscriptions/{{ subscription_id }}/resourceGroups/{rg}/providers/Microsoft.Network/virtualNetworks/{vnet_name}/subnets/default" '
-                 '--sku-name "Standard_2G"',
+                 '--subnet "/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Network/virtualNetworks/{}/subnets/default" '
+                 '--sku-name "Standard_2G"'.format(resource_group, cache_name, subscription_id, resource_group, vnet_name),
                  checks=[])
-
+        container_name = self.create_random_name(length=10)
+        self.cmd('az storage container create -n {container_name} -g {rg} --account-name {storage_name}')
         self.cmd('az hpc-cache blob-storage-target create '
-                 '--resource-group {rg} '
-                 '--cache-name "sc1" '
+                 '--resource-group {} '
+                 '--cache-name {} '
                  '--name "st1" '
-                 '--target-type "nfs3" '
-                 '--nfs3-target "10.0.44.44" '
-                 '--nfs3-usage-model "READ_HEAVY_INFREQ"',
+                 '--storage-account "/subscriptions/{}/resourceGroups/{rg}/providers/Microsoft.Storage/storageAccounts/{}" '
+                 '--container-name {} --virtual-namespace-path "/test"'.format(resource_group, cache_name,
+                                                                               subscription_id, resource_group,
+                                                                               storage_name, container_name),
                  checks=[])
 
         self.cmd('az hpc-cache storage-target show '
                  '--resource-group {rg} '
-                 '--cache-name "sc1" '
+                 '--cache-name {cache_name} '
                  '--name "st1"',
-                 checks=[])
-
-        self.cmd('az hpc-cache operation list',
                  checks=[])
 
         self.cmd('az hpc-cache show '
@@ -66,9 +67,6 @@ class StorageCacheScenarioTest(ScenarioTest):
         self.cmd('az hpc-cache skus list',
                  checks=[])
 
-        self.cmd('az hpc-cache operation list',
-                 checks=[])
-
         self.cmd('az hpc-cache upgrade-firmware '
                  '--resource-group {rg} '
                  '--name "sc1"',
@@ -76,35 +74,26 @@ class StorageCacheScenarioTest(ScenarioTest):
 
         self.cmd('az hpc-cache flush '
                  '--resource-group {rg} '
-                 '--name "sc"',
+                 '--name {cache_name}',
                  checks=[])
 
         self.cmd('az hpc-cache start '
                  '--resource-group {rg} '
-                 '--name "sc"',
+                 '--name {cache_name}',
                  checks=[])
 
         self.cmd('az hpc-cache stop '
                  '--resource-group {rg} '
-                 '--name "sc"',
+                 '--name {cache_name}',
                  checks=[])
 
-        self.cmd('az hpc-cache update '
+        self.cmd('az hpc-cache storage-target remove '
                  '--resource-group {rg} '
-                 '--name "sc1" '
-                 '--location "westus" '
-                 '--cache-size-gb "3072" '
-                 '--subnet "/subscriptions/{{ subscription_id }}/resourceGroups/{{ resource_group }}/providers/Microsoft.Network/virtualNetworks/{{ virtual_network_name }}/subnets/{{ subnet_name }}" '
-                 '--sku-name "Standard_2G"',
-                 checks=[])
-
-        self.cmd('az hpc-cache storage-target delete '
-                 '--resource-group {rg} '
-                 '--cache-name "sc1" '
+                 '--cache-name {cache_name} '
                  '--name "st1"',
                  checks=[])
 
         self.cmd('az hpc-cache delete '
                  '--resource-group {rg} '
-                 '--name "sc"',
+                 '--name {cache_name}',
                  checks=[])
