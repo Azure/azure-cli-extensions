370d5b66c23d9d1d1ef459ef6d3e62eca3cce75e
Zunli Hu <zuh@microsoft.com>
2020-03-16 10:58:18 +0800

    remove unused import

--- src/powerbidedicated/azext_powerbidedicated/_params.py
+++ src/powerbidedicated/azext_powerbidedicated/_params.py
@@ -14,7 +14,6 @@ from azure.cli.core.commands.parameters import (
     resource_group_name_type,
     get_location_type
 )
-from azure.cli.core.commands.validators import get_default_location_from_resource_group
 
 
 def load_arguments(self, _):
