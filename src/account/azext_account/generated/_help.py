a886bd578f6a2dd7fbc825fe6ce2933f5aba6aea
Zim Kalinowski <zikalino@microsoft.com>
2020-03-16 20:00:12 +0100

    fixing examples

--- src/account/azext_account/generated/_help.py
+++ src/account/azext_account/generated/_help.py
@@ -22,8 +22,8 @@ helps['account subscription create-subscription'] = """
         text: |-
                az account subscription create-subscription --billing-account-name \\
                "0aa27f2b-ec7f-5a65-71f6-a5ff0897bd55:ae0dae1e-de9a-41f6-8257-76b055d98372_2019-05-31" \\
-               --billing-profile-name "27VR-HDWX-BG7-TGB" --cost-center "135366376" --display-name \\
-               "Contoso MCA subscription" --sku-id "0001" --invoice-section-name "JGF7-NSBG-PJA-TGB"
+               --billing-profile-name "27VR-HDWX-BG7-TGB" --cost-center "135366376" --body-display-name \\
+               "Contoso MCA subscription" --body-sku-id "0001" --invoice-section-name "JGF7-NSBG-PJA-TGB"
 """
 
 helps['account subscription create-subscription-in-enrollment-account'] = """
@@ -34,7 +34,8 @@ helps['account subscription create-subscription-in-enrollment-account'] = """
         text: |-
                az account subscription create-subscription-in-enrollment-account --display-name \\
                "Test Ea Azure Sub" --offer-type "MS-AZR-0017P" --enrollment-account-name \\
-               "73f8ab6e-cfa0-42be-b886-be6e77c2980c"
+               "73f8ab6e-cfa0-42be-b886-be6e77c2980c" --body-display-name "My Subscription" \\
+               --body-sku-id sku-id
 """
 
 helps['account subscription create-csp-subscription'] = """
@@ -45,7 +46,7 @@ helps['account subscription create-csp-subscription'] = """
         text: |-
                az account subscription create-csp-subscription --billing-account-name \\
                "2bc54a6f-8d8a-5be1-5bff-bb4f285f512b:11a72812-d9a4-446e-9a1e-70c8bcadf5c0_2019-05-31" \\
-               --display-name "Contoso MCA subscription" --sku-id "0001" --customer-name \\
+               --body-display-name "Contoso MCA subscription" --body-sku-id "0001" --customer-name \\
                "e33ba30d-3718-4b15-bfaa-5627a57cda6f"
 """
 
