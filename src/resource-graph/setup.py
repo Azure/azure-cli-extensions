d31dc28a7809c2038e2de32abeabefe75325b694
MyronFanQiu <fanqiu@microsoft.com>
2020-03-17 12:39:41 +0800

    bump sdk and versions

--- src/resource-graph/setup.py
+++ src/resource-graph/setup.py
@@ -8,15 +8,13 @@
 from codecs import open
 from setuptools import setup, find_packages
 
-VERSION = "1.0.0"
+VERSION = "1.1.0"
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Intended Audience :: System Administrators',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
