9090ef23485be58233e87dde6ab082c184a00d69
Jun Sun <33297523+JunSun17@users.noreply.github.com>
2020-03-19 14:58:12 -0700

    Publish aks-preview extension. (#1421)

--- src/aks-preview/setup.py
+++ src/aks-preview/setup.py
@@ -8,7 +8,7 @@
 from codecs import open as open1
 from setuptools import setup, find_packages
 
-VERSION = "0.4.36"
+VERSION = "0.4.37"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
