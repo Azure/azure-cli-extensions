79907335609319f97580778c7fcc768ecd2b1b05
Feng Zhou <fenzhou@microsoft.com>
2020-03-19 16:03:28 +0800

    add env vars

--- scripts/ci/sync_extensions.py
+++ scripts/ci/sync_extensions.py
@@ -8,25 +8,27 @@
 import os
 import re
 import json
-from subprocess import check_output, check_call
-from pkg_resources import parse_version
+from subprocess import check_output
 
-DEFAULT_TARGET_INDEX_URL = "https://extmigrate.blob.core.windows.net/extensions/index.json"
+DEFAULT_TARGET_INDEX_URL = os.getenv('AZURE_EXTENSION_TARGET_INDEX_URL')
+STORAGE_ACCOUNT_KEY = os.getenv('AZURE_EXTENSION_TARGET_STORAGE_ACCOUNT_KEY')
+STORAGE_ACCOUNT = os.getenv('AZURE_EXTENSION_TARGET_STORAGE_ACCOUNT')
+STORAGE_CONTAINER = os.getenv('AZURE_EXTENSION_TARGET_STORAGE_CONTAINER')
 
 def _get_updated_extension_names():
     cmd = 'git --no-pager diff --diff-filter=ACMRT HEAD~1 -- src/index.json'
-    changed_content = check_output(cmd.split()).decode('utf-8')
-    changed_lines = [line.replace('+', '') for line in changed_content.splitlines() if line.startswith('+') and not line.startswith('+++')]
-    changed_lines = '\n'.join(changed_lines)
+    updated_content = check_output(cmd.split()).decode('utf-8')
+    updated_urls = [line.replace('+', '') for line in updated_content.splitlines() if line.startswith('+') and not line.startswith('+++') and 'downloadUrl' in line]
     updated_exts = []
     NAME_REGEX = r'.*/([^/]*)-\d+.\d+.\d+'
-    if not changed_lines:
+    if not updated_urls:
         return updated_exts
-    search_result = re.search(r'"downloadUrl":\s+"(.*?)"', changed_lines)
-    if search_result is not None:
-        url = search_result.group(1)
-        extension_name = re.findall(NAME_REGEX, url)[0].replace('_', '-')
-        updated_exts.append(extension_name)
+    for line in updated_urls:
+        search_result = re.search(r'"downloadUrl":\s+"(.*?)"', line)
+        if search_result is not None:
+            url = search_result.group(1)
+            extension_name = re.findall(NAME_REGEX, url)[0].replace('_', '-')
+            updated_exts.append(extension_name)
     return updated_exts
 
 def _download_file(url):
@@ -46,17 +48,11 @@ def _download_file(url):
 
 
 def main():
-    import re
-    import json
-    import os
     import shutil
     import tempfile
     from azure.storage.blob import BlockBlobService
-    storage_account_key = os.getenv('AZURE_SYNC_STORAGE_ACCOUNT_KEY')
-    storage_account = 'extmigrate'
-    storage_container = 'extensions'
-    updated_exts = _get_updated_extension_names()
 
+    updated_exts = _get_updated_extension_names()
     migrate_all = (os.getenv('AZURE_SYNC_ALL_EXTENSIONS') and os.getenv('AZURE_SYNC_ALL_EXTENSIONS').lower() == 'true')
     if not migrate_all and not updated_exts:
         print('index.json not changed. End task.')
@@ -70,13 +66,13 @@ def main():
     os.mkdir(os.path.join(temp_dir, 'target'))
     target_index_path = os.path.join(temp_dir, 'target', 'index.json')
     open(target_index_path, 'wb').write(target_index_file.content)
-    client = BlockBlobService(account_name=storage_account, account_key=storage_account_key)
+    client = BlockBlobService(account_name=STORAGE_ACCOUNT, account_key=STORAGE_ACCOUNT_KEY)
     updated_indexes = []
     if migrate_all:
         print('Syncing all extensions...\n')
         updated_exts = current_extensions.keys()
         # backup the old index.json
-        client.create_blob_from_path(container_name=storage_container, blob_name='index.json.sav',
+        client.create_blob_from_path(container_name=STORAGE_CONTAINER, blob_name='index.json.sav',
                                      file_path=os.path.abspath(target_index_path))
         inital_index = \
 """{
@@ -90,13 +86,13 @@ def main():
         print('Uploading {}'.format(extension_name))
         if migrate_all:
             for ext in current_extensions[extension_name]:
-                _migrate_wheel(ext, updated_indexes, client, True, storage_container, temp_dir)
+                _migrate_wheel(ext, updated_indexes, client, True, temp_dir)
         else:
             ext = current_extensions[extension_name][-1]
-            _migrate_wheel(ext, updated_indexes, client, True, storage_container, temp_dir)
+            _migrate_wheel(ext, updated_indexes, client, True, temp_dir)
 
     update_target_extension_index(updated_indexes, target_index_path)
-    client.create_blob_from_path(container_name=storage_container, blob_name='index.json',
+    client.create_blob_from_path(container_name=STORAGE_CONTAINER, blob_name='index.json',
                                  file_path=os.path.abspath(target_index_path))
     print("\nSync finished, extensions available in:")
     for updated_index in updated_indexes:
@@ -104,20 +100,20 @@ def main():
     shutil.rmtree(temp_dir)
 
 
-def _migrate_wheel(ext, updated_indexes, client, overwrite, storage_container, temp_dir):
+def _migrate_wheel(ext, updated_indexes, client, overwrite, temp_dir):
     download_url = ext['downloadUrl']
     response = _download_file(download_url)
     whl_file = download_url.split('/')[-1]
     whl_path = os.path.join(temp_dir, whl_file)
     open(whl_path, 'wb').write(response.content)
     if not overwrite:
-        exists = client.exists(container_name=storage_container, blob_name=whl_file)
+        exists = client.exists(container_name=STORAGE_CONTAINER, blob_name=whl_file)
         if exists:
             print("Skipping '{}' as it already exists...".format(whl_file))
             return
-    client.create_blob_from_path(container_name=storage_container, blob_name=whl_file,
+    client.create_blob_from_path(container_name=STORAGE_CONTAINER, blob_name=whl_file,
                                  file_path=os.path.abspath(whl_path))
-    url = client.make_blob_url(container_name=storage_container, blob_name=whl_file)
+    url = client.make_blob_url(container_name=STORAGE_CONTAINER, blob_name=whl_file)
     updated_index = ext
     updated_index['downloadUrl'] = url
     updated_indexes.append(updated_index)
@@ -131,15 +127,13 @@ def update_target_extension_index(updated_indexes, target_index_path):
         curr_index = json.loads(infile.read())
     for entry in updated_indexes:
         url = entry['downloadUrl']
-        extension_name = re.findall(NAME_REGEX, url)[0]
-        extension_name = extension_name.replace('_', '-')
-
+        extension_name = re.findall(NAME_REGEX, url)[0].replace('_', '-')
         if extension_name not in curr_index['extensions'].keys():
             print("Adding '{}' to index...".format(extension_name))
             curr_index['extensions'][extension_name] = [entry]
         else:
             print("Updating '{}' in index...".format(extension_name))
-            if curr_index['extensions'][extension_name][-1]['filename'] == entry['filename']:
+            if curr_index['extensions'][extension_name][-1]['filename'] == entry['filename']:  # in case of overwrite
                 curr_index['extensions'][extension_name][-1] = entry
             else:
                 curr_index['extensions'][extension_name].append(entry)
