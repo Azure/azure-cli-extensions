f5bf3832eacd3d9bf7660b9c8ed4bff36a5d773b
MyronFanQiu <fanqiu@microsoft.com>
2020-03-23 15:46:33 +0800

    md

--- src/resource-graph/setup.py
+++ src/resource-graph/setup.py
@@ -24,7 +24,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = []
 
-with open('README.rst', 'r', encoding='utf-8') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     README = f.read()
 with open('HISTORY.rst', 'r', encoding='utf-8') as f:
     HISTORY = f.read()
