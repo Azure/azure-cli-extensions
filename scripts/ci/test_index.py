76e2b20c811c8b28af0c9d5cd66965c55d9ba3b1
Jianhui Harold <haroldrandom@gmail.com>
2020-03-30 17:19:03 +0800

    typo

--- scripts/ci/test_index.py
+++ scripts/ci/test_index.py
@@ -14,9 +14,9 @@ import tempfile
 import unittest
 import hashlib
 import shutil
-try
+try:
     from wheel.install import WHEEL_INFO_RE
-except ImportError
+except ImportError:
     from wheel.wheelfile import WHEEL_INFO_RE
 
 from util import get_ext_metadata, get_whl_from_url, get_index_data, verify_dependency
