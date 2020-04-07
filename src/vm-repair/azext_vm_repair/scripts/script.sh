a6817b9c9291e802663fa8edc06152684928a4fd
Ubuntu <dibaskar@ubuntu18dev.qhaikempv3lehamphb0oflcsdg.bx.internal.cloudapp.net>
2020-04-07 17:24:25 +0000

    changes to unlock script

--- src/vm-repair/azext_vm_repair/scripts/script.sh
+++ src/vm-repair/azext_vm_repair/scripts/script.sh
@@ -5,13 +5,27 @@ mkdir /{investigateboot,investigateroot}
 
 #Getting boot and root partition info
 
-boot_part=`fdisk -l | grep -i sdd | grep -iv disk | awk '{print $1}' | sed -n 1p`
-root_part=`fdisk -l | grep -i sdd | grep -iv disk | awk '{print $1}' | sed -n 2p`
+root=`fdisk -l | grep -i sdd | grep -iv disk | sed -n 1p | awk '$4 > 60000000{print $1}'`
+if [ -z $root ]
+then
+        boot_part=`fdisk -l | grep -i sdd | grep -iv disk | awk '{print $1}' | sed -n 1p`
+        root_part=`fdisk -l | grep -i sdd | grep -iv disk | awk '{print $1}' | sed -n 2p`
+else
+        root_part="$root"
+        boot_part=`fdisk -l | grep -i sdd | grep -iv disk | awk '{print $1}' | sed -n 2p`
+fi
 
+mount_cmd=`mount -o nouuid 2> /dev/null`
+if [ $? -gt 0 ]
+then
+        mount_cmd="mount"
+else
+        mount_cmd="mount -o nouuid"
+fi
 
 #mouning the boot partition
 
-mount -o nouuid  $boot_part /investigateboot
+$mount_cmd $boot_part /investigateboot
 
 #unlocking the root partition using passphrase
 
@@ -19,9 +33,9 @@ cryptsetup luksOpen --key-file /mnt/azure_bek_disk/LinuxPassPhraseFileName_1_0 -
 
 #mounting the unlocked root partition
 
-mount -o nouuid /dev/mapper/investigateosencrypt /investigateroot
+$mount_cmd /dev/mapper/investigateosencrypt /investigateroot
 
 #nmounting boot and mounting it inside rootpartition.
 
 umount -l /investigateboot
-mount -o nouuid $boot_part /investigateroot/boot
+$mount_cmd $boot_part /investigateroot/boot
