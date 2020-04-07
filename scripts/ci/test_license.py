0b69da22160f04bb004179d44bd5dcd8f45f33c9
Jianhui Harold <haroldrandom@gmail.com>
2020-04-07 11:34:46 +0800

    test

--- scripts/ci/test_license.py
+++ scripts/ci/test_license.py
@@ -8,12 +8,15 @@ def main():
     home = '/home/vsts/work/1/s/'
 
     for d, sd, files in os.walk(home):
-        print('-' * 10, d, '-' * 10)
+        print('-' * 30, d, '-' * 30)
         print(sd)
 
         # for i, x in enumerate(sd):
         #     if x in _IGNORE_SUBDIRS or x.startswith('.'):
         #         print(x)
+
+        py_files = [os.path.join(d, p) for p in files if p.endswith('.py')]
+        print(py_files)
 
 
 if __name__ == '__main__':
