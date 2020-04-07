5ed28b189ad0dad715ebfed2a0e0fe90dcafba64
Jianhui Harold <haroldrandom@gmail.com>
2020-04-07 11:13:25 +0800

    test

---
+++ scripts/ci/test_license.py
@@ -0,0 +1,17 @@
+import os
+
+
+_IGNORE_SUBDIRS = ['__pycache__', 'vendored_sdks', 'site-packages', 'env']
+
+
+def main():
+    home = '/home/vsts/work/1/s/'
+
+    for d, sd, files in os.walk(home):
+        for i, x in enumerate(sd):
+            if x in _IGNORE_SUBDIRS or x.startswith('.'):
+                print(x)
+
+
+if __name__ == '__main__':
+    main()
