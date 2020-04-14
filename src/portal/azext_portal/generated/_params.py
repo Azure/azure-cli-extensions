06f1814afe9b3090ea9ed6baf7ada686a20db835
YalinLi0312 <yall@microsoft.com>
2020-04-14 14:41:12 -0700

    Apply style check to all customized files

--- src/portal/azext_portal/generated/_params.py
+++ src/portal/azext_portal/generated/_params.py
@@ -19,29 +19,44 @@ from azure.cli.core.commands.validators import get_default_location_from_resourc
 def load_arguments(self, _):
 
     with self.argument_context('portal dashboard list') as c:
-        c.argument('resource_group_name', resource_group_name_type, help='The name of the resource group.')
+        c.argument('resource_group_name', resource_group_name_type,
+                   help='The name of the resource group.')
 
     with self.argument_context('portal dashboard show') as c:
-        c.argument('resource_group_name', resource_group_name_type, help='The name of the resource group.')
-        c.argument('name', options_list=['--name', '-n'], help='The name of the dashboard.')
+        c.argument('resource_group_name', resource_group_name_type,
+                   help='The name of the resource group.')
+        c.argument('name', options_list=[
+                   '--name', '-n'], help='The name of the dashboard.')
 
     with self.argument_context('portal dashboard create') as c:
-        c.argument('resource_group_name', resource_group_name_type, help='The name of the resource group.')
-        c.argument('name', options_list=['--name', '-n'], help='The name of the dashboard.')
-        c.argument('location', arg_type=get_location_type(self.cli_ctx), validator=get_default_location_from_resource_group)
+        c.argument('resource_group_name', resource_group_name_type,
+                   help='The name of the resource group.')
+        c.argument('name', options_list=[
+                   '--name', '-n'], help='The name of the dashboard.')
+        c.argument('location', arg_type=get_location_type(self.cli_ctx),
+                   validator=get_default_location_from_resource_group)
         c.argument('tags', tags_type)
-        c.argument('input_path', type=file_type, help='The path to the dashboard properties JSON file.', completer=FilesCompleter())
+        c.argument('input_path', type=file_type,
+                   help='The path to the dashboard properties JSON file.', completer=FilesCompleter())
 
     with self.argument_context('portal dashboard update') as c:
-        c.argument('resource_group_name', resource_group_name_type, help='The name of the resource group.')
-        c.argument('name', options_list=['--name', '-n'], help='The name of the dashboard.')
-        c.argument('input_path', type=file_type, help='The path to the dashboard properties JSON file.', completer=FilesCompleter())
+        c.argument('resource_group_name', resource_group_name_type,
+                   help='The name of the resource group.')
+        c.argument('name', options_list=[
+                   '--name', '-n'], help='The name of the dashboard.')
+        c.argument('input_path', type=file_type,
+                   help='The path to the dashboard properties JSON file.', completer=FilesCompleter())
 
     with self.argument_context('portal dashboard delete') as c:
-        c.argument('resource_group_name', resource_group_name_type, help='The name of the resource group.')
-        c.argument('name', options_list=['--name', '-n'], help='The name of the dashboard.')
+        c.argument('resource_group_name', resource_group_name_type,
+                   help='The name of the resource group.')
+        c.argument('name', options_list=[
+                   '--name', '-n'], help='The name of the dashboard.')
 
     with self.argument_context('portal dashboard import') as c:
-        c.argument('resource_group_name', resource_group_name_type, help='The name of the resource group.')
-        c.argument('name', options_list=['--name', '-n'], help='The name of the dashboard.')
-        c.argument('input_path', type=file_type, help='The path to the dashboard json file.', completer=FilesCompleter())
+        c.argument('resource_group_name', resource_group_name_type,
+                   help='The name of the resource group.')
+        c.argument('name', options_list=[
+                   '--name', '-n'], help='The name of the dashboard.')
+        c.argument('input_path', type=file_type,
+                   help='The path to the dashboard json file.', completer=FilesCompleter())
