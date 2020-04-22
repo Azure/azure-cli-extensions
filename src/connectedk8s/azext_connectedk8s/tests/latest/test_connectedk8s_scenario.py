b747e9277f297215b037004e903754a3548292fb
Akash Keshari <akkeshar@microsoft.com>
2020-04-22 13:14:06 +0530

    Updated test file

--- src/connectedk8s/azext_connectedk8s/tests/latest/test_connectedk8s_scenario.py
+++ src/connectedk8s/azext_connectedk8s/tests/latest/test_connectedk8s_scenario.py
@@ -22,15 +22,16 @@ class Connectedk8sScenarioTest(ScenarioTest):
             'name': 'test1'
         })
 
-        self.cmd('az connectedk8s connect -g {rg} -n {name} -l eastus2euap --tags foo=doo', checks=[
+        self.cmd('connectedk8s connect -g {rg} -n {name} -l eastus2euap --tags foo=doo', checks=[
             self.check('tags.foo', 'doo'),
             self.check('name', '{name}')
         ])
-        count = len(self.cmd('az connectedk8s list').get_output_in_json())
-        self.cmd('az connectedk8s show -g {rg} -n {name}', checks=[
+        count = len(self.cmd('connectedk8s list').get_output_in_json())
+        self.cmd('connectedk8s show -g {rg} -n {name}', checks=[
             self.check('name', '{name}'),
-            self.check('resourceGroup', '{rg}')
+            self.check('resourceGroup', '{rg}'),
+            self.check('tags.foo', 'boo')
         ])
-        self.cmd('az connectedk8s delete -g {rg} -n {name} -y')
+        self.cmd('connectedk8s delete -g {rg} -n {name} -y')
         final_count = len(self.cmd('connectedk8s list').get_output_in_json())
         self.assertTrue(final_count, count - 1)
