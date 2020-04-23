5337e03272b40f1658bb140ba8476200bd00b347
nanthi <nanthi@NANTHI01>
2020-04-22 19:19:42 -0700

    addressed more comments

--- src/k8sconfiguration/azext_k8sconfiguration/_validators.py
+++ src/k8sconfiguration/azext_k8sconfiguration/_validators.py
@@ -6,23 +6,6 @@
 from knack.util import CLIError
 
 
-def example_name_or_id_validator(cmd, namespace):
-    # Example of a storage account name or ID validator.
-    # See: https://github.com/Azure/azure-cli/blob/dev/doc/authoring_command_modules/
-    # authoring_commands.md#supporting-name-or-id-parameters
-    from azure.cli.core.commands.client_factory import get_subscription_id
-    from msrestazure.tools import is_valid_resource_id, resource_id
-    if namespace.storage_account:
-        if not is_valid_resource_id(namespace.RESOURCE):
-            namespace.storage_account = resource_id(
-                subscription=get_subscription_id(cmd.cli_ctx),
-                resource_group=namespace.resource_group_name,
-                namespace='Microsoft.Storage',
-                type='storageAccounts',
-                name=namespace.storage_account
-            )
-
-
 def validate_configuration_type(configuration_type):
     if configuration_type.lower() != 'sourcecontrolconfiguration':
         raise CLIError('Invalid configuration-type.  Valid value is "sourceControlConfiguration"')
