1756c518cb640c6e0c2f0a07eeb7bf0272add1c7
nanthi <nanthi@NANTHI01>
2020-04-23 11:04:00 -0700

    remove Optional from help message

--- src/k8sconfiguration/azext_k8sconfiguration/_params.py
+++ src/k8sconfiguration/azext_k8sconfiguration/_params.py
@@ -25,28 +25,28 @@ def load_arguments(self, _):
         c.argument('name', sourcecontrolconfiguration_type, options_list=['--name', '-n'])
         c.argument('cluster_name', options_list=['--cluster-name', '-c'], help='Name of the Kubernetes cluster')
         c.argument('cluster_type', arg_type=get_enum_type(['connectedClusters', 'managedClusters']),
-                   help='Optional parameter to specify Onprem clusters (default) or AKS clusters.')
+                   help='Specify Arc clusters (default) or AKS clusters.')
         c.argument('repository_url', options_list=['--repository-url', '-u'],
                    help='Url of the sourceControl repository')
         c.argument('enable_helm_operator', arg_type=get_three_state_flag(),
                    help='Enable support for Helm chart deployments')
         c.argument('cluster_scoped', action='store_true',
-                   help='''Optional switch to set scope of the operator to be cluster. Default scope is 'namespace'.''')
+                   help='''Switch to set scope of the operator to be cluster. Default scope is 'namespace'.''')
         c.argument('configuration_type', validator=validate_configuration_type,
                    arg_type=get_enum_type(['sourceControlConfiguration']),
                    help='Type of the configuration')
         c.argument('helm_operator_params',
-                   help='Optional. Chart values for the Helm Operator (if enabled)')
+                   help='Chart values for the Helm Operator (if enabled)')
         c.argument('helm_operator_version',
-                   help='''Optional. Chart version of the Helm Operator, if enabled.  Default:'0.2.0'.''')
+                   help='''Chart version of the Helm Operator, if enabled.  Default:'0.2.0'.''')
         c.argument('operator_params',
-                   help='Optional. Parameters for the Operator.')
+                   help='Parameters for the Operator.')
         c.argument('operator_instance_name',
                    help='Instance name of the Operator.')
         c.argument('operator_namespace',
                    help='Namespace in which to install the Operator.')
         c.argument('operator_type',
-                   help='''Optional. Type of the operator. Valid value is 'flux'.''')
+                   help='''Type of the operator. Valid value is 'flux'.''')
 
     with self.argument_context('k8sconfiguration list') as c:
         c.argument('sourcecontrolconfiguration', sourcecontrolconfiguration_type, id_part=None)
