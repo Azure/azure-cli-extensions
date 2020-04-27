0cd0d35f5d28d152d31974297673167a1bf0794e
AssafTaubenfeldMSFT <astauben@microsoft.com>
2020-04-27 12:59:26 +0300

    Update _help.py

--- src/kusto/azext_kusto/generated/_help.py
+++ src/kusto/azext_kusto/generated/_help.py
@@ -555,4 +555,4 @@ helps['kusto database-principal-assignment wait'] = """
 type: command
 short-summary: Wait for a managed Kusto cluster to reach a desired state.
 long-summary: If an operation on a cluster was interrupted or was started with `--no-wait`, use this command to wait for it to complete.
-""
+"""
