dbbce53a700c4adda5be82e305ab1cb041605657
AssafTaubenfeldMSFT <astauben@microsoft.com>
2020-04-30 18:32:42 +0300

    Update _help.py

--- src/kusto/azext_kusto/manual/_help.py
+++ src/kusto/azext_kusto/manual/_help.py
@@ -557,6 +557,3 @@ type: command
 short-summary: Wait for a managed Kusto cluster to reach a desired state.
 long-summary: If an operation on a cluster was interrupted or was started with `--no-wait`, use this command to wait for it to complete.
 """
-
-
-
