14ec6289d1281bcbe641aec4a548510ffdb86bac
Jianhui Harold <haroldrandom@gmail.com>
2020-05-06 10:53:43 +0800

    [storage-ors-preview] fix no azext metadata (#1648)

--- src/storage-ors-preview/setup.py
+++ src/storage-ors-preview/setup.py
@@ -16,7 +16,7 @@ except ImportError:
 
 # TODO: Confirm this is the right version number you want and it matches your
 # HISTORY.rst entry.
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -56,5 +56,5 @@ setup(
     classifiers=CLASSIFIERS,
     packages=find_packages(),
     install_requires=DEPENDENCIES,
-    package_data={'azext_storage-ors-preview': ['azext_metadata.json']},
+    package_data={'azext_storage_ors_preview': ['azext_metadata.json']},
 )
