a001ce9888a1624872199813f8a2ef5ad549c826
harold random <haroldrandom@gmail.com>
2020-05-26 01:08:10 +0800

    add test test_export_create_with_schedule_in_subscription_scope

--- src/costmanagement/azext_costmanagement/tests/latest/test_costmanagement_export.py
+++ src/costmanagement/azext_costmanagement/tests/latest/test_costmanagement_export.py
@@ -86,3 +86,82 @@ class CostManagementExportTest(ScenarioTest):
         self.assertEqual(data['status'], 'Inactive')
         self.assertIsNone(data['timePeriod'])
         self.assertEqual(data['timeframe'], timeframe)
+
+    @ResourceGroupPreparer(name_prefix='test_export_schedule')
+    @StorageAccountPreparer(name_prefix='test_export_schedule'.replace('_', ''))
+    def test_export_create_with_schedule_in_subscription_scope(self, resource_group, storage_account):
+        self.kwargs.update({
+            'scope': '/subscriptions/{}'.format(self.get_subscription_id()),
+        })
+
+        export_name = 'ep-02'
+        storage_container = 'export'
+        timeframe = 'TheLastMonth'
+        storage_account_id = '/subscriptions/{sub}/resourceGroups/{rg}/providers/Microsoft.Storage/storageAccounts/{account_name}'.format(
+            sub=self.get_subscription_id(),
+            rg=resource_group,
+            account_name=storage_account
+        )
+
+        self.kwargs.update({
+            'export_name': export_name,
+            'storage_account_id': storage_account_id,
+            'storage_container': storage_container,
+            'timeframe': timeframe
+        })
+
+        creation_data = self.cmd('costmanagement export create '
+                                 '--scope {scope} '
+                                 '--name {export_name} '
+                                 '--storage-account-id {storage_account_id} '
+                                 '--storage-container {storage_container} '
+                                 '--timeframe {timeframe} '
+                                 '--recurrence "Weekly" '
+                                 '--recurrence-period from="2020-06-01T00:00:00Z" to="2020-10-31T00:00:00Z" '
+                                 '--schedule-status Active').get_output_in_json()
+        self._test_export_create_with_schedule_in_subscription_scope_assertions(creation_data,
+                                                                                export_name,
+                                                                                storage_container,
+                                                                                storage_account_id,
+                                                                                timeframe)
+
+        # show an export
+        show_data = self.cmd('costmanagement export show --scope {scope} --name {export_name}').get_output_in_json()
+        self._test_export_create_with_schedule_in_subscription_scope_assertions(show_data,
+                                                                                export_name,
+                                                                                storage_container,
+                                                                                storage_account_id,
+                                                                                timeframe)
+
+        # list exports
+        list_data = self.cmd('costmanagement export list --scope {scope}').get_output_in_json()
+        self.assertEqual(len(list_data), 1)
+        self._test_export_create_with_schedule_in_subscription_scope_assertions(list_data[0],
+                                                                                export_name,
+                                                                                storage_container,
+                                                                                storage_account_id,
+                                                                                timeframe)
+
+        self.cmd('costmanagement export delete --scope {scope} --name {export_name}')
+
+        with self.assertRaisesRegex(SystemExit, '3'):
+            self.cmd('costmanagement export show --scope {scope} --name {export_name}')
+
+    def _test_export_create_with_schedule_in_subscription_scope_assertions(self, data, export_name, storage_container, storage_account_id, timeframe):
+        self.assertEqual(data['name'], export_name)
+        self.assertIsNone(data['aggregation'], None)
+        self.assertIsNone(data['configuration'], None)
+        self.assertDictEqual(data['destination'], {
+            'container': storage_container,
+            'resourceId': storage_account_id,
+            'rootFolderPath': None
+        })
+        self.assertEqual(data['typePropertiesDefinitionType'], 'Usage')    # default is Usage
+        self.assertEqual(data['recurrence'], 'Weekly')
+        self.assertDictEqual(data['recurrencePeriod'], {
+            "fromProperty": "2020-06-01T00:00:00+00:00",
+            "to": "2020-10-31T00:00:00+00:00"
+        })
+        self.assertEqual(data['status'], 'Active')
+        self.assertIsNone(data['timePeriod'])
+        self.assertEqual(data['timeframe'], timeframe)
