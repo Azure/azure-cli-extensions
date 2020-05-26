30f457967d766ba90122a76d24cae04a1f59f329
Harold Zeng <haroldrandom@gmail.com>
2020-05-26 06:38:54 +0000

    update update command

--- src/costmanagement/azext_costmanagement/manual/custom.py
+++ src/costmanagement/azext_costmanagement/manual/custom.py
@@ -74,5 +74,77 @@ def costmanagement_export_create(cmd,
                                    recurrence_period=schedule_recurrence_period)
 
 
+def costmanagement_export_update(cmd,
+                                 client,
+                                 scope,
+                                 export_name,
+                                 delivery_storage_container=None,
+                                 delivery_storage_account_id=None,
+                                 delivery_directory=None,
+                                 definition_type=None,
+                                 definition_timeframe=None,
+                                 definition_time_period=None,
+                                 definition_dataset_configuration=None,
+                                 definition_dataset_aggregation=None,
+                                 definition_dataset_grouping=None,
+                                 definition_dataset_filter=None,
+                                 schedule_status=None,
+                                 schedule_recurrence=None,
+                                 schedule_recurrence_period=None):
+
+    export_instance = client.get(scope=scope, export_name=export_name)
+
+    print(type(export_instance))
+    print(export_instance)
+
+    # if isinstance(definition_dataset_aggregation, str):
+    #     definition_dataset_aggregation = json.loads(definition_dataset_aggregation)
+    # if isinstance(definition_dataset_filter, str):
+    #     definition_dataset_filter = json.loads(definition_dataset_filter)
+
+    # delivery_info_destination = {
+    #     'resource_id': delivery_storage_account_id,
+    #     'container': delivery_storage_container,
+    #     'root_folder_path': delivery_directory,
+    # }
+
+    with cmd.update_context(export_instance) as c:
+        # c.set_param('status', schedule_status)
+        # c.set_param('recurrence', schedule_recurrence)
+        # c.set_param('recurrence_period', )
+
+        c.set_param('type', 'ActualCost')
+
+    print('-' * 100)
+    print(export_instance)
+
+    # return client.create_or_update(scope=scope,
+    #                                export_name=export_name,
+    #                                type=definition_type,
+    #                                timeframe=definition_timeframe,
+    #                                time_period=definition_time_period,
+    #                                configuration=definition_dataset_configuration,
+    #                                aggregation=definition_dataset_aggregation,
+    #                                grouping=definition_dataset_grouping,
+    #                                filter=definition_dataset_filter,
+    #                                destination=delivery_info_destination,
+    #                                status=schedule_status,
+    #                                recurrence=schedule_recurrence,
+    #                                recurrence_period=schedule_recurrence_period)
+    return client.create_or_update(scope=scope,
+                                   export_name=export_name,
+                                   type=export_instance.type_properties_definition_type,
+                                   timeframe=export_instance.timeframe,
+                                   time_period=export_instance.time_period,
+                                   configuration=export_instance.configuration,
+                                   aggregation=export_instance.aggregation,
+                                   grouping=export_instance.grouping,
+                                   filter=export_instance.filter,
+                                   destination=export_instance.destination,
+                                   status=export_instance.status,
+                                   recurrence=export_instance.recurrence,
+                                   recurrence_period=export_instance.recurrence_period)
+
+
 def costmanagement_export_list(cmd, client, scope):
     return client.list(scope=scope).value   # value exist even the result is empty
