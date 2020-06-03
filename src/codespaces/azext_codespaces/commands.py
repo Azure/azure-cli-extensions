3320394f06cb0bb1ab6c3b5644b2ce3c72323bd5
Derek Bekoe <debekoe@microsoft.com>
2020-06-03 00:48:01 -0700

    more pr feedback

--- src/codespaces/azext_codespaces/commands.py
+++ src/codespaces/azext_codespaces/commands.py
@@ -22,12 +22,12 @@ def load_command_table(self, _):
     with self.command_group('codespace plan', plan_operations, client_factory=cf_codespaces_plan) as g:
         g.custom_command('list', 'list_plans')
         g.custom_command('create', 'create_plan')
-        g.command('show', 'get')
+        g.show_command('show', 'get')
         g.command('delete', 'delete')
 
     with self.command_group('codespace', plan_operations, client_factory=cf_codespaces_plan) as g:
         g.custom_command('list', 'list_codespaces', table_transformer=transform_codespace_list_output)
-        g.custom_command('show', 'get_codespace', table_transformer=transform_codespace_item_output)
+        g.custom_show_command('show', 'get_codespace', table_transformer=transform_codespace_item_output)
         g.custom_command('create', 'create_codespace', table_transformer=transform_codespace_item_output)
         g.custom_command('open', 'open_codespace')
         g.custom_command('delete', 'delete_codespace', confirmation="Are you sure you want to delete this Codespace?")
@@ -36,7 +36,7 @@ def load_command_table(self, _):
 
     with self.command_group('codespace location', plan_operations, client_factory=cf_codespaces_plan) as g:
         g.custom_command('list', 'list_available_locations', table_transformer=transform_location_list_output)
-        g.custom_command('show', 'get_location_details', table_transformer=transform_location_detail_output)
+        g.custom_show_command('show', 'get_location_details', table_transformer=transform_location_detail_output)
 
     # Mark all commands as in preview
     with self.command_group('codespace', is_preview=True):
