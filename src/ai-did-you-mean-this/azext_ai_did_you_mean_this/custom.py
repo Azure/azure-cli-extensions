c202f4f36fe45c62841829c7570233c7cadb8ad3
Christopher OToole <chotool@microsoft.com>
2020-06-05 16:29:16 -0700

    Remove format string modifier from statement in show_extension_version.

--- src/ai-did-you-mean-this/azext_ai_did_you_mean_this/custom.py
+++ src/ai-did-you-mean-this/azext_ai_did_you_mean_this/custom.py
@@ -32,7 +32,7 @@ logger = get_logger(__name__)
 # Commands
 # note: at least one command is required in order for the CLI to load the extension.
 def show_extension_version():
-    print(f'Current version: 0.2.0')
+    print('Current version: 0.2.0')
 
 
 def _log_debug(msg, *args, **kwargs):
