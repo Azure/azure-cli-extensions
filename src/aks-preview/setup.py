6723a0fba65584afc0fb423dc225c0e02c26667f
Qingqing Zheng <qizhe@microsoft.com>
2020-06-16 11:14:38 -0700

    update aks-preview version

--- src/aks-preview/setup.py
+++ src/aks-preview/setup.py
@@ -8,7 +8,7 @@
 from codecs import open as open1
 from setuptools import setup, find_packages
 
-VERSION = "0.4.51"
+VERSION = "0.4.52"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
