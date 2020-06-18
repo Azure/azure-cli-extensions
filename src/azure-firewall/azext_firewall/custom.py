7e17b0397fe2cba8da429c187125c180cdc1de10
Harold Zeng <haroldrandom@gmail.com>
2020-06-18 15:43:57 +0800

    fix wrong key

--- src/azure-firewall/azext_firewall/custom.py
+++ src/azure-firewall/azext_firewall/custom.py
@@ -84,10 +84,11 @@ def create_azure_firewall(cmd, resource_group_name, azure_firewall_name, locatio
             firewall.additional_properties = {}
         firewall.additional_properties['Network.SNAT.PrivateRanges'] = private_ranges
 
-    firewall.additional_properties['DNSEnableProxy'] = enable_dns_proxy if enable_dns_proxy is not None else False
-    firewall.additional_properties['DNSRequireProxyForNetworkRules'] = \
+    firewall.additional_properties['Network.DNS.EnableProxy'] = \
+        enable_dns_proxy if enable_dns_proxy is not None else False
+    firewall.additional_properties['Network.DNS.RequireProxyForNetworkRules'] = \
         require_dns_proxy_for_network_rules if require_dns_proxy_for_network_rules is not None else True
-    firewall.additional_properties['DNSServer'] = dns_servers
+    firewall.additional_properties['Network.DNS.Servers'] = ','.join(dns_servers or '')
 
     return client.create_or_update(resource_group_name, azure_firewall_name, firewall)
 
@@ -114,11 +115,11 @@ def update_azure_firewall(cmd, instance, tags=None, zones=None, private_ranges=N
             instance.virtual_hub = SubResource(id=virtual_hub)
 
     if enable_dns_proxy is not None:
-        instance.additional_properties['DNSEnableProxy'] = enable_dns_proxy
+        instance.additional_properties['Network.DNS.EnableProxy'] = enable_dns_proxy
     if require_dns_proxy_for_network_rules is not None:
-        instance.additional_properties['DNSRequireProxyForNetworkRules'] = require_dns_proxy_for_network_rules
+        instance.additional_properties['Network.DNS.RequireProxyForNetworkRules'] = require_dns_proxy_for_network_rules
     if dns_servers is not None:
-        instance.additional_properties['DNSServer'] = dns_servers
+        instance.additional_properties['Network.DNS.Servers'] = ','.join(dns_servers or '')
     if threat_intel_mode is not None:
         instance.threat_intel_mode = threat_intel_mode
     return instance
