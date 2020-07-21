7b77ac03820ada254f4aacc851c9a9196baab116
astauben <astauben@microsoft.com>
2020-07-21 13:27:12 +0300

    Add files via upload

--- src/kusto/azext_kusto/vendored_sdks/kusto/_configuration.py
+++ src/kusto/azext_kusto/vendored_sdks/kusto/_configuration.py
@@ -11,14 +11,13 @@ from typing import TYPE_CHECKING
 from azure.core.configuration import Configuration
 from azure.core.pipeline import policies
 
-from ._version import VERSION
-
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from typing import Any
 
     from azure.core.credentials import TokenCredential
 
+VERSION = "unknown"
 
 class KustoManagementClientConfiguration(Configuration):
     """Configuration for KustoManagementClient.
@@ -47,9 +46,10 @@ class KustoManagementClientConfiguration(Configuration):
 
         self.credential = credential
         self.subscription_id = subscription_id
-        self.api_version = "2020-02-15"
+        self.api_version = "2020-06-14"
         self.credential_scopes = ['https://management.azure.com/.default']
-        kwargs.setdefault('sdk_moniker', 'mgmt-kusto/{}'.format(VERSION))
+        self.credential_scopes.extend(kwargs.pop('credential_scopes', []))
+        kwargs.setdefault('sdk_moniker', 'kustomanagementclient/{}'.format(VERSION))
         self._configure(**kwargs)
 
     def _configure(
