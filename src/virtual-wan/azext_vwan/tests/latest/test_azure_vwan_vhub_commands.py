e3b992f14a7109b831c4eb82d510f5cacd845eed
MyronFanQiu <fanqiu@microsoft.com>
2020-07-22 16:50:48 +0800

    finish test

--- src/virtual-wan/azext_vwan/tests/latest/test_azure_vwan_vhub_commands.py
+++ src/virtual-wan/azext_vwan/tests/latest/test_azure_vwan_vhub_commands.py
@@ -37,7 +37,10 @@ class AzureVWanVHubScenario(ScenarioTest):
         self.cmd('network vhub create -g {rg} -n {vhub} --vwan {vwan}  --address-prefix 10.0.0.0/24 -l SouthCentralUS --sku Standard')
 
         self.cmd('network vhub route add -g {rg} --vhub-name {vhub} --next-hop 10.0.0.7 --address-prefixes 10.3.3.0/28')
-        self.cmd('network vhub route reset -g {rg} --vhub-name {vhub}')
+        self.cmd('network vhub route reset -g {rg} --vhub-name {vhub}', checks=[
+            self.check('@[0].addressPrefixes[0]', '10.3.3.0/28'),
+            self.check('@[0].nextHopIpAddress', '10.0.0.7')
+        ])
 
     @ResourceGroupPreparer(name_prefix='cli_test_azure_vhub_connection')
     def test_azure_vhub_connection_basic_scenario(self, resource_group):
