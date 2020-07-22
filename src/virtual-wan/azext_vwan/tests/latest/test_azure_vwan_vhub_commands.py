82f2ccf686a51c2167c989a422e24c25a39f38b1
Yu Chen <ychenu@microsoft.com>
2020-07-22 14:57:45 +0800

    [Network] Bump network sdk to 2020-05-01 (#2052)

--- src/virtual-wan/azext_vwan/tests/latest/test_azure_vwan_vhub_commands.py
+++ src/virtual-wan/azext_vwan/tests/latest/test_azure_vwan_vhub_commands.py
@@ -91,10 +91,6 @@ class AzureVWanVHubScenario(ScenarioTest):
             self.check('length(@)', 1)
         ])
 
-        self.cmd('network vpn-server-config list', checks=[
-            self.check('length(@)', 1)
-        ])
-
         self.cmd('network vpn-server-config ipsec-policy add -n {vserverconfig} -g {rg} '
                  '--ipsec-encryption AES256 --ipsec-integrity SHA256 '
                  '--sa-lifetime 86471 --sa-data-size 429496 --ike-encryption AES256 '
@@ -139,7 +135,7 @@ class AzureVWanVHubScenario(ScenarioTest):
         with self.assertRaisesRegexp(SystemExit, '3'):
             self.cmd('network vpn-server-config show -n {vserverconfig} -g {rg}')
 
-    @ResourceGroupPreparer(name_prefix='cli_test_azure_vhub_connection', location='westus')
+    @ResourceGroupPreparer(name_prefix='cli_test_azure_p2s_vpn_gateway', location='westcentralus')
     def test_azure_p2s_vpn_gateway_basic_scenario(self, resource_group):
         self.kwargs.update({
             'vwan': 'clitestvwan',
@@ -155,7 +151,7 @@ class AzureVWanVHubScenario(ScenarioTest):
         })
 
         self.cmd('network vwan create -n {vwan} -g {rg} --type Standard')
-        self.cmd('network vhub create -g {rg} -n {vhub} --vwan {vwan}  --address-prefix 10.5.0.0/16 -l westus --sku Standard')
+        self.cmd('network vhub create -g {rg} -n {vhub} --vwan {vwan}  --address-prefix 10.5.0.0/16 -l westcentralus --sku Standard')
         self.cmd('network vpn-server-config create -n {vserverconfig} -g {rg} '
                  '--vpn-client-root-certs "{cert_file}" '
                  '--vpn-client-revoked-certs "{pem_file}"')
@@ -168,9 +164,6 @@ class AzureVWanVHubScenario(ScenarioTest):
         self.cmd('az network p2s-vpn-gateway update -g {rg} -n {vp2sgateway} --scale-unit 3 '
                  '--vpn-server-config {vserverconfig2} --address-space 13.0.0.0/24 12.0.0.0/24')
         self.cmd('az network p2s-vpn-gateway list -g {rg}', checks=[
-            self.check('length(@)', 1)
-        ])
-        self.cmd('az network p2s-vpn-gateway list', checks=[
             self.check('length(@)', 1)
         ])
         self.cmd('az network p2s-vpn-gateway show -g {rg} -n {vp2sgateway}', checks=[
