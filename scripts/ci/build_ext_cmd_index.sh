2d23eabaa2a0ed9d30f8288d527f7c4dadcf7781
Feng Zhou <fenzhou@microsoft.com>
2020-07-29 13:19:00 +0800

    add code change check

--- scripts/ci/build_ext_cmd_index.sh
+++ scripts/ci/build_ext_cmd_index.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+changed_content=$(git --no-pager diff --diff-filter=ACMRT HEAD~$AZURE_EXTENSION_COMMIT_NUM -- src/index.json)
+if [[ -z "$changed_content" ]]; then
+    echo "index.json not modified. End task."
+    exit 0
+fi
+
 pip install azure-cli-core azure-cli requests
 pip install azure-storage-blob==1.5.0
 echo "Listing Available Extensions:"
@@ -8,10 +14,13 @@ az extension list-available -otable
 # turn off telemetry as it crowds output
 export AZURE_CORE_COLLECT_TELEMETRY=False
 
+# wait for the index.json to be synced in storage account
+# Remove this when we can support using customized index.json
+sleep 360
+
 output=$(az extension list-available --query [].name -otsv)
 # azure-cli-iot-ext is the deprecated old versions of the renamed azure-iot extension
-# disable hack temporarily as the url is broken
-blocklist=("azure-cli-iot-ext" "hack")
+blocklist=("azure-cli-iot-ext")
 
 rm -f ~/.azure/extCmdIndexToUpload.json
 
@@ -28,7 +37,6 @@ for ext in $output; do
         echo "Failed to load:" $ext
         exit 1
     fi
-    python $(cd $(dirname $0); pwd)/update_ext_cmd_index.py $ext
 done
 
-python $(cd $(dirname $0); pwd)/upload_ext_cmd_index.py
+python $(cd $(dirname $0); pwd)/update_ext_cmd_index.py $output
--- scripts/ci/update_ext_cmd_index.py
+++ scripts/ci/update_ext_cmd_index.py
@@ -3,12 +3,22 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import filecmp
 import os
 import sys
 from azure.cli.core import get_default_cli
 from azure.cli.core._session import Session
 from azure.cli.core.commands import _load_extension_command_loader
 from azure.cli.core.extension import get_extension_modname, get_extension_path
+from azure.storage.blob import BlockBlobService
+from sync_extensions import download_file
+
+STORAGE_ACCOUNT_KEY = os.getenv('AZURE_EXTENSION_CMD_INDEX_STORAGE_ACCOUNT_KEY')
+STORAGE_ACCOUNT = os.getenv('AZURE_EXTENSION_CMD_INDEX_STORAGE_ACCOUNT')
+STORAGE_CONTAINER = os.getenv('AZURE_EXTENSION_CMD_INDEX_STORAGE_CONTAINER')
+
+az_cli = get_default_cli()
+file_name = 'extCmdIndexToUpload.json'
 
 
 def merge(data, key, value):
@@ -22,9 +32,7 @@ def merge(data, key, value):
             merge(data[key], k, v)
 
 
-def main():
-    az_cli = get_default_cli()
-    ext_name = sys.argv[1]
+def update_cmd_index(ext_name):
     print(f"Processing {ext_name}")
 
     ext_dir = get_extension_path(ext_name)
@@ -39,7 +47,7 @@ def main():
                                                                 "", ext_mod)
 
     EXT_CMD_INDEX = Session()
-    EXT_CMD_INDEX.load(os.path.expanduser(os.path.join('~', '.azure', 'extCmdIndexToUpload.json')))
+    EXT_CMD_INDEX.load(os.path.expanduser(os.path.join('~', '.azure', file_name)))
     root = {}
     for cmd_name, _ in extension_command_table.items():
         parts = cmd_name.split()
@@ -57,5 +65,27 @@ def main():
         merge(EXT_CMD_INDEX.data, k, v)
     EXT_CMD_INDEX.save_with_retry()
 
+
+def upload_cmd_index():
+    blob_file_name = 'extCmdIndex.json'
+    downloaded_file_name = 'extCmdIndexDownloaded.json'
+    file_path = os.path.expanduser(os.path.join('~', '.azure', file_name))
+
+    client = BlockBlobService(account_name=STORAGE_ACCOUNT, account_key=STORAGE_ACCOUNT_KEY)
+    client.create_blob_from_path(container_name=STORAGE_CONTAINER, blob_name=blob_file_name,
+                                 file_path=file_path)
+
+    url = client.make_blob_url(container_name=STORAGE_CONTAINER, blob_name=blob_file_name)
+
+    download_file_path = os.path.expanduser(os.path.join('~', '.azure', downloaded_file_name))
+    download_file(url, download_file_path)
+    if filecmp.cmp(file_path, download_file_path):
+        print("extCmdIndex.json uploaded successfully. URL: {}".format(url))
+    else:
+        raise Exception("Failed to update extCmdIndex.json in storage account")
+
+
 if __name__ == '__main__':
-    main()
+    for ext in sys.argv[1:]:
+        update_cmd_index(ext)
+    upload_cmd_index()
