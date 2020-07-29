fc8bfbc97f84f20ea899a149bc8d226743f28d8e
Anumita Shenoy <ansheno@microsoft.com>
2020-07-30 01:18:15 +0530

    [ConnectedK8s] Added update command (#3)

--- src/connectedk8s/setup.py
+++ src/connectedk8s/setup.py
@@ -17,7 +17,7 @@ except ImportError:
 # TODO: Confirm this is the right version number you want and it matches your
 # HISTORY.rst entry.
 
-VERSION = '0.2.1'
+VERSION = '0.2.3'
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
