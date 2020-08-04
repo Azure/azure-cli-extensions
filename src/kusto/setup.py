9ae6cef9f73618a728e165c18d10a60089dd9ed4
astauben <astauben@microsoft.com>
2020-08-04 09:47:48 +0300

    Update setup.py

--- src/kusto/setup.py
+++ src/kusto/setup.py
@@ -10,7 +10,7 @@ from codecs import open
 from setuptools import setup, find_packages
 
 # HISTORY.rst entry.
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 try:
     from .manual.version import VERSION
 except ImportError:
