180f3cc80d6db33c9bcadf45bd32bc5e60f55313
Artem Pavlichenko <brentwoodpav@gmail.com>
2020-08-19 23:05:31 -0700

    Added mysql default migration options and selected tables options for pg migrations

--- src/dms-preview/azext_dms/scenario_inputs.py
+++ src/dms-preview/azext_dms/scenario_inputs.py
@@ -7,6 +7,7 @@ from knack.util import CLIError
 from knack.prompting import prompt, prompt_pass
 from azext_dms.vendored_sdks.datamigration.models import (MigrateMySqlAzureDbForMySqlSyncDatabaseInput,
                                                           MigratePostgreSqlAzureDbForPostgreSqlSyncDatabaseInput,
+                                                          MigratePostgreSqlAzureDbForPostgreSqlSyncDatabaseTableInput,
                                                           MigrateMySqlAzureDbForMySqlSyncTaskInput,
                                                           MigratePostgreSqlAzureDbForPostgreSqlSyncTaskInput,
                                                           MongoDbMigrationSettings,
@@ -24,10 +25,11 @@ def get_migrate_mysql_to_azuredbformysql_sync_input(database_options_json,
     database_options = []
 
     for d in database_options_json:
+        def_migration_setting_input={"fullLoadSubTasks":"5","inlineLobMaxSize":"0","limitLOBSize":"true","lobChunkSize":"64","lobMaxSize":"32"}
         database_options.append(MigrateMySqlAzureDbForMySqlSyncDatabaseInput(
             name=d.get('name', None),
             target_database_name=d.get('target_database_name', None),
-            migration_setting=d.get('migrationSetting', None),
+            migration_setting=d.get('migrationSetting', def_migration_setting_input),
             source_setting=d.get('sourceSetting', None),
             target_setting=d.get('targetSetting', None)))
 
@@ -42,12 +44,15 @@ def get_migrate_postgresql_to_azuredbforpostgresql_sync_input(database_options_j
     database_options = []
 
     for d in database_options_json:
+        selected_tables = d.get('selectedTables', None)
+        table_input = [MigratePostgreSqlAzureDbForPostgreSqlSyncDatabaseTableInput(name=t) for t in selected_tables] if selected_tables is not None else None
         database_options.append(MigratePostgreSqlAzureDbForPostgreSqlSyncDatabaseInput(
             name=d.get('name', None),
             target_database_name=d.get('target_database_name', None),
             migration_setting=d.get('migrationSetting', None),
             source_setting=d.get('sourceSetting', None),
-            target_setting=d.get('targetSetting', None)))
+            target_setting=d.get('targetSetting', None),
+            selected_tables=table_input))
 
     return MigratePostgreSqlAzureDbForPostgreSqlSyncTaskInput(source_connection_info=source_connection_info,
                                                               target_connection_info=target_connection_info,
