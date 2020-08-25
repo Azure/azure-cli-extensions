9510ceb63a8204f953eac32a241dda0f332c0183
Zunli Hu <zuh@microsoft.com>
2020-08-25 10:38:19 +0800

    add license

--- src/storage-blob-preview/azext_storage_blob_preview/commands.py
+++ src/storage-blob-preview/azext_storage_blob_preview/commands.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from ._client_factory import cf_blob_client, cf_container_client, cf_blob_service, cf_blob_lease_client
 
 from azure.cli.core.commands import CliCommandType
