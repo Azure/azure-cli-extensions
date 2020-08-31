cbd142664837b810444773b69703d8d899be110b
Zunli Hu <zuh@microsoft.com>
2020-08-31 15:41:24 +0800

    rearrange command

--- src/storage-blob-preview/azext_storage_blob_preview/commands.py
+++ src/storage-blob-preview/azext_storage_blob_preview/commands.py
@@ -46,10 +46,8 @@ def load_command_table(self, _):  # pylint: disable=too-many-locals, too-many-st
         g.storage_custom_command_oauth('list', 'list_blobs', client_factory=cf_container_client,
                                        transform=transform_blob_list_output,
                                        table_transformer=transform_blob_output)
-        g.storage_custom_command_oauth('show', 'show_blob_v2', transform=transform_blob_json_output,
-                                       table_transformer=transform_blob_output,
-                                       exception_handler=show_exception_handler)
-        g.storage_custom_command_oauth('upload', 'upload_blob')
+
+
         g.storage_custom_command_oauth('copy start', 'copy_blob')
         g.storage_command_oauth('delete', 'delete_blob')
         g.storage_custom_command_oauth('download', 'download_blob')
@@ -60,9 +58,13 @@ def load_command_table(self, _):  # pylint: disable=too-many-locals, too-many-st
         g.storage_command_oauth('metadata show', 'get_blob_properties', exception_handler=show_exception_handler,
                                 transform=transform_metadata)
         g.storage_command_oauth('metadata update', 'set_blob_metadata')
-        g.storage_command_oauth('snapshot', 'create_snapshot')  # need to refine output
         g.storage_custom_command_oauth('set-tier', 'set_blob_tier_v2')
+        g.storage_command_oauth('snapshot', 'create_snapshot')  # need to refine output
+        g.storage_custom_command_oauth('show', 'show_blob_v2', transform=transform_blob_json_output,
+                                       table_transformer=transform_blob_output,
+                                       exception_handler=show_exception_handler)
         g.storage_command_oauth('undelete', 'undelete_blob')
+        g.storage_custom_command_oauth('upload', 'upload_blob')
 
     with self.command_group('storage blob', blob_service_sdk, resource_type=CUSTOM_DATA_STORAGE_BLOB,
                             min_api='2019-12-12',
