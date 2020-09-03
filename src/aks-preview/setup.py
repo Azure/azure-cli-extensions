745bbb3a1a6e188ffce50e933c85b94e6b20b0c5
Bo Wang <61758179+bowang-666@users.noreply.github.com>
2020-09-02 21:57:59 -0700

    Release aks-preview 0.4.62 (#2294)

--- src/aks-preview/setup.py
+++ src/aks-preview/setup.py
@@ -8,7 +8,7 @@
 from codecs import open as open1
 from setuptools import setup, find_packages
 
-VERSION = "0.4.61"
+VERSION = "0.4.62"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
