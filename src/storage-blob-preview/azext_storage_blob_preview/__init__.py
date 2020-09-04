bfe5db023509f1054dd5266d1dbb317f5bf30327
Zunli Hu <zuh@microsoft.com>
2020-09-04 17:26:45 +0800

    {storage-blob-preview} Storage blob features with api version 2019-12-12 (#2136)
    * update index
    
    * create new extension
    
    * add vendored SDK
    
    * create new extension
    
    * refine structure
    
    * support basic list
    
    * list with next marker
    
    * list with include
    
    * support delimiter
    
    * add test for list
    
    * support show
    
    * add upload interface
    
    * copy start interface
    
    * validate source url
    
    * refine
    
    * support secondary mode
    
    * add test
    
    * download
    
    * add sas token
    
    * generate sas
    
    * refine help
    
    * add container sas
    
    * sas pass
    
    * set-tier
    
    * tags  interface
    
    * fix requires_sync in python sdk
    
    * test filter
    
    * add if_tags
    
    * remove if tag in precondition
    
    * refine sdk
    
    * support upload with tier
    
    * fix SDK in generate_sas
    
    * add tag conditions for existing command
    
    * add metadata
    
    * support snaoshot
    
    * support undelete
    
    * add help for download
    
    * test pass for tag
    
    * add license
    
    * fix style
    
    * fix linter
    
    * fix setup.py
    
    * fix denpendency
    
    * revert
    
    * add test for append upload
    
    * add test for append upload
    
    * list test pass
    
    * test pass
    
    * test pass
    
    * rearrange command
    
    * add exist

--- src/storage-blob-preview/azext_storage_blob_preview/__init__.py
+++ src/storage-blob-preview/azext_storage_blob_preview/__init__.py
@@ -6,35 +6,28 @@
 from azure.cli.core import AzCommandsLoader
 from azure.cli.core.profiles import register_resource_type
 from azure.cli.core.commands import AzCommandGroup, AzArgumentContext
+from .profiles import CUSTOM_DATA_STORAGE_BLOB
 
-import azext_storage_preview._help  # pylint: disable=unused-import
-from .profiles import CUSTOM_DATA_STORAGE, CUSTOM_MGMT_STORAGE, CUSTOM_MGMT_PREVIEW_STORAGE, CUSTOM_DATA_STORAGE_ADLS
+from ._help import helps  # pylint: disable=unused-import
 
 
 class StorageCommandsLoader(AzCommandsLoader):
     def __init__(self, cli_ctx=None):
         from azure.cli.core.commands import CliCommandType
-
-        register_resource_type('latest', CUSTOM_DATA_STORAGE, '2018-03-28')
-        register_resource_type('latest', CUSTOM_DATA_STORAGE_ADLS, '2019-02-02-preview')
-        register_resource_type('latest', CUSTOM_MGMT_STORAGE, '2018-07-01')
-        register_resource_type('latest', CUSTOM_MGMT_PREVIEW_STORAGE, '2018-03-01-preview')
-        storage_custom = CliCommandType(operations_tmpl='azext_storage_preview.custom#{}')
-
+        register_resource_type('latest', CUSTOM_DATA_STORAGE_BLOB, '2019-12-12')
+        storage_custom = CliCommandType(operations_tmpl='azure.cli.command_modules.storage.custom#{}')
         super(StorageCommandsLoader, self).__init__(cli_ctx=cli_ctx,
-                                                    resource_type=CUSTOM_DATA_STORAGE,
+                                                    resource_type=CUSTOM_DATA_STORAGE_BLOB,
                                                     custom_command_type=storage_custom,
                                                     command_group_cls=StorageCommandGroup,
                                                     argument_context_cls=StorageArgumentContext)
 
     def load_command_table(self, args):
-        super(StorageCommandsLoader, self).load_command_table(args)
         from .commands import load_command_table
         load_command_table(self, args)
         return self.command_table
 
     def load_arguments(self, command):
-        super(StorageCommandsLoader, self).load_arguments(command)
         from ._params import load_arguments
         load_arguments(self, command)
 
@@ -58,6 +51,7 @@ class StorageArgumentContext(AzArgumentContext):
 
     def register_content_settings_argument(self, settings_class, update, arg_group=None, guess_from_file=None):
         from ._validators import get_content_setting_validator
+        from azure.cli.core.commands.parameters import get_three_state_flag
 
         self.ignore('content_settings')
         self.extra('content_type', default=None, help='The content MIME type.', arg_group=arg_group,
@@ -69,6 +63,13 @@ class StorageArgumentContext(AzArgumentContext):
                         'used to attach additional metadata.')
         self.extra('content_cache_control', default=None, help='The cache control string.', arg_group=arg_group)
         self.extra('content_md5', default=None, help='The content\'s MD5 hash.', arg_group=arg_group)
+        if update:
+            self.extra('clear_content_settings', help='If this flag is set, then if any one or more of the '
+                       'following properties (--content-cache-control, --content-disposition, --content-encoding, '
+                       '--content-language, --content-md5, --content-type) is set, then all of these properties are '
+                       'set together. If a value is not provided for a given property when at least one of the '
+                       'properties listed below is set, then that property will be cleared.',
+                       arg_type=get_three_state_flag())
 
     def register_path_argument(self, default_file_param=None, options_list=None):
         from ._validators import get_file_path_validator
@@ -85,7 +86,7 @@ class StorageArgumentContext(AzArgumentContext):
         self.ignore('directory_name')
 
     def register_source_uri_arguments(self, validator, blob_only=False):
-        self.argument('source_uri', options_list=('--source-uri', '-u'), validator=validator, required=False,
+        self.argument('source_url', options_list=('--source-uri', '-u'), validator=validator, required=False,
                       arg_group='Copy Source')
         self.extra('source_sas', default=None, arg_group='Copy Source',
                    help='The shared access signature for the source storage account.')
@@ -105,31 +106,31 @@ class StorageArgumentContext(AzArgumentContext):
             self.extra('source_share', default=None, arg_group='Copy Source',
                        help='The share name for the source storage account.')
 
-    def register_common_storage_account_options(self):
-        from azure.cli.core.commands.parameters import get_three_state_flag, get_enum_type
-        from ._validators import validate_encryption_services
+    def register_precondition_options(self, prefix=''):
+        self.extra('{}if_modified_since'.format(prefix), arg_group='Precondition',
+                   help="Commence only if modified since supplied UTC datetime (Y-m-d'T'H:M'Z').")
+        self.extra('{}if_unmodified_since'.format(prefix), arg_group='Precondition',
+                   help="Commence only if modified since supplied UTC datetime (Y-m-d'T'H:M'Z').")
+        self.extra('{}if_match'.format(prefix), arg_group='Precondition',
+                   help="An ETag value, or the wildcard character (*). Specify this header to perform the "
+                   "operation only if the resource's ETag matches the value specified.")
+        self.extra('{}if_none_match'.format(prefix), arg_group='Precondition',
+                   help="An ETag value, or the wildcard character (*). Specify this header to perform "
+                   "the operation only if the resource's ETag does not match the value specified. Specify the wildcard "
+                   "character (*) to perform the operation only if the resource does not exist, and fail the operation "
+                   "if it does exist.")
+        self.extra('{}if_tags_match_condition'.format(prefix), arg_group='Precondition',
+                   options_list=['--{}tags-condition'.format(prefix.replace('_', '-'))],
+                   help='Specify a SQL where clause on blob tags to operate only on blobs with a matching value.')
 
-        t_access_tier, t_sku_name, t_encryption_services = self.command_loader.get_models(
-            'AccessTier', 'SkuName', 'EncryptionServices', resource_type=CUSTOM_MGMT_STORAGE)
+    def register_blob_arguments(self):
+        self.extra('blob_name', required=True)
+        self.extra('container_name', required=True)
+        self.extra('timeout', help='Request timeout in seconds. Applies to each call to the service.', type=int)
 
-        self.argument('https_only', help='Allows https traffic only to storage service.',
-                      arg_type=get_three_state_flag())
-        self.argument('sku', help='The storage account SKU.', arg_type=get_enum_type(t_sku_name))
-        self.argument('assign_identity', action='store_true', resource_type=CUSTOM_MGMT_STORAGE,
-                      min_api='2017-06-01',
-                      help='Generate and assign a new Storage Account Identity for this storage account for use '
-                           'with key management services like Azure KeyVault.')
-        self.argument('access_tier', arg_type=get_enum_type(t_access_tier),
-                      help='The access tier used for billing StandardBlob accounts. Cannot be set for StandardLRS, '
-                           'StandardGRS, StandardRAGRS, or PremiumLRS account types. It is required for '
-                           'StandardBlob accounts during creation')
-
-        if t_encryption_services:
-            encryption_choices = list(
-                t_encryption_services._attribute_map.keys())  # pylint: disable=protected-access
-            self.argument('encryption_services', arg_type=get_enum_type(encryption_choices),
-                          resource_type=CUSTOM_MGMT_STORAGE, min_api='2016-12-01', nargs='+',
-                          validator=validate_encryption_services, help='Specifies which service(s) to encrypt.')
+    def register_container_arguments(self):
+        self.extra('container_name', required=True)
+        self.extra('timeout', help='Request timeout in seconds. Applies to each call to the service.', type=int)
 
 
 class StorageCommandGroup(AzCommandGroup):
@@ -148,7 +149,7 @@ class StorageCommandGroup(AzCommandGroup):
             self._register_data_plane_oauth_arguments(command_name)
 
     def storage_command_oauth(self, *args, **kwargs):
-        _merge_new_exception_handler(kwargs, self.get_handler_suppress_403())
+        _merge_new_exception_handler(kwargs, self.get_handler_suppress_some_400())
         self.storage_command(*args, oauth=True, **kwargs)
 
     def storage_custom_command(self, name, method_name, oauth=False, **kwargs):
@@ -158,32 +159,39 @@ class StorageCommandGroup(AzCommandGroup):
             self._register_data_plane_oauth_arguments(command_name)
 
     def storage_custom_command_oauth(self, *args, **kwargs):
-        _merge_new_exception_handler(kwargs, self.get_handler_suppress_403())
+        _merge_new_exception_handler(kwargs, self.get_handler_suppress_some_400())
         self.storage_custom_command(*args, oauth=True, **kwargs)
 
-    def get_handler_suppress_403(self):
+    @classmethod
+    def get_handler_suppress_some_400(cls):
         def handler(ex):
-            from azure.cli.core.profiles import get_sdk
-            from knack.log import get_logger
-
-            logger = get_logger(__name__)
-            t_error = get_sdk(self.command_loader.cli_ctx,
-                              CUSTOM_DATA_STORAGE,
-                              'common._error#AzureHttpError')
-            if isinstance(ex, t_error) and ex.status_code == 403:
-                message = """
+            if hasattr(ex, 'status_code') and ex.status_code == 403 and hasattr(ex, 'error_code'):
+                # TODO: Revisit the logic here once the service team updates their response
+                if ex.error_code == 'AuthorizationPermissionMismatch':
+                    message = """
 You do not have the required permissions needed to perform this operation.
 Depending on your operation, you may need to be assigned one of the following roles:
-    "Storage Blob Data Contributor (Preview)"
-    "Storage Blob Data Reader (Preview)"
-    "Storage Queue Data Contributor (Preview)"
-    "Storage Queue Data Reader (Preview)"
+    "Storage Blob Data Contributor"
+    "Storage Blob Data Reader"
+    "Storage Queue Data Contributor"
+    "Storage Queue Data Reader"
 
 If you want to use the old authentication method and allow querying for the right account key, please use the "--auth-mode" parameter and "key" value.
-                """
-                logger.error(message)
-                return
-            raise ex
+                    """
+                    ex.args = (message,)
+                elif ex.error_code == 'AuthorizationFailure':
+                    message = """
+The request may be blocked by network rules of storage account. Please check network rule set using 'az storage account show -n accountname --query networkRuleSet'.
+If you want to change the default action to apply when no rule matches, please use 'az storage account update'.
+                    """
+                    ex.args = (message,)
+                elif ex.error_code == 'AuthenticationFailed':
+                    message = """
+Authentication failure. This may be caused by either invalid account key, connection string or sas token value provided for your storage account.
+                    """
+                    ex.args = (message,)
+            if hasattr(ex, 'status_code') and ex.status_code == 409 and ex.error_code == 'NoPendingCopyOperation':
+                pass
 
         return handler
 
@@ -196,7 +204,6 @@ If you want to use the old authentication method and allow querying for the righ
             return
 
         group_name = 'Storage Account'
-
         command.add_argument('account_name', '--account-name', required=False, default=None,
                              arg_group=group_name,
                              completer=get_resource_name_completion_list('Microsoft.Storage/storageAccounts'),
@@ -240,12 +247,10 @@ def _merge_new_exception_handler(kwargs, handler):
     first = kwargs.get('exception_handler')
 
     def new_handler(ex):
-        try:
-            handler(ex)
-        except Exception:  # pylint: disable=broad-except
-            if not first:
-                raise
-            first(ex)
+        handler(ex)
+        if not first:
+            raise ex
+        first(ex)
     kwargs['exception_handler'] = new_handler
 
 
