1a9e247eccd7d904f24602d1eafd548e6cd623be
shiyue <shiyuezju@gmail.com>
2020-09-17 22:52:44 +0800

    handle error when read last exception failed

--- src/next/azext_next/custom.py
+++ src/next/azext_next/custom.py
@@ -27,11 +27,28 @@ def _get_last_exception(cmd):
     '''Get last executed command from local log files'''
     import os
     history_file_name = os.path.join(cmd.cli_ctx.config.config_dir, 'recommendation', 'exception_history.log')
+    if not os.path.exists(history_file_name):
+        return ''
     with open(history_file_name, "r") as f:
         lines = f.read().splitlines()
         return lines[-1].replace('Message: ', '')
     return ''
 
+
+def _process_exception(exception_str):
+    '''Extract more information from the exception string'''
+    return exception_str
+
+
+def _handle_error_no_exception_found():
+    '''You choose to solve the previous problems but no exception found'''
+    from azure.cli.core.azclierror import AzCLIError
+    from azure.cli.core.azclierror import AzCLIErrorType
+    error_msg = 'The error information is missing, please try the original command again.'
+    recommendation = 'You can set the recommendation type to 1 to get all available recommendations.'
+    az_error = AzCLIError(AzCLIErrorType.ClientError, error_msg)
+    az_error.set_recommendation(recommendation)
+    az_error.print_error()
 
 def _update_last_cmd(cmd):
     import os
@@ -40,7 +57,7 @@ def _update_last_cmd(cmd):
         f.write("{}\n".format(cmd))
 
 
-def _get_recommend_from_api(last_cmd, type, top_num=5, extra_data=None):  # pylint: disable=unused-argument
+def _get_recommend_from_api(last_cmd, type, top_num=5, error_info=None):  # pylint: disable=unused-argument
     '''query next command from web api'''
     import requests
     url = _get_api_url()
@@ -48,7 +65,7 @@ def _get_recommend_from_api(last_cmd, type, top_num=5, extra_data=None):  # pyli
         "command": last_cmd,
         "type": type,
         "top_num": top_num,
-        'extra_data': extra_data
+        'error_info': error_info
     }
     response = requests.post(url, json.dumps(payload))
     if response.status_code != 200:
@@ -94,7 +111,13 @@ Please select the type of recommendation you need:
     print(msg)
     option = _read_int("What kind of recommendation do you want? (RETURN is to set all): ", 1)
     last_cmd = _get_last_cmd(cmd)
-    recommends = _get_recommend_from_api(last_cmd, option)
+    last_exception = _get_last_exception(cmd)
+    processed_exception = _process_exception(last_exception)
+    if option == 2 and not processed_exception:
+        _handle_error_no_exception_found()
+        return None
+
+    recommends = _get_recommend_from_api(last_cmd, option, error_info=processed_exception)
     if not recommends:
         raise CLIError("Failed to get recommend for '{}'.".format(last_cmd))
     print()
