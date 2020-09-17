6f54ca58b3b2eb54120621acbd158ce0d5c5d150
Tyler James Leonhardt <tylerl0706@gmail.com>
2020-09-17 07:40:21 -0700

    rev version to 0.2.0 (#2383)

--- src/connectedmachine/setup.py
+++ src/connectedmachine/setup.py
@@ -10,7 +10,7 @@ from codecs import open
 from setuptools import setup, find_packages
 
 # HISTORY.rst entry.
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 try:
     from azext_connectedmachine.manual.version import VERSION
 except ImportError:
