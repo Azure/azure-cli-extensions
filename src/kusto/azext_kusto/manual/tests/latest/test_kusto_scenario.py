d13726c433f8cbd160006c88ea7c17d73ee74a18
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2020-10-12 10:06:16 +0800

    fix-kusto-test (#8)

--- src/kusto/azext_kusto/manual/tests/latest/test_kusto_scenario.py
+++ src/kusto/azext_kusto/manual/tests/latest/test_kusto_scenario.py
@@ -22,7 +22,10 @@ TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
 @try_manual
 def setup(test, rg):
-    test.kwargs.update({'eventhub_namespace': "codegenlivetest", 'eventhub_name': 'livetest'})
+    test.kwargs.update({
+        'eventhub_namespace': test.create_random_name(prefix='codegenlivetest', length=20),
+        'eventhub_name': test.create_random_name(prefix='livetest', length=15)
+    })
     test.cmd('az eventhubs namespace create --name {eventhub_namespace} -g {rg}')
     test.cmd('az eventhubs eventhub create --name {eventhub_name} --namespace-name {eventhub_namespace} -g {rg}')
 
