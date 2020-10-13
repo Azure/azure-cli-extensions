204b786b915e1763288135f8b796ab74239c4a77
SDK Automation <sdkautomation@microsoft.com>
2020-10-13 01:59:50 +0000

    Generated from b07d84ae7cf0cc4be3d7085d74f76ddceda94bd5

--- src/kusto/setup.py
+++ src/kusto/setup.py
@@ -10,9 +10,9 @@ from codecs import open
 from setuptools import setup, find_packages
 
 # HISTORY.rst entry.
-VERSION = '0.1.1'
+VERSION = '0.1.0'
 try:
-    from .manual.version import VERSION
+    from azext_kusto.manual.version import VERSION
 except ImportError:
     pass
 
