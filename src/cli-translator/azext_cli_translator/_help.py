e94981a0faf45ced34828b3fb4ce9d1d12135677
Xiaojian Xu <xiaojxu@microsoft.com>
2020-10-23 09:09:54 +0800

    fix CI error

--- src/cli-translator/azext_cli_translator/_help.py
+++ src/cli-translator/azext_cli_translator/_help.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+# pylint: disable=line-too-long
 
 from knack.help_files import helps
 
@@ -20,7 +21,6 @@ helps['cli-translator arm translate'] = """
     short-summary: Translate ARM template to CLI scripts
     examples:
         - name: Translate ARM template.json and parameters.json to CLI scripts
-          text: |
-            az cli-translator arm translate --target-subscription 00000000-0000-0000-0000-000000000000
-            --resource-group groupName --template armTemplatePath --parameters armParametersPath
+          text: |-
+            az cli-translator arm translate --target-subscription 00000000-0000-0000-0000-000000000000 --resource-group groupName --template armTemplatePath --parameters armParametersPath
 """
