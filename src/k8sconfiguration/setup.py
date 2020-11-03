bcc6739c1fa28c3aa5da50b038c70d104905c85b
nanthi <nanthi@NANTHI01>
2020-11-03 10:27:00 -0800

    Updated paramiko version, etc.

--- src/k8sconfiguration/setup.py
+++ src/k8sconfiguration/setup.py
@@ -33,7 +33,7 @@ CLASSIFIERS = [
 ]
 
 # TODO: Add any additional SDK dependencies here
-DEPENDENCIES = ["pycryptodome~=3.9.8", "paramiko~=2.7"]
+DEPENDENCIES = ["pycryptodome~=3.9.8", "paramiko~=2.6.0"]
 
 with open('README.rst', 'r', encoding='utf-8') as f:
     README = f.read()
