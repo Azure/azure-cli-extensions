9cd92563dbd2cc89a459fe558782a6c8841cc171
Harold Zeng <haroldrandom@gmail.com>
2020-11-06 08:40:23 +0000

    upgrade API version

--- src/azure-firewall/azext_firewall/profiles.py
+++ src/azure-firewall/azext_firewall/profiles.py
@@ -5,4 +5,4 @@
 
 from azure.cli.core.profiles import CustomResourceType
 
-CUSTOM_FIREWALL = CustomResourceType('azext_firewall.vendored_sdks.v2020_05_01', 'NetworkManagementClient')
+CUSTOM_FIREWALL = CustomResourceType('azext_firewall.vendored_sdks.v2020_07_01', 'NetworkManagementClient')
