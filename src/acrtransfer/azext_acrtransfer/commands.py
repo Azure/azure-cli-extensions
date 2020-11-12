60849f902e313e03b910461ddeebe90a832f4520
Matt Benedix <mabenedi@microsoft.com>
2020-11-12 12:17:57 -0800

    add subcommand groups

--- src/acrtransfer/azext_acrtransfer/commands.py
+++ src/acrtransfer/azext_acrtransfer/commands.py
@@ -16,11 +16,24 @@ def load_command_table(self, _):
 
 
     with self.command_group('acrtransfer', acrtransfer_sdk, client_factory=cf_acrtransfer) as g:
-        g.custom_command('create', 'create_acrtransfer')
-        g.custom_command('delete', 'delete_acrtransfer')
-        g.custom_command('list', 'list_acrtransfer')
-        g.custom_command('show', 'get_acrtransfer')
-        g.generic_update_command('update', setter_name='update', custom_func_name='update_acrtransfer')
+        g.custom_command('importpipeline create', 'create_importpipeline')
+        g.custom_command('importpipeline delete', 'delete_importpipeline')
+        g.custom_command('importpipeline list', 'list_importpipeline')
+        g.custom_command('importpipeline show', 'get_importpipeline')
+        g.custom_command('importpipeline update', 'update_importpipeline')
+
+        g.custom_command('exportpipeline create', 'create_exportpipeline')
+        g.custom_command('exportpipeline delete', 'delete_exportpipeline')
+        g.custom_command('exportpipeline list', 'list_exportpipeline')
+        g.custom_command('exportpipeline show', 'get_exportpipeline')
+        g.custom_command('exportpipeline update', 'update_exportpipeline')
+
+        g.custom_command('pipelinerun create', 'create_pipelinerun')
+        g.custom_command('pipelinerun delete', 'delete_pipelinerun')
+        g.custom_command('pipelinerun list', 'list_pipelinerun')
+        g.custom_command('pipelinerun show', 'get_pipelinerun')
+        g.custom_command('pipelinerun update', 'update_pipelinerun')
+        #g.generic_update_command('update', setter_name='update', custom_func_name='update_acrtransfer')
 
 
     with self.command_group('acrtransfer', is_preview=True):
--- src/acrtransfer/azext_acrtransfer/custom.py
+++ src/acrtransfer/azext_acrtransfer/custom.py
@@ -6,20 +6,53 @@
 from knack.util import CLIError
 
 
-def create_acrtransfer(cmd, client, resource_group_name, registry_name, location=None, tags=None):
-    raise CLIError('TODO: Implement `acrtransfer create`')
+def create_importpipeline(cmd, client, resource_group_name, registry_name, location=None, tags=None):
+    raise CLIError('TODO: Implement `importpipeline create`')
 
+def list_importpipeline(cmd, client, resource_group_name=None):
+    raise CLIError('TODO: Implement `importpipeline list`')
 
-def list_acrtransfer(cmd, client, resource_group_name=None):
-    raise CLIError('TODO: Implement `acrtransfer list`')
+def delete_importpipeline(cmd, client, resource_group_name=None):
+    raise CLIError('TODO: Implement `importpipeline list`')
 
-def delete_acrtransfer(cmd, client, resource_group_name=None):
-    raise CLIError('TODO: Implement `acrtransfer list`')
+def get_importpipeline(cmd, client, resource_group_name=None):
+    raise CLIError('TODO: Implement `importpipeline list`')
 
-def get_acrtransfer(cmd, client, resource_group_name=None):
-    raise CLIError('TODO: Implement `acrtransfer list`')
+def update_importpipeline(cmd, instance, tags=None):
+    with cmd.update_context(instance) as c:
+        c.set_param('tags', tags)
+    return instance
 
-def update_acrtransfer(cmd, instance, tags=None):
+def create_exportpipeline(cmd, client, resource_group_name, registry_name, location=None, tags=None):
+    raise CLIError('TODO: Implement `exportpipeline create`')
+
+def list_exportpipeline(cmd, client, resource_group_name=None):
+    raise CLIError('TODO: Implement `exportpipeline list`')
+
+def delete_exportpipeline(cmd, client, resource_group_name=None):
+    raise CLIError('TODO: Implement `exportpipeline list`')
+
+def get_exportpipeline(cmd, client, resource_group_name=None):
+    raise CLIError('TODO: Implement `exportpipeline list`')
+
+def update_exportpipeline(cmd, instance, tags=None):
+    with cmd.update_context(instance) as c:
+        c.set_param('tags', tags)
+    return instance
+
+def create_pipelinerun(cmd, client, resource_group_name, registry_name, location=None, tags=None):
+    raise CLIError('TODO: Implement `pipelinerun create`')
+
+def list_pipelinerun(cmd, client, resource_group_name=None):
+    raise CLIError('TODO: Implement `pipelinerun list`')
+
+def delete_pipelinerun(cmd, client, resource_group_name=None):
+    raise CLIError('TODO: Implement `pipelinerun list`')
+
+def get_pipelinerun(cmd, client, resource_group_name=None):
+    raise CLIError('TODO: Implement `pipelinerun list`')
+
+def update_pipelinerun(cmd, instance, tags=None):
     with cmd.update_context(instance) as c:
         c.set_param('tags', tags)
     return instance
