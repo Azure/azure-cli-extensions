75faf3d2d278667a54dddf2fc89b30bee8090e9c
Limin Gu <48228819+limingu@users.noreply.github.com>
2020-11-12 20:02:59 -0800

    Update src/datadog/azext_datadog/manual/commands.py
    Co-authored-by: Feng Zhou <55177366+fengzhou-msft@users.noreply.github.com>

--- src/datadog/azext_datadog/manual/commands.py
+++ src/datadog/azext_datadog/manual/commands.py
@@ -8,6 +8,5 @@ from azure.cli.core.commands import CliCommandType
 
 def load_command_table(self, _):
 
-    datadog_link = CliCommandType(operations_tmpl='azure.cli.extension.datadog.link#{}')
-    with self.command_group('datadog link', datadog_link, is_experimental=True) as g:
+    with self.command_group('datadog link', is_experimental=True) as g:
         g.custom_command('create', 'datadog_link_create')
