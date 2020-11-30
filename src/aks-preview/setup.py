74f1143c317100487e919169cde5c0a55753ea79
Akshay Gupta <aksgupta@microsoft.com>
2020-11-29 18:01:25 -0800

    bump version to 0.4.69

--- src/aks-preview/setup.py
+++ src/aks-preview/setup.py
@@ -8,7 +8,7 @@
 from codecs import open as open1
 from setuptools import setup, find_packages
 
-VERSION = "0.4.68"
+VERSION = "0.4.69"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
