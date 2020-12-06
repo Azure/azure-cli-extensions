3736664d9e35b90a29bab14ac7b2d373113e3e9a
Matt Benedix <mabenedi@microsoft.com>
2020-12-06 14:11:22 -0800

    change parameter names

--- src/acrtransfer/azext_acrtransfer/importpipeline.py
+++ src/acrtransfer/azext_acrtransfer/importpipeline.py
@@ -7,7 +7,7 @@
 from .vendored_sdks.containerregistry.v2019_12_01_preview.models._models_py3 import ImportPipeline, ImportPipelineSourceProperties, PipelineTriggerProperties, PipelineSourceTriggerProperties
 from .utility_functions import print_poll_output, create_identity_properties, print_keyvault_policy_output, print_pipeline_output, print_lite_pipeline_output
 
-def create_importpipeline(cmd, client, resource_group_name, registry_name, import_pipeline_name, keyvault_secret_uri, storage_account_container_uri, options=None, user_assigned_identity_resource_id=None):
+def create_importpipeline(cmd, client, resource_group_name, registry_name, import_pipeline_name, keyvault_secret_uri, storage_account_container_uri, options=None, user_assigned_identity_resource_id=None, source_trigger_enabled="True"):
     keyvault_secret_uri = keyvault_secret_uri.lower()
     storage_account_container_uri = storage_account_container_uri.lower()
 
@@ -19,9 +19,8 @@ def create_importpipeline(cmd, client, resource_group_name, registry_name, impor
     identity_properties = create_identity_properties(user_assigned_identity_resource_id)
     import_pipeline_source_properties = ImportPipelineSourceProperties(key_vault_uri=keyvault_secret_uri, uri=storage_account_container_uri)
 
-    source_trigger_status = "Disabled" if "DisableSourceTrigger" in options_list else "Enabled"
-    if source_trigger_status == "Disabled":
-        options_list.remove("DisableSourceTrigger")
+    source_trigger_enabled = False if source_trigger_enabled.lower() == "false" else True
+    source_trigger_status = "Disabled" if not source_trigger_enabled else "Enabled"
 
     pipeline_source_trigger_properties = PipelineSourceTriggerProperties(status=source_trigger_status)
     pipeline_trigger_properties = PipelineTriggerProperties(source_trigger=pipeline_source_trigger_properties)
