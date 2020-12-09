1d9e645f74167d819180f34fe70ef1b4a392ce68
Mark Baker <marbaker@microsoft.com>
2020-12-08 19:19:15 -0500

    Fixing some linter errors

--- src/healthcareapis/azext_healthcareapis/vendored_sdks/subscription/operations/_private_endpoint_connection_operations.py
+++ src/healthcareapis/azext_healthcareapis/vendored_sdks/subscription/operations/_private_endpoint_connection_operations.py
@@ -25,6 +25,7 @@ if TYPE_CHECKING:
     T = TypeVar('T')
     ClsType = Optional[Callable[[PipelineResponse[HttpRequest, HttpResponse], T, Dict[str, Any]], Any]]
 
+
 class PrivateEndpointConnectionOperations(object):
     """PrivateEndpointConnectionOperations operations.
 
