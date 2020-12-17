c454ca1b2b443a9694bb9410c37a1dca12fabeac
Harold Zeng <haroldrandom@gmail.com>
2020-12-17 04:49:44 +0000

    fix style

--- src/azure-firewall/azext_firewall/tests/latest/test_azure_firewall_scenario.py
+++ src/azure-firewall/azext_firewall/tests/latest/test_azure_firewall_scenario.py
@@ -459,8 +459,7 @@ class AzureFirewallScenario(ScenarioTest):
                      self.check('ruleCollections[2].rules[0].ruleType', 'ApplicationRule'),
                      self.check('ruleCollections[2].rules[0].terminateTls', True),
                      self.check('ruleCollections[2].rules[0].targetUrls', "['www.google.com', 'www.bing.com']")
-                ]
-            )
+                ])
 
         self.cmd('az network firewall policy rule-collection-group collection add-nat-collection -n nat-collection-2 \
                                  --policy-name {policy} --rule-collection-group-name {collectiongroup} -g {rg} --collection-priority 1000 \
