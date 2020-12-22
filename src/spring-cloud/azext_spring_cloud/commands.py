37d3ab6911d0aa39393909725afdb0f526df36ee
Qingyi Liu <qingyliu@microsoft.com>
2020-12-22 10:48:01 +0800

    delete supports_no_wait=True

--- src/spring-cloud/azext_spring_cloud/commands.py
+++ src/spring-cloud/azext_spring_cloud/commands.py
@@ -100,7 +100,7 @@ def load_command_table(self, _):
         g.custom_command('unbind', 'domain_unbind')
 
     with self.command_group('spring-cloud app-insights', is_preview=True, client_factory=cf_spring_cloud_20201101preview) as g:
-        g.custom_command('update', 'app_insights_update', supports_no_wait=True)
+        g.custom_command('update', 'app_insights_update')
         g.custom_show_command('show', 'app_insights_show')
 
     with self.command_group('spring-cloud'):
