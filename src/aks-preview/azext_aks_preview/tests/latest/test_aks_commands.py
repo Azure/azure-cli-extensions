c4b2d6beed49fceef20a7e9a08570e5d2de60440
Akshay Gupta <aksgupta@microsoft.com>
2020-12-22 22:24:07 -0800

    revert arg change

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -171,7 +171,7 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             'name': aks_name
         })
 
-        create_cmd = 'aks create --resource-group={resource_group} --name={name} --enable-managed-identity --generate-ssh-keys ' \
+        create_cmd = 'aks create --resource-group={resource_group} --name={name} --enable-managed-identity --service-principal xxxx --client-secret yyyy --generate-ssh-keys ' \
                      '-a ingress-appgw --appgw-subnet-cidr 10.2.0.0/16 -o json'
         self.cmd(create_cmd, checks=[
             self.check('provisioningState', 'Succeeded'),
