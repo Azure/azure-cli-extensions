b26eded97b85bcafdaf8fd819f3657a780e6a8e6
jonathan-innis <jonathan.innis.ji@gmail.com>
2020-12-30 15:51:08 -0600

    Update the error message for validation failure

--- src/k8sconfiguration/azext_k8sconfiguration/_validators.py
+++ src/k8sconfiguration/azext_k8sconfiguration/_validators.py
@@ -33,9 +33,9 @@ def validate_configuration_name(configuration_name):
 def __validate_k8s_name(param_value, param_name, max_len):
     if len(param_value) > max_len:
         raise InvalidArgumentValueError(
-            'Invalid {0} parameter. Valid {0}s can be a maximum of {1} characters'.format(param_name, max_len),
-            'Try shortening the length of your {0}'.format(param_name))
+            'Invalid {0}'.format(param_name),
+            'Valid {0}s can be a maximum of {1} characters'.format(param_name, max_len))
     if not re.match(r'^[a-z0-9]([-a-z0-9]*[a-z0-9])?([a-z0-9]([-a-z0-9]*[a-z0-9])?)*$', param_value):
         raise InvalidArgumentValueError(
-            'Invalid {0} parameter. Valid {0}s can only contain lowercase alphanumeric characters and hyphens'.format(param_name),
-            'Try checking that your {0} only contains these characters'.format(param_name))
+            'Invalid {0}'.format(param_name),
+            'Valid {0}s can only contain lowercase alphanumeric characters and hyphens'.format(param_name))
