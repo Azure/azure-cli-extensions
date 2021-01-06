ed7758ca8ce4ced434eb08afe75654b879d43ba9
Lee Burton <lburton@microsoft.com>
2021-01-05 21:06:23 -0800

    Change to store_true

--- src/ssh/azext_ssh/_params.py
+++ src/ssh/azext_ssh/_params.py
@@ -25,7 +25,7 @@ def load_arguments(self, _):
         c.argument('private_key_file', options_list=['--private-key-file', '-i'], help='The RSA private key file path')
         c.argument('use_private_ip', options_list=['--prefer-private-ip'],
                    help='Will use a private IP if available. By default only public IPs are used.')
-        c.argument('overwrite', arg_type=get_three_state_flag(), options_list=['--overwrite'],
+        c.argument('overwrite', action='store_true', options_list=['--overwrite'],
                    help='Overwrites the config file if this flag is set')
 
     with self.argument_context('ssh cert') as c:
