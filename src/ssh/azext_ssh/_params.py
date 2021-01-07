39abed9a3de4f902fbd93c01980c56f2a56bfb05
Lee Burton <lburton@microsoft.com>
2021-01-06 17:11:43 -0800

    Update _params.py

--- src/ssh/azext_ssh/_params.py
+++ src/ssh/azext_ssh/_params.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+
 def load_arguments(self, _):
 
     with self.argument_context('ssh vm') as c:
