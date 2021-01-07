9429ba12cd521c153167961fabbf9311e1884ca6
Guoxun Wei <guwe@microsoft.com>
2021-01-07 16:52:44 +0800

    live only

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -467,6 +467,7 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             self.check('addonProfiles.ACCSGXDevicePlugin.config', None)
         ])
 
+    @live_only()
     @AllowLargeResponse()
     @ResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='westus2')
     def test_aks_create_with_virtual_node_addon(self, resource_group, resource_group_location):
