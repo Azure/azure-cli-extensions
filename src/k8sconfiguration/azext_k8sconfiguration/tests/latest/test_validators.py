a66106b9983f5bbc777073bfdfa19ce997754ded
Jonathan Innis <jonathan.innis.ji@gmail.com>
2021-01-10 21:27:35 -0600

    Update test_validators.py

--- src/k8sconfiguration/azext_k8sconfiguration/tests/latest/test_validators.py
+++ src/k8sconfiguration/azext_k8sconfiguration/tests/latest/test_validators.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 import unittest
 import base64
 from azure.cli.core.azclierror import InvalidArgumentValueError, MutuallyExclusiveArgumentError
