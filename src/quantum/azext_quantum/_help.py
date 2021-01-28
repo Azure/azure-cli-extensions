b0baffff6e8ef947df477763b813498c98fc3db7
Ricardo Espinoza <ricardoe@microsoft.com>
2021-01-28 02:11:59 -0800

    Add help to each individual command

--- src/quantum/azext_quantum/_help.py
+++ src/quantum/azext_quantum/_help.py
@@ -11,37 +11,115 @@ helps['quantum'] = """
     short-summary: Manage Azure Quantum Workspaces and submit jobs to Azure Quantum Providers.
 """
 
+helps['quantum execute'] = """
+    type: command
+    short-summary: Submit a job to run on Azure Quantum, and waits for the result.
+    examples:
+      - name: Submit the Q# program from the current folder and wait for the result.
+        text: |-
+            az quantum execute -g MyResourceGroup -w MyWorkspace -l MyLocation
+"""
+
+helps['quantum run'] = """
+    type: command
+    short-summary: Equivalent to `az quantum execute` 
+    examples:
+      - name: Submit the Q# program from the current folder and wait for the result.
+        text: |-
+            az quantum run -g MyResourceGroup -w MyWorkspace -l MyLocation
+"""
+
 helps['quantum job'] = """
     type: group
     short-summary: Manage jobs for Azure Quantum.
+"""
+
+helps['quantum job list'] = """
+    type: command
+    short-summary: Get the list of jobs in a Quantum Workspace.
     examples:
-      - name: Get the list of jobs from an Azure Quantum workspace
+      - name: Get the list of jobs from an Azure Quantum workspace.
         text: |-
-            az quantum job list -g MyResourceGroup -w MyWorkspace
-      - name: Submit the Q# program from the current folder
+            az quantum job list -g MyResourceGroup -w MyWorkspace -l MyLocation
+"""
+
+helps['quantum job output'] = """
+    type: command
+    short-summary: Get the results of running a Q# job.
+    examples:
+      - name: Print the results of a successful Azure Quantum job.
         text: |-
-            az quantum job submit -g MyResourceGroup -w MyWorkspace \\
-               -l MyLocation --job-name MyJob
-      - name: Get the status of an Azure Quantum job
-        text: |-
-            az quantum job show -g MyResourceGroup -w MyWorkspace \\
-                -id yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy --query status
-      - name: Print the results of a successful Azure Quantum job
-        text: |-
-            az quantum job output -g MyResourceGroup -w MyWorkspace \\
+            az quantum job output -g MyResourceGroup -w MyWorkspace -l MyLocation \\
                 -id yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy -o table
 """
+
+helps['quantum job show'] = """
+    type: command
+    short-summary: Get the job's status and details.
+    examples:
+      - name: Get the status of an Azure Quantum job.
+        text: |-
+            az quantum job show -g MyResourceGroup -w MyWorkspace -l MyLocation \\
+                -id yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy --query status
+"""
+
+helps['quantum job submit'] = """
+    type: command
+    short-summary: Submit a Q# project to run on Azure Quantum.
+    examples:
+      - name: Submit the Q# program from the current folder.
+        text: |-
+            az quantum job submit -g MyResourceGroup -w MyWorkspace -l MyLocation \\
+               -l MyLocation --job-name MyJob
+"""
+
+helps['quantum job wait'] = """
+    type: command
+    short-summary: Place the CLI in a waiting state until the job finishes running.
+    examples:
+      - name: Wait for completion of a job for 60 seconds.
+        text: |-
+            az quantum job wait -g MyResourceGroup -w MyWorkspace -l MyLocation \\
+                -id yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy --max-poll-wait-secs 60 -o table
+"""
+
 
 helps['quantum target'] = """
     type: group
     short-summary: Manage targets for Azure Quantum workspaces.
+"""
+
+helps['quantum target clear'] = """
+    type: command
+    short-summary: Clear the default target-id.
+    examples:
+      - name: Clear the default target-id.
+        text: |-
+            az quantum target clear
+"""
+
+helps['quantum target list'] = """
+    type: command
+    short-summary: Get the list of providers and their targets in an Azure Quantum workspace.
     examples:
       - name: Get the list of targets available in a Azure Quantum workspaces
         text: |-
             az quantum target list -g MyResourceGroup -w MyWorkspace -l MyLocation
-      - name: Select a default when submitting jobs to Azure Quantum
+"""
+
+helps['quantum target set'] = """
+    type: command
+    short-summary: Select the default target to use when submitting jobs to Azure Quantum.
+    examples:
+      - name: Select a default when submitting jobs to Azure Quantum.
         text: |-
             az quantum target set -t target-id
+"""
+
+helps['quantum target show'] = """
+    type: command
+    short-summary: Get the details of the given (or current) target to use when submitting jobs to Azure Quantum.
+    examples:
       - name: Show the currently selected default target
         text: |-
             az quantum target show
@@ -50,23 +128,82 @@ helps['quantum target'] = """
 helps['quantum workspace'] = """
     type: group
     short-summary: Manage Azure Quantum workspaces.
+"""
+
+helps['quantum workspace clear'] = """
+    type: command
+    short-summary: Clear the default Azure Quantum workspace.
     examples:
-      - name: Get the list of Azure Quantum workspaces available
+      - name: Clear the default Azure Quantum workspace if previously set.
         text: |-
-            az quantum workspace list
-      - name: Create a new Azure Quantum workspace
+            az quantum workspace clear
+"""
+
+helps['quantum workspace create'] = """
+    type: command
+    short-summary: Create a new Azure Quantum workspace.
+    examples:
+      - name: Create a new Azure Quantum workspace.
         text: |-
-            az quantum workspace create -g MyResourceGroup -w MyWorkspace -l MyLocation -sa MyStorageAccountName
-      - name: Delete an Azure Quantum workspace that is no longer being used
+            az quantum workspace create -g MyResourceGroup -w MyWorkspace -l MyLocation \\
+                -sa MyStorageAccountName
+"""
+
+helps['quantum workspace delete'] = """
+    type: command
+    short-summary: Delete the given (or current) Azure Quantum workspace.
+    examples:
+      - name: Delete an Azure Quantum workspace by name and group.
         text: |-
             az quantum workspace delete -g MyResourceGroup -w MyWorkspace
-      - name: Select a default Azure Quantum workspace for future commands
+      - name: Delete and clear the default Azure Quantum workspace (if one has been set).
         text: |-
-            az quantum workspace set -g MyResourceGroup -w MyWorkspace -l MyLocation
-      - name: List the quota information of a workspace
+            az quantum workspace delete
+"""
+
+helps['quantum workspace list'] = """
+    type: command
+    short-summary: Get the list of Azure Quantum workspaces available.
+    examples:
+      - name: Get the list of all Azure Quantum workspaces available.
+        text: |-
+            az quantum workspace list
+      - name: Get the list Azure Quantum workspaces available in a location.
+        text: |-
+            az quantum workspace list -l MyLocation
+
+"""
+
+helps['quantum workspace quotas'] = """
+    type: command
+    short-summary: List the quotas for the given (or current) Azure Quantum workspace.
+    examples:
+      - name: List the quota information of the default workspace if set.
+        text: |-
+            az quantum workspace quotas
+      - name: List the quota information of a specified Azure Quantum workspace.
         text: |-
             az quantum workspace quotas -g MyResourceGroup -w MyWorkspace -l MyLocation
-      - name: Show the currently selected default Azure Quantum workspace
+"""
+
+helps['quantum workspace set'] = """
+    type: command
+    short-summary: Select a default Azure Quantum workspace for future commands.
+    examples:
+      - name: Set the default Azure Quantum workspace.
+        text: |-
+            az quantum workspace set -g MyResourceGroup -w MyWorkspace -l MyLocation
+"""
+
+helps['quantum workspace show'] = """
+    type: command
+    short-summary: Get the details of the given (or current) Azure Quantum workspace.
+    examples:
+      - name: Show the currently selected default Azure Quantum workspace.
         text: |-
             az quantum workspace show
+      - name: Show the details of a provided Azure Quantum workspace.
+        text: |-
+            az quantum workspace show -g MyResourceGroup -w MyWorkspace -l MyLocation
+
 """
