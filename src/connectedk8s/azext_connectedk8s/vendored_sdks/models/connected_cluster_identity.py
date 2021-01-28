ce57645824f7f546aab8613a91e4dd46c52f44be
Arpit Gupta <arpgu@microsoft.com>
2021-01-28 11:48:02 +0530

    Updated sdk

--- src/connectedk8s/azext_connectedk8s/vendored_sdks/models/connected_cluster_identity.py
+++ src/connectedk8s/azext_connectedk8s/vendored_sdks/models/connected_cluster_identity.py
@@ -29,7 +29,8 @@ class ConnectedClusterIdentity(Model):
     :param type: Required. The type of identity used for the connected
      cluster. The type 'SystemAssigned, includes a system created identity. The
      type 'None' means no identity is assigned to the connected cluster.
-     Possible values include: 'None', 'SystemAssigned'
+     Possible values include: 'None', 'SystemAssigned'. Default value:
+     "SystemAssigned" .
     :type type: str or
      ~azure.mgmt.hybridkubernetes.models.ResourceIdentityType
     """
@@ -50,4 +51,4 @@ class ConnectedClusterIdentity(Model):
         super(ConnectedClusterIdentity, self).__init__(**kwargs)
         self.principal_id = None
         self.tenant_id = None
-        self.type = kwargs.get('type', None)
+        self.type = kwargs.get('type', "SystemAssigned")
