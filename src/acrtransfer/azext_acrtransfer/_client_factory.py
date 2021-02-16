353ba70e7bd157799ed574a769c4a43451760462
Matt Benedix <mabenedi@microsoft.com>
2021-02-16 09:41:24 -0800

    Update _client_factory.py

--- src/acrtransfer/azext_acrtransfer/_client_factory.py
+++ src/acrtransfer/azext_acrtransfer/_client_factory.py
@@ -4,6 +4,7 @@
 # --------------------------------------------------------------------------------------------
 # pylint: disable=line-too-long
 
+
 def cf_acrtransfer(cli_ctx, *_):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
     from azext_acrtransfer.vendored_sdks.containerregistry.v2019_12_01_preview._container_registry_management_client import ContainerRegistryManagementClient
