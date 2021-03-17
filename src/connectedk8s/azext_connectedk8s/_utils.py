85d99c6a5f958d1526d5402521c704054d1e4237
Arpit Gupta <arpgu@microsoft.com>
2021-03-17 13:28:05 +0530

    Added request retry, timeout adaptors

--- src/connectedk8s/azext_connectedk8s/_utils.py
+++ src/connectedk8s/azext_connectedk8s/_utils.py
@@ -23,12 +23,29 @@ from kubernetes.client.rest import ApiException
 from azext_connectedk8s._client_factory import _resource_client_factory
 import azext_connectedk8s._constants as consts
 from kubernetes import client as kube_client
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 
 logger = get_logger(__name__)
 
 # pylint: disable=line-too-long
 # pylint: disable=bare-except
+
+
+class TimeoutHTTPAdapter(HTTPAdapter):
+    def __init__(self, *args, **kwargs):
+        self.timeout = consts.DEFAULT_REQUEST_TIMEOUT
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+            del kwargs["timeout"]
+        super().__init__(*args, **kwargs)
+
+    def send(self, request, **kwargs):
+        timeout = kwargs.get("timeout")
+        if timeout is None:
+            kwargs["timeout"] = self.timeout
+        return super().send(request, **kwargs)
 
 
 def validate_location(cmd, location):
@@ -330,13 +347,19 @@ def flatten(dd, separator='.', prefix=''):
 
 
 def get_latest_kubernetes_version():
+    retries = Retry(total=3, backoff_factor=1, status_forcelist=[413, 429, 500, 502, 503, 504])
+    req_session = requests.Session()
+    adapter = TimeoutHTTPAdapter(max_retries=retries)
+    req_session.mount("https://", adapter)
+    req_session.mount("http://", adapter)
+
     url = "https://api.github.com/repos/kubernetes/kubernetes/releases/latest"
 
     payload = {}
     headers = {'Accept': 'application/vnd.github.v3+json'}
     try:
-        response = requests.request("GET", url, headers=headers, data=payload)
-        if response.ok:
+        response = req_session.request("GET", url, headers=headers, data=payload)
+        if response.status_code == 200:
             latest_release = json.loads(response.text)
             return latest_release["tag_name"]
         else:
--- src/connectedk8s/azext_connectedk8s/custom.py
+++ src/connectedk8s/azext_connectedk8s/custom.py
@@ -124,7 +124,7 @@ def create_connectedk8s(cmd, client, resource_group_name, cluster_name, https_pr
         if int(current_version_minor) < int(latest_version_minor) - 2:
             logger.warning("The Kubernetes cluster you are trying to connect to Azure Arc is of version {}, which is out of Azure Arc's support window. Learn more at {}".format(kubernetes_version, "https://aka.ms/ArcK8sVersionSupportPolicy"))
     except Exception as e:
-        logger.warning("Please ensure that the version of your Kubernetes cluster is within the Azure Arc's support window. Learn more at {}".format(kubernetes_version, "https://aka.ms/ArcK8sVersionSupportPolicy"))
+        logger.warning("Please ensure that the version of your Kubernetes cluster is within the Azure Arc's support window. Learn more at {}".format("https://aka.ms/ArcK8sVersionSupportPolicy"))
 
     if distribution == 'auto':
         kubernetes_distro = get_kubernetes_distro(configuration)  # (cluster heuristics)
