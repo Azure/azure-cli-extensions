0fcab91279e8c0476863e47173cb70b0b864d229
Atharva Mulmuley <atharvam@microsoft.com>
2021-03-19 22:23:29 +0530

    updated logging statement

--- src/connectedk8s/azext_connectedk8s/custom.py
+++ src/connectedk8s/azext_connectedk8s/custom.py
@@ -1152,7 +1152,7 @@ def enable_features(cmd, client, resource_group_name, cluster_name, features, ku
             features.remove("custom-locations")
             if len(features) == 0:
                 telemetry.set_user_fault()
-                raise CLIError("Based on the warning displayed above, 'custom-locations' cannot be enabled.")
+                raise CLIError("Failed to enable 'custom-locations' feature.")
 
     # Send cloud information to telemetry
     send_cloud_telemetry(cmd)
@@ -1961,7 +1961,7 @@ def check_cl_registration_and_get_oid(cmd):
                 enable_custom_locations = False
     except Exception as e:
         enable_custom_locations = False
-        logger.warning("Unable to fetch registration state of 'Microsoft.ExtendedLocation'. Failed to enable Custom Locations feature...")
+        logger.warning("Unable to fetch registration state of 'Microsoft.ExtendedLocation'. Failed to enable 'custom-locations' feature...")
         telemetry.set_exception(exception=e, fault_type=consts.Custom_Locations_Registration_Check_Fault_Type,
                                 summary='Unable to fetch status of Custom Locations RP registration.')
     return enable_custom_locations, custom_locations_oid
