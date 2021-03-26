4747a1d3f80248e95280e3dd87db882c98ddd8a5
Limin Gu <limgu@microsoft.com>
2021-03-26 14:19:00 -0700

    [Datadog] Release new CLI with stable api version

--- src/datadog/azext_datadog/vendored_sdks/datadog/aio/_microsoft_datadog_client.py
+++ src/datadog/azext_datadog/vendored_sdks/datadog/aio/_microsoft_datadog_client.py
@@ -6,16 +6,14 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import TYPE_CHECKING
+from typing import Any, Optional, TYPE_CHECKING
 
-from azure.mgmt.core import ARMPipelineClient
+from azure.mgmt.core import AsyncARMPipelineClient
 from msrest import Deserializer, Serializer
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
-    from typing import Any, Optional
-
-    from azure.core.credentials import TokenCredential
+    from azure.core.credentials_async import AsyncTokenCredential
 
 from ._configuration import MicrosoftDatadogClientConfiguration
 from .operations import MarketplaceAgreementsOperations
@@ -23,24 +21,24 @@ from .operations import MonitorsOperations
 from .operations import Operations
 from .operations import TagRulesOperations
 from .operations import SingleSignOnConfigurationsOperations
-from . import models
+from .. import models
 
 
 class MicrosoftDatadogClient(object):
     """MicrosoftDatadogClient.
 
     :ivar marketplace_agreements: MarketplaceAgreementsOperations operations
-    :vartype marketplace_agreements: microsoft_datadog_client.operations.MarketplaceAgreementsOperations
+    :vartype marketplace_agreements: microsoft_datadog_client.aio.operations.MarketplaceAgreementsOperations
     :ivar monitors: MonitorsOperations operations
-    :vartype monitors: microsoft_datadog_client.operations.MonitorsOperations
+    :vartype monitors: microsoft_datadog_client.aio.operations.MonitorsOperations
     :ivar operations: Operations operations
-    :vartype operations: microsoft_datadog_client.operations.Operations
+    :vartype operations: microsoft_datadog_client.aio.operations.Operations
     :ivar tag_rules: TagRulesOperations operations
-    :vartype tag_rules: microsoft_datadog_client.operations.TagRulesOperations
+    :vartype tag_rules: microsoft_datadog_client.aio.operations.TagRulesOperations
     :ivar single_sign_on_configurations: SingleSignOnConfigurationsOperations operations
-    :vartype single_sign_on_configurations: microsoft_datadog_client.operations.SingleSignOnConfigurationsOperations
+    :vartype single_sign_on_configurations: microsoft_datadog_client.aio.operations.SingleSignOnConfigurationsOperations
     :param credential: Credential needed for the client to connect to Azure.
-    :type credential: ~azure.core.credentials.TokenCredential
+    :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: The ID of the target subscription.
     :type subscription_id: str
     :param str base_url: Service URL
@@ -49,16 +47,15 @@ class MicrosoftDatadogClient(object):
 
     def __init__(
         self,
-        credential,  # type: "TokenCredential"
-        subscription_id,  # type: str
-        base_url=None,  # type: Optional[str]
-        **kwargs  # type: Any
-    ):
-        # type: (...) -> None
+        credential: "AsyncTokenCredential",
+        subscription_id: str,
+        base_url: Optional[str] = None,
+        **kwargs: Any
+    ) -> None:
         if not base_url:
             base_url = 'https://management.azure.com'
         self._config = MicrosoftDatadogClientConfiguration(credential, subscription_id, **kwargs)
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -75,15 +72,12 @@ class MicrosoftDatadogClient(object):
         self.single_sign_on_configurations = SingleSignOnConfigurationsOperations(
             self._client, self._config, self._serialize, self._deserialize)
 
-    def close(self):
-        # type: () -> None
-        self._client.close()
+    async def close(self) -> None:
+        await self._client.close()
 
-    def __enter__(self):
-        # type: () -> MicrosoftDatadogClient
-        self._client.__enter__()
+    async def __aenter__(self) -> "MicrosoftDatadogClient":
+        await self._client.__aenter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
-        self._client.__exit__(*exc_details)
+    async def __aexit__(self, *exc_details) -> None:
+        await self._client.__aexit__(*exc_details)
