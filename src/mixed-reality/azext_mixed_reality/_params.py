cdaf755497265f3262abaad9f40b5e9d3a32f438
msyyc <70930885+msyyc@users.noreply.github.com>
2021-04-09 16:09:01 +0800

    review

--- src/mixed-reality/azext_mixed_reality/_params.py
+++ src/mixed-reality/azext_mixed_reality/_params.py
@@ -19,7 +19,7 @@ def load_arguments(self, _):
         c.argument('resource_group_name', arg_type=resource_group_name_type)
         c.argument('account_name', type=str, help='Name of an mixed reality account.',
                    options_list=['--account-name', '--name', '-n', c.deprecate(target='--spatial-anchors-account-name', hide=True)], id_part='name')  # pylint: disable=line-too-long
-        c.argument('location', arg_type=get_location_type(self.cli_ctx), required=False, validator=get_default_location_from_resource_group)  # pylint: disable=line-too-long
+        c.argument('location', arg_type=get_location_type(self.cli_ctx), validator=get_default_location_from_resource_group)  # pylint: disable=line-too-long
         c.argument('tags', tags_type)
         c.argument('sku', action=AddSku, nargs='+', help='The sku associated with this account')
         c.argument('kind', action=AddSku, nargs='+', help='The kind of account, if supported')
@@ -32,8 +32,7 @@ def load_arguments(self, _):
         c.argument('resource_group_name', resource_group_name_type)
         c.argument('account_name', type=str, help='Name of an mixed reality account.', arg_type=name_type, id_part='name')  # pylint: disable=line-too-long
         c.argument('tags', tags_type)
-        c.argument('location', arg_type=get_location_type(self.cli_ctx), required=False,
-                   validator=get_default_location_from_resource_group)
+        c.argument('location', arg_type=get_location_type(self.cli_ctx), validator=get_default_location_from_resource_group)  # pylint: disable=line-too-long
         c.argument('sku', action=AddSku, nargs='+', help='The sku associated with this account')
         c.argument('kind', action=AddSku, nargs='+', help='The kind of account, if supported')
         c.argument('storage_account_name', type=str, help='The name of the storage account associated with this accountId')  # pylint: disable=line-too-long
