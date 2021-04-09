22e20a080c1bffcec9564d2ebdb11deaf8d1c801
Jun Sun <33297523+JunSun17@users.noreply.github.com>
2021-04-09 11:03:45 -0700

    {aks} aks-preview vendor 2021-03-01. (#3243)
    * aks-preview vendor 2021-03-01.
    
    * Update api version in recording.
    
    * Fix run command.
    
    * Update some test recording.

--- src/aks-preview/azext_aks_preview/custom.py
+++ src/aks-preview/azext_aks_preview/custom.py
@@ -53,7 +53,7 @@ from azure.graphrbac.models import (ApplicationCreateParameters,
                                     KeyCredential,
                                     ServicePrincipalCreateParameters,
                                     GetObjectsParameters)
-from .vendored_sdks.azure_mgmt_preview_aks.v2021_02_01.models import (ContainerServiceLinuxProfile,
+from .vendored_sdks.azure_mgmt_preview_aks.v2021_03_01.models import (ContainerServiceLinuxProfile,
                                                                       ManagedClusterWindowsProfile,
                                                                       ContainerServiceNetworkProfile,
                                                                       ManagedClusterServicePrincipalProfile,
@@ -2170,8 +2170,7 @@ def aks_runcommand(cmd, client, resource_group_name, name, command_string="", co
     if not command_string:
         raise CLIError('Command cannot be empty.')
 
-    request_payload = RunCommandRequest()
-    request_payload.command = command_string
+    request_payload = RunCommandRequest(command=command_string)
     request_payload.context = _get_command_context(command_files)
     if mc.aad_profile is not None and mc.aad_profile.managed:
         request_payload.cluster_token = _get_dataplane_aad_token(
