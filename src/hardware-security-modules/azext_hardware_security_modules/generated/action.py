650e904485bd72eafe4ee1ff45898882d85b293c
SDKAuto <sdkautomation@microsoft.com>
2021-04-12 21:17:35 +0000

    CodeGen from PR 13886 in Azure/azure-rest-api-specs change the value of license header (#13886)

--- src/hardware-security-modules/azext_hardware_security_modules/generated/action.py
+++ src/hardware-security-modules/azext_hardware_security_modules/generated/action.py
@@ -10,14 +10,14 @@
 # pylint: disable=protected-access
 
 import argparse
-from knack.util import CLIError
 from collections import defaultdict
+from knack.util import CLIError
 
 
-class AddNetworkProfileSubnet(argparse.Action):
+class AddSubnet(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         action = self.get_action(values, option_string)
-        namespace.network_profile_subnet = action
+        namespace.subnet = action
 
     def get_action(self, values, option_string):  # pylint: disable=no-self-use
         try:
@@ -33,13 +33,15 @@ class AddNetworkProfileSubnet(argparse.Action):
             v = properties[k]
             if kl == 'id':
                 d['id'] = v[0]
+            else:
+                raise CLIError('Unsupported Key {} is provided for parameter subnet. All possible keys are: id'.format(k))
         return d
 
 
-class AddNetworkProfileNetworkInterfaces(argparse._AppendAction):
+class AddNetworkInterfaces(argparse._AppendAction):
     def __call__(self, parser, namespace, values, option_string=None):
         action = self.get_action(values, option_string)
-        super(AddNetworkProfileNetworkInterfaces, self).__call__(parser, namespace, action, option_string)
+        super(AddNetworkInterfaces, self).__call__(parser, namespace, action, option_string)
 
     def get_action(self, values, option_string):  # pylint: disable=no-self-use
         try:
@@ -55,4 +57,7 @@ class AddNetworkProfileNetworkInterfaces(argparse._AppendAction):
             v = properties[k]
             if kl == 'private-ip-address':
                 d['private_ip_address'] = v[0]
+            else:
+                raise CLIError('Unsupported Key {} is provided for parameter network_interfaces. All possible keys '
+                               'are: private-ip-address'.format(k))
         return d
