d41104b31762370fb7695869c6bb92cffe069e4a
msyyc <70930885+msyyc@users.noreply.github.com>
2021-04-14 15:54:24 +0800

    [mixed-reality] Add new command group: `az remote-rendering-account` (#3215)
    * code test style version
    
    * Update _client_factory.py
    
    * credscan
    
    * linter
    
    * update help message
    
    * udpate setup.py
    
    * update help message
    
    * style
    
    * review
    
    * review
    
    * review
    
    * avoid breaking chagne
    
    * review
    
    * style
    
    * review
    
    * test
    
    * compatible with old version
    
    * review

---
+++ src/mixed-reality/HISTORY.rst
@@ -0,0 +1,17 @@
+.. :changelog:
+
+Release History
+===============
+
+0.0.3
+++++++
+Add new command `az remote-rendering-account`
+Add new command for `az spatial-anchors-account`
+
+0.0.2
+++++++
+Add azext_metadata.json in package
+
+0.0.1
+++++++
+* Initial release.
--- src/mixed-reality/azext_mixed_reality/tests/latest/test_remote_rendering_account.py
+++ src/mixed-reality/azext_mixed_reality/tests/latest/test_remote_rendering_account.py
@@ -7,54 +7,53 @@ from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer)
 from .recording_processors import KeyReplacer
 
 
-class SpatialAnchorsAccountScenarioTest(ScenarioTest):
+class RemoteRenderingAccountScenarioTest(ScenarioTest):
 
     def __init__(self, method_name):
-        super(SpatialAnchorsAccountScenarioTest, self).__init__(
+        super(RemoteRenderingAccountScenarioTest, self).__init__(
             method_name, recording_processors=[KeyReplacer()]
         )
 
     @ResourceGroupPreparer(location='eastus2', parameter_name_for_location='location')
-    def test_spatial_anchors_account_scenario(self, resource_group, location):
-
+    def test_remote_rendering_account_scenario(self, resource_group, location):
         self.kwargs.update({
             'rg': resource_group,
-            'account_name': 'spatial-anchors-account-name',
-            'account_name1': 'spatial-anchors-account-name1',
+            'account_name': 'remote-rendering-account-name',
+            'account_name1': 'remote-rendering-account-name1',
             'storage_account_name': 'storage_account_name',
             'storage_account_name1': 'storage_account_name1',
         })
 
         # Create with minimum parameters
-        self.cmd('spatial-anchors-account create -g {rg} -n {account_name}', checks=self.not_exists('tags'))
+        self.cmd('remote-rendering-account create -g {rg} -n {account_name}', checks=self.not_exists('tags'))
 
         # Create with more parameters
-        self.cmd('spatial-anchors-account create -g {rg} -n {account_name1} '
+        self.cmd('remote-rendering-account create -g {rg} -n {account_name1} '
                  '--storage-account-name {storage_account_name} --tag tag=tag',
                  checks=[self.exists('tags'),
                          self.check('storageAccountName', '{storage_account_name}')])
 
-        self.cmd('spatial-anchors-account update -g {rg} -n {account_name} '
+        self.cmd('remote-rendering-account update -g {rg} -n {account_name} '
                  '--storage-account-name {storage_account_name1}',
                  checks=self.check('storageAccountName', '{storage_account_name1}'))
 
-        self.cmd('spatial-anchors-account show -g {rg} -n {account_name}')
-        self.cmd('spatial-anchors-account list -g {rg}', checks=self.check('length(@)', 2))
-        self.cmd('spatial-anchors-account delete -g {rg} -n {account_name1}')
-        self.cmd('spatial-anchors-account list -g {rg}', checks=self.check('length(@)', 1))
+        self.cmd('remote-rendering-account show -g {rg} -n {account_name}')
+        self.cmd('remote-rendering-account list -g {rg}', checks=self.check('length(@)', 2))
+        self.cmd('remote-rendering-account delete -g {rg} -n {account_name1}')
+        self.cmd('remote-rendering-account list -g {rg}', checks=self.check('length(@)', 1))
 
         # key
         x = 'primaryKey'
         y = 'secondaryKey'
-        key = self.cmd('spatial-anchors-account key show -g {rg} -n {account_name}').get_output_in_json()
-        key1 = self.cmd('spatial-anchors-account key renew -g {rg} -n {account_name}').get_output_in_json()
+        key = self.cmd('remote-rendering-account key show -g {rg} -n {account_name}').get_output_in_json()
+        key1 = self.cmd('remote-rendering-account key renew -g {rg} -n {account_name}').get_output_in_json()
         self.assertEqual(key[y], key1[y])
         # self.assertNotEqual(key[x], key1[x])  # only for live test
 
-        key2 = self.cmd('spatial-anchors-account key renew -g {rg} -n {account_name} -k primary').get_output_in_json()
+        key2 = self.cmd('remote-rendering-account key renew -g {rg} -n {account_name} -k primary').get_output_in_json()
         self.assertEqual(key2[y], key1[y])
         # self.assertNotEqual(key2[x], key1[x])  # only for live test
 
-        key3 = self.cmd('spatial-anchors-account key renew -g {rg} -n {account_name} -k secondary').get_output_in_json()
+        key3 = self.cmd('remote-rendering-account key renew -g {rg} -n {account_name} -k secondary').get_output_in_json()
         self.assertEqual(key2[x], key3[x])
         # self.assertNotEqual(key2[y], key3[y])  # only for live test
