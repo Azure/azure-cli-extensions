108f20de429f95a7359901ef39d9490e625db4ae
Bo Zhang <bzhan@microsoft.com>
2021-04-15 16:50:44 +0800

    Fix readme.md

--- src/front-door/setup.py
+++ src/front-door/setup.py
@@ -24,7 +24,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = []
 
-with open('README.md', 'r', encoding='utf-8') as f:
+with open('readme.md', 'r', encoding='utf-8') as f:
     README = f.read()
 with open('HISTORY.rst', 'r', encoding='utf-8') as f:
     HISTORY = f.read()
