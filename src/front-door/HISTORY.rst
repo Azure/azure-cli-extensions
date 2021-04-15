5233dacced4fff065f976dfd4a3993906e9f2644
Bo Zhang <bzhan@microsoft.com>
2021-04-15 16:36:10 +0800

    Add HISTORY.rst

---
+++ src/front-door/HISTORY.rst
@@ -0,0 +1,16 @@
+.. :changelog:
+
+Release History
+===============
+
+1.14.0
+++++++
+* Migrate to Track2 SDK.
+
+1.13.0
+++++++
+* az network front-door frontend-endpoint enable-https: allow secret-version to be optional to always use the 'Latest' version to support certificate auto-rotation.
+
+1.12.0
+++++++
+* Add az network front-door check-name-availability command: Check the availability of a Front Door resource name.
--- src/front-door/setup.py
+++ src/front-door/setup.py
@@ -24,6 +24,11 @@ CLASSIFIERS = [
 
 DEPENDENCIES = []
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    README = f.read()
+with open('HISTORY.rst', 'r', encoding='utf-8') as f:
+    HISTORY = f.read()
+
 setup(
     name='front-door',
     version=VERSION,
@@ -32,6 +37,7 @@ setup(
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
     url='https://github.com/Azure/azure-cli-extensions/tree/master/src/front-door',
+    long_description=README + '\n\n' + HISTORY,
     classifiers=CLASSIFIERS,
     package_data={'azext_front_door': ['azext_metadata.json']},
     packages=find_packages(),
