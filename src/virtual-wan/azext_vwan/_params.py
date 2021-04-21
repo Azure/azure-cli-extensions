3ee1ed1382c3e6810f18c9cc26f05422158f5203
Kerwin(Kaihui) Sun <sunkaihuisos@gmail.com>
2021-04-21 18:00:30 +0800

    [Virtual-Wan] migrate to track2 (#3273)

--- src/virtual-wan/azext_vwan/_params.py
+++ src/virtual-wan/azext_vwan/_params.py
@@ -61,7 +61,8 @@ def load_arguments(self, _):
         c.argument('vpn_gateway', help='Name or ID of a VPN gateway.', validator=get_network_resource_name_or_id('vpn_gateway', 'vpnGateways'))
 
     with self.argument_context('network vhub get-effective-routes') as c:
-        c.argument('virtual_wan_resource_type', options_list='--resource-type')
+        c.argument('virtual_wan_resource_type', options_list='--resource-type', help='The type of the specified resource like RouteTable, ExpressRouteConnection, HubVirtualNetworkConnection, VpnConnection and P2SConnection.')
+        c.argument('resource_id', options_list='--resource-id', help='The resource whose effective routes are being requested')
 
     with self.argument_context('network vhub connection') as c:
         c.argument('virtual_hub_name', vhub_name_type)
@@ -216,7 +217,7 @@ def load_arguments(self, _):
         c.argument('resource_name', p2s_vpn_gateway_name_type, id_part=None)
 
     with self.argument_context('network p2s-vpn-gateway vpn-client') as c:
-        c.argument('authentication_method', arg_type=get_enum_type(AuthenticationMethod))
+        c.argument('authentication_method', arg_type=get_enum_type(AuthenticationMethod), help='VPN client authentication method.')
     # endregion
 
     # region Routing Configuration
