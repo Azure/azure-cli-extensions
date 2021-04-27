160e6d2d6347c437b018dfde94c0edcf6e1d0914
Chenyang Liu <chenyl@microsoft.com>
2021-04-27 11:37:43 +0800

    Update to use enum

--- src/webpubsub/azext_webpubsub/_params.py
+++ src/webpubsub/azext_webpubsub/_params.py
@@ -7,7 +7,8 @@
 from knack.arguments import CLIArgumentType
 from azure.cli.core.commands.parameters import (
     tags_type,
-    get_three_state_flag
+    get_three_state_flag,
+    get_enum_type
 )
 from .vendored_sdks.azure.mgmt.webpubsub.models import WebPubSubRequestType
 from ._actions import (
@@ -15,7 +16,8 @@ from ._actions import (
 )
 
 WEBPUBSUB_KEY_TYPE = ['primary', 'secondary']
-
+SKU_TYPE = ['Standard_S1', 'Free_F1']
+NETWORK_RULE_TYPE = []
 
 def load_arguments(self, _):
     from azure.cli.core.commands.validators import get_default_location_from_resource_group
@@ -32,18 +34,18 @@ def load_arguments(self, _):
         c.argument('unit_count', help='The number of signalr service unit count', type=int)
 
     with self.argument_context('webpubsub update') as c:
-        c.argument('sku', help='The sku name of the signalr service. E.g. Standard_S1, Free_F1')
+        c.argument('sku', arg_type=get_enum_type(SKU_TYPE), help='The sku name of the signalr service.')
         c.argument('unit_count', help='The number of signalr service unit count', type=int)
 
     with self.argument_context('webpubsub key regenerate') as c:
-        c.argument('key_type', help='The name of access key to regenerate', choices=WEBPUBSUB_KEY_TYPE)
+        c.argument('key_type', arg_type=get_enum_type(WEBPUBSUB_KEY_TYPE), help='The name of access key to regenerate')
 
     # Network Rule
     with self.argument_context('webpubsub network-rule update') as c:
         c.argument('connection_name', nargs='*', help='Space-separeted list of private endpoint connection name.', required=False, arg_group='Private Endpoint Connection')
         c.argument('public_network', arg_type=get_three_state_flag(), help='Set rules for public network.', required=False, arg_group='Public Network')
-        c.argument('allow', nargs='*', help='The allowed virtual network rule. Space-separeted list of scope to assign. Allowed values: ClientConnection, ServerConnection, RESTAPI, Trace', type=WebPubSubRequestType, required=False)
-        c.argument('deny', nargs='*', help='The denied virtual network rule. Space-separeted list of scope to assign. Allowed values: ClientConnection, ServerConnection, RESTAPI, Trace', type=WebPubSubRequestType, required=False)
+        c.argument('allow', arg_type=get_enum_type(WebPubSubRequestType), nargs='*', help='The allowed virtual network rule. Space-separeted list of scope to assign.', type=WebPubSubRequestType, required=False)
+        c.argument('deny', arg_type=get_enum_type(WebPubSubRequestType), nargs='*', help='The denied virtual network rule. Space-separeted list of scope to assign.', type=WebPubSubRequestType, required=False)
 
     with self.argument_context('webpubsub event-handler update') as c:
         c.argument('items', help='A JSON-formatted string containing event handler items')
