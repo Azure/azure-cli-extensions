a8d668046963081a643a9304c6f75b0e27c44bcb
Jonathan Innis <jonathan.innis.ji@gmail.com>
2021-04-30 00:25:59 -0700

    [k8s-extension] Release 0.3.0 with Microsoft.AzureML (#3308)

--- src/k8s-extension/azext_k8s_extension/partner_extensions/AzureDefender.py
+++ src/k8s-extension/azext_k8s_extension/partner_extensions/AzureDefender.py
@@ -7,13 +7,13 @@
 
 from knack.log import get_logger
 
-from azext_k8s_extension.vendored_sdks.models import ExtensionInstance
-from azext_k8s_extension.vendored_sdks.models import ExtensionInstanceUpdate
-from azext_k8s_extension.vendored_sdks.models import ScopeCluster
-from azext_k8s_extension.vendored_sdks.models import Scope
+from ..vendored_sdks.models import ExtensionInstance
+from ..vendored_sdks.models import ExtensionInstanceUpdate
+from ..vendored_sdks.models import ScopeCluster
+from ..vendored_sdks.models import Scope
 
-from azext_k8s_extension.partner_extensions.PartnerExtensionModel import PartnerExtensionModel
-from azext_k8s_extension.partner_extensions.ContainerInsights import _get_container_insights_settings
+from .PartnerExtensionModel import PartnerExtensionModel
+from .ContainerInsights import _get_container_insights_settings
 
 logger = get_logger(__name__)
 
