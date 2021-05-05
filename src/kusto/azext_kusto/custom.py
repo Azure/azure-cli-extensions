c132a574d1d46ac4358ea96e4bdd2b55746e9d18
Assaf Taubenfeld <astauben@microsoft.com>
2021-05-05 13:24:15 +0300

    revert

--- src/kusto/azext_kusto/custom.py
+++ src/kusto/azext_kusto/custom.py
@@ -9,7 +9,7 @@
 # --------------------------------------------------------------------------
 # pylint: disable=wildcard-import
 # pylint: disable=unused-wildcard-import
- 
+
 from .generated.custom import *  # noqa: F403
 try:
     from .manual.custom import *  # noqa: F403
