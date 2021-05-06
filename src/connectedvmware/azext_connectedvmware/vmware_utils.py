7d4c8f348f2229505f84c2ce8b7a0aaad8c01fcc
Santosh Mishra <sanmishra@microsoft.com>
2021-05-06 13:34:33 +0530

    Oboarding connected vmware az cli extension

---
+++ src/connectedvmware/azext_connectedvmware/vmware_utils.py
@@ -0,0 +1,60 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from knack.util import CLIError
+
+
+def get_resource_id(
+    cmd,
+    resource_group_name,
+    provider_name_space,
+    resource_type,
+    resource,
+    child_type_1=None,
+    child_name_1=None,
+):
+    """
+    Gets the resource id for the resource if name is given.
+    """
+    from azure.cli.core.commands.client_factory import get_subscription_id
+    from msrestazure.tools import is_valid_resource_id, resource_id
+
+    if not is_valid_resource_id(resource):
+        if child_type_1 and child_name_1:
+            return resource_id(
+                subscription=get_subscription_id(cmd.cli_ctx),
+                resource_group=resource_group_name,
+                namespace=provider_name_space,
+                type=resource_type,
+                name=resource,
+                child_type_1=child_type_1,
+                child_name_1=child_name_1,
+            )
+        else:
+            return resource_id(
+                subscription=get_subscription_id(cmd.cli_ctx),
+                resource_group=resource_group_name,
+                namespace=provider_name_space,
+                type=resource_type,
+                name=resource,
+            )
+    else:
+        return resource
+
+
+def create_dictionary_from_arg_string(values, option_string=None):
+    """
+    Creates and returns dictionary from a string containing params in KEY=VALUE format.
+    """
+    params_dict = {}
+    for item in values:
+        try:
+            key, value = item.split('=', 1)
+            params_dict[key.lower()] = value
+        except ValueError:
+            raise CLIError(
+                'usage error: {} KEY=VALUE [KEY=VALUE ...]'.format(option_string)
+            )
+    return params_dict
