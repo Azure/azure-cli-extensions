38d60844175b8a4704c94a380791e0935c64cd2e
fenwickt <69487545+fenwickt@users.noreply.github.com>
2021-05-07 00:46:11 -0700

    Support new api version for resource-graph module  (#3344)
    * changes to support new version (no vendored sdk yet)
    
    * added vendored_sdk and tests
    
    * fixed error handling, applied linter feedback
    
    * version update
    
    * updated vendored sdk to latest published, return response, deprecated CLIErrors
    
    * fixed style and tests, reverted response to dict to avoid exposing facets
    
    * empty commit to retrigger checks
    
    Co-authored-by: makharch <makharch@microsoft.com>

--- src/resource-graph/azext_resourcegraph/vendored_sdks/resourcegraph/aio/__init__.py
+++ src/resource-graph/azext_resourcegraph/vendored_sdks/resourcegraph/aio/__init__.py
@@ -7,13 +7,4 @@
 # --------------------------------------------------------------------------
 
 from ._resource_graph_client import ResourceGraphClient
-from ._version import VERSION
-
-__version__ = VERSION
 __all__ = ['ResourceGraphClient']
-
-try:
-    from ._patch import patch_sdk  # type: ignore
-    patch_sdk()
-except ImportError:
-    pass
