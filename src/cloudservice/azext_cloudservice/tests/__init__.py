3366007e9615e031da3db5f6b0ccb458731b24a9
Feiyue Yu <fey@microsoft.com>
2021-05-13 16:11:45 +0800

    [Cloudservice] 0.2.0 (#3299)
    * [Cloudservice] 0.2.0
    
    * Update src/cloudservice/azext_cloudservice/manual/_help.py
    
    * Update src/cloudservice/azext_cloudservice/manual/_params.py
    
    * style
    
    * style
    
    * SDK version

--- src/cloudservice/azext_cloudservice/tests/__init__.py
+++ src/cloudservice/azext_cloudservice/tests/__init__.py
@@ -8,6 +8,9 @@
 # Changes may cause incorrect behavior and will be lost if the code is
 # regenerated.
 # --------------------------------------------------------------------------
+# pylint: disable=import-outside-toplevel
+# pylint: disable=inconsistent-return-statements
+
 import inspect
 import logging
 import os
