41dc528dc5fddccd4ec73756bc2daa1b98763b8e
xfz11 <xiaofanzhou@microsoft.com>
2021-05-19 11:38:19 +0800

    ci fail

--- scripts/ci/util.py
+++ scripts/ci/util.py
@@ -88,7 +88,7 @@ def get_whl_from_url(url, filename, tmp_dir, whl_cache=None):
             import time
             time.sleep(0.5)
             continue
-    if !r:
+    if not r:
         print('get url={} fail'.format(url))
 
     ext_file = os.path.join(tmp_dir, filename)
