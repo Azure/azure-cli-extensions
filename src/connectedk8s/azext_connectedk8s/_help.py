747d3fb29a650bec77849fcb25e6808ed077a0d0
Arpit Gupta <arpgu@microsoft.com>
2021-05-19 17:41:08 +0530

    Nit

--- src/connectedk8s/azext_connectedk8s/_help.py
+++ src/connectedk8s/azext_connectedk8s/_help.py
@@ -133,7 +133,7 @@ helps['connectedk8s troubleshoot'] = """
         text: az connectedk8s troubleshoot -g MyResourceGroup -n ConnectedCluster --storage-account MyStorageAccount --sas-token "MySasToken"
       - name: using the resource id of a storage account resource you own.
         text: az connectedk8s troubleshoot -g MyResourceGroup -n ConnectedCluster --storage-account "MyStoreageAccountResourceId"
-      - name: using the storagea account in diagnostics settings for your connected cluster.
+      - name: using the storage account in diagnostics settings for your connected cluster.
         text: az connectedk8s troubleshoot -g MyResourceGroup -n ConnectedCluster
       - name: Onboard a connected kubernetes cluster by specifying the kubeconfig and kubecontext.
         text: az connectedk8s troubleshoot -g resourceGroupName -n connectedClusterName --kube-config /path/to/kubeconfig --kube-context kubeContextName
