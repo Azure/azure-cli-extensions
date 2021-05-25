a5175ba69345f87db866da5e7b77c1641989a200
Limin Gu <limgu@microsoft.com>
2021-05-25 13:14:25 -0700

    [Datadog] Release CLI for GA

--- src/datadog/azext_datadog/generated/commands.py
+++ src/datadog/azext_datadog/generated/commands.py
@@ -34,7 +34,7 @@ def load_command_table(self, _):
         g.custom_command('list', 'datadog_monitor_list')
         g.custom_show_command('show', 'datadog_monitor_show')
         g.custom_command('create', 'datadog_monitor_create', supports_no_wait=True)
-        g.custom_command('update', 'datadog_monitor_update')
+        g.custom_command('update', 'datadog_monitor_update', supports_no_wait=True)
         g.custom_command('delete', 'datadog_monitor_delete', supports_no_wait=True, confirmation=True)
         g.custom_command('get-default-key', 'datadog_monitor_get_default_key')
         g.custom_command('list-api-key', 'datadog_monitor_list_api_key')
