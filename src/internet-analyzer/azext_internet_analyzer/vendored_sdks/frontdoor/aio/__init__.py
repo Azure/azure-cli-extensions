b022435e992ba607ff4c4a51d2093156f66bf0f3
Collin Schedler <coschedl@microsoft.com>
2021-05-26 14:07:38 -0700

    Replace the frontdoor SDK under azext_internet_analyzer’s vendored_sdks directory with the target version

--- src/internet-analyzer/azext_internet_analyzer/vendored_sdks/frontdoor/aio/__init__.py
+++ src/internet-analyzer/azext_internet_analyzer/vendored_sdks/frontdoor/aio/__init__.py
@@ -7,13 +7,4 @@
 # --------------------------------------------------------------------------
 
 from ._front_door_management_client import FrontDoorManagementClient
-from ._version import VERSION
-
-__version__ = VERSION
 __all__ = ['FrontDoorManagementClient']
-
-try:
-    from ._patch import patch_sdk  # type: ignore
-    patch_sdk()
-except ImportError:
-    pass
