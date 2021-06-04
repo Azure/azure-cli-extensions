df4b3bf21c01cb133e0462d1d5915b79c0d08230
Cameron Taggart <cameron.taggart@microsoft.com>
2021-06-03 18:34:59 -0700

    custom_show_command

--- src/vmware/azext_vmware/commands.py
+++ src/vmware/azext_vmware/commands.py
@@ -65,18 +65,18 @@ def load_command_table(self, _):
 
     with self.command_group('vmware addon vr', vmware_sdk, client_factory=cf_vmware) as g:
         g.custom_command('create', 'addon_vr_create')
-        g.custom_command('show', 'addon_vr_show')
+        g.custom_show_command('show', 'addon_vr_show')
         g.custom_command('update', 'addon_vr_update')
         g.custom_command('delete', 'addon_vr_delete')
 
     with self.command_group('vmware addon hcx', vmware_sdk, client_factory=cf_vmware) as g:
         g.custom_command('create', 'addon_hcx_create')
-        g.custom_command('show', 'addon_hcx_show')
+        g.custom_show_command('show', 'addon_hcx_show')
         g.custom_command('update', 'addon_hcx_update')
         g.custom_command('delete', 'addon_hcx_delete')
 
     with self.command_group('vmware addon srm', vmware_sdk, client_factory=cf_vmware) as g:
         g.custom_command('create', 'addon_srm_create')
-        g.custom_command('show', 'addon_srm_show')
+        g.custom_show_command('show', 'addon_srm_show')
         g.custom_command('update', 'addon_srm_update')
         g.custom_command('delete', 'addon_srm_delete')
