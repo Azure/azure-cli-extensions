d2e621b89d3f71aac7fc8033b6e4544e98a1d046
Sambit Rath <es15btech11015@iith.ac.in>
2021-06-09 19:10:38 +0530

    [Dataprotection] Onboard new RP Dataprotection to azure-cli-extensions (#3459)
    * Initial commit
    
    * ci fixes
    
    * addes service name
    
    * Added codeowners
    
    * Added more tests
    
    * Added backup-vault list command
    
    * Renamed commands
    
    * typo
    
    * linter fix
    
    * update
    
    * resolving comments
    
    * Changed restore request parameter name

---
+++ src/dataprotection/azext_dataprotection/manual/Manifests/AzureDisk.py
@@ -0,0 +1,80 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+manifest = '''
+{
+    "isProxyResource": false,
+    "resourceType": "Microsoft.Compute/disks",
+    "parentResourceType": "Microsoft.Compute/disks",
+    "datasourceType": "Microsoft.Compute/disks",
+    "allowedRestoreModes": [ "RecoveryPointBased" ],
+    "allowedRestoreTargetTypes": [ "AlternateLocation" ],
+    "itemLevelRecoveyEnabled": false,
+    "policySettings": {
+        "supportedRetentionTags": [ "Daily", "Weekly" ],
+        "supportedDatastoreTypes": [ "OperationalStore" ],
+        "disableAddRetentionRule": false,
+        "disableCustomRetentionTag": true,
+        "backupScheduleSupported": true,
+        "supportedBackupFrequency": [ "Daily", "Hourly" ],
+        "defaultPolicy": {
+        "policyRules": [
+            {
+            "backupParameters": {
+                "backupType": "Incremental",
+                "objectType": "AzureBackupParams"
+            },
+            "trigger": {
+                "schedule": {
+                "repeatingTimeIntervals": [
+                    "R/2020-04-05T13:00:00+00:00/PT4H"
+                ]
+                },
+                "taggingCriteria": [
+                {
+                    "tagInfo": {
+                    "tagName": "Default",
+                    "id": "Default_"
+                    },
+                    "taggingPriority": 99,
+                    "isDefault": true
+                }
+                ],
+                "objectType": "ScheduleBasedTriggerContext"
+            },
+            "dataStore": {
+                "dataStoreType": "OperationalStore",
+                "objectType": "DataStoreInfoBase"
+            },
+            "name": "BackupHourly",
+            "objectType": "AzureBackupRule"
+            },
+            {
+            "lifecycles": [
+                {
+                "deleteAfter": {
+                    "objectType": "AbsoluteDeleteOption",
+                    "duration": "P7D"
+                },
+                "sourceDataStore": {
+                    "dataStoreType": "OperationalStore",
+                    "objectType": "DataStoreInfoBase"
+                }
+                }
+            ],
+            "isDefault": true,
+            "name": "Default",
+            "objectType": "AzureRetentionRule"
+            }
+        ],
+        "name": "DiskPolicy1",
+        "datasourceTypes": [
+            "Microsoft.Compute/disks"
+        ],
+        "objectType": "BackupPolicy"
+        }
+    }
+}
+'''
