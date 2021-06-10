93e9d255537cb29edd0636e4f2ee5cb75460b290
Eric Mattingly <ericmattingly@github.com>
2021-06-09 19:40:52 -0700

    add logging settings to node config (#3483)

--- src/aks-preview/setup.py
+++ src/aks-preview/setup.py
@@ -8,7 +8,7 @@
 from codecs import open as open1
 from setuptools import setup, find_packages
 
-VERSION = "0.5.17"
+VERSION = "0.5.18"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
