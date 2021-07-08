2d40dd84e880cc80c500271b9d0895143d3d1a81
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-07-07 23:23:41 -0700

    Update src/aks-preview/azext_aks_preview/custom.py
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- src/aks-preview/azext_aks_preview/custom.py
+++ src/aks-preview/azext_aks_preview/custom.py
@@ -2802,19 +2802,19 @@ def _ensure_container_insights_for_monitoring(cmd,
                                                         {
                                                            "name": "ContainerInsightsExtension",
                                                            "streams": [
-                                                                "Microsoft-Perf",
-                                                                "Microsoft-ContainerInventory",
-                                                                "Microsoft-ContainerLog",
-                                                                "Microsoft-ContainerLogV2",
-                                                                "Microsoft-ContainerNodeInventory",
-                                                                "Microsoft-KubeEvents",
-                                                                "Microsoft-KubeHealth",
-                                                                "Microsoft-KubeMonAgentEvents",
-                                                                "Microsoft-KubeNodeInventory",
-                                                                "Microsoft-KubePodInventory",
-                                                                "Microsoft-KubePVInventory",
-                                                                "Microsoft-KubeServices",
-                                                                "Microsoft-InsightsMetrics"
+                                                               "Microsoft-Perf",
+                                                               "Microsoft-ContainerInventory",
+                                                               "Microsoft-ContainerLog",
+                                                               "Microsoft-ContainerLogV2",
+                                                               "Microsoft-ContainerNodeInventory",
+                                                               "Microsoft-KubeEvents",
+                                                               "Microsoft-KubeHealth",
+                                                               "Microsoft-KubeMonAgentEvents",
+                                                               "Microsoft-KubeNodeInventory",
+                                                               "Microsoft-KubePodInventory",
+                                                               "Microsoft-KubePVInventory",
+                                                               "Microsoft-KubeServices",
+                                                               "Microsoft-InsightsMetrics"
                                                            ],
                                                            "extensionName": "ContainerInsights"
                                                         }
