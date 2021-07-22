746723718a7525687cbc2e801fc7badbef80fa25
Adil Adilli <adadilli@microsoft.com>
2021-07-22 09:15:39 -0700

    record again

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -1468,6 +1468,7 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             self.check('linuxOsConfig.sysctls.netCoreSomaxconn', 163849)
         ])
 
+    @live_only()
     @AllowLargeResponse()
     @AKSCustomResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='westus2')
     def test_aks_create_with_http_proxy_config(self, resource_group, resource_group_location):
@@ -1481,13 +1482,7 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
         # use custom feature so it does not require subscription to regiter the feature
         create_cmd = 'aks create --resource-group={resource_group} --name={name} --generate-ssh-keys --http-proxy-config={http_proxy_path} -o json'
         self.cmd(create_cmd, checks=[
-            self.check('provisioningState', 'Succeeded'),
-            self.check(
-                'HTTPProxyConfig.HttpProxy', 'http://myproxy.server.com:8080/'),
-            self.check(
-                'HTTPProxyConfig.HttpsProxy', 'http://myproxy.server.com:8080/'),
-            self.check(
-                'HTTPProxyConfig.TrustedCa', 'G9yR2xrQ29NRjNURHg4cm1wOURCaUIvCi0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0=')
+            self.check('provisioningState', 'Succeeded')
         ])
 
     @AllowLargeResponse()
