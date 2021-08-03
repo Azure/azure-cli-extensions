c7e74450b9569fff89fe626fa46051f1531fd035
Haitao Chen <haitch@users.noreply.github.com>
2021-08-03 23:12:09 +0000

    fix nolonger existing import

--- src/aks-preview/azext_aks_preview/commands.py
+++ src/aks-preview/azext_aks_preview/commands.py
@@ -16,7 +16,6 @@ from ._format import aks_versions_table_format
 from ._format import aks_upgrades_table_format
 from ._format import aks_pod_identities_table_format
 from ._format import aks_pod_identity_exceptions_table_format
-from ._format import aks_run_command_result_format
 
 
 def load_command_table(self, _):
