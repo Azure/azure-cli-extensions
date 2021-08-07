438514f86cc73872426122763969ea8a7a661c05
Yuwei Zhou <yuwzho@microsoft.com>
2021-08-07 15:01:11 +0800

    [enterprise] az spring-cloud app deployment list

--- src/spring-cloud/azext_spring_cloud/tier_routing_deployment.py
+++ src/spring-cloud/azext_spring_cloud/tier_routing_deployment.py
@@ -5,14 +5,21 @@
 
 # pylint: disable=wrong-import-order
 from ._util_enterprise import is_enterprise_tier, get_client
-from .custom import (deployment_get)
+from .custom import (deployment_get as deployment_get_standard, deployment_list as deployment_list_standard)
 from knack.log import get_logger
 
 logger = get_logger(__name__)
 
 
-def deployment_get_routing(cmd, client,
-            resource_group,
-            service, app, name):
+def deployment_get(cmd, client,
+                   resource_group,
+                   service, app, name):
     client = get_client(cmd) if is_enterprise_tier(cmd, resource_group, service) else client
-    return deployment_get(cmd, client, resource_group, service, app, name)
+    return deployment_get_standard(cmd, client, resource_group, service, app, name)
+
+
+def deployment_list(cmd, client,
+                    resource_group,
+                    service, app):
+    client = get_client(cmd) if is_enterprise_tier(cmd, resource_group, service) else client
+    return deployment_list_standard(cmd, client, resource_group, service, app)
