904ed1f9bbd1d3f0ff6fd2e4c22c2a23aa22c383
Justin Davies <juda@microsoft.com>
2021-08-12 11:39:11 +0100

    Add podMaxPids tests

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -1465,6 +1465,7 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
         self.cmd(nodepool_cmd, checks=[
             self.check('provisioningState', 'Succeeded'),
             self.check('kubeletConfig.cpuCfsQuotaPeriod', '200ms'),
+            self.check('kubeletConfig.podMaxPids', 120),
             self.check('linuxOsConfig.sysctls.netCoreSomaxconn', 163849)
         ])
 
