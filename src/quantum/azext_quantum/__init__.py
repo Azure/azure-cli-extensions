9c1901cec533e56139eefd505dd803db6739ee54
Warren Jones (Harvey Nash <v-wjones@microsoft.com>
2021-09-23 01:07:55 -0700

    Create version_check_helper.py and add unit tests

--- src/quantum/azext_quantum/__init__.py
+++ src/quantum/azext_quantum/__init__.py
@@ -4,8 +4,6 @@
 # --------------------------------------------------------------------------------------------
 
 from azure.cli.core import AzCommandsLoader
-from datetime import datetime
-from ._version_check_helper import check_version
 
 import azext_quantum._help  # pylint: disable=unused-import
 
@@ -33,9 +31,12 @@ class QuantumCommandsLoader(AzCommandsLoader):
         from azext_quantum._params import load_arguments
         load_arguments(self, command)
 
-        # Once each day, see if the user is running the latest version of the quantum extension.
-        # If not, recommend upgrading.
-        check_version(self.cli_ctx_config, CLI_REPORTED_VERSION, str(datetime.today()).split(' ')[0])
-
+        # See if the user is running the latest version of the quantum extension.
+        from ._version_check_helper import check_version
+        from .operations.workspace import _show_tip
+        from datetime import datetime
+        message = check_version(self.cli_ctx_config, CLI_REPORTED_VERSION, str(datetime.today()).split(' ')[0])
+        if message is not None:
+            _show_tip(message)
 
 COMMAND_LOADER_CLS = QuantumCommandsLoader
