24fa43b13f3c9d3c02633ba68177f54331546a12
Warren Jones <warrenjones@msn.com>
2021-09-25 00:59:50 -0700

    Add quantum version check (#3900)
    * Add quantum version check
    
    * Remove whitespace and brake up long message
    
    * Move version check to load_arguments
    
    * Move version check to helper file
    
    * Create version_check_helper.py and add unit tests
    
    * Remove extraneous comment lines from quantum_workspace.py
    
    * Add blank line to satisfy Flake8

--- src/quantum/azext_quantum/__init__.py
+++ src/quantum/azext_quantum/__init__.py
@@ -19,6 +19,9 @@ class QuantumCommandsLoader(AzCommandsLoader):
     def __init__(self, cli_ctx=None):
         super(QuantumCommandsLoader, self).__init__(cli_ctx=cli_ctx)
 
+        # Save the CLI context config for use in load_arguments
+        self.cli_ctx_config = cli_ctx.config
+
     def load_command_table(self, args):
         from azext_quantum.commands import load_command_table
         load_command_table(self, args)
@@ -27,6 +30,14 @@ class QuantumCommandsLoader(AzCommandsLoader):
     def load_arguments(self, command):
         from azext_quantum._params import load_arguments
         load_arguments(self, command)
+
+        # See if the user is running the latest version of the quantum extension.
+        from ._version_check_helper import check_version
+        from .operations.workspace import _show_tip
+        from datetime import datetime
+        message = check_version(self.cli_ctx_config, CLI_REPORTED_VERSION, str(datetime.today()).split(' ')[0])
+        if message is not None:
+            _show_tip(message)
 
 
 COMMAND_LOADER_CLS = QuantumCommandsLoader
