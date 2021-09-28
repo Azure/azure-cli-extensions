749d7e8e095083e9686345b8f16d86a4bee69608
Graham Zuber <grzuber@microsoft.com>
2021-09-28 08:26:21 -0700

    Improved extension summary, added author details.

--- src/functionapp/setup.py
+++ src/functionapp/setup.py
@@ -43,13 +43,11 @@ with open('HISTORY.rst', 'r', encoding='utf-8') as f:
 setup(
     name='functionapp',
     version=VERSION,
-    description='Microsoft Azure Command-Line Tools Functionapp Extension',
-    # TODO: Update author and email, if applicable
-    author='Microsoft Corporation',
-    author_email='azpycli@microsoft.com',
-    # TODO: change to your extension source code repo if the code will not be put in azure-cli-extensions repo
+    description='Additional commands for Azure Functions.',
+    long_description='Support for managing Azure Functions resources and configs.',
+    author='Graham Zuber',
+    author_email='grzuber@microsoft.com',
     url='https://github.com/Azure/azure-cli-extensions/tree/master/src/functionapp',
-    long_description=README + '\n\n' + HISTORY,
     license='MIT',
     classifiers=CLASSIFIERS,
     packages=find_packages(exclude=["tests"]),
