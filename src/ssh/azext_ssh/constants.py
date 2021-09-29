74c35d7a3658f2c0970e53abe2acd5d16922bddc
Vivian Thiebaut <vthiebaut@microsoft.com>
2021-09-29 17:15:34 -0400

    Added license header to constants file

--- src/ssh/azext_ssh/constants.py
+++ src/ssh/azext_ssh/constants.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 CLIENT_PROXY_VERSION = "1.3.017131"
 CLIENT_PROXY_RELEASE = "release10-09-21"
 CLIENT_PROXY_STORAGE_URL = "https://sshproxysa.blob.core.windows.net"
