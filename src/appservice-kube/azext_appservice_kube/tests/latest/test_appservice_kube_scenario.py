9382be0ac4c08ef99f0b83f6dc7aa5bd875a1d96
Silas Strawn <strawnsc@gmail.com>
2021-09-30 14:48:01 -0500

    remove out-of-date test

--- src/appservice-kube/azext_appservice_kube/tests/latest/test_appservice_kube_scenario.py
+++ src/appservice-kube/azext_appservice_kube/tests/latest/test_appservice_kube_scenario.py
@@ -13,36 +13,6 @@ from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer, RoleBasedSer
 TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
 
+# TODO
 class AppserviceKubernetesScenarioTest(ScenarioTest):
-    @AllowLargeResponse()
-    @ResourceGroupPreparer(location='North Central US (Stage)')
-    @RoleBasedServicePrincipalPreparer()
-    def test_kube_environment_create(self, resource_group, sp_name, sp_password):
-        kube_env_name = self.create_random_name('kube_env_test', 16)
-
-        self.kwargs.update({
-            'name': kube_env_name
-        })
-
-        create_cmd = 'appservice kube create -g {rg} -n {name} ' \
-                     '--client-id {sp} --client-secret {sp_pass}'
-
-        self.cmd(create_cmd, checks=[
-            self.check('provisioningState', 'Succeeded'),
-            self.check('name', '{name}'),
-            self.check('servicePrincipalClientId', '{sp}')
-        ])
-
-        count = len(self.cmd('appservice kube list -g {rg}').get_output_in_json())
-
-        self.cmd('appservice kube show -g {rg} -n {name}', checks=[
-            self.check('name', '{name}'),
-            self.check('resourceGroup', '{rg}'),
-            self.check('servicePrincipalClientId', '{sp}')
-        ])
-
-        self.cmd('appservice-kube delete -g {rg} -n {name}')
-
-        final_count = len(self.cmd('appservice-kube list').get_output_in_json())
-
-        self.assertTrue(final_count, count - 1)
+    pass
