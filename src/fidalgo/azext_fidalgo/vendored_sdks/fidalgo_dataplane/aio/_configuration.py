d700d97da37b6e28565e0925370f34f1259b06be
tbyfield <68976176+tbyfield@users.noreply.github.com>
2021-10-04 13:04:25 -0700

    Regenerated after removing credential-scopes: openid from generation config

--- src/fidalgo/azext_fidalgo/vendored_sdks/fidalgo_dataplane/aio/_configuration.py
+++ src/fidalgo/azext_fidalgo/vendored_sdks/fidalgo_dataplane/aio/_configuration.py
@@ -38,7 +38,7 @@ class FidalgoDataplaneClientConfiguration(Configuration):
 
         self.credential = credential
         self.api_version = "2021-09-01-privatepreview"
-        self.credential_scopes = kwargs.pop('credential_scopes', ['openid'])
+        self.credential_scopes = kwargs.pop('credential_scopes', [])
         kwargs.setdefault('sdk_moniker', 'fidalgo/{}'.format(VERSION))
         self._configure(**kwargs)
 
@@ -55,5 +55,7 @@ class FidalgoDataplaneClientConfiguration(Configuration):
         self.custom_hook_policy = kwargs.get('custom_hook_policy') or policies.CustomHookPolicy(**kwargs)
         self.redirect_policy = kwargs.get('redirect_policy') or policies.AsyncRedirectPolicy(**kwargs)
         self.authentication_policy = kwargs.get('authentication_policy')
+        if not self.credential_scopes and not self.authentication_policy:
+            raise ValueError("You must provide either credential_scopes or authentication_policy as kwargs")
         if self.credential and not self.authentication_policy:
             self.authentication_policy = policies.AsyncBearerTokenCredentialPolicy(self.credential, *self.credential_scopes, **kwargs)
