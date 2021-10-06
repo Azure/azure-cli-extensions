a2daf7068b2dd87e471d068afcd5bb8a3fc5a089
Warren Jones (Harvey Nash <v-wjones@microsoft.com>
2021-10-06 09:51:27 -0700

    Delete commented-out code

--- src/quantum/tests.live/Run.ps1
+++ src/quantum/tests.live/Run.ps1
@@ -9,24 +9,8 @@ param (
 # For debug, print all relevant environment variables:
 Get-ChildItem env:AZURE*, env:*VERSION, env:*OUTDIR | Format-Table | Out-String | Write-Host
 
-<# >>>>> Code from IQ# Run.ps1 script: The CLI Extension tests probably won't use Pester <<<<<
-# Install and run Pester
-Import-Module Pester
-
-$config = [PesterConfiguration]::Default
-$config.Run.Exit = $true
-$config.TestResult.Enabled = $true
-$config.TestResult.OutputPath = "TestResults.xml"
-$config.TestResult.OutputFormat = "JUnitXml"
-$config.Output.Verbosity = "Detailed"
-
-Invoke-Pester -Configuration $config -Verbose 
-<<<<< #>
-
 # Run the Quantum CLI Extension tests in an azdev environment
 env\Scripts\activate.ps1
 azdev extension add quantum
 az account set -s 916dfd6d-030c-4bd9-b579-7bb6d1926e97
 azdev test quantum --live
-#<<<<< Will this work? <<<<<
-#<<<<< What about the Azure login? Does that happen somewhere else? <<<<<
