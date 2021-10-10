f204c00d2ffd7af5714220074f5e0480fe688ce0
Warren Jones (Harvey Nash <v-wjones@microsoft.com>
2021-10-10 16:00:42 -0700

    Add azdev setup

--- src/quantum/tests.live/Run.ps1
+++ src/quantum/tests.live/Run.ps1
@@ -10,7 +10,11 @@ param (
 Get-ChildItem env:AZURE*, env:*VERSION, env:*OUTDIR | Format-Table | Out-String | Write-Host
 
 # Run the Quantum CLI Extension tests in an azdev environment
+python -m venv env
 env\Scripts\activate.ps1
+python -m pip install -U pip
+pip install azdev
+azdev setup --cli ./src/subrepos/azure_cli --repo ./src/subrepos/cli_extensions
 azdev extension add quantum
 az account set -s 916dfd6d-030c-4bd9-b579-7bb6d1926e97
 azdev test quantum --live
