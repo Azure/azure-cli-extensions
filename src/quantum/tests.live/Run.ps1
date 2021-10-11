2095d4538ba23927412142e34a07a012b1986a42
Warren Jones (Harvey Nash <v-wjones@microsoft.com>
2021-10-10 17:22:19 -0700

    Add Set-Location in Run.ps1

--- src/quantum/tests.live/Run.ps1
+++ src/quantum/tests.live/Run.ps1
@@ -10,11 +10,12 @@ param (
 Get-ChildItem env:AZURE*, env:*VERSION, env:*OUTDIR | Format-Table | Out-String | Write-Host
 
 # Run the Quantum CLI Extension tests in an azdev environment
+Set-Location ../../..
 python -m venv env
 env\Scripts\activate.ps1
 python -m pip install -U pip
 pip install azdev
-azdev setup --cli /a/1/s/src/subrepos/azure_cli --repo /a/1/s/src/subrepos/cli_extension
+azdev setup --cli ../azure_cli --repo .
 azdev extension add quantum
 az account set -s 916dfd6d-030c-4bd9-b579-7bb6d1926e97
 azdev test quantum --live
