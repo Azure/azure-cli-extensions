97360a2dbd421363a29bff5a598992de400daedd
kai ru <69238381+kairu-ms@users.noreply.github.com>
2021-10-21 15:26:55 +0800

    Update src/kusto/azext_kusto/generated/_help.py

--- src/kusto/azext_kusto/generated/_help.py
+++ src/kusto/azext_kusto/generated/_help.py
@@ -926,7 +926,7 @@ helps['kusto private-endpoint-connection create'] = """
     type: command
     short-summary: "Approve or reject a private endpoint connection with a given name."
     parameters:
-      - name: --private-link-service-connection-state --connection-state
+      - name: --connection-state --private-link-service-connection-state
         short-summary: "Connection State of the Private Endpoint Connection."
         long-summary: |
             Usage: --private-link-service-connection-state status=XX description=XX
