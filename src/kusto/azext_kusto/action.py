dee4376e8cfd1907a3ca49bc21c8ba07a4b5432f
Zohar Hen <zoharhen@microsoft.com>
2021-10-25 09:42:28 +0300

    Update action.py

--- src/kusto/azext_kusto/action.py
+++ src/kusto/azext_kusto/action.py
@@ -13,8 +13,5 @@
 from .generated.action import *  # noqa: F403
 try:
     from .manual.action import *  # noqa: F403
-except ImportError as e:
-    if e.name.endswith('manual.action'):
-        pass
-    else:
-        raise e
+except ImportError:
+    pass
