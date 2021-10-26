101e1f708233ab26c889f059542821c4c78c8f4c
Wenhao Zhang <sjtu_zhangwh@163.com>
2021-10-26 14:42:47 +0800

    Support tls feature in api version 2021-09-01-preview (#3976)
    * Basic certificate command done
    
    * without test
    
    * pass simple test
    
    * further update
    
    * further update
    
    * workaround to create api response payload type missing
    
    * comment removed
    
    * comment removed
    
    * Update according to comments
    
    * refine
    
    * Update according to review
    
    * Update
    
    * Add sdk for new api version
    
    * Update
    
    * Update IT
    
    * Update test records
    
    * fix linter issue
    
    * Add test for new feature
    
    * Add change log and upgrade version
    
    * Update record file
    
    * fix version error
    
    * fix according to comments
    
    * Add missing error import
    
    * Update to live test
    
    * Add file to help test
    
    * Remove record only
    
    Co-authored-by: Wenhao Zhang <wenhaozhang@microsoft.com>

--- src/spring-cloud/azext_spring_cloud/tests/latest/test_asc_scenario.py
+++ src/spring-cloud/azext_spring_cloud/tests/latest/test_asc_scenario.py
@@ -64,3 +64,72 @@ class CustomDomainTests(ScenarioTest):
 
         self.cmd('spring-cloud certificate remove --name {cert} -g {rg} -s {serviceName}')
         self.cmd('spring-cloud certificate show --name {cert} -g {rg} -s {serviceName}', expect_failure=True)
+
+
+class SslTests(ScenarioTest):
+
+    def test_load_public_cert_to_app(self):
+        py_path = os.path.abspath(os.path.dirname(__file__))
+        baltiCertPath = os.path.join(py_path, 'files/BaltimoreCyberTrustRoot.crt.pem')
+        digiCertPath = os.path.join(py_path, 'files/DigiCertGlobalRootCA.crt.pem')
+        loadCertPath = os.path.join(py_path, 'files/load_certificate.json')
+
+        self.kwargs.update({
+            'cert': 'test-cert',
+            'keyVaultUri': 'https://integration-test-prod.vault.azure.net/',
+            'KeyVaultCertName': 'cli-unittest',
+            'baltiCert': 'balti-cert',
+            'digiCert': 'digi-cert',
+            'baltiCertPath': baltiCertPath,
+            'digiCertPath': digiCertPath,
+            'loadCertPath': loadCertPath,
+            'app': 'test-app',
+            'serviceName': 'cli-unittest',
+            'rg': 'cli',
+            'location': 'westus'
+        })
+
+        self.cmd('group create -n {rg} -l {location}')
+        self.cmd('spring-cloud create -n {serviceName} -g {rg} -l {location}')
+
+        self.cmd(
+            'spring-cloud certificate add --name {digiCert} -f {digiCertPath} -g {rg} -s {serviceName}',
+            checks=[
+                self.check('name', '{digiCert}')
+            ])
+
+        self.cmd(
+            'spring-cloud certificate add --name {baltiCert} -f {baltiCertPath} -g {rg} -s {serviceName}',
+            checks=[
+                self.check('name', '{baltiCert}')
+            ])
+
+        self.cmd(
+            'spring-cloud certificate show --name {digiCert} -g {rg} -s {serviceName}', checks=[
+            self.check('name', '{digiCert}')
+        ])
+
+        self.cmd(
+            'spring-cloud certificate show --name {baltiCert} -g {rg} -s {serviceName}', checks=[
+            self.check('name', '{baltiCert}')
+        ])
+
+        cert_result = self.cmd(
+            'spring-cloud certificate list -g {rg} -s {serviceName}').get_output_in_json()
+        self.assertTrue(len(cert_result) == 2)
+
+        self.cmd(
+            'spring-cloud app create --name {app} -f {loadCertPath} -g {rg} -s {serviceName}')
+
+        self.cmd(
+            'spring-cloud app append-loaded-public-certificate --name {app} --certificate-name {digiCert} --load-trust-store true -g {rg} -s {serviceName}')
+
+        app_result = self.cmd(
+            'spring-cloud certificate list-reference-app --name {digiCert} -g {rg} -s {serviceName}').get_output_in_json()
+        self.assertTrue(len(app_result) > 0)
+
+        app_result = self.cmd(
+            'spring-cloud certificate list-reference-app --name {digiCert} -g {rg} -s {serviceName}').get_output_in_json()
+        self.assertTrue(len(app_result) > 0)
+
+        self.cmd('spring-cloud delete -n {serviceName} -g {rg}')
