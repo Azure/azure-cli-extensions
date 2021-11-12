7d48ac8a91f301f702e5c046c002337722de7a83
yishiwang@microsoft.com <yishiwang@microsoft.com>
2021-11-12 16:04:06 +0800

    linter

--- src/storage-preview/azext_storage_preview/profiles.py
+++ src/storage-preview/azext_storage_preview/profiles.py
@@ -9,7 +9,7 @@ from azure.cli.core.profiles import CustomResourceType
 CUSTOM_DATA_STORAGE = CustomResourceType('azext_storage_preview.vendored_sdks.azure_storage', None)
 CUSTOM_DATA_STORAGE_ADLS = CustomResourceType('azext_storage_preview.vendored_sdks.azure_adls_storage_preview', None)
 CUSTOM_MGMT_STORAGE = CustomResourceType('azext_storage_preview.vendored_sdks.azure_mgmt_storage',
-                                                 'StorageManagementClient')
+                                         'StorageManagementClient')
 CUSTOM_DATA_STORAGE_FILESHARE = CustomResourceType('azext_storage_preview.vendored_sdks.azure_storagev2.fileshare',
                                                    None)
 CUSTOM_DATA_STORAGE_FILEDATALAKE = CustomResourceType('azext_storage_preview.vendored_sdks.azure_storage_filedatalake',
