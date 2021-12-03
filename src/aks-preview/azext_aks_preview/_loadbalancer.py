626c505c27d1ed746716bd432b91c3b4f53b3345
Tyler Lloyd <Tyler.Lloyd@microsoft.com>
2021-12-02 23:39:00 -0500

    ipv6 count should be preserved during an aks update command

--- src/aks-preview/azext_aks_preview/_loadbalancer.py
+++ src/aks-preview/azext_aks_preview/_loadbalancer.py
@@ -60,17 +60,18 @@ def configure_load_balancer_profile(managed_outbound_ip_count, managed_outbound_
     if (managed_outbound_ip_count or managed_outbound_ipv6_count or
             outbound_ip_resources or outbound_ip_prefix_resources):
         # ips -> i_ps due to track 2 naming issue
-        profile.managed_outbound_i_ps = None
-        # ips -> i_ps due to track 2 naming issue
         profile.outbound_i_ps = None
         profile.outbound_ip_prefixes = None
         if managed_outbound_ip_count:
             # ips -> i_ps due to track 2 naming issue
-            profile.managed_outbound_i_ps = ManagedClusterLoadBalancerProfileManagedOutboundIPs(
-                count=managed_outbound_ip_count
-            )
+            if profile.managed_outbound_i_ps and isinstance(profile.managed_outbound_i_ps, ManagedClusterLoadBalancerProfileManagedOutboundIPs):
+                profile.managed_outbound_i_ps.count = managed_outbound_ip_count
+            else:
+                profile.managed_outbound_i_ps = ManagedClusterLoadBalancerProfileManagedOutboundIPs(
+                    count=managed_outbound_ip_count
+                )
         if managed_outbound_ipv6_count:
-            if profile.managed_outbound_i_ps:
+            if profile.managed_outbound_i_ps and isinstance(profile.managed_outbound_i_ps, ManagedClusterLoadBalancerProfileManagedOutboundIPs):
                 profile.managed_outbound_i_ps.count_ipv6 = managed_outbound_ipv6_count
             else:
                 # ips -> i_ps due to track 2 naming issue
