43e746dd3a938e707713a94554de5fe39d2720b5
Tyler Lloyd <Tyler.Lloyd@microsoft.com>
2021-12-13 14:08:02 -0500

    adding live test for updating outbound ips

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -2956,6 +2956,66 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             'aks delete -g {resource_group} -n {name} --yes --no-wait', checks=[self.is_empty()])
 
     @AllowLargeResponse()
+    @AKSCustomResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='westus2', preserve_default_location=True)
+    def test_aks_create_and_update_outbound_ips(self, resource_group, resource_group_location):
+        # kwargs for string formatting
+        aks_name = self.create_random_name('cliakstest', 16)
+        init_pip_name = self.create_random_name('cliakstest', 16)
+        update_pip_name = self.create_random_name('cliakstest', 16)
+        self.kwargs.update({
+            'resource_group': resource_group,
+            'name': aks_name,
+            'location': resource_group_location,
+            'resource_type': 'Microsoft.ContainerService/ManagedClusters',
+            'init_pip_name': init_pip_name,
+            'update_pip_name': update_pip_name,
+            'ssh_key_value': self.generate_ssh_keys(),
+        })
+
+        create_init_pip = 'network public-ip create -g {resource_group} -n {init_pip_name} --sku Standard'
+        init_pip = self.cmd(create_init_pip, checks=[
+            self.check('publicIp.provisioningState', 'Succeeded')
+        ]).get_output_in_json()
+
+        create_update_pip = 'network public-ip create -g {resource_group} -n {update_pip_name} --sku Standard'
+        update_pip = self.cmd(create_update_pip, checks=[
+            self.check('publicIp.provisioningState', 'Succeeded')
+        ]).get_output_in_json()
+
+        init_pip_id = init_pip['publicIp']['id']
+        update_pip_id = update_pip['publicIp']['id']
+
+        assert init_pip_id is not None
+        assert update_pip_id is not None
+        self.kwargs.update({
+            'init_pip_id': init_pip_id,
+            'update_pip_id': update_pip_id,
+        })
+
+        # create cluster
+        create_cmd = 'aks create --resource-group={resource_group} --name={name} --location={location} ' \
+                     '--ssh-key-value={ssh_key_value} --load-balancer-outbound-ips {init_pip_id}'
+
+        self.cmd(create_cmd, checks=[
+            self.check('provisioningState', 'Succeeded'),
+            self.check('networkProfile.loadBalancerProfile.effectiveOutboundIPs[] | length(@)', 1),
+            self.check('networkProfile.loadBalancerProfile.effectiveOutboundIPs[0].id', init_pip_id)
+        ])
+
+        # update cluster
+        update_cmd = 'aks update -g {resource_group} -n {name} --load-balancer-outbound-ips {update_pip_id}'
+
+        self.cmd(update_cmd, checks=[
+            self.check('provisioningState', 'Succeeded'),
+            self.check('networkProfile.loadBalancerProfile.effectiveOutboundIPs[] | length(@)', 1),
+            self.check('networkProfile.loadBalancerProfile.effectiveOutboundIPs[0].id', update_pip_id)
+        ])
+
+        # delete
+        self.cmd(
+            'aks delete -g {resource_group} -n {name} --yes --no-wait', checks=[self.is_empty()])
+
+    @AllowLargeResponse()
     @AKSCustomResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='centraluseuap', preserve_default_location=True)
     def test_aks_create_and_update_ipv6_count(self, resource_group, resource_group_location):
         # reset the count so in replay mode the random names will start with 0
