f230d554015a1058e53069ad5e4a2f8b6fd42613
Ningting Pan <ninpan@microsoft.com>
2021-12-28 10:37:13 +0800

    fix comment

--- src/spring-cloud/azext_spring_cloud/_validators_enterprise.py
+++ src/spring-cloud/azext_spring_cloud/_validators_enterprise.py
@@ -5,15 +5,15 @@
 
 # pylint: disable=too-few-public-methods, unused-argument, redefined-builtin
 
-from azure.cli.core.util import CLIError
+from azure.cli.core.azclierror import ClientRequestError
 from ._util_enterprise import is_enterprise_tier
 
 
 def only_support_enterprise(cmd, namespace):
     if namespace.resource_group and namespace.service and not is_enterprise_tier(cmd, namespace.resource_group, namespace.service):
-        raise CLIError("'{}' only supports for Enterprise tier Spring instance.".format(namespace.command))
+        raise ClientRequestError("'{}' only supports for Enterprise tier Spring instance.".format(namespace.command))
 
 
 def not_support_enterprise(cmd, namespace):
     if namespace.resource_group and namespace.service and is_enterprise_tier(cmd, namespace.resource_group, namespace.service):
-        raise CLIError("'{}' doesn't support for Enterprise tier Spring instance.".format(namespace.command))
+        raise ClientRequestError("'{}' doesn't support for Enterprise tier Spring instance.".format(namespace.command))
