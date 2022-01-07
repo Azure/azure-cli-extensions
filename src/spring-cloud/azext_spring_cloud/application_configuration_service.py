9114b078792397e33c5e8884d9b9318562432e90
Ningting Pan <ninpan@microsoft.com>
2022-01-07 16:55:11 +0800

    fix

--- src/spring-cloud/azext_spring_cloud/application_configuration_service.py
+++ src/spring-cloud/azext_spring_cloud/application_configuration_service.py
@@ -9,7 +9,6 @@ import json
 from azure.cli.core.azclierror import ClientRequestError, ValidationError
 from azure.cli.core.commands.client_factory import get_subscription_id
 from azure.cli.core.util import sdk_no_wait
-from azure.core.exceptions import ResourceNotFoundError
 from knack.log import get_logger
 from msrestazure.tools import resource_id
 
