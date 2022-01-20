c9ef88120ed07fb46a6c64dbc06446e61e07c0df
Liran Englender <lienglender@microsoft.com>
2022-01-20 13:44:30 +0200

    fix more linter issues

--- src/alertsmanagement/azext_alertsmanagement/_help.py
+++ src/alertsmanagement/azext_alertsmanagement/_help.py
@@ -80,8 +80,6 @@ helps['monitor alert-processing-rule create'] = """
     type: command
     short-summary: Create an alert processing rule.
     parameters:
-      - name: scopes
-        short-summary: List of resource IDs (space-delimited) for scope. The rule will apply to alerts that fired on resources within that scope.
       - name: --filter-alert-context
         short-summary: "Filter alerts by alert context (payload)."
         long-summary: |
@@ -104,77 +102,27 @@ helps['monitor alert-processing-rule create'] = """
     examples:
       - name: Create or update a rule that adds an action group to all alerts in a subscription
         text: |-
-              az monitor alert-processing-rule create
-              --name 'AddActionGroupToSubscription'
-              --rule-type AddActionGroups
-              --scopes "/subscriptions/MySubscriptionId"
-              --action-groups "/subscriptions/MySubscriptionId/resourcegroups/MyResourceGroup1/providers/microsoft.insights/actiongroups/ActionGroup1"
-              --enabled true
-              --resource-group alertscorrelationrg
-              --description "Add ActionGroup1 to all alerts in the subscription"
+              az monitor alert-processing-rule create --name 'AddActionGroupToSubscription' --rule-type AddActionGroups -scopes "/subscriptions/MySubscriptionId" --action-groups "/subscriptions/MySubscriptionId/resourcegroups/MyResourceGroup1/providers/microsoft.insights/actiongroups/ActionGroup1" --enabled true --resource-group alertscorrelationrg --description "Add ActionGroup1 to all alerts in the subscription"
 
       - name: Create or update a rule that adds two action groups to all Sev0 and Sev1 alerts in two resource groups
         text: |-
-              az monitor alert-processing-rule create
-              --name 'AddActionGroupsBySeverity'
-              --rule-type AddActionGroups
-              --action-groups "/subscriptions/MySubscriptionId/resourcegroups/MyResourceGroup1/providers/microsoft.insights/actiongroups/MyActionGroupId1" "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup2/providers/microsoft.insights/actionGroups/MyActionGroup2"
-              --scopes "/subscriptions/MySubscriptionId"
-              --resource-group alertscorrelationrg
-              --filter-severity Equals Sev0 Sev1
-              --description "Add AGId1 and AGId2 to all Sev0 and Sev1 alerts in these resourceGroups"
+              az monitor alert-processing-rule create --name 'AddActionGroupsBySeverity' --rule-type AddActionGroups --action-groups "/subscriptions/MySubscriptionId/resourcegroups/MyResourceGroup1/providers/microsoft.insights/actiongroups/MyActionGroupId1" "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup2/providers/microsoft.insights/actionGroups/MyActionGroup2" --scopes "/subscriptions/MySubscriptionId" --resource-group alertscorrelationrg --filter-severity Equals Sev0 Sev1 --description "Add AGId1 and AGId2 to all Sev0 and Sev1 alerts in these resourceGroups"
 
       - name: Create or update a rule that removes all action groups from alerts on a specific VM during a one-off maintenance window (1800-2000 at a specific date, Pacific Standard Time)
         text: |-
-              az monitor alert-processing-rule create
-              --name 'RemoveActionGroupsMaintenanceWindow'
-              --rule-type RemoveAllActionGroups
-              --scopes "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup1/providers/Microsoft.Compute/virtualMachines/VMName"
-              --resource-group alertscorrelationrg
-              --schedule-start-datetime '2022-01-02 18:00:00'
-              --schedule-end-datetime '2022-01-02 20:00:00'
-              --schedule-time-zone 'Pacific Standard Time'
-              --description "Removes all ActionGroups from all Alerts on VMName during the maintenance window"
+              az monitor alert-processing-rule create --name 'RemoveActionGroupsMaintenanceWindow' --rule-type RemoveAllActionGroups --scopes "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup1/providers/Microsoft.Compute/virtualMachines/VMName" --resource-group alertscorrelationrg --schedule-start-datetime '2022-01-02 18:00:00' --schedule-end-datetime '2022-01-02 20:00:00' --schedule-time-zone 'Pacific Standard Time' --description "Removes all ActionGroups from all Alerts on VMName during the maintenance window"
 
       - name: Create or update a rule that removes all action groups from all alerts in a subscription coming from a specific alert rule
         text: |-
-              az monitor alert-processing-rule create
-              --name 'RemoveActionGroupsSpecificAlertRule'
-              --rule-type RemoveAllActionGroups
-              --scopes "/subscriptions/MySubscriptionId"
-              --resource-group alertscorrelationrg
-              --filter-alert-rule-id Equals "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup1/providers/microsoft.insights/activityLogAlerts/RuleName"
-              --description "Removes all ActionGroups from all Alerts that fire on above AlertRule"
+              az monitor alert-processing-rule create --name 'RemoveActionGroupsSpecificAlertRule' --rule-type RemoveAllActionGroups --scopes "/subscriptions/MySubscriptionId" --resource-group alertscorrelationrg --filter-alert-rule-id Equals "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup1/providers/microsoft.insights/activityLogAlerts/RuleName" --description "Removes all ActionGroups from all Alerts that fire on above AlertRule"
 
       - name: Create or update a rule that removes all action groups from all alerts on any VM in two resource groups during a recurring maintenance window (2200-0400 every Sat and Sun, India Standard Time)
         text: |-
-              az monitor alert-processing-rule create
-              --name 'RemoveActionGroupsRecurringMaintenance'
-              --rule-type RemoveAllActionGroups
-              --scopes "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup1" "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup2"
-              --resource-group alertscorrelationrg
-              --filter-resource-type Equals "microsoft.compute/virtualmachines"
-              --schedule-time-zone "India Standard Time"
-              --schedule-recurrence-type Weekly
-              --schedule-recurrence-start-time "22:00:00"
-              --schedule-recurrence-end-time "04:00:00"
-              --schedule-recurrence Sunday Saturday
-              --description "Remove all ActionGroups from all Virtual machine Alerts during the recurring maintenance"
+              az monitor alert-processing-rule create --name 'RemoveActionGroupsRecurringMaintenance' --rule-type RemoveAllActionGroups --scopes "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup1" "/subscriptions/MySubscriptionId/resourceGroups/MyResourceGroup2" --resource-group alertscorrelationrg --filter-resource-type Equals "microsoft.compute/virtualmachines" --schedule-time-zone "India Standard Time" --schedule-recurrence-type Weekly --schedule-recurrence-start-time "22:00:00" --schedule-recurrence-end-time "04:00:00" --schedule-recurrence Sunday Saturday --description "Remove all ActionGroups from all Virtual machine Alerts during the recurring maintenance"
 
       - name: Create or update a rule that removes all action groups outside business hours (Mon-Fri 09:00-17:00, Eastern Standard Time)
         text: |-
-              az monitor alert-processing-rule create
-              --name 'RemoveActionGroupsOutsideBusinessHours'
-              --rule-type RemoveAllActionGroups
-              --scopes "/subscriptions/MySubscriptionId"
-              --resource-group alertscorrelationrg
-              --schedule-time-zone "Eastern Standard Time"
-              --schedule-recurrence-type Daily
-              --schedule-recurrence-start-time "17:00:00"
-              --schedule-recurrence-end-time "09:00:00"
-              --schedule-recurrence-2-type Weekly
-              --schedule-recurrence-2 Saturday Sunday
-              --description "Remove all ActionGroups outside business hours"
+              az monitor alert-processing-rule create --name 'RemoveActionGroupsOutsideBusinessHours' --rule-type RemoveAllActionGroups --scopes "/subscriptions/MySubscriptionId" --resource-group alertscorrelationrg --schedule-time-zone "Eastern Standard Time" --schedule-recurrence-type Daily --schedule-recurrence-start-time "17:00:00" --schedule-recurrence-end-time "09:00:00" --schedule-recurrence-2-type Weekly --schedule-recurrence-2 Saturday Sunday --description "Remove all ActionGroups outside business hours"
 """
 
 helps['monitor alert-processing-rule update'] = """
