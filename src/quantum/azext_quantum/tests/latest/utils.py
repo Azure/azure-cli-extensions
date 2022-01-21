9bf7e428694ea0693ed2e91e1a19ba244b9ab92b
Warren Jones <v-wjones@microsoft.com>
2022-01-21 15:14:24 -0800

    Use short storage account name in utils.py

--- src/quantum/azext_quantum/tests/latest/utils.py
+++ src/quantum/azext_quantum/tests/latest/utils.py
@@ -7,7 +7,7 @@ TEST_SUBS_DEFAULT = "916dfd6d-030c-4bd9-b579-7bb6d1926e97"
 TEST_RG_DEFAULT = "e2e-scenarios"
 TEST_WORKSPACE_DEFAULT = "e2e-qsharp-tests"
 TEST_WORKSPACE_DEFAULT_LOCATION = "westus2"
-TEST_WORKSPACE_DEFAULT_STORAGE = "/subscriptions/916dfd6d-030c-4bd9-b579-7bb6d1926e97/resourceGroups/e2e-scenarios/providers/Microsoft.Storage/storageAccounts/e2etests"
+TEST_WORKSPACE_DEFAULT_STORAGE = "e2etests"
 TEST_WORKSPACE_DEFAULT_PROVIDER_SKU_LIST = "Microsoft/Basic"
 TEST_CAPABILITIES_DEFAULT = "new.microsoft;submit.microsoft" 
 
