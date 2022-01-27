e650dfce2c27ef096f75545c09200abc516ff87e
kai ru <69238381+kairu-ms@users.noreply.github.com>
2022-01-27 15:59:41 +0800

    Update src/alertsmanagement/setup.py

--- src/alertsmanagement/setup.py
+++ src/alertsmanagement/setup.py
@@ -8,6 +8,11 @@
 
 from codecs import open
 from setuptools import setup, find_packages
+try:
+    from azure_bdist_wheel import cmdclass
+except ImportError:
+    from distutils import log as logger
+    logger.warn("Wheel is not available, disabling bdist_wheel hook")
 
 # HISTORY.rst entry.
 VERSION = '0.2.0'
