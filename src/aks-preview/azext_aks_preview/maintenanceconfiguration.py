af2bc7688c8b82776a795c43c185b317e0b62499
Allan Zhang <xiazhan@microsoft.com>
2022-01-28 10:53:43 -0800

    fix as per comments

--- src/aks-preview/azext_aks_preview/maintenanceconfiguration.py
+++ src/aks-preview/azext_aks_preview/maintenanceconfiguration.py
@@ -13,7 +13,7 @@ from msrest import Serializer, Deserializer
 logger = get_logger(__name__)
 
 
-def getMaintenanceConfiguration(cmd, client, config_file, weekday, start_hour):
+def getMaintenanceConfiguration(cmd, config_file, weekday, start_hour):
     if config_file is not None and weekday is not None:
         raise CLIError('either config-file or weekday can be supplied.')
     if weekday is None and start_hour is not None:
@@ -33,7 +33,9 @@ def getMaintenanceConfiguration(cmd, client, config_file, weekday, start_hour):
         result.not_allowed_time = []
         return result
 
-    return _get_maintenance_config(cmd, client, config_file)
+    maintenance_config = get_file_json(config_file)
+    logger.info(maintenance_config)
+    return maintenance_config
 
 
 def aks_maintenanceconfiguration_update_internal(
@@ -48,19 +50,5 @@ def aks_maintenanceconfiguration_update_internal(
 ):
     logger.info('resource_group_name: %s, cluster_name: %s, config_name: %s, config_file: %s, weekday: %s, start_hour: %s ', resource_group_name, cluster_name, config_name, config_file, weekday, start_hour)
 
-    config = getMaintenanceConfiguration(cmd, client, config_file, weekday, start_hour)
+    config = getMaintenanceConfiguration(cmd, config_file, weekday, start_hour)
     return client.create_or_update(resource_group_name=resource_group_name, resource_name=cluster_name, config_name=config_name, parameters=config)
-
-
-def _get_maintenance_config(cmd, client, file_path):
-    # get models
-    MaintenanceConfiguration = cmd.get_models('MaintenanceConfiguration', resource_type=CUSTOM_MGMT_AKS_PREVIEW, operation_group='maintenance_configurations')
-    TimeInWeek = cmd.get_models('TimeInWeek', resource_type=CUSTOM_MGMT_AKS_PREVIEW, operation_group='maintenance_configurations')
-    TimeSpan = cmd.get_models('TimeSpan', resource_type=CUSTOM_MGMT_AKS_PREVIEW, operation_group='maintenance_configurations')
-
-    maintenance_config = get_file_json(file_path)
-    logger.info(maintenance_config)
-    result = client._deserialize('MaintenanceConfiguration', maintenance_config)
-    logger.info(result)
-    
-    return result
