c9f0df8b2fe52c9aa9adbf37120adb0825064377
RuiJun Hu (MSFT) <v-ruih@microsoft.com>
2022-02-10 10:46:11 +0800

    update url links (#4354)
    Co-authored-by: cxznmhdcxz <>

--- src/logic/setup.py
+++ src/logic/setup.py
@@ -47,7 +47,7 @@ setup(
     # TODO: Update author and email, if applicable
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
-    url='https://github.com/Azure/azure-cli-extensions/tree/master/src/logic',
+    url='https://github.com/Azure/azure-cli-extensions/tree/main/src/logic',
     long_description=README + '\n\n' + HISTORY,
     license='MIT',
     classifiers=CLASSIFIERS,
--- src/redisenterprise/setup.py
+++ src/redisenterprise/setup.py
@@ -48,7 +48,7 @@ setup(
     description='Microsoft Azure Command-Line Tools RedisEnterpriseManagementClient Extension',
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
-    url='https://github.com/Azure/azure-cli-extensions/tree/master/src/redisenterprise',
+    url='https://github.com/Azure/azure-cli-extensions/tree/main/src/redisenterprise',
     long_description=README + '\n\n' + HISTORY,
     license='MIT',
     classifiers=CLASSIFIERS,
