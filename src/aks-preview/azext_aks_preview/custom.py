43cbc625eca743e89720a0e22b503c399ca097d2
Tongyao Si <tosi@microsoft.com>
2022-02-28 18:44:45 +0800

    Fix lint

--- src/aks-preview/azext_aks_preview/custom.py
+++ src/aks-preview/azext_aks_preview/custom.py
@@ -2606,8 +2606,8 @@ def _put_managed_cluster_ensuring_permission(
         if need_grant_vnet_permission_to_cluster_identity:
             instant_cluster = client.get(resource_group_name, name)
             if not add_role_assignment(cmd, 'Network Contributor',
-                                        instant_cluster.identity.principal_id, scope=vnet_subnet_id,
-                                        is_service_principal=False):
+                                       instant_cluster.identity.principal_id, scope=vnet_subnet_id,
+                                       is_service_principal=False):
                 logger.warning('Could not create a role assignment for subnet. '
                                'Are you an Owner on this subscription?')
 
