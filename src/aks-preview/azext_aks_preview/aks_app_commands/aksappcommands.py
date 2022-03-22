6cd1e0062d5a5ad7109c7fe45ee174344979e790
mahines <marcus.hines@microsoft.com>
2022-03-22 16:13:51 -0400

    attempting to download binaries, but headers may be blocking

--- src/aks-preview/azext_aks_preview/aks_app_commands/aksappcommands.py
+++ src/aks-preview/azext_aks_preview/aks_app_commands/aksappcommands.py
@@ -2,6 +2,8 @@
 
 import shutil
 import subprocess
+import requests
+
 
 def aks_web_app_init():
     validation = pre_setup_validations()
@@ -12,8 +14,8 @@ def aks_web_app_init():
 def pre_setup_validations() -> bool:
     print("The DraftV2 setup is in progress ...")
     print("Checking if Binary exist ...")
-    cmd_exist = cmd_exists("ifconfig")
-    print(cmd_exist)
+    cmd_exist = False #cmd_exists("ifconfig")
+    #print(cmd_exist)
 
     isDownloadSuccessful = False
     if not cmd_exist:
@@ -38,10 +40,30 @@ def run_binary():
     print("prepare exiting mechanism.")
 
 
-def downloadBinary() -> bool:
-    print("Attempting to download DraftV2 binary")
-
-
 def cmd_finish():
     print("Depending ont the exit_code display message")
     print("We are done Stop.")
+
+
+def downloadBinary() -> bool:
+    print("Attempting to download DraftV2 binary")
+    url = "https://github.com/Azure/draftv2/releases/latest/"
+
+    # Downloading the file by sending the request to the URL
+    req = requests.get(url)
+    result = False
+
+    if req.ok:
+        # Split URL to get the file name
+        filename = url.split('/')[-1]
+
+        # Writing the file to the local file system
+        with open(filename,'wb') as output_file:
+            output_file.write(req.content)
+        print("download of DraftV2 binary was successful with a status code: " + str(req.status_code))
+        return True
+
+    print("Download of DraftV2 binary was unsuccessful with a status code: " + str(req.status_code))
+    return False
+
+
