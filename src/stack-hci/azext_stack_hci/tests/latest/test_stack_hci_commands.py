1204d56ac447679050bc86fc7407600748f64f77
necusjz <necusjz@gmail.com>
2022-03-23 15:13:43 +0800

    add test for arc-setting crud

--- src/stack-hci/azext_stack_hci/tests/latest/test_stack_hci_commands.py
+++ src/stack-hci/azext_stack_hci/tests/latest/test_stack_hci_commands.py
@@ -48,8 +48,38 @@ class StackHciClientTest(ScenarioTest):
             ]
         )
         self.cmd('stack-hci cluster delete -n {cluster_name} -g {rg} --yes')
-        self.cmd('ad app delete --id {client_id}')
 
     @ResourceGroupPreparer(name_prefix='cli_test_stack_hci_', location='eastus')
     def test_stack_hci_arc_setting_crud(self):
-        pass
+        self.kwargs.update({
+            'cluster_name': 'cli-test-cluster',
+            'app_name': 'cli-test-app'
+        })
+        self.kwargs['client_id'] = self.cmd('ad app create --display-name {app_name}').get_output_in_json()['appId']
+        self.kwargs['tenant_id'] = self.cmd('account show').get_output_in_json()['tenantId']
+        self.cmd(
+            'stack-hci cluster create -n {cluster_name} -g {rg} --aad-client-id {client_id} --aad-tenant-id {tenant_id}'
+        )
+
+        self.cmd(
+            'stack-hci arc-setting create -n default -g {rg} --cluster-name {cluster_name}',
+            checks=[
+                self.check('name', 'default'),
+                self.check('type', 'microsoft.azurestackhci/clusters/arcsettings')
+            ]
+        )
+        self.cmd(
+            'stack-hci arc-setting list -g {rg} --cluster-name {cluster_name}',
+            checks=[
+                self.check('length(@)', 1),
+                self.check('@[0].name', 'default')
+            ]
+        )
+        self.cmd(
+            'stack-hci arc-setting show -n default -g {rg} --cluster-name {cluster_name}',
+            checks=[
+                self.check('name', 'default'),
+                self.check('type', 'microsoft.azurestackhci/clusters/arcsettings')
+            ]
+        )
+        self.cmd('stack-hci arc-setting delete -n default -g {rg} --cluster-name {cluster_name} --no-wait --yes')
