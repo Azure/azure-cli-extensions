ad6ff2706abb83726a8ae30bf4bd92187b84b8a4
Calvin Chan <calvinch4n@gmail.com>
2022-03-24 14:19:11 -0700

    Add basic test case

--- src/containerapp/HISTORY.rst
+++ src/containerapp/HISTORY.rst
@@ -5,4 +5,6 @@ Release History
 
 0.1.0
 ++++++
-* Initial release.
+* Initial release for Container App support with Microsoft.App RP.
+* Subgroup commands for dapr, github-action, ingress, registry, revision & secrets
+* Various bugfixes for create & update commands
--- src/containerapp/azext_containerapp/tests/latest/test_containerapp_scenario.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerapp_scenario.py
@@ -4,14 +4,38 @@
 # --------------------------------------------------------------------------------------------
 
 import os
+import time
 import unittest
 
 from azure.cli.testsdk.scenario_tests import AllowLargeResponse
-from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer)
+from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer, JMESPathCheck)
 
 
 TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
 
 class ContainerappScenarioTest(ScenarioTest):
-    pass
+    @AllowLargeResponse(8192)
+    @ResourceGroupPreparer(location="centraluseuap")
+    def test_containerapp_e2e(self, resource_group):
+        containerapp_name = self.create_random_name(prefix='containerapp-e2e', length=24)
+        env_name = self.create_random_name(prefix='containerapp-e2e-env', length=24)
+
+        self.cmd('containerapp env create -g {} -n {}'.format(resource_group, env_name))
+
+        # Sleep in case env create takes a while
+        time.sleep(60)
+        self.cmd('containerapp env list -g {}'.format(resource_group), checks=[
+            JMESPathCheck('length(@)', 1),
+            JMESPathCheck('[0].name', env_name),
+        ])
+
+        self.cmd('containerapp create -g {} -n {} --environment {}'.format(resource_group, containerapp_name, env_name), checks=[
+            JMESPathCheck('name', containerapp_name)
+        ])
+
+        # Sleep in case containerapp create takes a while
+        time.sleep(60)
+        self.cmd('containerapp show -g {} -n {}'.format(resource_group, containerapp_name), checks=[
+            JMESPathCheck('name', containerapp_name)
+        ])
