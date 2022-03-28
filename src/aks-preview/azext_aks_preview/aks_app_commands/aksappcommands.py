6f775df134a46ce7d119ade463281b141ec19951
mahines <marcus.hines@microsoft.com>
2022-03-28 16:56:17 -0400

    download and run binary

--- src/aks-preview/azext_aks_preview/aks_app_commands/aksappcommands.py
+++ src/aks-preview/azext_aks_preview/aks_app_commands/aksappcommands.py
@@ -9,28 +9,28 @@ from pathlib import Path
 
 
 def aks_web_app_init():
-    validation = pre_setup_validations()
-    if validation:
-        run_binary()
-        cmd_finish()
+    filePath = binaryPreCheck()
+    if filePath:
+        isRunSuccessful = run_binary(filePath)
+        if isRunSuccessful is 0:
+            cmd_finish()
+            return
 
-def pre_setup_validations() -> bool:
+    raise ValueError("Binary was NOT executed successfully")
+
+# If setup is valid this method returns the correct binary path to execute
+def binaryPreCheck() -> str:
     print("The DraftV2 setup is in progress ...")
 
-    paths = getPaths()
-    draftV2BinaryExists = isExists(paths)
+    draftV2BinaryPath = findExistingPath(getPotentialPaths())
+    if draftV2BinaryPath:
+        return draftV2BinaryPath
 
-    isDownloadSuccessful = False
-
-    if not draftV2BinaryExists:
-        print("DraftV2 binary not found in the following paths:")
-        isDownloadSuccessful = downloadBinary()
-
-    # Considered valid if binary already exists, or we were able to successfully download the binary
-    return draftV2BinaryExists or isDownloadSuccessful
+    print("DraftV2 binary not found in the following paths:")
+    return downloadBinary()
 
 
-def isExists(paths) -> bool:
+def findExistingPath(paths) -> str:
     print("Checking if DraftV2 binary exists ...")
 
     operatingSystem = platform.system()
@@ -39,47 +39,44 @@ def isExists(paths) -> bool:
 
     if not paths:
         print("List of given DraftV2 paths is empty")
-        return False
+        return None
 
     for path in paths:
-        if os.path.exists(path + "/" + filename):
-            print("Existing binary found at: " + path)
-            return True
-    return False
-
+        binaryFilePath = path + "/" + filename
+        if os.path.exists(binaryFilePath):
+            print("Existing binary found at: " + binaryFilePath)
+            return binaryFilePath
+    return ""
 
 # Returns a list of potential draftV2 binary paths
-def getPaths():
+def getPotentialPaths():
     result = [str(Path.home()) + "/" +".aksapp"]
     paths = os.environ['PATH'].split(':')
 
     for path in paths:
         if "draftv2" in path:
             result.append(path)
-
     return result
 
 
+def run_binary(path):
+    if path is None:
+        raise ValueError("The given Binary path was null or empty")
 
-def run_binary():
     print("Running DraftV2 Binary ...")
-    # for keen mind here is the difference of .popen vs .run
-    # subprocess.run(["ls", "-l"], shell=True, check=True, stdout=subprocess.PIPE) # This will run the command and return any output
-    process = subprocess.Popen(
-        ['ifconfig'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    process = subprocess.Popen([path], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     stdout, stderr = process.communicate()
     exit_code = process.wait()
-    print("If non zero then binary ran fine and job is done.")
     print(stdout, stderr, exit_code)
-    print("prepare exiting mechanism.")
+    return exit_code
 
 
 def cmd_finish():
-    print("Depending ont the exit_code display message")
+    # Clean up logic can go here if needed
     print("We are done Stop.")
 
 
-def downloadBinary() -> bool:
+def downloadBinary() -> str:
     # prompt user to download binary. If users says no, we error out and tell them that this requires the binary
     permissionToDownload = input("The required binary was not found. Would you like us to download the required binary for you? Y/n: ")
 
@@ -112,9 +109,10 @@ def downloadBinary() -> bool:
         with open(filename,'wb') as output_file:
             output_file.write(req.content)
         print("Download of DraftV2 binary was successful with a status code: " + str(req.status_code))
-        return True
+        os.chmod(binaryPath + "/" + filename, 0o777)
+        return binaryPath + "/" + filename
 
     print("Download of DraftV2 binary was unsuccessful with a status code: " + str(req.status_code))
-    return False
+    return None
 
 
