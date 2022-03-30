cac9fc86947ffccd36bdc992c48bfca3b6951330
mahines <marcus.hines@microsoft.com>
2022-03-30 10:39:54 -0400

    adding copywrite comment

--- src/aks-preview/azext_aks_preview/aks_app_commands/aksappcommands.py
+++ src/aks-preview/azext_aks_preview/aks_app_commands/aksappcommands.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#
 
 import shutil
 import subprocess
