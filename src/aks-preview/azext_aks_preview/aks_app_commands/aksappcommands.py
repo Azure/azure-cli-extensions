b6d5e0f7b39b8ade2ddf5c5088158a0cad94833c
mahines <marcus.hines@microsoft.com>
2022-04-01 08:28:27 -0400

    changing type hint import

--- src/aks-preview/azext_aks_preview/aks_app_commands/aksappcommands.py
+++ src/aks-preview/azext_aks_preview/aks_app_commands/aksappcommands.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from __future__ import annotations
+from typing import List
 import shutil
 import subprocess
 import requests
