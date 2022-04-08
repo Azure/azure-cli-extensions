1e70478a4befb77ea894e530b7535d557faa69ef
necusjz <necusjz@gmail.com>
2022-04-08 19:02:16 +0800

    add tests for inbound crud

--- src/dns-resolver/azext_dnsresolver/tests/latest/test_dns_resolver_commands.py
+++ src/dns-resolver/azext_dnsresolver/tests/latest/test_dns_resolver_commands.py
@@ -34,7 +34,7 @@ class DnsResolverClientTest(ScenarioTest):
             'dns-resolver list -g {rg}',
             checks=[
                 self.check('length(@)', 1),
-                self.check('@[0].name', '{dns_resolver_name}')
+                self.check('[0].name', '{dns_resolver_name}')
             ]
         )
         self.cmd('dns-resolver update -n {dns_resolver_name} -g {rg} --tags key=value2')
@@ -50,28 +50,40 @@ class DnsResolverClientTest(ScenarioTest):
     @ResourceGroupPreparer(name_prefix='cli_test_dns_resolver_', location='eastus')
     def test_dns_resolver_inbound_crud(self):
         self.kwargs.update({
+            'endpoint_name': self.create_random_name('endpoint-', 16),
             'dns_resolver_name': self.create_random_name('dns-resolver-', 20),
             'vnet_name': self.create_random_name('vnet-', 12),
             'subnet_name': self.create_random_name('subnet-', 12)
         })
 
         self.cmd('network vnet create -n {vnet_name} -g {rg}')
+        self.cmd('network vnet subnet create -n {subnet_name} -g {rg} --vnet-name {vnet_name} --address-prefixes 10.0.0.0/24')
         self.kwargs['vnet_id'] = self.cmd('network vnet show -n {vnet_name} -g {rg}').get_output_in_json()['id']
-        self.cmd('network vnet subnet create -n {subnet_name} -g {rg} --vnet-name {vnet_name} --address-prefixes 10.0.0.0/21')
-        self.kwargs['subnet_id'] = self.cmd('network vnet subnet show -n {subnet_name} -g {rg}').get_output_in_json()['id']
+        self.kwargs['subnet_id'] = self.cmd('network vnet subnet show -n {subnet_name} -g {rg} --vnet-name {vnet_name}').get_output_in_json()['id']
+        self.cmd('dns-resolver create -n {dns_resolver_name} -g {rg} --id {vnet_id}')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        self.cmd(
+            'dns-resolver inbound-endpoint create -n {endpoint_name} -g {rg} --dns-resolver-name {dns_resolver_name} '
+            '--ip-configurations private-ip-address="" private-ip-allocation-method=Dynamic id={subnet_id} '
+            '--tags key=value1',
+            checks=[
+                self.check('name', '{endpoint_name}'),
+                self.check('type', 'Microsoft.Network/dnsResolvers/inboundEndpoints')
+            ]
+        )
+        self.cmd(
+            'dns-resolver inbound-endpoint list -g {rg} --dns-resolver-name {dns_resolver_name}',
+            checks=[
+                self.check('length(@)', 1),
+                self.check('[0].name', '{endpoint_name}')
+            ]
+        )
+        self.cmd('dns-resolver inbound-endpoint update -n {endpoint_name} -g {rg} --dns-resolver-name {dns_resolver_name} --tags key=value2')
+        self.cmd(
+            'dns-resolver inbound-endpoint show -n {endpoint_name} -g {rg} --dns-resolver-name {dns_resolver_name}',
+            checks=[
+                self.check('name', '{endpoint_name}'),
+                self.check('tags.key', 'value2')
+            ]
+        )
+        self.cmd('dns-resolver inbound-endpoint delete -n {endpoint_name} -g {rg} --dns-resolver-name {dns_resolver_name} -y')
