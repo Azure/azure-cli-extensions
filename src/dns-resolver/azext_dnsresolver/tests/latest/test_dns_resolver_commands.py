1e24c1f0e5f07c98ad3d1c106b2ded085acebe15
necusjz <necusjz@gmail.com>
2022-04-08 17:52:34 +0800

    add tests for dns resolver crud

---
+++ src/dns-resolver/azext_dnsresolver/tests/latest/test_dns_resolver_commands.py
@@ -0,0 +1,77 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# pylint: disable=line-too-long
+# pylint: disable=too-many-lines
+
+from azure.cli.testsdk import (
+    ResourceGroupPreparer,
+    ScenarioTest
+)
+
+
+class DnsResolverClientTest(ScenarioTest):
+    @ResourceGroupPreparer(name_prefix='cli_test_dns_resolver_', location='eastus')
+    def test_dns_resolver_crud(self):
+        self.kwargs.update({
+            'dns_resolver_name': self.create_random_name('dns-resolver-', 20),
+            'vnet_name': self.create_random_name('vnet-', 12),
+        })
+
+        self.cmd('network vnet create -n {vnet_name} -g {rg}')
+        self.kwargs['vnet_id'] = self.cmd('network vnet show -n {vnet_name} -g {rg}').get_output_in_json()['id']
+
+        self.cmd(
+            'dns-resolver create -n {dns_resolver_name} -g {rg} --id {vnet_id} --tags key=value1',
+            checks=[
+                self.check('name', '{dns_resolver_name}'),
+                self.check('type', 'Microsoft.Network/dnsResolvers')
+            ]
+        )
+        self.cmd(
+            'dns-resolver list -g {rg}',
+            checks=[
+                self.check('length(@)', 1),
+                self.check('@[0].name', '{dns_resolver_name}')
+            ]
+        )
+        self.cmd('dns-resolver update -n {dns_resolver_name} -g {rg} --tags key=value2')
+        self.cmd(
+            'dns-resolver show -n {dns_resolver_name} -g {rg}',
+            checks=[
+                self.check('name', '{dns_resolver_name}'),
+                self.check('tags.key', 'value2')
+            ]
+        )
+        self.cmd('dns-resolver delete -n {dns_resolver_name} -g {rg} -y')
+
+    @ResourceGroupPreparer(name_prefix='cli_test_dns_resolver_', location='eastus')
+    def test_dns_resolver_inbound_crud(self):
+        self.kwargs.update({
+            'dns_resolver_name': self.create_random_name('dns-resolver-', 20),
+            'vnet_name': self.create_random_name('vnet-', 12),
+            'subnet_name': self.create_random_name('subnet-', 12)
+        })
+
+        self.cmd('network vnet create -n {vnet_name} -g {rg}')
+        self.kwargs['vnet_id'] = self.cmd('network vnet show -n {vnet_name} -g {rg}').get_output_in_json()['id']
+        self.cmd('network vnet subnet create -n {subnet_name} -g {rg} --vnet-name {vnet_name} --address-prefixes 10.0.0.0/21')
+        self.kwargs['subnet_id'] = self.cmd('network vnet subnet show -n {subnet_name} -g {rg}').get_output_in_json()['id']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
