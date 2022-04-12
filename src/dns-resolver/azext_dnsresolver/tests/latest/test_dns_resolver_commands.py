564b5bc42ae3a310aa1aea830630f3a23b63b571
necusjz <necusjz@gmail.com>
2022-04-12 16:56:23 +0800

    add tests for rule

--- src/dns-resolver/azext_dnsresolver/tests/latest/test_dns_resolver_commands.py
+++ src/dns-resolver/azext_dnsresolver/tests/latest/test_dns_resolver_commands.py
@@ -143,7 +143,7 @@ class DnsResolverClientTest(ScenarioTest):
         self.kwargs['vnet_id'] = self.cmd('network vnet show -n {vnet_name} -g {rg}').get_output_in_json()['id']
         self.kwargs['subnet_id'] = self.cmd('network vnet subnet show -n {subnet_name} -g {rg} --vnet-name {vnet_name}').get_output_in_json()['id']
         self.cmd('dns-resolver create -n {dns_resolver_name} -g {rg} --id {vnet_id}')
-        self.kwargs['outbound_id'] = self.cmd('dns-resolver outbound-endpoint create -n {endpoint_name} -g {rg} --dns-resolver-name {dns_resolver_name} --id={subnet_id} --tags key=value1').get_output_in_json()['id']
+        self.kwargs['outbound_id'] = self.cmd('dns-resolver outbound-endpoint create -n {endpoint_name} -g {rg} --dns-resolver-name {dns_resolver_name} --id={subnet_id}').get_output_in_json()['id']
 
         self.cmd(
             'dns-resolver forwarding-ruleset create -n {ruleset_name} -g {rg} '
@@ -169,3 +169,48 @@ class DnsResolverClientTest(ScenarioTest):
             ]
         )
         self.cmd('dns-resolver forwarding-ruleset delete -n {ruleset_name} -g {rg} -y')
+
+    @ResourceGroupPreparer(name_prefix='cli_test_dns_resolver_', location='eastus')
+    def test_dns_resolver_rule_crud(self):
+        self.kwargs.update({
+            'rule_name': self.create_random_name('rule-', 12),
+            'ruleset_name': self.create_random_name('ruleset-', 12),
+            'endpoint_name': self.create_random_name('endpoint-', 16),
+            'dns_resolver_name': self.create_random_name('dns-resolver-', 20),
+            'vnet_name': self.create_random_name('vnet-', 12),
+            'subnet_name': self.create_random_name('subnet-', 12)
+        })
+
+        self.cmd('network vnet create -n {vnet_name} -g {rg}')
+        self.cmd('network vnet subnet create -n {subnet_name} -g {rg} --vnet-name {vnet_name} --address-prefixes 10.0.0.0/24')
+        self.kwargs['vnet_id'] = self.cmd('network vnet show -n {vnet_name} -g {rg}').get_output_in_json()['id']
+        self.kwargs['subnet_id'] = self.cmd('network vnet subnet show -n {subnet_name} -g {rg} --vnet-name {vnet_name}').get_output_in_json()['id']
+        self.cmd('dns-resolver create -n {dns_resolver_name} -g {rg} --id {vnet_id}')
+        self.kwargs['outbound_id'] = self.cmd('dns-resolver outbound-endpoint create -n {endpoint_name} -g {rg} --dns-resolver-name {dns_resolver_name} --id={subnet_id}').get_output_in_json()['id']
+        self.cmd('dns-resolver forwarding-ruleset create -n {ruleset_name} -g {rg} --outbound-endpoints id={outbound_id}')
+
+        self.cmd(
+            'dns-resolver forwarding-rule create -n {rule_name} -g {rg} --ruleset-name {ruleset_name} '
+            '--domain-name contoso.com. --forwarding-rule-state Enabled --metadata additionalProp=value1 '
+            '--target-dns-servers ip-address=10.0.0.1 port=53',
+            checks=[
+                self.check('name', '{rule_name}'),
+                self.check('type', 'Microsoft.Network/dnsForwardingRulesets/forwardingRules')
+            ]
+        )
+        self.cmd(
+            'dns-resolver forwarding-rule list -g {rg} --ruleset-name {ruleset_name}',
+            checks=[
+                self.check('length(@)', 1),
+                self.check('[0].name', '{rule_name}')
+            ]
+        )
+        self.cmd('dns-resolver forwarding-rule update -n {rule_name} -g {rg} --ruleset-name {ruleset_name} --forwarding-rule-state Disabled --metadata additionalProp=value2')
+        self.cmd(
+            'dns-resolver forwarding-rule show -n {rule_name} -g {rg} --ruleset-name {ruleset_name}',
+            checks=[
+                self.check('forwardingRuleState', 'Disabled'),
+                self.check('metadata.additionalProp', 'value2')
+            ]
+        )
+        self.cmd('dns-resolver forwarding-rule delete -n {rule_name} -g {rg} --ruleset-name {ruleset_name} -y')
