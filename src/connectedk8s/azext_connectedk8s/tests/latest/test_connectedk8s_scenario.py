91396c288d51863b13b847e25c8f043f8765183a
Akash Keshari <akkeshar@microsoft.com>
2022-04-28 16:03:09 +0530

    added pvt link test and re-recorded the tests

--- src/connectedk8s/azext_connectedk8s/tests/latest/test_connectedk8s_scenario.py
+++ src/connectedk8s/azext_connectedk8s/tests/latest/test_connectedk8s_scenario.py
@@ -7,7 +7,7 @@ import os
 import unittest
 
 from azure.cli.testsdk import (LiveScenarioTest, ResourceGroupPreparer)  # pylint: disable=import-error
-from azure.cli.testsdk.scenario_tests import AllowLargeResponse  # pylint: disable=import-error
+#from azure.cli.testsdk.scenario_tests import AllowLargeResponse  # pylint: disable=import-error
 
 
 TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
@@ -26,11 +26,12 @@ class Connectedk8sScenarioTest(LiveScenarioTest):
         self.kwargs.update({
             'name': self.create_random_name(prefix='cc-', length=12),
             'kubeconfig': "%s" % (_get_test_data_file(managed_cluster_name + '-config.yaml')),
+            'kubeconfigpls': "%s" % (_get_test_data_file('pls-config.yaml')),
             'managed_cluster_name': managed_cluster_name
         })
         self.cmd('aks create -g akkeshar -n {} -s Standard_B2s -l westeurope -c 1 --generate-ssh-keys'.format(managed_cluster_name))
         self.cmd('aks get-credentials -g akkeshar -n {managed_cluster_name} -f {kubeconfig}')
-        self.cmd('connectedk8s connect -g akkeshar -n {name} -l eastus2euap --tags foo=doo --kube-config {kubeconfig}', checks=[
+        self.cmd('connectedk8s connect -g akkeshar -n {name} -l eastus --tags foo=doo --kube-config {kubeconfig}', checks=[
             self.check('tags.foo', 'doo'),
             self.check('name', '{name}')
         ])
@@ -44,3 +45,12 @@ class Connectedk8sScenarioTest(LiveScenarioTest):
 
         # delete the kube config
         os.remove("%s" % (_get_test_data_file(managed_cluster_name + '-config.yaml')))
+
+        # Private link test
+        self.cmd('aks get-credentials -g akkeshar -n akkeshar -f {kubeconfigpls}')
+        self.cmd('connectedk8s connect -g akkeshar -n cliplscc -l eastus --tags foo=doo --kube-config {kubeconfigpls} --enable-private-link true --pls-arm-id /subscriptions/1bfbb5d0-917e-4346-9026-1d3b344417f5/resourceGroups/akkeshar-pls/providers/Microsoft.HybridCompute/privateLinkScopes/testpls', checks=[
+            self.check('name', 'cliplscc')
+        ])
+        self.cmd('connectedk8s delete -g akkeshar -n cliplscc --kube-config {kubeconfigpls} -y')
+
+        os.remove("%s" % (_get_test_data_file('pls-config.yaml')))
