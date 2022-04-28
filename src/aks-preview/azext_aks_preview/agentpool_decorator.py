e3e7a83d23d3f4019ea9ba49c10d43d66a89077e
Fuming Zhang <fumingzhang@microsoft.com>
2022-04-28 16:35:30 +0800

    update agentpool decorator and add tests

--- src/aks-preview/azext_aks_preview/agentpool_decorator.py
+++ src/aks-preview/azext_aks_preview/agentpool_decorator.py
@@ -3,61 +3,37 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import base64
 import os
-from math import isnan
-from typing import Dict, List, Tuple, TypeVar, Union
+from types import SimpleNamespace
+from typing import Dict, TypeVar, Union, List
 
-from azure.cli.command_modules.acs._client_factory import cf_agent_pools
-from azure.cli.command_modules.acs._consts import (
-    CONST_DEFAULT_NODE_OS_TYPE,
-    CONST_DEFAULT_NODE_VM_SIZE,
-    CONST_DEFAULT_WINDOWS_NODE_VM_SIZE,
-    CONST_NODEPOOL_MODE_SYSTEM,
-    CONST_NODEPOOL_MODE_USER,
-    CONST_SCALE_DOWN_MODE_DELETE,
-    CONST_SCALE_SET_PRIORITY_REGULAR,
-    CONST_SCALE_SET_PRIORITY_SPOT,
-    CONST_SPOT_EVICTION_POLICY_DELETE,
-    CONST_VIRTUAL_MACHINE_SCALE_SETS,
-    AgentPoolDecoratorMode,
-    DecoratorEarlyExitException,
-    DecoratorMode,
-)
-from azure.cli.command_modules.acs._helpers import get_snapshot_by_snapshot_id
-from azure.cli.command_modules.acs._validators import extract_comma_separated_string
-from azure.cli.command_modules.acs.base_decorator import BaseAKSContext, BaseAKSModels, BaseAKSParamDict
-from azure.cli.command_modules.acs.decorator import safe_list_get
-from azure.cli.core import AzCommandsLoader
-from azure.cli.core.azclierror import (
-    ArgumentUsageError,
-    CLIInternalError,
-    InvalidArgumentValueError,
-    MutuallyExclusiveArgumentError,
-    RequiredArgumentMissingError,
-)
-from azure.cli.core.commands import AzCliCommand
-from azure.cli.core.profiles import ResourceType
-from azure.cli.core.util import get_file_json, sdk_no_wait
-from knack.log import get_logger
-
+from azure.cli.command_modules.acs._consts import AgentPoolDecoratorMode, DecoratorMode
 from azure.cli.command_modules.acs.agentpool_decorator import (
-    AKSAgentPoolParamDict,
+    AKSAgentPoolAddDecorator,
     AKSAgentPoolContext,
     AKSAgentPoolModels,
-    AKSAgentPoolAddDecorator,
+    AKSAgentPoolParamDict,
     AKSAgentPoolUpdateDecorator,
 )
+from azure.cli.core.azclierror import InvalidArgumentValueError
+from azure.cli.core.commands import AzCliCommand
+from azure.cli.core.profiles import ResourceType
+from azure.cli.core.util import read_file_content
+from knack.log import get_logger
+
+from azext_aks_preview._client_factory import cf_agent_pools
+from azext_aks_preview._consts import CONST_WORKLOAD_RUNTIME_OCI_CONTAINER
+from azext_aks_preview._helpers import get_nodepool_snapshot_by_snapshot_id
 
 logger = get_logger(__name__)
 
 # type variables
 AgentPool = TypeVar("AgentPool")
 AgentPoolsOperations = TypeVar("AgentPoolsOperations")
-Snapshot = TypeVar("Snapshot")
-KubeletConfig = TypeVar("KubeletConfig")
-LinuxOSConfig = TypeVar("LinuxOSConfig")
 
-# pylint: disable=too-many-instance-attributes,too-few-public-methods
+
+# pylint: disable=too-few-public-methods
 class AKSPreviewAgentPoolModels(AKSAgentPoolModels):
     """Store the models used in aks agentpool series of commands.
 
@@ -75,6 +51,124 @@ class AKSPreviewAgentPoolContext(AKSAgentPoolContext):
         agentpool_decorator_mode: AgentPoolDecoratorMode,
     ):
         super().__init__(cmd, raw_parameters, models, decorator_mode, agentpool_decorator_mode)
+        self.__external_functions = None
+
+    @property
+    def external_functions(self) -> SimpleNamespace:
+        if self.__external_functions is None:
+            external_functions = vars(super().external_functions)
+            external_functions["cf_agent_pools"] = cf_agent_pools
+            external_functions["get_snapshot_by_snapshot_id"] = get_nodepool_snapshot_by_snapshot_id
+            self.__external_functions = SimpleNamespace(**external_functions)
+        return self.__external_functions
+
+    def get_zones(self) -> Union[List[str], None]:
+        """Obtain the value of zones.
+
+        Note: Inherited and extended in aks-preview to add support for a different parameter name (node_zones).
+
+        :return: list of strings or None
+        """
+        zones = super().get_zones()
+        if zones is not None:
+            return zones
+        # read the original value passed by the command
+        return self.raw_param.get("node_zones")
+
+    def get_host_group_id(self) -> Union[str, None]:
+        """Obtain the value of host_group_id.
+
+        :return: string or None
+        """
+        # read the original value passed by the command
+        host_group_id = self.raw_param.get("host_group_id")
+        # try to read the property value corresponding to the parameter from the `agentpool` object
+        if (
+            self.agentpool and
+            self.agentpool.host_group_id is not None
+        ):
+            host_group_id = self.agentpool.host_group_id
+
+        # this parameter does not need dynamic completion
+        # this parameter does not need validation
+        return host_group_id
+
+    def get_crg_id(self) -> Union[str, None]:
+        """Obtain the value of crg_id.
+
+        :return: string or None
+        """
+        # read the original value passed by the command
+        crg_id = self.raw_param.get("crg_id")
+        # try to read the property value corresponding to the parameter from the `agentpool` object
+        if (
+            self.agentpool and
+            self.agentpool.capacity_reservation_group_id is not None
+        ):
+            crg_id = self.agentpool.capacity_reservation_group_id
+
+        # this parameter does not need dynamic completion
+        # this parameter does not need validation
+        return crg_id
+
+    def get_message_of_the_day(self) -> Union[str, None]:
+        """Obtain the value of message_of_the_day.
+
+        :return: string or None
+        """
+        # read the original value passed by the command
+        message_of_the_day = None
+        message_of_the_day_file_path = self.raw_param.get("message_of_the_day")
+
+        if message_of_the_day_file_path:
+            if not os.path.isfile(message_of_the_day_file_path):
+                raise InvalidArgumentValueError(
+                    "{} is not valid file, or not accessable.".format(
+                        message_of_the_day_file_path
+                    )
+                )
+            message_of_the_day = read_file_content(
+                message_of_the_day_file_path)
+            message_of_the_day = base64.b64encode(
+                bytes(message_of_the_day, 'ascii')).decode('ascii')
+
+        # try to read the property value corresponding to the parameter from the `mc` object
+        if self.agentpool and self.agentpool.message_of_the_day is not None:
+            message_of_the_day = self.agentpool.message_of_the_day
+
+        # this parameter does not need dynamic completion
+        # this parameter does not need validation
+        return message_of_the_day
+
+    def get_gpu_instance_profile(self) -> Union[str, None]:
+        """Obtain the value of gpu_instance_profile.
+
+        :return: string or None
+        """
+        # read the original value passed by the command
+        gpu_instance_profile = self.raw_param.get("gpu_instance_profile")
+        # try to read the property value corresponding to the parameter from the `mc` object
+        if self.agentpool and self.agentpool.gpu_instance_profile is not None:
+            gpu_instance_profile = self.agentpool.gpu_instance_profile
+
+        # this parameter does not need dynamic completion
+        # this parameter does not need validation
+        return gpu_instance_profile
+
+    def get_workload_runtime(self) -> Union[str, None]:
+        """Obtain the value of workload_runtime, default value is CONST_WORKLOAD_RUNTIME_OCI_CONTAINER.
+
+        :return: string or None
+        """
+        # read the original value passed by the command
+        workload_runtime = self.raw_param.get("workload_runtime", CONST_WORKLOAD_RUNTIME_OCI_CONTAINER)
+        # try to read the property value corresponding to the parameter from the `mc` object
+        if self.agentpool and self.agentpool.workload_runtime is not None:
+            workload_runtime = self.agentpool.workload_runtime
+
+        # this parameter does not need dynamic completion
+        # this parameter does not need validation
+        return workload_runtime
 
 
 class AKSPreviewAgentPoolAddDecorator(AKSAgentPoolAddDecorator):
@@ -86,7 +180,81 @@ class AKSPreviewAgentPoolAddDecorator(AKSAgentPoolAddDecorator):
         resource_type: ResourceType,
         agentpool_decorator_mode: AgentPoolDecoratorMode,
     ):
+        self.__raw_parameters = raw_parameters
         super().__init__(cmd, client, raw_parameters, resource_type, agentpool_decorator_mode)
+
+    def init_models(self) -> None:
+        """Initialize an AKSPreviewAgentPoolModels object to store the models.
+
+        :return: None
+        """
+        self.models = AKSPreviewAgentPoolModels(self.cmd, self.resource_type, self.agentpool_decorator_mode)
+
+    def init_context(self) -> None:
+        """Initialize an AKSPreviewAgentPoolContext object to store the context in the process of assemble the
+        AgentPool object.
+
+        :return: None
+        """
+        self.context = AKSPreviewAgentPoolContext(
+            self.cmd,
+            AKSAgentPoolParamDict(self.__raw_parameters),
+            self.models,
+            DecoratorMode.CREATE,
+            self.agentpool_decorator_mode,
+        )
+
+    def set_up_preview_vm_properties(self, agentpool: AgentPool) -> AgentPool:
+        """Set up preview vm related properties for the AgentPool object.
+
+        :return: the AgentPool object
+        """
+        self._ensure_agentpool(agentpool)
+
+        agentpool.host_group_id = self.context.get_host_group_id()
+        agentpool.capacity_reservation_group_id = self.context.get_crg_id()
+        return agentpool
+
+    def set_up_motd(self, agentpool: AgentPool) -> AgentPool:
+        """Set up message of the day for the AgentPool object.
+
+        :return: the AgentPool object
+        """
+        self._ensure_agentpool(agentpool)
+
+        agentpool.message_of_the_day = self.context.get_message_of_the_day()
+        return agentpool
+
+    def set_up_gpu_propertes(self, agentpool: AgentPool) -> AgentPool:
+        """Set up gpu related properties for the AgentPool object.
+
+        :return: the AgentPool object
+        """
+        self._ensure_agentpool(agentpool)
+
+        agentpool.gpu_instance_profile = self.context.get_gpu_instance_profile()
+        agentpool.workload_runtime = self.context.get_workload_runtime()
+        return agentpool
+
+    def construct_agentpool_profile_preview(self) -> AgentPool:
+        """The overall controller used to construct the preview AgentPool profile.
+
+        The completely constructed AgentPool object will later be passed as a parameter to the underlying SDK
+        (mgmt-containerservice) to send the actual request.
+
+        :return: the AgentPool object
+        """
+        # construct the default AgentPool profile
+        agentpool = self.construct_agentpool_profile_default(bypass_restore_defaults=True)
+        # set up preview vm properties
+        agentpool = self.set_up_preview_vm_properties(agentpool)
+        # set up message of the day
+        agentpool = self.set_up_motd(agentpool)
+        # set up gpu profiles
+        agentpool = self.set_up_gpu_propertes(agentpool)
+        # restore defaults
+        agentpool = self._restore_defaults_in_agentpool(agentpool)
+        return agentpool
 
 
 class AKSPreviewAgentPoolUpdateDecorator(AKSAgentPoolUpdateDecorator):
@@ -98,4 +266,38 @@ class AKSPreviewAgentPoolUpdateDecorator(AKSAgentPoolUpdateDecorator):
         resource_type: ResourceType,
         agentpool_decorator_mode: AgentPoolDecoratorMode,
     ):
+        self.__raw_parameters = raw_parameters
         super().__init__(cmd, client, raw_parameters, resource_type, agentpool_decorator_mode)
+
+    def init_models(self) -> None:
+        """Initialize an AKSPreviewAgentPoolModels object to store the models.
+
+        :return: None
+        """
+        self.models = AKSPreviewAgentPoolModels(self.cmd, self.resource_type, self.agentpool_decorator_mode)
+
+    def init_context(self) -> None:
+        """Initialize an AKSPreviewAgentPoolContext object to store the context in the process of assemble the
+        AgentPool object.
+
+        :return: None
+        """
+        self.context = AKSPreviewAgentPoolContext(
+            self.cmd,
+            AKSAgentPoolParamDict(self.__raw_parameters),
+            self.models,
+            DecoratorMode.UPDATE,
+            self.agentpool_decorator_mode,
+        )
+
+    def update_agentpool_profile_preview(self, agentpools: List[AgentPool] = None) -> AgentPool:
+        """The overall controller used to update the preview AgentPool profile.
+
+        The completely constructed AgentPool object will later be passed as a parameter to the underlying SDK
+        (mgmt-containerservice) to send the actual request.
+
+        :return: the AgentPool object
+        """
+        # fetch and update the default AgentPool profile
+        agentpool = self.update_agentpool_profile_default(agentpools)
+        return agentpool
