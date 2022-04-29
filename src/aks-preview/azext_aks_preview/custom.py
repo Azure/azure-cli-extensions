73765c3d5fe7cc651b2091c4fa186eb3bdd793dc
Paul Miller <pmiller@microsoft.com>
2022-04-29 22:56:23 +0000

    more lint

--- src/aks-preview/azext_aks_preview/custom.py
+++ src/aks-preview/azext_aks_preview/custom.py
@@ -908,14 +908,14 @@ def aks_update(cmd,     # pylint: disable=too-many-statements,too-many-branches,
 
 # Do a get and put to try and bring clusters to a desired state.
 def aks_reconcile(cmd, client, resource_group_name, name,  # pylint: disable=unused-argument
-                    aks_custom_headers, no_wait):
+                  aks_custom_headers, no_wait):
     mc = client.get(resource_group_name, name)
     headers = get_aks_custom_headers(aks_custom_headers)
     return sdk_no_wait(no_wait, client.begin_create_or_update,
-                        resource_group_name=resource_group_name,
-                        resource_name=name,
-                        parameters=mc,
-                        headers=headers)
+                       resource_group_name=resource_group_name,
+                       resource_name=name,
+                       parameters=mc,
+                       headers=headers)
 
 
 # pylint: disable=unused-argument
@@ -1826,7 +1826,8 @@ def aks_agentpool_get_upgrade_profile(cmd,   # pylint: disable=unused-argument
                                       cluster_name,
                                       nodepool_name):
     return client.get_upgrade_profile(resource_group_name, cluster_name, nodepool_name)
-    
+
+
 def aks_agentpool_reconcile(cmd,     # pylint: disable=unused-argument
                             client,
                             resource_group_name,
