5b7787d53d63e75fbaf2fda2ea4d0ee81fbb9a6c
Dikla Tzemah Weyl <dtzemahweyl@microsoft.com>
2022-05-03 17:10:14 +0300

    change versions

--- src/azure-firewall/azext_firewall/_params.py
+++ src/azure-firewall/azext_firewall/_params.py
@@ -172,24 +172,24 @@ def load_arguments(self, _):
     with self.argument_context('network firewall policy', arg_group='Intrustion Detection') as c:
         c.argument('intrusion_detection_mode',
                    is_preview=True,
-                   min_api='2021-08-01',
+                   min_api='2020-07-01',
                    options_list=['--idps-mode'],
                    arg_type=get_enum_type(FirewallPolicyIntrusionDetectionStateType),
                    help='IDPS mode')
 
-    with self.argument_context('network firewall policy', arg_group='TLS Inspection', min_api='2021-08-01', is_preview=True) as c:
+    with self.argument_context('network firewall policy', arg_group='TLS Inspection', min_api='2020-07-01', is_preview=True) as c:
         c.argument('key_vault_secret_id',
                    help="Secret Id of (base-64 encoded unencrypted pfx) Secret or Certificate object stored in KeyVault")
         c.argument('certificate_name', options_list=['--cert-name'], help='Name of the CA certificate')
 
-    with self.argument_context('network firewall policy intrusion-detection', min_api='2021-08-01') as c:
+    with self.argument_context('network firewall policy intrusion-detection', min_api='2020-07-01') as c:
         c.argument('firewall_policy_name', options_list=['--policy-name'], help='The name of the Firewall Policy.')
 
-    with self.argument_context('network firewall policy intrusion-detection', min_api='2021-08-01', arg_group='IDPS Signature Override') as c:
+    with self.argument_context('network firewall policy intrusion-detection', min_api='2020-07-01', arg_group='IDPS Signature Override') as c:
         c.argument('signature_mode', options_list=['--mode'], help='The signature state', arg_type=get_enum_type(FirewallPolicyIntrusionDetectionStateType))
         c.argument('signature_id', help='Signature id')
 
-    with self.argument_context('network firewall policy intrusion-detection', min_api='2021-08-01', arg_group='IDPS Bypass Rule') as c:
+    with self.argument_context('network firewall policy intrusion-detection', min_api='2020-07-01', arg_group='IDPS Bypass Rule') as c:
         c.argument('bypass_rule_name', options_list=['--rule-name'], help='Name of the bypass traffic rule')
         c.argument('bypass_rule_description', options_list=['--rule-description'], help='Description of the bypass traffic rule')
         c.argument('bypass_rule_protocol', options_list=['--rule-protocol'], arg_type=get_enum_type(FirewallPolicyIntrusionDetectionProtocol), help='The rule bypass protocol')
