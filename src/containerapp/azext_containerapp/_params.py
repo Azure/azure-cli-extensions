8960d75234c43f5572bcb34ebd01419e4ef1a622
Haroon Feisal <haroonfeisal@microsoft.com>
2022-05-04 19:44:15 -0400

    Fixed linter issue.

--- src/containerapp/azext_containerapp/_params.py
+++ src/containerapp/azext_containerapp/_params.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-# pylint: disable=line-too-long, too-many-statements, consider-using-f-string
+# pylint: disable=line-too-long, too-many-statements, consider-using-f-string, option_length_too_long
 
 from knack.arguments import CLIArgumentType
 
