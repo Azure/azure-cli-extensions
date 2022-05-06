30f8270e9f90dc7d2a3bd55c828731383b7a2d4c
dorothyhu <72949285+dorothyhu@users.noreply.github.com>
2022-05-05 21:20:47 -0700

    Update src/connectedmachine/azext_connectedmachine/generated/_params.py
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- src/connectedmachine/azext_connectedmachine/generated/_params.py
+++ src/connectedmachine/azext_connectedmachine/generated/_params.py
@@ -68,7 +68,7 @@ def load_arguments(self, _):
         c.argument('type_handler_version', type=str, help='Specify the version of the script handler.')
         c.argument('enable_auto_upgrade', arg_type=get_three_state_flag(), help='Indicates whether the extension '
                    'should be automatically upgraded by the platform if there is a newer version available.')
-        c.argument('auto_upgrade_minor', arg_type=get_three_state_flag(), help='Indicates whether the extension should '
+        c.argument('auto_upgrade_minor', arg_type=get_three_state_flag(), help='Indicate whether the extension should '
                    'use a newer minor version if one is available at deployment time. Once deployed, however, the '
                    'extension will not upgrade minor versions unless redeployed, even with this property set to true.')
         c.argument('settings', type=validate_file_or_dict, help='Json formatted public settings for the extension. '
