d88abbdef55f6c0512d5486fe5e0e4a2171a3a6a
Silas Strawn <strawnsc@gmail.com>
2022-05-12 14:52:27 -0700

    fix linter

--- src/containerapp/azext_containerapp/_params.py
+++ src/containerapp/azext_containerapp/_params.py
@@ -151,9 +151,9 @@ def load_arguments(self, _):
         c.argument('name', id_part=None)
         c.argument('storage_name', help="Name of the storage.")
         c.argument('access_mode', id_part=None, arg_type=get_enum_type(["ReadWrite", "ReadOnly"]), help="Access mode for the AzureFile storage.")
-        c.argument('azure_file_account_key', help="Key of the AzureFile storage account.")
-        c.argument('azure_file_share_name', help="Name of the share on the AzureFile storage.")
-        c.argument('azure_file_account_name', help="Name of the AzureFile storage account.")
+        c.argument('azure_file_account_key', options_list=["--azure-file-account-key", "--storage-account-key", "-k"], help="Key of the AzureFile storage account.")
+        c.argument('azure_file_share_name', options_list=["--azure-file-share-name", "--file-share", "-f"], help="Name of the share on the AzureFile storage.")
+        c.argument('azure_file_account_name', options_list=["--azure-file-account-name", "--account-name", "-a"], help="Name of the AzureFile storage account.")
 
     with self.argument_context('containerapp identity') as c:
         c.argument('user_assigned', nargs='+', help="Space-separated user identities.")
