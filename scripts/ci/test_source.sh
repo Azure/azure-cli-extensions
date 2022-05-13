e8bcc94ba42e376a68b79e9831767af846f46197
ZelinWang <zelinwang@microsoft.com>
2022-05-13 11:05:02 +0800

    Update test_source.sh

--- scripts/ci/test_source.sh
+++ scripts/ci/test_source.sh
@@ -4,6 +4,8 @@ set -ex
 # Install CLI & CLI testsdk
 echo "Installing azure-cli-testsdk, azure-cli-core, azure-cli from source code"
 git clone https://github.com/Azure/azure-cli --depth 1
+# Because the new version has breaking change, so bump to 1.21.1
+pip install azure-core==1.21.1
 pip install -e azure-cli/src/azure-cli-testsdk
 pip install -e azure-cli/src/azure-cli-core
 pip install -e azure-cli/src/azure-cli
