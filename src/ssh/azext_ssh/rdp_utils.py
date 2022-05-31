b8f99ff2cc1a192ebbba2d71bbefd69b00d9a23b
Vivian Thiebaut <vthiebaut@microsoft.com>
2022-05-31 10:50:18 -0400

    RDP

--- src/ssh/azext_ssh/rdp_utils.py
+++ src/ssh/azext_ssh/rdp_utils.py
@@ -16,10 +16,10 @@ from . import connectivity_utils
 
 def start_rdp_connection(ssh_info, delete_keys, delete_cert):
     resource_port = 3389
-    local_port = 13389
+    local_port = _get_open_port()
 
     while not is_local_port_open(local_port):
-        local_port = random.randrange(1025, 65534)
+        local_port = _get_open_port()
 
     if ssh_info.ssh_args == None:
         ssh_info.ssh_args = ['-L', f"{local_port}:localhost:{resource_port}", "-N"]
@@ -43,7 +43,7 @@ def start_rdp_connection(ssh_info, delete_keys, delete_cert):
 def start_ssh_tunnel(op_info, ssh_conn):
     try:
         # About cleanup: For now, since it's windows only, we won't ever need to clear credentials.
-        # Unless --force-delete-credentials is used, but let's ignore that scenario for now.
+        # Unless --force-delete-credentials is used, but let's ignore that for now.
         env = os.environ.copy()
         if op_info.is_arc():
             env['SSHPROXY_RELAY_INFO'] = connectivity_utils.format_relay_info_string(op_info.relay_info)
@@ -61,6 +61,7 @@ def start_ssh_tunnel(op_info, ssh_conn):
         command = [ssh_utils._get_ssh_client_path('ssh', op_info.ssh_client_folder), op_info.get_host()]
         command = command + op_info.build_args() + op_info.ssh_args
 
+        logger.debug(f"Startng SSH tunnel with command: {command}")
         ssh_sub = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, env=env, universal_newlines=True, encoding='utf-8')
     except Exception as e:
         ssh_conn.send("SSH_FAIL")
@@ -74,6 +75,7 @@ def start_ssh_tunnel(op_info, ssh_conn):
                 print(next_line)
             else:
                 log_list.append(next_line)
+            # Is this message coming from upstream? Check today.
             if "debug1: Entering interactive session." in next_line:
                 ssh_conn.send("SSH_OK")
                 break
@@ -90,6 +92,7 @@ def start_ssh_tunnel(op_info, ssh_conn):
             kill_process(ssh_sub.pid) 
             break
         
+    # printing errors 
     while next_line:
         next_line = ssh_sub.stderr.readline()
         if print_ssh_logs:
@@ -151,3 +154,12 @@ def is_local_port_open(local_port):
             logger.info('Port %s is open', local_port)
             is_port_open = True
         return is_port_open
+
+
+def _get_open_port():
+    import socket
+    from contextlib import closing
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
