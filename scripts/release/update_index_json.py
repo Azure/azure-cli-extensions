c7f9918594eb5de94516ffe3439c1ae8c1f4c70d
ZelinWang <zelinwang@microsoft.com>
2022-06-02 14:51:48 +0800

    Update update_index_json.py

--- scripts/release/update_index_json.py
+++ scripts/release/update_index_json.py
@@ -5,6 +5,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# test
 import logging
 import subprocess
 import shlex
