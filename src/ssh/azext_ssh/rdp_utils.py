4eca350d2f9cf1d7e89c3ba9b57856998dbec846
Vivian Thiebaut <vthiebaut@microsoft.com>
2022-06-03 17:50:00 -0400

    Refactor rdp utils

--- src/ssh/azext_ssh/rdp_utils.py
+++ src/ssh/azext_ssh/rdp_utils.py
@@ -4,7 +4,10 @@ import subprocess
 import multiprocessing as mp
 import os
 import psutil
-import random
+
+import colorama
+from colorama import Fore
+from colorama import Style
 
 from knack import log
 
@@ -15,36 +18,65 @@ from . import ssh_utils
 from . import connectivity_utils
 
 def start_rdp_connection(ssh_info, delete_keys, delete_cert):
-    resource_port = 3389
-    local_port = _get_open_port()
-
-    while not is_local_port_open(local_port):
+    try:
+        resource_port = 3389
         local_port = _get_open_port()
 
-    if ssh_info.ssh_args == None:
-        ssh_info.ssh_args = ['-L', f"{local_port}:localhost:{resource_port}", "-N"]
-    else:
-        ssh_info.ssh_args = ['-L', f"{local_port}:localhost:{resource_port}", "-N"] + ssh_info.ssh_args
+        while not is_local_port_open(local_port):
+            local_port = _get_open_port()
 
-    rdp_conn, ssh_conn = mp.Pipe()
-    rdp_process = mp.Process(target=call_rdp, args=(rdp_conn, local_port))
-    rdp_process.start()
+        if ssh_info.ssh_args == None:
+            ssh_info.ssh_args = ['-L', f"{local_port}:localhost:{resource_port}", "-N"]
+        else:
+            ssh_info.ssh_args = ['-L', f"{local_port}:localhost:{resource_port}", "-N"] + ssh_info.ssh_args
 
-    start_ssh_tunnel(ssh_info, ssh_conn)
-    ssh_utils.do_cleanup(delete_keys, delete_cert, ssh_info.cert_file, ssh_info.private_key_file, ssh_info.public_key_file)
-    
-    if rdp_process.is_alive():
-        # This is also temporary, because I need to figure out how to kill the process smoothly if needed
-        try:
-            kill_process(rdp_process.pid)
-        except:
-            return
+        rdp_conn, ssh_conn = mp.Pipe()
+        rdp_process = mp.Process(target=call_rdp, args=(rdp_conn, local_port))
+        rdp_process.start()
+
+        ssh_process, print_ssh_logs = start_ssh_tunnel(ssh_info, ssh_conn)
+        ssh_sucess, log_list = wait_for_ssh_connection(ssh_process, print_ssh_logs, ssh_conn)
+        
+        # whether the connection succeeds or fails, we will want to clean-up credentials either way
+        ssh_utils.do_cleanup(delete_keys, delete_cert, ssh_info.cert_file, ssh_info.private_key_file, ssh_info.public_key_file)
+        
+        if ssh_sucess:
+            wait_for_rdp_conclusion(ssh_conn, ssh_process)
+        
+        print_error_messages_from_log(log_list, print_ssh_logs, ssh_process)
+        
+    finally:
+        ssh_utils.do_cleanup(delete_keys, delete_cert, ssh_info.cert_file, ssh_info.private_key_file, ssh_info.public_key_file)
+        #if rdp_process and rdp_process.is_alive():
+        #    kill_process(rdp_process.pid)
+        #if ssh_process and ssh_process.poll():
+        #    kill_process(ssh_process.pid)
 
 
-def start_ssh_tunnel(op_info, ssh_conn, delete_keys, delete_cert):
+def is_local_port_open(local_port):
+    import socket
+    from contextlib import closing
+    is_port_open = False
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        if sock.connect_ex(('', local_port)) == 0:
+            logger.info('Port %s is NOT open', local_port)
+        else:
+            logger.info('Port %s is open', local_port)
+            is_port_open = True
+        return is_port_open
+
+
+def _get_open_port():
+    import socket
+    from contextlib import closing
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
+def start_ssh_tunnel(op_info, ssh_conn):
     try:
-        # About cleanup: It is possible to RDP into Linux machines (see https://docs.microsoft.com/en-us/azure/virtual-machines/linux/use-remote-desktop?tabs=azure-cli)
-        # Clean-up is necessary
         env = os.environ.copy()
         if op_info.is_arc():
             env['SSHPROXY_RELAY_INFO'] = connectivity_utils.format_relay_info_string(op_info.relay_info)
@@ -64,48 +96,65 @@ def start_ssh_tunnel(op_info, ssh_conn, delete_keys, delete_cert):
 
         logger.debug(f"Startng SSH tunnel with command: {command}")
         ssh_sub = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, env=env, universal_newlines=True, encoding='utf-8')
+
+        return ssh_sub, print_ssh_logs
+    
     except Exception as e:
         ssh_conn.send("SSH_FAIL")
-        ssh_utils.do_cleanup(delete_keys, delete_cert, op_info.cert_file, op_info.private_key_file, op_info.public_key_file)
         raise e
-    
-    log_list = []
-    while True:
-        try:
-            next_line = ssh_sub.stderr.readline()
-            if print_ssh_logs:
-                print(next_line)
-            else:
-                log_list.append(next_line)
-            # Is this message coming from upstream? Check today.
-            if "debug1: Entering interactive session." in next_line:
-                ssh_conn.send("SSH_OK")
-                ssh_utils.do_cleanup(delete_keys, delete_cert, op_info.cert_file, op_info.private_key_file, op_info.public_key_file)
-                break
-        except:
-            ssh_conn.send("SSH_FAIL")
 
-        if ssh_sub.poll() is not None:
-            ssh_conn.send("SSH_FAIL")
-            return
-    
+
+def wait_for_ssh_connection(ssh_sub, print_ssh_logs, ssh_conn):
+    log_list = []
+    ssh_sucess = False
     while True:
-        msg = ssh_conn.recv()
-        if msg == "RDP_CLOSE":
-            kill_process(ssh_sub.pid) 
-            break
-        
-    # printing errors 
-    while next_line:
         next_line = ssh_sub.stderr.readline()
         if print_ssh_logs:
             print(next_line)
         else:
             log_list.append(next_line)
+        if "debug1: Entering interactive session." in next_line:
+            ssh_conn.send("SSH_OK")
+            ssh_sucess = True
+            break
+
+        if ssh_sub.poll() is not None:
+            ssh_conn.send("SSH_FAIL")
+            ssh_sucess = False
+            break
+
+    return ssh_sucess, log_list
+
+
+def wait_for_rdp_conclusion(ssh_conn, ssh_process):
+    while True:
+        msg = ssh_conn.recv()
+        if msg == "RDP_CLOSE":
+            kill_process(ssh_process.pid) 
+            break
+        
+
+def print_error_messages_from_log(log_list, print_ssh_logs, ssh_process):
+    # Finish reading logs
+    next_line = ssh_process.stderr.readline()
+    while next_line:
+        if print_ssh_logs:
+            print(next_line)
+        else:
+            log_list.append(next_line)
+        next_line = ssh_process.stderr.readline()
+    
+    # Print error messages on log
+    if ssh_process.returncode != 0 and not print_ssh_logs:
+        for line in log_list:
+            if "debug1:" not in line:
+                print(line)
 
 
 def call_rdp(rdp_conn, local_port):
     from azure.cli.command_modules.network._process_helper import launch_and_wait
+    
+    # Wait for SSH connection
     while True:
         msg = rdp_conn.recv()
         if msg == "SSH_OK":
@@ -113,12 +162,16 @@ def call_rdp(rdp_conn, local_port):
         elif msg == "SSH_FAIL":
             return
 
+    # Launch RDP process
     if platform.system() == 'Windows':
+        colorama.init()
+        print(Fore.GREEN + f"Launching Remote Desktop Connection process." + Style.RESET_ALL)
         command = [_get_rdp_path(), f"/v:localhost:{local_port}"]
         launch_and_wait(command)
         rdp_conn.send("RDP_CLOSE")
 
 
+# Cut "get ssh path into get system32 folder and get ssh path?"
 def _get_rdp_path(rdp_command="mstsc"):
     rdp_path = rdp_command
     if platform.system() == 'Windows':
@@ -145,24 +198,3 @@ def kill_process(pid):
         proc.kill()
     process.kill()
 
-
-def is_local_port_open(local_port):
-    import socket
-    from contextlib import closing
-    is_port_open = False
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        if sock.connect_ex(('', local_port)) == 0:
-            logger.info('Port %s is NOT open', local_port)
-        else:
-            logger.info('Port %s is open', local_port)
-            is_port_open = True
-        return is_port_open
-
-
-def _get_open_port():
-    import socket
-    from contextlib import closing
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(('', 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
