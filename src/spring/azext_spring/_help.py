93af0e483fbb6969aac17360f76dfa48b0ff8ab7
Ruoyu Wang <ruoyuwang@microsoft.com>
2022-06-06 14:36:28 +0800

    fix help.py

--- src/spring/azext_spring/_help.py
+++ src/spring/azext_spring/_help.py
@@ -229,11 +229,11 @@ helps['spring app deploy'] = """
     short-summary: Deploy source code or pre-built binary to an app and update related configurations.
     examples:
     - name: Deploy source code to an app. This will pack current directory, build binary with Pivotal Build Service and then deploy to the app.
-      text: az spring app deploy -n MyApp -s MyCluster -g MyResourceGroup
+      text: az spring app deploy -n MyApp -s MyCluster -g MyResourceGroup --source-path
     - name: Deploy a pre-built jar to an app with jvm options and environment variables.
       text: az spring app deploy -n MyApp -s MyCluster -g MyResourceGroup --jar-path app.jar --jvm-options="-XX:+UseG1GC -XX:+UseStringDeduplication" --env foo=bar
     - name: Deploy source code to a specific deployment of an app.
-      text: az spring app deploy -n MyApp -s MyCluster -g MyResourceGroup -d green-deployment
+      text: az spring app deploy -n MyApp -s MyCluster -g MyResourceGroup -d green-deployment --source-path
     - name: Deploy a container image on Docker Hub to an app.
       text: az spring app deploy -n MyApp -s MyCluster -g MyResourceGroup --container-image contoso/your-app:v1
     - name: Deploy a container image on a private registry to an app.
@@ -362,7 +362,7 @@ helps['spring app deployment create'] = """
     short-summary: Create a staging deployment for the app. To deploy code or update setting to an existing deployment, use `az spring app deploy/update --deployment <staging deployment>`.
     examples:
     - name: Deploy source code to a new deployment of an app. This will pack current directory, build binary with Pivotal Build Service and then deploy.
-      text: az spring app deployment create -n green-deployment --app MyApp -s MyCluster -g MyResourceGroup
+      text: az spring app deployment create -n green-deployment --app MyApp -s MyCluster -g MyResourceGroup --source-path
     - name: Deploy a pre-built jar to an app with jvm options and environment variables.
       text: az spring app deployment create -n green-deployment --app MyApp -s MyCluster -g MyResourceGroup --jar-path app.jar --jvm-options="-XX:+UseG1GC -XX:+UseStringDeduplication" --env foo=bar
     - name: Deploy a container image on Docker Hub to an app.
