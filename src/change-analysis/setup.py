170be3910c02279d4eef60f3cf186d0f0b1943f0
Lingling Tong <ltong@microsoft.com>
2022-06-07 06:04:57 -0700

    add service name and address cr feedback

--- src/change-analysis/setup.py
+++ src/change-analysis/setup.py
@@ -41,7 +41,7 @@ setup(
     license='MIT',
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
-    url='https://github.com/Azure/azure-cli-extensions/tree/master/src/change-analysis',
+    url='https://github.com/Azure/azure-cli-extensions/tree/main/src/change-analysis',
     classifiers=CLASSIFIERS,
     packages=find_packages(exclude=["tests"]),
     package_data={'azext_change_analysis': ['azext_metadata.json']},
