76697e072c8e0655d73d47ed5c5ec61f324cafae
svagadia <t-svagadia@microsoft.com>
2022-06-22 11:28:17 +0530

    @svagadia Diagnoser Code Draft - (Removed linter issue - Iteration 5)

--- src/connectedk8s/azext_connectedk8s/_help.py
+++ src/connectedk8s/azext_connectedk8s/_help.py
@@ -121,3 +121,11 @@ helps['connectedk8s disable-features'] = """
   - name: Disable multiple features.
     text: az connectedk8s disable-features -n clusterName -g resourceGroupName --features custom-locations azure-rbac
 """
+
+helps['connectedk8s troubleshoot'] = """
+  type: command
+  short-summary: Perform diagnostic check on the arc-enabled kubernetes cluster.
+  examples:
+  - name: Executes diagnostic checks on a connected kubernetes cluster
+    text: az connectedk8s troubleshoot -n clusterName -g resourceGroupName 
+"""
