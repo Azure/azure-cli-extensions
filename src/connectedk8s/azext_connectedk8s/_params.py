15e5e892fc21bcafbdc325c8c031dbf53a6c3289
svagadia <106307351+svagadia@users.noreply.github.com>
2022-07-15 13:49:56 +0530

    Azure Arc Diagnostic (#5025)

--- src/connectedk8s/azext_connectedk8s/_params.py
+++ src/connectedk8s/azext_connectedk8s/_params.py
@@ -91,3 +91,9 @@ def load_arguments(self, _):
         c.argument('context_name', options_list=['--kube-context'], help='If specified, overwrite the default context name.')
         c.argument('path', options_list=['--file', '-f'], type=file_type, completer=FilesCompleter(), default=os.path.join(os.path.expanduser('~'), '.kube', 'config'), help="Kubernetes configuration file to update. If not provided, updates the file '~/.kube/config'. Use '-' to print YAML to stdout instead.")
         c.argument('api_server_port', options_list=['--port'], help='Port used for accessing connected cluster.')
+
+    with self.argument_context('connectedk8s troubleshoot') as c:
+        c.argument('tags', tags_type)
+        c.argument('cluster_name', options_list=['--name', '-n'], help='The name of the connected cluster.')
+        c.argument('kube_config', options_list=['--kube-config'], help='Path to the kube config file.')
+        c.argument('kube_context', options_list=['--kube-context'], help='Kubconfig context from current machine.')
