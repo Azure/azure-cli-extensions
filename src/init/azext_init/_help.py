897a2d43407de511cf69be2708a7335477fcd3a7
zhoxing-ms <Zhou.Xing@microsoft.com>
2022-07-18 11:03:33 +0800

    Refine doc for az init

--- src/init/azext_init/_help.py
+++ src/init/azext_init/_help.py
@@ -10,4 +10,8 @@ from knack.help_files import helps  # pylint: disable=unused-import
 helps['init'] = """
     type: command
     short-summary: It's an effortless setting up tool for configs.
+    long-summary: |
+        This tool can help you quickly know and set up global configurations that are suitable for your current usage environment:
+        It supports setting bundles for interactive mode and automatic mode to help you quickly set the recommended configurations.
+        It also supports walk through style to help you get familiar with the common configurations and customize them one by one.
 """
