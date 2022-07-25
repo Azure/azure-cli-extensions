7168022dad09f3e955d70436595352f966915217
zhoxing-ms <Zhou.Xing@microsoft.com>
2022-07-25 14:39:12 +0800

    Improve summary message

--- src/init/azext_init/_help.py
+++ src/init/azext_init/_help.py
@@ -8,16 +8,15 @@ from knack.help_files import helps  # pylint: disable=unused-import
 
 
 helps['init'] = """
-    type: command
-    short-summary: It's an effortless setting up tool for configs.
+type: command
+short-summary: It's an effortless setting up tool for configs by offering an interactive question-answer experience
 long-summary: |
-The Azure CLI offers `az init` to help you set global configurations through an interactive format.  Global settings are grouped into bundles that help you optimize your settings for scenarios such as "interaction" or "automation".  It also supports a walk-through experience to help you get familiar with the common configurations and customize them one by one.
+    The Azure CLI offers `az init` to help you quickly understand and set global configurations that are suitable for your usage environment:
+    Global settings are grouped into bundles that help you optimize your settings for scenarios such as "interaction" or "automation". So it supports setting bundles for interactive and automation scenarios to help you quickly set up the recommended configurations.
+    It also supports a walk-through experience to help you get familiar with the common configurations and customize them one by one.
 
-The purpose of `az init` is not to replace the `az config` command, but to simplify the setting of configurations by offering an interactive question-answer experience.  While both `az init` and `az config` modify the same configuration file, `az init` is designed to only set the most common configurations.  For more granular control of all available configurations, use `az config`.  For more information on configuring your Azure CLI environment, see https://docs.microsoft.com/en-us/cli/azure/azure-cli-configuration.
-        az init can help you quickly understand and set up global configurations that are suitable for your current usage environment:
-        It supports setting bundles for interactive and automation scenarios to help you quickly set the recommended configurations.
-        It also supports a walk-through experience to help you get familiar with the common configurations and customize them one by one.
-
-        The purpose of "az init" is not to replace the "az config" command, but to simplify common configurations by supporting setting bundles.
-        While "az init" and "az config" modify the same configuration file, "az init" is aimed at an interactive, standardized approach to managing your configuration and "az config" is better at more granular control and setting configurations in automation scenarios.
+    The purpose of `az init` is not to replace the `az config` command, but to simplify the setting of configurations by offering an interactive question-answer experience.  
+    While both `az init` and `az config` modify the same configuration file, `az init` is aimed at an interactive, standardized approach to managing your configuration and designed to only set the most common configurations.  
+    For more granular control of all available configurations, please use `az config`.  
+    For more information on configuring your Azure CLI environment, please see https://docs.microsoft.com/en-us/cli/azure/azure-cli-configuration.
 """
