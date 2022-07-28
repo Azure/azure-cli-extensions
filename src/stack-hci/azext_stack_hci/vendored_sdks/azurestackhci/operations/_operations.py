6a6badcaabcf93d1d91d6d3bcf065ba164fda4e8
songlu <37168047+PARADISSEEKR@users.noreply.github.com>
2022-07-28 09:45:05 +0800

    [Stack-hci] Upgrade api-version to `2022-05-01` (#5132)

--- src/stack-hci/azext_stack_hci/vendored_sdks/azurestackhci/operations/_operations.py
+++ src/stack-hci/azext_stack_hci/vendored_sdks/azurestackhci/operations/_operations.py
@@ -61,7 +61,7 @@ class Operations(object):
             401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
         }
         error_map.update(kwargs.pop('error_map', {}))
-        api_version = "2022-01-01"
+        api_version = "2022-05-01"
         accept = "application/json"
 
         # Construct URL
