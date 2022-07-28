8c9cf144ab93a60233a440eddcbc4e72cd0cf738
Yugang Wang <yugangw@microsoft.com>
2022-07-28 12:09:46 -0700

    fix lint error

--- src/amg/azext_amg/_params.py
+++ src/amg/azext_amg/_params.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-# pylint: disable=line-too-long
+# pylint: disable=line-too-long, too-many-statements
 
 
 def load_arguments(self, _):
