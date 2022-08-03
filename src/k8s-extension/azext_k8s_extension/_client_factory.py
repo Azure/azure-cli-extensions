29d0f273f62de7ab20dee671ee914a8752f609ce
Deeksha Sharma <deesharma@microsoft.com>
2022-08-02 22:30:04 -0700

    const

--- src/k8s-extension/azext_k8s_extension/_client_factory.py
+++ src/k8s-extension/azext_k8s_extension/_client_factory.py
@@ -5,7 +5,7 @@
 
 from azure.cli.core.commands.client_factory import get_mgmt_service_client
 from azure.cli.core.profiles import ResourceType
-
+from . import consts
 
 def cf_k8s_extension(cli_ctx, **kwargs):
     from .vendored_sdks import SourceControlConfigurationClient
@@ -17,19 +17,19 @@ def cf_k8s_extension_operation(cli_ctx, _):
 
 
 def cf_k8s_cluster_extension_types_operation(cli_ctx, _):
-    return cf_k8s_extension(cli_ctx, api_version='2022-01-15-preview').cluster_extension_types
+    return cf_k8s_extension(cli_ctx, consts.EXTENSION_TYPE_API_VERSION).cluster_extension_types
 
 
 def cf_k8s_cluster_extension_type_operation(cli_ctx, _):
-    return cf_k8s_extension(cli_ctx, api_version='2022-01-15-preview').cluster_extension_type
+    return cf_k8s_extension(cli_ctx, consts.EXTENSION_TYPE_API_VERSION).cluster_extension_type
 
 
 def cf_k8s_location_extension_types_operation(cli_ctx, _):
-    return cf_k8s_extension(cli_ctx, api_version='2022-01-15-preview').location_extension_types
+    return cf_k8s_extension(cli_ctx, consts.EXTENSION_TYPE_API_VERSION).location_extension_types
 
 
 def cf_k8s_extension_type_versions_operation(cli_ctx, _):
-    return cf_k8s_extension(cli_ctx, api_version='2022-01-15-preview').extension_type_versions
+    return cf_k8s_extension(cli_ctx, consts.EXTENSION_TYPE_API_VERSION).extension_type_versions
 
 
 def cf_resource_groups(cli_ctx, subscription_id=None):
