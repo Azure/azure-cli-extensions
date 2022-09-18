45278c7fd2b666b2a3c313b1a59293325afca537
Raphael Yu <xiangy@microsoft.com>
2022-09-19 01:32:14 +0800

    Update the test cases.

--- src/spring/azext_spring/tests/latest/test_asa_scenario.py
+++ src/spring/azext_spring/tests/latest/test_asa_scenario.py
@@ -213,6 +213,7 @@ class CustomImageTest(ScenarioTest):
             self.check('name', 'default'),
             self.check('properties.source.type', 'Container'),
             self.check('properties.source.customContainer.containerImage', '{containerImage}'),
+            self.check('properties.source.customContainer.languageFramework', None),
         ])
 
         self.cmd('spring app deploy -g {resourceGroup} -s {serviceName} -n {app} --container-image {containerImage} --container-command "java" --container-args "-cp /app/resources:/app/classes:/app/libs/* hello.Application"', checks=[
@@ -223,8 +224,9 @@ class CustomImageTest(ScenarioTest):
             self.check('properties.source.customContainer.args', ['-cp', '/app/resources:/app/classes:/app/libs/*', 'hello.Application']),
         ])
 
-        self.cmd('spring app deployment create -g {resourceGroup} -s {serviceName} --app {app} -n green --container-image {containerImage}', checks=[
+        self.cmd('spring app deployment create -g {resourceGroup} -s {serviceName} --app {app} -n green --container-image {containerImage} --language-framework springboot', checks=[
             self.check('name', 'green'),
             self.check('properties.source.type', 'Container'),
             self.check('properties.source.customContainer.containerImage', '{containerImage}'),
+            self.check('properties.source.customContainer.languageFramework', 'springboot'),
         ])
