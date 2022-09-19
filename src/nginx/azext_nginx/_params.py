c2dad3a099af1f38e5217ce6f31a093bed8fab6b
George Ngugi <georgengugi@microsoft.com>
2022-09-19 16:43:47 +0300

    remove cmds in params.py

--- src/nginx/azext_nginx/_params.py
+++ src/nginx/azext_nginx/_params.py
@@ -7,17 +7,5 @@
 from knack.arguments import CLIArgumentType
 
 
-def load_arguments(self, _):
-
-    from azure.cli.core.commands.parameters import tags_type
-    from azure.cli.core.commands.validators import get_default_location_from_resource_group
-
-    nginx_name_type = CLIArgumentType(options_list='--nginx-name-name', help='Name of the Nginx.', id_part='name')
-
-    with self.argument_context('nginx') as c:
-        c.argument('tags', tags_type)
-        c.argument('location', validator=get_default_location_from_resource_group)
-        c.argument('nginx_name', nginx_name_type, options_list=['--name', '-n'])
-
-    with self.argument_context('nginx list') as c:
-        c.argument('nginx_name', nginx_name_type, id_part=None)
+def load_arguments(self, _):  # pylint: disable=unused-argument
+    pass
