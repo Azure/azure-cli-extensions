4e5ae178834da17cb73bfb5d47982337dcf1a9c8
deeksha345 <34255011+deeksha345@users.noreply.github.com>
2022-09-26 13:56:00 -0700

    Update _client_factory.py

--- src/k8s-extension/azext_k8s_extension/_client_factory.py
+++ src/k8s-extension/azext_k8s_extension/_client_factory.py
@@ -13,24 +13,24 @@ def cf_k8s_extension(cli_ctx, **kwargs):
     return get_mgmt_service_client(cli_ctx, SourceControlConfigurationClient, **kwargs)
 
 
-def cf_k8s_extension_operation(cli_ctx, _):
+def cf_k8s_extension_operation(cli_ctx, *_):
     return cf_k8s_extension(cli_ctx).extensions
 
 
-def cf_k8s_cluster_extension_types_operation(cli_ctx, _):
-    return cf_k8s_extension(cli_ctx, consts.EXTENSION_TYPE_API_VERSION).cluster_extension_types
+def cf_k8s_cluster_extension_types_operation(cli_ctx, *_):
+    return cf_k8s_extension(cli_ctx, api_version=consts.EXTENSION_TYPE_API_VERSION).cluster_extension_types
 
 
-def cf_k8s_cluster_extension_type_operation(cli_ctx, _):
-    return cf_k8s_extension(cli_ctx, consts.EXTENSION_TYPE_API_VERSION).cluster_extension_type
+def cf_k8s_cluster_extension_type_operation(cli_ctx, *_):
+    return cf_k8s_extension(cli_ctx, api_version=consts.EXTENSION_TYPE_API_VERSION).cluster_extension_type
 
 
-def cf_k8s_location_extension_types_operation(cli_ctx, _):
-    return cf_k8s_extension(cli_ctx, consts.EXTENSION_TYPE_API_VERSION).location_extension_types
+def cf_k8s_location_extension_types_operation(cli_ctx, *_):
+    return cf_k8s_extension(cli_ctx, api_version=consts.EXTENSION_TYPE_API_VERSION).location_extension_types
 
 
-def cf_k8s_extension_type_versions_operation(cli_ctx, _):
-    return cf_k8s_extension(cli_ctx, consts.EXTENSION_TYPE_API_VERSION).extension_type_versions
+def cf_k8s_extension_type_versions_operation(cli_ctx, *_):
+    return cf_k8s_extension(cli_ctx, api_version=consts.EXTENSION_TYPE_API_VERSION).extension_type_versions
 
 
 def cf_resource_groups(cli_ctx, subscription_id=None):
