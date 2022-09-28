52a428bc5ea7ef423aa274e72321e9e090f2ed84
Deeksha Sharma <deesharma@microsoft.com>
2022-09-28 16:57:27 -0700

    [k8s-extension] Update extension CLI to v1.3.6

--- testing/test/helper/Constants.ps1
+++
@@ -1,7 +0,0 @@
-$ENVCONFIG = Get-Content -Path $PSScriptRoot/../../settings.json | ConvertFrom-Json
-$SUCCESS_MESSAGE = "Successfully installed the extension"
-$FAILED_MESSAGE = "Failed to install the extension"
-
-$POD_RUNNING = "Running"
-
-$MAX_RETRY_ATTEMPTS = 10
