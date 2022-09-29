a53894fd29d5e7fcedd9e0e61a24e31723810083
Deeksha Sharma <deesharma@microsoft.com>
2022-09-29 15:15:23 -0700

    [k8s-extension] Update extension CLI to v1.3.6

--- testing/test/configurations/Constants.ps1
+++
@@ -1,8 +0,0 @@
-$ENVCONFIG = Get-Content -Path $PSScriptRoot\..\..\settings.json | ConvertFrom-Json
-$SUCCESS_MESSAGE = "Successfully installed the operator"
-$FAILED_MESSAGE = "Failed the install of the operator"
-$TMP_DIRECTORY = "$PSScriptRoot\..\..\tmp"
-
-$POD_RUNNING = "Running"
-
-$MAX_RETRY_ATTEMPTS = 18
