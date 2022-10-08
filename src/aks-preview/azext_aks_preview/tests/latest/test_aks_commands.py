2bd9f7acff9d6111a21bd27d9b75d9c9a8742bc2
mattstam <mattstam@live.com>
2022-10-08 01:54:19 -0700

    remove from matrix, no live_only

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -5751,7 +5751,6 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             'aks delete -g {resource_group} -n {name} --yes --no-wait', checks=[self.is_empty()])
     
     # live only due to workspace is not mocked correctly
-    @live_only()
     @AllowLargeResponse()
     @AKSCustomResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='westus2')
     def test_aks_create_with_kube_proxy_config(self, resource_group, resource_group_location):
