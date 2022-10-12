fe280ff4393909b67b9b417e6487d2fdcd8cb21c
bragi92 <kadubey@microsoft.com>
2022-10-11 23:39:56 -0700

    Update src/aks-preview/azext_aks_preview/_help.py
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- src/aks-preview/azext_aks_preview/_help.py
+++ src/aks-preview/azext_aks_preview/_help.py
@@ -850,7 +850,7 @@ helps['aks update'] = """
           type: bool
           short-summary: Comma-separated list of additional Kubernetes label keys that will be used in the resource' labels metric. By default the metric contains only name and namespace labels. To include additional labels provide a list of resource names in their plural form and Kubernetes label keys you would like to allow for them (e.g. '=namespaces=[k8s-label-1,k8s-label-n,...],pods=[app],...)'. A single '*' can be provided per resource instead to allow any labels, but that has severe performance implications (e.g. '=pods=[*]').
         - name: --ksm-metric-annotations-allow-list
-          type: bool
+          type: string
           short-summary: Comma-separated list of additional Kubernetes label keys that will be used in the resource' labels metric. By default the metric contains only name and namespace labels. To include additional labels provide a list of resource names in their plural form and Kubernetes label keys you would like to allow for them (e.g.'=namespaces=[k8s-label-1,k8s-label-n,...],pods=[app],...)'. A single '*' can be provided per resource instead to allow any labels, but that has severe performance implications (e.g. '=pods=[*]').
         - name: --grafana-resource-id
           type: bool
