2365b766a011a5e79e1554cc83fce2732efcf731
pensh <pensh@microsoft.com>
2022-10-16 11:15:46 +0800

    remove unnecessary changes

--- src/spring/azext_spring/commands.py
+++ src/spring/azext_spring/commands.py
@@ -173,7 +173,7 @@ def load_command_table(self, _):
                             exception_handler=handle_asc_exception) as g:
         g.custom_command('create', 'deployment_create', supports_no_wait=True)
 
-    with self.command_group('spring app deployment', client_factory=cf_spring_20220901preview,
+    with self.command_group('spring app deployment', client_factory=cf_spring_20220501preview,
                             exception_handler=handle_asc_exception) as g:
         g.custom_command('list', 'deployment_list',
                          table_transformer=transform_spring_deployment_output)
