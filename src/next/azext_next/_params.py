33df71aed909b525dc9fcc571fcf1243afe52fc8
Qinkai Wu <32201005+ReaNAiveD@users.noreply.github.com>
2022-10-19 17:00:52 +0800

    Update src/next/azext_next/_params.py
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- src/next/azext_next/_params.py
+++ src/next/azext_next/_params.py
@@ -9,5 +9,5 @@ from knack.arguments import CLIArgumentType  # pylint: disable=unused-import
 
 def load_arguments(self, _):  # pylint: disable=unused-argument
     with self.argument_context('next') as c:
-        c.argument('scenario_only', options_list=['--scenario', '-s'], action='store_true', help='recommend e2e scenarios only')
-        c.argument('command_only', options_list=['--command', '-c'], action='store_true', help='recommend commands only')
+        c.argument('scenario_only', options_list=['--scenario', '-s'], action='store_true', help='Specify this parameter will only recommend E2E scenarios')
+        c.argument('command_only', options_list=['--command', '-c'], action='store_true', help='Specify this parameter will only recommend commands')
