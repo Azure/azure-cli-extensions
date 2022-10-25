80434e6eddb0e7abed11fdb1d4b9b87548bc954f
zmssp <pensh@microsoft.com>
2022-10-25 14:54:04 +0800

    Add remote debugging support for spring (#5453)

--- src/spring/azext_spring/_help.py
+++ src/spring/azext_spring/_help.py
@@ -216,6 +216,21 @@ helps['spring app restart'] = """
     short-summary: Restart instances of the app, default to production deployment.
 """
 
+helps['spring app enable-remote-debugging'] = """
+    type: command
+    short-summary: Enable remote debugging for a deployment.
+"""
+
+helps['spring app disable-remote-debugging'] = """
+    type: command
+    short-summary: Disable remote debugging for a deployment.
+"""
+
+helps['spring app get-remote-debugging-config'] = """
+    type: command
+    short-summary: Get the remote debugging configuration of a deployment.
+"""
+
 helps['spring app deploy'] = """
     type: command
     short-summary: Deploy source code or pre-built binary to an app and update related configurations.
