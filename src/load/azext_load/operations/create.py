9150932565e240e510fddbd71bd8911d98842d75
Venkat Ragavan <50309787+venkatr21@users.noreply.github.com>
2022-11-17 16:53:33 +0530

    added license in custom files

--- src/load/azext_load/operations/create.py
+++ src/load/azext_load/operations/create.py
@@ -1,3 +1,9 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#
+# --------------------------------------------------------------------------------------------
+
 from azure.cli.core.aaz import has_value
 from azure.cli.core.azclierror import (
     InvalidArgumentValueError
