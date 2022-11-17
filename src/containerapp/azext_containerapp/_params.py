6a4bee4b876968daf72126efb4dff7d8ecc1d175
Haroon Feisal <38823870+haroonf@users.noreply.github.com>
2022-11-17 15:43:45 -0800

    Update src/containerapp/azext_containerapp/_params.py
    Co-authored-by: Anthony Chu <anthony@anthonychu.ca>

--- src/containerapp/azext_containerapp/_params.py
+++ src/containerapp/azext_containerapp/_params.py
@@ -253,7 +253,7 @@ def load_arguments(self, _):
         c.argument('action', arg_type=get_enum_type(['Allow', 'Deny']), help='Whether the IP security restriction allows or denies access. All restrictions must be use the same action. If no restrictions are set, all traffic is allowed.')
         c.argument('rule_name', help="The IP security restriction name.")
         c.argument('description', help="The description of the IP security restriction.")
-        c.argument('ip_address', help="The ip address range of the ip security restriction.")
+        c.argument('ip_address', help="The address range of the IP security restriction in IPv4 CIDR notation. (for example, '198.51.100.14/24')")
 
     with self.argument_context('containerapp ingress access-restriction list') as c:
         c.argument('name', id_part=None)
