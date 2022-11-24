5501a71c8fe9170270ef2d60e40ab701719e760d
Sayan Naskar <nascarsayan@gmail.com>
2022-11-24 08:02:46 +0530

    [connectedvmware] Bump up api version, add vm retain support  (#5556)

--- src/connectedvmware/azext_connectedvmware/custom.py
+++ src/connectedvmware/azext_connectedvmware/custom.py
@@ -7,8 +7,8 @@
 
 from pwinput import pwinput
 from knack.util import CLIError
-from azext_connectedvmware.vmware_utils import get_resource_id
 from azure.cli.core.util import sdk_no_wait
+from azext_connectedvmware.vmware_utils import get_resource_id
 from .vmware_constants import (
     VMWARE_NAMESPACE,
     VCENTER_RESOURCE_TYPE,
@@ -975,11 +975,12 @@ def delete_vm(
     resource_group_name,
     resource_name,
     force=False,
+    retain=None,
     no_wait=False,
 ):
 
     return sdk_no_wait(
-        no_wait, client.begin_delete, resource_group_name, resource_name, force
+        no_wait, client.begin_delete, resource_group_name, resource_name, force, retain,
     )
 
 
