10bd24796ae920aea2a9dd9027b67faaad420c30
Warren Jones <v-wjones@microsoft.com>
2022-12-19 15:55:52 -0800

    Replace license header in storage.py

--- src/quantum/azext_quantum/storage.py
+++ src/quantum/azext_quantum/storage.py
@@ -1,7 +1,8 @@
-##
+# --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-##
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 import logging
 from typing import Any, Dict
 from azure.core import exceptions
