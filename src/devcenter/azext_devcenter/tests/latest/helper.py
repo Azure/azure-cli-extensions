3f3f5c2c63537f68e3dd4aea90e67e990142d6c8
Amanda Lim <amlim@microsoft.com>
2023-01-20 12:18:02 -0800

    updates to test

--- src/devcenter/azext_devcenter/tests/latest/helper.py
+++ src/devcenter/azext_devcenter/tests/latest/helper.py
@@ -107,11 +107,11 @@ def create_sig(self):
 
 def create_sig_with_role_assignments(self):
 
+    create_sig(self)
+
     self.kwargs.update({
         'windows365ObjectId': '8eec7c09-06ae-48e9-aafd-9fb31a5d5175'
     })
-
-    create_sig(self)
 
     self.cmd('az role assignment create --role "Contributor" '
                  '--assignee "{identityPrincipalId}" '
