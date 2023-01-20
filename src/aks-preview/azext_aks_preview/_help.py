7a3f75cb1ab75adaaf7abfad8115b3a67a51e682
Charlie McBride <33269602+charliedmcb@users.noreply.github.com>
2023-01-19 18:08:26 -0800

    Update src/aks-preview/azext_aks_preview/_help.py
    Co-authored-by: FumingZhang <81607949+FumingZhang@users.noreply.github.com>

--- src/aks-preview/azext_aks_preview/_help.py
+++ src/aks-preview/azext_aks_preview/_help.py
@@ -726,7 +726,7 @@ helps['aks update'] = """
           short-summary: Specify the upgrade channel for autoupgrade. It could be rapid, stable, patch, node-image or none, none means disable autoupgrade.
         - name: --node-os-upgrade-channel
           type: string
-          short-summary: Manner in which the OS on your nodes is updated. It could be NodeImage, None, SecurityPatch or Unmanaged
+          short-summary: Manner in which the OS on your nodes is updated. It could be NodeImage, None, SecurityPatch or Unmanaged.
         - name: --enable-managed-identity
           type: bool
           short-summary: Update current cluster to managed identity to manage cluster resource group.
