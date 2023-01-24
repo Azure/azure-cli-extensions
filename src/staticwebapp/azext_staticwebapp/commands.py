bfc0d43a872775329b2007a99766b264456850fa
Silas Strawn <strawnsc@gmail.com>
2023-01-24 14:27:38 -0800

    mark command group as preview

--- src/staticwebapp/azext_staticwebapp/commands.py
+++ src/staticwebapp/azext_staticwebapp/commands.py
@@ -5,7 +5,7 @@
 
 
 def load_command_table(self, _):
-    with self.command_group('staticwebapp dbconnection') as g:
+    with self.command_group('staticwebapp dbconnection', is_preview=True) as g:
         g.custom_command('create', 'create_dbconnection')
         g.custom_command('show', 'show_dbconnection')
         g.custom_command('delete', 'delete_dbconnection')
