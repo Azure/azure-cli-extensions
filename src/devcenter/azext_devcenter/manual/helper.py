e97e711e860c9f8a92da28c9c04e074f64a22aaf
Amanda Lim <amlim@microsoft.com>
2023-02-08 14:17:09 -0800

    Fix PR build errors

--- src/devcenter/azext_devcenter/manual/helper.py
+++ src/devcenter/azext_devcenter/manual/helper.py
@@ -1,3 +1,9 @@
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+#
+# --------------------------------------------------------------------------
 import json
 from azure.cli.core.azclierror import ResourceNotFoundError
 from azure.cli.core.util import send_raw_request
