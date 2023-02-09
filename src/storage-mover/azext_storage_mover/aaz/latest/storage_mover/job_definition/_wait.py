6a3daeafcb22134e7e7305b1bdeb4ae3959c5014
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2023-02-09 16:13:11 +0800

    change job-definition update to PATCH

--- src/storage-mover/azext_storage_mover/aaz/latest/storage_mover/job_definition/_wait.py
+++ src/storage-mover/azext_storage_mover/aaz/latest/storage_mover/job_definition/_wait.py
@@ -241,27 +241,21 @@ class Wait(AAZWaitCommand):
             system_data = cls._schema_on_200.system_data
             system_data.created_at = AAZStrType(
                 serialized_name="createdAt",
-                flags={"read_only": True},
             )
             system_data.created_by = AAZStrType(
                 serialized_name="createdBy",
-                flags={"read_only": True},
             )
             system_data.created_by_type = AAZStrType(
                 serialized_name="createdByType",
-                flags={"read_only": True},
             )
             system_data.last_modified_at = AAZStrType(
                 serialized_name="lastModifiedAt",
-                flags={"read_only": True},
             )
             system_data.last_modified_by = AAZStrType(
                 serialized_name="lastModifiedBy",
-                flags={"read_only": True},
             )
             system_data.last_modified_by_type = AAZStrType(
                 serialized_name="lastModifiedByType",
-                flags={"read_only": True},
             )
 
             return cls._schema_on_200
