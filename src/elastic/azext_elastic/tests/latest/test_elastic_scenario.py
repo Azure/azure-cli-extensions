722ca28e8c089c750ab4f8f4f5578eea4a71a8ef
songlu <37168047+PARADISSEEKR@users.noreply.github.com>
2023-03-01 17:26:51 +0800

    elastic

--- src/elastic/azext_elastic/tests/latest/test_elastic_scenario.py
+++ src/elastic/azext_elastic/tests/latest/test_elastic_scenario.py
@@ -8,102 +8,17 @@
 # regenerated.
 # --------------------------------------------------------------------------
 
-import os
-from azure.cli.testsdk import ScenarioTest
-from azure.cli.testsdk import ResourceGroupPreparer
-from .example_steps import step_monitor_create
-from .example_steps import step_monitor_show
-from .example_steps import step_monitor_list
-from .example_steps import step_monitor_list2
-from .example_steps import step_monitor_update
-from .example_steps import step_deployment_info_list
-from .example_steps import step_monitored_resource_list
-from .example_steps import step_tag_rule_create
-from .example_steps import step_tag_rule_show
-from .example_steps import step_tag_rule_list
-from .example_steps import step_tag_rule_delete
-from .example_steps import step_vm_collection_update
-from .example_steps import step_vm_host_list
-from .example_steps import step_vm_ingestion_detail
-from .example_steps import step_monitor_delete
-from .. import (
-    try_manual,
-    raise_if,
-    calc_coverage
-)
+from azure.cli.testsdk import *
 
 
-TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
-
-
-# Env setup_scenario
-@try_manual
-def setup_scenario(test):
-    pass
-
-
-# Env cleanup_scenario
-@try_manual
-def cleanup_scenario(test):
-    pass
-
-
-# Testcase: Scenario
-@try_manual
-def call_scenario(test):
-    setup_scenario(test)
-    step_monitor_create(test, checks=[
-        test.check("name", "{myMonitor}", case_sensitive=False),
-        test.check("location", "westus2", case_sensitive=False),
-        test.check("sku.name", "ess-monthly-consumption_Monthly", case_sensitive=False),
-        test.check("tags.Environment", "Dev", case_sensitive=False),
-    ])
-    step_monitor_show(test, checks=[
-        test.check("name", "{myMonitor}", case_sensitive=False),
-        test.check("location", "westus2", case_sensitive=False),
-        test.check("tags.Environment", "Dev", case_sensitive=False),
-    ])
-    step_monitor_list(test, checks=[
-        test.check('length(@)', 37),
-    ])
-    step_monitor_list2(test, checks=[
-        test.check('length(@)', 1),
-    ])
-    step_monitor_update(test, checks=[
-        test.check("name", "{myMonitor}", case_sensitive=False),
-        test.check("location", "westus2", case_sensitive=False),
-        test.check("sku.name", "ess-monthly-consumption_Monthly", case_sensitive=False),
-        test.check("tags.Environment", "Dev", case_sensitive=False),
-    ])
-    step_deployment_info_list(test, checks=[])
-    step_monitored_resource_list(test, checks=[])
-    step_tag_rule_create(test, checks=[])
-    step_tag_rule_show(test, checks=[])
-    step_tag_rule_list(test, checks=[])
-    # Error  (ResourceDeletionFailed) Resource deletion failed as RP returned status code: 'UnprocessableEntity'
-    # step_tag_rule_delete(test, checks=[])
-    step_vm_collection_update(test, checks=[])
-    step_vm_host_list(test, checks=[])
-    step_vm_ingestion_detail(test, checks=[])
-    step_monitor_delete(test, checks=[])
-    cleanup_scenario(test)
-
-
-# Test class for Scenario
-@try_manual
-class ElasticScenarioTest(ScenarioTest):
-    def __init__(self, *args, **kwargs):
-        super(ElasticScenarioTest, self).__init__(*args, **kwargs)
+class ElasticScenario(ScenarioTest):
+    @ResourceGroupPreparer(name_prefix='cli_test_elastic_monitor', location='eastus')
+    def test_elastic_monitor(self, resource_group):
         self.kwargs.update({
-            'subscription_id': self.get_subscription_id()
+            'monitor': self.create_random_name('monitor', 20),
         })
-
-        self.kwargs.update({
-            'myMonitor': 'myMonitor',
-        })
-
-    @ResourceGroupPreparer(name_prefix='clitestelastic_myResourceGroup'[:7], key='rg', parameter_name='rg')
-    def test_elastic_Scenario(self, rg):
-        call_scenario(self)
-        calc_coverage(__file__)
-        raise_if()
+        self.cmd('elastic monitor create -n {mobile_network} -g {rg} --identifier {{mcc:001,mnc:01}}', checks=[
+            self.check('publicLandMobileNetworkIdentifier.mcc', '001'),
+            self.check('publicLandMobileNetworkIdentifier.mnc', '01')
+        ])
+        
