b487a76ce5ab01d90f6f900dffa7edd0d12d2826
gaoyp830 <51546332+gaoyp830@users.noreply.github.com>
2023-03-06 17:50:15 -0800

    new version

--- src/reservation/setup.py
+++ src/reservation/setup.py
@@ -10,7 +10,7 @@ from codecs import open
 from setuptools import setup, find_packages
 
 # HISTORY.rst entry.
-VERSION = '0.2.0'
+VERSION = '0.3.0'
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
