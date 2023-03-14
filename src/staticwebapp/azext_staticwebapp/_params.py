11b3ba39902336e82206de2fe47020ac6d25662e
Xing Zhou <Zhou.Xing@microsoft.com>
2023-03-14 15:39:08 +0800

    Apply suggestions from code review
    Co-authored-by: Yan Zhu <105691024+yanzhudd@users.noreply.github.com>

--- src/staticwebapp/azext_staticwebapp/_params.py
+++ src/staticwebapp/azext_staticwebapp/_params.py
@@ -9,14 +9,14 @@ def load_arguments(self, _):
     from azure.cli.core.commands.parameters import resource_group_name_type
 
     with self.argument_context('staticwebapp dbconnection') as c:
-        c.argument('name', options_list=['--name', '-n'], help="Name of the Static Web App")
+        c.argument('name', options_list=['--name', '-n'], help="Name of the Static Web App.")
         c.argument('resource_group_name', resource_group_name_type)
         c.argument('environment', help="Name of the environment of Static Web App.",
                    options_list=["--environment", "-e"])
 
     with self.argument_context('staticwebapp dbconnection create') as c:
         c.argument('db_resource_id', options_list=['--db-resource-id', '-d'],
-                   help="The azure resource ID for the database server/account to connect to e.g. '/subscriptions/MySubId/resourceGroups/MyResourceGroup/providers/Microsoft.Sql/servers/MyServer' for an Azure SQL database")
+                   help="The azure resource ID for the database server/account to connect to e.g. '/subscriptions/MySubId/resourceGroups/MyResourceGroup/providers/Microsoft.Sql/servers/MyServer' for an Azure SQL database.")
         c.argument('db_name', options_list=['--db-name', '-b'], help="The name of the database to connect to. Not required for CosmosDB.")
         c.argument('username', options_list=["--username", "-u"], help="The username to use for authentication with the database. Not required for all databases.")
         c.argument('password', options_list=["--password", "-p"], help="The password to use for authentication with the database. Not required for all databases.")
@@ -25,4 +25,4 @@ def load_arguments(self, _):
 
     with self.argument_context('staticwebapp dbconnection show') as c:
         c.argument('detailed', options_list=['--detailed', '-d'], action='store_true',
-                   default=False, help="Get detailed information on database connections")
+                   default=False, help="Get detailed information on database connections.")
