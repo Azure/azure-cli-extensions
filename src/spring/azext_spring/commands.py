124391b5364fe2fa6c72e715ffb5b59eff57d3fb
Yuwei Zhou <yuwzho@microsoft.com>
2023-03-15 18:40:53 +0800

    Update commands.py

--- src/spring/azext_spring/commands.py
+++ src/spring/azext_spring/commands.py
@@ -267,7 +267,7 @@ def load_command_table(self, _):
                               table_transformer=transform_dev_tool_portal_output)
         g.custom_command('create', 'create', table_transformer=transform_dev_tool_portal_output, validator=validate_dev_tool_portal, supports_no_wait=True)
         g.custom_command('update', 'update', table_transformer=transform_dev_tool_portal_output, validator=validate_dev_tool_portal, supports_no_wait=True)
-        g.custom_command('delete', 'delete', confirmation=True)
+        g.custom_command('delete', 'delete', supports_no_wait=True, confirmation=True)
 
     with self.command_group('spring application-live-view', is_preview=True,
                             custom_command_type=application_live_view_cmd_group,
