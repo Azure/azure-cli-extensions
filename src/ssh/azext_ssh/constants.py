54fd2c22c7bf79237217d189f260d5edb965e2a0
Vivian Thiebaut <vthiebaut@microsoft.com>
2023-03-17 15:30:28 -0400

    initial changes to add error handling to resource type

--- src/ssh/azext_ssh/constants.py
+++ src/ssh/azext_ssh/constants.py
@@ -17,6 +17,8 @@ RECOMMENDATION_SSH_CLIENT_NOT_FOUND = (Fore.YELLOW + "Ensure OpenSSH is installe
                                        "--ssh-client-folder to provide OpenSSH folder path." + Style.RESET_ALL)
 RECOMMENDATION_RESOURCE_NOT_FOUND = (Fore.YELLOW + "Please ensure the active subscription is set properly "
                                      "and resource exists." + Style.RESET_ALL)
+RECOMMENDATION_FORBIDDEN = (Fore.YELLOW + "Please ensure that you have the necessary permissions to connect to the " +
+                            "target resource and that the active subscription is set properly." + Style.RESET_ALL)
 RDP_TERMINATE_SSH_WAIT_TIME_IN_SECONDS = 30
 
 ARC_RESOURCE_TYPE_PLACEHOLDER = "arc_resource_type_placeholder"
