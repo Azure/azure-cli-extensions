f276af1fc221f85c5ffcb072de264658026b6405
xiaofanzhou <xiaofanzhou@microsoft.com>
2023-03-23 16:46:25 +0800

    lint

--- src/serviceconnector-passwordless/azext_serviceconnector_passwordless/config.py
+++ src/serviceconnector-passwordless/azext_serviceconnector_passwordless/config.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-# TODO: Confirm this is the right version number you want and it matches your
-# HISTORY.rst entry.
-VERSION='0.3.0'
-NAME='serviceconnector-passwordless'
+
+
+VERSION = '0.3.0'
+NAME = 'serviceconnector-passwordless'
