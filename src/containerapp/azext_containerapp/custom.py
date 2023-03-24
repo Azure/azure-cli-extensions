9f9c26ff3c2fae952bdbcc63e1d0da6c78a54a46
xinyu pang <1042945277@qq.com>
2023-03-24 15:24:54 +0800

    fix checking

--- src/containerapp/azext_containerapp/custom.py
+++ src/containerapp/azext_containerapp/custom.py
@@ -1155,6 +1155,8 @@ def update_managed_environment(cmd,
 
     # Custom domains
     if hostname:
+        if not certificate_file:
+            raise ValidationError("Must provide certificate-file if updating the DNS suffix for the environment's custom domain.")
         safe_set(env_def, "properties", "customDomainConfiguration", value={})
         cert_def = env_def["properties"]["customDomainConfiguration"]
         blob, _ = load_cert_file(certificate_file, certificate_password)
