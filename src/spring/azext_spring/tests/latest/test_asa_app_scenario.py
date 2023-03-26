03711fbe8c7eb7d2e09127bd89b643c73c0b71db
pensh <pensh@microsoft.com>
2023-03-26 09:29:14 +0800

    update test

--- src/spring/azext_spring/tests/latest/test_asa_app_scenario.py
+++ src/spring/azext_spring/tests/latest/test_asa_app_scenario.py
@@ -55,6 +55,18 @@ class AppDeploy(ScenarioTest):
         with self.assertRaisesRegexp(CLIError, "112404: Exit code 0: purposely stopped."):
             self.cmd('spring app deploy -n {app} -g {rg} -s {serviceName} --artifact-path {file} --version v2 --runtime-version NetCore_31 --main-entry test')
 
+        self.cmd('spring app update -n {app} -g {rg} -s {serviceName} --main-entry test1', checks=[
+            self.check('properties.activeDeployment.name', 'default'),
+            self.check('properties.activeDeployment.properties.deploymentSettings.resourceRequests.cpu', '2'),
+            self.check('properties.activeDeployment.properties.deploymentSettings.resourceRequests.memory', '1Gi'),
+            self.check('properties.activeDeployment.sku.capacity', 1),
+            self.check('properties.activeDeployment.properties.source.type', 'NetCoreZip'),
+            self.check('properties.activeDeployment.properties.source.version', 'v2'),
+            self.check('properties.activeDeployment.properties.source.runtimeVersion', 'NetCore_31'),
+            self.check('properties.activeDeployment.properties.source.netCoreMainEntryPath', 'test1'),
+            self.check('properties.activeDeployment.properties.deploymentSettings.environmentVariables', {'bas': 'baz'}),
+        ])
+
         # keep deploy .net
         with self.assertRaisesRegexp(CLIError, "112404: Exit code 0: purposely stopped."):
             self.cmd('spring app deploy -n {app} -g {rg} -s {serviceName} --artifact-path {file} --version v3 --main-entry test3')
@@ -88,6 +100,16 @@ class AppCRUD(ScenarioTest):
             self.check('properties.source.runtimeVersion', 'Java_11'),
             self.check('sku.capacity', 2),
             self.check('properties.deploymentSettings.environmentVariables', {'foo': 'bar'}),
+        ])
+
+        self.cmd('spring app update -n {app} -g {rg} -s {serviceName} --runtime-version Java_11', checks=[
+            self.check('properties.activeDeployment.name', 'default'),
+            self.check('properties.activeDeployment.properties.deploymentSettings.resourceRequests.cpu', '2'),
+            self.check('properties.activeDeployment.properties.deploymentSettings.resourceRequests.memory', '1Gi'),
+            self.check('properties.activeDeployment.sku.capacity', 1),
+            self.check('properties.activeDeployment.properties.source.type', 'Jar'),
+            self.check('properties.activeDeployment.properties.source.runtimeVersion', 'Java_11'),
+            self.check('properties.activeDeployment.properties.deploymentSettings.environmentVariables', {'foo': 'bar'}),
         ])
         self.cmd('spring app delete -n {app} -g {rg} -s {serviceName}')
 
