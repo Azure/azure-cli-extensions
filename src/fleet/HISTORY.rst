1a2f502a705d52af4931dd533f7c94fc39e45957
Pankil Daru <daru.pankil05@gmail.com>
2023-04-05 23:21:26 +0000

    upgrade fleet sdk

--- src/fleet/HISTORY.rst
+++ src/fleet/HISTORY.rst
@@ -10,12 +10,8 @@ Release History
 0.1.1
 ++++++
 * Upgrade SDK version to 2022-07-02-preview
-<<<<<<< HEAD
 * Bug fixes
 
 0.2.0
 ++++++
 * Use new SDK
-=======
-* Bug fixes
->>>>>>> ed3f463e9ef7980eff196504a8bb29800c123eba
--- src/fleet/azext_fleet/vendored_sdks/models.py
+++ src/fleet/azext_fleet/vendored_sdks/models.py
@@ -4,10 +4,4 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-<<<<<<< HEAD
 from .v2023_03_15_preview.models import *
-=======
-from .v2017_07_01.models import *
-from .v2019_04_30.models import *
-from .v2022_07_01.models import *
->>>>>>> ed3f463e9ef7980eff196504a8bb29800c123eba
