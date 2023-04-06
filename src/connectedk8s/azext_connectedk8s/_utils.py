f829b01fceedbd9c8ba6ccc0b2ac7faa03c195e4
Deeksha Sharma <deesharma@microsoft.com>
2023-04-06 12:50:20 -0700

    removing unneeded env variable update

--- src/connectedk8s/azext_connectedk8s/_utils.py
+++ src/connectedk8s/azext_connectedk8s/_utils.py
@@ -568,7 +568,6 @@ def helm_install_release(chart_path, subscription_id, kubernetes_distro, kuberne
     if container_log_path is not None:
         cmd_helm_install.extend(["--set", "systemDefaultValues.fluent-bit.containerLogPath={}".format(container_log_path)])
     if enable_oidc_issuer:
-        cmd_helm_install.extend(["--set", "systemDefaultValues.signingkeycontroller.oidcenabled={}".format(True)])
         cmd_helm_install.extend(["--set", "global.enableOidcIssuer={}".format(True)])
     if kube_config:
         cmd_helm_install.extend(["--kubeconfig", kube_config])
