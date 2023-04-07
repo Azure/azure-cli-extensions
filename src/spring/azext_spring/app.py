362582053be7af6c799ececa569d071d988e2883
pensh <pensh@microsoft.com>
2023-04-07 13:20:17 +0800

    remove unnecessary import

--- src/spring/azext_spring/app.py
+++ src/spring/azext_spring/app.py
@@ -2,7 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-import logging
 from datetime import datetime
 
 # pylint: disable=wrong-import-order
