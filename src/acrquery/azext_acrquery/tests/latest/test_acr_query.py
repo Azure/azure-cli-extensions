d009546013de061d54e6fd7c003d1fb8abe37f13
Caroline Barker <cabarker@microsoft.com>
2023-04-25 11:59:18 -0700

    Integration test files

--- src/acrquery/azext_acrquery/tests/latest/test_acr_query.py
+++ src/acrquery/azext_acrquery/tests/latest/test_acr_query.py
@@ -16,40 +16,33 @@ class AcrQueryTests(ScenarioTest):
             'registry_name': 'metadataunittest',
             'resource_group': 'cabarker',
             'repository_name': 'nginx',
-            'query': '"Manifests | limit 1"',
-            'username': 'user',
-            'password': 'password',
-            'password_name': 'password'
+            'password': 'password'
         })
-
         # Query with bearer auth and filter by count
         credentials = self.cmd(
             'acr credential show -n {registry_name} -g {resource_group}').get_output_in_json()
 
-        self.kwargs['username'] = credentials['username']
-        self.kwargs['password'] = credentials['passwords'][0]['value']
+        username = credentials['username']
+        password = credentials['passwords'][0]['value']
 
         self.cmd(
-            'acr query -n {registry_name} -q {query} --username {username} --password {password} ',
+            'acr query -n {} -q {} --username {} --password {} '.format('metadataunittest', '"Manifests | limit 1"', username, password),
             checks=[self.check('count', 1)])
 
         # Query with basic auth and filter by repository
         token = self.cmd('acr login -n {registry_name} --expose-token').get_output_in_json()
-        self.kwargs['username'] = EMPTY_GUID
-        self.kwargs['password'] = token["accessToken"]
+        bearer = token["accessToken"]
 
         self.cmd(
-            'acr query -n {registry_name} --repository {repository_name} -q {query} --username {username} --password {password} ',
+            'acr query -n {} --repository {} -q {} --username {} --password {} '.format('metadataunittest', 'nginx', '"Manifests | limit 1"', EMPTY_GUID, bearer),
             checks=[self.check('count', 1)])
 
         # Renew credentials
         self.cmd(
-            'acr credential renew -n {registry_name} --password-name {password_name} ')
+            'acr credential renew -n {registry_name} --password-name {password} ')
 
         # Filter by size
-        self.kwargs['query'] = '"Manifests | where imageSize > 500"'
-        manifests = self.cmd(
-            'acr query -n {registry_name} -q {query}').get_output_in_json()
+        manifests = self.cmd('acr query -n {} -q {}'.format('metadataunittest', '"Manifests | where imageSize > 500"')).get_output_in_json()
 
         for manifest in manifests["data"]:
             assert manifest["size"] > 500
