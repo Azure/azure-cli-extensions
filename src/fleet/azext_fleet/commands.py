d1975f3342092f4ddc76ce33842f65f54af31f90
Pankil Daru <daru.pankil05@gmail.com>
2023-04-26 04:12:47 +0000

    fix linter

--- src/fleet/azext_fleet/commands.py
+++ src/fleet/azext_fleet/commands.py
@@ -50,7 +50,7 @@ def load_command_table(self, _):
     # fleet update runs command group
     with self.command_group("fleet updaterun", update_runs_sdk, client_factory=cf_update_runs) as g:
         g.custom_command("create", "create_update_run", supports_no_wait=True)
-        g.custom_command("show", "show_update_run")
+        g.custom_show_command("show", "show_update_run")
         g.custom_command("list", "list_update_run")
         g.custom_command("delete", "delete_update_run", supports_no_wait=True)
         g.custom_command("start", "start_update_run", supports_no_wait=True)
