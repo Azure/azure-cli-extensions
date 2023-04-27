a431e226b7bf8d56d6b921bdcda1183bc48f9114
Priya Shet <priyashet@microsoft.com>
2023-04-27 18:51:24 +0000

    NetworkCloud tests added for L3network

---
+++ src/networkcloud/azext_networkcloud/aaz/operations/virtualmachine/console/_show.py
@@ -0,0 +1,34 @@
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# pylint: disable=unused-wildcard-import,wildcard-import
+# pylint: disable=protected-access, too-few-public-methods
+# flake8: noqa
+
+"""
+Provides VirtualMachine Console show customization
+"""
+from azext_networkcloud.aaz.latest.networkcloud.virtualmachine.console._show import \
+    Show
+from azure.cli.core.aaz import *
+
+from .common import VirtualMachineConsole
+
+
+class VirtualMachineConsoleShow(Show):
+    '''
+    This custom code inherits from generate virtual machine console functions.
+    It is integrated into the generated code via:
+    cli-ext/v20221212preview/ext/src/networkcloud/azext_networkcloud/commands.py
+    '''
+    @classmethod
+    def _build_arguments_schema(cls, *args, **kwargs):
+        args_schema = super()._build_arguments_schema(*args, **kwargs)
+        return VirtualMachineConsole._build_arguments_schema(args_schema)
+
+    @register_callback
+    def pre_operations(self):
+        return VirtualMachineConsole.pre_operations(self.ctx.args)
