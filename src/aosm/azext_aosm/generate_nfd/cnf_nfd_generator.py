1b77c7ef50b0796acf3fdb0a1692b65b64aaff93
Jacob Darby <jdarby@microsoft.com>
2023-05-04 15:11:18 +0100

    add psudo code for CNF generation

--- src/aosm/azext_aosm/generate_nfd/cnf_nfd_generator.py
+++ src/aosm/azext_aosm/generate_nfd/cnf_nfd_generator.py
@@ -3,8 +3,13 @@
 # License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------
 """Contains a class for generating VNF NFDs and associated resources."""
-from typing import Dict, Any
+import os
+from typing import Dict, List, Any, Tuple
 from azext_aosm.generate_nfd.nfd_generator_base import NFDGenerator
+from knack.log import get_logger
+from azext_aosm.vendored_sdks.models import AzureArcKubernetesHelmApplication
+
+logger = get_logger(__name__)
 
 
 class CnfNfdGenerator(NFDGenerator):
@@ -19,3 +24,72 @@ class CnfNfdGenerator(NFDGenerator):
         super(NFDGenerator, self).__init__(
             config=config,
         )
+
+    def generate_nfd(self):
+        pass
+
+    def write(self):
+        pass
+
+    def _create_nfd_folder(self):
+        pass
+
+    def generate_nf_applications(
+        self, helm_packages_config: List[Any]
+    ) -> List[Dict[str, Any]]:
+        # This will mostly call the functions below.
+        nf_applications = []
+
+        for helm_package in helm_packages_config:
+            nf_applications.append(self.generate_nf_application(helm_package))
+        return nf_applications
+
+    def generate_nf_application(
+        self, helm_package: Dict[Any, Any]
+    ) -> Dict[str, Any]:
+        (name, version) = self.get_chart_name_and_version(helm_package)
+        return {
+            "artifactType": "HelmPackage",
+            "name": helm_package["name"],
+            "dependsOnProfile": helm_package["dependsOnProfile"],
+            "artifactProfile": {
+                "artifactStore": {"id": "acrArtifactStore.id"},
+                "helmArtifactProfile": {
+                    "helmPackageName": name,
+                    "helmPackageVersionRange": version,
+                    "registryValuesPaths": [
+                        "'global.registry.docker.repoPath'"
+                    ],
+                    "imagePullSecretsValuesPaths": [
+                        "'global.registry.docker.imagePullSecrets'"
+                    ],
+                },
+            },
+            "deployParametersMappingRuleProfile": {
+                "applicationEnablement": "'Enabled'",
+                "helmMappingRuleProfile": {
+                    "releaseNamespace": "'PACKAGE_NAME'",
+                    "releaseName": "'PACKAGE_NAME'",
+                    "helmPackageVersion": "'PACKAGE_VERSION'",
+                    "values": self.generate_parmeter_mappings(),
+                },
+            },
+        }
+
+    def generate_deployment_parameters_schema(self):
+        # We need to take the parameters requested by the mapping file (values.nondef.yaml)
+        # and generate the deployment parameters schema based on values.schema.json.
+        # Basically take the bits of the schema that are relevant to the parameters requested.
+        pass
+
+    def get_chart_name_and_version(self, helm_package: Dict[Any, Any]) -> Tuple[str, str]:
+        # We need to get the chart name and version from the Chart.yaml file.
+        return ("chart_name", "chart_version")
+
+    def some_fun_to_check_ragistry_and_image_secret_path(self):
+        # Need to work out what we are doing here???
+        pass
+
+    def generate_parmeter_mappings(self) -> str:
+        # Basically copy the values.nondef.yaml file to the right place.
+        pass
