ca2b8d4841c189d06e0c7651dde87d60a12f46cc
anfranci <anfranci@microsoft.com>
2023-05-09 10:55:10 -0500

    add containerapp job to azcli

--- src/containerapp/azext_containerapp/tests/latest/test_containerappjob_crud.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerappjob_crud.py
@@ -17,7 +17,9 @@ class ContainerAppJobsCRUDOperationsTest(ScenarioTest):
     @AllowLargeResponse(8192)
     @ResourceGroupPreparer(location="northcentralus")
     @live_only()  # encounters 'CannotOverwriteExistingCassetteException' only when run from recording (passes when run live)
-    def test_containerapp_job_crudoperations_e2e(self, resource_group):
+    
+    # test for CRUD operations on Container App Job resource with trigger type as manual
+    def test_containerapp_manualjob_crudoperations_e2e(self, resource_group):
         import requests
 
         env = self.create_random_name(prefix='env', length=24)
@@ -30,7 +32,8 @@ class ContainerAppJobsCRUDOperationsTest(ScenarioTest):
             JMESPathCheck('name', env)            
         ])
 
-        # create a Container App Job resource
+        ## test for CRUD operations on Container App Job resource with trigger type as manual
+        # create a Container App Job resource with trigger type as manual
         self.cmd("az containerapp job create --resource-group {} --name {} --environment {} --replica-timeout 200 --replica-retry-limit 2 --trigger-type manual --image mcr.microsoft.com/k8se/quickstart:latest --cpu '0.25' --memory '0.5Gi'".format(resource_group, job, env))
 
         # verify the container app job resource
@@ -65,3 +68,37 @@ class ContainerAppJobsCRUDOperationsTest(ScenarioTest):
         # verify the Container App Job resource is deleted
         jobs_list = self.cmd("az containerapp job list --resource-group {}".format(resource_group)).get_output_in_json()
         self.assertTrue(len(jobs_list) == 0)
+        
+        ## test for CRUD operations on Container App Job resource with trigger type as schedule
+        job2 = self.create_random_name(prefix='job2', length=24)
+
+        # create a Container App Job resource with trigger type as schedule
+        self.cmd("az containerapp job create --resource-group {} --name {} --environment {} --replica-timeout 200 --replica-retry-limit 2 --trigger-type schedule --cron-expression '*/5 * * * *' --image mcr.microsoft.com/k8se/quickstart:latest --cpu '0.25' --memory '0.5Gi'".format(resource_group, job2, env))
+
+        # verify the container app job resource
+        self.cmd("az containerapp job show --resource-group {} --name {}".format(resource_group, job2), checks=[
+            JMESPathCheck('name', job2),
+            JMESPathCheck('properties.configuration.replicaTimeout', 200),
+            JMESPathCheck('properties.configuration.replicaRetryLimit', 2),
+            JMESPathCheck('properties.configuration.triggerType', "schedule", case_sensitive=False),
+            JMESPathCheck('properties.configuration.scheduleTriggerConfig.cronExpression', "*/5 * * * *"),
+        ])
+
+        # get list of Container App Jobs
+        jobs_list = self.cmd("az containerapp job list --resource-group {}".format(resource_group)).get_output_in_json()
+        self.assertTrue(len(jobs_list) == 1)
+
+        # update the Container App Job resource
+        self.cmd("az containerapp job update --resource-group {} --name {} --replica-timeout 300 --replica-retry-limit 1 --cron-expression '*/10 * * * *' --image mcr.microsoft.com/k8se/quickstart:latest --cpu '0.5' --memory '1.0Gi'".format(resource_group, job2))
+
+        # verify the updated Container App Job resource
+        self.cmd("az containerapp job show --resource-group {} --name {}".format(resource_group, job2), checks=[
+            JMESPathCheck('name', job2),
+            JMESPathCheck('properties.configuration.replicaTimeout', 300),
+            JMESPathCheck('properties.configuration.replicaRetryLimit', 1),
+            JMESPathCheck('properties.configuration.triggerType', "schedule", case_sensitive=False),
+            JMESPathCheck('properties.configuration.scheduleTriggerConfig.cronExpression', "*/10 * * * *"),
+            JMESPathCheck('properties.template.containers[0].image', "mcr.microsoft.com/k8se/quickstart:latest"),
+            JMESPathCheck('properties.template.containers[0].resources.cpu', "0.5"),
+            JMESPathCheck('properties.template.containers[0].resources.memory', "1Gi"),
+        ])
