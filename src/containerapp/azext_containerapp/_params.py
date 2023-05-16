04cd7e7c2509b7df8e57832b5cb00ce1bea25383
daniv-msft <daniv@microsoft.com>
2023-05-16 16:06:05 -0700

    Remove one space in the params.py file

--- src/containerapp/azext_containerapp/_params.py
+++ src/containerapp/azext_containerapp/_params.py
@@ -415,7 +415,7 @@ def load_arguments(self, _):
         c.argument('min_nodes', help="The minimum node count for the workload profile")
         c.argument('max_nodes', help="The maximum node count for the workload profile")
 
-     with self.argument_context('containerapp env workload-profile add') as c:
+    with self.argument_context('containerapp env workload-profile add') as c:
         c.argument('workload_profile_type', help="The type of workload profile to add to this environment. Run 'az containerapp env workload-profile list-supported -l <region>' to check the options for your region.")
         c.argument('min_nodes', help="The minimum node count for the workload profile")
         c.argument('max_nodes', help="The maximum node count for the workload profile")
