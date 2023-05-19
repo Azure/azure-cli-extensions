0eb0462f34e24e05c55b18a9106449eda4710422
Cormac McCarthy <corm@microsoft.com>
2023-05-18 18:02:39 -0700

    Add live tests for 'up' source-to-cloud scenarios

--- src/containerapp/azext_containerapp/tests/latest/test_containerapp_up.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerapp_up.py
@@ -38,3 +38,59 @@ class ContainerAppUpImageTest(ScenarioTest):
 
         self.cmd(f"containerapp up --image {image} --environment {env_name} -g {resource_group} -n {app_name} -l {TEST_LOCATION.upper()}")
 
+
+    @ResourceGroupPreparer(location="eastus2")
+    def test_containerapp_up_source_with_dockerfile_e2e(self, resource_group):
+        self.cmd('configure --defaults location={}'.format(TEST_LOCATION))
+
+        source_path = os.path.join(TEST_DIR, "source_using_dockerfile")
+        env_name = self.create_random_name(prefix='env', length=24)
+        self.cmd(f'containerapp env create -g {resource_group} -n {env_name}')
+        app_name = self.create_random_name(prefix='containerapp', length=24)
+        self.cmd(f"containerapp up --source {source_path} --environment {env_name} -g {resource_group} -n {app_name}")
+
+        app = self.cmd(f"containerapp show -g {resource_group} -n {app_name}").get_output_in_json()
+        url = app["properties"]["configuration"]["ingress"]["fqdn"]
+        url = url if url.startswith("http") else f"http://{url}"
+        resp = requests.get(url)
+        self.assertTrue(resp.ok)
+
+        self.cmd(f"containerapp up --source {source_path} --environment {env_name} -g {resource_group} -n {app_name} -l {TEST_LOCATION.upper()}")
+
+
+    @ResourceGroupPreparer(location="eastus2")
+    def test_containerapp_up_source_with_acr_task_e2e(self, resource_group):
+        self.cmd('configure --defaults location={}'.format(TEST_LOCATION))
+
+        source_path = os.path.join(TEST_DIR, "source_using_acr_task")
+        env_name = self.create_random_name(prefix='env', length=24)
+        self.cmd(f'containerapp env create -g {resource_group} -n {env_name}')
+        app_name = self.create_random_name(prefix='containerapp', length=24)
+        self.cmd(f"containerapp up --source {source_path} --environment {env_name} -g {resource_group} -n {app_name}")
+
+        app = self.cmd(f"containerapp show -g {resource_group} -n {app_name}").get_output_in_json()
+        url = app["properties"]["configuration"]["ingress"]["fqdn"]
+        url = url if url.startswith("http") else f"http://{url}"
+        resp = requests.get(url)
+        self.assertTrue(resp.ok)
+
+        self.cmd(f"containerapp up --source {source_path} --environment {env_name} -g {resource_group} -n {app_name} -l {TEST_LOCATION.upper()}")
+
+
+    @ResourceGroupPreparer(location="eastus2")
+    def test_containerapp_up_source_with_buildpack_e2e(self, resource_group):
+        self.cmd('configure --defaults location={}'.format(TEST_LOCATION))
+
+        source_path = os.path.join(TEST_DIR, "source_using_buildpack")
+        env_name = self.create_random_name(prefix='env', length=24)
+        self.cmd(f'containerapp env create -g {resource_group} -n {env_name}')
+        app_name = self.create_random_name(prefix='containerapp', length=24)
+        self.cmd(f"containerapp up --source {source_path} --environment {env_name} -g {resource_group} -n {app_name}")
+
+        app = self.cmd(f"containerapp show -g {resource_group} -n {app_name}").get_output_in_json()
+        url = app["properties"]["configuration"]["ingress"]["fqdn"]
+        url = url if url.startswith("http") else f"http://{url}"
+        resp = requests.get(url)
+        self.assertTrue(resp.ok)
+
+        self.cmd(f"containerapp up --source {source_path} --environment {env_name} -g {resource_group} -n {app_name} -l {TEST_LOCATION.upper()}")
