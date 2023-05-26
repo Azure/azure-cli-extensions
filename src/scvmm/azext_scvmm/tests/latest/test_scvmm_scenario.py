30168bf5740f101ff3014d003c68f6d984523901
Madhup Choubey <mchoubey@microsoft.com>
2023-05-26 18:33:44 +0530

    test cases

--- src/scvmm/azext_scvmm/tests/latest/test_scvmm_scenario.py
+++ src/scvmm/azext_scvmm/tests/latest/test_scvmm_scenario.py
@@ -15,41 +15,44 @@ class ScVmmScenarioTest(ScenarioTest):
     def test_scvmm(self):
         self.kwargs.update(
             {
+                'subscription': '204898ee-cd13-4332-b9d4-55ca5c25496d',
                 'resource_group': 'aadk8test',
                 'location': 'eastus2euap',
                 'custom_location': 'arcvmm-azcli-test-cl',
                 'vmmserver_name': 'arcvmm-azcli-test-vmmserver',
-                'icloud_name': 'cloud-qos',
-                'icloud_uuid': '3e1798f2-32a5-4121-8ed7-2527bf4e23d9',
-                'cloud_name': 'arcvmm-azcli-test-cloud-qos',
-                'ivmt_name': 'vmt-minion',
-                'ivmt_uuid': 'da3bed37-98f9-444f-badd-268a90924937',
-                'vmt_name': 'arcvmm-azcli-test-vmt-minion',
-                'ivnet_name': 'vnet-561',
-                'ivnet_uuid': '19cab3a9-e51d-48ef-9944-2ef6dc5d98a4',
-                'vnet_name': 'arcvmm-azcli-test-vnet-561',
+                'icloud_name': 'sojilani',
+                'icloud_uuid': 'ca912988-afcf-4c2e-b3c1-b1ddc8392f6b',
+                'cloud_name': 'arcvmm-azcli-test-sojilani',
+                'ivmt_name': 'vmt-win',
+                'ivmt_uuid': '97df5464-106e-4f82-8508-1ac3767adde6',
+                'vmt_name': 'arcvmm-azcli-test-vmt-win',
+                'ivnet_name': 'vnet-562',
+                'ivnet_uuid': 'a6f9432f-23d7-4124-9887-11c3ab173463',
+                'vnet_name': 'arcvmm-azcli-test-vnet-562',
                 'avset_string': 'avset1',
                 'avset_name': 'arcvmm-azcli-test-avset1',
                 'vm_name': 'arcvmm-azcli-test-vm-1',
                 'disk_name': 'disk_1',
                 'nic_name': 'nic_1',
+                'checkpoint_name': 'arcvmm-azcli-checkpoint',
+                'checkpoint_description': 'arcvmm-azcli-checkpoint',
             }
         )
 
         self.cmd(
-            'az scvmm vmmserver show -g {resource_group} --name {vmmserver_name}',
+            'az scvmm vmmserver show --subscription {subscription} -g {resource_group} --name {vmmserver_name}',
             checks=[
                 self.check('name', '{vmmserver_name}'),
             ],
         )
 
         self.cmd(
-            'az scvmm vmmserver list -g {resource_group}',
+            'az scvmm vmmserver list --subscription {subscription} -g {resource_group}',
             checks=[self.greater_than('length(@)', 0)],
         )
 
         self.cmd(
-            "az scvmm vmmserver inventory-item show -g {resource_group} -v {vmmserver_name}"
+            "az scvmm vmmserver inventory-item show --subscription {subscription} -g {resource_group} -v {vmmserver_name}"
             " -i {icloud_uuid}",
             checks=[
                 self.check('inventoryItemName', '{icloud_name}'),
@@ -59,7 +62,7 @@ class ScVmmScenarioTest(ScenarioTest):
         )
 
         self.cmd(
-            "az scvmm vmmserver inventory-item show -g {resource_group} -v {vmmserver_name}"
+            "az scvmm vmmserver inventory-item show --subscription {subscription} -g {resource_group} -v {vmmserver_name}"
             " -i {ivmt_uuid}",
             checks=[
                 self.check('inventoryItemName', '{ivmt_name}'),
@@ -68,7 +71,7 @@ class ScVmmScenarioTest(ScenarioTest):
             ],
         )
         self.cmd(
-            "az scvmm vmmserver inventory-item show -g {resource_group} -v {vmmserver_name}"
+            "az scvmm vmmserver inventory-item show --subscription {subscription} -g {resource_group} -v {vmmserver_name}"
             " -i {ivnet_uuid}",
             checks=[
                 self.check('inventoryItemName', '{ivnet_name}'),
@@ -78,7 +81,7 @@ class ScVmmScenarioTest(ScenarioTest):
         )
 
         self.cmd(
-            'az scvmm cloud create -g {resource_group} -l {location} --custom-location'
+            'az scvmm cloud create --subscription {subscription} -g {resource_group} -l {location} --custom-location'
             ' {custom_location} -v {vmmserver_name} -i {icloud_uuid} --name {cloud_name}',
             checks=[
                 self.check('provisioningState', 'Succeeded'),
@@ -86,7 +89,7 @@ class ScVmmScenarioTest(ScenarioTest):
         )
 
         self.cmd(
-            'az scvmm vm-template create -g {resource_group} -l {location} --custom-location'
+            'az scvmm vm-template create --subscription {subscription} -g {resource_group} -l {location} --custom-location'
             ' {custom_location} -v {vmmserver_name} -i {ivmt_uuid} --name {vmt_name}',
             checks=[
                 self.check('provisioningState', 'Succeeded'),
@@ -94,7 +97,7 @@ class ScVmmScenarioTest(ScenarioTest):
         )
 
         self.cmd(
-            'az scvmm virtual-network create -g {resource_group} -l {location} --custom-location'
+            'az scvmm virtual-network create --subscription {subscription} -g {resource_group} -l {location} --custom-location'
             ' {custom_location} -v {vmmserver_name} -i {ivnet_uuid} --name {vnet_name}',
             checks=[
                 self.check('provisioningState', 'Succeeded'),
@@ -102,7 +105,7 @@ class ScVmmScenarioTest(ScenarioTest):
         )
 
         self.cmd(
-            'az scvmm avset create -g {resource_group} -l {location} --custom-location'
+            'az scvmm avset create --subscription {subscription} -g {resource_group} -l {location} --custom-location'
             ' {custom_location} -v {vmmserver_name} -a {avset_string} --name {avset_name}',
             checks=[
                 self.check('provisioningState', 'Succeeded'),
@@ -110,7 +113,7 @@ class ScVmmScenarioTest(ScenarioTest):
         )
 
         self.cmd(
-            'az scvmm vm create -g {resource_group} -l {location} --custom-location'
+            'az scvmm vm create --subscription {subscription} -g {resource_group} -l {location} --custom-location'
             ' {custom_location} -c {cloud_name} -t {vmt_name} -a {avset_name} -n {vm_name} --disk'
             ' name={disk_name} disk-size=2 bus=0 --nic name={nic_name} network={vnet_name}',
             checks=[
@@ -121,7 +124,7 @@ class ScVmmScenarioTest(ScenarioTest):
         )
 
         self.cmd(
-            'az scvmm vm update -g {resource_group} -n {vm_name}'
+            'az scvmm vm update --subscription {subscription} -g {resource_group} -n {vm_name}'
             ' --cpu-count 2 --dynamic-memory-enabled true --tags client=azcli',
             checks=[
                 self.check('provisioningState', 'Succeeded'),
@@ -132,33 +135,48 @@ class ScVmmScenarioTest(ScenarioTest):
         )
 
         self.cmd(
-            'az scvmm vm disk show -g {resource_group} --vm-name {vm_name} -n {disk_name}',
+            'az scvmm vm disk show --subscription {subscription} -g {resource_group} --vm-name {vm_name} -n {disk_name}',
             checks=[
                 self.check('name', '{disk_name}'),
             ],
         )
 
         self.cmd(
-            'az scvmm vm nic show -g {resource_group} --vm-name {vm_name} -n {nic_name}',
+            'az scvmm vm nic show --subscription {subscription} -g {resource_group} --vm-name {vm_name} -n {nic_name}',
             checks=[
                 self.check('name', '{nic_name}'),
             ],
         )
 
-        self.cmd('az scvmm vm start -g {resource_group} --name {vm_name}')
+        self.cmd('az scvmm vm start --subscription {subscription} -g {resource_group} --name {vm_name}')
 
         self.cmd(
-            'az scvmm vm stop -g {resource_group} --name {vm_name} --skip-shutdown'
+            'az scvmm vm stop --subscription {subscription} -g {resource_group} --name {vm_name} --skip-shutdown'
         )
-
-        self.cmd('az scvmm vm delete -g {resource_group} --name {vm_name} -y')
-
-        self.cmd('az scvmm avset delete -g {resource_group} --name {avset_name} -y')
 
         self.cmd(
-            'az scvmm virtual-network delete -g {resource_group} --name {vnet_name} -y'
+            'az scvmm vm create-checkpoint --subscription {subscription} -g {resource_group} --name {vm_name} --checkpoint-name {checkpoint_name} --checkpoint-description {checkpoint_description}',
+        )
+        alias_sub = self.cmd('az scvmm vm show --subscription {subscription} -g {resource_group} --name {vm_name}').get_output_in_json()
+        checkpoint_id = alias_sub['checkpoints'][0]['checkpointId']
+        self.kwargs.update({'checkpoint_id': checkpoint_id})
+
+        self.cmd(
+            'az scvmm vm restore-checkpoint --subscription {subscription} -g {resource_group} --name {vm_name} --checkpoint-id {checkpoint_id}',
         )
 
-        self.cmd('az scvmm vm-template delete -g {resource_group} --name {vmt_name} -y')
+        self.cmd(
+            'az scvmm vm delete-checkpoint --subscription {subscription} -g {resource_group} --name {vm_name} --checkpoint-id {checkpoint_id}',
+        )
 
-        self.cmd('az scvmm cloud delete -g {resource_group} --name {cloud_name} -y')
+        self.cmd('az scvmm vm delete --subscription {subscription} -g {resource_group} --name {vm_name} --deleteFromHost -y')
+
+        self.cmd('az scvmm avset delete --subscription {subscription} -g {resource_group} --name {avset_name} -y')
+
+        self.cmd(
+            'az scvmm virtual-network delete --subscription {subscription} -g {resource_group} --name {vnet_name} -y'
+        )
+
+        self.cmd('az scvmm vm-template delete --subscription {subscription} -g {resource_group} --name {vmt_name} -y')
+
+        self.cmd('az scvmm cloud delete --subscription {subscription} -g {resource_group} --name {cloud_name} -y')
