8fa1a47da373607f92123e267bd9cb44d59d9e9c
joannajiang112628 <joannajiang@microsoft.com>
2023-05-26 16:23:05 -0700

    adjust CLI

--- src/communication/azext_communication/__init__.py
+++ src/communication/azext_communication/__init__.py
@@ -31,6 +31,17 @@ class CommunicationServiceManagementClientCommandsLoader(AzCommandsLoader):
 
     def load_command_table(self, args):
         from azext_communication.generated.commands import load_command_table
+        from azure.cli.core.aaz import load_aaz_command_table
+        try:
+            from . import aaz
+        except ImportError:
+            aaz = None
+        if aaz:
+            load_aaz_command_table(
+                loader=self,
+                aaz_pkg_name=aaz.__name__,
+                args=args
+            )
         load_command_table(self, args)
         try:
             from azext_communication.manual.commands import load_command_table as load_command_table_manual
