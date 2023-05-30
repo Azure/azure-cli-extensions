e4ac247ffc3d35f862522dd764fd149d9912e468
Vishesh Patel <133762830+vishesh155@users.noreply.github.com>
2023-05-30 17:45:05 +0530

    Added license header in recovery_point.py

--- src/dataprotection/azext_dataprotection/manual/aaz_operations/recovery_point.py
+++ src/dataprotection/azext_dataprotection/manual/aaz_operations/recovery_point.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from azure.cli.core.aaz import AAZStrArg
 from azext_dataprotection.aaz.latest.dataprotection.recovery_point import List as _RecoveryPointList
 from knack.util import CLIError
