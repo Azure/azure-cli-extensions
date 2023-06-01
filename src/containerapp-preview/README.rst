6a86c8df595c3aaf46461081225eab93b9cffca5
xinyu pang <1042945277@qq.com>
2023-06-01 14:43:29 +0800

    init containerapp-preview

---
+++ src/containerapp-preview/README.rst
@@ -0,0 +1,5 @@
+Microsoft Azure CLI 'containerapp-preview' Extension
+==========================================
+
+This package is for the 'containerapp-preview' extension.
+i.e. 'az containerapp-preview'
---
+++ src/containerapp-preview/azext_containerapp_preview/_params.py
@@ -0,0 +1,20 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+# pylint: disable=line-too-long
+
+from knack.arguments import CLIArgumentType
+
+
+def load_arguments(self, _):
+
+    from azure.cli.core.commands.parameters import tags_type
+    from azure.cli.core.commands.validators import get_default_location_from_resource_group
+
+    name_type = CLIArgumentType(options_list=['--name', '-n'])
+
+    with self.argument_context('containerapp') as c:
+        c.argument('tags', tags_type)
+        c.argument('location', validator=get_default_location_from_resource_group)
+        c.argument('name', name_type, options_list=['--name', '-n'])
