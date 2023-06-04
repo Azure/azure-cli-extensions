15956a1a67784aefa4bc1d5e0e9546d5490621b1
Vishesh Patel <133762830+vishesh155@users.noreply.github.com>
2023-06-04 15:40:45 +0530

    Minor aaz generation change job, recovery-point

--- src/dataprotection/azext_dataprotection/aaz/latest/dataprotection/job/_list.py
+++ src/dataprotection/azext_dataprotection/aaz/latest/dataprotection/job/_list.py
@@ -45,7 +45,6 @@ class List(AAZCommand):
 
         _args_schema = cls._args_schema
         _args_schema.resource_group = AAZResourceGroupNameArg(
-            help="Name of resource group. You can configure the default group using `az configure --defaults group=<name>`.",
             required=True,
         )
         _args_schema.vault_name = AAZStrArg(
