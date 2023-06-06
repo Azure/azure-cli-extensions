fb8037468d4a582eff77d1ef984aff7c6562cb8b
Himanshu Bisht <himanshubishtcse@gmail.com>
2023-06-07 01:13:50 +0530

    Added examples to testrun commands

--- src/load/azext_load/data_plane/load_test_run/help.py
+++ src/load/azext_load/data_plane/load_test_run/help.py
@@ -12,8 +12,14 @@ helps[
     "load test-run create"
 ] = """
 type: command
-short-summary: Create new load test run.
-long-summary: Create a new load test run.
+short-summary: Create a new load test run.
+examples:
+    - name: Create a test run for a test and wait for test run completion.
+      text: |
+        az load test-run create --load-test-resource {load_test_resource} --resource-group {resource_group} --test-id {test_id} --test-run-id {test_run_id} --wait
+    - name: Rerun an existing test run.
+      text: |
+        az load test-run create --load-test-resource {load_test_resource} --resource-group {resource_group} --test-id {test_id} --test-run-id {test_run_id} --description {description} --existing-test-run-id {existing_test_run_id}
 """
 
 helps[
@@ -21,15 +27,22 @@ helps[
 ] = """
 type: command
 short-summary: List all test runs.
-long-summary: List all test runs in the given load test resource.
+examples:
+    - name: List all tests runs in a test.
+      text: |
+        az load test-run list --load-test-resource {load_test_resource} --resource-group {resource_group} --test-id {test_id}
 """
 
 helps[
     "load test-run show"
 ] = """
 type: command
-short-summary: Show details of test run.
+short-summary: Show details of a test run.
 long-summary: Show details of the test run identified by given test-run-id.
+examples:
+    - name: Get the details of a test run.
+      text: |
+        az load test-run show --load-test-resource {load_test_resource} --resource-group {resource_group} --test-run-id {test_run_id}
 """
 
 helps[
@@ -37,86 +50,119 @@ helps[
 ] = """
 type: command
 short-summary: Update existing load test run.
-long-summary: Update an existing load test run.
+examples:
+    - name: Get the details of a test run.
+      text: |
+        az load test-run show --load-test-resource {load_test_resource} --resource-group {resource_group} --test-run-id {test_run_id}
 """
 
 helps[
     "load test-run stop"
 ] = """
 type: command
-short-summary: Stop running load test run.
-long-summary: Stop a running load test run.
+short-summary: Stop running a load test run.
+examples:
+    - name: Stop a test run.
+      text: |
+        az load test-run stop --load-test-resource {load_test_resource} --resource-group {resource_group} --test-run-id {test_run_id} --yes
 """
 
 helps[
     "load test-run delete"
 ] = """
 type: command
-short-summary: Delete existing load test run.
-long-summary: Delete an existing load test run.
+short-summary: Delete an existing load test run.
+examples:
+    - name: Delete a test run.
+      text: |
+        az load test-run delete --load-test-resource {load_test_resource} --resource-group {resource_group} --test-run-id {test_run_id} --yes
 """
 
 helps[
     "load test-run download-files"
 ] = """
 type: command
-short-summary: Download files of existing load test run.
-long-summary: Download files of an existing load test run.
+short-summary: Download files for an existing load test run.
+examples:
+    - name: Download input, log and result files for a test run. The directory should already exist.
+      text: |
+        az load test-run download-files --load-test-resource {load_test_resource} --resource-group {resource_group} --test-run-id {test_run_id} --path {path} --input --log --result
+    - name: Download input and log files for a test run by creating the directory if it does not exist. 
+      text: |
+        az load test-run download-files --load-test-resource {load_test_resource} --resource-group {resource_group} --test-run-id {test_run_id} --path {path} --input --log --force
 """
 
 helps[
     "load test-run app-component add"
 ] = """
 type: command
-short-summary: Add app component to test run.
-long-summary: Add app component to an existing load test run.
+short-summary: Add an app component to a test run.
+examples:
+    - name: Add an app component to a test run.
+      text: |
+        az load test-run app-component add --test-run-id {test_run_id} --load-test-resource {load_test_resource} --resource-group {resource_group} --app-component-name {app_component_name} --app-component-type {app_component_type} --app-component-id {app_component_id}
 """
 
 helps[
     "load test-run app-component list"
 ] = """
 type: command
-short-summary: List all app component of test run.
-long-summary: List all app component of a test run.
+short-summary: List all app component of a test run.
+examples:
+    - name: List all app components for a test run.
+      text: |
+        az load test-run app-component list --test-run-id {test_run_id} --load-test-resource {load_test_resource} --resource-group {resource_group} 
 """
 
 helps[
     "load test-run app-component remove"
 ] = """
 type: command
-short-summary: Remove an app component from test run.
-long-summary: Remove the given app component from the test run.
+short-summary: Remove an app component from a test run.
+examples:
+    - name: Remove an app component from a test run.
+      text: |
+        az load test-run app-component remove --test-run-id {test_run_id} --load-test-resource {load_test_resource} --resource-group {resource_group} --app-component-id {app_component_id} --yes
 """
 
 helps[
     "load test-run server-metric add"
 ] = """
 type: command
-short-summary: Add server-metrics to test run.
-long-summary: Add server-metrics to an existing load test run.
+short-summary: Add a server-metric to a test run.
+examples:
+    - name: Add a server metric for an app component to a test run.
+      text: |
+        az load test-run server-metric add --test-run-id {test_run_id} --load-test-resource {load_test_resource} --resource-group {resource_group} --metric-id {server_metric_id} --metric-name {server_metric_name} --metric-namespace {server_metric_namespace} --aggregation {aggregation} --app-component-type {app_component_type} --app-component-id {app_component_id}
 """
 
 helps[
     "load test-run server-metric list"
 ] = """
 type: command
-short-summary: List all server-metrics of test run.
-long-summary: List all server-metrics of a test run.
+short-summary: List all server-metrics of a test run.
+examples:
+    - name: List all server metrics for a test run.
+      text: |
+        az load test-run server-metric list --test-run-id {test_run_id} --load-test-resource {load_test_resource} --resource-group {resource_group}
 """
 
 helps[
     "load test-run server-metric remove"
 ] = """
 type: command
-short-summary: Remove an server-metrics from test run.
-long-summary: Remove the given server-metrics from the test run.
+short-summary: Remove a server-metric from a test run.
+examples:
+    - name: Remove a server metric from a test run.
+      text: |
+        az load test-run server-metric remove --test-run-id {test_run_id} --load-test-resource {load_test_resource} --resource-group {resource_group} --metric-id {server_metric_id} --yes
 """
 
 helps[
     "load test-run metrics get-namespaces"
 ] = """
 type: command
-short-summary: Get all metric namespaces for load test run.
+short-summary: Get all metric namespaces for a load test run.
 long-summary: Get all metric namespaces for a load test run.
 """
 
@@ -124,7 +170,7 @@ helps[
     "load test-run metrics list"
 ] = """
 type: command
-short-summary: List metrics for load test run.
+short-summary: List metrics for a load test run.
 long-summary: List metrics for a load test run.
 """
 
@@ -132,7 +178,7 @@ helps[
     "load test-run metrics get-definitions"
 ] = """
 type: command
-short-summary: Get all metric definitions for load test run.
+short-summary: Get all metric definitions for a load test run.
 long-summary: Get all metric definitions for a load test run.
 """
 
@@ -140,6 +186,6 @@ helps[
     "load test-run metrics get-dimensions"
 ] = """
 type: command
-short-summary: Get all metric dimensions for load test run.
+short-summary: Get all metric dimensions for a load test run.
 long-summary: Get all metric dimensions for a load test run.
 """
