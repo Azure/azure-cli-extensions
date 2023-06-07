d547cd75d90c8fca19b5fb28f8f04b9f50170846
xiaofanzhou <xiaofanzhou@microsoft.com>
2023-06-07 12:18:45 +0800

    license

--- src/serviceconnector-passwordless/azext_serviceconnector_passwordless/_utils.py
+++ src/serviceconnector-passwordless/azext_serviceconnector_passwordless/_utils.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from knack.prompting import prompt, prompt_y_n, NoTTYException
 from knack.log import get_logger
 from azure.cli.core import telemetry
