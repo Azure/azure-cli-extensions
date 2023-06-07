79e4c196bea2f84330e77c4b742cc91f153a1aac
xinyu pang <1042945277@qq.com>
2023-06-07 16:25:00 +0800

    fix

--- src/containerapp-preview/azext_containerapp_preview/_help.py
+++ src/containerapp-preview/azext_containerapp_preview/_help.py
@@ -3,3 +3,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+
+from knack.help_files import helps  # pylint: disable=unused-import
+
