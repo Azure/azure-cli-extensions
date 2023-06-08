a1a48d6f32399792406602ef70a3f1965c9a96c3
xinyu pang <1042945277@qq.com>
2023-06-08 15:09:00 +0800

    fix lint

--- src/containerapp-preview/azext_containerapp_preview/commands.py
+++ src/containerapp-preview/azext_containerapp_preview/commands.py
@@ -5,22 +5,5 @@
 
 # pylint: disable=line-too-long
 
-from azext_containerapp_preview._client_factory import ex_handler_factory
-
-
-def transform_containerapp_output(app):
-    props = ['name', 'location', 'resourceGroup', 'provisioningState']
-    result = {k: app[k] for k in app if k in props}
-
-    try:
-        result['fqdn'] = app['properties']['configuration']['ingress']['fqdn']
-    except:
-        result['fqdn'] = None
-
-    return result
-
-
 def load_command_table(self, _):
-
-    with self.command_group('containerapp', is_preview=True) as g:
-        g.custom_command('create', 'create_containerapp', supports_no_wait=True, exception_handler=ex_handler_factory(), table_transformer=transform_containerapp_output)
+    return
