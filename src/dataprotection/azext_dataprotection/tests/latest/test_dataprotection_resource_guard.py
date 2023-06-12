071bd045970ca42702740561ceb9d1e731b513cd
Vishesh Patel <133762830+vishesh155@users.noreply.github.com>
2023-06-12 15:09:08 +0530

    Completed backup-vault tests

--- src/dataprotection/azext_dataprotection/tests/latest/test_dataprotection_resource_guard.py
+++ src/dataprotection/azext_dataprotection/tests/latest/test_dataprotection_resource_guard.py
@@ -9,13 +9,12 @@
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
 from azure.cli.testsdk.scenario_tests import AllowLargeResponse
 
-class TestResourceGuardScenarios(ScenarioTest):
+class ResourceGuardScenarioTest(ScenarioTest):
 
     @AllowLargeResponse()
     @ResourceGroupPreparer(name_prefix='clitestdataprotection_resourceguard_')
-    def test_dataprotection_resource_guard(test, resource_group):
+    def test_dataprotection_resource_guard_create(test):
         test.kwargs.update({
-            'rg':resource_group,
             'resourceGuardName':'cli-test-resource-guard',
         })
         test.cmd('az dataprotection resource-guard create -g "{rg}" -n "{resourceGuardName}"', checks=[
@@ -30,10 +29,9 @@ class TestResourceGuardScenarios(ScenarioTest):
         test.cmd('az dataprotection resource-guard delete -g "{rg}" -n "{resourceGuardName}" -y')
 
     @AllowLargeResponse()
-    @ResourceGroupPreparer(name_prefix='clitestdataprotection_resourceguard_update_')
-    def test_dataprotection_resource_guard_update(test, resource_group):
+    @ResourceGroupPreparer(name_prefix='clitestdataprotection_resourceguard_')
+    def test_dataprotection_resource_guard_update(test):
         test.kwargs.update({
-            'rg':resource_group,
             'resourceGuardName':'cli-test-resource-guard',
             'resourceType': 'Microsoft.RecoveryServices/vaults'
         })
@@ -41,26 +39,21 @@ class TestResourceGuardScenarios(ScenarioTest):
             test.check('name', "{resourceGuardName}")
         ])
         test.cmd('az dataprotection resource-guard update -g "{rg}" -n "{resourceGuardName}" --resource-type "{resourceType}" --critical-operation-exclusion-list deleteProtection getSecurityPIN', checks=[
-            test.check('name', "{resourceGuardName}"),
             test.check('length(properties.vaultCriticalOperationExclusionList)', 2)
         ])
         test.cmd('az dataprotection resource-guard list-protected-operations -g "{rg}" -n "{resourceGuardName}" --resource-type "Microsoft.RecoveryServices/vaults"', checks=[
             test.check('length(@)', 4)
         ])
         test.cmd('az dataprotection resource-guard update -g "{rg}" -n "{resourceGuardName}" --critical-operation-exclusion-list deleteProtection', checks=[
-            test.check('name', "{resourceGuardName}"),
             test.check('length(properties.vaultCriticalOperationExclusionList)', 2)
         ])
         test.cmd('az dataprotection resource-guard update -g "{rg}" -n "{resourceGuardName}" --critical-operation-exclusion-list []', checks=[
-            test.check('name', "{resourceGuardName}"),
             test.check('length(properties.vaultCriticalOperationExclusionList)', 2)
         ])
         test.cmd('az dataprotection resource-guard update -g "{rg}" -n "{resourceGuardName}"  --resource-type "{resourceType}" --tags Purpose="Testing"', checks=[
-            test.check('name', "{resourceGuardName}"),
             test.check('tags.Purpose', "Testing"),
             test.check('length(properties.vaultCriticalOperationExclusionList)', 2)
         ])
         test.cmd('az dataprotection resource-guard update -g "{rg}" -n "{resourceGuardName}" --resource-type "{resourceType}" --critical-operation-exclusion-list []', checks=[
-            test.check('name', "{resourceGuardName}"),
             test.check('length(properties.vaultCriticalOperationExclusionList)', 0)
         ])
