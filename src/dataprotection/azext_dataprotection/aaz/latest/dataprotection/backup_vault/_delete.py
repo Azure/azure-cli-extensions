9ecde6ad460202286bcf15e27f95b4473b7751f9
Zubair Abid <zubairabid1999+github@gmail.com>
2023-06-12 12:08:56 +0530

    [dataprotection] Add support for cross-subscription restore for Backup vaults (#6386)
    * Update backup-vault to 2023-01-01, Enable csr flag, Comments for initialize restores
    
    * Removed incorrect validations for alternate/original location restores
    
    * code changes for CSR - Adding target-resource-id (for restore as files)
    
    * Set up new version details, to be fully tested before merge.
    
    * Testing complete.
    
    * Updated test runs
    
    * Fix documentation issue
    
    * Remove live-test only lines from AKS test
    
    * Updated AAZ version, new codegen
    
    ---------
    
    Co-authored-by: Zubair Abid <zubairabid@microsoft.com>

--- src/dataprotection/azext_dataprotection/aaz/latest/dataprotection/backup_vault/_delete.py
+++ src/dataprotection/azext_dataprotection/aaz/latest/dataprotection/backup_vault/_delete.py
@@ -24,16 +24,17 @@ class Delete(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2022-12-01",
+        "version": "2023-01-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.dataprotection/backupvaults/{}", "2022-12-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.dataprotection/backupvaults/{}", "2023-01-01"],
         ]
     }
 
+    AZ_SUPPORT_NO_WAIT = True
+
     def _handler(self, command_args):
         super()._handler(command_args)
-        self._execute_operations()
-        return None
+        return self.build_lro_poller(self._execute_operations, None)
 
     _args_schema = None
 
@@ -59,7 +60,7 @@ class Delete(AAZCommand):
 
     def _execute_operations(self):
         self.pre_operations()
-        self.BackupVaultsDelete(ctx=self.ctx)()
+        yield self.BackupVaultsDelete(ctx=self.ctx)()
         self.post_operations()
 
     @register_callback
@@ -76,12 +77,33 @@ class Delete(AAZCommand):
         def __call__(self, *args, **kwargs):
             request = self.make_request()
             session = self.client.send_request(request=request, stream=False, **kwargs)
-            if session.http_response.status_code in [200]:
-                return self.on_200(session)
             if session.http_response.status_code in [202]:
-                return self.on_202(session)
+                return self.client.build_lro_polling(
+                    self.ctx.args.no_wait,
+                    session,
+                    self.on_200,
+                    self.on_error,
+                    lro_options={"final-state-via": "azure-async-operation"},
+                    path_format_arguments=self.url_parameters,
+                )
+            if session.http_response.status_code in [200]:
+                return self.client.build_lro_polling(
+                    self.ctx.args.no_wait,
+                    session,
+                    self.on_200,
+                    self.on_error,
+                    lro_options={"final-state-via": "azure-async-operation"},
+                    path_format_arguments=self.url_parameters,
+                )
             if session.http_response.status_code in [204]:
-                return self.on_204(session)
+                return self.client.build_lro_polling(
+                    self.ctx.args.no_wait,
+                    session,
+                    self.on_204,
+                    self.on_error,
+                    lro_options={"final-state-via": "azure-async-operation"},
+                    path_format_arguments=self.url_parameters,
+                )
 
             return self.on_error(session.http_response)
 
@@ -122,16 +144,13 @@ class Delete(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2022-12-01",
+                    "api-version", "2023-01-01",
                     required=True,
                 ),
             }
             return parameters
 
         def on_200(self, session):
-            pass
-
-        def on_202(self, session):
             pass
 
         def on_204(self, session):
