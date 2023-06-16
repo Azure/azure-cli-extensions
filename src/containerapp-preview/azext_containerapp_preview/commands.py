94eebe462ca6151712b1f85057e61a2b2ee04763
xinyu pang <1042945277@qq.com>
2023-06-16 12:32:58 +0800

    add show/delete/list containerapp in preview

--- src/containerapp-preview/azext_containerapp_preview/commands.py
+++ src/containerapp-preview/azext_containerapp_preview/commands.py
@@ -3,22 +3,20 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 from azure.cli.command_modules.appservice.commands import ex_handler_factory
-
+from ._utils import (_get_or_add_extension, _get_azext_module, GA_CONTAINERAPP_EXTENSION_NAME)
 
 # pylint: disable=line-too-long
 
-def transform_containerapp_output(app):
-    props = ['name', 'location', 'resourceGroup', 'provisioningState']
-    result = {k: app[k] for k in app if k in props}
-
-    try:
-        result['fqdn'] = app['properties']['configuration']['ingress']['fqdn']
-    except:
-        result['fqdn'] = None
-
-    return result
-
 
 def load_command_table(self, _):
+    if not _get_or_add_extension(self, GA_CONTAINERAPP_EXTENSION_NAME):
+        return
+    azext_commands = _get_azext_module(
+        GA_CONTAINERAPP_EXTENSION_NAME, "azext_containerapp.commands")
+
     with self.command_group('containerapp', is_preview=True) as g:
-        g.custom_command('create', 'create_containerapp', supports_no_wait=True, exception_handler=ex_handler_factory(), table_transformer=transform_containerapp_output)
+        g.custom_show_command('show', 'show_containerapp', table_transformer=azext_commands.transform_containerapp_output)
+        g.custom_command('list', 'list_containerapp', table_transformer=azext_commands.transform_containerapp_list_output)
+        g.custom_command('create', 'create_containerapp', supports_no_wait=True, exception_handler=ex_handler_factory(), table_transformer=azext_commands.transform_containerapp_output)
+        # g.custom_command('update', 'update_containerapp', supports_no_wait=True, exception_handler=ex_handler_factory(), table_transformer=azext_commands.transform_containerapp_output)
+        g.custom_command('create', 'create_containerapp', supports_no_wait=True, exception_handler=ex_handler_factory(), table_transformer=azext_commands.transform_containerapp_output)
--- src/containerapp-preview/azext_containerapp_preview/containerapp_decorator.py
+++ src/containerapp-preview/azext_containerapp_preview/containerapp_decorator.py
@@ -5,7 +5,7 @@ from azure.cli.core.commands import AzCliCommand
 from azure.cli.core.azclierror import ValidationError, RequiredArgumentMissingError
 from msrestazure.tools import parse_resource_id
 
-from ._clients import ContainerAppClient, ManagedEnvironmentClient, ConnectedEnvironmentClient
+from ._clients import ManagedEnvironmentClient, ConnectedEnvironmentClient
 from ._utils import (_get_azext_module, GA_CONTAINERAPP_EXTENSION_NAME)
 
 logger = get_logger(__name__)
@@ -16,17 +16,8 @@ class ContainerAppPreviewCreateDecorator(_get_azext_module(GA_CONTAINERAPP_EXTEN
         self, cmd: AzCliCommand, client: Any, raw_parameters: Dict, models: str
     ):
         super().__init__(cmd, client, raw_parameters, models)
-        azext_default_utils = _get_azext_module(
-            GA_CONTAINERAPP_EXTENSION_NAME, "azext_containerapp._utils")
 
-        self.azext_default_utils = azext_default_utils
-        self.environment_client = self.get_environment_client()
-
-    def register_provider(self):
-        super().register_provider()
-
-    def validate_arguments(self):
-        super().validate_arguments()
+        self.azext_default_utils = _get_azext_module(GA_CONTAINERAPP_EXTENSION_NAME, "azext_containerapp._utils")
 
     def construct_containerapp(self):
         containerapp_def = super().construct_containerapp()
@@ -43,17 +34,17 @@ class ContainerAppPreviewCreateDecorator(_get_azext_module(GA_CONTAINERAPP_EXTEN
         return super().post_process_containerapp(containerapp_def, r)
 
     def set_up_extended_location(self, containerapp_def):
-        parsed_env = parse_resource_id(self.get_env())  # custom_location check here perhaps
+        parsed_env = parse_resource_id(self.get_argument_managed_env())  # custom_location check here perhaps
         env_name = parsed_env['name']
         env_rg = parsed_env['resource_group']
         env_info = self.get_environment_client().show(cmd=self.cmd, resource_group_name=env_rg, name=env_name)
-        if self.get_environment_type() == "connected":
+        if self.get_argument_environment_type() == "connected":
             if not containerapp_def.get('extendedLocation'):
                 containerapp_def["extendedLocation"] = env_info["extendedLocation"]
         return containerapp_def
 
     def get_environment_client(self):
-        if self.get_yaml():
+        if self.get_argument_yaml():
             yaml_containerapp = self.get_yaml_containerapp()
             if type(yaml_containerapp) != dict:  # pylint: disable=unidiomatic-typecheck
                 raise ValidationError(
@@ -63,9 +54,9 @@ class ContainerAppPreviewCreateDecorator(_get_azext_module(GA_CONTAINERAPP_EXTEN
                 raise RequiredArgumentMissingError(
                     'environmentId is required. This can be retrieved using the `az containerapp env show -g MyResourceGroup -n MyContainerappEnvironment --query id` command. Please see https://aka.ms/azure-container-apps-yaml for a valid containerapps YAML spec.')
         else:
-            env = self.get_env()
+            env = self.get_argument_managed_env()
 
-        environment_type = self.get_environment_type()
+        environment_type = self.get_argument_environment_type()
         if not env and not environment_type:
             return ManagedEnvironmentClient
 
@@ -82,29 +73,20 @@ class ContainerAppPreviewCreateDecorator(_get_azext_module(GA_CONTAINERAPP_EXTEN
                 logger.warning(
                     "User passed a managedEnvironment resource id but specified --environment-type connected. Using environment type managed.")
 
-        self.set_environment_type(environment_type)
-        self.set_env(env)
+        self.set_argument_environment_type(environment_type)
+        self.set_argument_managed_env(env)
 
-        if environment_type == "managed":
-            return ManagedEnvironmentClient
-        else:
+        if environment_type == "connected":
             return ConnectedEnvironmentClient
+        else:
+            return ManagedEnvironmentClient
 
     def get_yaml_containerapp(self):
         load_file = self.azext_default_utils.load_yaml_file(self.get_yaml())
         return self.azext_default_utils.process_loaded_yaml(load_file)
 
-    def get_environment_type(self):
+    def get_argument_environment_type(self):
         return self.get_param("environment_type")
 
-    def set_environment_type(self, environment_type):
+    def set_argument_environment_type(self, environment_type):
         self.set_param("environment_type", environment_type)
-
-    def get_env(self):
-        return self.get_param("env")
-
-    def set_env(self, env):
-        self.set_param("env", env)
-
-    def get_yaml(self):
-        return self.get_param("yaml")
