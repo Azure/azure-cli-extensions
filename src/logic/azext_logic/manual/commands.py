a28af0ce51bdd7dd613139f2274453f64da612a3
Jing-song <37168047+Jing-song@users.noreply.github.com>
2023-06-26 12:14:27 +0800

    Update commands.py

--- src/logic/azext_logic/manual/commands.py
+++ src/logic/azext_logic/manual/commands.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+
 def load_command_table(self, _):  # pylint: disable=unused-argument
     with self.command_group('logic integration-account map'):
         from azext_logic.manual.custom import MapCreate, MapUpdate
