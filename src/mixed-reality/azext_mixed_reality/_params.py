5489b745bee039be89e0463cf3080f5e4bcbd92f
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2023-06-27 13:39:05 +0800

    fix for `az spatial-anchors-account create` make --sku equal to --kind if exist. `az spatial-anchors-account key renew` bring back `--key` instead of --serial

--- src/mixed-reality/azext_mixed_reality/_params.py
+++ src/mixed-reality/azext_mixed_reality/_params.py
@@ -22,26 +22,27 @@ account_key_type = CLIArgumentType(
 
 
 def load_arguments(self, _):
-    with self.argument_context('spatial-anchors-account') as c:
-        c.argument('resource_group_name', arg_type=resource_group_name_type)
-        c.argument('account_name', type=str, help='Name of an mixed reality account.', arg_type=name_type, id_part='name')  # pylint: disable=line-too-long
-        c.argument('location', arg_type=get_location_type(self.cli_ctx), validator=get_default_location_from_resource_group)  # pylint: disable=line-too-long
-        c.argument('tags', arg_type=tags_type)
-        c.argument('sku', action=AddSku, nargs='+', help='The SKU associated with this account')
-        c.argument('kind', action=AddSku, nargs='+', help='The kind of account, if supported')
-        c.argument('storage_account_name', type=str, help='The name of the storage account associated with this accountId')  # pylint: disable=line-too-long
-
-    with self.argument_context('spatial-anchors-account key renew') as c:
-        c.argument('key', arg_type=account_key_type)
-
-    with self.argument_context('remote-rendering-account') as c:
-        c.argument('resource_group_name', resource_group_name_type)
-        c.argument('account_name', type=str, help='Name of an mixed reality account.', arg_type=name_type, id_part='name')  # pylint: disable=line-too-long
-        c.argument('tags', arg_type=tags_type)
-        c.argument('location', arg_type=get_location_type(self.cli_ctx), validator=get_default_location_from_resource_group)  # pylint: disable=line-too-long
-        c.argument('sku', action=AddSku, nargs='+', help='The SKU associated with this account')
-        c.argument('kind', action=AddSku, nargs='+', help='The kind of account, if supported')
-        c.argument('storage_account_name', type=str, help='The name of the storage account associated with this accountId')  # pylint: disable=line-too-long
-
-    with self.argument_context('remote-rendering-account key renew') as c:
-        c.argument('key', arg_type=account_key_type)
+    pass
+    # with self.argument_context('spatial-anchors-account') as c:
+    #     c.argument('resource_group_name', arg_type=resource_group_name_type)
+    #     c.argument('account_name', type=str, help='Name of an mixed reality account.', arg_type=name_type, id_part='name')  # pylint: disable=line-too-long
+    #     c.argument('location', arg_type=get_location_type(self.cli_ctx), validator=get_default_location_from_resource_group)  # pylint: disable=line-too-long
+    #     c.argument('tags', arg_type=tags_type)
+    #     c.argument('sku', action=AddSku, nargs='+', help='The SKU associated with this account')
+    #     c.argument('kind', action=AddSku, nargs='+', help='The kind of account, if supported')
+    #     c.argument('storage_account_name', type=str, help='The name of the storage account associated with this accountId')  # pylint: disable=line-too-long
+    #
+    # with self.argument_context('spatial-anchors-account key renew') as c:
+    #     c.argument('key', arg_type=account_key_type)
+    #
+    # with self.argument_context('remote-rendering-account') as c:
+    #     c.argument('resource_group_name', resource_group_name_type)
+    #     c.argument('account_name', type=str, help='Name of an mixed reality account.', arg_type=name_type, id_part='name')  # pylint: disable=line-too-long
+    #     c.argument('tags', arg_type=tags_type)
+    #     c.argument('location', arg_type=get_location_type(self.cli_ctx), validator=get_default_location_from_resource_group)  # pylint: disable=line-too-long
+    #     c.argument('sku', action=AddSku, nargs='+', help='The SKU associated with this account')
+    #     c.argument('kind', action=AddSku, nargs='+', help='The kind of account, if supported')
+    #     c.argument('storage_account_name', type=str, help='The name of the storage account associated with this accountId')  # pylint: disable=line-too-long
+    #
+    # with self.argument_context('remote-rendering-account key renew') as c:
+    #     c.argument('key', arg_type=account_key_type)
