c9a161a84c5341c675c798e67dbacc488365eb00
xinyu pang <1042945277@qq.com>
2023-07-07 15:08:05 +0800

    add test for managed, add helps, parse resource type and compare

--- src/containerapp-preview/azext_containerapp_preview/_validators.py
+++ src/containerapp-preview/azext_containerapp_preview/_validators.py
@@ -2,12 +2,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-from ._constants import MANAGED_ENVIRONMENT_TYPE, CONNECTED_ENVIRONMENT_TYPE
+from ._constants import (MANAGED_ENVIRONMENT_TYPE,
+                         CONNECTED_ENVIRONMENT_TYPE,
+                         MANAGED_ENVIRONMENT_RESOURCE_TYPE,
+                         CONNECTED_ENVIRONMENT_RESOURCE_TYPE,
+                         CONTAINERAPP_NAMESPACE)
 
 
 def validate_env_name_or_id(cmd, namespace):
     from azure.cli.core.commands.client_factory import get_subscription_id
-    from msrestazure.tools import is_valid_resource_id, resource_id
+    from msrestazure.tools import is_valid_resource_id, resource_id, parse_resource_id
 
     if not namespace.managed_env or not namespace.resource_group_name:
         return
@@ -19,10 +23,14 @@ def validate_env_name_or_id(cmd, namespace):
         environment_type = namespace.environment_type
 
     if namespace.managed_env:
-        if "connectedEnvironments" in namespace.managed_env:
-            environment_type = CONNECTED_ENVIRONMENT_TYPE
-        if "managedEnvironments" in namespace.managed_env:
-            environment_type = MANAGED_ENVIRONMENT_TYPE
+        if is_valid_resource_id(namespace.managed_env):
+            env_dict = parse_resource_id(namespace.managed_env)
+            resource_type = env_dict.get("resource_type")
+            if resource_type:
+                if CONNECTED_ENVIRONMENT_RESOURCE_TYPE.lower() == resource_type.lower():
+                    environment_type = CONNECTED_ENVIRONMENT_TYPE
+                if MANAGED_ENVIRONMENT_RESOURCE_TYPE.lower() == resource_type.lower():
+                    environment_type = MANAGED_ENVIRONMENT_TYPE
     if namespace.__dict__.get("custom_location") or namespace.__dict__.get("connected_cluster_id"):
         environment_type = CONNECTED_ENVIRONMENT_TYPE
     # Validate resource id / format resource id
@@ -31,8 +39,8 @@ def validate_env_name_or_id(cmd, namespace):
             namespace.managed_env = resource_id(
                 subscription=get_subscription_id(cmd.cli_ctx),
                 resource_group=namespace.resource_group_name,
-                namespace='Microsoft.App',
-                type='connectedEnvironments',
+                namespace=CONTAINERAPP_NAMESPACE,
+                type=CONNECTED_ENVIRONMENT_RESOURCE_TYPE,
                 name=namespace.managed_env
             )
     elif environment_type == MANAGED_ENVIRONMENT_TYPE:
@@ -40,7 +48,7 @@ def validate_env_name_or_id(cmd, namespace):
             namespace.managed_env = resource_id(
                 subscription=get_subscription_id(cmd.cli_ctx),
                 resource_group=namespace.resource_group_name,
-                namespace='Microsoft.App',
-                type='managedEnvironments',
+                namespace=CONTAINERAPP_NAMESPACE,
+                type=MANAGED_ENVIRONMENT_RESOURCE_TYPE,
                 name=namespace.managed_env
             )
