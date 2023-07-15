946fa0741b4dcc76303ebdb6eb6d5451177de7bc
anfranci <anfranci@microsoft.com>
2023-07-15 17:29:36 -0500

    update start payload format for job execution start to remove template property

--- src/containerapp/azext_containerapp/_clients.py
+++ src/containerapp/azext_containerapp/_clients.py
@@ -15,7 +15,7 @@ from knack.log import get_logger
 
 logger = get_logger(__name__)
 
-CURRENT_API_VERSION = "2023-05-02-preview"
+CURRENT_API_VERSION = "2023-04-01-preview"
 POLLING_TIMEOUT = 600  # how many seconds before exiting
 POLLING_SECONDS = 2  # how many seconds between requests
 POLLING_TIMEOUT_FOR_MANAGED_CERTIFICATE = 1500  # how many seconds before exiting
