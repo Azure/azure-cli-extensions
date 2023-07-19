8b267ce0bc16e86b09e23dddc64165fa3cc5bd85
Fuming Zhang <fumingzhang@microsoft.com>
2023-07-19 12:51:45 +0800

    update

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -6089,12 +6089,11 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             # self.check('ingressProfile.webAppRouting.enabled', False)
         ])
 
-    @live_only()
     @AllowLargeResponse()
     @AKSCustomResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='westus2')
     def test_aks_create_and_update_web_application_routing_dns_zone(self, resource_group, resource_group_location):
         aks_name = self.create_random_name('cliakstest', 16)
-        dns_zone_name = self.create_random_name('cliakstest', 16)
+        dns_zone_name = self.create_random_name('cliakstest', 16) + ".xyz"
         self.kwargs.update({
             'resource_group': resource_group,
             'name': aks_name,
