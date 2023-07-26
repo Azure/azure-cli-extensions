557f05e574f0dd428f5463142b873cbfeab18687
snehapar9 <snehapar@microsoft.com>
2023-07-26 13:07:44 -0700

    Add live_only to tests

--- src/containerapp/azext_containerapp/tests/latest/test_containerapp_create.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerapp_create.py
@@ -13,6 +13,7 @@ from azext_containerapp.tests.latest.utils import create_and_verify_containerapp
 TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
 class ContainerAppCreateTest(ScenarioTest):
+    @live_only()
     @ResourceGroupPreparer(location="eastus")
     def test_containerapp_create_source_with_Dockerfile_e2e(self, resource_group):
         source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_dockerfile"))
@@ -28,12 +29,14 @@ class ContainerAppCreateTest(ScenarioTest):
         target_port = '8080'
         create_and_verify_containerapp_create(self, resource_group=resource_group, source_path=source_path, ingress=ingress, target_port=target_port)
 
+    @live_only()
     @ResourceGroupPreparer(location="eastus")
     def test_containerapp_create_source_and_image_e2e(self, resource_group):
        image = "mcr.microsoft.com/dotnet/runtime:7.0"
        source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_dockerfile"))
        create_and_verify_containerapp_create(self,resource_group=resource_group, image=image, source_path=source_path)
 
+    @live_only()
     @ResourceGroupPreparer(location="eastus")
     def test_containerapp_create_source_with_acr_task_e2e(self, resource_group):
         source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_acr_task"))
