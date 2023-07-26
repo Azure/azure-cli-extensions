cd8599d9840330c188d7a6b14af04bcdbe646714
Patryk Kulik <patrykkulik@microsoft.com>
2023-07-26 14:23:20 +0100

    Cleanups

--- src/aosm/azext_aosm/tests/latest/test_vnf_publish_and_delete.py
+++ src/aosm/azext_aosm/tests/latest/test_vnf_publish_and_delete.py
@@ -13,17 +13,24 @@ NSD_INPUT_TEMPLATE_NAME = "vnf_nsd_input_template.json"
 NSD_INPUT_FILE_NAME = "nsd_input.json"
 ARM_TEMPLATE_RELATIVE_PATH = "scenario_test_mocks/vnf_mocks/ubuntu_template.json"
 
-def update_resource_group_in_input_file(input_template_name: str, output_file_name: str, resource_group: str) -> str:
+
+def update_resource_group_in_input_file(
+    input_template_name: str, output_file_name: str, resource_group: str
+) -> str:
     code_dir = os.path.dirname(__file__)
-    templates_dir = os.path.join(code_dir, "scenario_test_mocks", "mock_input_templates")
+    templates_dir = os.path.join(
+        code_dir, "scenario_test_mocks", "mock_input_templates"
+    )
     input_template_path = os.path.join(templates_dir, input_template_name)
 
     with open(input_template_path, "r", encoding="utf-8") as file:
         contents = file.read()
-    
+
     jinja_template = Template(contents)
 
-    rendered_template = jinja_template.render(publisher_resource_group_name=resource_group)
+    rendered_template = jinja_template.render(
+        publisher_resource_group_name=resource_group
+    )
 
     output_path = os.path.join(templates_dir, output_file_name)
 
@@ -36,22 +43,30 @@ def update_resource_group_in_input_file(input_template_name: str, output_file_na
 class VnfNsdTest(ScenarioTest):
     @ResourceGroupPreparer()
     def test_vnf_nsd_publish_and_delete(self, resource_group):
-        # TODO: should be using a temporary resource group
+        # We are overriding a resource group name here because we need to have some 
+        # resources predeployed in order to get around the timeout bug in the testing framework.
         resource_group = "patrykkulik-test"
 
-        nfd_input_file_path = update_resource_group_in_input_file(NFD_INPUT_TEMPLATE_NAME, NFD_INPUT_FILE_NAME, resource_group)
+        nfd_input_file_path = update_resource_group_in_input_file(
+            NFD_INPUT_TEMPLATE_NAME, NFD_INPUT_FILE_NAME, resource_group
+        )
 
-        self.cmd(f'az aosm nfd build -f "{nfd_input_file_path}" --definition-type vnf --force')
-                
-        self.cmd(f'az aosm nfd publish -f "{nfd_input_file_path}" --definition-type vnf --debug')
+        self.cmd(
+            f'az aosm nfd build -f "{nfd_input_file_path}" --definition-type vnf --force'
+        )
 
-        # TODO: should I run gets on things to make sure they exist?
-    
-        nsd_input_file_path = update_resource_group_in_input_file(NSD_INPUT_TEMPLATE_NAME, NSD_INPUT_FILE_NAME, resource_group)
+        self.cmd(
+            f'az aosm nfd publish -f "{nfd_input_file_path}" --definition-type vnf --debug'
+        )
+
+        nsd_input_file_path = update_resource_group_in_input_file(
+            NSD_INPUT_TEMPLATE_NAME, NSD_INPUT_FILE_NAME, resource_group
+        )
 
         self.cmd(f'az aosm nsd build -f "{nsd_input_file_path}" --debug --force')
         self.cmd(f'az aosm nsd publish -f "{nsd_input_file_path}" --debug')
 
-        self.cmd(f'az aosm nfd delete --definition-type vnf -f "{nfd_input_file_path}" --debug --force')
+        self.cmd(
+            f'az aosm nfd delete --definition-type vnf -f "{nfd_input_file_path}" --debug --force'
+        )
         self.cmd(f'az aosm nsd delete -f "{nsd_input_file_path}" --debug --force')
-
