2d6d95bf4769e37e304c9f168fa28a7a5091df1f
rahulm-msft <117837698+rahulm-msft@users.noreply.github.com>
2023-07-31 10:55:43 +0530

    ManagedNetworkFabric - CLI Extension for 2023-06-15 API version (#6553)
    * Added GA CLI extension
    
    * updated CLI version
    
    * updating linter exclusion
    
    * updated linterexclusion.yml
    
    * updated config file
    
    * updated test scenarios
    
    * Replacing subscription ID
    
    * supported InternetGatewayRules and added fltten workaround for Internetgateways
    
    * updated README.md
    
    * updated linter exclusion

--- src/managednetworkfabric/azext_managednetworkfabric/tests/latest/test_routepolicy.py
+++ src/managednetworkfabric/azext_managednetworkfabric/tests/latest/test_routepolicy.py
@@ -33,7 +33,6 @@ def call_scenario1(test):
     setup_scenario1(test)
     step_create_s1(test, checks=[])
     step_show(test, checks=[])
-    step_list_subscription(test, checks=[])
     step_list_resource_group(test, checks=[])
     step_delete(test, checks=[])
     cleanup_scenario1(test)
@@ -43,7 +42,6 @@ def call_scenario2(test):
     setup_scenario2(test)
     step_create_s2(test, checks=[])
     step_show(test, checks=[])
-    step_list_subscription(test, checks=[])
     step_list_resource_group(test, checks=[])
     step_delete(test, checks=[])
     cleanup_scenario2(test)
@@ -52,13 +50,13 @@ def step_create_s1(test, checks=None):
     '''routepolicy create operation with IpCommunities'''
     if checks is None:
         checks = []
-    test.cmd('az networkfabric routepolicy create --resource-group {rg} --resource-name {name} --location {location} --statements {statements_with_ipcommunity}', checks=checks)
+    test.cmd('az networkfabric routepolicy create --resource-group {rg} --resource-name {name} --location {location} --nf-id {nfId} --address-family-type {addressFamilyType} --statements {statements_with_ipcommunity}', checks=checks)
 
 def step_create_s2(test, checks=None):
     '''routepolicy create operation with IpExtendedCommunities'''
     if checks is None:
         checks = []
-    test.cmd('az networkfabric routepolicy create --resource-group {rg} --resource-name {name} --location {location} --statements {statements_with_ipextcommunity}', checks=checks)
+    test.cmd('az networkfabric routepolicy create --resource-group {rg} --resource-name {name} --location {location} --nf-id {nfId} --address-family-type {addressFamilyType} --statements {statements_with_ipextcommunity}', checks=checks)
 
 def step_show(test, checks=None):
     '''routepolicy show operation'''
@@ -66,13 +64,6 @@ def step_show(test, checks=None):
         checks = []
     test.cmd(
         'az networkfabric routepolicy show --resource-name {name} --resource-group {rg}')
-    
-def step_delete(test, checks=None):
-    '''routepolicy delete operation'''
-    if checks is None:
-        checks = []
-    test.cmd(
-        'az networkfabric routepolicy delete --resource-name {name} --resource-group {rg}')
 
 def step_list_resource_group(test, checks=None):
     '''routepolicy list by resource group operation'''
@@ -80,13 +71,16 @@ def step_list_resource_group(test, checks=None):
         checks = []
     test.cmd('az networkfabric routepolicy list --resource-group {rg}')
 
-def step_list_subscription(test, checks=None):
-    '''routepolicy list by subscription operation'''
+def step_delete(test, checks=None):
+    '''routepolicy delete operation'''
     if checks is None:
         checks = []
-    test.cmd('az networkfabric routepolicy list')
+    test.cmd(
+        'az networkfabric routepolicy delete --resource-name {name} --resource-group {rg}')
+    test.cmd(
+        'az networkfabric routepolicy wait --resource-name {name} --resource-group {rg} --deleted')
 
-class RoutePolicyScenarioTest1(ScenarioTest):
+class GA_RoutePolicyScenarioTest1(ScenarioTest):
     ''' Route Policy Scenario1 test'''
 
     def __init__(self, *args, **kwargs):
@@ -95,14 +89,13 @@ class RoutePolicyScenarioTest1(ScenarioTest):
             'name': CONFIG.get('ROUTE_POLICY', 'name'),
             'rg': CONFIG.get('ROUTE_POLICY', 'resource_group'),
             'location': CONFIG.get('ROUTE_POLICY', 'location'),
+            'nfId': CONFIG.get('ROUTE_POLICY', 'nf_id'),
+            'addressFamilyType': CONFIG.get('ROUTE_POLICY', 'address_family_type'),
             'statements_with_ipcommunity': CONFIG.get('ROUTE_POLICY', 'statements_with_ipcommunity'),
             'statements_with_ipextcommunity': CONFIG.get('ROUTE_POLICY', 'statements_with_ipextcommunity')
         })
 
-    def test_route_policy_scenario1(self):
+    def test_GA_route_policy_scenario1(self):
         ''' test scenario for Route Policy CRUD operations'''
         call_scenario1(self)
-
-    def test_route_policy_scenario2(self):
-        ''' test scenario for Route Policy CRUD operations'''
         call_scenario2(self)
