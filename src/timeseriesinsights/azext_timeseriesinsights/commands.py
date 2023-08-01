8891292a565a9ffe048fb070a922abd65b844435
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2023-08-01 12:35:21 +0800

    [TimeSeriesInsights] `az tsi access-policy/environment/reference-data-set`: Migrate to Codegen V2 (#6577)
    * initial gen
    
    * az tsi environment gen1 create tested
    
    * az tsi access-policy tested
    
    * location option list add -l
    
    * change to patch for reference-data-set update, fix referenceDataSetName regex pattern for create, tests passed for reference-data-set
    
    * tests passed for environment gen1
    
    * tests passed for environment gen2
    
    * unwrap more class into obj
    
    * dont use aaz for event-source eventhub
    
    * testsed event-source iothub which still uses sdk as well
    
    * remove all unused code, lint, rerun test, update version
    
    * move command import into function, use inheritance

--- src/timeseriesinsights/azext_timeseriesinsights/commands.py
+++ src/timeseriesinsights/azext_timeseriesinsights/commands.py
@@ -10,26 +10,22 @@ from azure.cli.core.commands import CliCommandType
 
 
 def load_command_table(self, _):
-    from azext_timeseriesinsights._client_factory import cf_environment
-    timeseriesinsights_environment = CliCommandType(
-        operations_tmpl='azext_timeseriesinsights.vendored_sdks.timeseriesinsights.operations._environments_operations#'
-        'EnvironmentsOperations.{}',
-        client_factory=cf_environment)
-    with self.command_group('tsi environment', timeseriesinsights_environment, client_factory=cf_environment) as g:
-        g.custom_command('list', 'timeseriesinsights_environment_list')
-        g.custom_show_command('show', 'timeseriesinsights_environment_show')
-        g.custom_command('delete', 'timeseriesinsights_environment_delete', confirmation=True)
-        g.custom_wait_command('wait', 'timeseriesinsights_environment_show')
+    with self.command_group('tsi environment') as g:
+        from .custom import EnvironmentList
+        self.command_table['tsi environment list'] = EnvironmentList(loader=self)
 
-    with self.command_group('tsi environment gen1', timeseriesinsights_environment,
-                            client_factory=cf_environment) as g:
+    with self.command_group('tsi environment gen1') as g:
         g.custom_command('create', 'timeseriesinsights_environment_gen1_create', supports_no_wait=True)
         g.custom_command('update', 'timeseriesinsights_environment_gen1_update', supports_no_wait=True)
 
-    with self.command_group('tsi environment gen2', timeseriesinsights_environment,
-                            client_factory=cf_environment) as g:
+    with self.command_group('tsi environment gen2') as g:
         g.custom_command('create', 'timeseriesinsights_environment_gen2_create', supports_no_wait=True)
         g.custom_command('update', 'timeseriesinsights_environment_gen2_update', supports_no_wait=True)
+
+    with self.command_group('tsi reference-data-set') as g:
+        g.custom_command('create', 'timeseriesinsights_reference_data_set_create')
+        from .custom import ReferenceDataSetList
+        self.command_table['tsi reference-data-set list'] = ReferenceDataSetList(loader=self)
 
     from azext_timeseriesinsights._client_factory import cf_event_source
     timeseriesinsights_event_source = CliCommandType(
@@ -50,29 +46,3 @@ def load_command_table(self, _):
                             client_factory=cf_event_source) as g:
         g.custom_command('create', 'timeseriesinsights_event_source_iot_hub_create')
         g.custom_command('update', 'timeseriesinsights_event_source_iot_hub_update')
-
-    from azext_timeseriesinsights._client_factory import cf_reference_data_set
-    timeseriesinsights_reference_data_set = CliCommandType(
-        operations_tmpl='azext_timeseriesinsights.vendored_sdks.timeseriesinsights.operations._reference_data_sets_oper'
-        'ations#ReferenceDataSetsOperations.{}',
-        client_factory=cf_reference_data_set)
-    with self.command_group('tsi reference-data-set', timeseriesinsights_reference_data_set,
-                            client_factory=cf_reference_data_set) as g:
-        g.custom_command('list', 'timeseriesinsights_reference_data_set_list')
-        g.custom_show_command('show', 'timeseriesinsights_reference_data_set_show')
-        g.custom_command('create', 'timeseriesinsights_reference_data_set_create')
-        g.custom_command('update', 'timeseriesinsights_reference_data_set_update')
-        g.custom_command('delete', 'timeseriesinsights_reference_data_set_delete', confirmation=True)
-
-    from azext_timeseriesinsights._client_factory import cf_access_policy
-    timeseriesinsights_access_policy = CliCommandType(
-        operations_tmpl='azext_timeseriesinsights.vendored_sdks.timeseriesinsights.operations._access_policies_operatio'
-        'ns#AccessPoliciesOperations.{}',
-        client_factory=cf_access_policy)
-    with self.command_group('tsi access-policy', timeseriesinsights_access_policy,
-                            client_factory=cf_access_policy) as g:
-        g.custom_command('list', 'timeseriesinsights_access_policy_list')
-        g.custom_show_command('show', 'timeseriesinsights_access_policy_show')
-        g.custom_command('create', 'timeseriesinsights_access_policy_create')
-        g.custom_command('update', 'timeseriesinsights_access_policy_update')
-        g.custom_command('delete', 'timeseriesinsights_access_policy_delete', confirmation=True)
