bd407cbbca98733cb534a4cddd49219acf975e99
xinyu pang <1042945277@qq.com>
2023-08-02 15:50:35 +0800

    add history

--- src/containerapp/azext_containerapp/_validators.py
+++ src/containerapp/azext_containerapp/_validators.py
@@ -224,7 +224,7 @@ def validate_env_name_or_id(cmd, namespace):
     from azure.cli.core.commands.client_factory import get_subscription_id
     from msrestazure.tools import is_valid_resource_id, resource_id, parse_resource_id
 
-    if not namespace.managed_env or not namespace.resource_group_name:
+    if not namespace.managed_env:
         return
 
     # Set environment type
@@ -233,15 +233,15 @@ def validate_env_name_or_id(cmd, namespace):
     if namespace.__dict__.get("environment_type"):
         environment_type = namespace.environment_type
 
-    if namespace.managed_env:
-        if is_valid_resource_id(namespace.managed_env):
-            env_dict = parse_resource_id(namespace.managed_env)
-            resource_type = env_dict.get("resource_type")
-            if resource_type:
-                if CONNECTED_ENVIRONMENT_RESOURCE_TYPE.lower() == resource_type.lower():
-                    environment_type = CONNECTED_ENVIRONMENT_TYPE
-                if MANAGED_ENVIRONMENT_RESOURCE_TYPE.lower() == resource_type.lower():
-                    environment_type = MANAGED_ENVIRONMENT_TYPE
+    if is_valid_resource_id(namespace.managed_env):
+        env_dict = parse_resource_id(namespace.managed_env)
+        resource_type = env_dict.get("resource_type")
+        if resource_type:
+            if CONNECTED_ENVIRONMENT_RESOURCE_TYPE.lower() == resource_type.lower():
+                environment_type = CONNECTED_ENVIRONMENT_TYPE
+            if MANAGED_ENVIRONMENT_RESOURCE_TYPE.lower() == resource_type.lower():
+                environment_type = MANAGED_ENVIRONMENT_TYPE
+
     # Validate resource id / format resource id
     if environment_type == CONNECTED_ENVIRONMENT_TYPE:
         if not is_valid_resource_id(namespace.managed_env):
@@ -252,7 +252,7 @@ def validate_env_name_or_id(cmd, namespace):
                 type=CONNECTED_ENVIRONMENT_RESOURCE_TYPE,
                 name=namespace.managed_env
             )
-    elif environment_type == MANAGED_ENVIRONMENT_TYPE:
+    else:
         if not is_valid_resource_id(namespace.managed_env):
             namespace.managed_env = resource_id(
                 subscription=get_subscription_id(cmd.cli_ctx),
