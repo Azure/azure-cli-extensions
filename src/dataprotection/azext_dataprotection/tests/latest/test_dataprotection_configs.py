6f1cc55fb73673e17c1d00aab39bdd0fe82e5d3f
Zubair Abid <zubairabid@microsoft.com>
2023-08-14 09:35:54 +0530

    Tests passing for aks hooks

--- src/dataprotection/azext_dataprotection/tests/latest/test_dataprotection_configs.py
+++ src/dataprotection/azext_dataprotection/tests/latest/test_dataprotection_configs.py
@@ -29,22 +29,24 @@ class ConfigScenarioTest(ScenarioTest):
             'friendlyName': "clitest-friendly-aks",
             'permissionsScope': "ResourceGroup",
             'policyRuleName': 'BackupHourly',
+            'payloadBackupHooks': "[{ \'name\':\'name1\',\'namespace\':\'ns1\' },{ \'name\':\'name2\',\'namespace\':\'ns2\'}]",
+            'payloadRestoreHooks': "[{'name':'restorehookname','namespace':'default'},{'name':'restorehookname1','namespace':'hrweb'}]",
         })
 
         # backup_instance_guid = "faec6818-0720-11ec-bd1b-c8f750f92764"
         backup_config_json = test.cmd('az dataprotection backup-instance initialize-backupconfig --datasource-type AzureKubernetesService '
-                                      '--backup-hook-refs "[{\'name\':\'name1\',\'namespace\':\'ns1\'},{\'name\':\'name2\',\'namespace\':\'ns2\'}]"', checks=[
+                                      '--backup-hook-refs "{payloadBackupHooks}"', checks=[
             test.check('include_cluster_scope_resources', True),
             test.check('snapshot_volumes', True),
             test.check('length(backup_hook_references)', 2),
-            test.check('backup_hook_references.name', 'name1')
+            test.check('backup_hook_references[0].name', 'name1')
         ]).get_output_in_json()
 
         restore_config_json = test.cmd('az dataprotection backup-instance initialize-restoreconfig --datasource-type "{dataSourceType}" '
-                                       '--restore-hook-refs "[{\'name\':\'restorehookname\',\'namespace\':\'default\'},{\'name\':\'restorehookname1\',\'namespace\':\'hrweb\'}]"', checks=[
+                                       '--restore-hook-refs "{payloadRestoreHooks}"', checks=[
             test.check("persistent_volume_restore_mode", "RestoreWithVolumeData"),
             test.check("conflict_policy", "Skip"),
             test.check("include_cluster_scope_resources", True),
             test.check('length(restore_hook_references)', 2),
-            test.check('restore_hook_references.name', 'restorehookname')
+            test.check('restore_hook_references[0].name', 'restorehookname')
         ]).get_output_in_json()
