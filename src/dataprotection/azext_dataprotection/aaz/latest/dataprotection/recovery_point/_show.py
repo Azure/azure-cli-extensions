8655f76ce812a3eedea06f76e4e77711d3041e74
Zubair Abid <zubairabid1999+github@gmail.com>
2023-08-14 10:31:30 +0530

    [dataprotection] AKS Hooks for backup and restore, time validation fix (#6562)
    * Generated 2023-05-01 for BI CRUD, Restore, validates
    
    * Fixes to helptext in AAZ
    
    * string-list prototype, won't work, filler code
    
    * backupconfig is now generated fine
    
    * fix format issue, fix time format issue (unrelated), restore boilerplate
    
    * Fixed time validation properly + AKS Hooks is complete
    
    * Upgraded all files to 2023-05-01
    
    * regenerated code with correct swagger
    
    * Minor changes fixing breaks
    
    * recorded tests
    
    * Should fix most issues
    
    * Fix static analysis issues
    
    * Added test for AKS hooks
    
    * Tests passing for aks hooks
    
    * Updated history and setup for release
    
    ---------
    
    Co-authored-by: Zubair Abid <zubairabid@microsoft.com>

--- src/dataprotection/azext_dataprotection/aaz/latest/dataprotection/recovery_point/_show.py
+++ src/dataprotection/azext_dataprotection/aaz/latest/dataprotection/recovery_point/_show.py
@@ -23,9 +23,9 @@ class Show(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2023-01-01",
+        "version": "2023-05-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.dataprotection/backupvaults/{}/backupinstances/{}/recoverypoints/{}", "2023-01-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.dataprotection/backupvaults/{}/backupinstances/{}/recoverypoints/{}", "2023-05-01"],
         ]
     }
 
@@ -148,7 +148,7 @@ class Show(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2023-01-01",
+                    "api-version", "2023-05-01",
                     required=True,
                 ),
             }
