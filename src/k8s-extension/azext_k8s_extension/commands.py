aebbef4a73dbfa207e24b2ff6ed3c0e41fbec73e
Arif Lakhani <ariflakhani@microsoft.com>
2023-08-14 10:28:28 -0700

    Fix linting issues

--- src/k8s-extension/azext_k8s_extension/commands.py
+++ src/k8s-extension/azext_k8s_extension/commands.py
@@ -47,4 +47,3 @@ def load_command_table(self, _):
         g.custom_command('list', 'list_extension_types_by_cluster', deprecate_info=g.deprecate(redirect='az k8s-extension extension-types list-by-cluster', hide=True), table_transformer=k8s_extension_types_list_table_format)
 
         g.custom_command('list-versions', 'list_extension_type_versions_by_cluster', deprecate_info=g.deprecate(redirect='az k8s-extension extension-types list-versions-by-cluster', hide=True), table_transformer=k8s_extension_type_versions_list_table_format)
-
