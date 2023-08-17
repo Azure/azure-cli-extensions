1fdb9b28fad2e709f39a4131f4ac8401352e7f4c
Priya Shet <priya.shet@gmail.com>
2023-08-16 21:34:36 -0500

    Updated NetworkCloud 2023-07-01 latest changes for stable version 1.0.0 (#6648)

--- src/networkcloud/azext_networkcloud/aaz/latest/networkcloud/kubernetescluster/_show.py
+++ src/networkcloud/azext_networkcloud/aaz/latest/networkcloud/kubernetescluster/_show.py
@@ -13,7 +13,6 @@ from azure.cli.core.aaz import *
 
 @register_command(
     "networkcloud kubernetescluster show",
-    is_preview=True,
 )
 class Show(AAZCommand):
     """Get properties of the provided the Kubernetes cluster.
