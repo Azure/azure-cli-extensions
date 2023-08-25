2a926c438bd44d4ce27ee60b4fbdb407c3d14b8d
rafaeldnferreira <93147560+rafaeldnferreira@users.noreply.github.com>
2023-08-25 03:14:51 +0100

    Richer example for bulk-sim-upload (#6687)
    Co-authored-by: Rafael Ferreira <rf4@metaswitch.com>

--- src/mobile-network/azext_mobile_network/aaz/latest/mobile_network/sim/group/_bulk_upload_sims.py
+++ src/mobile-network/azext_mobile_network/aaz/latest/mobile_network/sim/group/_bulk_upload_sims.py
@@ -17,8 +17,9 @@ from azure.cli.core.aaz import *
 class BulkUploadSims(AAZCommand):
     """Bulk upload SIMs to a SIM group.
 
-    :example: Uploading multiple sims to a sim group
-        az mobile-network sim group bulk-upload-sims -g rg --sim-group-name SimGroup --sims [{name:bulk-upload-sim-01,authentication-key:00000000000000000000000000000000,operator-key-code:00000000000000000000000000000000,international-msi:0000000000},{name:bulk-upload-sim-02,authentication-key:00000000000000000000000000000001,operator-key-code:00000000000000000000000000000001,international-msi:0000000001}]
+    :example: Uploading multiple sims with to a sim group
+        az mobile-network sim group bulk-upload-sims -g rg --sim-group-name SimGroup --sims "[{name:bulk-upload-sim-01,authentication-key:00000000000000000000000000000000,operator-key-code:00000000000000000000000000000000,international-msi:0000000000},{name:bulk-upload-sim-02,authentication-key:00000000000000000000000000000001,operator-key-code:00000000000000000000000000000001,international-msi:0000000001}]"
+        az mobile-network sim group bulk-upload-sims -g rg --sim-group-name SimGroup --sims "[{name:bulk-upload-sim-01,authentication-key:00000000000000000000000000000000,operator-key-code:00000000000000000000000000000000,international-msi:0000000000,icc-id:00000000000000000000,device-type:camera,sim-policy:{id:/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.MobileNetwork/mobileNetworks/mobile-network/simPolicies/policy01},static-ip-configuration:[{attached-data-network:{id:/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.MobileNetwork/packetCoreControlPlanes/pccp01/packetCoreDataPlanes/pccp01/attachedDataNetworks/internet1},slice:{id:/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.MobileNetwork/mobileNetworks/mobile-network/slices/slice01},static-ip:{ipv4-address:2.4.0.10}}]}]"
     """
 
     _aaz_info = {
