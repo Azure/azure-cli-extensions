3bce709cbd0b2f2f4829fc71ae990a0d2694c3a4
xinyu pang <1042945277@qq.com>
2023-08-28 16:32:50 +0800

    add help and params

--- src/containerapp/azext_containerapp/_params.py
+++ src/containerapp/azext_containerapp/_params.py
@@ -13,7 +13,7 @@ from azure.cli.core.commands.parameters import (resource_group_name_type, get_lo
 from ._validators import (validate_memory, validate_cpu, validate_managed_env_name_or_id, validate_registry_server,
                           validate_registry_user, validate_registry_pass, validate_target_port, validate_ingress,
                           validate_storage_name_or_id, validate_cors_max_age, validate_env_name_or_id,
-                          validate_allow_insecure)
+                          validate_allow_insecure, validate_custom_location_name_or_id)
 from ._constants import UNAUTHENTICATED_CLIENT_ACTION, FORWARD_PROXY_CONVENTION, MAXIMUM_CONTAINER_APP_NAME_LENGTH, LOG_TYPE_CONSOLE, LOG_TYPE_SYSTEM
 
 
@@ -519,3 +519,16 @@ def load_arguments(self, _):
     with self.argument_context('containerapp') as c:
         c.argument('managed_env', validator=validate_env_name_or_id, options_list=['--environment'], help="Name or resource ID of the container app's environment.")
         c.argument('environment_type', arg_type=get_enum_type(["managed", "connected"]), help="Type of environment.", is_preview=True)
+
+    with self.argument_context('containerapp connected-env') as c:
+        c.argument('name', name_type, help='Name of the Container Apps environment.')
+        c.argument('resource_group_name', arg_type=resource_group_name_type)
+        c.argument('location', arg_type=get_location_type(self.cli_ctx), help='Location of resource. Examples: eastus2, northeurope')
+        c.argument('tags', arg_type=tags_type)
+        c.argument('custom_location', help="Resource ID of custom location. List using 'az customlocation list'.", validator=validate_custom_location_name_or_id)
+
+    with self.argument_context('containerapp connected-env', arg_group='Dapr') as c:
+        c.argument('dapr_ai_connection_string', options_list=['--dapr-ai-connection-string', '--dapr-connection'], help='Connection string used for Dapr application insights.')
+
+    with self.argument_context('containerapp connected-env', arg_group='Network') as c:
+        c.argument('static_ip', help='Static IP value.')
