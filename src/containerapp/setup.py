eb9a42fb0e42c45ab49e7be692a4cbe25ff1bc6e
xinyu pang <1042945277@qq.com>
2023-08-30 12:32:10 +0800

    add dependency

--- src/containerapp/setup.py
+++ src/containerapp/setup.py
@@ -47,7 +47,9 @@ CLASSIFIERS = [
 ]
 
 # TODO: Add any additional SDK dependencies here
-DEPENDENCIES = ['pycomposefile>=0.0.29', 'docker']
+DEPENDENCIES = ['pycomposefile>=0.0.29',
+                'docker',
+                'azure-mgmt-kubernetesconfiguration==2.0.0']
 
 # Install pack CLI to build runnable application images from source
 _ = get_pack_exec_path()
