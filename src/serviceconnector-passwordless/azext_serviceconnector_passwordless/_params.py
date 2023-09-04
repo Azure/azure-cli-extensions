8d9b5169232ff9db6e7bb18d9e6a12f5a99e8a0c
xiaofanzhou <xiaofanzhou@microsoft.com>
2023-09-04 16:03:28 +0800

    supporting customized keys in passwordless

--- src/serviceconnector-passwordless/azext_serviceconnector_passwordless/_params.py
+++ src/serviceconnector-passwordless/azext_serviceconnector_passwordless/_params.py
@@ -14,7 +14,8 @@ from azure.cli.command_modules.serviceconnector._params import (
     add_vnet_block,
     add_connection_string_argument,
     add_secret_store_argument,
-    add_local_connection_block
+    add_local_connection_block,
+    add_customized_keys_argument
 )
 from azure.cli.command_modules.serviceconnector._validators import (
     get_default_object_id_of_current_user
@@ -63,6 +64,7 @@ def load_arguments(self, _):
             add_secret_store_argument(c)
             add_vnet_block(c, target)
             add_local_connection_block(c)
+            add_customized_keys_argument(c)
             c.argument('yes', arg_type=yes_arg_type)
 
     for source in SOURCE_RESOURCES_PARAMS:
@@ -77,4 +79,5 @@ def load_arguments(self, _):
                 add_secret_store_argument(c)
                 add_vnet_block(c, target)
                 add_connection_string_argument(c, source, target)
+                add_customized_keys_argument(c)
                 c.argument('yes', arg_type=yes_arg_type)
