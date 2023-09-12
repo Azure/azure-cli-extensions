ccefea749ad06194fdae2e927f71b2588e9e1ce0
Felix Song <felixsong@microsoft.com>
2023-09-12 17:38:00 +0800

    update url logic for log stream legion

--- src/spring/azext_spring/_log_stream.py
+++ src/spring/azext_spring/_log_stream.py
@@ -10,7 +10,10 @@ class LogStream:
     def __init__(self, client, resource_group, service):
         test_keys = client.services.list_test_keys(resource_group, service)
         self.primary_key = test_keys.primary_key
+        # non-legion arch:
         # https://primary:xxxx[key]@servicename.test.azuremicrosoervice.io -> servicename.azuremicroservice.io
+        # legion arch:
+        # https://primary:xxxx[key]@t.cert-name.region.azuremicrosoervice.io -> svc.cert-name.region.azuremicrosoervice.io
         test_url = test_keys.primary_test_endpoint
-        base_url = test_url.replace('.test.', '.')
+        base_url = test_url.replace('.test.', '.').replace('@t.', '@svc.')
         self.base_url = re.sub('https://.+?\@', '', base_url)
