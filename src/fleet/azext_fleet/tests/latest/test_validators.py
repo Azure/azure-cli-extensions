1ee26b73a04b9486d65a96daab9c0ee65709f0a8
Sean Hobbs <sehobbs@microsoft.com>
2023-09-15 14:51:14 -0700

    TestValidateAgentSubnetID

--- src/fleet/azext_fleet/tests/latest/test_validators.py
+++ src/fleet/azext_fleet/tests/latest/test_validators.py
@@ -17,6 +17,11 @@ class ApiServerSubnetIDNamespace:
     def _init_(self, apiserver_subnet_id):
         self.apiserver_subnet_id = apiserver_subnet_id
 
+class AgentSubnetIDNamespace:
+
+    def _init_(self, agent_subnet_id):
+        self.agent_subnet_id = agent_subnet_id
+
 class TestValidateMemberClusterId(unittest.TestCase):
     def test_invalid_member_cluster_id(self):
         invalid_member_cluster_id = "dummy cluster id"
@@ -48,6 +53,22 @@ class TestValidateApiServerSubnetID(unittest.TestCase):
             namespace = ApiServerSubnetIDNamespace(apiserver_subnet_id=valid_apiserver_subnetid)
 
             self.assertIsNone(validators.validate_apiserver_subnet_id(namespace))
+
+class TestValidateAgentSubnetID(unittest.TestCase):
+        def test_invalid_agent_subnet_id(self):
+            invalid_agent_subnet_id = ""
+            namespace = AgentSubnetIDNamespace(agent_subnet_id=invalid_agent_subnet_id)
+            err = ("--agent-subnet-id is not a valid Azure resource ID.")
+
+            with self.assertRaises(CLIError) as cm:
+                validators.validate_agent_subnet_id(namespace)
+            self.assertEqual(str(cm.exception), err)
+
+        def test_valid_agent_subnet_id(self):
+            valid_agent_subnetid = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rgname/providers/Microsoft.Network/virtualNetworks/vnname/subnets/subnetname"
+            namespace = AgentSubnetIDNamespace(agent_subnet_id=valid_agent_subnetid)
+
+            self.assertIsNone(validators.validate_agent_subnet_id(namespace))
 
 if __name__ == "__main__":
     unittest.main()
