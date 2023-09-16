22258056d4de8de52f8239aa5c44c485f9258f7e
nshankar <nshankar@microsoft.ghe.com>
2023-09-16 00:07:43 -0400

    update deletion check
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -7466,7 +7466,6 @@ Message: The provided location 'useast' is not available for resource group. '''
         self.cmd(update_cmd, checks=[
             self.check('serviceMeshProfile.mode', 'Istio'),
             self.check('serviceMeshProfile.istio.components.egressGateways[0].enabled', None),
-            self.check('serviceMeshProfile.istio.components.egressGateways[0].nodeSelector', None)
         ])
 
         # delete the cluster
