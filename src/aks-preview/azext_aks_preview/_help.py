f30704c81089cc81ec5e5b06a4fb11cc8f53c5df
deveshdama <87668846+deveshdama@users.noreply.github.com>
2023-09-18 23:16:41 -0700

    Update src/aks-preview/azext_aks_preview/_help.py
    Co-authored-by: Sanya Kochhar <42152676+SanyaKochhar@users.noreply.github.com>

--- src/aks-preview/azext_aks_preview/_help.py
+++ src/aks-preview/azext_aks_preview/_help.py
@@ -2648,7 +2648,7 @@ helps['aks mesh disable-ingress-gateway'] = """
 helps['aks mesh get-revisions'] = """
     type: command
     short-summary: Discover available Azure Service Mesh revisions and their compatibility.
-    long-summary: This command discovers available Azure Service Mesh revisions and their compatibility for the given location.
+    long-summary: This command discovers available Azure Service Mesh revisions and their compatibility information for the given location.
     examples:
       - name: Discover Azure Service Mesh revisions.
         text: az aks mesh get-revisions --location westus2
