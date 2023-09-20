905c11cf7fad1eb2676fa72e9aef5e983a6d2d77
Zubair Abid <zubairabid@microsoft.com>
2023-09-20 16:24:14 +0530

    Add license file to preparers

--- src/dataprotection/azext_dataprotection/tests/latest/preparers.py
+++ src/dataprotection/azext_dataprotection/tests/latest/preparers.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from azure.cli.testsdk import CliTestError, ResourceGroupPreparer
 from azure.cli.testsdk.preparers import AbstractPreparer, SingleValueReplacer
 from azure.cli.testsdk.base import execute
