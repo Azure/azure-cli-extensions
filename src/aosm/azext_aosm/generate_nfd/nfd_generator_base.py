c2e58e11c064e2a8564180cf55b3458d6fa7477c
Chaos Chhapi <chaos.chhapi@metaswitch.com>
2023-09-20 15:41:24 +0100

    Fix license header formatting

--- src/aosm/azext_aosm/generate_nfd/nfd_generator_base.py
+++ src/aosm/azext_aosm/generate_nfd/nfd_generator_base.py
@@ -1,7 +1,7 @@
-# --------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT
-# License. See License.txt in the project root for license information.
-# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 """Contains a base class for generating NFDs."""
 from abc import ABC, abstractmethod
 from pathlib import Path
