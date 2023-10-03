49b6ce325fd929c0b5b2b69c8e6d3abed9bc8884
Sean Hobbs <ealianis@outlook.com>
2023-10-03 14:12:10 -0700

    lint fixes

--- src/fleet/azext_fleet/custom.py
+++ src/fleet/azext_fleet/custom.py
@@ -132,9 +132,9 @@ def update_fleet(cmd,
         resource_type=CUSTOM_MGMT_FLEET,
         operation_group="fleets"
     )
-    
+
     managed_service_identity = None
-    if enable_managed_identity == False:
+    if enable_managed_identity is False:
         managed_service_identity = fleet_managed_service_identity_model(type="None")
     else:
         managed_service_identity = fleet_managed_service_identity_model(type="SystemAssigned")
