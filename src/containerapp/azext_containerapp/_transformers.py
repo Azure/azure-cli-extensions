a991d1208ea2648fd03beeb30db2ffa1a0d185c9
Patrick Bouchon <pbouchon@microsoft.com>
2023-10-17 18:04:26 -0500

    hide containerapps and jobs env vars on create and update

--- src/containerapp/azext_containerapp/_transformers.py
+++ src/containerapp/azext_containerapp/_transformers.py
@@ -3,7 +3,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
+from azure.cli.command_modules.containerapp._utils import safe_get
 
 def transform_containerapp_output(app):
     props = ['name', 'location', 'resourceGroup', 'provisioningState']
@@ -15,6 +15,38 @@ def transform_containerapp_output(app):
         result['fqdn'] = None
 
     return result
+
+
+def clean_up_sensitive_values(response_json):
+    for container in safe_get(response_json, "properties", "template", "containers", default=[]):
+        if "env" in container:
+            for env in container["env"]:
+                if env.get("value"):
+                    del env["value"]
+
+    if safe_get(response_json, "properties", "template") and "scale" in response_json["properties"]["template"]:
+        for rule in safe_get(response_json, "properties", "template", "scale", "rules", default=[]):
+            for (key, val) in rule.items():
+                if key != "name":
+                    val["metadata"] = dict((k, "") for k, v in val["metadata"].items())
+                    
+    return response_json
+
+
+def transform_sensitive_values_wrapper():
+
+    def transform_sensitive_values(response_json):
+        return clean_up_sensitive_values(response_json)
+
+    return transform_sensitive_values
+
+
+def transform_sensitive_values_list_output_wrapper():
+
+    def transform_sensitive_values_list_output(apps):
+        return [clean_up_sensitive_values(a) for a in apps]
+
+    return transform_sensitive_values_list_output
 
 
 def transform_containerapp_list_output(apps):
