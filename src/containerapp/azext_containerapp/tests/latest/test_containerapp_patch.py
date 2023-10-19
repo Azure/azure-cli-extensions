60d07338a5cd77bb679eaefce23f9135f84f4195
Cormac McCarthy <corm@microsoft.com>
2023-10-19 16:36:25 -0700

    [containerapp] Update builder image and patch logic

--- src/containerapp/azext_containerapp/tests/latest/test_containerapp_patch.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerapp_patch.py
@@ -15,39 +15,40 @@ TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
 class ContainerAppPatchTest(ScenarioTest):
     @live_only()
+    @AllowLargeResponse(8192)
     @ResourceGroupPreparer(location = "eastus2")
     def test_containerapp_patch_list_and_apply_with_resource_group_e2e(self, resource_group):
-        source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack"))
+        old_version = "7.0.9-debian-bullseye"
+        old_run_image = f"mcr.microsoft.com/oryx/dotnetcore:{old_version}"
+        image_to_deploy = f"sourcetocloudtestacr.azurecr.io/sample/dotnet:{old_version}"
         ingress = 'external'
         target_port = '8080'
 
         # Generate a name for the Container App
         app_name = self.create_random_name(prefix='containerapp', length=24)
 
-        # Create container app and re-tag image to an older version to simulate patch list
-        old_tag = "run-dotnet-aspnet-7.0.1-cbl-mariner2.0"
-        create_and_verify_containerapp_up(self,resource_group,source_path=source_path,ingress=ingress,target_port=target_port,app_name=app_name)
-        self._retag_image_to_older_version_and_push(resource_group=resource_group,app_name=app_name,old_tag=old_tag)
+        # Create a Container App using a .NET 7.0 image with an outdated run image that is eligible for patching
+        create_and_verify_containerapp_up(self, resource_group, image=image_to_deploy, ingress=ingress, target_port=target_port, app_name=app_name)
 
         # Execute and verify patch list command
         patchable_images = self.cmd(f'containerapp patch list -g {resource_group}').get_output_in_json()
-        newRunImageTag = patchable_images[0]["newRunImage"].split(':')[1]
-        self.assertEquals(newRunImageTag,_utils.get_latest_buildpack_run_tag("aspnet", "7.0"))
-        self.assertEquals(patchable_images[0]["oldRunImage"], old_tag)
+        self.assertTrue(len(patchable_images) == 1)
+        self.assertEquals(patchable_images[0]["oldRunImage"], old_run_image)
 
         # Execute and verify patch apply command
         self.cmd(f'containerapp patch apply -g {resource_group}')
-        app = self.cmd(f"containerapp show -g {resource_group} -n {app_name}").get_output_in_json()
-        patched_image = app["properties"]["template"]["containers"][0]["image"]
-        patched_image_tag = patched_image.split(':')[1]
-        self.assertEquals(patched_image_tag,_utils.get_latest_buildpack_run_tag("aspnet", "7.0"))
+        patchable_images = self.cmd(f'containerapp patch list -g {resource_group}').get_output_in_json()
+        self.assertTrue(len(patchable_images) == 0)
 
     @live_only()
+    @AllowLargeResponse(8192)
     @ResourceGroupPreparer(location = "eastus2")
     def test_containerapp_patch_list_and_apply_with_environment_e2e(self, resource_group):
-        source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack"))
+        old_version = "7.0.9-debian-bullseye"
+        old_run_image = f"mcr.microsoft.com/oryx/dotnetcore:{old_version}"
+        image_to_deploy = f"sourcetocloudtestacr.azurecr.io/sample/dotnet:{old_version}"
         ingress = 'external'
-        target_port = '80'
+        target_port = '8080'
 
         # Generate a name for the Container App
         app_name = self.create_random_name(prefix='containerapp', length=24)
@@ -56,25 +57,21 @@ class ContainerAppPatchTest(ScenarioTest):
         env_name = self.create_random_name(prefix='env', length=24)
         self.cmd(f'containerapp env create -g {resource_group} -n {env_name}')
 
-        # Create container app and re-tag image to an older version to simulate patch list
-        old_tag = "run-dotnet-aspnet-7.0.1-cbl-mariner2.0"
-        create_and_verify_containerapp_up(self,resource_group,env_name=env_name,source_path=source_path,ingress=ingress,target_port=target_port,app_name=app_name)
-        self._retag_image_to_older_version_and_push(resource_group=resource_group,app_name=app_name,old_tag=old_tag)
+        # Create a Container App using a .NET 7.0 image with an outdated run image that is eligible for patching
+        create_and_verify_containerapp_up(self, resource_group, env_name=env_name, image=image_to_deploy, ingress=ingress, target_port=target_port, app_name=app_name)
 
         # Execute and verify patch list command
         patchable_images = self.cmd(f'containerapp patch list -g {resource_group} --environment {env_name}').get_output_in_json()
-        newRunImageTag = patchable_images[0]["newRunImage"].split(':')[1]
-        self.assertEquals(newRunImageTag,_utils.get_latest_buildpack_run_tag("aspnet", "7.0"))
-        self.assertEquals(patchable_images[0]["oldRunImage"], old_tag)
+        self.assertTrue(len(patchable_images) == 1)
+        self.assertEquals(patchable_images[0]["oldRunImage"], old_run_image)
 
         # Execute and verify patch apply command
         self.cmd(f'containerapp patch apply -g {resource_group} --environment {env_name}')
-        app = self.cmd(f"containerapp show -g {resource_group} -n {app_name}").get_output_in_json()
-        patched_image = app["properties"]["template"]["containers"][0]["image"]
-        patched_image_tag = patched_image.split(':')[1]
-        self.assertEquals(patched_image_tag,_utils.get_latest_buildpack_run_tag("aspnet", "7.0"))
+        patchable_images = self.cmd(f'containerapp patch list -g {resource_group} --environment {env_name}').get_output_in_json()
+        self.assertTrue(len(patchable_images) == 0)
 
     @live_only()
+    @AllowLargeResponse(8192)
     @ResourceGroupPreparer(location = "eastus2")
     def test_containerapp_patch_list_and_apply_with_show_all_e2e(self, resource_group):
         image = "mcr.microsoft.com/k8se/quickstart:latest"
@@ -86,68 +83,97 @@ class ContainerAppPatchTest(ScenarioTest):
         env_name = self.create_random_name(prefix='env', length=24)
         self.cmd(f'containerapp env create -g {resource_group} -n {env_name}')
 
-        create_and_verify_containerapp_up(self,resource_group,env_name=env_name,image=image,app_name=app_name)
+        create_and_verify_containerapp_up(self, resource_group, env_name=env_name, image=image, app_name=app_name)
 
         # Execute and verify patch list command
         patch_cmd = f'containerapp patch list -g {resource_group} --environment {env_name} --show-all'
         output = self.cmd(patch_cmd).get_output_in_json()
-        self.assertEquals(output[0]["targetImageName"],"mcr.microsoft.com/k8se/quickstart:latest")
+        self.assertEquals(output[0]["targetImageName"], "mcr.microsoft.com/k8se/quickstart:latest")
 
         # Execute and verify patch apply command
         self.cmd(f'containerapp patch apply -g {resource_group} --environment {env_name} --show-all')
         app = self.cmd(f"containerapp show -g {resource_group} -n {app_name}").get_output_in_json()
         image = app["properties"]["template"]["containers"][0]["image"]
-        self.assertEquals(image,"mcr.microsoft.com/k8se/quickstart:latest")
+        self.assertEquals(image, "mcr.microsoft.com/k8se/quickstart:latest")
 
 
     @live_only()
+    @AllowLargeResponse(8192)
     @ResourceGroupPreparer(location = "eastus2")
     def test_containerapp_patch_list_and_apply_without_arguments_e2e(self, resource_group):
-        source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack"))
+        old_version = "7.0.9-debian-bullseye"
+        old_run_image = f"mcr.microsoft.com/oryx/dotnetcore:{old_version}"
+        image_to_deploy = f"sourcetocloudtestacr.azurecr.io/sample/dotnet:{old_version}"
         ingress = 'external'
-        target_port = '80'
+        target_port = '8080'
 
         # Generate a name for the Container App
         app_name = self.create_random_name(prefix='containerapp', length=24)
 
-        # Create container app and re-tag image to an older version to simulate patch list
-        old_tag = "run-dotnet-aspnet-7.0.1-cbl-mariner2.0"
-        create_and_verify_containerapp_up(self,resource_group,source_path=source_path,ingress=ingress,target_port=target_port,app_name=app_name)
-        self._retag_image_to_older_version_and_push(resource_group=resource_group,app_name=app_name,old_tag=old_tag)
+        # Create a Container App using a .NET 7.0 image with an outdated run image that is eligible for patching
+        create_and_verify_containerapp_up(self, resource_group, image=image_to_deploy, ingress=ingress, target_port=target_port, app_name=app_name)
 
         # Execute and verify patch list command
         self.cmd(f'configure --defaults group={resource_group}')
-        patch_cmd = f'containerapp patch list'
-        patchable_images = self.cmd(patch_cmd).get_output_in_json()
-        newRunImageTag = patchable_images[0]["newRunImage"].split(':')[1]
-        self.assertEquals(newRunImageTag,_utils.get_latest_buildpack_run_tag("aspnet", "7.0"))
-        self.assertEquals(patchable_images[0]["oldRunImage"], old_tag)
+        patchable_images = self.cmd(f'containerapp patch list').get_output_in_json()
+        self.assertTrue(len(patchable_images) == 1)
+        self.assertEquals(patchable_images[0]["oldRunImage"], old_run_image)
 
         # Execute and verify patch apply command
         self.cmd(f'containerapp patch apply')
-        app = self.cmd(f"containerapp show -g {resource_group} -n {app_name}").get_output_in_json()
-        patched_image = app["properties"]["template"]["containers"][0]["image"]
-        patched_image_tag = patched_image.split(':')[1]
-        self.assertEquals(patched_image_tag,_utils.get_latest_buildpack_run_tag("aspnet", "7.0"))
+        patchable_images = self.cmd(f'containerapp patch list').get_output_in_json()
+        self.assertTrue(len(patchable_images) == 0)
 
-    def _retag_image_to_older_version_and_push(self,resource_group,app_name,old_tag):
-        client = docker.from_env()
-        app = self.cmd(f"containerapp show -g {resource_group} -n {app_name}").get_output_in_json()
-        image_name = app["properties"]["template"]["containers"][0]["image"]
-        current_image = client.images.get(image_name)
-        registry = app["properties"]["configuration"]["registries"][0]["server"]
-        repo = app["properties"]["template"]["containers"][0]["name"]
+    @live_only()
+    @AllowLargeResponse(8192)
+    @ResourceGroupPreparer(location = "eastus2")
+    def test_containerapp_patch_list_and_apply_with_node18_e2e(self, resource_group):
+        old_version = "18.16.1-debian-bullseye"
+        old_run_image = f"mcr.microsoft.com/oryx/node:{old_version}"
+        image_to_deploy = f"sourcetocloudtestacr.azurecr.io/sample/node:{old_version}"
+        ingress = 'external'
+        target_port = '8080'
 
-        # Re-tag image to an older version to simulate patch list
-        old_image_name = registry + "/" + repo + ":" + old_tag
-        current_image.tag(registry + "/" + repo, tag = old_tag)
+        # Generate a name for the Container App
+        app_name = self.create_random_name(prefix='containerapp', length=24)
 
-        # Re-tag and push
-        client.images.push(registry + "/" + repo, tag = old_tag)
-        self.cmd('az containerapp update -n {} -g {} --image {}'.format(app_name, resource_group, old_image_name))
-        app = self.cmd(f"containerapp show -g {resource_group} -n {app_name}").get_output_in_json()
-        retagged_image = app["properties"]["template"]["containers"][0]["image"]
-        retagged_image_tag = retagged_image.split(':')[1]
+        # Create a Container App using a Node 18 image with an outdated run image that is eligible for patching
+        create_and_verify_containerapp_up(self, resource_group, image=image_to_deploy, ingress=ingress, target_port=target_port, app_name=app_name)
 
-        # Verify if image was updated
-        self.assertEquals(retagged_image_tag,old_tag)
+        # Execute and verify patch list command
+        self.cmd(f'configure --defaults group={resource_group}')
+        patchable_images = self.cmd(f'containerapp patch list').get_output_in_json()
+        self.assertTrue(len(patchable_images) == 1)
+        self.assertEquals(patchable_images[0]["oldRunImage"], old_run_image)
+
+        # Execute and verify patch apply command
+        self.cmd(f'containerapp patch apply')
+        patchable_images = self.cmd(f'containerapp patch list').get_output_in_json()
+        self.assertTrue(len(patchable_images) == 0)
+
+    @live_only()
+    @AllowLargeResponse(8192)
+    @ResourceGroupPreparer(location = "eastus2")
+    def test_containerapp_patch_list_and_apply_with_python310_e2e(self, resource_group):
+        old_version = "3.10.4-debian-bullseye"
+        old_run_image = f"mcr.microsoft.com/oryx/python:{old_version}"
+        image_to_deploy = f"sourcetocloudtestacr.azurecr.io/sample/python:{old_version}"
+        ingress = 'external'
+        target_port = '8080'
+
+        # Generate a name for the Container App
+        app_name = self.create_random_name(prefix='containerapp', length=24)
+
+        # Create a Container App using a Python 3.9 image with an outdated run image that is eligible for patching
+        create_and_verify_containerapp_up(self, resource_group, image=image_to_deploy, ingress=ingress, target_port=target_port, app_name=app_name)
+
+        # Execute and verify patch list command
+        self.cmd(f'configure --defaults group={resource_group}')
+        patchable_images = self.cmd(f'containerapp patch list').get_output_in_json()
+        self.assertTrue(len(patchable_images) == 1)
+        self.assertEquals(patchable_images[0]["oldRunImage"], old_run_image)
+
+        # Execute and verify patch apply command
+        self.cmd(f'containerapp patch apply')
+        patchable_images = self.cmd(f'containerapp patch list').get_output_in_json()
+        self.assertTrue(len(patchable_images) == 0)
