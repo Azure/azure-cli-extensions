33d0a1679a284b173af0d25aeb4f87b11cfacbb9
Abhilash Iyer <547048+9lash@users.noreply.github.com>
2023-10-23 16:42:23 +0000

    Fix get api calls

--- src/connectedk8s/azext_connectedk8s/custom.py
+++ src/connectedk8s/azext_connectedk8s/custom.py
@@ -648,9 +648,9 @@ def get_kubernetes_distro(api_response):  # Heuristic
                 return "eks"
             if labels.get("minikube.k8s.io/version"):
                 return "minikube"
-            if annotations.get("node.aksedge.io/distro: aks_edge_k3s"):
+            if annotations.get("node.aksedge.io/distro") == 'aks_edge_k3s':
                 return "aks_edge_k3s"
-            if annotations.get("node.aksedge.io/distro: aks_edge_k8s"):
+            if annotations.get("node.aksedge.io/distro") == 'aks_edge_k8s':
                 return "aks_edge_k8s"
             if provider_id.startswith("kind://"):
                 return "kind"
