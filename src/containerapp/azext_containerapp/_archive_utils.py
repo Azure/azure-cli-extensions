3872e050f5e6f2df8a1b586ce944831d6c6f75cc
Dan Vouaux <daniv@microsoft.com>
2023-11-09 13:16:08 -0800

    Fix CI + udpate history

--- src/containerapp/azext_containerapp/_archive_utils.py
+++ src/containerapp/azext_containerapp/_archive_utils.py
@@ -186,7 +186,7 @@ def _load_dockerignore_file(source_location, original_docker_file_name):
 
 
 def _archive_file_recursively(tar, name, arcname, parent_ignored, parent_matching_rule_index, ignore_check):
-    if os.path.isfile(name) and (arcname == "" or arcname == None):
+    if os.path.isfile(name) and (arcname == "" or arcname is None):
         # If the file is in the root dir, use its name as the arcname
         arcname = os.path.basename(name)
 
