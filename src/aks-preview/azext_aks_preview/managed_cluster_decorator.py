3df06aead891d75ff714382073fbc734f82c2c01
Suneha Bose <sunehabose@microsoft.com>
2023-11-09 00:59:50 -0800

    more feedback change

--- src/aks-preview/azext_aks_preview/managed_cluster_decorator.py
+++ src/aks-preview/azext_aks_preview/managed_cluster_decorator.py
@@ -2750,14 +2750,14 @@ class AKSPreviewManagedClusterCreateDecorator(AKSManagedClusterCreateDecorator):
         self._ensure_mc(mc)
 
         addons = self.context.get_enable_addons()
-        if "web_application_routing" in addons:
+        if "web_application_routing" in addons or self.context.get_enable_app_routing():
             if mc.ingress_profile is None:
                 mc.ingress_profile = self.models.ManagedClusterIngressProfile()
-            dns_zone_resource_ids = self.context.get_dns_zone_resource_ids()
-            mc.ingress_profile.web_app_routing = self.models.ManagedClusterIngressProfileWebAppRouting(
-                enabled=True,
-                dns_zone_resource_ids=dns_zone_resource_ids,
-            )
+            mc.ingress_profile.web_app_routing = self.models.ManagedClusterIngressProfileWebAppRouting(enabled=True)
+            if "web_application_routing" in addons:
+                dns_zone_resource_ids = self.context.get_dns_zone_resource_ids()
+                mc.ingress_profile.web_app_routing.dns_zone_resource_ids = dns_zone_resource_ids
+
         return mc
 
     def set_up_workload_auto_scaler_profile(self, mc: ManagedCluster) -> ManagedCluster:
