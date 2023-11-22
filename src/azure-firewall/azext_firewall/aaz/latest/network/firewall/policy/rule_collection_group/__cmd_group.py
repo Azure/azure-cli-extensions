0a84c7f493d651dfae8a7d599fb73d4a31b29cee
Maria Tatarnikava <mtatarnikava@microsoft.com>
2023-11-22 12:28:07 +0000

    adding new param httpheaderstoinsert to rule collection groups

--- src/azure-firewall/azext_firewall/aaz/latest/network/firewall/policy/rule_collection_group/__cmd_group.py
+++ src/azure-firewall/azext_firewall/aaz/latest/network/firewall/policy/rule_collection_group/__cmd_group.py
@@ -8,10 +8,17 @@
 # pylint: skip-file
 # flake8: noqa
 
-from .__cmd_group import *
-from ._create import *
-from ._delete import *
-from ._list import *
-from ._show import *
-from ._update import *
-from ._wait import *
+from azure.cli.core.aaz import *
+
+
+@register_command_group(
+    "network firewall policy rule-collection-group",
+    is_preview=True,
+)
+class __CMDGroup(AAZCommandGroup):
+    """Manage and configure Azure firewall policy rule collection group.
+    """
+    pass
+
+
+__all__ = ["__CMDGroup"]
--- src/azure-firewall/azext_firewall/aaz/latest/network/firewall/policy/rule_collection_group/_delete.py
+++ src/azure-firewall/azext_firewall/aaz/latest/network/firewall/policy/rule_collection_group/_delete.py
@@ -12,20 +12,18 @@ from azure.cli.core.aaz import *
 
 
 @register_command(
-    "network firewall learned-ip-prefix",
+    "network firewall policy rule-collection-group delete",
     is_preview=True,
+    confirmation="Are you sure you want to perform this operation?",
 )
-class LearnedIpPrefix(AAZCommand):
-    """Retrieves a list of all IP prefixes that azure firewall has learned to not SNAT.
-
-    :example: List Learned IP Prefixes
-        az network firewall learned-ip-prefix -g MyResourceGroup -n MyFirewall
+class Delete(AAZCommand):
+    """Delete an Azure Firewall policy rule collection group.
     """
 
     _aaz_info = {
         "version": "2022-01-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.network/azurefirewalls/{}/learnedipprefixes", "2022-01-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.network/firewallpolicies/{}/rulecollectiongroups/{}", "2022-01-01"],
         ]
     }
 
@@ -33,7 +31,7 @@ class LearnedIpPrefix(AAZCommand):
 
     def _handler(self, command_args):
         super()._handler(command_args)
-        return self.build_lro_poller(self._execute_operations, self._output)
+        return self.build_lro_poller(self._execute_operations, None)
 
     _args_schema = None
 
@@ -46,20 +44,26 @@ class LearnedIpPrefix(AAZCommand):
         # define Arg Group ""
 
         _args_schema = cls._args_schema
-        _args_schema.name = AAZStrArg(
-            options=["-n", "--name"],
-            help="Azure Firewall name.",
+        _args_schema.policy_name = AAZStrArg(
+            options=["--policy-name"],
+            help="The name of the Firewall Policy.",
             required=True,
             id_part="name",
         )
         _args_schema.resource_group = AAZResourceGroupNameArg(
             required=True,
         )
+        _args_schema.name = AAZStrArg(
+            options=["-n", "--name"],
+            help="The name of the Firewall Policy Rule Collection Group.",
+            required=True,
+            id_part="child_name_1",
+        )
         return cls._args_schema
 
     def _execute_operations(self):
         self.pre_operations()
-        yield self.AzureFirewallsListLearnedPrefixes(ctx=self.ctx)()
+        yield self.FirewallPolicyRuleCollectionGroupsDelete(ctx=self.ctx)()
         self.post_operations()
 
     @register_callback
@@ -70,11 +74,7 @@ class LearnedIpPrefix(AAZCommand):
     def post_operations(self):
         pass
 
-    def _output(self, *args, **kwargs):
-        result = self.deserialize_output(self.ctx.vars.instance, client_flatten=True)
-        return result
-
-    class AzureFirewallsListLearnedPrefixes(AAZHttpOperation):
+    class FirewallPolicyRuleCollectionGroupsDelete(AAZHttpOperation):
         CLIENT_TYPE = "MgmtClient"
 
         def __call__(self, *args, **kwargs):
@@ -98,19 +98,28 @@ class LearnedIpPrefix(AAZCommand):
                     lro_options={"final-state-via": "location"},
                     path_format_arguments=self.url_parameters,
                 )
+            if session.http_response.status_code in [204]:
+                return self.client.build_lro_polling(
+                    self.ctx.args.no_wait,
+                    session,
+                    self.on_204,
+                    self.on_error,
+                    lro_options={"final-state-via": "location"},
+                    path_format_arguments=self.url_parameters,
+                )
 
             return self.on_error(session.http_response)
 
         @property
         def url(self):
             return self.client.format_url(
-                "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/azureFirewalls/{azureFirewallName}/learnedIPPrefixes",
+                "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/firewallPolicies/{firewallPolicyName}/ruleCollectionGroups/{ruleCollectionGroupName}",
                 **self.url_parameters
             )
 
         @property
         def method(self):
-            return "POST"
+            return "DELETE"
 
         @property
         def error_format(self):
@@ -120,11 +129,15 @@ class LearnedIpPrefix(AAZCommand):
         def url_parameters(self):
             parameters = {
                 **self.serialize_url_param(
-                    "azureFirewallName", self.ctx.args.name,
+                    "firewallPolicyName", self.ctx.args.policy_name,
                     required=True,
                 ),
                 **self.serialize_url_param(
                     "resourceGroupName", self.ctx.args.resource_group,
+                    required=True,
+                ),
+                **self.serialize_url_param(
+                    "ruleCollectionGroupName", self.ctx.args.name,
                     required=True,
                 ),
                 **self.serialize_url_param(
@@ -144,45 +157,15 @@ class LearnedIpPrefix(AAZCommand):
             }
             return parameters
 
-        @property
-        def header_parameters(self):
-            parameters = {
-                **self.serialize_header_param(
-                    "Accept", "application/json",
-                ),
-            }
-            return parameters
-
         def on_200(self, session):
-            data = self.deserialize_http_content(session)
-            self.ctx.set_var(
-                "instance",
-                data,
-                schema_builder=self._build_schema_on_200
-            )
+            pass
 
-        _schema_on_200 = None
-
-        @classmethod
-        def _build_schema_on_200(cls):
-            if cls._schema_on_200 is not None:
-                return cls._schema_on_200
-
-            cls._schema_on_200 = AAZObjectType()
-
-            _schema_on_200 = cls._schema_on_200
-            _schema_on_200.ip_prefixes = AAZListType(
-                serialized_name="ipPrefixes",
-            )
-
-            ip_prefixes = cls._schema_on_200.ip_prefixes
-            ip_prefixes.Element = AAZStrType()
-
-            return cls._schema_on_200
+        def on_204(self, session):
+            pass
 
 
-class _LearnedIpPrefixHelper:
-    """Helper class for LearnedIpPrefix"""
+class _DeleteHelper:
+    """Helper class for Delete"""
 
 
-__all__ = ["LearnedIpPrefix"]
+__all__ = ["Delete"]
