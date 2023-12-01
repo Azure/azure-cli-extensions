8cf7a072b004847b7da868312da4fd3cfec19982
Jing-song <v-jingszhang@microsoft.com>
2023-12-01 16:51:07 +0800

    [Logic] `az logic workflow`: Add new command group `identity` to support managing identity (#7029)

--- src/logic/azext_logic/aaz/latest/logic/workflow/identity/__cmd_group.py
+++ src/logic/azext_logic/aaz/latest/logic/workflow/identity/__cmd_group.py
@@ -8,9 +8,16 @@
 # pylint: skip-file
 # flake8: noqa
 
-from .__cmd_group import *
-from ._create import *
-from ._delete import *
-from ._list import *
-from ._show import *
-from ._update import *
+from azure.cli.core.aaz import *
+
+
+@register_command_group(
+    "logic workflow identity",
+)
+class __CMDGroup(AAZCommandGroup):
+    """Manage workflow identity
+    """
+    pass
+
+
+__all__ = ["__CMDGroup"]
