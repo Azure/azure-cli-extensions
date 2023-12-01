ac38606d5f0cc61a84254d34331d7d499c92f62d
Jing-song <v-jingszhang@microsoft.com>
2023-12-01 14:27:11 +0800

    Update setup.py

--- src/logic/setup.py
+++ src/logic/setup.py
@@ -34,16 +34,16 @@ with open('HISTORY.rst', 'r', encoding='utf-8') as f:
     HISTORY = f.read()
 
 setup(
-    name='logicab',
+    name='logic',
     version=VERSION,
-    description='Microsoft Azure Command-Line Tools Logicab Extension.',
+    description='Microsoft Azure Command-Line Tools Logic Extension.',
     long_description=README + '\n\n' + HISTORY,
     license='MIT',
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
-    url='https://github.com/Azure/azure-cli-extensions/tree/main/src/logicab',
+    url='https://github.com/Azure/azure-cli-extensions/tree/main/src/logic',
     classifiers=CLASSIFIERS,
     packages=find_packages(exclude=["tests"]),
-    package_data={'azext_logicab': ['azext_metadata.json']},
+    package_data={'azext_logic': ['azext_metadata.json']},
     install_requires=DEPENDENCIES
 )
