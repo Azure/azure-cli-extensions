7b7fd6b870dc005c678efb4c7ce71140995ae40f
Radhika <radhikajain@microsoft.com>
2023-12-08 18:58:26 +0530

    Update

--- src/load/azext_load/data_plane/utils/utils.py
+++ src/load/azext_load/data_plane/utils/utils.py
@@ -423,6 +423,7 @@ def create_or_update_test_with_config(
     new_body["loadTestConfiguration"]["quickStartTest"] = False
 
     new_body["passFailCriteria"] = {}
+    
     for key in body.get("passFailCriteria", {}):
         new_body["passFailCriteria"][key] = None
     if yaml_test_body.get("passFailCriteria") is not None:
