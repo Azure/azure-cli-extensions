71b9b3ec57aa594c59d57a662d72bc565f617153
ZelinWang <zelinwang@microsoft.com>
2023-12-13 11:15:10 +0800

    update

--- scripts/ci/azdev_linter_style.py
+++ scripts/ci/azdev_linter_style.py
@@ -9,17 +9,18 @@ This script is used to run azdev linter and azdev style on extensions.
 It's only working on ADO by default. If want to run locally,
 please update the target branch/commit to find diff in function find_modified_files_against_master_branch()
 """
-
-import os
 import json
-from subprocess import check_output, check_call
-from pkg_resources import parse_version
+import logging
+import os
+from subprocess import check_call, check_output
 
 import service_name
+from pkg_resources import parse_version
 
+logger = logging.getLogger(__name__)
 
 def separator_line():
-    print('-' * 100)
+    logger.info('-' * 100)
 
 
 class ModifiedFilesNotAllowedError(Exception):
@@ -50,7 +51,7 @@ class AzExtensionHelper:
 
     @staticmethod
     def _cmd(cmd):
-        print(cmd)
+        logger.info(cmd)
         check_call(cmd, shell=True)
 
     def add_from_url(self, url):
@@ -66,7 +67,7 @@ class AzdevExtensionHelper:
 
     @staticmethod
     def _cmd(cmd):
-        print(cmd)
+        logger.info(cmd)
         check_call(cmd, shell=True)
 
     def add_from_code(self):
@@ -94,17 +95,17 @@ def find_modified_files_against_master_branch():
     ado_pr_target_branch = 'origin/' + os.environ.get('ADO_PULL_REQUEST_TARGET_BRANCH')
 
     separator_line()
-    print('pull request target branch:', ado_pr_target_branch)
+    logger.info('pull request target branch:', ado_pr_target_branch)
 
     cmd = 'git --no-pager diff --name-only --diff-filter=ACMRT {} -- src/'.format(ado_pr_target_branch)
     files = check_output(cmd.split()).decode('utf-8').split('\n')
     files = [f for f in files if len(f) > 0]
 
     if files:
-        print('modified files:')
+        logger.info('modified files:')
         separator_line()
         for f in files:
-            print(f)
+            logger.info(f)
 
     return files
 
@@ -170,7 +171,7 @@ def azdev_on_external_extension(index_json, azdev_type):
         # azdev test support external extension
         # azdev_extension.style()
 
-        print('Checking service name for external extensions')
+        logger.info('Checking service name for external extensions')
         service_name.check()
 
         az_extension.remove()
@@ -186,7 +187,7 @@ def azdev_on_internal_extension(modified_files, azdev_type):
 
     if not extension_names:
         separator_line()
-        print('no extension source code modified, no extension needs to be checked')
+        logger.info('no extension source code modified, no extension needs to be checked')
 
     for name in extension_names:
         separator_line()
@@ -198,7 +199,7 @@ def azdev_on_internal_extension(modified_files, azdev_type):
         if azdev_type in ['all', 'style']:
             azdev_extension.style()
 
-        print('Checking service name for internal extensions')
+        logger.info('Checking service name for internal extensions')
         service_name.check()
 
         azdev_extension.remove()
@@ -213,7 +214,7 @@ def main():
                              'Supported values: linter, style, test, all, all is the default.', default='all')
     args = parser.parse_args()
     azdev_type = args.type
-    print('azdev type:', azdev_type)
+    logger.info('azdev type:', azdev_type)
     modified_files = find_modified_files_against_master_branch()
 
     if len(modified_files) == 1 and contain_index_json(modified_files):
@@ -234,7 +235,7 @@ def main():
             azdev_on_internal_extension(modified_files, azdev_type)
         else:
             separator_line()
-            print('no extension source code modified, no extension needs to be checked')
+            logger.info('no extension source code modified, no extension needs to be checked')
             separator_line()
 
 
@@ -242,5 +243,5 @@ if __name__ == '__main__':
     try:
         main()
     except ModifiedFilesNotAllowedError as e:
-        print(e)
+        logger.error(e)
         exit(1)
