d88fdb0b2e772e460cc6e56935db178a8fbd054b
Jingshu918 <138486531+Jingshu918@users.noreply.github.com>
2023-12-18 14:46:13 +0800

    Support --self-contained-interactive-authoring-enabled in integration-runtime self-hosted create (#7084)
    * Support --self-contained-interactive-authoring-enabled in integration-runtime self-hosted create
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * change
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix version

--- src/datafactory/azext_datafactory/tests/__init__.py
+++ src/datafactory/azext_datafactory/tests/__init__.py
@@ -40,7 +40,7 @@ def try_manual(func):
         module_path = __path__[0].lower()
         if not decorated_path.startswith(module_path):
             raise Exception("Decorator can only be used in submodules!")
-        manual_path = os.path.join(decorated_path[module_path.rfind(os.path.sep) + 1 :])
+        manual_path = os.path.join(decorated_path[module_path.rfind(os.path.sep) + 1:])
         manual_file_path, manual_file_name = os.path.split(manual_path)
         module_name, _ = os.path.splitext(manual_file_name)
         manual_module = "..manual." + ".".join(
