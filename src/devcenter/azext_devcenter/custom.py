2657a392d3910e9be12e0ca5753e6f0257374571
Amanda Lim <amlim@microsoft.com>
2023-12-19 13:02:26 -0800

    Fix environment def example and update style

--- src/devcenter/azext_devcenter/custom.py
+++ src/devcenter/azext_devcenter/custom.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 # pylint: disable=protected-access
+# pylint: disable=too-many-lines
 
 from datetime import datetime
 import json
