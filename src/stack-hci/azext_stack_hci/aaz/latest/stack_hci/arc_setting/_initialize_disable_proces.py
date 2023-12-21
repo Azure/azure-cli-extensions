342046d69e009452334e2911858edba838cb6c29
Jing-song <v-jingszhang@microsoft.com>
2023-12-21 12:44:03 +0800

    upgrade api (#7082)

--- src/stack-hci/azext_stack_hci/aaz/latest/stack_hci/arc_setting/_initialize_disable_proces.py
+++ src/stack-hci/azext_stack_hci/aaz/latest/stack_hci/arc_setting/_initialize_disable_proces.py
@@ -22,9 +22,9 @@ class InitializeDisableProces(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2023-03-01",
+        "version": "2023-08-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.azurestackhci/clusters/{}/arcsettings/{}/initializedisableprocess", "2023-03-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.azurestackhci/clusters/{}/arcsettings/{}/initializedisableprocess", "2023-08-01"],
         ]
     }
 
@@ -143,7 +143,7 @@ class InitializeDisableProces(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2023-03-01",
+                    "api-version", "2023-08-01",
                     required=True,
                 ),
             }
