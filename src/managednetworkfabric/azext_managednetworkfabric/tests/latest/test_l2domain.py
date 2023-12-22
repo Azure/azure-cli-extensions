0da42a83a6cf8b7b2f24c337fb422fc9a286a6c1
aytripathi <119933278+aytripathi@users.noreply.github.com>
2023-12-22 10:46:42 +0530

    Added extension support for NNF 4.2 release (#7095)
    * Added extension support for NNF 4.2 release
    
    * added whitespaces in around arithmetic operator
    
    * Added new line at end of file
    
    * Fixed flake8 linting issues
    
    * Removed test_managednetworkfabric.py file as it was not in use
    
    ---------
    
    Co-authored-by: aytripathi <aytripathi@DESKTOP-DN8JFGH>

--- src/managednetworkfabric/azext_managednetworkfabric/tests/latest/test_l2domain.py
+++ src/managednetworkfabric/azext_managednetworkfabric/tests/latest/test_l2domain.py
@@ -12,13 +12,16 @@ L2 Domain tests scenarios
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
 from .config import CONFIG
 
+
 def setup_scenario1(test):
     ''' Env setup_scenario1 '''
     pass
 
+
 def cleanup_scenario1(test):
     '''Env cleanup_scenario1 '''
     pass
+
 
 def call_scenario1(test):
     ''' # Testcase: scenario1'''
@@ -33,11 +36,14 @@ def call_scenario1(test):
     step_delete(test, checks=[])
     cleanup_scenario1(test)
 
+
 def step_create(test, checks=None):
     '''l2domain create operation'''
     if checks is None:
         checks = []
-    test.cmd('az networkfabric l2domain create --resource-group {rg} --resource-name {name} --location {location} --nf-id {nf_id} --vlan-id {vlan_id} --mtu {mtu}', checks=checks)
+    test.cmd(
+        'az networkfabric l2domain create --resource-group {rg} --resource-name {name} --location {location} --nf-id {nf_id} --vlan-id {vlan_id} --mtu {mtu}', checks=checks)
+
 
 def step_show(test, checks=None):
     '''l2domain show operation'''
@@ -46,11 +52,14 @@ def step_show(test, checks=None):
     test.cmd(
         'az networkfabric l2domain show --resource-name {name} --resource-group {rg}')
 
+
 def step_update(test, checks=None):
     '''l2domain update operation'''
     if checks is None:
         checks = []
-    test.cmd('az networkfabric l2domain update --resource-group {rg} --resource-name {name} --mtu {updated_mtu}', checks=checks)
+    test.cmd(
+        'az networkfabric l2domain update --resource-group {rg} --resource-name {name} --mtu {updated_mtu}', checks=checks)
+
 
 def step_list_resource_group(test, checks=None):
     '''l2domain list by resource group operation'''
@@ -58,23 +67,29 @@ def step_list_resource_group(test, checks=None):
         checks = []
     test.cmd('az networkfabric l2domain list --resource-group {rg}')
 
+
 def step_list_subscription(test, checks=None):
     '''l2domain list by subscription operation'''
     if checks is None:
         checks = []
     test.cmd('az networkfabric l2domain list')
 
+
 def step_update_admin_state_Enable(test, checks=None):
     '''l2domain Update admin state operation'''
     if checks is None:
         checks = []
-    test.cmd('az networkfabric l2domain update-admin-state --resource-group {rg} --resource-name {name} --state {state_Enable}')
+    test.cmd(
+        'az networkfabric l2domain update-admin-state --resource-group {rg} --resource-name {name} --state {state_Enable}')
+
 
 def step_update_admin_state_Disable(test, checks=None):
     '''l2domain Update admin state operation'''
     if checks is None:
         checks = []
-    test.cmd('az networkfabric l2domain update-admin-state --resource-group {rg} --resource-name {name} --state {state_Disable}')
+    test.cmd(
+        'az networkfabric l2domain update-admin-state --resource-group {rg} --resource-name {name} --state {state_Disable}')
+
 
 def step_delete(test, checks=None):
     '''l2domain delete operation'''
@@ -82,6 +97,7 @@ def step_delete(test, checks=None):
         checks = []
     test.cmd(
         'az networkfabric l2domain delete --resource-name {name} --resource-group {rg}')
+
 
 class GA_L2DomainScenarioTest1(ScenarioTest):
     ''' L2 Domain Scenario test'''
