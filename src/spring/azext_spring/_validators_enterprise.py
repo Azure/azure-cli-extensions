c00dd3ff16003af14f33d6748081c3f90dcaa773
Moary Chen <v-shilichen@microsoft.com>
2023-12-25 17:23:07 +0800

    [Spring] Add new params to support auto binding (#7098)

--- src/spring/azext_spring/_validators_enterprise.py
+++ src/spring/azext_spring/_validators_enterprise.py
@@ -657,3 +657,39 @@ def validate_build_cert_reference(cmd, namespace):
     get_cert_resource_id(cert_names, cmd, namespace, result)
 
     namespace.build_certificates = result
+
+
+def validate_create_app_binding_default_service_registry(cmd, namespace):
+    if namespace.bind_service_registry:
+        namespace.bind_service_registry = _get_eactly_one_service_registry_resource_id(cmd,
+                                                                                       namespace.resource_group,
+                                                                                       namespace.service)
+
+
+def _get_eactly_one_service_registry_resource_id(cmd, resource_group, service):
+    client = get_client(cmd)
+    service_registry_resources = list(client.service_registries.list(resource_group, service))
+    if len(service_registry_resources) == 0:
+        raise ClientRequestError('App cannot bind to service registry because it is not configured.')
+    if len(service_registry_resources) > 1:
+        raise ClientRequestError('App cannot bind to multiple service registries.')
+    return service_registry_resources[0].id
+
+
+def validate_create_app_binding_default_application_configuration_service(cmd, namespace):
+    if namespace.bind_application_configuration_service:
+        namespace.bind_application_configuration_service \
+            = _get_eactly_one_application_configuration_service_resource_id(cmd,
+                                                                            namespace.resource_group,
+                                                                            namespace.service)
+
+
+def _get_eactly_one_application_configuration_service_resource_id(cmd, resource_group, service):
+    client = get_client(cmd)
+    acs_resources = list(client.configuration_services.list(resource_group, service))
+    if len(acs_resources) == 0:
+        raise ClientRequestError('App cannot bind to application configuration service '
+                                 'because it is not configured.')
+    if len(acs_resources) > 1:
+        raise ClientRequestError('App cannot bind to multiple application configuration services.')
+    return acs_resources[0].id
