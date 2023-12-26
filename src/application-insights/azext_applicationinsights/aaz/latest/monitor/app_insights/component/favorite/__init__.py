8459911f6b5318c607cbfaa3a561871ac1b1d206
Jing-song <v-jingszhang@microsoft.com>
2023-12-26 14:39:09 +0800

    [Application-insights] `az monitor app-insights component`: Add new command group `favorite` to support managing favorite (#7108)
    * app-insights

--- src/application-insights/azext_applicationinsights/aaz/latest/monitor/app_insights/component/favorite/__init__.py
+++ src/application-insights/azext_applicationinsights/aaz/latest/monitor/app_insights/component/favorite/__init__.py
@@ -9,4 +9,8 @@
 # flake8: noqa
 
 from .__cmd_group import *
-from ._migrate_to_new_pricing_model import *
+from ._create import *
+from ._delete import *
+from ._list import *
+from ._show import *
+from ._update import *
