9c4a7a3c60c70f1ad87dfb59a97b7d3f677ab846
AllyWang <liwang1025@gmail.com>
2023-12-27 15:25:54 +0800

    fix style

--- src/monitor-control-service/azext_amcs/manual/custom.py
+++ src/monitor-control-service/azext_amcs/manual/custom.py
@@ -3,7 +3,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
+# pylint: disable=raise-missing-from, consider-using-f-string
 
 def _data_collection_rules_create(client,
                                   resource_group_name,
