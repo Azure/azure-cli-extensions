bdc8040295c2070c53f72568ad5ee7f52d6d4ce5
Amit Brahma <ambrahma@microsoft.com>
2024-01-03 12:15:16 +0530

    Upgrade mycli and pgcli dependencies to latest versions

--- src/rdbms-connect/setup.py
+++ src/rdbms-connect/setup.py
@@ -35,8 +35,8 @@ CLASSIFIERS = [
 DEPENDENCIES = [
     'setproctitle~=1.3.3',
     'psycopg2==2.9.3',
-    'mycli==1.26.1',
-    'pgcli==3.4.1',
+    'mycli==1.27.0',
+    'pgcli==4.0.1',
     'cryptography~=38.0.0'
 ]
 
