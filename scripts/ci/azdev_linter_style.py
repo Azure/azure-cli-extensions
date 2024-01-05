4f721e32a3f55ca08e2cf7898c82cf422ada1146
ZelinWang <zelinwang@microsoft.com>
2024-01-05 11:25:56 +0800

    Update azdev_linter_style.py

--- scripts/ci/azdev_linter_style.py
+++ scripts/ci/azdev_linter_style.py
@@ -12,6 +12,7 @@ please update the target branch/commit to find diff in function find_modified_fi
 import json
 import logging
 import os
+import re
 import shutil
 from subprocess import CalledProcessError, check_call, check_output
 
@@ -92,25 +93,28 @@ class AzdevExtensionHelper:
         self._cmd('azdev extension build {}'.format(self.extension_name))
 
     def check_extension_name(self):
+        extension_root_dir_name = self.extension_name
         original_cwd = os.getcwd()
         dist_dir = os.path.join(original_cwd, 'dist')
         files = os.listdir(dist_dir)
         logger.info(f"wheel files in the dist directory: {files}")
-        logger.info(f"extension name is: {self.extension_name}")
         for f in files:
             if f.endswith('.whl'):
+                NAME_REGEX = r'(.*)-\d+.\d+.\d+'
+                extension_name = re.findall(NAME_REGEX, f)[0]
+                extension_name = extension_name.replace('_', '-')
+                logger.info(f"extension name is: {extension_name}")
                 ext_file = os.path.join(dist_dir, f)
                 break
-        metadata = get_ext_metadata(dist_dir, ext_file, self.extension_name)
+        metadata = get_ext_metadata(dist_dir, ext_file, extension_name)
         logger.info(f"metadata in the wheel file is: {metadata}")
-        logger.info(f"metadata name in setup.py is: {metadata['name']}")
         shutil.rmtree(dist_dir)
-        if '_' in self.extension_name:
+        if '_' in extension_root_dir_name:
             raise ValueError(f"Underscores `_` are not allowed in the extension root directory, "
                              f"please change it to a hyphen `-`.")
-        if metadata['name'] != self.extension_name:
+        if metadata['name'] != extension_name:
             raise ValueError(f"The name {metadata['name']} in setup.py "
-                             f"is not the same as the extension name {self.extension_name}! \n"
+                             f"is not the same as the extension name {extension_name}! \n"
                              f"Please fix the name in setup.py!")
 
 
