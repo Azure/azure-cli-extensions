469c5da246f0197889e9d035afca009a636cbc43
Jing-song <v-jingszhang@microsoft.com>
2024-01-19 14:47:54 +0800

    workbook (#7141)

--- src/application-insights/azext_applicationinsights/aaz/latest/monitor/app_insights/workbook/identity/__cmd_group.py
+++ src/application-insights/azext_applicationinsights/aaz/latest/monitor/app_insights/workbook/identity/__cmd_group.py
@@ -8,9 +8,16 @@
 # pylint: skip-file
 # flake8: noqa
 
-from .__cmd_group import *
-from ._create import *
-from ._delete import *
-from ._list import *
-from ._show import *
-from ._update import *
+from azure.cli.core.aaz import *
+
+
+@register_command_group(
+    "monitor app-insights workbook identity",
+)
+class __CMDGroup(AAZCommandGroup):
+    """Manage workbook idneity
+    """
+    pass
+
+
+__all__ = ["__CMDGroup"]
