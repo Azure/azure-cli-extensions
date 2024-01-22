2c695cca1956c31509a77a7facc9b45f8a493940
Michael Dai <michaelkira@live.cn>
2024-01-22 17:02:51 +0800

    update

--- src/containerapp/azext_containerapp/tests/latest/test_containerapp_env_telemetry.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerapp_env_telemetry.py
@@ -19,7 +19,7 @@ TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 class ContainerappEnvTelemetryScenarioTest(ScenarioTest):
     @AllowLargeResponse(8192)
     @ResourceGroupPreparer(location="northeurope")
-    def test_containerapp_env_telemetry(self, resource_group):
+    def test_containerapp_env_telemetry_e2e(self, resource_group):
         self.cmd('configure --defaults location={}'.format(TEST_LOCATION))
 
         env_name = self.create_random_name(prefix='containerapp-env', length=24)
@@ -29,6 +29,17 @@ class ContainerappEnvTelemetryScenarioTest(ScenarioTest):
         traces_destinations = "appInsights"
         logs_destinations = "appInsights"
         metrics_destinations = "dataDog"
+
+        self.cmd('containerapp env create -g {} -n {}'.format(resource_group, env_name))
+
+        while containerapp_env["properties"]["provisioningState"].lower() == "waiting":
+            time.sleep(5)
+            containerapp_env = self.cmd('containerapp env show -g {} -n {}'.format(resource_group, env_name)).get_output_in_json()
+
+        self.cmd('containerapp env list -g {}'.format(resource_group), checks=[
+            JMESPathCheck('length(@)', 1),
+            JMESPathCheck('[0].name', env_name),
+        ])
 
         self.cmd(f'containerapp env telemetry set -g {resource_group} -n {env_name} --app-insights-connection-string {ai_conn_str} --open-telemetry-dataDog-site {data_dog_site} --open-telemetry-dataDog-key {data_dog_key} --open-telemetry-traces-destinations {traces_destinations} --open-telemetry-logs-destinations {logs_destinations} --open-telemetry-metrics-destinations {metrics_destinations}', checks=[
             JMESPathCheck('name', env_name),
