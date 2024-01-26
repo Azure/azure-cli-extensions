30525f9e448344bdd09f35f577576967612bd04a
Aleena Brown (from Dev Box) <aleenabrown@microsoft.com>
2024-01-26 15:14:54 -0800

    Add example for Create a ticket for Generic Quota increase for any Azure Service.

--- src/support/azext_support/aaz/latest/support/in_subscription/tickets/_create.py
+++ src/support/azext_support/aaz/latest/support/in_subscription/tickets/_create.py
@@ -28,6 +28,9 @@ class Create(AAZCommand):
 
     :example: Create a ticket to request Quota increase for Compute VM Cores.
         az support in-subscription tickets create --contact-country "USA" --contact-email "abc@contoso.com" --contact-first-name "Foo" --contact-language "en-US" --contact-last-name "Bar" --contact-method "email" --contact-timezone "Pacific Standard Time" --description "QuotaTicketDescription" --diagnostic-consent "Yes" --problem-classification "/providers/Microsoft.Support/services/QuotaServiceNameGuid/problemClassifications/CoresQuotaProblemClassificationNameGuid" --severity "minimal" --ticket-name "QuotaTestTicketName" --title "QuotaTicketTitle"  --quota-change-version "1.0" --quota-change-requests [0].region="EASTUS" --quota-change-requests [0].payload="'{\`"VMFamily\`":\`"DSv3 Series\`",\`"NewLimit\`":\`"110\`"}'"
+
+    :example: Create a ticket for Generic Quota increase for any Azure Service.
+        az support in-subscription tickets create --contact-country "USA" --contact-email "abc@contoso.com" --contact-first-name "Foo" --contact-language "en-US" --contact-last-name "Bar" --contact-method "email" --contact-timezone "Pacific Standard Time" --description "QuotaTicketDescription" --diagnostic-consent "Yes" --problem-classification "/providers/Microsoft.Support/services/QuotaServiceNameGuid/problemClassifications/GenericProblemClassificationNameGuid" --severity "minimal" --ticket-name "QuotaTestTicketName" --title "QuotaTicketTitle"
     """
 
     _aaz_info = {
