34a967c52ca684bdaf838331b7e698312eb0c940
yilims <brightmonkeyli@163.com>
2024-01-26 12:49:11 +0800

    refine test case

--- src/containerapp/azext_containerapp/tests/latest/test_containerapp_create_update_up_java.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerapp_create_update_up_java.py
@@ -15,7 +15,6 @@ TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
 class ContainerAppCreateTest(ScenarioTest):
 
-    
     @ResourceGroupPreparer(location="eastus")
     def test_containerapp_up_artifact_with_buildpack_java(self, resource_group):
         artifact_path = os.path.join(TEST_DIR, os.path.join("data", "artifact_built_using_buildpack", "sample.jar"))
@@ -26,7 +25,16 @@ class ContainerAppCreateTest(ScenarioTest):
                                           build_env_vars=build_env_vars, ingress=ingress, target_port=target_port,
                                           requires_acr_prerequisite=True)
 
-    
+    @ResourceGroupPreparer(location="eastus")
+    def test_containerapp_up_source_with_buildpack_java(self, resource_group):
+        source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack_java"))
+        ingress = 'external'
+        target_port = '8080'
+        build_env_vars = 'BP_JVM_VERSION=21 BP_MAVEN_VERSION=4 "BP_MAVEN_BUILD_ARGUMENTS=-Dmaven.test.skip=true --no-transfer-progress package"'
+        create_and_verify_containerapp_up(self, resource_group=resource_group, source_path=source_path,
+                                          build_env_vars=build_env_vars, ingress=ingress, target_port=target_port)
+
+    @live_only()
     @ResourceGroupPreparer(location="eastus")
     def test_containerapp_create_artifact_with_buildpack_java(self, resource_group):
         artifact_path = os.path.join(TEST_DIR, os.path.join("data", "artifact_built_using_buildpack", "sample.jar"))
@@ -37,7 +45,7 @@ class ContainerAppCreateTest(ScenarioTest):
                                                          artifact_path=artifact_path, build_env_vars=build_env_vars,
                                                          ingress=ingress, target_port=target_port)
 
-    
+    @live_only()
     @ResourceGroupPreparer(location="eastus")
     def test_containerapp_create_source_with_buildpack_java(self, resource_group):
         source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack_java"))
@@ -47,13 +55,3 @@ class ContainerAppCreateTest(ScenarioTest):
         create_and_verify_containerapp_create_and_update(self, resource_group=resource_group, source_path=source_path,
                                                          build_env_vars=build_env_vars, ingress=ingress,
                                                          target_port=target_port)
-
-    
-    @ResourceGroupPreparer(location="eastus")
-    def test_containerapp_up_source_with_buildpack_java(self, resource_group):
-        source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack_java"))
-        ingress = 'external'
-        target_port = '8080'
-        build_env_vars = 'BP_JVM_VERSION=21 BP_MAVEN_VERSION=4 "BP_MAVEN_BUILD_ARGUMENTS=-Dmaven.test.skip=true --no-transfer-progress package"'
-        create_and_verify_containerapp_up(self, resource_group=resource_group, source_path=source_path,
-                                          build_env_vars=build_env_vars, ingress=ingress, target_port=target_port)
