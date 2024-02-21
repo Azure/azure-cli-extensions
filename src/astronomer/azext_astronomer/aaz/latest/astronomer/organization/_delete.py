c2a17831e0505b07db7710182304ea498ffd3d2b
Zeng Taoxu <v-taoxuzeng@microsoft.com>
2024-02-21 14:18:18 +0800

    update

--- src/astronomer/azext_astronomer/aaz/latest/astronomer/organization/_delete.py
+++ src/astronomer/azext_astronomer/aaz/latest/astronomer/organization/_delete.py
@@ -85,7 +85,16 @@ class Delete(AAZCommand):
                 return self.client.build_lro_polling(
                     self.ctx.args.no_wait,
                     session,
-                    None,
+                    self.on_200,
+                    self.on_error,
+                    lro_options={"final-state-via": "location"},
+                    path_format_arguments=self.url_parameters,
+                )
+            if session.http_response.status_code in [200]:
+                return self.client.build_lro_polling(
+                    self.ctx.args.no_wait,
+                    session,
+                    self.on_200,
                     self.on_error,
                     lro_options={"final-state-via": "location"},
                     path_format_arguments=self.url_parameters,
@@ -144,6 +153,9 @@ class Delete(AAZCommand):
                 ),
             }
             return parameters
+
+        def on_200(self, session):
+            pass
 
         def on_204(self, session):
             pass
