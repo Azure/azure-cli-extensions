00b84a75fb338a462b407435548dc927d754a508
snehapar <snehapar@microsoft.com>
2024-02-21 23:23:34 -0800

    Add live attribute to test

--- src/containerapp/azext_containerapp/tests/latest/test_containerapp_up.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerapp_up.py
@@ -66,6 +66,9 @@ class ContainerAppUpImageTest(ScenarioTest):
         target_port = '8080'
         create_and_verify_containerapp_up_with_multiple_environments(self, resource_group=resource_group, source_path=source_path, ingress=ingress, target_port=target_port, location="eastus2")
 
+    # We have to use @live_only() here as cloud builder and build resource name is generated randomly
+    # and no matched request could be found for all builder/build ARM requests
+    @live_only()
     @ResourceGroupPreparer(location="eastus2")
     def test_containerapp_up_source_with_default_registry_image(self, resource_group):
         source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_source_to_cloud"))
