89bdfebd4561d4fb40899736dd90e22a37beee46
Haitao Chen <haitch@microsoft.com>
2024-02-22 23:49:15 -0800

    display supportPlan for get-versions command

--- src/aks-preview/azext_aks_preview/_format.py
+++ src/aks-preview/azext_aks_preview/_format.py
@@ -135,29 +135,6 @@ def aks_upgrades_table_format(result):
     # use ordered dicts so headers are predictable
     return parsed.search(result, Options(dict_cls=OrderedDict, custom_functions=_custom_functions(preview)))
 
-
-def aks_versions_table_format(result):
-    """Format get-versions results as a summary for display with "-o table"."""
-
-    # get preview orchestrator version
-    preview = {}
-
-    def find_preview_versions():
-        for orchestrator in result.get('orchestrators', []):
-            if orchestrator.get('isPreview', False):
-                preview[orchestrator['orchestratorVersion']] = True
-    find_preview_versions()
-
-    parsed = compile_jmes("""orchestrators[].{
-        kubernetesVersion: orchestratorVersion | set_preview(@),
-        upgrades: upgrades[].orchestratorVersion || [`None available`] | sort_versions(@) | set_preview_array(@) | join(`, `, @)
-    }""")
-    # use ordered dicts so headers are predictable
-    results = parsed.search(result, Options(
-        dict_cls=OrderedDict, custom_functions=_custom_functions(preview)))
-    return sorted(results, key=lambda x: version_to_tuple(x.get('kubernetesVersion')), reverse=True)
-
-
 def version_to_tuple(version):
     """Removes preview suffix"""
     if version.endswith('(preview)'):
@@ -171,8 +148,9 @@ def flatten_version_table(release_info):
     flattened = []
     for release in release_info:
         isPreview = release.get("isPreview", False)
+        supportPlan = release.get("capabilities", {}).get("supportPlan", {})
         for k, v in release.get("patchVersions", {}).items():
-            item = {"version": k, "upgrades": v.get("upgrades", []), "isPreview": isPreview}
+            item = {"version": k, "upgrades": v.get("upgrades", []), "isPreview": isPreview, "supportPlan": supportPlan}
             flattened.append(item)
     return flattened
 
@@ -256,7 +234,8 @@ def aks_versions_table_format(result):
     parsed = compile_jmes("""[].{
         kubernetesVersion: version,
         isPreview: isPreview,
-        upgrades: upgrades || [`None available`] | sort_versions(@) | join(`, `, @)
+        upgrades: upgrades || [`None available`] | sort_versions(@) | join(`, `, @),
+        supportPlan: supportPlan | join(`, `, @)
     }""")
     # use ordered dicts so headers are predictable
     results = parsed.search(version_table, Options(
