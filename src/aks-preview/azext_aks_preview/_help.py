39ad6cbfc30c658395c78664bbd2a39a45ca59bc
Ashish Nair <nairashu@gmail.com>
2024-02-28 20:09:55 -0800

    Update src/aks-preview/azext_aks_preview/_help.py to use first-person voice.
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- src/aks-preview/azext_aks_preview/_help.py
+++ src/aks-preview/azext_aks_preview/_help.py
@@ -594,7 +594,7 @@ helps['aks create'] = f"""
           short-summary: Configure SSH setting for all node pools in this cluster. Use "disabled" to disable SSH access, "localuser" to enable SSH access using private key.
         - name: --pod-ip-allocation-mode
           type: string
-          short-summary: Sets the ip allocation mode for how Pod IPs from the Azure Pod Subnet are allocated to the nodes in the AKS cluster. The choice is between dynamic batches of individual IPs or static allocation of a set of CIDR blocks. Accepted Values are "DynamicIndividual" or "StaticBlock".
+          short-summary: Set the ip allocation mode for how Pod IPs from the Azure Pod Subnet are allocated to the nodes in the AKS cluster. The choice is between dynamic batches of individual IPs or static allocation of a set of CIDR blocks. Accepted Values are "DynamicIndividual" or "StaticBlock".
           long-summary: |
               Used together with the "azure" network plugin.
               Requires --pod-subnet-id.
