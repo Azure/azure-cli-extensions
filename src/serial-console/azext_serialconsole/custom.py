50e6303c385e19ed43752e2976b1bbe2c33537ad
Dominika Bobik <dbobik@microsoft.com>
2024-03-05 17:15:41 -0800

    auth flow changes

--- src/serial-console/azext_serialconsole/custom.py
+++ src/serial-console/azext_serialconsole/custom.py
@@ -37,6 +37,7 @@ class GlobalVariables:
         self.block_print = False
         self.trycount = 0
         self.os_is_windows = False
+        self.new_auth_flow = "0"
 
 
 class PrintClass:
@@ -428,8 +429,9 @@ class SerialConsole:
 
     def connect(self):
         def on_open(_):
-            pass
-
+            if (self.new_auth_flow == "1"):
+                GV.websocket_instance.send(self.access_token);
+    
         def on_message(_, message):
             if GV.first_message:
                 PC.clear_screen()
@@ -455,7 +457,7 @@ class SerialConsole:
         def connect_thread():
             if self.load_websocket_url():
                 GV.websocket_instance = websocket.WebSocketApp(
-                    self.websocket_url + "?authorization=" + self.access_token,
+                    self.websocket_url + "?authorization=" + self.access_token + "?new=" + self.new_auth_flow,
                     on_open=on_open,
                     on_message=on_message,
                     on_error=on_error,
@@ -560,7 +562,7 @@ class SerialConsole:
                     ws.close()
 
             wsapp = websocket.WebSocketApp(
-                self.websocket_url + "?authorization=" + self.access_token + "?new=0", on_message=on_message)
+                self.websocket_url + "?authorization=" + self.access_token + "?new=" + self.new_auth_flow, on_message=on_message)
             wsapp.run_forever()
             GV.loading = False
             if GV.trycount == 0:
