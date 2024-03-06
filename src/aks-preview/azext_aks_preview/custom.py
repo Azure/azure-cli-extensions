af2859897962eb2e10d47645273757baa3735ce7
Devesh Dama <ddama@microsoft.com>
2024-03-05 16:04:44 -0800

    fix typos

--- src/aks-preview/azext_aks_preview/custom.py
+++ src/aks-preview/azext_aks_preview/custom.py
@@ -2967,12 +2967,10 @@ def aks_mesh_get_upgrades(
     upgradeProfiles = client.list_mesh_upgrade_profiles(resource_group_name, name)
     is_empty, upgradeProfiles = check_iterator(upgradeProfiles)
     if is_empty:
-        logger.warning("No mesh upgrade profiles found for the cluster '%s'" +
+        logger.warning("No mesh upgrade profiles found for the cluster '%s' " +
                        "in the resource group '%s'.", name, resource_group_name)
         return None
-    # 'upgradeProfiles' is an ItemPaged object
     upgrades = []
-    # Iterate over items within pages
     for item in upgradeProfiles:
         upgrades.append(item.properties)
 
