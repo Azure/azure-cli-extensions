b66dd8da736d211b90ad310e3c37f762d6e2b1d7
yilims <brightmonkeyli@163.com>
2024-03-26 16:50:04 +0800

    code refactor

--- src/containerapp/azext_containerapp/tests/latest/test_containerapp_create_update_up_java.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerapp_create_update_up_java.py
@@ -17,33 +17,36 @@ from azext_containerapp.tests.latest.utils import create_and_verify_containerapp
 TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
 
+def download_java_source(source_path):
+    response = requests.get("https://api.github.com/repos/Azure/java-buildpack-e2e-test/releases/tags/v1.0.6")
+    response.raise_for_status()
+    data = response.json()
+    if data['assets']:
+        for asset in data['assets']:
+            if asset['name'] == 'java-source-spring.tar.gz':
+                source_url = asset['browser_download_url']
+                response = requests.get(source_url, stream=True)
+                response.raise_for_status()
+
+                with tempfile.NamedTemporaryFile(suffix=".tar.gz", delete=False) as temp_file:
+                    for chunk in response.iter_content(chunk_size=8192):
+                        if chunk:
+                            temp_file.write(chunk)
+
+                if os.path.exists(source_path):
+                    shutil.rmtree(source_path)
+
+                with tarfile.open(temp_file.name, 'r:gz') as tar:
+                    tar.extractall(path=source_path)
+
+                os.remove(temp_file.name)
+
+
+def cleanup(source_path):
+    shutil.rmtree(source_path)
+
+
 class ContainerAppCreateTest(ScenarioTest):
-    @classmethod
-    def setUpClass(cls):
-        super(ContainerAppCreateTest, cls).setUpClass()
-        response = requests.get("https://api.github.com/repos/Azure/java-buildpack-e2e-test/releases/tags/v1.0.6")
-        response.raise_for_status()
-        data = response.json()
-        if data['assets']:
-            for asset in data['assets']:
-                if asset['name'] == 'java-source-spring.tar.gz':
-                    source_url = asset['browser_download_url']
-                    response = requests.get(source_url, stream=True)
-                    response.raise_for_status()
-
-                    with tempfile.NamedTemporaryFile(suffix=".tar.gz", delete=False) as temp_file:
-                        for chunk in response.iter_content(chunk_size=8192):
-                            if chunk:
-                                temp_file.write(chunk)
-
-                    source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack_java"))
-                    if os.path.exists(source_path):
-                        shutil.rmtree(source_path)
-
-                    with tarfile.open(temp_file.name, 'r:gz') as tar:
-                        tar.extractall(path=source_path)
-
-                    os.remove(temp_file.name)
 
     # We have to use @live_only() here as cloud builder and build resource name is generated randomly
     # and no matched request could be found for all builder/build ARM requests.
@@ -61,13 +64,17 @@ class ContainerAppCreateTest(ScenarioTest):
     @live_only()
     @ResourceGroupPreparer(location="eastus")
     def test_containerapp_up_source_with_buildpack_java(self, resource_group):
-        source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack_java"))
+        source_path = os.path.join(TEST_DIR, os.path.join("data", "up_source_with_buildpack_java"))
+        download_java_source(source_path)
         ingress = 'external'
         target_port = '8080'
         build_env_vars = 'BP_JVM_VERSION=21 BP_MAVEN_VERSION=4 "BP_MAVEN_BUILD_ARGUMENTS=-Dmaven.test.skip=true --no-transfer-progress package"'
-        create_and_verify_containerapp_up(self, resource_group=resource_group, source_path=source_path,
-                                          build_env_vars=build_env_vars, ingress=ingress, target_port=target_port,
-                                          no_log_destination=True)
+        try:
+            create_and_verify_containerapp_up(self, resource_group=resource_group, source_path=source_path,
+                                              build_env_vars=build_env_vars, ingress=ingress, target_port=target_port,
+                                              no_log_destination=True)
+        finally:
+            cleanup(source_path)
 
     @live_only()
     @ResourceGroupPreparer(location="eastus")
@@ -83,16 +90,15 @@ class ContainerAppCreateTest(ScenarioTest):
     @live_only()
     @ResourceGroupPreparer(location="eastus")
     def test_containerapp_create_source_with_buildpack_java(self, resource_group):
-        source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack_java"))
+        source_path = os.path.join(TEST_DIR, os.path.join("data", "create_source_with_buildpack_java"))
+        download_java_source(source_path)
         ingress = 'external'
         target_port = '8080'
         build_env_vars = 'BP_JVM_VERSION=21 BP_MAVEN_VERSION=4 "BP_MAVEN_BUILD_ARGUMENTS=-Dmaven.test.skip=true --no-transfer-progress package"'
-        create_and_verify_containerapp_create_and_update(self, resource_group=resource_group, source_path=source_path,
-                                                         build_env_vars=build_env_vars, ingress=ingress,
-                                                         target_port=target_port)
-
-    @classmethod
-    def tearDownClass(cls):
-        super(ContainerAppCreateTest, cls).tearDownClass()
-        source_path = os.path.join(TEST_DIR, os.path.join("data", "source_built_using_buildpack_java"))
-        shutil.rmtree(source_path)
+        try:
+            create_and_verify_containerapp_create_and_update(self, resource_group=resource_group,
+                                                             source_path=source_path,
+                                                             build_env_vars=build_env_vars, ingress=ingress,
+                                                             target_port=target_port)
+        finally:
+            cleanup(source_path)
