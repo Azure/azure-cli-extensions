da27925f91bb05f67ced04702e20017778fffcfe
Sean Hobbs <ealianis@outlook.com>
2024-04-01 00:02:06 -0700

    [az fleet] Added skip command to UpdateRun group. (#7391)
    * Added az fleet updaterun skip command
    
    * Add skip_properties.json and remove skiptargets.json
    
    * Update skip_properties.json and test_fleet_hubless_scenario.py
    
    * style fix
    
    * Refactor skip target in fleet updaterun command
    
    * Update fleet updaterun skip command to support multiple targets
    
    * Update help message for fleet updaterun skip command
    
    * Fix handling of merge in _handle_merge function
    
    * Update 'fleet updaterun skip' argument type to string array
    
    * Updated config merge logic
    
    * Update version to 1.1.0 and add new commands in HISTORY.rst and setup.py
    
    ---------
    
    Co-authored-by: Sean Hobbs <sehobbs@microsoft.com>

--- src/fleet/azext_fleet/_help.py
+++ src/fleet/azext_fleet/_help.py
@@ -274,7 +274,18 @@ helps['fleet updaterun stop'] = """
     examples:
         - name: Stop an update run.
           text: az fleet updaterun stop -g MyFleetResourceGroup -f MyFleetName -n NameofUpdateRun
+"""
 
+helps['fleet updaterun skip'] = """
+    type: command
+    short-summary: Sets targets to be skipped within an UpdateRun.
+    parameters:
+        - name: --targets
+          type: string array
+          short-summary: Space-separated list of targets to skip.  Targets must be of the form 'targetType:targetName' such as Group:MyGroup. Valid target types are ('Member', 'Group', 'Stage', 'AfterStageWait'). The target type is case-sensitive.
+    examples:
+        - name: Set two targets to be skipped.
+          text: az fleet updaterun skip -g MyFleetResourceGroup -f MyFleetName -n NameofUpdateRun --targets Group:my-group-name Stage:my-stage-name
 """
 
 helps['fleet updaterun wait'] = """
