6e0a3c758cf6ce86a52c2e6cfcde94e3d9cf58c9
Anika Islam <anikaislam@microsoft.com>
2024-04-04 13:37:08 -0400

    resolving merge issues in command.py file

--- src/support/azext_support/commands.py
+++ src/support/azext_support/commands.py
@@ -71,6 +71,10 @@ def load_command_table(self, _):
     self.command_table['support no-subscription tickets create'] = TicketCreateNoSubscription(loader=self)
     self.command_table['support in-subscription tickets list'] = TicketList(loader=self)
     self.command_table['support no-subscription tickets list'] = TicketListNoSubscription(loader=self)
+    
+    with self.command_group("support no-subscription file") as g:
+        g.custom_command("upload", "upload_files_no_subscription")
+
     with self.command_group("support in-subscription file") as g:
         g.custom_command("upload", "upload_files_in_subscription")
 
