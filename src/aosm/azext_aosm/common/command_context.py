78a7affc6f46cf451a77066173492424294b1f08
jordlay <72226943+jordlay@users.noreply.github.com>
2024-04-05 11:50:54 +0100

    added license headers to fix release jobs (#172)
    Co-authored-by: Jordan <jordan.layton@metaswitch.com>

--- src/aosm/azext_aosm/common/command_context.py
+++ src/aosm/azext_aosm/common/command_context.py
@@ -1,5 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 from dataclasses import dataclass, field
-
 from azure.cli.core import AzCli
 from azure.cli.core.commands.client_factory import get_mgmt_service_client
 from azure.cli.core.profiles import ResourceType
