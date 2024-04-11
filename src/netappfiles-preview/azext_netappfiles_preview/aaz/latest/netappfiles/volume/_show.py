a7565288299bf66b4c6d5046f43588b7e3d3a63e
Audunn Baldvinsson <audunn.baldvinsson@netapp.com>
2024-04-11 14:44:13 +0000

    backupenabled support 2023-07-01-preview

--- src/netappfiles-preview/azext_netappfiles_preview/aaz/latest/netappfiles/volume/_show.py
+++ src/netappfiles-preview/azext_netappfiles_preview/aaz/latest/netappfiles/volume/_show.py
@@ -22,9 +22,9 @@ class Show(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2023-05-01-preview",
+        "version": "2023-07-01-preview",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.netapp/netappaccounts/{}/capacitypools/{}/volumes/{}", "2023-05-01-preview"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.netapp/netappaccounts/{}/capacitypools/{}/volumes/{}", "2023-07-01-preview"],
         ]
     }
 
@@ -153,7 +153,7 @@ class Show(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2023-05-01-preview",
+                    "api-version", "2023-07-01-preview",
                     required=True,
                 ),
             }
