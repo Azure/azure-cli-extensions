27c77ad02397cc5e2f6b80a74d702ed2fd77afe5
Andy Churchard <andy.churchard@metaswitch.com>
2024-04-17 12:15:43 +0100

    Remove parameters from help file. Instead, use the help defined in _params.py. Minor updates to text of latter.

--- src/aosm/azext_aosm/_help.py
+++ src/aosm/azext_aosm/_help.py
@@ -30,18 +30,6 @@ helps[
     short-summary: Generate configuration file for building an AOSM publisher Network Function Definition.
     long-summary: |
       Generates a configuration file that you can use to build an AOSM Network Function Definition (NFD). The configuration file is a JSONC file that contains the required parameters for building the NFD. You must complete the configuration file with your specific values before building the NFD.
-    parameters:
-      - name: --definition-type
-        type: string
-        short-summary: Required. Type of network function definition to be built.
-        long-summary: |
-          The type of AOSM definition to be built. The generate config file differs depending on type. This can be one of the following values:
-          - cnf: Containerised Network Function
-          - vnf: Virtual Network Function on Azure Core
-          - vnf-nexus: Virtual Network Function on Nexus
-      - name: --output-file
-        type: string
-        short-summary: Optional. The name of the output file to write the generated config text to.
     examples:
       - name: Generate a configuration file for a Containerised Network Function.
         text: az aosm nfd generate-config --definition-type cnf
@@ -60,20 +48,6 @@ helps[
     short-summary: Build an AOSM Network Function Definition.
     long-summary: |
       Builds an AOSM Network Function Definition (NFD) based on the configuration file provided. The NFD is built from the Helm charts or ARM templates specified in the configuration file. The output is a directory which can either be published directly (using the aosm nfd publish command) or manually customized before publishing.
-    parameters:
-      - name: --definition-type
-        type: string
-        short-summary: Required. Type of network function definition to be built.
-        long-summary: |
-          The type of AOSM definition to be built. The generate config file differs depending on type. This can be one of the following values:
-          - cnf: Containerised Network Function
-          - vnf: Virtual Network Function on Azure Core
-          - vnf-nexus: Virtual Network Function on Nexus
-      - name: --config-file
-        type: string
-        short-summary: Required. The path to the configuration file.
-        long-summary: |
-          The path to the configuration file. This is a JSONC file that contains the required parameters for building the NFD.
     examples:
       - name: Build a Containerised Network Function.
         text: az aosm nfd build --definition-type cnf --config-file my-cnf-input-config.jsonc
@@ -90,23 +64,6 @@ helps[
     short-summary: Publish a pre-built AOSM Network Function definition.
     long-summary: |
       Publishes a pre-built AOSM Network Function Definition (NFD) to Azure. The NFD must be built using the aosm nfd build command before it can be published. The NFD and other required resources (publisher resource, artifact manifest(s), storage account(s) etc.) is published to the specified resource group in the currently active Azure subscription.
-    parameters:
-      - name: --definition-type
-        type: string
-        short-summary: Required. Type of network function definition to be built.
-        long-summary: |
-          The type of AOSM definition to be built. The generate config file differs depending on type. This can be one of the following values:
-          - cnf: Containerised Network Function
-          - vnf: Virtual Network Function on Azure Core
-          - vnf-nexus: Virtual Network Function on Nexus
-      - name: --build-output-folder
-        type: string
-        short-summary: Required. Path to the folder to publish, created by the build command.
-      - name: --no-subscription-permissions
-        type: flag
-        short-summary: Optional. Pass this flag if you do not have permission to import to the Publisher subscription.
-        long-summary: |
-          Applies only when publishing CNFs - ignored for VNF. Pass this flag if you do not have permission to import to the Publisher subscription (Contributor role + AcrPush role, or a custom role that allows the importImage action and AcrPush over the whole subscription). Using this flag causes image artifacts to be pulled to your local machine and then pushed to the Artifact Store. This is slower than a copy entirely within Azure, but is an alterative if you do not have the required permissions.
     examples:
       - name: Publish a Containerised Network Function.
         text: az aosm nfd publish --definition-type cnf --build-output-folder my-cnf-output-folder
@@ -134,10 +91,6 @@ helps[
     short-summary: Generate configuration file for building an AOSM publisher Network Service Design.
     long-summary: |
       Generates a configuration file that you can use to build an AOSM Network Service Design (NSD). The configuration file is a JSONC file that contains the required parameters for building the NSD. You must complete the configuration file with your specific values before building the NSD.
-    parameters:
-      - name: --output-file
-        type: string
-        short-summary: Optional. The name of the output file to write the generated config text to.
     examples:
       - name: Generate a configuration file for a Network Service Design.
         text: az aosm nsd generate-config
@@ -152,12 +105,6 @@ helps[
     short-summary: Build an AOSM Network Service Design.
     long-summary: |
       Builds an AOSM Network Service Design (NSD) based on the configuration file provided. The NSD is built from the Network Function Definitions (NFD) and ARM templates specifying supporting infrastructure, as specified in the configuration file. The output is a directory which can either be published directly (using the aosm nsd publish command) or manually customized before publishing.
-    parameters:
-      - name: --config-file
-        type: string
-        short-summary: Required. The path to the configuration file.
-        long-summary: |
-          The path to the configuration file. This is a JSONC file that contains the required parameters for building the NFD.
     examples:
       - name: Build a Network Service Design.
         text: az aosm nsd build --config-file my-nsd-input-config.jsonc
@@ -170,10 +117,6 @@ helps[
     short-summary: Publish a pre-built AOSM Network Service Design.
     long-summary: |
       Publishes a pre-built AOSM Network Service Design (NSD) to Azure. The NSD must be built using the aosm nsd build command before it can be published. The NSD and other required resources (publisher resource, artifact manifest(s), storage account(s) etc.) is published to the specified resource group in the currently active Azure subscription.
-    parameters:
-      - name: --build-output-folder
-        type: string
-        short-summary: Required. Path to the folder to publish, created by the build command.
     examples:
       - name: Publish a Network Service Design.
         text: az aosm nsd publish --build-output-folder my-nsd-output-folder
--- src/aosm/azext_aosm/_params.py
+++ src/aosm/azext_aosm/_params.py
@@ -37,13 +37,13 @@ def load_arguments(self: AzCommandsLoader, _):
         c.argument(
             "definition_type",
             arg_type=definition_type,
-            help="Type of AOSM definition to be published.",
+            help="Type of AOSM definition to be published. The config file differs depending on type.",
             required=True,
         )
         c.argument(
             "output_file",
             options_list=["--output-file"],
-            help="The name of the output file.",
+            help="The name of the output file to write the generated config text to.",
             required=False,
         )
         c.argument(
@@ -51,7 +51,7 @@ def load_arguments(self: AzCommandsLoader, _):
             options_list=["--config-file", "-f"],
             type=file_type,
             completer=FilesCompleter(allowednames="*.jsonc"),
-            help="The path to the configuration file.",
+            help="The path to the configuration file. This is a JSONC file that contains the required parameters for building the NFD.",
         )
         c.argument(
             "clean",
@@ -83,12 +83,12 @@ def load_arguments(self: AzCommandsLoader, _):
             options_list=["--no-subscription-permissions", "-u"],
             arg_type=get_three_state_flag(),
             help=(
-                "CNF definition_type publish only - ignored for VNF. "
+                "Used only for CNF publish - ignored in all other scenarios. "
                 "Pass this flag if you do not have permission to import to the "
                 "Publisher subscription (Contributor role + AcrPush role, or a "
                 "custom role that allows the importImage action and AcrPush over the "
                 "whole subscription). Using this flag causes image artifacts to be "
-                "pulled to your local machine and then pushed to the Artifact Store. "
+                "pulled to your local machine and then pushed to the Artifact Store. This is slower than a copy entirely within Azure, but is an alternative if you do not have the required permissions. "
                 "Requires Docker to be installed locally."
             ),
         )
@@ -97,7 +97,7 @@ def load_arguments(self: AzCommandsLoader, _):
         c.argument(
             "output_file",
             options_list=["--output-file"],
-            help="The name of the output file.",
+            help="The name of the output file to write the generated config text to.",
             required=False,
         )
         c.argument(
@@ -105,7 +105,7 @@ def load_arguments(self: AzCommandsLoader, _):
             options_list=["--config-file", "-f"],
             type=file_type,
             completer=FilesCompleter(allowednames="*.jsonc"),
-            help="The path to the configuration file.",
+            help="The path to the configuration file. This is a JSONC file that contains the required parameters for building the NSD.",
         )
         c.argument("skip", arg_type=ns_skip_steps, help="Optional skip steps")
         c.argument(
