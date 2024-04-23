5eb30fb30ed329074d885df78152f1aa56ca0c60
Shahed Abu Qamar <sabuqamar@microsoft.com>
2024-04-23 10:19:44 +0100

    fix failed versioning

--- src/network-analytics/azext_network_analytics/aaz/latest/network_analytics/data_product/profiles.py
+++
@@ -1,8 +0,0 @@
-# --------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-# --------------------------------------------------------------------------------------------
-
-from azure.cli.core.profiles import CustomResourceType
-
-DATA_STORAGE_BLOB_CONTAINER = CustomResourceType('azure.storage.blob', 'ContainerClient')
