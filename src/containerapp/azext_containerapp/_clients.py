ad46b546a94575bbf2fc7c59f4e9a45f7dd94cd7
xinyu pang <46143499+Greedygre@users.noreply.github.com>
2024-04-25 17:07:16 +0800

    [Container app] Upgrade api-version to 2024-02-02-preview (#7545)

--- src/containerapp/azext_containerapp/_clients.py
+++ src/containerapp/azext_containerapp/_clients.py
@@ -27,7 +27,7 @@ from knack.log import get_logger
 
 logger = get_logger(__name__)
 
-PREVIEW_API_VERSION = "2023-11-02-preview"
+PREVIEW_API_VERSION = "2024-02-02-preview"
 POLLING_TIMEOUT = 1500  # how many seconds before exiting
 POLLING_SECONDS = 2  # how many seconds between requests
 POLLING_TIMEOUT_FOR_MANAGED_CERTIFICATE = 1500  # how many seconds before exiting
