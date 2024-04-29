43e14d15e28dec9b0dc158305e046b8ca24a9fa3
Jingnan Xu <jingnanxu@microsoft.com>
2024-04-29 16:08:00 +0800

    rerun UT

--- src/front-door/azext_front_door/custom_waf.py
+++ src/front-door/azext_front_door/custom_waf.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+from azure.cli.core.aaz._base import has_value
 from azure.cli.core.aaz import AAZBoolArg
 from azext_front_door.aaz.latest.network.front_door.waf_policy import Create as _WafPolicyCreate, \
     Update as _WafPolicyUpdate
@@ -12,35 +13,41 @@ class WafPolicyCreate(_WafPolicyCreate):
     @classmethod
     def _build_arguments_schema(cls, *args, **kwargs):
         args_schema = super()._build_arguments_schema(*args, **kwargs)
-        args_schema.identity_type = AAZBoolArg(
+        args_schema.disabled = AAZBoolArg(
             options=['--disabled'],
             help='Create in a disabled state.',
             default=False,
-            blank=False
+            blank=True,
         )
         args_schema.enabled_state._registered = False
         return args_schema
 
-    def pre_operation(self):
+    def pre_operations(self):
         args = self.ctx.args
         args.location = 'global'
-        args.enabled_state = not args.disabled
+        if args.disabled.to_serialized_data():
+            args.enabled_state = 'Disabled'
+        else:
+            args.enabled_state = 'Enabled'
 
 
 class WafPolicyUpdate(_WafPolicyUpdate):
     @classmethod
     def _build_arguments_schema(cls, *args, **kwargs):
         args_schema = super()._build_arguments_schema(*args, **kwargs)
-        args_schema.identity_type = AAZBoolArg(
+        args_schema.disabled = AAZBoolArg(
             options=['--disabled'],
             help='Create in a disabled state.',
             default=False,
-            blank=False
+            blank=True,
         )
         args_schema.enabled_state._registered = False
         return args_schema
 
-    def pre_operation(self):
+    def pre_operations(self):
         args = self.ctx.args
         args.location = 'global'
-        args.enabled_state = not args.disabled
+        if args.disabled.to_serialized_data():
+            args.enabled_state = 'Disabled'
+        else:
+            args.enabled_state = 'Enabled'
