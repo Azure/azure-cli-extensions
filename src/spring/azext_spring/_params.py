2e7ff141c48f71c4847f03865f39f10b67627763
Wenhao Zhang <wenhaozhang@microsoft.com>
2024-05-15 14:50:34 +0800

    Remove useless code

--- src/spring/azext_spring/_params.py
+++ src/spring/azext_spring/_params.py
@@ -1294,9 +1294,9 @@ def load_arguments(self, _):
 
     with self.argument_context('spring private-dns-zone add') as c:
         c.argument('service', service_name_type)
-        c.argument('zone_id', type=str, help='The resource id of the private DNS zone which you would like to configure with the service instance.')
+        c.argument('zone_id', help='The resource id of the private DNS zone which you would like to configure with the service instance.')
     with self.argument_context('spring private-dns-zone update') as c:
         c.argument('service', service_name_type)
-        c.argument('zone_id', type=str, help='The resource id of the private DNS zone which you would like to configure with the service instance.')
+        c.argument('zone_id', help='The resource id of the private DNS zone which you would like to configure with the service instance.')
     with self.argument_context('spring private-dns-zone clean') as c:
         c.argument('service', service_name_type)
