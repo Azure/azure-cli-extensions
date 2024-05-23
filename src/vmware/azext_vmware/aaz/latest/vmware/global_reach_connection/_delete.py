23dd27cb75bfa2f9fa02ae5429f538c11bc61787
Jonathan He <jonathanhe@microsoft.com>
2024-05-23 15:58:49 -0400

    gloal reach connections

--- src/vmware/azext_vmware/aaz/latest/vmware/global_reach_connection/_delete.py
+++ src/vmware/azext_vmware/aaz/latest/vmware/global_reach_connection/_delete.py
@@ -13,16 +13,16 @@ from azure.cli.core.aaz import *
 
 @register_command(
     "vmware global-reach-connection delete",
-    confirmation="This will delete the global reach connection. Are you sure?",
+    confirmation="Are you sure you want to perform this operation?",
 )
 class Delete(AAZCommand):
     """Delete a global reach connection in a private cloud
     """
 
     _aaz_info = {
-        "version": "2023-03-01",
+        "version": "2023-09-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.avs/privateclouds/{}/globalreachconnections/{}", "2023-03-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.avs/privateclouds/{}/globalreachconnections/{}", "2023-09-01"],
         ]
     }
 
@@ -54,7 +54,7 @@ class Delete(AAZCommand):
         )
         _args_schema.private_cloud = AAZStrArg(
             options=["-c", "--private-cloud"],
-            help="The name of the private cloud.",
+            help="Name of the private cloud",
             required=True,
             id_part="name",
             fmt=AAZStrArgFormat(
@@ -156,7 +156,7 @@ class Delete(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2023-03-01",
+                    "api-version", "2023-09-01",
                     required=True,
                 ),
             }
