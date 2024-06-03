5ba55afb486d52e58512e70e0d6c54d2276e97a7
jtamma <jtamma@microsoft.com>
2024-06-04 02:06:34 +0530

    Fixed pipelines failures

--- src/nexusidentity/azext_nexusidentity/tests/latest/test_nexusidentity_scenario.py
+++ src/nexusidentity/azext_nexusidentity/tests/latest/test_nexusidentity_scenario.py
@@ -3,45 +3,40 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import os
+'''
+Nexus Identity Ssh-Key Geneation Scenario Test
+'''
 
-from azure_devtools.scenario_tests import AllowLargeResponse
-from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer)
+from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
+
+def setup_scenario1(test):
+    ''' Env setup_scenario1 '''
+    pass
 
 
-TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
+def cleanup_scenario1(test):
+    '''Env cleanup_scenario1 '''
+    pass
 
+def call_scenario1(test):
+    ''' # Testcase: scenario1'''
+    setup_scenario1(test)
+    step_gen_keys(test, checks=[])
+    cleanup_scenario1(test)
+
+def step_gen_keys(test, checks=None):
+    '''Generate Nexus Identity ssh keys '''
+    if checks is None:
+        checks = []
+    test.cmd('az nexusidentity gen-keys')
 
 class NexusidentityScenarioTest(ScenarioTest):
+    ''' Nexus Identity Ssh-Key Generation Scenario Test '''
 
-    @ResourceGroupPreparer(name_prefix='cli_test_nexusidentity')
-    def test_nexusidentity(self, resource_group):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        self.kwargs.update({
-            'name': 'test1'
-        })
+    def test_nexusidentity_scenario1(self):
 
-        self.cmd(
-            'nexusidentity create -g {rg} -n {name} --tags foo=doo',
-            checks=[
-                self.check(
-                    'tags.foo',
-                    'doo'),
-                self.check(
-                    'name',
-                    '{name}')])
-        self.cmd(
-            'nexusidentity update -g {rg} -n {name} --tags foo=boo',
-            checks=[
-                self.check(
-                    'tags.foo',
-                    'boo')])
-        count = len(self.cmd('nexusidentity list').get_output_in_json())
-        self.cmd('nexusidentity show - {rg} -n {name}', checks=[
-            self.check('name', '{name}'),
-            self.check('resourceGroup', '{rg}'),
-            self.check('tags.foo', 'boo')
-        ])
-        self.cmd('nexusidentity delete -g {rg} -n {name}')
-        final_count = len(self.cmd('nexusidentity list').get_output_in_json())
-        self.assertTrue(final_count, count - 1)
+        # Testcase: scenario1
+        call_scenario1(self)
