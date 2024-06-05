ecc44b0bd0ff67c11ae1a9c6fa8fb5e95166bfa4
Zeng Taoxu <96050289+ZengTaoxu@users.noreply.github.com>
2024-06-05 10:32:55 +0800

    [Network Manager] Add commands `az network manager routing-config` (#7670)

--- src/network-manager/azext_network_manager/aaz/latest/network/manager/routing_config/__cmd_group.py
+++ src/network-manager/azext_network_manager/aaz/latest/network/manager/routing_config/__cmd_group.py
@@ -8,3 +8,17 @@
 # pylint: skip-file
 # flake8: noqa
 
+from azure.cli.core.aaz import *
+
+
+@register_command_group(
+    "network manager routing-config",
+    is_preview=True,
+)
+class __CMDGroup(AAZCommandGroup):
+    """Manage network-manager routing-config with network.
+    """
+    pass
+
+
+__all__ = ["__CMDGroup"]
