5bc43ac32086268683b6913e45114d75a3c5683b
Dominika Bobik <dominikabobik12@gmail.com>
2024-06-11 17:05:17 -0700

    [Serial Console] Migrate to a new authentication flow (#7653)
    * changed readme
    
    * added slag
    
    * tests finally work
    
    * auth flow changes
    
    * working stage
    
    * removed semicolon
    
    * formatting
    
    * removed debug statements from tests
    
    * formatting
    
    * formatting v3456
    
    * fixed typo
    
    * send token after first message is recieved
    
    * remove flag
    
    * changed version
    
    * rebase leftover
    
    * recorsdings tests
    
    * updated tests
    
    * tests
    
    * version update
    
    * change to trigger build
    
    * Update src/serial-console/azext_serialconsole/custom.py
    
    Co-authored-by: kai ru <69238381+kairu-ms@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dominika Bobik <dbobik@microsoft.com>
    Co-authored-by: kai ru <69238381+kairu-ms@users.noreply.github.com>

--- src/serial-console/azext_serialconsole/custom.py
+++ src/serial-console/azext_serialconsole/custom.py
@@ -301,7 +301,6 @@ class SerialConsole:
 
         self.websocket_url = None
         self.access_token = None
-        self.new_auth_flow = "1"
 
     @staticmethod
     def listen_for_keys():
@@ -434,8 +433,7 @@ class SerialConsole:
 
         def on_message(_, message):
             if GV.first_message:
-                if self.new_auth_flow == "1":
-                    GV.websocket_instance.send(self.access_token)
+                GV.websocket_instance.send(self.access_token)
                 GV.first_message = False
                 GV.loading = False
                 PC.clear_screen()
@@ -460,7 +458,7 @@ class SerialConsole:
         def connect_thread():
             if self.load_websocket_url():
                 GV.websocket_instance = websocket.WebSocketApp(
-                    self.websocket_url + "?authorization=" + self.access_token + "&new=" + self.new_auth_flow,
+                    self.websocket_url,
                     on_open=on_open,
                     on_message=on_message,
                     on_error=on_error,
@@ -565,8 +563,7 @@ class SerialConsole:
                     ws.close()
 
             wsapp = websocket.WebSocketApp(
-                self.websocket_url + "?authorization=" + self.access_token + "&new="
-                + self.new_auth_flow, on_message=on_message)
+                self.websocket_url, on_message=on_message)
             wsapp.run_forever()
             GV.loading = False
             if GV.trycount == 0:
