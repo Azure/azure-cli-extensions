08dbb4c5454e544761124ef628044774e372cdd8
AllyW <AllyW@users.noreply.github.com>
2024-06-13 14:19:48 +0800

    [monitor-control-service] Update api-version with issue fix (#7616)
    * add api-version update and enum support extension

--- src/monitor-control-service/azext_amcs/aaz/latest/monitor/data_collection/endpoint/_show.py
+++ src/monitor-control-service/azext_amcs/aaz/latest/monitor/data_collection/endpoint/_show.py
@@ -22,9 +22,9 @@ class Show(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2022-06-01",
+        "version": "2023-03-11",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.insights/datacollectionendpoints/{}", "2022-06-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.insights/datacollectionendpoints/{}", "2023-03-11"],
         ]
     }
 
@@ -120,7 +120,7 @@ class Show(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2022-06-01",
+                    "api-version", "2023-03-11",
                     required=True,
                 ),
             }
@@ -271,6 +271,10 @@ class Show(AAZCommand):
             metadata = cls._schema_on_200.properties.metadata
             metadata.provisioned_by = AAZStrType(
                 serialized_name="provisionedBy",
+                flags={"read_only": True},
+            )
+            metadata.provisioned_by_immutable_id = AAZStrType(
+                serialized_name="provisionedByImmutableId",
                 flags={"read_only": True},
             )
             metadata.provisioned_by_resource_id = AAZStrType(
