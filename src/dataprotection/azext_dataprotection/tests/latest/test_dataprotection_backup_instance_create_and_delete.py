def4282dfed6b781ae1423d1d8fe61ec900c24aa
Zubair Abid <zubairabid@microsoft.com>
2024-06-26 15:33:08 +0530

    Rerunning all tests

--- src/dataprotection/azext_dataprotection/tests/latest/test_dataprotection_backup_instance_create_and_delete.py
+++ src/dataprotection/azext_dataprotection/tests/latest/test_dataprotection_backup_instance_create_and_delete.py
@@ -6,6 +6,7 @@
 # pylint: disable=line-too-long
 # pylint: disable=unused-import
 
+from random import randint
 from azure.cli.testsdk import ScenarioTest
 from azure.cli.testsdk.scenario_tests import AllowLargeResponse
 from ..utils import track_job_to_completion
@@ -94,6 +95,16 @@ class BackupInstanceCreateDeleteScenarioTest(ScenarioTest):
             'storageAccountId': '/subscriptions/38304e13-357e-405e-9e9a-220351dcce8c/resourceGroups/clitest-dpp-rg/providers/Microsoft.Storage/storageAccounts/clitestsadonotdelete',
             'policyRuleName': "BackupWeekly"
         })
+        # Cleanup existing blob/containers in the storage account and create a new one
+        existing_containers = test.cmd('storage container list --account-name "{storageAccountName}" --auth-mode login').get_output_in_json()
+        for container in existing_containers:
+            container_name = container['name']
+            test.kwargs.update({"containerName": container_name})
+            test.cmd('storage container delete --name "{containerName}" --account-name "{storageAccountName}" --auth-mode login')
+        new_container_name = 'cont{}'.format(randint(0, 100))
+        test.kwargs.update({"new_container_name": new_container_name})
+        test.cmd(' storage container create --name "{new_container_name}" --account-name "{storageAccountName}" --auth-mode login')
+
         backup_config_json = test.cmd('az dataprotection backup-instance initialize-backupconfig --datasource-type "{dataSourceType}" '
                                       '--include-all-containers --storage-account-rg "{rg}" --storage-account-name "{storageAccountName}"').get_output_in_json()
         print(backup_config_json)
