e748d84caa405aa3ed3f3714f6f95d29a97b0f21
james fan <jamesfan@microsoft.com>
2024-06-27 01:51:34 -0700

    fixed common

--- src/nginx/azext_nginx/custom.py
+++ src/nginx/azext_nginx/custom.py
@@ -3,6 +3,9 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# pylint: disable=no-self-use, no-member, too-many-lines, unused-argument, skip-file
+# pylint: disable=protected-access, too-few-public-methods, line-too-long
+
 from .aaz.latest.nginx.deployment.configuration._update import Update as _ConfigurationUpdate
 from azure.cli.core.aaz import *
 
