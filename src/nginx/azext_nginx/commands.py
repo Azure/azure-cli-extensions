5558ed34e5e5ffaf89556a0878aab086541ce3e3
james fan <jamesfan@microsoft.com>
2024-06-27 01:04:38 -0700

    make config patch to ignore payload from get

--- src/nginx/azext_nginx/commands.py
+++ src/nginx/azext_nginx/commands.py
@@ -5,6 +5,8 @@
 
 # pylint: disable=line-too-long
 
-
 def load_command_table(self, _):  # pylint: disable=unused-argument
-    pass
+    with self.command_group('nginx deployment configuration') as g:
+        from .custom import ConfigurationUpdate
+        self.command_table["nginx deployment configuration update"] = ConfigurationUpdate(loader=self)
+        
