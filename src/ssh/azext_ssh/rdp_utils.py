318a9fb2f4e09b44db9440b62a718d3d2687eac6
Vivian Thiebaut <vthiebaut@microsoft.com>
2024-07-01 16:22:13 -0400

    Start RDP process using subprocess library instead of _process_helper

--- src/ssh/azext_ssh/rdp_utils.py
+++ src/ssh/azext_ssh/rdp_utils.py
@@ -82,12 +82,12 @@ def start_rdp_connection(ssh_info, delete_keys, delete_cert):
 
 
 def call_rdp(local_port):
-    from . import _process_helper
     if platform.system() == 'Windows':
         print_styled_text((Style.SUCCESS, "Launching Remote Desktop Connection"))
         print_styled_text((Style.IMPORTANT, "To close this session, close the Remote Desktop Connection window."))
         command = [_get_rdp_path(), f"/v:localhost:{local_port}"]
-        _process_helper.launch_and_wait(command)
+        process = subprocess.Popen(command)
+        process.wait()
 
 
 def is_local_port_open(local_port):
