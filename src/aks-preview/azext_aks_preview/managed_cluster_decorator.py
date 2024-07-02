8cbb8b715a1e9df7ca66fcca1f7487ff3d1848b9
Tyler Lloyd <tylloyd@microsoft.com>
2024-07-02 17:31:45 +0000

    chore: set to None not empty string

--- src/aks-preview/azext_aks_preview/managed_cluster_decorator.py
+++ src/aks-preview/azext_aks_preview/managed_cluster_decorator.py
@@ -2977,7 +2977,7 @@ class AKSPreviewManagedClusterCreateDecorator(AKSManagedClusterCreateDecorator):
             network_profile.network_plugin is not None and
             self.context.raw_param.get("network_plugin") is None
         ):
-            network_profile.network_plugin = ""
+            network_profile.network_plugin = None
 
         # set up pod_cidrs, service_cidrs and ip_families
         (
