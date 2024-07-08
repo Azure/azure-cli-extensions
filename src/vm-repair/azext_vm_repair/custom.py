784951be46712a2fcb7a1367d4b0f8f1f5e861b3
Wayland/Weilan <267128979@qq.com>
2024-07-08 04:47:38 -0400

    Update custom.py

--- src/vm-repair/azext_vm_repair/custom.py
+++ src/vm-repair/azext_vm_repair/custom.py
@@ -646,19 +646,13 @@ def reset_nic(cmd, vm_name, resource_group_name, yes=False):
             update_ip_command = 'az network nic ip-config update -g {g} --nic-name {nic} -n {config} --private-ip-address {ip}' \
                                 .format(g=resource_group_name, nic=primary_nic_name, config=ipconfig_name, ip=swap_ip_address)
         _call_az_command(update_ip_command)
-            # Check if IP is updated
-    while True:
-        get_current_ip_command = 'az network nic ip-config wait --updatede -g {g} --nic-name {nic} -n ' \
+        
+         # Wait for IP updated
+        wait_ip_update_command = 'az network nic ip-config wait --updatede -g {g} --nic-name {nic} -n ' \
                                 .format(g=resource_group_name, nic=primary_nic_name)
         
-        current_ip_address = _call_az_command(get_current_ip_command)
-        if current_ip_address == swap_ip_address:
-            logger.info('IP address updated successfully to {ip}\n'.format(ip=swap_ip_address))
-            break
-        else:
-            logger.info('IP address not updated yet. Waiting for a few minutes before retrying...\n')
-            time.sleep(300)
-            # Sleep for 5 minutes 
+        current_ip_address = _call_az_command(wait_ip_update_command)
+
             
         # 4) Change things back. This will also invoke and wait for a VM restart.
         logger.info('NIC reset is complete. Now reverting back to your original configuration...\n')
