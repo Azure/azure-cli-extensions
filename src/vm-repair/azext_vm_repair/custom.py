56e5c3cf2f6925db38989dbca1f07d9d1988b276
Wayland/Weilan <267128979@qq.com>
2024-07-10 05:44:12 -0400

    VM repair 1.0.7 (#7786)

--- src/vm-repair/azext_vm_repair/custom.py
+++ src/vm-repair/azext_vm_repair/custom.py
@@ -648,7 +648,7 @@ def reset_nic(cmd, vm_name, resource_group_name, yes=False):
         _call_az_command(update_ip_command)
         
          # Wait for IP updated
-        wait_ip_update_command = 'az network nic ip-config wait --updated -g {g} --nic-name {nic} -n ' \
+        wait_ip_update_command = 'az network nic ip-config wait --updated -g {g} --nic-name {nic}' \
                                 .format(g=resource_group_name, nic=primary_nic_name)
         _call_az_command(wait_ip_update_command)
 
