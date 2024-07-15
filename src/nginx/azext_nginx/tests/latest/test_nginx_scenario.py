d8edbac3cf1aa3e53637959832c0719c04525a1a
james fan <jamesfan@microsoft.com>
2024-07-15 14:56:57 -0700

    fix test

--- src/nginx/azext_nginx/tests/latest/test_nginx_scenario.py
+++ src/nginx/azext_nginx/tests/latest/test_nginx_scenario.py
@@ -86,9 +86,9 @@ class NginxScenarioTest(ScenarioTest):
         if self.is_live:
             time.sleep(15)
 
-        self.cmd("role assignment create --role 'Key Vault Administrator' --assignee-object-id {identity_object_id} --scope {kv_resource_id} --assignee-principal-type 'ServicePrincipal'")
+        # self.cmd("role assignment create --role 'Key Vault Administrator' --assignee-object-id {identity_object_id} --scope {kv_resource_id} --assignee-principal-type 'ServicePrincipal'")
         # RBAC takes about 15 min to pass CI, local test can be changed to 15
-        time.sleep(15)
+        # time.sleep(15)
         self.cmd('nginx deployment certificate create --certificate-name {cert_name} --deployment-name {deployment_name} --location {location} --resource-group {rg} --certificate-path /etc/nginx/test.cert --key-path /etc/nginx/test.key --key-vault-secret-id {kv_secret_id}', checks=[
             self.check('properties.provisioningState', 'Succeeded'),
             self.check('name', self.kwargs['cert_name']),
