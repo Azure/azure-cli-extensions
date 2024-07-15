e7d52f3946b72cb640d553a40306f12f49a25584
Leo Zhang <t-zhangleo@microsoft.com>
2024-07-15 14:37:05 -0700

    refactor migrate into having it's own functions.

--- src/amg/azext_amg/migrate.py
+++ src/amg/azext_amg/migrate.py
@@ -8,102 +8,26 @@ logger = get_logger(__name__)
 
 
 def migrate(backup_url, backup_headers, restore_url, restore_headers, dry_run, folders_to_include=None, folders_to_exclude=None):
-    # TODO: refactor, make helper functions that this will call and just return the summaries.
-    datasources_created_summary = []
-    datasources_remapped_summary = []
-    folders_created_summary = []
-    library_panels_synced_summary = {}
-    dashboards_synced_summary = {}
-    snapshots_synced_summary = {}
-    annotations_synced_summary = []
-
     # get all datasources to be backedup
     all_datasources = get_all_datasources(backup_url, backup_headers)
     all_restore_datasources = get_all_datasources(restore_url, restore_headers)
-    # since in our remapping, we use the name and type to check if it is the same datasource.
-    restore_datasources_names = {(ds['name'], ds['type']) for ds in all_restore_datasources}
-
-    for datasource in all_datasources:
-        if (datasource['name'], datasource['type']) not in restore_datasources_names:
-            if not dry_run:
-                create_datasource(restore_url, datasource, restore_headers)
-            datasources_created_summary.append(datasource['name'])
-        else:
-            logger.warning("Datasource %s already exists, remapping", datasource['name'])
-            datasources_remapped_summary.append(datasource['name'])
-    # grab all the new datasources now. since we have created the datasources, we can now do the mapping.
-    # do the mapping from the backup to the restore.
-    set_uid_mapping(all_datasources, all_restore_datasources)
+    (datasources_created_summary, datasources_remapped_summary) = _migrate_datasources(all_datasources, all_restore_datasources, restore_url, restore_headers, dry_run)
 
     all_folders = get_all_folders(backup_url, backup_headers, folders_to_include=folders_to_include, folders_to_exclude=folders_to_exclude)
     all_restore_folders = get_all_folders(restore_url, restore_headers, folders_to_include=folders_to_include, folders_to_exclude=folders_to_exclude)
-    restore_folder_uids = {restore_content['uid'] for (restore_content, _) in all_restore_folders}
+    folders_created_summary = _migrate_folders(all_folders, all_restore_folders, restore_url, restore_headers, dry_run)
 
-    for folder in all_folders:
-        content_folder_settings, content_folder_permissions = folder
-        # create a folder if it does not exist
-        if content_folder_settings['uid'] not in restore_folder_uids:
-            if not dry_run:
-                create_folder(restore_url, content_folder_settings, restore_headers)
-            folders_created_summary.append(content_folder_settings['title'])
-        else:
-            logger.warning("Folder %s already exists, skipping", content_folder_settings['title'])
-
-    # get all the dashboards first, so we can get the library panels
     all_dashboards = get_all_dashboards(backup_url, backup_headers, folders_to_include=folders_to_include, folders_to_exclude=folders_to_exclude)
-
     all_library_panels = get_all_library_panels(backup_url, backup_headers)
-    library_panels_uids_to_update = get_all_library_panels_uids_from_dashboards(all_dashboards)
-    # only update the library panels that are not included / excluded.
-    all_library_panels_filtered = [panel for panel in all_library_panels if panel['uid'] in library_panels_uids_to_update]
-    for library_panel in all_library_panels_filtered:
-        library_panel['id'] = None
-        if not dry_run:
-            # in create_library_panel, it does the patch if the library panel already exists.
-            create_library_panel(restore_url, library_panel, restore_headers)
-
-        panel_folder_name = library_panel['meta']['folderName']
-        if panel_folder_name not in library_panels_synced_summary:
-            library_panels_synced_summary[panel_folder_name] = set()
-        library_panels_synced_summary[panel_folder_name].add(library_panel['name'])
-
-    # we don't backup provisioned dashboards, so we don't need to restore them
-    for dashboard in all_dashboards:
-        dashboard['dashboard']['id'] = None
-        # TODO: add the override flag, we don't need to delete & create, override will just do it.
-        if not dry_run:
-            create_dashboard(restore_url, dashboard, restore_headers)
-
-        folder_title = dashboard['meta']['folderTitle']
-        if folder_title not in dashboards_synced_summary:
-            dashboards_synced_summary[folder_title] = []
-        dashboards_synced_summary[folder_title].append(dashboard['dashboard']['title'])
+    (library_panels_synced_summary, dashboards_synced_summary) = _migrate_library_panels_and_dashboards(all_dashboards, all_library_panels, restore_url, restore_headers, dry_run)
 
     # get the list of snapshots to backup
     all_snapshots = get_all_snapshots(backup_url, backup_headers)
-    for snapshot in all_snapshots:
-        if not dry_run:
-            create_snapshot(restore_url, snapshot, restore_headers)
-        
-        try:
-            snapshot_name = snapshot['dashboard']['title']
-        except KeyError:
-            snapshot_name = "Untitled Snapshot"
-
-        snapshot_folder_title = snapshot['meta']['folderTitle']
-        if snapshot_folder_title not in snapshots_synced_summary:
-            snapshots_synced_summary[snapshot_folder_title] = []
-        snapshots_synced_summary[snapshot_folder_title].append(snapshot_name)
-
+    snapshots_synced_summary = _migrate_snapshots(all_snapshots, restore_url, restore_headers, dry_run)
+   
     # get all the annotations
     all_annotations = get_all_annotations(backup_url, backup_headers)
-    for annotation in all_annotations:
-        if not dry_run:
-            create_annotation(restore_url, annotation, restore_headers)
-
-        # can do text to get the text, annotations don't have titles.
-        # annotations_synced_summary.append(annotation['text'])
-        annotations_synced_summary.append(annotation['id'])
+    annotations_synced_summary = _migrate_annotations(all_annotations, restore_url, restore_headers, dry_run)
 
     output = [
         (Style.IMPORTANT, f"\n\nDry run: {dry_run if dry_run else False}\n"),
@@ -134,7 +58,78 @@ def migrate(backup_url, backup_headers, restore_url, restore_headers, dry_run, f
     print_styled_text(output)
 
 
-def get_all_library_panels_uids_from_dashboards(all_dashboards):
+def _migrate_datasources(all_datasources, all_restore_datasources, restore_url, restore_headers, dry_run):
+    datasources_created_summary = []
+    datasources_remapped_summary = []
+
+    # since in our remapping, we use the name and type to check if it is the same datasource.
+    restore_datasources_names = {(ds['name'], ds['type']) for ds in all_restore_datasources}
+
+    for datasource in all_datasources:
+        if (datasource['name'], datasource['type']) not in restore_datasources_names:
+            if not dry_run:
+                create_datasource(restore_url, datasource, restore_headers)
+            datasources_created_summary.append(datasource['name'])
+        else:
+            logger.warning("Datasource %s already exists, remapping", datasource['name'])
+            datasources_remapped_summary.append(datasource['name'])
+    # grab all the new datasources now. since we have created the datasources, we can now do the mapping.
+    # do the mapping from the backup to the restore.
+    set_uid_mapping(all_datasources, all_restore_datasources)
+
+    return (datasources_created_summary, datasources_remapped_summary)
+
+
+def _migrate_folders(all_folders, all_restore_folders, restore_url, restore_headers, dry_run):
+    folders_created_summary = []
+    restore_folder_uids = {restore_content['uid'] for (restore_content, _) in all_restore_folders}
+
+    for folder in all_folders:
+        content_folder_settings, content_folder_permissions = folder
+        # create a folder if it does not exist
+        if content_folder_settings['uid'] not in restore_folder_uids:
+            if not dry_run:
+                create_folder(restore_url, content_folder_settings, restore_headers)
+            folders_created_summary.append(content_folder_settings['title'])
+        else:
+            logger.warning("Folder %s already exists, skipping", content_folder_settings['title'])
+    return folders_created_summary
+
+
+def _migrate_library_panels_and_dashboards(all_dashboards, all_library_panels, restore_url, restore_headers, dry_run):
+    library_panels_synced_summary = {}
+    dashboards_synced_summary = {}
+
+    library_panels_uids_to_update = _get_all_library_panels_uids_from_dashboards(all_dashboards)
+    # only update the library panels that are not included / excluded.
+    all_library_panels_filtered = [panel for panel in all_library_panels if panel['uid'] in library_panels_uids_to_update]
+    for library_panel in all_library_panels_filtered:
+        library_panel['id'] = None
+        if not dry_run:
+            # in create_library_panel, it does the patch if the library panel already exists.
+            create_library_panel(restore_url, library_panel, restore_headers)
+
+        panel_folder_name = library_panel['meta']['folderName']
+        if panel_folder_name not in library_panels_synced_summary:
+            library_panels_synced_summary[panel_folder_name] = set()
+        library_panels_synced_summary[panel_folder_name].add(library_panel['name'])
+
+    # we don't backup provisioned dashboards, so we don't need to restore them
+    for dashboard in all_dashboards:
+        dashboard['dashboard']['id'] = None
+        # TODO: add the override flag, we don't need to delete & create, override will just do it.
+        if not dry_run:
+            create_dashboard(restore_url, dashboard, restore_headers)
+
+        folder_title = dashboard['meta']['folderTitle']
+        if folder_title not in dashboards_synced_summary:
+            dashboards_synced_summary[folder_title] = []
+        dashboards_synced_summary[folder_title].append(dashboard['dashboard']['title'])
+
+    return (library_panels_synced_summary, dashboards_synced_summary)
+
+
+def _get_all_library_panels_uids_from_dashboards(all_dashboards):
     library_panels_uids_to_update = set()
     for dashboard in all_dashboards:
         for panel in dashboard['dashboard']['panels']:
@@ -142,3 +137,34 @@ def get_all_library_panels_uids_from_dashboards(all_dashboards):
                 library_panels_uids_to_update.add(panel['libraryPanel']['uid'])
 
     return library_panels_uids_to_update
+
+
+def _migrate_snapshots(all_snapshots, restore_url, restore_headers, dry_run):
+    snapshots_synced_summary = {}
+    for snapshot in all_snapshots:
+        if not dry_run:
+            create_snapshot(restore_url, snapshot, restore_headers)
+        
+        try:
+            snapshot_name = snapshot['dashboard']['title']
+        except KeyError:
+            snapshot_name = "Untitled Snapshot"
+
+        snapshot_folder_title = snapshot['meta']['folderTitle']
+        if snapshot_folder_title not in snapshots_synced_summary:
+            snapshots_synced_summary[snapshot_folder_title] = []
+        snapshots_synced_summary[snapshot_folder_title].append(snapshot_name)
+    return snapshots_synced_summary
+
+
+def _migrate_annotations(all_annotations, restore_url, restore_headers, dry_run):
+    annotations_synced_summary = []
+    for annotation in all_annotations:
+        if not dry_run:
+            create_annotation(restore_url, annotation, restore_headers)
+
+        # can do text to get the text, annotations don't have titles.
+        # annotations_synced_summary.append(annotation['text'])
+        annotations_synced_summary.append(str(annotation['id']))
+
+    return annotations_synced_summary
