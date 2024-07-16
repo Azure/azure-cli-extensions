c7b8fd0de89216d100c8c1000d22edba640dae38
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2024-07-16 10:19:04 +0800

    lint

--- src/storage-preview/azext_storage_preview/operations/task_assignment.py
+++ src/storage-preview/azext_storage_preview/operations/task_assignment.py
@@ -2,7 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-import time
 
 from knack.log import get_logger
 from datetime import datetime
