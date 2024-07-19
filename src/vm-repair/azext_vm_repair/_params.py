41be43527cecf9f2ae7b49d1be2108489bd76522
Haider Agha <64601174+haagha@users.noreply.github.com>
2024-07-19 23:02:59 +0000

    changes

--- src/vm-repair/azext_vm_repair/_params.py
+++ src/vm-repair/azext_vm_repair/_params.py
@@ -34,6 +34,15 @@ def load_arguments(self, _):
         c.argument('distro', help='Option to create repair vm from a specific linux distro (rhel7|rhel8|suse12|ubuntu20|centos7|oracle7)')
         c.argument('yes', help='Option to skip prompt for associating public ip and confirm yes to it in no Tty mode')
 
+    with self.argument_context('vm repair reuse') as c:
+        c.argument('repair_vm_name', help='Name of repair VM.')
+        c.argument('copy_disk_name', help='Name of OS disk copy.')
+        c.argument('repair_group_name', help='Name for new or existing resource group that will contain repair VM.')
+        c.argument('unlock_encrypted_vm', help='Option to auto-unlock encrypted VMs using current subscription auth.')
+        c.argument('enable_nested', help='enable nested hyperv.')
+        c.argument('associate_public_ip', help='Option to create repair vm with public ip')
+        c.argument('yes', help='Option to skip prompt for associating public ip and confirm yes to it in no Tty mode')
+
     with self.argument_context('vm repair restore') as c:
         c.argument('repair_vm_id', help='Repair VM resource id.')
         c.argument('disk_name', help='Name of fixed data disk. Defaults to the first data disk in the repair VM.')
