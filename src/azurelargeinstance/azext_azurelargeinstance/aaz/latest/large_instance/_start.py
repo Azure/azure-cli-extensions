ff9222ec0e5a9daa2444e123db3e92739e1c20ee
Ashwin Jeyaseelan <ashwinjeyaseelan@ashwins-mbp4.mynetworksettings.com>
2024-08-29 16:45:06 -0700

    Generated new methods and updated readme

--- src/azurelargeinstance/azext_azurelargeinstance/aaz/latest/large_instance/_start.py
+++ src/azurelargeinstance/azext_azurelargeinstance/aaz/latest/large_instance/_start.py
@@ -22,9 +22,9 @@ class Start(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2024-04-10",
+        "version": "2024-08-01-preview",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.azurelargeinstance/azurelargeinstances/{}/start", "2024-04-10"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.azurelargeinstance/azurelargeinstances/{}/start", "2024-08-01-preview"],
         ]
     }
 
@@ -140,7 +140,7 @@ class Start(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2024-04-10",
+                    "api-version", "2024-08-01-preview",
                     required=True,
                 ),
             }
