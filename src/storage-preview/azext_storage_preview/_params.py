a8cd4d324c95e7bfbb74505ff4b2d6e0ff71bdac
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2024-09-04 17:29:17 +0800

    adding `--extended-groups` to `az storage account local-user create/update` add ` --enable-extended-groups` for `az storage account create`

--- src/storage-preview/azext_storage_preview/_params.py
+++ src/storage-preview/azext_storage_preview/_params.py
@@ -315,6 +315,8 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
         c.argument('public_network_access', arg_type=get_enum_type(public_network_access_enum), min_api='2021-06-01',
                    help='Enable or disable public network access to the storage account. '
                         'Possible values include: `Enabled` or `Disabled`.')
+        c.argument('enable_extended_groups', arg_type=get_three_state_flag(),
+                   help='Enables extended group support with local users feature, if set to true.')
 
     with self.argument_context('storage account update', resource_type=CUSTOM_MGMT_STORAGE) as c:
         t_tls_version = self.get_models('MinimumTlsVersion', resource_type=CUSTOM_MGMT_STORAGE)
@@ -387,6 +389,8 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
                         'Possible values include: `Enabled` or `Disabled`.')
         c.argument('account_name', acct_name_type, options_list=['--name', '-n'])
         c.argument('resource_group_name', required=False, validator=process_resource_group)
+        c.argument('enable_extended_groups', arg_type=get_three_state_flag(),
+                   help='Enables extended group support with local users feature, if set to true.')
 
     for scope in ['storage account create', 'storage account update']:
         with self.argument_context(scope, arg_group='Customer managed key', min_api='2017-06-01',
@@ -951,5 +955,7 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
                             'Set it to false to disallow using ACL authorization.')
             c.argument('extended_groups', nargs='+',
                        help='Supplementary group membership. Only applicable for local users enabled for NFSv3 access.')
-            c.argument('is_nfsv3_enabled', arg_type=get_three_state_flag(),
-                       help='Indicates if the local user is enabled for access with NFSv3 protocol.')
+
+    with self.argument_context('storage account local-user create') as c:
+        c.argument('is_nfsv3_enabled', arg_type=get_three_state_flag(),
+            help='Indicates if the local user is enabled for access with NFSv3 protocol.')
