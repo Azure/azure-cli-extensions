313c03b84e6d40f4f169e1aa869ef42111e9f1c4
jkore1911 <jkore@microsoft.com>
2024-09-20 20:59:11 +0530

    CLI Refresh for Elastic

--- src/elastic/azext_elastic/tests/latest/test_elastic_scenario.py
+++ src/elastic/azext_elastic/tests/latest/test_elastic_scenario.py
@@ -50,6 +50,12 @@ class ElasticScenario(ScenarioTest):
         self.cmd('elastic monitor list-deployment-info --monitor-name {monitor} -g {rg}', checks=[
             self.check('status', 'Healthy')
         ])
+        self.cmd('elastic monitor list-connected-partner-resource --monitor-name {monitor} -g {rg}', checks=[
+            self.check('type(@)', 'array')
+        ])
+        self.cmd('elastic monitor get-billing-info --monitor-name {monitor} -g {rg}', checks=[
+                 self.check('length(marketplaceSaasInfo)', 5)
+        ])
         self.cmd('elastic monitor list-resource --monitor-name {monitor} -g {rg}')
         self.cmd('elastic monitor list-upgradable-version --monitor-name {monitor} -g {rg}')
         self.cmd('elastic monitor list-vm-host --monitor-name {monitor} -g {rg}')
@@ -114,3 +120,51 @@ class ElasticScenario(ScenarioTest):
             self.check('properties.logRules.sendActivityLogs', False),
             self.check('properties.logRules.sendSubscriptionLogs', False)
         ])
+    @ResourceGroupPreparer(name_prefix='cli_test_elastic_monitor', location='eastus')
+    def test_elastic_monitor_open_ai_integration(self, resource_group):
+        email = self.cmd('account show').get_output_in_json()['user']['name']
+        self.kwargs.update({
+            'monitor': self.create_random_name('monitor', 20),
+            'email': email
+        })
+        self.cmd('elastic monitor create -n {monitor} -g {rg} --user-info {{firstName:Alice,lastName:bob,companyName:Micosoft,emailAddress:{email}}} --sku {{name:ess-monthly-consumption_Monthly}}', checks=[
+            self.check('name', '{monitor}'),
+            self.check('resourceGroup', '{rg}'),
+            self.check('sku.name', 'ess-monthly-consumption_Monthly'),
+        ])
+        self.cmd('elastic monitor open-ai-integration create -n default -g {rg} --monitor-name {monitor} --key {api_key} --open-ai-connector-id {connector_id} --open-ai-resource-endpoint {endpoint} --open-ai-resource-id {resource_id}', checks=[
+            self.check('name', 'default'),
+            self.check('resourceGroup', '{rg}'),
+            self.check('properties.key', '{api_key}'),
+            self.check('properties.openAIConnectorId', '{connector_id}'),
+            self.check('properties.openAIResourceEndpoint', '{endpoint}'),
+            self.check('properties.openAIResourceId', '{resource_id}'),
+        ])
+        self.cmd('elastic monitor open-ai-integration update -n default -g {rg} --monitor-name {monitor} --key {openai_key} --open-ai-connector-id {connector_id} --open-ai-resource-endpoint {resource_endpoint} --open-ai-resource-id {resource_id}', checks=[
+            self.check('name', 'default'),
+            self.check('resourceGroup', '{rg}'),
+            self.check('properties.key', '{openai_key}'),
+            self.check('properties.openAIConnectorId', '{connector_id}'),
+            self.check('properties.openAIResourceEndpoint', '{resource_endpoint}'),
+            self.check('properties.openAIResourceId', '{resource_id}')
+        ])
+        self.cmd('elastic monitor open-ai-integration list -g {rg} --monitor-name {monitor}', checks=[
+            self.check('[0].name', 'openAIIntegrationRule1'),
+            self.check('[0].properties.open_ai_connector_id', 'connectorID1'),
+            self.check('[0].properties.open_ai_resource_id', 'resourceID1'),
+            self.check('[0].properties.open_ai_resource_endpoint', 'https://openai.endpoint'),
+            self.check('[0].properties.last_refresh_at', '2024-09-20T00:00:00Z'),
+            self.check('[0].type', 'Microsoft.Elastic/monitors/openAIIntegrations')
+        ])
+        self.cmd('elastic monitor open-ai-integration show -n default -g {rg} --monitor-name {monitor}', checks=[
+            self.check('name', 'default'),
+            self.check('resourceGroup', '{rg}'),
+            self.check('properties.openAIConnectorId', '{open_ai_connector_id}'),
+            self.check('properties.openAIResourceEndpoint', '{open_ai_resource_endpoint}'),
+            self.check('properties.openAIResourceId', '{open_ai_resource_id}'),
+            self.check('properties.lastRefreshAt', '{last_refresh_at}')
+        ])
+
+
+
+
