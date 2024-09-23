00e37f395e848117754ec894e9795c7a33bd6f4f
Xiaofan Zhou <xiaofanzhou@microsoft.com>
2024-09-23 13:01:40 +0800

    style

--- src/serviceconnector-passwordless/azext_serviceconnector_passwordless/_credential_free.py
+++ src/serviceconnector-passwordless/azext_serviceconnector_passwordless/_credential_free.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-# pylint: disable=no-member, too-many-lines, anomalous-backslash-in-string, redefined-outer-name, no-else-raise, attribute-defined-outside-init
+# pylint: disable=no-member, too-many-lines, anomalous-backslash-in-string, redefined-outer-name, no-else-raise, attribute-defined-outside-init,too-many-positional-arguments
 
 import struct
 import sys
