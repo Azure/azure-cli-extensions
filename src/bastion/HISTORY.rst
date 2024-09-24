fbcd995aef68ab0cc8829b2413c2026704b46f9a
aavalang <56377848+aavalang@users.noreply.github.com>
2024-09-23 23:03:40 -0700

    {AzureBastion] Bug fixes for connect introduced in 1.20  (#8020)
    * fix for issues 8003 and 8004
    
    * version bump
    
    * version bump

--- src/bastion/HISTORY.rst
+++ src/bastion/HISTORY.rst
@@ -2,14 +2,18 @@
 
 Release History
 ===============
-1.1.1
+1.3.0
 ++++++
-* Fix for configure option, enabling it for disable gateway and ip connect scenarios.
+* Bug fixes for connect rdp/ssh/tunnel.
 
 1.2.0
 ++++++
 * Update SDk to 2024-01-01
 * Allow create bastion with zones and session recording
+
+1.1.1
+++++++
+* Fix for configure option, enabling it for disable gateway and ip connect scenarios.
 
 1.1.0
 ++++++
--- src/bastion/azext_bastion/aaz/latest/network/bastion/_update.py
+++ src/bastion/azext_bastion/aaz/latest/network/bastion/_update.py
@@ -47,7 +47,7 @@ class Update(AAZCommand):
         # define Arg Group ""
 
         _args_schema = cls._args_schema
-        _args_schema.bastion_host_name = AAZStrArg(
+        _args_schema.name = AAZStrArg(
             options=["-n", "--name", "--bastion-host-name"],
             help="The name of the Bastion Host.",
             required=True,
@@ -318,7 +318,7 @@ class Update(AAZCommand):
         def url_parameters(self):
             parameters = {
                 **self.serialize_url_param(
-                    "bastionHostName", self.ctx.args.bastion_host_name,
+                    "bastionHostName", self.ctx.args.name,
                     required=True,
                 ),
                 **self.serialize_url_param(
@@ -417,7 +417,7 @@ class Update(AAZCommand):
         def url_parameters(self):
             parameters = {
                 **self.serialize_url_param(
-                    "bastionHostName", self.ctx.args.bastion_host_name,
+                    "bastionHostName", self.ctx.args.name,
                     required=True,
                 ),
                 **self.serialize_url_param(
