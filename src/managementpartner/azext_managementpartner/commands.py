186eed0b52ec5e2c4eca53b05d7ed41f1917b0dd
RuiJun Hu (MSFT) <v-ruih@microsoft.com>
2024-09-26 18:17:26 +0800

    fix style

--- src/managementpartner/azext_managementpartner/commands.py
+++ src/managementpartner/azext_managementpartner/commands.py
@@ -7,11 +7,11 @@
 def load_command_table(self, _):
 
     with self.command_group('managementpartner'):
-        
+
         from .aaz.latest.managementpartner import Create, Delete, Update
         self.command_table['managementpartner create'] = Create(loader=self)
         self.command_table['managementpartner delete'] = Delete(loader=self)
         self.command_table['managementpartner update'] = Update(loader=self)
-        
+
         from .custom import Show
         self.command_table['managementpartner show'] = Show(loader=self)
