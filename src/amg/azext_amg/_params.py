7d15f2551d3516fdb33092e327469ab6a3ceffe7
Alan Zhang <alanzhang@microsoft.com>
2024-10-01 13:29:45 -0700

    Replace underscore in param

--- src/amg/azext_amg/_params.py
+++ src/amg/azext_amg/_params.py
@@ -126,5 +126,5 @@ def load_arguments(self, _):
 
     with self.argument_context("grafana integrations monitor") as c:
         c.argument("monitor_name", help="name of the Azure Monitor workspace")
-        c.argument("monitor_resource_group_name", options_list=["--monitor_resource_group_name", "--monitor-rg-name"], help="name of the resource group of the Azure Monitor workspace")
+        c.argument("monitor_resource_group_name", options_list=["--monitor-resource-group-name", "--monitor-rg-name"], help="name of the resource group of the Azure Monitor workspace")
         c.argument("skip_role_assignments", options_list=["-s", "--skip-role-assignments"], arg_type=get_three_state_flag(), help="skip assigning the appropriate role on the Azure Monitor workspace to let Grafana read data from it. Default: false")
