e7708979e0d222d51c37b1d84b33675319c4a8f2
OrosBogdanAndrei <97595460+OrosBogdanAndrei@users.noreply.github.com>
2024-10-09 10:55:03 +0300

    changed param description for kb-numbers-to-include (#8043)
    * changed param description for kb-numbers-to-include
    
    * updated version
    cleaned up comments
    
    * addressed github actions requirements
    
    * Update HISTORY.rst
    
    * Update setup.py
    
    * Update src/maintenance/setup.py
    
    Co-authored-by: kai ru <69238381+kairu-ms@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Bogdan Oros <boros@microsoft.com>
    Co-authored-by: kai ru <69238381+kairu-ms@users.noreply.github.com>

--- src/maintenance/azext_maintenance/aaz/latest/maintenance/assignment/_update_subscription.py
+++ src/maintenance/azext_maintenance/aaz/latest/maintenance/assignment/_update_subscription.py
@@ -19,7 +19,7 @@ class UpdateSubscription(AAZCommand):
     """Update configuration for resource.
 
     :example: ConfigurationAssignments_UpdateSubscription
-        az maintenance assignment update-subscription  --maintenance-configuration-id "/subscriptions/00000000-0000-0000-0000-00000000/resourcegroups/exmaplerg2/providers/Microsoft. Maintenance/maintenanceConfigurations/config1"  --name assignmentname  --filter-locations eastus2euap centraluseuap  --filter-os-types windows linux  --filter-tags "{{tagKey1:[tagKey1Val1,tagKey1Val2],tagKey2:[tagKey2Val1,tagKey2Val2]}}"  --filter-tags-operator All
+        az maintenance assignment update-subscription  --maintenance-configuration-id "/subscriptions/00000000-0000-0000-0000-00000000/resourcegroups/exmaplerg2/providers/Microsoft. Maintenance/maintenanceConfigurations/config1"  --name assignmentname  --filter-locations eastus2euap centraluseuap  --filter-os-types windows linux  --filter-tags "{tagKey1:[tagKey1Val1,tagKey1Val2],tagKey2:[tagKey2Val1,tagKey2Val2]}"  --filter-tags-operator All
     """
 
     _aaz_info = {
