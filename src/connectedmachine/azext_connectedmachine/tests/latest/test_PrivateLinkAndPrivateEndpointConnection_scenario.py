6f976263266fa78a0757e2145428717286067241
Yao Tong <yaotong@microsoft.com>
2024-10-10 23:12:45 -0400

    update tests

--- src/connectedmachine/azext_connectedmachine/tests/latest/test_PrivateLinkAndPrivateEndpointConnection_scenario.py
+++ src/connectedmachine/azext_connectedmachine/tests/latest/test_PrivateLinkAndPrivateEndpointConnection_scenario.py
@@ -37,7 +37,7 @@ class PrivateLinkAndPrivateEndpointConnectionScenarioTest(ScenarioTest):
 
     @ResourceGroupPreparer(name_prefix='cli_test_privatelink')
     def test_private_link(self):
-        rand_string = 'test2'
+        rand_string = 'test3'
         self.kwargs.update({
             'machine': 'LAPTOP-S0HSJ7FB',
             'rg': 'ytongtest',
