9c5717ad4d37bba5e306fd2b137b50f2b87b0c3f
Sandido <adamsandor930@gmail.com>
2024-10-24 11:46:27 -0400

    better message

--- src/vm-repair/azext_vm_repair/_params.py
+++ src/vm-repair/azext_vm_repair/_params.py
@@ -31,7 +31,7 @@ def load_arguments(self, _):
         c.argument('unlock_encrypted_vm', help='Option to auto-unlock encrypted VMs using current subscription auth.')
         c.argument('encrypt_recovery_key', help='Option to auto-unlock encrypted VMs using provided recovery password.')
         c.argument('enable_nested', help='enable nested hyperv.')
-        c.argument('associate_public_ip', help='Option to create repair vm with public ip. If this parameter is not used, a private ip will be made.')
+        c.argument('associate_public_ip', help='Option to create a repair vm with a public ip. If this parameter is not used, a private ip will be made.')
         c.argument('distro', help='Option to create repair vm from a specific linux distro (rhel7|rhel8|sles12|sles15|ubuntu20|centos7|centos8|oracle7)')
         c.argument('yes', help='Option to skip prompt for associating public ip in no Tty mode')
         c.argument('disable_trusted_launch', help='Option to disable Trusted Launch security type on the repair vm by setting the security type to Standard.')
