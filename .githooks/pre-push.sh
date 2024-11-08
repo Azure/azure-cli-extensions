762c606123a315314d4d11ed18896a30f396d7b8
kairu <kairu@microsoft.com>
2024-11-08 05:24:43 +0000

    fix error message

--- .githooks/pre-push.sh
+++ .githooks/pre-push.sh
@@ -2,19 +2,10 @@
 
 echo "\033[0;32mRunning pre-push hook in bash ...\033[0m"
 
-<<<<<<< HEAD
 # run azdev_active script
-$scriptPath = Join-Path $PSScriptRoot "azdev_active.sh"
-. $scriptPath
+scriptPath="${0%/*}/azdev_active.sh"
+. "$scriptPath"
 if [ $? -ne 0 ]; then
-=======
-# Check if in the python environment
-PYTHON_FILE=$(which python)
-echo "Python file path: $PYTHON_FILE"
-
-if [ -z "$PYTHON_FILE" ]; then
-    echo "\033[0;31mError: Please activate the python environment first.\033[0m"
->>>>>>> 95083cf1a (fix error message)
     exit 1
 fi
 
