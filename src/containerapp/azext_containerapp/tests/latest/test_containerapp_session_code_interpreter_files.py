cc0d1dfe19c56019a4a0da90e7a5494c5d90062c
Yitao Pan <yitaopan@microsoft.com>
2024-11-11 14:31:06 +0800

    Add new test for session file path

--- src/containerapp/azext_containerapp/tests/latest/test_containerapp_session_code_interpreter_files.py
+++ src/containerapp/azext_containerapp/tests/latest/test_containerapp_session_code_interpreter_files.py
@@ -14,10 +14,10 @@ from .utils import create_containerapp_env
 
 TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
-class ContainerAppSessionCodeInterperterTests(ScenarioTest):
+class ContainerAppSessionCodeInterperterFilesTests(ScenarioTest):
     @AllowLargeResponse(8192)
     @ResourceGroupPreparer()
-    def test_containerapp_session_code_interpreter_e2e(self, resource_group):
+    def test_containerapp_session_code_interpreter_files_e2e(self, resource_group):
         location = TEST_LOCATION
         self.cmd('configure --defaults location={}'.format(location))
 
@@ -56,53 +56,61 @@ class ContainerAppSessionCodeInterperterTests(ScenarioTest):
         ])
 
         # upload a file also add session pool location
-        txt_file = os.path.join(TEST_DIR, 'cert.txt')
-        self.cmd('containerapp session code-interpreter upload-file -n {} -g {} --identifier {} --filepath "{}" --session-pool-location {}'.format(
+        txt_file_name = 'cert.txt'
+        txt_file = os.path.join(TEST_DIR, txt_file_name)
+        path = '/'
+        self.cmd('containerapp session code-interpreter upload-file -n {} -g {} --identifier {} --filepath "{}" --path {} --session-pool-location {}'.format(
             sessionpool_name_python,
             resource_group,
             identifier_name,
             txt_file,
+            path,
             TEST_LOCATION),
             checks=[
-            JMESPathCheck('name', 'cert.txt'),
+            JMESPathCheck('name', txt_file_name),
         ])
 
         # list files
-        files_list = self.cmd("containerapp session code-interpreter list-files -n {} -g {} --identifier {}".format(
+        files_list = self.cmd("containerapp session code-interpreter list-files -n {} -g {} --identifier {} --path {}".format(
             sessionpool_name_python,
             resource_group,
-            identifier_name)).get_output_in_json()
+            identifier_name,
+            path)).get_output_in_json()
         self.assertTrue(len(files_list["value"]) == 1)
 
         # check content
-        file_content = self.cmd("containerapp session code-interpreter show-file-content -n {} -g {} --identifier {} --filename {}".format(
+        file_content = self.cmd("containerapp session code-interpreter show-file-content -n {} -g {} --identifier {} --filename {} --path {}".format(
             sessionpool_name_python,
             resource_group,
             identifier_name,
-            "cert.txt")).get_output_in_json()
+            txt_file_name,
+            path)).get_output_in_json()
         self.assertTrue(file_content == '\"testing\"')
 
         # check metadata
-        self.cmd("containerapp session code-interpreter show-file-metadata -n {} -g {} --identifier {} --filename {}".format(
+        self.cmd("containerapp session code-interpreter show-file-metadata -n {} -g {} --identifier {} --filename {} --path {}".format(
             sessionpool_name_python,
             resource_group,
             identifier_name,
-            "cert.txt"),
+            txt_file_name,
+            path),
             checks=[
-            JMESPathCheck('name', 'cert.txt'),
+            JMESPathCheck('name', txt_file_name),
         ])
 
         # delete file
-        self.cmd("containerapp session code-interpreter delete-file -n {} -g {} --identifier {} --filename {} --yes".format(
+        self.cmd("containerapp session code-interpreter delete-file -n {} -g {} --identifier {} --filename {} --path {} --yes".format(
             sessionpool_name_python,
             resource_group,
             identifier_name,
-            "cert.txt"
+            txt_file_name,
+            path
             ))
-        files_list = self.cmd("containerapp session code-interpreter list-files -n {} -g {} --identifier {}".format(
+        files_list = self.cmd("containerapp session code-interpreter list-files -n {} -g {} --identifier {} --path {}".format(
             sessionpool_name_python,
             resource_group,
-            identifier_name
+            identifier_name,
+            path
             )).get_output_in_json()
         self.assertTrue(len(files_list["value"]) == 0)
 
