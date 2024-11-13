161ed5d6dad3211da7edbbf7436fa0ac4b321c2a
RuiJun Hu (MSFT) <v-ruih@microsoft.com>
2024-11-13 14:49:02 +0800

    lb destination

--- src/virtual-network-tap/azext_vnettap/tests/latest/test_virtual_network_tap.py
+++ src/virtual-network-tap/azext_vnettap/tests/latest/test_virtual_network_tap.py
@@ -13,7 +13,7 @@ import json
 
 class VirtualNetworkTAPScenarioTest(ScenarioTest):
 
-    @ResourceGroupPreparer(name_prefix= 'cli_test_vnet_tap_', location='westus', random_name_length=40)
+    @ResourceGroupPreparer(name_prefix= 'cli_test_vnet_tap_', location='eastus2', random_name_length=40)
     def test_vnet_tap(self, resource_group):
 
         self.kwargs.update({
@@ -24,7 +24,9 @@ class VirtualNetworkTAPScenarioTest(ScenarioTest):
             'nic': 'destnic',
             'nic_ipconfig': 'nicipconfig1',
             'tap1': 'tap1',
-            'vtap_config': 'vtapconfig',
+            'tap2': 'tap2',
+            'vtap_config1': 'vtapconfig1',
+            'vtap_config2': 'vtapconfig2'
         })
         self.cmd('network vnet create -g {rg} -n {vnet1} --subnet-name {subnet1}')
         self.cmd('network nic create -g {rg} -n {source_nic} --subnet {subnet1} --vnet-name {vnet1} --accelerated-networking true --auxiliary-mode AcceleratedConnections --auxiliary-sku A1', checks=[
@@ -32,6 +34,7 @@ class VirtualNetworkTAPScenarioTest(ScenarioTest):
                   self.check('NewNIC.auxiliarySku', 'A1')
                 ])
 
+        # case: create vtap with nic destination
         self.cmd('network nic create -g {rg} -n {nic} --subnet {subnet1} --vnet-name {vnet1} ')
         self.cmd('network nic ip-config create -g {rg} --nic-name {nic} --name {nic_ipconfig}')
         self.kwargs['nic_dest'] = self.cmd('network nic ip-config show -g {rg} --nic-name {nic} --name {nic_ipconfig}').get_output_in_json()['id']
@@ -39,7 +42,19 @@ class VirtualNetworkTAPScenarioTest(ScenarioTest):
                   self.check('type', 'Microsoft.Network/virtualNetworkTaps'),
                   self.check('destinationNetworkInterfaceIPConfiguration.id', self.kwargs['nic_dest'])
                 ])
-        self.cmd('network nic vtap-config create -g {rg} -n {vtap_config} --nic {source_nic} --vnet-tap {tap1}', checks=[
+        # create vtap config
+        self.cmd('network nic vtap-config create -g {rg} -n {vtap_config1} --nic {source_nic} --vnet-tap {tap1}', checks=[
                   self.check('type', 'Microsoft.Network/networkInterfaces/tapConfigurations'),
-                  self.check('name', self.kwargs['vtap_config'])
+                  self.check('name', self.kwargs['vtap_config1'])
                 ])
+        self.cmd('network nic vtap-config delete -g {rg} -n {vtap_config1} --nic {source_nic} -y')
+
+        # case: create vtap with lb destination
+        self.cmd('network lb create -g {rg} -n {lb}  --sku Standard --subnet {subnet1} --vnet-name {vnet1}')
+        self.kwargs['lb_dest'] = self.cmd('network lb frontend-ip show -g {rg} --lb-name {lb} --name LoadBalancerFrontEnd').get_output_in_json()['id']
+        self.cmd('network vnet tap create -g {rg} -n {tap2} --destination {lb_dest}', checks=[
+                  self.check('type', 'Microsoft.Network/virtualNetworkTaps'),
+                  self.check('name', self.kwargs['tap2'])
+                ])
+        self.cmd('network nic vtap-config create -g {rg} -n {vtap_config2} --nic {source_nic} --vnet-tap {tap2}')
+        self.cmd('network nic vtap-config delete -g {rg} -n {vtap_config2} --nic {source_nic} -y')
