e1760f40a395ea1ef5faaf135a4aa5e22933db14
necusjz <necusjz@gmail.com>
2024-11-25 14:23:31 +0800

    {Misc.} Enable devcontainer (#8300)
    * feat: initialize devcontainer
    
    * feat: add login logic
    
    * feat: add setup logic
    
    * chore: dynamic repo name
    
    * fix: unable to locate cli
    
    * chore: sync with upstream
    
    * chore: use git instead
    
    * chore: obtain head branch name

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# define color codes
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+NC="\033[0m"  # no color
+
+setup_repo() {
+    local DIR_NAME="$1"
+    local DIR_PATH="/workspaces/$DIR_NAME"
+    local REPO="Azure/$DIR_NAME"
+
+    if [ -d "$DIR_PATH" ]; then
+        echo -e "\n${YELLOW}($DIR_NAME) Pulling the latest changes from upstream...${NC}"
+    else
+        echo -e "\n${GREEN}($DIR_NAME) Forking and cloning the repository...${NC}"
+        gh repo fork "$REPO" --clone=true
+    fi
+
+    HEAD_BRANCH=$(git -C "$DIR_PATH" remote show upstream | grep "HEAD branch" | awk '{print $NF}')
+    git -C "$DIR_PATH" pull -r upstream "$HEAD_BRANCH"
+}
+
+SECONDS=0
+
+pip install aaz-dev
+
+# azdev repositories
+setup_repo "azure-cli"
+setup_repo "azure-cli-extensions"
+
+azdev setup -c -r ./azure-cli-extensions
+
+# aaz-dev repositories
+setup_repo "aaz"
+setup_repo "azure-rest-api-specs"
+setup_repo "azure-rest-api-specs-pr"
+
+ELAPSED_TIME=$SECONDS
+echo -e "\n${YELLOW}Elapsed time: $((ELAPSED_TIME / 60)) min $((ELAPSED_TIME % 60)) sec.${NC}"
+echo -e "${GREEN}Finished setup! Please launch the codegen tool via \`aaz-dev run -c azure-cli -e azure-cli-extensions -s azure-rest-api-specs -a aaz\`.${NC}\n"
