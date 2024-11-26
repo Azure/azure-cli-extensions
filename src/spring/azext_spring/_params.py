fedface9e3bebd9e5982e31c8f556db85e5f27cb
RuiJun Hu (MSFT) <v-ruih@microsoft.com>
2024-11-26 16:31:51 +0800

    update summary
    Co-authored-by: AllyW <AllyW@users.noreply.github.com>

--- src/spring/azext_spring/_params.py
+++ src/spring/azext_spring/_params.py
@@ -60,7 +60,7 @@ env_type = CLIArgumentType(
     validator=validate_env, help="Space-separated environment variables in 'key[=value]' format.", nargs='*')
 build_env_type = CLIArgumentType(
     validator=validate_build_env, help="Space-separated environment variables in 'key[=value]' format.", nargs='*')
-service_name_type = CLIArgumentType(options_list=['--service', '-s'], help='The name of Azure Spring Apps instance, you can configure the default service using `az configure --defaults spring=<name>.`', configured_default='spring')
+service_name_type = CLIArgumentType(options_list=['--service', '-s'], help='The name of Azure Spring Apps instance, you can configure the default service using `az configure --defaults spring=<name>`.', configured_default='spring')
 app_name_type = CLIArgumentType(help='App name, you can configure the default app using az configure --defaults spring-cloud-app=<name>.', validator=validate_app_name, configured_default='spring-app')
 sku_type = CLIArgumentType(arg_type=get_enum_type(['Basic', 'Standard', 'Enterprise', 'StandardGen2']), help='Name of SKU.')
 source_path_type = CLIArgumentType(nargs='?', const='.',
