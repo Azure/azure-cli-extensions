1a105cc2a7561a83fb2a9004569ced01b55cb003
Warren Jones (Insight Global LLC) <v-warrjones@microsoft.com>
2024-12-05 10:09:58 -0800

    Expanded the unit test

--- src/quantum/azext_quantum/tests/latest/test_quantum_jobs.py
+++ src/quantum/azext_quantum/tests/latest/test_quantum_jobs.py
@@ -204,6 +204,18 @@ class QuantumJobsScenarioTest(ScenarioTest):
         self.cmd(f"az quantum workspace create -g {test_resource_group} -w {test_workspace_temp} -l {test_location} -a {test_storage} -r {test_provider_sku_list} --skip-autoadd")
         self.cmd(f"az quantum workspace set -g {test_resource_group} -w {test_workspace_temp} -l {test_location}")
 
+        # Submit a job to Rigetti and look for SAS tokens in URIs in the output
+        results = self.cmd("az quantum job submit -t rigetti.sim.qvm --job-input-format rigetti.quil.v1 -t rigetti.sim.qvm --job-input-file src/quantum/azext_quantum/tests/latest/input_data/bell-state.quil --job-output-format rigetti.quil-results.v1 -o json").get_output_in_json()
+        self.assertIsNotNone(results["containerUri"])
+        self.assertIn("?se", results["containerUri"])
+        self.assertIn("&sp=racw&sv=", results["containerUri"])
+        self.assertIn("&sr=", results["containerUri"])
+        self.assertIn("&sig=", results["containerUri"])
+        self.assertIsNotNone(results["inputDataUri"])
+        self.assertIn("&sig=", results["inputDataUri"])
+        self.assertIsNotNone(results["outputDataUri"])
+        self.assertIn("&sig=", results["outputDataUri"])
+
         # Run a Quil pass-through job on Rigetti
         results = self.cmd("az quantum run -t rigetti.sim.qvm --job-input-format rigetti.quil.v1 -t rigetti.sim.qvm --job-input-file src/quantum/azext_quantum/tests/latest/input_data/bell-state.quil --job-output-format rigetti.quil-results.v1 -o json").get_output_in_json()
         self.assertIn("ro", results)
@@ -211,13 +223,6 @@ class QuantumJobsScenarioTest(ScenarioTest):
         # Run a Qiskit pass-through job on IonQ
         results = self.cmd("az quantum run -t ionq.simulator --shots 100 --job-input-format ionq.circuit.v1 --job-input-file src/quantum/azext_quantum/tests/latest/input_data/Qiskit-3-qubit-GHZ-circuit.json --job-output-format ionq.quantum-results.v1 --job-params count=100 content-type=application/json -o json").get_output_in_json()
         self.assertIn("histogram", results)
-
-        # Submit job to Rigetti and look for evidence of SAS tokens in the URIs
-        results = self.cmd("az quantum job submit -t rigetti.sim.qvm --job-input-format rigetti.quil.v1 -t rigetti.sim.qvm --job-input-file src/quantum/azext_quantum/tests/latest/input_data/bell-state.quil --job-output-format rigetti.quil-results.v1 -o json").get_output_in_json()
-        self.assertIn("?se", results)
-        self.assertIn("&sp=racw&sv=", results)
-        self.assertIn("&sr=", results)
-        self.assertIn("&sig=", results)
 
         self.cmd(f'az quantum workspace delete -g {test_resource_group} -w {test_workspace_temp}')
 
