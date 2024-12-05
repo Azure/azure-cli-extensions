f1985f72affac8715db6d8ec04fa103f51700f2d
RuiJun Hu (MSFT) <v-ruih@microsoft.com>
2024-12-05 11:13:59 +0800

    fix html tag lint (#8347)

--- src/new-relic/azext_new_relic/aaz/latest/new_relic/monitor/_list.py
+++ src/new-relic/azext_new_relic/aaz/latest/new_relic/monitor/_list.py
@@ -47,7 +47,7 @@ class List(AAZCommand):
 
         _args_schema = cls._args_schema
         _args_schema.resource_group = AAZResourceGroupNameArg(
-            help="Name of resource group. You can configure the default group using az configure --defaults group=<name>.",
+            help="Name of resource group. You can configure the default group using `az configure --defaults group=<name>`.",
         )
         return cls._args_schema
 
--- src/new-relic/azext_new_relic/aaz/latest/new_relic/monitor/monitored_subscription/_create.py
+++ src/new-relic/azext_new_relic/aaz/latest/new_relic/monitor/monitored_subscription/_create.py
@@ -66,7 +66,7 @@ class Create(AAZCommand):
         )
         _args_schema.resource_group = AAZResourceGroupNameArg(
             options=["--resource-group","--g"],
-            help="Name of resource group. You can configure the default group using az configure --defaults group=<name>.",
+            help="Name of resource group. You can configure the default group using `az configure --defaults group=<name>`.",
             required=True,
         )
 
