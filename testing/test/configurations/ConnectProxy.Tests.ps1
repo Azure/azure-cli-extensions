2358a49d601de41c413dfcc4380fdc7a534e5759
Jorge Daboub <52983326+JorgeDaboub@users.noreply.github.com>
2024-12-10 11:44:33 -0600

    Add Kubectl Check (#29)
    Add Kubectl Check to Proxy Test

--- testing/test/configurations/ConnectProxy.Tests.ps1
+++ testing/test/configurations/ConnectProxy.Tests.ps1
@@ -39,13 +39,49 @@ Describe 'Connectedk8s Proxy Scenario' {
         } -ArgumentList $ENVCONFIG.arcClusterName, $ENVCONFIG.resourceGroup
 
         # Wait for a certain amount of time (e.g., 30 seconds)
-        Start-Sleep -Seconds 30
+        Start-Sleep -Seconds 60
 
         # Display the output
         Write-Host "Proxy Job State: $($proxyJob.State)"
 
         # Check if the job ran successfully
         $proxyJob.State | Should -Be 'Running'
+        
+        # Check if the kubeconfig file has been updated to use the proxy
+        $kubeconfigPath = "~/.kube/config"
+        $kubeconfig = Get-Content $kubeconfigPath -Raw | ConvertFrom-Yaml
+        # Extract the current context
+        $currentContext = $kubeconfig.'current-context'
+
+        # Validate that the current context is for the arc machine
+        $currentContext | Should -Be $ENVCONFIG.arcClusterName
+
+        # Find the cluster associated with the current context
+        $context = $kubeconfig.contexts | Where-Object { $_.name -eq $currentContext }
+        $clusterName = $context.context.cluster
+
+        # Retrieve the server URL for the cluster
+        $cluster = $kubeconfig.clusters | Where-Object { $_.name -eq $clusterName }
+        $server = $cluster.cluster.server
+
+        # Validate the server URL
+        $server | Should -Match "^https://127.0.0.1:47011/proxies/"
+                
+        # Check if the proxy command ran successfully
+        $kubectlJob = Start-Job -ScriptBlock {
+            try {
+                $output =  kubectl get pods -n azure-arc 2>&1
+                return @{ Success = $LASTEXITCODE -eq 0; Output = $output }
+            } catch {
+                return @{ Success = $false; Output = $_.Exception.Message }
+            }
+        }
+
+        $kubectlJob | Wait-Job
+        $kubectlResult = Receive-Job -Job $kubectlJob
+
+        # Assert that the result is 0
+        $kubectlResult.Success | Should -BeTrue
 
         Stop-Job -Job $proxyJob
         Remove-Job -Job $proxyJob
