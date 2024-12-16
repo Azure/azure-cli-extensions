28608511359268e212fa7d2d2ddf05e5673d1519
Amanda Lim <amlim@microsoft.com>
2024-12-16 10:00:03 -0800

    Update version

--- src/devcenter/azext_devcenter/commands.py
+++ src/devcenter/azext_devcenter/commands.py
@@ -456,7 +456,7 @@ def load_command_table(self, _):
     with self.command_group("devcenter dev customization-group") as g:
         g.custom_command("list", "devcenter_customization_group_list")
         g.custom_show_command("show", "devcenter_customization_group_show")
-        g.custom_command("create", "devcenter_customization_group_create", supports_no_wait=True)
+        g.custom_command("create", "devcenter_customization_group_create")
 
     with self.command_group("devcenter dev customization-task") as g:
         g.custom_command("list", "devcenter_customization_task_list")
