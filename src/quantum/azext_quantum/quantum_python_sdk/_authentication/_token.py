40af90129334b1f615df28b0e32af7d1666b2a9b
Warren Jones (Insight Global LLC) <v-warrjones@microsoft.com>
2024-12-23 12:09:42 -0800

    Deleted a commented-out line

--- src/quantum/azext_quantum/quantum_python_sdk/_authentication/_token.py
+++ src/quantum/azext_quantum/quantum_python_sdk/_authentication/_token.py
@@ -8,7 +8,6 @@ import logging
 import os
 import time
 
-# from azure.identity import CredentialUnavailableError
 # This "from" statement was changed so there is no external dependency on the azure.identity PyPI package
 from ...vendored_sdks.azure_identity import CredentialUnavailableError
 from azure.core.credentials import AccessToken
