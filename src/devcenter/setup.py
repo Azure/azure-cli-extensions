ab4d87e1dc26473ea85b8463a783fd47cff02fb6
RuiJun Hu (MSFT) <v-ruih@microsoft.com>
2024-12-24 15:44:24 +0800

    remove version import

--- src/devcenter/setup.py
+++ src/devcenter/setup.py
@@ -11,10 +11,6 @@ from setuptools import setup, find_packages
 
 # HISTORY.rst entry.
 VERSION = '6.2.3'
-try:
-    from azext_devcenter.manual.version import VERSION
-except ImportError:
-    pass
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
--- src/guestconfig/setup.py
+++ src/guestconfig/setup.py
@@ -11,10 +11,6 @@ from setuptools import setup, find_packages
 
 # HISTORY.rst entry.
 VERSION = '0.1.1'
-try:
-    from .manual.version import VERSION
-except ImportError:
-    pass
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
