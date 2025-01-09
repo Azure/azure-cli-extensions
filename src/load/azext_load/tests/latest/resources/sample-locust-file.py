81eb3ccad3ebc9a600e01b6f8a38bf4aa78c9569
Mouly Bhardwaj <mbhardwaj@microsoft.com>
2025-01-09 14:26:37 +0530

    azdev scan fix for locust file

--- src/load/azext_load/tests/latest/resources/sample-locust-file.py
+++ src/load/azext_load/tests/latest/resources/sample-locust-file.py
@@ -3,13 +3,16 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import urllib.parse
+
 from locust import HttpUser, constant_throughput, task
 
 base_url = "https://app-petclinic-yalrccgejv64o.azurewebsites.net/"
 
 
 class WebsiteUser(HttpUser):
-    host = base_url
+    
+    host = urllib.parse.quote(base_url, safe=':/')
     wait_time = constant_throughput(1)
 
     @task
