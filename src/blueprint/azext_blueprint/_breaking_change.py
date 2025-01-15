3720e19a1fab50131c126380f49a0258b5ff9045
Dante DG <test>
2025-01-15 17:11:16 -0500

    Adjusted deprecation message.

--- src/blueprint/azext_blueprint/_breaking_change.py
+++ src/blueprint/azext_blueprint/_breaking_change.py
@@ -10,9 +10,9 @@ from azure.cli.core.breaking_change import register_command_group_deprecate, reg
 register_command_group_deprecate('az blueprint')
 
 message = """Blueprints and associated commands will be deprecated
-' as early as July 2026. Customers are encouraged to transition to
-' Template Specs and Deployments Stacks to support their scenarios beyond that date.
-' Migration documentation is available at
-' https://learn.microsoft.com/en-us/azure/azure-resource-manager/bicep/migrate-blueprint."""
+as early as July 2026. Customers are encouraged to transition to
+Template Specs and Deployments Stacks to support their scenarios beyond that date.
+Migration documentation is available at
+https://learn.microsoft.com/en-us/azure/azure-resource-manager/bicep/migrate-blueprint."""
 
 register_other_breaking_change('az blueprint', message)
