ec4f21fff3506fa3b58c09ee9d8927ec19913d32
Karthik Gumpu <gumpukarthik@gmail.com>
2025-01-16 13:25:10 +0530

    Add commands for NSP Logging configuration - Microsoft.Network (#8137)
    * Add commands for NSP Loggign configuration
    
    * Update change history
    
    * Add tests
    
    * Updated commands
    
    * Add examples
    
    * Updated contract
    
    * Remove default value in Update command
    
    * Update tests
    
    ---------
    
    Co-authored-by: Karthik Gumpu (from Dev Box) <karthikgumpu@microsoft.com>

--- src/nsp/azext_nsp/tests/latest/test_nsp.py
+++ src/nsp/azext_nsp/tests/latest/test_nsp.py
@@ -199,3 +199,36 @@ class NspScenario(ScenarioTest):
 
         # delete link reference
         self.cmd('az network perimeter link-reference delete --perimeter-name {nsp2_name} --resource-group {rg} --name {ref2_name} --yes')
+    
+    @ResourceGroupPreparer(name_prefix='test_nsp_logging_configuration_crud', location='eastus2euap')
+    def test_nsp_logging_configuration_crud(self, resource_group):
+        self.kwargs.update({
+            'nsp_name': 'TestNetworkSecurityPerimeter',
+            'logging_config_name': 'instance'
+        })
+
+        # Create NSP
+        self.cmd('network perimeter create --name {nsp_name} -l eastus2euap --resource-group {rg}')
+
+        # Create logging configuration
+        self.cmd('network perimeter logging-configuration create --perimeter-name {nsp_name} --resource-group {rg} --enabled-log-categories "[\'NspPublicInboundPerimeterRulesDenied\']"')
+
+        # Show logging configuration and verify the enabled log categories
+        self.cmd('network perimeter logging-configuration show --perimeter-name {nsp_name} --resource-group {rg}', checks=[
+            self.check('properties.enabledLogCategories', "[\'NspPublicInboundPerimeterRulesDenied\']")
+        ])
+
+        # Update logging configuration
+        self.cmd('network perimeter logging-configuration update --name {logging_config_name} --perimeter-name {nsp_name} --resource-group {rg} --enabled-log-categories "[\'NspPublicInboundPerimeterRulesDenied\', \'NspPublicOutboundPerimeterRulesDenied\']"')
+
+        # Show logging configuration and verify the updated enabled log categories
+        self.cmd('network perimeter logging-configuration show --perimeter-name {nsp_name} --resource-group {rg}', checks=[
+            self.check('properties.enabledLogCategories', "[\'NspPublicInboundPerimeterRulesDenied\', \'NspPublicOutboundPerimeterRulesDenied\']")
+        ])
+
+        # Delete logging configuration
+        self.cmd('network perimeter logging-configuration delete --perimeter-name {nsp_name} --resource-group {rg} --yes')
+
+        # Verify the logging configuration is deleted
+        self.cmd('network perimeter logging-configuration show --perimeter-name {nsp_name} --resource-group {rg}', expect_failure=True)
+
