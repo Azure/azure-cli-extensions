0fb074081ee39d54a8c4765cb9815329a8060131
Priya Shet <priya.shet@gmail.com>
2025-01-27 20:41:04 -0600

    NeworkCloud : Azure cli extension updates for Networkcloud Api 2024-10-01-preview (#8415)
    * Azure cli extension updates for Networkcloud Api 2024-10-01-preview
    
    * updated cluster/VM paramters to have short name for options
    
    * update unit tests to resolve CI errors
    
    ---------
    
    Co-authored-by: priya shet <priyashet@microsoft.com>

--- src/networkcloud/azext_networkcloud/aaz/latest/networkcloud/kubernetescluster/_create.py
+++ src/networkcloud/azext_networkcloud/aaz/latest/networkcloud/kubernetescluster/_create.py
@@ -13,6 +13,7 @@ from azure.cli.core.aaz import *
 
 @register_command(
     "networkcloud kubernetescluster create",
+    is_preview=True,
 )
 class Create(AAZCommand):
     """Create a new Kubernetes cluster or update the properties of the existing one.
@@ -22,9 +23,9 @@ class Create(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2024-07-01",
+        "version": "2024-10-01-preview",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.networkcloud/kubernetesclusters/{}", "2024-07-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.networkcloud/kubernetesclusters/{}", "2024-10-01-preview"],
         ]
     }
 
@@ -847,7 +848,7 @@ class Create(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2024-07-01",
+                    "api-version", "2024-10-01-preview",
                     required=True,
                 ),
             }
--- src/networkcloud/azext_networkcloud/aaz/latest/networkcloud/volume/_list.py
+++ src/networkcloud/azext_networkcloud/aaz/latest/networkcloud/volume/_list.py
@@ -13,6 +13,7 @@ from azure.cli.core.aaz import *
 
 @register_command(
     "networkcloud volume list",
+    is_preview=True,
 )
 class List(AAZCommand):
     """List volumes in the provided resource group or subscription.
@@ -25,10 +26,10 @@ class List(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2024-07-01",
+        "version": "2024-10-01-preview",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/providers/microsoft.networkcloud/volumes", "2024-07-01"],
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.networkcloud/volumes", "2024-07-01"],
+            ["mgmt-plane", "/subscriptions/{}/providers/microsoft.networkcloud/volumes", "2024-10-01-preview"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.networkcloud/volumes", "2024-10-01-preview"],
         ]
     }
 
@@ -115,7 +116,7 @@ class List(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2024-07-01",
+                    "api-version", "2024-10-01-preview",
                     required=True,
                 ),
             }
@@ -288,7 +289,7 @@ class List(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2024-07-01",
+                    "api-version", "2024-10-01-preview",
                     required=True,
                 ),
             }
