77859376534fd3ec5a2e4b1963f53b14e2ca217f
Himanshu Bisht <himanshubishtcse@gmail.com>
2025-02-24 22:25:45 +0000

    Minor fix for compatiblity with python 3.9, 3.10 and 3.11

--- src/load/azext_load/data_plane/utils/utils.py
+++ src/load/azext_load/data_plane/utils/utils.py
@@ -748,7 +748,9 @@ def upload_zipped_artifacts_helper(
             if wait and file_response.get("validationStatus") not in ("VALIDATION_SUCCESS", "NOT_VALIDATED"):
                 # pylint: disable=line-too-long
                 raise FileOperationError(
-                    f"ZIP artifact {zip_artifact} is not valid. Please check the file and try again. Current file status is {file_response.get("validationStatus")}"
+                    "ZIP artifact {} is not valid. Please check the file and try again. Current file status is {}".format(
+                        zip_artifact, file_response.get("validationStatus")
+                    )
                 )
 
 
