5fa2a2fdd0ed5241b8540e767ba3d92fec1ffd25
Warren Jones (Insight Global LLC) <v-warrjones@microsoft.com>
2025-02-26 15:51:58 -0800

    Deleted redundant #s

--- src/quantum/azext_quantum/tests/latest/test_quantum_jobs.py
+++ src/quantum/azext_quantum/tests/latest/test_quantum_jobs.py
@@ -39,10 +39,10 @@ class QuantumJobsScenarioTest(ScenarioTest):
         targets = self.cmd('az quantum target list -o json').get_output_in_json()
         assert len(targets) > 0
 
-    # # @pytest.fixture(autouse=True)
-    # # def _pass_fixtures(self, capsys):
-    # #     self.capsys = capsys
-    # # # See "TODO" in issue_cmd_with_param_missing un utils.py
+    # @pytest.fixture(autouse=True)
+    # def _pass_fixtures(self, capsys):
+    #     self.capsys = capsys
+    # # See "TODO" in issue_cmd_with_param_missing un utils.py
 
     def test_job_errors(self):
         issue_cmd_with_param_missing(self, "az quantum job cancel", "az quantum job cancel -g MyResourceGroup -w MyWorkspace -l MyLocation -j yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy\nCancel an Azure Quantum job by id.")
