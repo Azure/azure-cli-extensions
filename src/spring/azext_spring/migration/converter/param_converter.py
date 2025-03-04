325e4233ffa98eb48fea70f02c9f0449cc1419ea
Guitar Sheng <guitarsheng@microsoft.com>
2025-03-05 01:12:09 +0800

    fix byos duplicate name issue

--- src/spring/azext_spring/migration/converter/param_converter.py
+++ src/spring/azext_spring/migration/converter/param_converter.py
@@ -5,8 +5,13 @@ class ParamConverter(ConverterTemplate):
     def load_source(self, source):
         self.apps = source['apps']
         self.is_vnet = source['isVnet']
+        self.storages = source['storages']
 
     def calculate_data(self):
+        storage_map = {
+            storage['name'].split('/')[-1]: storage['properties']['accountName'] 
+            for storage in self.storages
+        }        
         self.data.setdefault("apps", [])
         storage_configs = []
         for app in self.apps:
@@ -23,9 +28,17 @@ class ParamConverter(ConverterTemplate):
                     storage_id = disk_props.get('storageId', '')
                     storage_name = self._get_resource_name(storage_id) if storage_id else ''
                     app_name = app['name'].split('/')[-1]
-                    containerAppEnvStorageAccountKey = "containerAppEnvStorageAccountKey_" + (app_name + "_" + storage_name).replace("-", "")
+                    account_name = storage_map.get(storage_name, '')
+                    share_name = disk_props.get('customPersistentDiskProperties', '').get('shareName', '')
+                    readOnly = disk_props.get('customPersistentDiskProperties', False).get('readOnly', False)
+                    access_mode = 'ReadOnly' if readOnly else 'ReadWrite'
+                    mount_path = disk_props.get('customPersistentDiskProperties').get('mountPath')     
+                    storage_unique_name = self._get_storage_unique_name(storage_name, account_name, share_name, mount_path, access_mode)
+                    containerAppEnvStorageAccountKey = "containerAppEnvStorageAccountKey_" + storage_unique_name
+                    # print("storage_unique_name:", storage_unique_name)
                     storage_config = {
                         'containerAppEnvStorageAccountKey': containerAppEnvStorageAccountKey,
+                        'accountName': account_name,
                     }
                     storage_configs.append(storage_config)
         self.data["storages"] = storage_configs
