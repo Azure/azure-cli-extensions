216f59fb8a7d4e241efbe77785260580b9136528
Guitar Sheng <guitarsheng@microsoft.com>
2025-03-06 05:37:27 +0800

    refactor eureka

--- src/spring/azext_spring/migration/converter/eureka_converter.py
+++ src/spring/azext_spring/migration/converter/eureka_converter.py
@@ -4,23 +4,16 @@ class EurekaConverter(ConverterTemplate):
 
     def __init__(self, input):
         def extract_data():
-            # TODO: Implement the extract_data method
-            pass
+            name = "eureka"
+            configurations = []
+            replicas = 1
+
+            return {
+                "eurekaName": name,
+                "configurations": configurations,
+                "replicas": replicas
+            }
         super().__init__(input, extract_data)
-
-    def load_source(self, source):
-        self.source = source
-
-    def calculate_data(self):
-        name = "eureka"
-        configurations = []
-        replicas = 1
-
-        self.data = {
-            "eurekaName": name,
-            "configurations": configurations,
-            "replicas": replicas
-        }
 
     def get_template_name(self):
         return "eureka.bicep"
