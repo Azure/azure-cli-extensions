fdf4af24ed348d9e67c583cad8a85dd2d47a0e9d
Himanshu Bisht <himanshubishtcse@gmail.com>
2025-03-09 10:36:53 +0000

    Optimizing utils and addressing PR comments

--- src/load/azext_load/data_plane/load_trigger/utils.py
+++ src/load/azext_load/data_plane/load_trigger/utils.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from azext_load.vendored_sdks.loadtesting.models import _models as models
 from azext_load.vendored_sdks.loadtesting.models import _enums as enums
 from knack.log import get_logger
-from azure.cli.core.azclierror import InvalidArgumentValueError, FileOperationError
+from azure.cli.core.azclierror import InvalidArgumentValueError
 
 logger = get_logger(__name__)
 
@@ -30,6 +30,87 @@ def get_recurrence_end_body(end_after_occurrence, end_after_date_time, existing_
     
     return existing_recurrence_end
 
+def handle_daily_recurrence(recurrence_interval, recurrence_end_body, **kwargs):
+    if recurrence_interval is None:
+        raise InvalidArgumentValueError("Recurrence interval is required for daily recurrence.")
+    if any(kwargs.values()):
+        raise InvalidArgumentValueError("Only recurrence interval should be provided for daily recurrence.")
+    return models.DailyRecurrence(
+        interval=recurrence_interval,
+        recurrence_end=recurrence_end_body
+    )
+
+def handle_weekly_recurrence(recurrence_interval, recurrence_week_days, recurrence_end_body, **kwargs):
+    if recurrence_interval is None:
+        raise InvalidArgumentValueError("Recurrence interval is required for weekly recurrence.")
+    if recurrence_week_days is None or len(recurrence_week_days) == 0:
+        raise InvalidArgumentValueError("Recurrence week days are required for weekly recurrence.")
+    if any(kwargs.values()):
+        raise InvalidArgumentValueError("Only recurrence interval and week days should be provided for weekly recurrence.")
+    return models.WeeklyRecurrence(
+        interval=recurrence_interval,
+        days_of_week=recurrence_week_days,
+        recurrence_end=recurrence_end_body
+    )
+
+def handle_hourly_recurrence(recurrence_interval, recurrence_end_body, **kwargs):
+    if recurrence_interval is None:
+        raise InvalidArgumentValueError("Recurrence interval is required for hourly recurrence.")
+    if any(kwargs.values()):
+        raise InvalidArgumentValueError("Only recurrence interval should be provided for hourly recurrence.")
+    return models.HourlyRecurrence(
+        interval=recurrence_interval,
+        recurrence_end=recurrence_end_body
+    )
+
+def handle_monthly_by_dates_recurrence(recurrence_interval, recurrence_dates_in_month, recurrence_end_body, **kwargs):
+    if recurrence_interval is None:
+        raise InvalidArgumentValueError("Recurrence interval is required for monthly by dates recurrence.")
+    if recurrence_dates_in_month is None or len(recurrence_dates_in_month) == 0:
+        raise InvalidArgumentValueError("Recurrence dates in month are required for monthly by dates recurrence.")
+    if any(kwargs.values()):
+        raise InvalidArgumentValueError("Only recurrence interval and dates in month should be provided for monthly by dates recurrence.")
+    return models.MonthlyRecurrenceByDates(
+        interval=recurrence_interval,
+        dates_in_month=recurrence_dates_in_month,
+        recurrence_end=recurrence_end_body
+    )
+
+def handle_monthly_by_days_recurrence(recurrence_interval, recurrence_week_days, recurrence_index, recurrence_end_body, **kwargs):
+    if recurrence_interval is None:
+        raise InvalidArgumentValueError("Recurrence interval is required for monthly by days recurrence.")
+    if recurrence_week_days is None or len(recurrence_week_days) == 0:
+        raise InvalidArgumentValueError("Recurrence week days are required for monthly by days recurrence.")
+    if recurrence_index not in range(1, 6):
+        raise InvalidArgumentValueError("Recurrence index should be between 1 and 5 for monthly by days recurrence.")
+    if any(kwargs.values()):
+        raise InvalidArgumentValueError("Only recurrence interval, week days, and index should be provided for monthly by days recurrence.")
+    return models.MonthlyRecurrenceByWeekDays(
+        interval=recurrence_interval,
+        week_days_in_month=recurrence_week_days,
+        index=recurrence_index,
+        recurrence_end=recurrence_end_body
+    )
+
+def handle_cron_recurrence(recurrence_cron_expression, recurrence_end_body, **kwargs):
+    if recurrence_cron_expression is None:
+        raise InvalidArgumentValueError("Recurrence cron expression is required for cron recurrence.")
+    if any(kwargs.values()):
+        raise InvalidArgumentValueError("Only recurrence cron expression should be provided for cron recurrence.")
+    return models.RecurrenceWithCron(
+        cron_expression=recurrence_cron_expression,
+        recurrence_end=recurrence_end_body
+    )
+
+recurrence_handlers = {
+    enums.Frequency.DAILY: handle_daily_recurrence,
+    enums.Frequency.WEEKLY: handle_weekly_recurrence,
+    enums.Frequency.HOURLY: handle_hourly_recurrence,
+    enums.Frequency.MONTHLY_BY_DATES: handle_monthly_by_dates_recurrence,
+    enums.Frequency.MONTHLY_BY_DAYS: handle_monthly_by_days_recurrence,
+    enums.Frequency.CRON: handle_cron_recurrence,
+}
+
 def get_recurrence_body(
     recurrence_type,
     recurrence_interval,
@@ -39,116 +120,67 @@ def get_recurrence_body(
     recurrence_week_days,
     recurrence_end_body,
 ):
-    validate_recurrence_input(recurrence_type,
-    recurrence_interval,
-    recurrence_index,
-    recurrence_cron_expression,
-    recurrence_dates_in_month,
-    recurrence_week_days)
-
-    if recurrence_type == enums.Frequency.DAILY:
-        return models.DailyRecurrence(
-            interval=recurrence_interval,
-            recurrence_end=recurrence_end_body
-        )
-    elif recurrence_type == enums.Frequency.WEEKLY:
-        return models.WeeklyRecurrence(
-            interval=recurrence_interval,
-            days_of_week=recurrence_week_days,
-            recurrence_end=recurrence_end_body
-        )
-    elif recurrence_type == enums.Frequency.HOURLY:
-        return models.HourlyRecurrence(
-            interval=recurrence_interval,
-            recurrence_end=recurrence_end_body
-        )
-    elif recurrence_type == enums.Frequency.MONTHLY_BY_DATES:
-        return models.MonthlyRecurrenceByDates(
-            interval=recurrence_interval,
-            dates_in_month=recurrence_dates_in_month,
-            recurrence_end=recurrence_end_body
-        )
-    elif recurrence_type == enums.Frequency.MONTHLY_BY_DAYS:
-        return models.MonthlyRecurrenceByWeekDays(
-            interval=recurrence_interval,
-            week_days_in_month=recurrence_week_days,
-            index=recurrence_index,
-            recurrence_end=recurrence_end_body
-        )
-    elif recurrence_type == enums.Frequency.CRON:
-        return models.RecurrenceWithCron(
-            cron_expression=recurrence_cron_expression,
-            recurrence_end=recurrence_end_body
-        )
-    else:
+    if recurrence_type is None:
+        if any([recurrence_interval, recurrence_index, recurrence_cron_expression, recurrence_dates_in_month, recurrence_week_days]):
+            raise InvalidArgumentValueError("Recurrence type is required.")
+        logger.debug("Recurrence type not provided. Treating it as one-time trigger.")
         return None
 
+    handler = recurrence_handlers.get(recurrence_type)
+    if handler:
+        return handler(
+            recurrence_interval=recurrence_interval,
+            recurrence_index=recurrence_index,
+            recurrence_cron_expression=recurrence_cron_expression,
+            recurrence_dates_in_month=recurrence_dates_in_month,
+            recurrence_week_days=recurrence_week_days,
+            recurrence_end_body=recurrence_end_body
+        )
+    return None
 
-def validate_recurrence_input(
+def get_recurrence_body_for_update(
     recurrence_type,
     recurrence_interval,
     recurrence_index,
     recurrence_cron_expression,
     recurrence_dates_in_month,
     recurrence_week_days,
+    recurrence_end_body,
+    existing_recurrence,
 ):
     if recurrence_type is None:
-        if any([recurrence_interval, recurrence_index, recurrence_cron_expression, recurrence_dates_in_month, recurrence_week_days]):
-            raise InvalidArgumentValueError("Recurrence type is required.")
-        logger.debug("Recurrence type not provided. Treating it as one-time trigger.")
-        return
-    
-    if recurrence_type == enums.Frequency.DAILY:
-        if recurrence_interval is None:
-            raise InvalidArgumentValueError("Recurrence interval is required for daily recurrence.")
-        
-        # Ensure other arguments are not provided
-        if any([recurrence_index, recurrence_cron_expression, recurrence_dates_in_month, recurrence_week_days]):
-            raise InvalidArgumentValueError("Only recurrence interval should be provided for daily recurrence.")
-    
-    if recurrence_type == enums.Frequency.WEEKLY:
-        if recurrence_interval is None:
-            raise InvalidArgumentValueError("Recurrence interval is required for weekly recurrence.")
-        if recurrence_week_days is None or len(recurrence_week_days) == 0:
-            raise InvalidArgumentValueError("Recurrence week days are required for weekly recurrence.")
-        # Ensure other arguments are not provided
-        if any([recurrence_index, recurrence_cron_expression, recurrence_dates_in_month]):
-            raise InvalidArgumentValueError("Only recurrence interval and week days should be provided for weekly recurrence.")
-    
-    if recurrence_type == enums.Frequency.HOURLY:
-        if recurrence_interval is None:
-            raise InvalidArgumentValueError("Recurrence interval is required for hourly recurrence.")
-        # Ensure other arguments are not provided
-        if any([recurrence_index, recurrence_cron_expression, recurrence_dates_in_month, recurrence_week_days]):
-            raise InvalidArgumentValueError("Only recurrence interval should be provided for hourly recurrence.")
-    
-    if recurrence_type == enums.Frequency.MONTHLY_BY_DATES:
-        if recurrence_interval is None:
-            raise InvalidArgumentValueError("Recurrence interval is required for monthly by dates recurrence.")
-        if recurrence_dates_in_month is None or len(recurrence_dates_in_month) == 0:
-            raise InvalidArgumentValueError("Recurrence dates in month are required for monthly by dates recurrence.")
-        # Ensure other arguments are not provided
-        if any([recurrence_index, recurrence_cron_expression, recurrence_week_days]):
-            raise InvalidArgumentValueError("Only recurrence interval and dates in month should be provided for monthly by dates recurrence.")
-    
-    if recurrence_type == enums.Frequency.MONTHLY_BY_DAYS:
-        if recurrence_interval is None:
-            raise InvalidArgumentValueError("Recurrence interval is required for monthly by days recurrence.")
-        if recurrence_week_days is None or len(recurrence_week_days) == 0:
-            raise InvalidArgumentValueError("Recurrence week days are required for monthly by days recurrence.")
-        if recurrence_index not in range(1, 6):
-            raise InvalidArgumentValueError("Recurrence index should be between 1 and 5 for monthly by days recurrence.")
-        # Ensure other arguments are not provided
-        if any([recurrence_cron_expression, recurrence_dates_in_month]):
-            raise InvalidArgumentValueError("Only recurrence interval, week days, and index should be provided for monthly by days recurrence.")
-    
-    if recurrence_type == enums.Frequency.CRON:
-        if recurrence_cron_expression is None:
-            raise InvalidArgumentValueError("Recurrence cron expression is required for cron recurrence.")
-        
-        # Ensure other arguments are not provided
-        if any([recurrence_interval, recurrence_index, recurrence_dates_in_month, recurrence_week_days]):
-            raise InvalidArgumentValueError("Only recurrence cron expression should be provided for cron recurrence.")
+        if existing_recurrence is None:
+            if any([recurrence_interval, recurrence_index, recurrence_cron_expression, recurrence_dates_in_month, recurrence_week_days]):
+                raise InvalidArgumentValueError("Updating recurrence properties of a non recurring schedule requires recurrence type.")
+            logger.debug("Recurrence type not provided. Treating it as one-time trigger.")
+            return None
+        recurrence_type = existing_recurrence.frequency
+        if recurrence_type == enums.Frequency.CRON:
+            recurrence_cron_expression = recurrence_cron_expression or existing_recurrence.cron_expression
+        elif recurrence_type == enums.Frequency.DAILY:
+            recurrence_interval = recurrence_interval or existing_recurrence.interval
+        elif recurrence_type == enums.Frequency.WEEKLY:
+            recurrence_interval = recurrence_interval or existing_recurrence.interval
+            recurrence_week_days = recurrence_week_days or existing_recurrence.days_of_week
+        elif recurrence_type == enums.Frequency.HOURLY:
+            recurrence_interval = recurrence_interval or existing_recurrence.interval
+        elif recurrence_type == enums.Frequency.MONTHLY_BY_DATES:
+            recurrence_interval = recurrence_interval or existing_recurrence.interval
+            recurrence_dates_in_month = recurrence_dates_in_month or existing_recurrence.days_of_month
+        elif recurrence_type == enums.Frequency.MONTHLY_BY_DAYS:
+            recurrence_interval = recurrence_interval or existing_recurrence.interval
+            recurrence_week_days = recurrence_week_days or existing_recurrence.days_of_week
+            recurrence_index = recurrence_index or existing_recurrence.index
+
+    return get_recurrence_body(
+        recurrence_type,
+        recurrence_interval,
+        recurrence_index,
+        recurrence_cron_expression,
+        recurrence_dates_in_month,
+        recurrence_week_days,
+        recurrence_end_body
+    )
         
 def get_schedule_trigger_body_for_update(
     existing_trigger_schedule,
@@ -182,53 +214,3 @@ def get_schedule_trigger_body_for_update(
         new_trigger_body.recurrence = existing_trigger_schedule.recurrence
     
     return new_trigger_body
-
-
-def get_recurrence_body_for_update(
-    recurrence_type,
-    recurrence_interval,
-    recurrence_index,
-    recurrence_cron_expression,
-    recurrence_dates_in_month,
-    recurrence_week_days,
-    recurrence_end_body,
-    existing_recurrence,
-):
-    # No recurrence type provided means update in existing recurrence
-    # Prioritize new values, but fallback to existing values if new values are not provided
-    if recurrence_type is None:
-        if existing_recurrence is None:
-            if any([recurrence_interval, recurrence_index, recurrence_cron_expression, recurrence_dates_in_month, recurrence_week_days]):
-                raise InvalidArgumentValueError("Updating recurrence properties of a non recurring schedule requires recurrence type.")
-            logger.debug("Recurrence type not provided. Treating it as one-time trigger.")
-            return None
-        recurrence_type = existing_recurrence.frequency
-        if recurrence_type == enums.Frequency.CRON:
-            recurrence_cron_expression = recurrence_cron_expression or existing_recurrence.cron_expression
-        elif recurrence_type == enums.Frequency.DAILY:
-            recurrence_interval = recurrence_interval or existing_recurrence.interval
-        elif recurrence_type == enums.Frequency.WEEKLY:
-            recurrence_interval = recurrence_interval or existing_recurrence.interval
-            recurrence_week_days = recurrence_week_days or existing_recurrence.days_of_week
-        elif recurrence_type == enums.Frequency.HOURLY:
-            recurrence_interval = recurrence_interval or existing_recurrence.interval
-        elif recurrence_type == enums.Frequency.MONTHLY_BY_DATES:
-            recurrence_interval = recurrence_interval or existing_recurrence.interval
-            recurrence_dates_in_month = recurrence_dates_in_month or existing_recurrence.days_of_month
-        elif recurrence_type == enums.Frequency.MONTHLY_BY_DAYS:
-            recurrence_interval = recurrence_interval or existing_recurrence.interval
-            recurrence_week_days = recurrence_week_days or existing_recurrence.days_of_week
-            recurrence_index = recurrence_index or existing_recurrence.index
-    
-    # if recurrence type is provided, override existing recurrence body with new values
-    return get_recurrence_body(
-        recurrence_type,
-        recurrence_interval,
-        recurrence_index,
-        recurrence_cron_expression,
-        recurrence_dates_in_month,
-        recurrence_week_days,
-        recurrence_end_body
-    )
-        
-    
