528d94fb78afbcde4525b66e31e34bb55b14e574
AllyW <liwang1025@gmail.com>
2025-03-13 10:16:12 +0800

    data from swagger

--- src/playwright-cli-extension/azext_playwright_cli_extension/aaz/latest/playwright_testing/auth_manager/account/access_token/__init__.py
+++ src/playwright-cli-extension/azext_playwright_cli_extension/aaz/latest/playwright_testing/auth_manager/account/access_token/__init__.py
@@ -8,4 +8,9 @@
 # pylint: skip-file
 # flake8: noqa
 
-from ._clients import *
+from .__cmd_group import *
+from ._create import *
+from ._delete import *
+from ._list import *
+from ._show import *
+from ._update import *
