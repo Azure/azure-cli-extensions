c1d9d78e5ab8432892bc860a76db24ecae2427eb
Warren Jones (Insight Global LLC) <v-warrjones@microsoft.com>
2025-03-17 11:08:35 -0700

    Removed import from vendored_sdks/__init__.py and restored version number to 1.0.0b4

--- src/quantum/azext_quantum/vendored_sdks/__init__.py
+++ src/quantum/azext_quantum/vendored_sdks/__init__.py
@@ -4,5 +4,3 @@
 # --------------------------------------------------------------------------------------------
 
 __import__('pkg_resources').declare_namespace(__name__)
-
-from .azure_quantum_python.workspace import *
