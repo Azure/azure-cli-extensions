0f818070b89ca4f188bad0463d1985f084e7feb8
Atharva Udapure <audapure@microsoft.com>
2025-03-19 11:52:00 +0530

    Fixed Schema version

--- src/workload-orchestration/azext_workload_orchestration/aaz/latest/workload_orchestration/schema/version/__cmd_group.py
+++ src/workload-orchestration/azext_workload_orchestration/aaz/latest/workload_orchestration/schema/version/__cmd_group.py
@@ -15,7 +15,7 @@ from azure.cli.core.aaz import *
     "workload-orchestration schema version",
 )
 class __CMDGroup(AAZCommandGroup):
-    """This is for edge Config-Manager
+    """workload-orchestration schema version helps to manage Schemas
     """
     pass
 
