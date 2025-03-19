bf00d4cb9c5ff1390b21eb5e4cb578d038744af1
Guitar Sheng <guitarsheng@microsoft.com>
2025-03-20 01:22:51 +0800

    fix issue

--- src/spring/azext_spring/migration/converter/param_converter.py
+++ src/spring/azext_spring/migration/converter/param_converter.py
@@ -11,7 +11,6 @@ class ParamConverter(BaseConverter):
     def __init__(self, source):
         def transform_data():
             apps = self.wrapper_data.get_apps()
-            storage_configs = []
             apps_data = []
             for app in apps:
                 apps_data.append({
@@ -23,17 +22,10 @@ class ParamConverter(BaseConverter):
                     "paramContainerAppImagePassword": self._get_param_name_of_container_image_password(app),
                     "image": self._get_container_image(app),
                 })
-                if 'properties' in app and 'customPersistentDisks' in app['properties']:
-                    disks = app['properties']['customPersistentDisks']
-                    for disk_props in disks:
-                        storage_config = {
-                            'paramContainerAppEnvStorageAccountKey': self._get_param_name_of_storage_account_key(disk_props),
-                            'accountName': self._get_storage_account_name(disk_props),
-                        }
-                        storage_configs.append(storage_config)
+
             return {
                 "apps": apps_data,
-                "storages": storage_configs,
+                "storages": self._get_app_storage_configs(),
                 "isVnet": self.wrapper_data.is_vnet()
             }
         super().__init__(source, transform_data)
