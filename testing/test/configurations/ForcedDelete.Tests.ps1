a660d9a2ffb018d770f648a449c98b6fe3c0d157
Atchut Kumar Barli <atchut@gmail.com>
2025-03-25 10:24:35 -0700

    Pass the force delete param to the API call (#4)
    * forcedelete
    
    * format
    
    * add code owner
    
    * mypy

--- testing/test/configurations/ForcedDelete.Tests.ps1
+++ testing/test/configurations/ForcedDelete.Tests.ps1
@@ -1,9 +1,9 @@
-Describe 'Basic Onboarding Scenario' {
+Describe 'Basic Onboarding with Force delete Scenario' {
     BeforeAll {
         . $PSScriptRoot/../helper/Constants.ps1
     }
 
-    It 'Check if basic onboarding works correctly' {
+    It 'Check if onboarding works correctly' {
         az connectedk8s connect -n $ENVCONFIG.arcClusterName -g $ENVCONFIG.resourceGroup -l $ARC_LOCATION --no-wait
         $? | Should -BeTrue
         Start-Sleep -Seconds 10
@@ -27,32 +27,8 @@ Describe 'Basic Onboarding Scenario' {
         $n | Should -BeLessOrEqual $MAX_RETRY_ATTEMPTS
     }
 
-    It 'Disable auto-upgrade' {
-        az connectedk8s update -n $ENVCONFIG.arcClusterName -g $ENVCONFIG.resourceGroup --auto-upgrade false
-        $? | Should -BeTrue
-        Start-Sleep -Seconds 10
-
-        # Loop and retry until the configuration installs
-        $n = 0
-        do 
-        {
-            $output = az connectedk8s show -n $ENVCONFIG.arcClusterName -g $ENVCONFIG.resourceGroup
-            $jsonOutput = [System.Text.Json.JsonDocument]::Parse($output)
-            $provisioningState = ($output | ConvertFrom-Json).provisioningState
-            $autoUpdate = $jsonOutput.RootElement.GetProperty("arcAgentProfile").GetProperty("agentAutoUpgrade").GetString()
-            Write-Host "Provisioning State: $provisioningState"
-            Write-Host "Auto Update: $autoUpdate"
-            if ($provisioningState -eq $SUCCEEDED -and $autoUpdate -eq "Disabled") {
-                break
-            }
-            Start-Sleep -Seconds 10
-            $n += 1
-        } while ($n -le $MAX_RETRY_ATTEMPTS)
-        $n | Should -BeLessOrEqual $MAX_RETRY_ATTEMPTS
-    }
-
-    It "Delete the connected instance" {
-        az connectedk8s delete -n $ENVCONFIG.arcClusterName -g $ENVCONFIG.resourceGroup -y
+    It "Force delete the connected instance" {
+        az connectedk8s delete -n $ENVCONFIG.arcClusterName -g $ENVCONFIG.resourceGroup --force -y
         $? | Should -BeTrue
 
         # Configuration should be removed from the resource model
