736f925b7dd23180792d0c0bee37dc55b05f008d
ninpan-ms <71061174+ninpan-ms@users.noreply.github.com>
2025-03-27 14:51:46 +0800

    [Spring] Support BYOC in migration command (#8609)

--- src/spring/azext_spring/migration/converter/param_converter.py
+++ src/spring/azext_spring/migration/converter/param_converter.py
@@ -11,25 +11,21 @@ class ParamConverter(BaseConverter):
     def __init__(self, source):
         def transform_data():
             apps = self.wrapper_data.get_apps()
-            storage_configs = []
             apps_data = []
             for app in apps:
                 apps_data.append({
                     "appName": self._get_resource_name(app),
                     "paramContainerAppImageName": self._get_param_name_of_container_image(app),
                     "paramTargetPort": self._get_param_name_of_target_port(app),
+                    "isByoc": self.wrapper_data.is_support_custom_container_image_for_app(app),
+                    "isPrivateImage": self.wrapper_data.is_private_custom_container_image(app),
+                    "paramContainerAppImagePassword": self._get_param_name_of_container_image_password(app),
+                    "image": self._get_container_image(app),
                 })
-                if 'properties' in app and 'customPersistentDisks' in app['properties']:
-                    disks = app['properties']['customPersistentDisks']
-                    for disk_props in disks:
-                        storage_config = {
-                            'paramContainerAppEnvStorageAccountKey': self._get_param_name_of_storage_account_key(disk_props),
-                            'accountName': self._get_storage_account_name(disk_props),
-                        }
-                        storage_configs.append(storage_config)
+
             return {
                 "apps": apps_data,
-                "storages": storage_configs,
+                "storages": self._get_app_storage_configs(),
                 "isVnet": self.wrapper_data.is_vnet()
             }
         super().__init__(source, transform_data)
