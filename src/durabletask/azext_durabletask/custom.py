c8314b60d5a65404191d8a6b55619ab0630bd1f9
Ryan Lettieri <ryanLettieri@microsoft.com>
2025-04-03 12:08:22 -0600

    linting
    Signed-off-by: Ryan Lettieri <ryanLettieri@microsoft.com>

--- src/durabletask/azext_durabletask/custom.py
+++ src/durabletask/azext_durabletask/custom.py
@@ -110,17 +110,18 @@ def _build_retention_policies(args_dict):
     return retention_policies
 
 def list_orchestrations(cmd, resource_group_name, scheduler_name, taskhub_name, max_items=100):
+
     # Get FQDN of the scheduler
     resource_group_name = resource_group_name
     scheduler_name = scheduler_name
-    scheduler = Show(cli_ctx=cmd.cli_ctx)(command_args={    
+    scheduler = Show(cli_ctx=cmd.cli_ctx)(command_args = {
         "resource_group": resource_group_name,
         "name": scheduler_name,
         "subscription": get_subscription_id(cmd.cli_ctx)
     })
 
     endpoint = scheduler['properties']['endpoint']
-    endpoint+="/v1/taskhubs/orchestrations/query"
+    endpoint += "/v1/taskhubs/orchestrations/query"
     grabbed_token = f"Bearer {get_bearer_token(cmd.cli_ctx)}"
 
     headers = {
@@ -129,13 +130,14 @@ def list_orchestrations(cmd, resource_group_name, scheduler_name, taskhub_name, 
         'x-taskhub': taskhub_name
     }
 
-    payload = {"filter":{},"pagination":{"startIndex":0,"count":max_items},"sort":[{"column":"LAST_UPDATED_AT","direction":"DESCENDING_SORT"}]}
+    payload = {"filter": {}, "pagination": {"startIndex": 0,"count": max_items}, "sort": [{"column": "LAST_UPDATED_AT", "direction": "DESCENDING_SORT"}]}
 
     client = httpx.Client(http2=True)
     response = client.post(endpoint, json=payload, headers=headers)
     return response.json()
 
 def get_bearer_token(cli_ctx):
+
     from azure.cli.core._profile import Profile
     from azure.cli.core.auth.util import resource_to_scopes
     profile = Profile(cli_ctx=cli_ctx)
