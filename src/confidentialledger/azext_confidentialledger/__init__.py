a033d4a8a06634077a57e04b1c553d1cd969a335
prathod09 <117223333+prathod09@users.noreply.github.com>
2025-04-15 06:54:11 +0000

    Update to resolve CI issues

--- src/confidentialledger/azext_confidentialledger/__init__.py
+++ src/confidentialledger/azext_confidentialledger/__init__.py
@@ -17,19 +17,14 @@ import azext_confidentialledger._help
 class ConfidentialLedgerCommandsLoader(AzCommandsLoader):
     def __init__(self, cli_ctx=None):
         from azure.cli.core.commands import CliCommandType
-        from azext_confidentialledger.generated._client_factory import (
-            cf_confidentialledger_cl,
-        )
 
         confidentialledger_custom = CliCommandType(
             operations_tmpl="azext_confidentialledger.custom#{}",
-            client_factory=cf_confidentialledger_cl,
         )
         parent = super()
         parent.__init__(cli_ctx=cli_ctx, custom_command_type=confidentialledger_custom)
 
     def load_command_table(self, args):
-        from azext_confidentialledger.generated.commands import load_command_table
         from azure.cli.core.aaz import load_aaz_command_table
         try:
             from . import aaz
@@ -42,24 +37,9 @@ class ConfidentialLedgerCommandsLoader(AzCommandsLoader):
                 args=args
             )
 
-        load_command_table(self, args)
-        try:
-            from azext_confidentialledger.manual.commands import (
-                load_command_table as load_command_table_manual,
-            )
-
-            load_command_table_manual(self, args)
-        except ImportError as e:
-            if e.name.endswith("manual.commands"):
-                pass
-            else:
-                raise e
         return self.command_table
 
     def load_arguments(self, command):
-        from azext_confidentialledger.generated._params import load_arguments
-
-        load_arguments(self, command)
         try:
             from azext_confidentialledger.manual._params import (
                 load_arguments as load_arguments_manual,
