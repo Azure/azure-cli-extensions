e8057f9ed7fee2e2f737898814c3459d5e346df6
sachinAtMS <guptasachin@microsoft.com>
2025-04-17 05:27:21 +0530

    removing local changes

--- src/cosmosdb-preview/azext_cosmosdb_preview/tests/latest/test_cosmosdb-cassandrami_scenario.py
+++ src/cosmosdb-preview/azext_cosmosdb_preview/tests/latest/test_cosmosdb-cassandrami_scenario.py
@@ -94,15 +94,13 @@ class ManagedCassandraScenarioTest(ScenarioTest):
         })
 
         # Create Cluster with azure_connection_method
-        self.cmd('az managed-cassandra cluster create -c {c} -l eastus2 -g {rg} -i password -q {azure_connection_method} --debug')
+        self.cmd('az managed-cassandra cluster create -c {c} -l eastus2 -g {rg} -i password -q {azure_connection_method}')
         cluster = self.cmd('az managed-cassandra cluster show -c {c} -g {rg}').get_output_in_json()
-        print(cluster)
         assert cluster['properties']['provisioningState'] == 'Succeeded'
 
         # Create Datacenter
         self.cmd('az managed-cassandra datacenter create -c {c} -d {d} -l eastus2 -g {rg} -n 3 -s {subnet_id} -i 10.0.0.8')
         datacenter = self.cmd('az managed-cassandra datacenter show -c {c} -d {d} -g {rg}').get_output_in_json()
-        print(datacenter)
         assert datacenter['properties']['provisioningState'] == 'Succeeded'
 
         # Clean up
