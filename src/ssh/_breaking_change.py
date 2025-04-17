9ca8f3a3eb1764e41b547ec90f48b543a23415a2
Steven Bucher <stevenbucher@microsoft.com>
2025-04-17 13:20:04 -0700

    edits per suggestion

--- src/ssh/_breaking_change.py
+++
@@ -1,5 +0,0 @@
-from azure.cli.core.breaking_change import register_required_flag_breaking_change, register_default_value_breaking_change, register_other_breaking_change
-
-register_other_breaking_change('ssh arc', 'On May 21st 2025, any ssh arc commands on versions <2.0.4 will no longer work. Please upgrade to az ssh version >=2.0.4', taget_version='2.0.4')
-register_other_breaking_change('ssh config', 'On May 21st 2025, any ssh commands connecting to ARC machines on versions <2.0.4 will no longer work. Please upgrade to az ssh version >=2.0.4', taget_version='2.0.4')
-register_other_breaking_change('ssh vm', 'On May 21st 2025, any ssh commands connecting to ARC machines on versions <2.0.4 will no longer work. Please upgrade to az ssh version >=2.0.4', taget_version='2.0.4')
