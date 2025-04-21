7955f8127388e6551cd8e543581a01362ad90136
sophie zhao <sophiezhao@microsoft.com>
2025-04-20 18:40:39 -0700

    fix invalid naming convensitons

--- src/aks-preview/azext_aks_preview/_help.py
+++ src/aks-preview/azext_aks_preview/_help.py
@@ -3363,7 +3363,7 @@ The output includes secrets that you must protect. Be sure that you do not inclu
     - name: --name -n
       type: string
       short-summary: Name of the extension instance
-    - name: --auto-upgrade-minor-version, --auto-upgrade
+    - name: --auto-upgrade
       type: bool
       short-summary: Automatically upgrade minor version of the core extension instance
     - name: --release-train
@@ -3372,16 +3372,16 @@ The output includes secrets that you must protect. Be sure that you do not inclu
     - name: --version
       type: string
       short-summary: Specify the version to install for the extension instance if --auto-upgrade-minor-version is not enabled.
-    - name: --configuration-settings --config
+    - name: --config
       type: string
       short-summary: Configuration Settings as key=value pair. Repeat parameter for each setting. Do not use this for secrets, as this value is returned in response.
-    - name: --config-protected-settings --config-protected
+    - name: --config-protected-settings 
       type: string
       short-summary: Configuration Protected Settings as key=value pair. Repeat parameter for each setting. Only the key is returned in response, the value is not.
-    - name: --config-settings-file --config-file
+    - name: --config-settings-file
       type: string
       short-summary: JSON file path for configuration-settings
-    - name: --config-protected-settings-file --config-protected-file
+    - name: --config-protected-settings-file
       type: string
       short-summary: JSON file path for configuration-protected-settings
 
