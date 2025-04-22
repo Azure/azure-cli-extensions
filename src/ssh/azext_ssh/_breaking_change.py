e2af07a27805bec8c7c108612c4a8065d92a79fa
Steven Bucher <stevenbucher@microsoft.com>
2025-04-21 17:54:58 -0700

    another small fix

--- src/ssh/azext_ssh/_breaking_change.py
+++ src/ssh/azext_ssh/_breaking_change.py
@@ -11,11 +11,11 @@ register_other_breaking_change(
     target_version='2.0.4')
 register_other_breaking_change(
     'ssh config',
-    'On May 21st 2025, any ssh commands connecting to ARC machines on versions <2.0.4 will no longer work. '
+    'On May 21st 2025, any ssh commands connecting to ARC machines on versions <2.0.4 will no longer work.'
     'Please upgrade to az ssh version >=2.0.4',
     target_version='2.0.4')
 register_other_breaking_change(
     'ssh vm',
-    'On May 21st 2025, any ssh commands connecting to ARC machines on versions <2.0.4 will no longer work. ' 
+    'On May 21st 2025, any ssh commands connecting to ARC machines on versions <2.0.4 will no longer work.' 
     'Please upgrade to az ssh version >=2.0.4',
     target_version='2.0.4')
