9d8a4074431ff79fba7543a67277106fd9e70415
Steven Bucher <stevenbucher@microsoft.com>
2025-04-21 18:05:03 -0700

    remove excess whitespace

--- src/ssh/azext_ssh/_breaking_change.py
+++ src/ssh/azext_ssh/_breaking_change.py
@@ -5,7 +5,7 @@
 from azure.cli.core.breaking_change import register_other_breaking_change
 
 register_other_breaking_change(
-    'ssh arc', 
+    'ssh arc',
     'On May 21st 2025, any ssh arc commands on versions <2.0.4 will no longer work. '
     'Please upgrade to az ssh version >=2.0.4',
     target_version='2.0.4')
@@ -16,6 +16,6 @@ register_other_breaking_change(
     target_version='2.0.4')
 register_other_breaking_change(
     'ssh vm',
-    'On May 21st 2025, any ssh commands connecting to ARC machines on versions <2.0.4 will no longer work. ' 
+    'On May 21st 2025, any ssh commands connecting to ARC machines on versions <2.0.4 will no longer work. '
     'Please upgrade to az ssh version >=2.0.4',
     target_version='2.0.4')
