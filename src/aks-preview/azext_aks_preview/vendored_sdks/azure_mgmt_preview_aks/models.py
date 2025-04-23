6d61328e553493a8d5de7547faf42ca8c23fed6c
Fuming Zhang <fumingzhang@microsoft.com>
2025-04-23 11:44:57 +1000

    update SDK

--- src/aks-preview/azext_aks_preview/vendored_sdks/azure_mgmt_preview_aks/models.py
+++ src/aks-preview/azext_aks_preview/vendored_sdks/azure_mgmt_preview_aks/models.py
@@ -4,4 +4,6 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-from .v2025_01_02_preview.models import *
+from .v2017_07_01.models import *
+from .v2019_09_30_preview.models import *
+from .v2025_02_01.models import *
