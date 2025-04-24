526cc47da6b0a132d919ee088a1a867be5751ebb
Niels Buit <nielsb@microsoft.com>
2025-04-24 12:33:36 +0200

    added network provider

---
+++ src/zones/azext_zones/resource_type_validators/microsoft_network.py
@@ -0,0 +1,73 @@
+from .._resourceTypeValidation import ZoneRedundancyValidationResult, register_resource_type
+from knack.log import get_logger
+
+
+@register_resource_type('microsoft.network')
+class microsoft_network:
+
+    @staticmethod
+    def validate(resource):
+        resourceSubType = resource['type'].split('/')[1]
+
+        _logger = get_logger("microsoft_network")
+        _logger.debug("Validating Microsoft.Network resource type: %s", resourceSubType)
+
+        match resourceSubType:
+            case 'applicationgateways':
+                return ZoneRedundancyValidationResult.Yes if resource['zones'] not in [None, []] else ZoneRedundancyValidationResult.No
+
+            case 'azurefirewalls':
+                return ZoneRedundancyValidationResult.Yes if resource['zones'] not in [None, []] and resource['sku']['capacity'] > 1 else ZoneRedundancyValidationResult.No
+
+            case 'connections':
+                # Network connections depend on the configuration of the Virtual Network Gateway
+                return ZoneRedundancyValidationResult.Dependent
+
+            case 'dnszones':
+                # Azure DNS is a global service, zone redundant by default
+                return ZoneRedundancyValidationResult.Always
+
+            case 'frontdoors':
+                # Front Door is a global resources and always zone redundant
+                return ZoneRedundancyValidationResult.Always
+
+            case 'loadbalancers':
+                return ZoneRedundancyValidationResult.Yes if resource['sku']['name'] in ['Standard'] and resource['frontendipconfigurations'][0].zones not in [None, []] else ZoneRedundancyValidationResult.No
+
+            case 'localnetworkgateways':
+                # Local network gateways depend on the configuration of the VPN Gateway
+                return ZoneRedundancyValidationResult.Dependent
+
+            case 'networkinterfaces':
+                # Network interfaces are in the zone of the virtual machines they are attached to
+                return ZoneRedundancyValidationResult.Dependent
+
+            case 'networksecuritygroups':
+                return ZoneRedundancyValidationResult.Always
+
+            case 'networkwatchers':
+                # Network watchers are zone redundant by default
+                return ZoneRedundancyValidationResult.Always
+
+            case 'privatednszones':
+                # Private DNS zones are zone redundant by default 
+                # https://learn.microsoft.com/azure/dns/private-dns-resiliency
+                return ZoneRedundancyValidationResult.Always
+
+            case 'privateendpoints':
+                return ZoneRedundancyValidationResult.Always
+
+            case 'publicipaddresses':
+                return ZoneRedundancyValidationResult.Yes if resource['sku']['name'] in ['Standard'] and resource['zones'] not in [None, []] else ZoneRedundancyValidationResult.No
+
+            case 'virtualnetworks':
+                # Virtual networks span all availability zones in a region.
+                # https://learn.microsoft.com/azure/virtual-network/virtual-networks-overview#virtual-networks-and-availability-zones
+                return ZoneRedundancyValidationResult.Always
+
+            case 'virtualnetworkgateways':
+                sku = resource['properties']['sku']['name']
+                return ZoneRedundancyValidationResult.Yes if sku.endswith('AZ') else ZoneRedundancyValidationResult.No
+
+
+        return ZoneRedundancyValidationResult.Unknown
