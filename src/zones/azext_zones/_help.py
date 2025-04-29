2fb2f691e26e89b025ea4d53c755288b28fdb66e
Niels Buit <nielsb@microsoft.com>
2025-04-29 15:23:02 +0200

    requested metadata updates

--- src/zones/azext_zones/_help.py
+++ src/zones/azext_zones/_help.py
@@ -15,4 +15,11 @@ helps['zones'] = """
 helps['zones validate'] = """
     type: command
     short-summary: Validates zone redundancy status of all resources in the current subscription context for which you have read access.
+    examples:
+      - name: Validate zone redundancy status of all resources in the specified resource group
+        text: |-
+          az zones validate --resource-groups myProductionRG --omit-dependent
+      - name: Validate zone redundancy status of all resources in the specified resource group, but omit the dependent/child resources
+        text: |-
+          az zones validate --resource-groups myProductionRG --omit-dependent
 """
