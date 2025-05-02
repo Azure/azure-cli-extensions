42942e6e49c3401984a87c316c76cd30ed50660e
Nikita Garg <nikitagarg@microsoft.com>
2025-05-02 12:53:56 +0530

    update example

--- src/redisenterprise/azext_redisenterprise/aaz/latest/redisenterprise/database/_force_link_to_replication_group.py
+++ src/redisenterprise/azext_redisenterprise/aaz/latest/redisenterprise/database/_force_link_to_replication_group.py
@@ -18,7 +18,7 @@ class ForceLinkToReplicationGroup(AAZCommand):
     """Forcibly recreates an existing database on the specified cluster, and rejoins it to an existing replication group. **IMPORTANT NOTE:** All data in this database will be discarded, and the database will temporarily be unavailable while rejoining the replication group.
 
     :example: How to relink a database after a regional outage
-        az redisenterprise database force-link-to-replication-group --resource-group rg1 --cluster-name cache1 --database-name default --group-nickname groupName --linked-databases id="/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Cache/redisEnterprise/cache1/databases/default" --linked-databases id="/subscriptions/11111111-1111-1111-1111-111111111111/resourceGroups/rg2/providers/Microsoft.Cache/redisEnterprise/cache2/databases/default"
+        az redisenterprise database force-link-to-replication-group --resource-group rg1 --cluster-name cache1 --database-name default --group-nickname groupName --linked-databases '[{id:"/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg1/providers/Microsoft.Cache/redisEnterprise/cache1/databases/default"},{id:"/subscriptions/11111111-1111-1111-1111-111111111111/resourceGroups/rg2/providers/Microsoft.Cache/redisEnterprise/cache2/databases/default"}]'
     """
 
     _aaz_info = {
