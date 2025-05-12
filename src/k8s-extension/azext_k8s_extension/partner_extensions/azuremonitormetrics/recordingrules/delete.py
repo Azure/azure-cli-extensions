64ae134e2ab2e12a1ec9dad614743b500b92d74f
bragi92 <kadubey@microsoft.com>
2025-05-12 15:12:23 -0700

    Update delete.py

--- src/k8s-extension/azext_k8s_extension/partner_extensions/azuremonitormetrics/recordingrules/delete.py
+++ src/k8s-extension/azext_k8s_extension/partner_extensions/azuremonitormetrics/recordingrules/delete.py
@@ -35,3 +35,9 @@ def delete_rules(cmd, cluster_subscription, cluster_resource_group_name, cluster
         cluster_resource_group_name,
         "KubernetesRecordingRulesRuleGroup-{0}".format(cluster_name)
     )
+    delete_rule(
+        cmd,
+        cluster_subscription,
+        cluster_resource_group_name,
+        truncate_rule_group_name("UXRecordingRulesRuleGroup - {0}".format(cluster_name))
+    )
