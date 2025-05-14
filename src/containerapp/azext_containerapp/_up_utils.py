df01d8f31292738c22b81daa569fe137dceb3574
harrli <harrli@microsoft.com>
2025-05-14 13:29:44 -0700

    Removed support for custom type model

--- src/containerapp/azext_containerapp/_up_utils.py
+++ src/containerapp/azext_containerapp/_up_utils.py
@@ -1154,7 +1154,7 @@ def _validate_azml_model_existence(cmd, model_registry, model_name, model_versio
                 logger.warning("Model loading class names cannot be automatically detected.")
                 model_load_class = []
     elif model_format.lower() == "custom":
-        ValidationError("Custom model format is not supported yet.")
+        raise ValidationError("Custom model format is not supported yet.")
     return model_asset_id, model_reference_endpoint, model_format, model_load_class
 
 
