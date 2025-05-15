eab1a5c244232036fa6d8b945f232123ed64d904
Niels Buit <buit@outlook.com>
2025-05-15 14:00:49 +0200

    {Zones} Add new `az zones` extension module (#8704)

---
+++ src/zones/azext_zones/resource_type_validators/microsoft_automation.py
@@ -0,0 +1,27 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from .._resourceTypeValidation import (
+    ZoneRedundancyValidationResult,
+    register_resource_type,
+)
+from knack.log import get_logger
+
+
+@register_resource_type("microsoft.automation")
+class microsoft_automation:
+    @staticmethod
+    def validate(resource):
+        resourceType = resource["type"]
+        resourceSubType = resourceType[resourceType.index("/") + 1:]
+
+        _logger = get_logger("microsoft_automation")
+        _logger.debug(
+            "Validating Microsoft.automation resource type: %s", resourceSubType
+        )
+
+        # Automation accounts are zone redundant by default
+        # https://learn.microsoft.com/azure/automation/automation-availability-zones
+        return ZoneRedundancyValidationResult.Always
---
+++ src/zones/azext_zones/resource_type_validators/microsoft_servicebus.py
@@ -0,0 +1,30 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from .._resourceTypeValidation import (
+    ZoneRedundancyValidationResult,
+    register_resource_type,
+)
+from knack.log import get_logger
+
+
+@register_resource_type("microsoft.servicebus")
+class microsoft_servicebus:
+    @staticmethod
+    def validate(resource):
+        resourceType = resource["type"]
+        resourceSubType = resourceType[resourceType.index("/") + 1:]
+
+        _logger = get_logger("microsoft_servicebus")
+        _logger.debug(
+            "Validating Microsoft.servicebus resource type: %s", resourceSubType
+        )
+
+        # Service Bus Namespaces
+        if resourceSubType == "namespaces":
+            # servicebus namespaces are always zone redundant
+            return ZoneRedundancyValidationResult.Always
+
+        return ZoneRedundancyValidationResult.Unknown
