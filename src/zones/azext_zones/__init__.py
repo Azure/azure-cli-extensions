91876c953fa89b840d12f0d22132a3f08e2f6222
Niels Buit <nielsb@microsoft.com>
2025-05-16 13:58:39 +0200

    move validator loading out of init

--- src/zones/azext_zones/__init__.py
+++ src/zones/azext_zones/__init__.py
@@ -4,10 +4,9 @@
 # --------------------------------------------------------------------------------------------
 
 
-import importlib
-from pathlib import Path
 from azure.cli.core import AzCommandsLoader
 from azext_zones._help import helps  # pylint: disable=unused-import
+from ._resourceTypeValidation import load_validators
 from knack.log import get_logger
 
 
@@ -20,7 +19,7 @@ class ZonesCommandsLoader(AzCommandsLoader):
         zones_custom = CliCommandType(
             operations_tmpl='azext_zones.custom#{}',
             client_factory=cf_zones)
-        self.load_validators()
+        load_validators()
         super(ZonesCommandsLoader, self).__init__(cli_ctx=cli_ctx,
                                                   custom_command_type=zones_custom)
 
@@ -32,27 +31,6 @@ class ZonesCommandsLoader(AzCommandsLoader):
     def load_arguments(self, command):
         from azext_zones._params import load_arguments
         load_arguments(self, command)
-
-    def load_validators(self):
-        # Import all the resource type validator modules dynamically:
-        validators_dir = Path(__file__).parent / "resource_type_validators"
-        self._logger.debug("Starting resource type validator module import from %s", validators_dir)
-
-        try:
-            if validators_dir.exists():
-                for file in validators_dir.glob("*.py"):
-                    if file.name != "__init__.py":
-                        try:
-                            self._logger.debug("Importing resource type validator module: %s", file.name)
-                            module_name = f".resource_type_validators.{file.stem}"
-                            importlib.import_module(module_name, package=__package__)
-                        except ImportError as e:
-                            self._logger.warning("Failed to import module %s: %s", module_name, str(e))
-            else:
-                self._logger.error("Resource type validators directory not found: %s", validators_dir)
-
-        except Exception as e:  # pylint: disable=broad-except
-            self._logger.warning("Error scanning for resource type validator modules: %s", str(e))
 
 
 COMMAND_LOADER_CLS = ZonesCommandsLoader
