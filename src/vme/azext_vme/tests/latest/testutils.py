ce4fb18b5aa85f3378537caf86797bf8e0a61681
Yihua Cao (AzureStack) <yihuacao@microsoft.com>
2025-05-21 15:20:24 +0800

    Fix style and linter errors

--- src/vme/azext_vme/tests/latest/testutils.py
+++ src/vme/azext_vme/tests/latest/testutils.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from azure.mgmt.resource import ResourceManagementClient
 import os
 import json
