ef52fed515bde98bb3060ddd6b77c684c9c7f89b
Atharva Udapure <audapure@microsoft.com>
2025-05-22 12:11:13 +0530

    Fixed Solution Template Linter Issue

--- src/workload-orchestration/azext_workload_orchestration/aaz/latest/workload_orchestration/solution_template/__init__.py
+++ src/workload-orchestration/azext_workload_orchestration/aaz/latest/workload_orchestration/solution_template/__init__.py
@@ -15,7 +15,6 @@ from ._list import *
 from ._list_untitled1 import *
 from ._remove_version import *
 from ._show import *
-from _bulk_deploy_solution import *
+from ._bulk_deploy_solution import *
 # from ._update import *
 from ._wait import *
-from _bulk_deploy_solution import *
