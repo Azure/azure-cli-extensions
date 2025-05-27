f1fcb9468c9b2730fa2cf8926920f7b16a6f37b7
sophie zhao <sophiezhao@microsoft.com>
2025-05-27 11:24:53 -0400

    fix style issue in helpers.py

--- src/aks-preview/azext_aks_preview/_helpers.py
+++ src/aks-preview/azext_aks_preview/_helpers.py
@@ -443,6 +443,7 @@ def get_all_extensions_in_allow_list(result):
             output.append(obj)
     return output
 
+
 def get_extension_in_allow_list(result):
     if _check_if_extension_type_is_in_allow_list(result.extension_type.lower()):
         return result
