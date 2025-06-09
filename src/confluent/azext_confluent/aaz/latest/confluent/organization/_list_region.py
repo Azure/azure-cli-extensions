193b633ceecd4ef2f89deefd930c413ba3fc179f
Srinivas <alluri02@users.noreply.github.com>
2025-06-09 10:52:55 +0530

    confluent cli description update (#8786)
    * update the cli description to support ai related quiries
    
    * updated the version in setup.py
    
    * Update src/confluent/setup.py
    
    as per the reviewer suggestion
    
    Co-authored-by: AllyW <AllyW@users.noreply.github.com>
    
    * updated the version as per the pr comments
    
    * updated test recordings for confluent
    
    * fix linter and style issues in confluent
    
    ---------
    
    Co-authored-by: Srinivas Alluri <sralluri@microsoft.com>
    Co-authored-by: AllyW <AllyW@users.noreply.github.com>

--- src/confluent/azext_confluent/aaz/latest/confluent/organization/_list_region.py
+++ src/confluent/azext_confluent/aaz/latest/confluent/organization/_list_region.py
@@ -12,16 +12,16 @@ from azure.cli.core.aaz import *
 
 
 @register_command(
-    "confluent organization create-user",
+    "confluent organization list-region",
 )
-class CreateUser(AAZCommand):
-    """Invite a new user to join the Confluent organization.
+class ListRegion(AAZCommand):
+    """List all the supported regions within a Confluent organization.
     """
 
     _aaz_info = {
         "version": "2024-02-13",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.confluent/organizations/{}/access/default/createinvitation", "2024-02-13"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.confluent/organizations/{}/listregions", "2024-02-13"],
         ]
     }
 
@@ -54,40 +54,19 @@ class CreateUser(AAZCommand):
         # define Arg Group "Body"
 
         _args_schema = cls._args_schema
-        _args_schema.email = AAZStrArg(
-            options=["--email"],
+        _args_schema.search_filters = AAZDictArg(
+            options=["--search-filters"],
             arg_group="Body",
-            help="Email of the logged in user",
-        )
-        _args_schema.organization_id = AAZStrArg(
-            options=["--organization-id"],
-            arg_group="Body",
-            help="Id of the organization",
-        )
-        _args_schema.upn = AAZStrArg(
-            options=["--upn"],
-            arg_group="Body",
-            help="Upn of the logged in user",
+            help="Search filters for the request",
         )
 
-        # define Arg Group "InvitedUserDetails"
-
-        _args_schema = cls._args_schema
-        _args_schema.auth_type = AAZStrArg(
-            options=["--auth-type"],
-            arg_group="InvitedUserDetails",
-            help="Auth type of the user",
-        )
-        _args_schema.invited_email = AAZStrArg(
-            options=["--invited-email"],
-            arg_group="InvitedUserDetails",
-            help="UPN/Email of the user who is being invited",
-        )
+        search_filters = cls._args_schema.search_filters
+        search_filters.Element = AAZStrArg()
         return cls._args_schema
 
     def _execute_operations(self):
         self.pre_operations()
-        self.AccessInviteUser(ctx=self.ctx)()
+        self.OrganizationListRegions(ctx=self.ctx)()
         self.post_operations()
 
     @register_callback
@@ -102,7 +81,7 @@ class CreateUser(AAZCommand):
         result = self.deserialize_output(self.ctx.vars.instance, client_flatten=True)
         return result
 
-    class AccessInviteUser(AAZHttpOperation):
+    class OrganizationListRegions(AAZHttpOperation):
         CLIENT_TYPE = "MgmtClient"
 
         def __call__(self, *args, **kwargs):
@@ -116,7 +95,7 @@ class CreateUser(AAZCommand):
         @property
         def url(self):
             return self.client.format_url(
-                "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Confluent/organizations/{organizationName}/access/default/createInvitation",
+                "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Confluent/organizations/{organizationName}/listRegions",
                 **self.url_parameters
             )
 
@@ -175,15 +154,11 @@ class CreateUser(AAZCommand):
                 typ=AAZObjectType,
                 typ_kwargs={"flags": {"required": True, "client_flatten": True}}
             )
-            _builder.set_prop("email", AAZStrType, ".email")
-            _builder.set_prop("invitedUserDetails", AAZObjectType)
-            _builder.set_prop("organizationId", AAZStrType, ".organization_id")
-            _builder.set_prop("upn", AAZStrType, ".upn")
+            _builder.set_prop("searchFilters", AAZDictType, ".search_filters")
 
-            invited_user_details = _builder.get(".invitedUserDetails")
-            if invited_user_details is not None:
-                invited_user_details.set_prop("auth_type", AAZStrType, ".auth_type")
-                invited_user_details.set_prop("invitedEmail", AAZStrType, ".invited_email")
+            search_filters = _builder.get(".searchFilters")
+            if search_filters is not None:
+                search_filters.set_elements(AAZStrType, ".")
 
             return self.serialize_content(_content_value)
 
@@ -205,27 +180,53 @@ class CreateUser(AAZCommand):
             cls._schema_on_200 = AAZObjectType()
 
             _schema_on_200 = cls._schema_on_200
-            _schema_on_200.accepted_at = AAZStrType()
-            _schema_on_200.auth_type = AAZStrType()
-            _schema_on_200.email = AAZStrType()
-            _schema_on_200.expires_at = AAZStrType()
-            _schema_on_200.id = AAZStrType()
-            _schema_on_200.kind = AAZStrType()
-            _schema_on_200.metadata = AAZObjectType()
-            _schema_on_200.status = AAZStrType()
+            _schema_on_200.data = AAZListType()
 
-            metadata = cls._schema_on_200.metadata
-            metadata.created_at = AAZStrType()
-            metadata.deleted_at = AAZStrType()
-            metadata.resource_name = AAZStrType()
+            data = cls._schema_on_200.data
+            data.Element = AAZObjectType()
+
+            _element = cls._schema_on_200.data.Element
+            _element.id = AAZStrType()
+            _element.kind = AAZStrType()
+            _element.properties = AAZObjectType(
+                flags={"client_flatten": True},
+            )
+
+            properties = cls._schema_on_200.data.Element.properties
+            properties.metadata = AAZObjectType()
+            properties.spec = AAZObjectType()
+
+            metadata = cls._schema_on_200.data.Element.properties.metadata
+            metadata.created_timestamp = AAZStrType(
+                serialized_name="createdTimestamp",
+            )
+            metadata.deleted_timestamp = AAZStrType(
+                serialized_name="deletedTimestamp",
+            )
+            metadata.resource_name = AAZStrType(
+                serialized_name="resourceName",
+            )
             metadata.self = AAZStrType()
-            metadata.updated_at = AAZStrType()
+            metadata.updated_timestamp = AAZStrType(
+                serialized_name="updatedTimestamp",
+            )
+
+            spec = cls._schema_on_200.data.Element.properties.spec
+            spec.cloud = AAZStrType()
+            spec.name = AAZStrType()
+            spec.packages = AAZListType()
+            spec.region_name = AAZStrType(
+                serialized_name="regionName",
+            )
+
+            packages = cls._schema_on_200.data.Element.properties.spec.packages
+            packages.Element = AAZStrType()
 
             return cls._schema_on_200
 
 
-class _CreateUserHelper:
-    """Helper class for CreateUser"""
+class _ListRegionHelper:
+    """Helper class for ListRegion"""
 
 
-__all__ = ["CreateUser"]
+__all__ = ["ListRegion"]
