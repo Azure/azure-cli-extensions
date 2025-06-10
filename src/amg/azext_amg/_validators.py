7072a553dd58bd67ec78f658413ee302dffc0fb9
Yishi Wang <yishiwang@microsoft.com>
2025-06-10 15:34:52 +0800

    Remove msrestazure for amg extension

--- src/amg/azext_amg/_validators.py
+++ src/amg/azext_amg/_validators.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from msrestazure.tools import parse_resource_id
+from azure.mgmt.core.tools import parse_resource_id
 
 from knack.util import CLIError
 
