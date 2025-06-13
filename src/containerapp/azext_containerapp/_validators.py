dd64376bf59e8482f5fb2ef9a446b027b6b38646
Bowen Wan <bowan@microsoft.com>
2025-06-13 13:12:28 +0800

    Update _validators.py

--- src/containerapp/azext_containerapp/_validators.py
+++ src/containerapp/azext_containerapp/_validators.py
@@ -219,8 +219,8 @@ def validate_target_port_range(cmd, namespace):
 def validate_session_timeout_in_seconds(cmd, namespace):
     timeout_in_seconds = namespace.timeout_in_seconds
     if timeout_in_seconds is not None:
-        if timeout_in_seconds <= 0 or timeout_in_seconds > 240:
-            raise ValidationError("Timeout in seconds must be in range [1, 240].")
+        if timeout_in_seconds <= 0 or timeout_in_seconds > 220:
+            raise ValidationError("Timeout in seconds must be in range [1, 220].")
 
 
 def validate_debug(cmd, namespace):
