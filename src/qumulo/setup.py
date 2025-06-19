08982beb173269f7f12da0fb22fcdc6dec150cef
Avish Porwal <avishporwal@microsoft.com>
2025-06-19 15:52:20 +0530

    CLI Description Update for Qumulo

--- src/qumulo/setup.py
+++ src/qumulo/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 
 # HISTORY.rst entry.
-VERSION = '2.0.0'
+VERSION = '2.0.1'
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
