8a19986ef4c347229e1e0fdd65cbd3c0984cc37e
anushkasingh16 <anushkasingh@microsoft.com>
2025-06-27 10:07:20 -0700

    json parsing

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -3016,8 +3016,8 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             "aks nodepool show --resource-group={resource_group} --cluster-name={name} --name={nodepool_name}"
         )
         result = self.cmd(show_cmd).get_output_in_json()
-        assert result["mode"] == "Required"
-        assert result["kubeDNSOverrides"] == {
+        assert result["localdnsprofile"]["mode"] == "Required"
+        assert result["localdnsprofile"]["kubeDNSOverrides"] == {
             ".": {
                 "cacheDurationInSeconds": 3600,
                 "forwardDestination": "ClusterCoreDNS",
@@ -3039,7 +3039,7 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
                 "serveStaleDurationInSeconds": 3600
             }
         }
-        assert result["vnetDNSOverrides"] == {
+        assert result["localdnsprofile"]["vnetDNSOverrides"] == {
             ".": {
                 "cacheDurationInSeconds": 3600,
                 "forwardDestination": "VnetDNS",
@@ -3106,8 +3106,8 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             "aks nodepool show --resource-group={resource_group} --cluster-name={name} --name={nodepool_name}"
         )
         result = self.cmd(show_cmd).get_output_in_json()
-        assert result["mode"] == "Required"
-        assert result["kubeDNSOverrides"] == {
+        assert result["localdnsprofile"]["mode"] == "Required"
+        assert result["localdnsprofile"]["kubeDNSOverrides"] == {
             ".": {
                 "cacheDurationInSeconds": 3600,
                 "forwardDestination": "ClusterCoreDNS",
@@ -3129,7 +3129,7 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
                 "serveStaleDurationInSeconds": 3600
             }
         }
-        assert result["vnetDNSOverrides"] == {
+        assert result["localdnsprofile"]["vnetDNSOverrides"] == {
             ".": {
                 "cacheDurationInSeconds": 3600,
                 "forwardDestination": "VnetDNS",
