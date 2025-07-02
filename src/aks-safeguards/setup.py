9e8d524281d638c59daad8a899ea953f66623a0c
Nick Keller <keller.nicholas@outlook.com>
2025-07-02 02:42:12 +0000

    Update Python version classifiers in setup.py

--- src/aks-safeguards/setup.py
+++ src/aks-safeguards/setup.py
@@ -20,9 +20,10 @@ CLASSIFIERS = [
     'Intended Audience :: System Administrators',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
     'License :: OSI Approved :: MIT License',
 ]
 
