ab15e9bbd59325f6793f5735da41e0c821725b2b
Fuming Zhang <fumingzhang@microsoft.com>
2025-07-03 16:08:55 +1000

    fix

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -16374,7 +16374,7 @@ spec:
                 self.check("provisioningState", "Succeeded"),
             ],
         ).get_output_in_json()
-        nrg = mc.node_resource_group
+        nrg = mc["nodeResourceGroup"]
 
         # create bastion
         list_vnet_cmd = f"network vnet list -g {nrg} -o json"
