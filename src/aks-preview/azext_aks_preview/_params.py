053126df90654f03b6831dc34f6ef8b348367b57
ximeng zhao <ximengzhao@microsoft.com>
2025-08-25 22:51:17 +0000

    Remove the automatic preview label from az help command

--- src/aks-preview/azext_aks_preview/_params.py
+++ src/aks-preview/azext_aks_preview/_params.py
@@ -640,7 +640,7 @@ def load_arguments(self, _):
             ),
         )
         c.argument(
-            "sku", is_preview=True, arg_type=get_enum_type(sku_names)
+            "sku", arg_type=get_enum_type(sku_names)
         )
         c.argument(
             "tier", arg_type=get_enum_type(sku_tiers), validator=validate_sku_tier
@@ -1155,7 +1155,7 @@ def load_arguments(self, _):
             ),
         )
         c.argument(
-            "sku", is_preview=True, arg_type=get_enum_type(sku_names)
+            "sku", arg_type=get_enum_type(sku_names)
         )
         c.argument(
             "tier", arg_type=get_enum_type(sku_tiers), validator=validate_sku_tier
