d317885d78120e9151fcda16178ec131bd4b1e0a
Neha Arora <neha.arora@microsoft.com>
2025-09-05 13:59:35 -0700

    AKS: Change --enable-azure-container-storage --disable-azure-container-storage behavior and add --container-storage-version

--- src/aks-preview/azext_aks_preview/custom.py
+++ src/aks-preview/azext_aks_preview/custom.py
@@ -728,6 +728,7 @@ def aks_create(
     enable_ai_toolchain_operator=False,
     # azure container storage
     enable_azure_container_storage=None,
+    container_storage_version=None,
     storage_pool_name=None,
     storage_pool_size=None,
     storage_pool_sku=None,
@@ -948,6 +949,7 @@ def aks_update(
     # azure container storage
     enable_azure_container_storage=None,
     disable_azure_container_storage=None,
+    container_storage_version=None,
     storage_pool_name=None,
     storage_pool_size=None,
     storage_pool_sku=None,
