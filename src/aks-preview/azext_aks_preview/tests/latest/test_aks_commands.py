3d980de4e6c35f25aaddee69432bbfc42ac49bbc
Santhosh Prabhu <sanpra1989@gmail.com>
2025-09-13 21:27:00 -0700

    fix: switch to westcentralus

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -14245,7 +14245,7 @@ spec:
     @AKSCustomResourceGroupPreparer(
         random_name_length=17,
         name_prefix="clitest",
-        location="eastus2euap",
+        location="westcentralus",
     )
     def test_aks_create_with_acns_performance(
         self, resource_group, resource_group_location
