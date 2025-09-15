b9c8a4b108be7cf1a4d84cceb37f434ca7c16730
khushishah513 <khushishah13524@gmail.com>
2025-09-15 05:21:36 +0000

    updating function keys decorator

--- src/containerapp/azext_containerapp/containerapp_function_keys_decorator.py
+++ src/containerapp/azext_containerapp/containerapp_function_keys_decorator.py
@@ -17,8 +17,8 @@ from ._client_factory import handle_raw_exception
 logger = get_logger(__name__)
 
 
-class ContainerAppFunctionsDecorator(BaseResource):
-    """Base decorator for Container App Functions operations"""
+class ContainerAppFunctionKeysDecorator(BaseResource):
+    """Base decorator for Container App Function Keys operations"""
 
     def __init__(self, cmd: AzCliCommand, client: Any, raw_parameters: Dict, models: str):
         super().__init__(cmd, client, raw_parameters, models)
@@ -26,8 +26,38 @@ class ContainerAppFunctionsDecorator(BaseResource):
     def get_argument_function_name(self):
         return self.get_param("function_name")
 
+    def get_argument_revision_name(self):
+        return self.get_param("revision_name")
 
-class ContainerAppFunctionKeysListDecorator(ContainerAppFunctionsDecorator):
+    def validate_common_arguments(self):
+        """Validate common arguments required for all function operations"""
+        resource_group_name = self.get_argument_resource_group_name()
+        name = self.get_argument_name()
+        revision_name = self.get_argument_revision_name()
+
+        if not resource_group_name:
+            raise ValidationError("Resource group name is required.")
+        
+        if not name:
+            raise ValidationError("Container app name is required.")
+        
+        if not revision_name:
+            raise ValidationError("Revision name is required.")
+
+        return resource_group_name, name, revision_name
+
+    def validate_function_arguments(self):
+        """Validate arguments required for function-specific operations"""
+        resource_group_name, name, revision_name = self.validate_common_arguments()
+        function_name = self.get_argument_function_name()
+
+        if not function_name:
+            raise ValidationError("Function name is required.")
+
+        return resource_group_name, name, revision_name, function_name
+
+
+class ContainerAppFunctionKeysListDecorator(ContainerAppFunctionKeysDecorator):
     """Decorator for listing function keys"""
 
     def __init__(self, cmd: AzCliCommand, client: Any, raw_parameters: Dict, models: str):
@@ -36,18 +66,7 @@ class ContainerAppFunctionKeysListDecorator(ContainerAppFunctionsDecorator):
     def list_keys(self):
         """List keys for a specific function"""
         try:
-            function_name = self.get_argument_function_name()
-            resource_group_name = self.get_argument_resource_group_name()
-            name = self.get_argument_name()
-
-            if not resource_group_name:
-                raise ValidationError("Resource group name is required.")
-
-            if not name:
-                raise ValidationError("Container app name is required.")
-
-            if not function_name:
-                raise ValidationError("Function name is required.")
+            resource_group_name, name, revision_name, function_name = self.validate_function_arguments()
 
             return self.client.list_function_keys(
                 cmd=self.cmd,
@@ -59,7 +78,7 @@ class ContainerAppFunctionKeysListDecorator(ContainerAppFunctionsDecorator):
             handle_raw_exception(e)
 
 
-class ContainerAppFunctionKeysUpdateDecorator(ContainerAppFunctionsDecorator):
+class ContainerAppFunctionKeysUpdateDecorator(ContainerAppFunctionKeysDecorator):
     """Decorator for updating function keys"""
 
     def __init__(self, cmd: AzCliCommand, client: Any, raw_parameters: Dict, models: str):
@@ -71,26 +90,21 @@ class ContainerAppFunctionKeysUpdateDecorator(ContainerAppFunctionsDecorator):
     def get_argument_key_value(self):
         return self.get_param("key_value")
 
+    def validate_update_arguments(self):
+        """Validate arguments required for updating function keys"""
+        resource_group_name, name, revision_name, function_name = self.validate_function_arguments()
+        key_name = self.get_argument_key_name()
+
+        if not key_name:
+            raise ValidationError("Key name is required.")
+
+        return resource_group_name, name, revision_name, function_name, key_name
+
     def update_keys(self):
         """Update keys for a specific function"""
         try:
-            function_name = self.get_argument_function_name()
-            key_name = self.get_argument_key_name()
+            resource_group_name, name, revision_name, function_name, key_name = self.validate_update_arguments()
             key_value = self.get_argument_key_value()
-            resource_group_name = self.get_argument_resource_group_name()
-            name = self.get_argument_name()
-
-            if not resource_group_name:
-                raise ValidationError("Resource group name is required.")
-
-            if not name:
-                raise ValidationError("Container app name is required.")
-
-            if not function_name:
-                raise ValidationError("Function name is required.")
-
-            if not key_name:
-                raise ValidationError("Key name is required.")
 
             return self.client.update_function_keys(
                 cmd=self.cmd,
@@ -104,7 +118,7 @@ class ContainerAppFunctionKeysUpdateDecorator(ContainerAppFunctionsDecorator):
             handle_raw_exception(e)
 
 
-class ContainerAppFunctionHostKeysListDecorator(ContainerAppFunctionsDecorator):
+class ContainerAppFunctionHostKeysListDecorator(ContainerAppFunctionKeysDecorator):
     """Decorator for listing host keys"""
 
     def __init__(self, cmd: AzCliCommand, client: Any, raw_parameters: Dict, models: str):
@@ -113,14 +127,7 @@ class ContainerAppFunctionHostKeysListDecorator(ContainerAppFunctionsDecorator):
     def list_host_keys(self):
         """List host keys for the container app function host"""
         try:
-            resource_group_name = self.get_argument_resource_group_name()
-            name = self.get_argument_name()
-
-            if not resource_group_name:
-                raise ValidationError("Resource group name is required.")
-
-            if not name:
-                raise ValidationError("Container app name is required.")
+            resource_group_name, name, revision_name = self.validate_common_arguments()
 
             return self.client.list_host_keys(
                 cmd=self.cmd,
@@ -131,7 +138,7 @@ class ContainerAppFunctionHostKeysListDecorator(ContainerAppFunctionsDecorator):
             handle_raw_exception(e)
 
 
-class ContainerAppFunctionHostKeysUpdateDecorator(ContainerAppFunctionsDecorator):
+class ContainerAppFunctionHostKeysUpdateDecorator(ContainerAppFunctionKeysDecorator):
     """Decorator for updating host keys"""
 
     def __init__(self, cmd: AzCliCommand, client: Any, raw_parameters: Dict, models: str):
@@ -146,26 +153,25 @@ class ContainerAppFunctionHostKeysUpdateDecorator(ContainerAppFunctionsDecorator
     def get_argument_key_value(self):
         return self.get_param("key_value")
 
+    def validate_host_key_arguments(self):
+        """Validate arguments required for updating host keys"""
+        resource_group_name, name, revision_name = self.validate_common_arguments()
+        key_type = self.get_argument_key_type()
+        key_name = self.get_argument_key_name()
+
+        if not key_type:
+            raise ValidationError("Key type is required.")
+        
+        if not key_name:
+            raise ValidationError("Key name is required.")
+
+        return resource_group_name, name, revision_name, key_type, key_name
+
     def update_host_keys(self):
         """Update host keys for the container app function host"""
         try:
-            key_type = self.get_argument_key_type()
-            key_name = self.get_argument_key_name()
+            resource_group_name, name, revision_name, key_type, key_name = self.validate_host_key_arguments()
             key_value = self.get_argument_key_value()
-            resource_group_name = self.get_argument_resource_group_name()
-            name = self.get_argument_name()
-
-            if not resource_group_name:
-                raise ValidationError("Resource group name is required.")
-
-            if not name:
-                raise ValidationError("Container app name is required.")
-
-            if not key_type:
-                raise ValidationError("Key type is required.")
-
-            if not key_name:
-                raise ValidationError("Key name is required.")
 
             return self.client.update_host_keys(
                 cmd=self.cmd,
