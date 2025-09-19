d9bd529aef6b589e557278f107c98c709554d06b
Shivam Kumar <shivamkumar@microsoft.com>
2025-09-19 11:00:42 +0000

    Adding command param in debug command

--- src/containerapp/azext_containerapp/custom.py
+++ src/containerapp/azext_containerapp/custom.py
@@ -3591,7 +3591,7 @@ def list_maintenance_config(cmd, resource_group_name, env_name):
     return r
 
 
-def containerapp_debug(cmd, resource_group_name, name, container=None, revision=None, replica=None):
+def containerapp_debug(cmd, resource_group_name, name, container=None, revision=None, replica=None, command=None):
     logger.warning("Connecting...")
     conn = DebugWebSocketConnection(
         cmd=cmd,
@@ -3599,7 +3599,8 @@ def containerapp_debug(cmd, resource_group_name, name, container=None, revision=
         name=name,
         revision=revision,
         replica=replica,
-        container=container
+        container=container,
+        command=command
     )
 
     encodings = [SSH_DEFAULT_ENCODING, SSH_BACKUP_ENCODING]
