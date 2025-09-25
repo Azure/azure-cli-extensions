6ff639549724e07195c1ced2d387cf6e98b5db36
Dominic Ayre <domayre@outlook.com>
2025-09-25 20:29:38 +0000

    Fix typo

--- src/confcom/azext_confcom/tests/latest/test_confcom_acifragmentgen.py
+++ src/confcom/azext_confcom/tests/latest/test_confcom_acifragmentgen.py
@@ -147,7 +147,7 @@ def test_acifragmentgen_fragment_upload_fragment(docker_image, cert_chain):
 
     if "referrers" in oras_result:
         fragment_ref = oras_result["referrers"][0]["reference"]
-    elif oras_result.get("manifests")[0].get("artifact_type") == "application/x-ms-ccepolicy-frag":
+    elif oras_result.get("manifests")[0].get("artifactType") == "application/x-ms-ccepolicy-frag":
         fragment_ref = oras_result["reference"]
     else:
         raise AssertionError(f"{oras_result=}")
@@ -237,7 +237,7 @@ def test_acifragmentgen_fragment_attach(docker_image, cert_chain, capsysbinary):
 
     if "referrers" in oras_result:
         fragment_ref = oras_result["referrers"][0]["reference"]
-    elif oras_result["manifests"][0].get("artifact_type") == "application/x-ms-ccepolicy-frag":
+    elif oras_result["manifests"][0].get("artifactType") == "application/x-ms-ccepolicy-frag":
         fragment_ref = oras_result["reference"]
     else:
         raise AssertionError(f"{oras_result=}")
