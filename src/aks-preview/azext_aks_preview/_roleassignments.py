751101a21dc600b5c7bef5b7f3777889493087c4
Fuming Zhang <fumingzhang@microsoft.com>
2025-10-03 15:02:45 +1000

    fix

--- src/aks-preview/azext_aks_preview/_roleassignments.py
+++ src/aks-preview/azext_aks_preview/_roleassignments.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# pylint: disable=unused-import
 from azure.cli.command_modules.acs._roleassignments import (
     add_role_assignment,
     add_role_assignment_executor,
