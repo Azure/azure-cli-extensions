a7b76f606c4446a7da6f0d8bf3ece92c620f1ea3
Nikita Garg <nikitagarg@microsoft.com>
2025-10-13 13:25:39 +0530

    style fix

--- src/redisenterprise/azext_redisenterprise/_breaking_change.py
+++ src/redisenterprise/azext_redisenterprise/_breaking_change.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-from azure.cli.core.breaking_change import (register_required_flag_breaking_change, 
+from azure.cli.core.breaking_change import (register_required_flag_breaking_change,
                                             register_default_value_breaking_change)
 
 register_required_flag_breaking_change('redisenterprise create', '--public-network-access')
