ec030e70c5bf87a80552506abd36c3150ce5a510
ravindradongade <ravindra.sd86@gmail.com>
2025-10-15 12:46:36 +0530

    updated the api version to 2025-06-01 (#9296)
    Co-authored-by: Ravindra Dongade <rdongade@microsoft.com>

--- src/workload-orchestration/azext_workload_orchestration/aaz/latest/workload_orchestration/solution_template/_bulk_deploy_solution.py
+++ src/workload-orchestration/azext_workload_orchestration/aaz/latest/workload_orchestration/solution_template/_bulk_deploy_solution.py
@@ -21,9 +21,9 @@ class BulkDeploySolution(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2025-08-01",
+        "version": "2025-06-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/Microsoft.Edge/solutiontemplates/{}/versions/{}/bulkdeploysolution", "2025-08-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/Microsoft.Edge/solutiontemplates/{}/versions/{}/bulkdeploysolution", "2025-06-01"],
         ]
     }
 
@@ -231,7 +231,7 @@ class BulkDeploySolution(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2025-08-01",
+                    "api-version", "2025-06-01",
                     required=True,
                 ),
             }
