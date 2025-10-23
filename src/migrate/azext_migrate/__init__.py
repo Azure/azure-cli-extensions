8ae69d277561bc060be4953200fee71375020921
Saif Al-Din Ali <saifaldinali@microsoft.com>
2025-10-23 00:11:47 -0700

    New version

--- src/migrate/azext_migrate/__init__.py
+++ src/migrate/azext_migrate/__init__.py
@@ -18,13 +18,24 @@ class MigrateCommandsLoader(AzCommandsLoader):
 
         super().__init__(
             cli_ctx=cli_ctx,
-            custom_command_type=migrate_custom,
-            resource_type=ResourceType.MGMT_MIGRATE
+            custom_command_type=migrate_custom
         )
 
     def load_command_table(self, args):
         from azext_migrate.commands \
             import load_command_table
+        
+        from azure.cli.core.aaz import load_aaz_command_table
+        try:
+            from . import aaz
+        except ImportError:
+            aaz = None
+        if aaz:
+            load_aaz_command_table(
+                loader=self,
+                aaz_pkg_name=aaz.__name__,
+                args=args
+            )
         load_command_table(self, args)
         return self.command_table
 
