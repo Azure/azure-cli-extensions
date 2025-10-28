6cd9f319a4f49c44bb04afc151e9c2c85f708328
Saif Al-Din Ali <saifaldinali@microsoft.com>
2025-10-28 14:20:55 -0700

    Revert "Create get job and remove replication commands"
    This reverts commit 0f7acb7d1c28a7dcf56b722ccad99604bebb66e5.

--- src/migrate/azext_migrate/commands.py
+++ src/migrate/azext_migrate/commands.py
@@ -6,11 +6,9 @@
 
 def load_command_table(self, _):
     # Azure Local Migration Commands
-    with self.command_group('migrate') as g:
+    with self.command_group('migrate local') as g:
         g.custom_command('get-discovered-server', 'get_discovered_server')
 
     with self.command_group('migrate local replication') as g:
         g.custom_command('init', 'initialize_replication_infrastructure')
         g.custom_command('new', 'new_local_server_replication')
-        g.custom_command('remove', 'remove_local_server_replication')
-        g.custom_command('get-job', 'get_local_replication_job')
