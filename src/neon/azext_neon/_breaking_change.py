bc9ccb9ae1d3626ca1e41cc2c1115d47aad5e13c
Raja singhal <singhalraja.30003@gmail.com>
2025-10-29 11:57:28 +0000

    Deprecate 'az neon' command group with end-of-life notice and migration guidance

---
+++ src/neon/azext_neon/_breaking_change.py
@@ -0,0 +1,9 @@
+
+from azure.cli.core.breaking_change import register_command_group_deprecate
+from azure.cli.core.breaking_change import register_other_breaking_change
+
+register_command_group_deprecate('az neon')
+
+message = """Deprecation Notice: The Neon Azure Native Integration is being deprecated and will reach end of life on January 31, 2026. Transfer your projects to a Neon managed organization today. Migration documentation is available at https://neon.com/docs/import/migrate-from-azure-native"""
+
+register_other_breaking_change('az neon', message)
