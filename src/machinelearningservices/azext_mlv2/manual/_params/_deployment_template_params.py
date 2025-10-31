663d1da57c38ef1368fc600b04b7571745136f1d
kshitij-microsoft <kchawla@microsoft.com>
2025-10-31 13:23:01 +0530

    make reg and version mandatory

--- src/machinelearningservices/azext_mlv2/manual/_params/_deployment_template_params.py
+++ src/machinelearningservices/azext_mlv2/manual/_params/_deployment_template_params.py
@@ -9,9 +9,11 @@ def add_deployment_template_common_param(
     c,
     name_help_message="Name of the deployment template.",
     version_help_message="Version of the deployment template.",
+    name_required=True,
+    version_required=True,
 ):
-    c.argument("name", options_list=["--name", "-n"], help=name_help_message)
-    c.argument("version", options_list=["--version", "-v"], help=version_help_message)
+    c.argument("name", options_list=["--name", "-n"], help=name_help_message, required=name_required)
+    c.argument("version", options_list=["--version", "-v"], help=version_help_message, required=version_required)
 
 
 def load_deployment_template_params(self):
@@ -20,58 +22,64 @@ def load_deployment_template_params(self):
         c.argument(
             "registry_name",
             options_list=["--registry-name", "-r"],
+            required=True,
             help="Name of the registry. This is required since deployment templates only support registry-name and not workspace.",
         )
 
     with self.argument_context("ml deployment-template get") as c:
         add_common_params(c)
-        add_deployment_template_common_param(c)
+        add_deployment_template_common_param(c, name_required=True, version_required=True)
         c.argument(
             "registry_name",
             options_list=["--registry-name", "-r"],
+            required=True,
             help="Name of the registry. This is required since deployment templates only support registry-name and not workspace.",
         )
 
     with self.argument_context("ml deployment-template create") as c:
         add_common_params(c)
-        add_deployment_template_common_param(c)
+        add_deployment_template_common_param(c, name_required=False, version_required=False)  # Optional for create since they can come from file
         add_lro_param(c)
         add_file_param(c, "deployment-template", "https://aka.ms/ml-cli-v2-deployment-template-yaml")
         add_override_param(c)
         c.argument(
             "registry_name",
             options_list=["--registry-name", "-r"],
+            required=True,
             help="Name of the registry. This is required since deployment templates only support registry-name and not workspace.",
         )
 
     with self.argument_context("ml deployment-template update") as c:
         add_common_params(c)
-        add_deployment_template_common_param(c)
+        add_deployment_template_common_param(c, name_required=True, version_required=True)
         add_override_param(c)
         add_description_param(c, help_message="Description of the deployment template.")
         add_tags_param(c)
         c.argument(
             "registry_name",
             options_list=["--registry-name", "-r"],
+            required=True,
             help="Name of the registry. This is required since deployment templates only support registry-name and not workspace.",
         )
 
     with self.argument_context("ml deployment-template archive") as c:
         add_common_params(c)
-        add_deployment_template_common_param(c)
+        add_deployment_template_common_param(c, name_required=True, version_required=True)
         add_lro_param(c)
         c.argument(
             "registry_name",
             options_list=["--registry-name", "-r"],
+            required=True,
             help="Name of the registry. This is required since deployment templates only support registry-name and not workspace.",
         )
 
     with self.argument_context("ml deployment-template restore") as c:
         add_common_params(c)
-        add_deployment_template_common_param(c)
+        add_deployment_template_common_param(c, name_required=True, version_required=True)
         add_lro_param(c)
         c.argument(
             "registry_name",
             options_list=["--registry-name", "-r"],
+            required=True,
             help="Name of the registry. This is required since deployment templates only support registry-name and not workspace.",
         )
