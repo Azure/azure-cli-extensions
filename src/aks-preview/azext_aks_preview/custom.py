1d0f45c2fd4483e5bd56385d1387d75fc4a6d64e
kingoliver@microsoft.com <kingoliver@microsoft.com>
2025-11-03 11:51:11 -0500

    catch enable / disable at the same time

--- src/aks-preview/azext_aks_preview/custom.py
+++ src/aks-preview/azext_aks_preview/custom.py
@@ -4353,6 +4353,10 @@ def aks_approuting_update(
         enable_default_domain=False,
         disable_default_domain=False
 ):
+
+    if enable_default_domain and disable_default_domain:
+        raise CLIError("Conflicting flags. Cannot --enable-default-domain and --disable-default-domain at the same time.")
+
     return _aks_approuting_update(
         cmd,
         client,
