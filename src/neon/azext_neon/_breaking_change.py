8934e12c8b1da64e0cfccb7dbd7b32013a0320a6
Raja singhal <singhalraja.30003@gmail.com>
2025-11-03 06:49:04 +0000

    Added license

--- src/neon/azext_neon/_breaking_change.py
+++ src/neon/azext_neon/_breaking_change.py
@@ -1,3 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 
 from azure.cli.core.breaking_change import register_command_group_deprecate
 from azure.cli.core.breaking_change import register_other_breaking_change
