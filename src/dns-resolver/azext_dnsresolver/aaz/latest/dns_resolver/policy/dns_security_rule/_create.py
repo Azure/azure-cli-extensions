1461e126b6270c5d7cd2770369a110138f13f78f
James Voong <jamesvoong@microsoft.com>
2025-11-04 14:21:11 -0800

    Remove invalid example

--- src/dns-resolver/azext_dnsresolver/aaz/latest/dns_resolver/policy/dns_security_rule/_create.py
+++ src/dns-resolver/azext_dnsresolver/aaz/latest/dns_resolver/policy/dns_security_rule/_create.py
@@ -20,9 +20,6 @@ class Create(AAZCommand):
     :example: Upsert DNS security rule
         az dns-resolver policy dns-security-rule create --resource-group sampleResourceGroup --policy-name sampleDnsResolverPolicy --dns-security-rule-name sampleDnsSecurityRule --location westus2 --tags "{key1:value1}" --priority 100 --action "{action-type:Block}" --domain-lists "[{id:/subscriptions/abdd4249-9f34-4cc6-8e42-c2e32110603e/resourceGroups/sampleResourceGroup/providers/Microsoft.Network/dnsResolverDomainLists/sampleDnsResolverDomainList}]" --rule-state Enabled
 
-    :example: Upsert DNS security rule without domain list
-        az dns-resolver policy dns-security-rule create --resource-group sampleResourceGroup --policy-name sampleDnsResolverPolicy --dns-security-rule-name sampleDnsSecurityRule --location westus2 --action "{action-type:Block}" --rule-state Enabled --priority 100 --tags "{key1:value1}"
-
     :example: Upsert DNS security rule with managed domain list
         az dns-resolver policy dns-security-rule create --resource-group sampleResourceGroup --policy-name sampleDnsResolverPolicy --dns-security-rule-name sampleDnsSecurityRule --location westus2 --action "{action-type:Block}" --rule-state Enabled --priority 100 --tags "{key1:value1}" --managed-domain-lists "[AzureDnsThreatIntel]"
     """
