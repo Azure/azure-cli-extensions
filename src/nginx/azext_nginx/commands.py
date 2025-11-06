382ba6113e9372b2d3225522a9f98c28ef22f0b6
Brian Kimutai <briankimutai@microsoft.com>
2025-11-06 12:22:00 +0300

    Added examples for WAF create and update

--- src/nginx/azext_nginx/commands.py
+++ src/nginx/azext_nginx/commands.py
@@ -9,3 +9,15 @@ def load_command_table(self, _):  # pylint: disable=unused-argument
     with self.command_group('nginx deployment configuration'):
         from .custom import ConfigurationUpdate
         self.command_table["nginx deployment configuration update"] = ConfigurationUpdate(loader=self)
+
+    with self.command_group('nginx deployment waf-policy') as g:
+        g.command('create', 'create_waf_policy', examples=[
+            {'name': 'Create a WAF policy', 
+            'text': 'az nginx deployment waf-policy create --name mypolicyname --deployment-name mydeploymentname --resource-group myresourcegroupname --filepath /path/to/my/policy.json --content mycontentfileinbase64encoding'}
+        ])
+
+    with self.command_group('nginx deployment waf-policy') as g:
+        g.command('update', 'update_waf_policy', examples=[
+            {'name': 'Update a WAF policy', 
+            'text': 'az nginx deployment waf-policy update --name mypolicyname --deployment-name mydeploymentname --resource-group myresourcegroupname --filepath /path/to/my/policy.json --content mycontentfileinbase64encoding'}
+        ])
