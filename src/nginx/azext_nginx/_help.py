ef08b13eb456edf80caa05d288f3a97a65ec537d
Brian Kimutai <briankimutai@microsoft.com>
2025-11-06 13:48:41 +0300

    Added examples for WAF create and update

--- src/nginx/azext_nginx/_help.py
+++ src/nginx/azext_nginx/_help.py
@@ -14,14 +14,14 @@ helps['nginx deployment waf-policy create'] = """
 type: command
 short-summary: Command to create an Nginx Deployment WAF Policy.
 examples:
-    - name: Create a WAF policy for a Nginx deployment
-      text: az nginx deployment waf-policy create --name mypolicyname --deployment-name mydeploymentname --resource-group myresourcegroupname --filepath /path/to/my/policy.json --content mycontentfileinbase64encoding
+  - name: Create a WAF policy for a Nginx deployment
+    text: az nginx deployment waf-policy create --name mypolicyname --deployment-name mydeploymentname --resource-group myresourcegroupname --filepath /path/to/my/policy.json --content mycontentfileinbase64encoding
 """
 
 helps['nginx deployment waf-policy update'] = """
 type: command
 short-summary: Command to update an Nginx Deployment WAF Policy.
 examples:
-    - name: Update a WAF policy for a Nginx deployment
-      text: az nginx deployment waf-policy update --name mypolicyname --deployment-name mydeploymentname --resource-group myresourcegroupname --filepath /path/to/my/policy.json --content mycontentfileinbase64encoding
+  - name: Update a WAF policy for a Nginx deployment
+    text: az nginx deployment waf-policy update --name mypolicyname --deployment-name mydeploymentname --resource-group myresourcegroupname --filepath /path/to/my/policy.json --content mycontentfileinbase64encoding
 """
