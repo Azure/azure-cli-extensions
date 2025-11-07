8d8885a51a62299dbd4f029c4c0dfe75f1ac730a
Dominic Ayre <domayre@outlook.com>
2025-11-07 14:54:52 +0000

    Add the --with-container flag to acifragmentgen

--- src/confcom/azext_confcom/custom.py
+++ src/confcom/azext_confcom/custom.py
@@ -3,8 +3,10 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import json
 import os
 import sys
+import tempfile
 from typing import Optional
 
 from azext_confcom import config
@@ -18,7 +20,9 @@ from azext_confcom.errors import eprint
 from azext_confcom.fragment_util import get_all_fragment_contents
 from azext_confcom.init_checks import run_initial_docker_checks
 from azext_confcom.kata_proxy import KataPolicyGenProxy
-from azext_confcom.security_policy import AciPolicy, OutputType
+from azext_confcom.security_policy import AciPolicy, OutputType, policy_deserialize
+from azext_confcom.lib.policy import Container
+from azext_confcom.lib.serialization import policy_serialize, policy_deserialize
 from azext_confcom.template_util import (
     get_image_name, inject_policy_into_template, inject_policy_into_yaml,
     pretty_print_func, print_existing_policy_from_arm_template,
@@ -386,6 +390,12 @@ def acifragmentgen_confcom(
         feed = get_image_name(image_target)
 
     fragment_text = policy.generate_fragment(namespace, svn, output_type, omit_id=omit_id)
+    with tempfile.NamedTemporaryFile(mode="w+", delete=True) as temp_policy_file:
+        temp_policy_file.write(fragment_text)
+        temp_policy_file.flush()
+        policy_obj = policy_deserialize(temp_policy_file.name)
+        policy_obj.containers.extend(Container(**json.loads(c)) for c in container_definitions)
+        fragment_text = policy_serialize(policy_obj)
 
     if output_type != security_policy.OutputType.DEFAULT and not no_print:
         print(fragment_text)
--- src/confcom/azext_confcom/lib/serialization.py
+++ src/confcom/azext_confcom/lib/serialization.py
@@ -1,15 +1,15 @@
-# --------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-# --------------------------------------------------------------------------------------------
+
+
 
 from dataclasses import asdict
 import json
 from pathlib import Path
 from textwrap import dedent
+from typing import Union
 
 from azext_confcom.lib.opa import opa_eval
-from azext_confcom.lib.policy import Container, Fragment, Policy
+from azext_confcom.lib.policy import Container, FragmentReference, Fragment, Policy
+import re
 
 
 def rego_dict_factory(fields: list) -> dict:
@@ -27,18 +27,19 @@ def rego_dict_factory(fields: list) -> dict:
     return rego_dict
 
 
-def policy_serialize(policy: Policy):
+# This is a single entrypoint for serializing both Policy and Fragment objects
+def policy_serialize(policy: Union[Policy, Fragment]):
 
-        policy_dict = asdict(policy)
-        fragments_json = json.dumps(policy_dict.pop("fragments"), indent=2)
-        containers_json = json.dumps(policy_dict.pop("containers"), indent=2)
+    if isinstance(policy, Fragment):
+        return fragment_serialize(policy)
 
-        return dedent(
+    policy_dict = asdict(policy)
+    fragments_json = json.dumps(policy_dict.pop("fragments"), indent=2)
+    containers_json = json.dumps(policy_dict.pop("containers"), indent=2)
+
+    return dedent(
 f"""
 package {policy_dict.pop('package')}
-
-import future.keywords.every
-import future.keywords.in
 
 api_version := "{policy_dict.pop('api_version')}"
 framework_version := "{policy_dict.pop('framework_version')}"
@@ -47,7 +48,7 @@ fragments := {fragments_json}
 
 containers := {containers_json}
 
-{chr(10).join(f"{key} := {str(value).lower()}" for key, value in policy_dict.items())}
+{chr(10).join(f"{key} := {str(value).lower()}" for key, value in policy_dict.items() if key.startswith("allow"))}
 
 mount_device := data.framework.mount_device
 unmount_device := data.framework.unmount_device
@@ -72,14 +73,44 @@ reason := {{"errors": data.framework.errors}}
         )
 
 
+def fragment_serialize(fragment: Fragment):
+
+    fragment_dict = asdict(fragment)
+    fragments_json = json.dumps(fragment_dict.pop("fragments"), indent=2)
+    containers_json = json.dumps(fragment_dict.pop("containers"), indent=2)
+
+    return dedent(
+f"""
+package {fragment_dict.pop('package')}
+
+svn := "{fragment_dict.pop('svn')}"
+framework_version := "{fragment_dict.pop('framework_version')}"
+
+fragments := {fragments_json}
+
+containers := {containers_json}
+"""
+        )
+
+
 def policy_deserialize(file_path: str):
-    raw_json = opa_eval(Path(file_path), "data.policy")["result"][0]["expressions"][0]["value"]
+
+    with open(file_path, 'r') as f:
+        content = f.read()
+
+    package_match = re.search(r'package\s+(\S+)', content)
+    package_name = package_match.group(1)
+
+    PolicyType = Policy if package_name == "policy" else Fragment
+
+    raw_json = opa_eval(Path(file_path), f"data.{package_name}")["result"][0]["expressions"][0]["value"]
 
     raw_fragments = raw_json.pop("fragments", [])
     raw_containers = raw_json.pop("containers", [])
 
-    return Policy(
-        fragments=[Fragment(**fragment) for fragment in raw_fragments],
+    return PolicyType(
+        package=package_name,
+        fragments=[FragmentReference(**fragment) for fragment in raw_fragments],
         containers=[Container(**container) for container in raw_containers],
         **raw_json
     )
