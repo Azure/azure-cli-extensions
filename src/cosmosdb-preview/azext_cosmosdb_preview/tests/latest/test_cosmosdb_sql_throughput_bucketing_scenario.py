37d08193062582dd93498e63b195b8446746f390
Achint Agrawal <ag.achint@gmail.com>
2025-11-07 12:49:19 +0530

    Add test and update documentation for IsDefaultBucket

--- src/cosmosdb-preview/azext_cosmosdb_preview/tests/latest/test_cosmosdb_sql_throughput_bucketing_scenario.py
+++ src/cosmosdb-preview/azext_cosmosdb_preview/tests/latest/test_cosmosdb_sql_throughput_bucketing_scenario.py
@@ -15,6 +15,7 @@ class Cosmosdb_throughputBucketingTest(ScenarioTest):
         col = self.create_random_name(prefix='cli', length=15)
         db_name = self.create_random_name(prefix='cli', length=15)
         throughput_buckets = '"[{\\"id\\": 1, \\"maxThroughputPercentage\\": 10 }, {\\"id\\": 2, \\"maxThroughputPercentage\\": 20 }]"'
+        updated_throughput_buckets = '"[{\\"id\\": 1, \\"maxThroughputPercentage\\": 15 }, {\\"id\\": 2, \\"maxThroughputPercentage\\": 25, \\"isDefaultBucket\\": true }]"'
         empty_throughput_buckets = '"[]"'
 
         # This test needs to be run on the following account since it has the feature flag enabled
@@ -24,6 +25,7 @@ class Cosmosdb_throughputBucketingTest(ScenarioTest):
             'db_name': db_name,
             'col': col,
             'throughput_buckets': throughput_buckets,
+            'updated_throughput_buckets': updated_throughput_buckets,
             'empty_throughput_buckets': empty_throughput_buckets
         })
 
@@ -43,8 +45,10 @@ class Cosmosdb_throughputBucketingTest(ScenarioTest):
         assert len(actual_throughput_buckets) == 2
         assert actual_throughput_buckets[0]["id"] == 1
         assert actual_throughput_buckets[0]["maxThroughputPercentage"] == 10
+        assert actual_throughput_buckets[0].get("isDefaultBucket") is None
         assert actual_throughput_buckets[1]["id"] == 2
         assert actual_throughput_buckets[1]["maxThroughputPercentage"] == 20
+        assert actual_throughput_buckets[1].get("isDefaultBucket") is None
 
         # Test: Change throughput without specifying throughput buckets - buckets should be retained
         self.cmd('az cosmosdb sql container throughput update -g {rg} -a {acc} -d {db_name} -n {col} --throughput 4000')
@@ -56,11 +60,29 @@ class Cosmosdb_throughputBucketingTest(ScenarioTest):
         assert len(retained_throughput_buckets) == 2
         assert retained_throughput_buckets[0]["id"] == 1
         assert retained_throughput_buckets[0]["maxThroughputPercentage"] == 10
+        assert retained_throughput_buckets[0].get("isDefaultBucket") is None
         assert retained_throughput_buckets[1]["id"] == 2
         assert retained_throughput_buckets[1]["maxThroughputPercentage"] == 20
+        assert retained_throughput_buckets[1].get("isDefaultBucket") is None
 
         # Verify throughput was actually updated
         assert throughput_resource_json["resource"]["throughput"] == 4000
+
+        # Test: Update throughput buckets with new values and verify isDefaultBucket is correctly set
+        self.cmd('az cosmosdb sql container throughput update -g {rg} -a {acc} -d {db_name} -n {col} --throughput 5000 --throughput-buckets {updated_throughput_buckets}')
+
+        throughput_resource_json = self.cmd('az cosmosdb sql container throughput show -g {rg} -a {acc} -d {db_name} -n {col}').get_output_in_json()
+        updated_buckets = throughput_resource_json["resource"]["throughputBuckets"]
+
+        # Verify updated buckets have correct values and default bucket is set
+        assert len(updated_buckets) == 2
+        assert updated_buckets[0]["id"] == 1
+        assert updated_buckets[0]["maxThroughputPercentage"] == 15
+        assert updated_buckets[0].get("isDefaultBucket") is None
+        assert updated_buckets[1]["id"] == 2
+        assert updated_buckets[1]["maxThroughputPercentage"] == 25
+        assert updated_buckets[1]["isDefaultBucket"] == True
+        assert throughput_resource_json["resource"]["throughput"] == 5000
 
         # Test: Set throughput buckets to empty array - buckets should be removed
         self.cmd('az cosmosdb sql container throughput update -g {rg} -a {acc} -d {db_name} -n {col} --throughput 4000 --throughput-buckets {empty_throughput_buckets}')
