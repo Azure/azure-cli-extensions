d5ea51626d6e91dc6e02bcacb72ad55d460f471c
Dominic Ayre <domayre@outlook.com>
2025-11-17 11:07:05 +0000

    Work around azdev test not respecting the session scope

--- src/confcom/azext_confcom/tests/conftest.py
+++ src/confcom/azext_confcom/tests/conftest.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import functools
 import importlib
 import subprocess
 import tempfile
@@ -13,12 +14,19 @@ import shutil
 from pathlib import Path
 
 
+# This is required to work around the implementation of azdev test. It runs
+# separate pytest invocations for each test, making it impossible to respect the
+# session scope. Therefore, we need to manually ensure wheels are not built
+# separately for each test.
+@functools.lru_cache()
+def get_wheel_dir():
+    return Path(tempfile.mkdtemp(prefix="confcom_wheels_"))
+
+
 # This fixture ensures tests are run against final built wheels of the extension
 # instead of the unbuilt local code, which may have breaking differences with
 # the thing we actually ship to users. All but the test modules themselves are
 # replaced with the wheel in case the tests themselves rely on unshipped code.
-
-
 @pytest.fixture(autouse=True, scope="session")
 def run_on_wheel(request):
 
@@ -26,7 +34,8 @@ def run_on_wheel(request):
     extensions_to_build = {module.__name__.split(".")[0] for module in modules_to_test}
     extension_dirs = {Path(a.split("/azext_")[0]) for a in request.config.args}
 
-    with tempfile.TemporaryDirectory() as build_dir:
+    build_dir = get_wheel_dir()
+    if not any(build_dir.iterdir()):
 
         # Delete the extensions build dir, as azdev extension build doesn't
         # reliably handle changes
@@ -37,16 +46,16 @@ def run_on_wheel(request):
         # Build all extensions being tested into wheels
         for extension in extensions_to_build:
             subprocess.run(
-                ["azdev", "extension", "build", extension.replace("azext_", ""), "--dist-dir", build_dir],
+                ["azdev", "extension", "build", extension.replace("azext_", ""), "--dist-dir", build_dir.as_posix()],
                 check=True,
             )
 
             # Add the wheel to the path and reload extension modules so the
             # tests pick up the wheel code over the unbuilt code
-            sys.path.insert(0, Path(build_dir).glob("*.whl").__next__().as_posix())
+            sys.path.insert(0, build_dir.glob("*.whl").__next__().as_posix())
             for module in list(sys.modules.values()):
                 if extension in module.__name__ and module not in modules_to_test:
                     del sys.modules[module.__name__]
                     importlib.import_module(module.__name__)
 
-        yield
+    yield
