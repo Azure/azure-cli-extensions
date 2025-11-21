68ea6cb3efaa7a2155fe5655548d67ca0ff111e2
Dominic Ayre <domayre@outlook.com>
2025-11-20 16:59:46 -0800

    [confcom] Fix opa installation on windows and in strict networking environments (#9440)
    * [confcom] Fix opa installation on windows and in strict networking environments
    
    * Bump version and history
    
    * Fix merge conflict from previous commits
    
    * Add clear error if user on unsupported platform
    
    * .

--- src/confcom/azext_confcom/tests/conftest.py
+++ src/confcom/azext_confcom/tests/conftest.py
@@ -14,6 +14,7 @@ import sys
 import shutil
 
 from pathlib import Path
+import zipfile
 
 
 # This fixture ensures tests are run against final built wheels of the extension
@@ -57,9 +58,9 @@ def run_on_wheel(request):
                     if (extension_dir / "build").exists():
                         shutil.rmtree((extension_dir / "build").as_posix(), ignore_errors=True)
 
-                if not any(build_dir.glob(f"{extension_name}*.whl")):
+                if not any(build_dir.glob(f"*{extension_name}*.whl")):
                     subprocess.run(
-                        ["azdev", "extension", "build", extension.replace("azext_", ""), "--dist-dir", build_dir.as_posix()],
+                        ["azdev", "extension", "build", extension_name, "--dist-dir", build_dir.as_posix()],
                         check=True,
                     )
 
@@ -68,7 +69,15 @@ def run_on_wheel(request):
 
     # Add the wheel to the path and reload extension modules so the
     # tests pick up the wheel code over the unbuilt code
-    sys.path.insert(0, build_dir.glob("*.whl").__next__().as_posix())
+    wheel_path = next(build_dir.glob("*.whl"))
+
+    expanded_dir = build_dir / f"{wheel_path.stem}_expanded"
+    if not expanded_dir.exists():
+        expanded_dir.mkdir(exist_ok=True)
+        with zipfile.ZipFile(wheel_path, "r") as z:
+            z.extractall(expanded_dir)
+
+    sys.path.insert(0, expanded_dir.resolve().as_posix())
     for module in list(sys.modules.values()):
         if extension in module.__name__ and module not in modules_to_test:
             del sys.modules[module.__name__]
