226b22752e55becf68868ccd40ab79273a1373a8
mihret <mihret.kidane@oracle.com>
2025-11-21 09:43:25 -0600

    tests and recordings

--- src/oracle-database/azext_oracle_database/tests/latest/test_oracle_basedb_delete.py
+++ src/oracle-database/azext_oracle_database/tests/latest/test_oracle_basedb_delete.py
@@ -5,7 +5,7 @@ class OracleBaseDbDeleteScenarioTest(ScenarioTest):
     def setUp(self):
         super().setUp()
         self.kwargs.update({
-            'resource_group': 'AzureCli',
+            'resource_group': 'PowerShellTestRg',
         })
 
     def test_basedb_delete_operations(self):
@@ -13,14 +13,14 @@ class OracleBaseDbDeleteScenarioTest(ScenarioTest):
         # Delete db systems
         self.cmd(
             'az oracle-database db-system delete '
-            '--name AzureCliSdkNe '
-            '--resource-group {resource_group} '
+            '--name bhDBiad1 '
+            '--resource-group bhtest '
             '--yes --no-wait'
         )
         # Delete network anchor
         self.cmd(
             'az oracle-database network-anchor delete '
-            '--name powershelltest '
+            '--name PowershellTest1 '
             '--resource-group {resource_group} '
             '--yes --no-wait'
         )
@@ -28,7 +28,7 @@ class OracleBaseDbDeleteScenarioTest(ScenarioTest):
         # Delete resource anchor
         self.cmd(
             'az oracle-database resource-anchor delete '
-            '--name  AzureCliTestRA '
+            '--name  powershell '
             '--resource-group {resource_group} '
             '--yes --no-wait'
         )
