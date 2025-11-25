8609ef5a2ac8e6e463ace495c8de7528f9e75f8a
pjohari-ms <84465928+pjohari-ms@users.noreply.github.com>
2025-11-25 14:47:44 +0530

    [CosmosDB] az cosmosdb fleet: Update GA contract (#9395)
    * Changed version number
    
    * Generated python SDK
    
    * Add test and update documentation for IsDefaultBucket
    
    * Successful recordings
    
    * Update and record throughput bucketing test
    
    * Garnet issues fixed
    
    * Fleet contract updated
    
    * Flake 8
    
    * Live_only tests due to missing role assignments
    
    * Mongo cluster tests working
    
    * Corrected live_only
    
    * Corrected HISTORY entries
    
    * Remaining recordings
    
    * Bug fix
    
    * Remaining role issue - live_only
    
    * Correct bucketing test recording
    
    * Updated versioning
    
    ---------
    
    Co-authored-by: Achint Agrawal <ag.achint@gmail.com>

--- src/cosmosdb-preview/azext_cosmosdb_preview/tests/latest/test_cosmosdb_sql_throughput_bucketing_scenario.py
+++ src/cosmosdb-preview/azext_cosmosdb_preview/tests/latest/test_cosmosdb_sql_throughput_bucketing_scenario.py
@@ -14,7 +14,7 @@ class Cosmosdb_throughputBucketingTest(ScenarioTest):
     def test_cosmosdb_sql_throughput_bucketing(self, resource_group):
         col = self.create_random_name(prefix='cli', length=15)
         db_name = self.create_random_name(prefix='cli', length=15)
-        throughput_buckets = '"[{\\"id\\": 1, \\"maxThroughputPercentage\\": 10 }, {\\"id\\": 2, \\"maxThroughputPercentage\\": 20 }]"'
+        throughput_buckets = '"[{\\"id\\": 1, \\"maxThroughputPercentage\\": 10 }, {\\"id\\": 2, \\"maxThroughputPercentage\\": 20, \\"isDefaultBucket\\": true }]"'
         empty_throughput_buckets = '"[]"'
 
         # This test needs to be run on the following account since it has the feature flag enabled
@@ -43,8 +43,10 @@ class Cosmosdb_throughputBucketingTest(ScenarioTest):
         assert len(actual_throughput_buckets) == 2
         assert actual_throughput_buckets[0]["id"] == 1
         assert actual_throughput_buckets[0]["maxThroughputPercentage"] == 10
+        assert actual_throughput_buckets[0].get("isDefaultBucket") is None
         assert actual_throughput_buckets[1]["id"] == 2
         assert actual_throughput_buckets[1]["maxThroughputPercentage"] == 20
+        assert actual_throughput_buckets[1]["isDefaultBucket"] == True
 
         # Test: Change throughput without specifying throughput buckets - buckets should be retained
         self.cmd('az cosmosdb sql container throughput update -g {rg} -a {acc} -d {db_name} -n {col} --throughput 4000')
@@ -56,8 +58,10 @@ class Cosmosdb_throughputBucketingTest(ScenarioTest):
         assert len(retained_throughput_buckets) == 2
         assert retained_throughput_buckets[0]["id"] == 1
         assert retained_throughput_buckets[0]["maxThroughputPercentage"] == 10
+        assert retained_throughput_buckets[0].get("isDefaultBucket") is None
         assert retained_throughput_buckets[1]["id"] == 2
         assert retained_throughput_buckets[1]["maxThroughputPercentage"] == 20
+        assert retained_throughput_buckets[1]["isDefaultBucket"] == True
 
         # Verify throughput was actually updated
         assert throughput_resource_json["resource"]["throughput"] == 4000
