f3345466b0d703f5f4180eeb6bc95dbb43e51acb
Dominic Ayre <domayre@outlook.com>
2025-11-24 17:06:37 -0800

    [confcom] Make dependency fetching more resilient (#9441)
    * [confcom] Make binary fetching more resilient
    
    * Fix azdev style
    
    * Bump version
    
    * Check dependency checksums for dependencies
    
    * Fix line lengths

--- src/confcom/azext_confcom/lib/opa.py
+++ src/confcom/azext_confcom/lib/opa.py
@@ -3,49 +3,50 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import platform
+import requests
 import hashlib
 import json
 import os
-from pathlib import Path
-import platform
 import subprocess
+
 from typing import Iterable
+from pathlib import Path
+from azext_confcom.lib.paths import get_binaries_dir
 
-import requests
 
-from azext_confcom.lib.binaries import get_binaries_dir
-
-_opa_path = os.path.abspath(os.path.join(get_binaries_dir(), "opa"))
-_opa_url = {
-    "Linux": "https://github.com/open-policy-agent/opa/releases/download/v1.10.1/opa_linux_amd64",
-    "Windows": "https://github.com/open-policy-agent/opa/releases/download/v1.10.1/opa_windows_amd64.exe",
-}
-_expected_sha256 = {
-    "Linux": "fe8e191d44fec33db2a3d0ca788b9f83f866d980c5371063620c3c6822792877",
-    "Windows": "4c932053350eabca47681208924046fbf3ad9de922d6853fb12cddf59aef15ce",
+_binaries_dir = get_binaries_dir()
+_opa_binaries = {
+    "Linux": {
+        "path": _binaries_dir / "opa",
+        "url": "https://github.com/open-policy-agent/opa/releases/download/v1.10.1/opa_linux_amd64",
+        "sha256": "fe8e191d44fec33db2a3d0ca788b9f83f866d980c5371063620c3c6822792877",
+    },
+    "Windows": {
+        "path": _binaries_dir / "opa.exe",
+        "url": "https://github.com/open-policy-agent/opa/releases/download/v1.10.1/opa_windows_amd64.exe",
+        "sha256": "4c932053350eabca47681208924046fbf3ad9de922d6853fb12cddf59aef15ce",
+    },
 }
 
 
 def opa_get():
 
-    if not all(platform.system() in mapping for mapping in [_opa_url, _expected_sha256]):
-        raise RuntimeError(f"OPA is not supported on platform: {platform.system()}")
+    for binary_info in _opa_binaries.values():
+        opa_fetch_resp = requests.get(binary_info["url"], verify=True)
+        opa_fetch_resp.raise_for_status()
 
-    opa_fetch_resp = requests.get(_opa_url[platform.system()], verify=True)
-    opa_fetch_resp.raise_for_status()
+        assert hashlib.sha256(opa_fetch_resp.content).hexdigest() == binary_info["sha256"]
 
-    assert hashlib.sha256(opa_fetch_resp.content).hexdigest() == _expected_sha256[platform.system()]
+        with open(binary_info["path"], "wb") as f:
+            f.write(opa_fetch_resp.content)
 
-    with open(_opa_path, "wb") as f:
-        f.write(opa_fetch_resp.content)
-
-    os.chmod(_opa_path, 0o755)
-    return _opa_path
+        os.chmod(binary_info["path"], 0o755)
 
 
 def opa_run(args: Iterable[str]) -> subprocess.CompletedProcess:
     return subprocess.run(
-        [_opa_path, *args],
+        [_opa_binaries[platform.system()]["path"], *args],
         check=True,
         stdout=subprocess.PIPE,
         text=True,
