51959d73be9d5a1ec3e2b0df3ebd3812a562d0b0
mihret <mihret.kidane@oracle.com>
2025-12-05 00:11:13 -0600

    tets recordings

--- src/oracle-database/azext_oracle_database/tests/latest/test_oracle_basedb_delete.py
+++ src/oracle-database/azext_oracle_database/tests/latest/test_oracle_basedb_delete.py
@@ -9,9 +9,10 @@ import unittest
 import time
 
 from azure.cli.testsdk.scenario_tests import AllowLargeResponse
-from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer)
+from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer, live_only)
 
 class OracleBaseDbDeleteScenarioTest(ScenarioTest):
+    @live_only()
     @AllowLargeResponse(size_kb=10240)
     @ResourceGroupPreparer(name_prefix='cli_test_odba_rg')
     def setUp(self):
@@ -25,8 +26,8 @@ class OracleBaseDbDeleteScenarioTest(ScenarioTest):
         # Delete db systems
         self.cmd(
             'az oracle-database db-system delete '
-            '--name bhDBiad1 '
-            '--resource-group bhtest '
+            '--name AzureCliSdkNewDdT '
+            '--resource-group AzureCli '
             '--yes --no-wait'
         )
         # Delete network anchor
