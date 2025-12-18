0a26a488aa2889ab6e3d361342fb86fbd0d845ab
FumingZhang <81607949+FumingZhang@users.noreply.github.com>
2025-12-19 10:34:59 +1100

    Update src/aks-preview/setup.py

--- src/aks-preview/setup.py
+++ src/aks-preview/setup.py
@@ -9,7 +9,7 @@ from codecs import open as open1
 
 from setuptools import find_packages, setup
 
-VERSION = "19.0.0b19"
+VERSION = "19.0.0b18"
 
 CLASSIFIERS = [
     "Development Status :: 4 - Beta",
