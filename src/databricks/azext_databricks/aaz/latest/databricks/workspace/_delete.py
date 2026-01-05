fde5ab160126989de0ab13996e65f318d84519be
LaurenK <50049809+2003LK@users.noreply.github.com>
2026-01-05 04:13:46 -0500

    Updating Azure Databricks API Version to 2025-10-01-preview (#9489)
    * update access_connector
    
    * update other files except workspace
    
    * Adding serverless
    
    * new ESC values, managed resource group not mandatory
    
    * All tests uncommented
    
    * fixed update for serverless
    
    * serverless working
    
    * esc working
    
    * all tests passed
    
    * Added to changelog
    
    * Add extension version to setup
    
    * Resolving comments
    
    ---------
    
    Co-authored-by: Lauren Kam <laurenkam@microsoft.com>

--- src/databricks/azext_databricks/aaz/latest/databricks/workspace/_delete.py
+++ src/databricks/azext_databricks/aaz/latest/databricks/workspace/_delete.py
@@ -23,9 +23,9 @@ class Delete(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2024-05-01",
+        "version": "2025-10-01-preview",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.databricks/workspaces/{}", "2024-05-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.databricks/workspaces/{}", "2025-10-01-preview"],
         ]
     }
 
@@ -155,7 +155,7 @@ class Delete(AAZCommand):
                     "forceDeletion", self.ctx.args.force_deletion,
                 ),
                 **self.serialize_query_param(
-                    "api-version", "2024-05-01",
+                    "api-version", "2025-10-01-preview",
                     required=True,
                 ),
             }
--- src/databricks/azext_databricks/aaz/latest/databricks/workspace/vnet_peering/_delete.py
+++ src/databricks/azext_databricks/aaz/latest/databricks/workspace/vnet_peering/_delete.py
@@ -22,9 +22,9 @@ class Delete(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2024-05-01",
+        "version": "2025-10-01-preview",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.databricks/workspaces/{}/virtualnetworkpeerings/{}", "2024-05-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.databricks/workspaces/{}/virtualnetworkpeerings/{}", "2025-10-01-preview"],
         ]
     }
 
@@ -156,7 +156,7 @@ class Delete(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2024-05-01",
+                    "api-version", "2025-10-01-preview",
                     required=True,
                 ),
             }
