32ecbcf7b184d10a945a641381a58278c724b8fc
Kunal Karmakar <kkarmakar@microsoft.com>
2026-01-14 15:43:46 +0530

    Update API version of all azure automation runtime environment commands

--- src/automation/azext_automation/aaz/latest/automation/runtime_environment/_update.py
+++ src/automation/azext_automation/aaz/latest/automation/runtime_environment/_update.py
@@ -22,9 +22,9 @@ class Update(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2023-05-15-preview",
+        "version": "2024-10-23",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.automation/automationaccounts/{}/runtimeenvironments/{}", "2023-05-15-preview"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.automation/automationaccounts/{}/runtimeenvironments/{}", "2024-10-23"],
         ]
     }
 
@@ -200,7 +200,7 @@ class Update(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2023-05-15-preview",
+                    "api-version", "2024-10-23",
                     required=True,
                 ),
             }
@@ -287,7 +287,7 @@ class Update(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2023-05-15-preview",
+                    "api-version", "2024-10-23",
                     required=True,
                 ),
             }
