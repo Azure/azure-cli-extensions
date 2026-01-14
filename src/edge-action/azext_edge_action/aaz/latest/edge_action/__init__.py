2eeced4b3d5cf729b084b259894ab7053501b310
tundwed <55289657+tundwed@users.noreply.github.com>
2026-01-13 17:32:03 -0800

    Upgrade edge-action extension to API version 2025-12-01-preview (#9503)
    - Upgrade to API version 2025-12-01-preview
    - Remove add-attachment and delete-attachment commands (no longer supported in new API)
    - Retain custom fixes for get-version-code and swap-default commands
    
    Co-authored-by: Danny Tundwe (from Dev Box) <dannytundwe@microsoft.com>

--- src/edge-action/azext_edge_action/aaz/latest/edge_action/__init__.py
+++ src/edge-action/azext_edge_action/aaz/latest/edge_action/__init__.py
@@ -9,10 +9,8 @@
 # flake8: noqa
 
 from .__cmd_group import *
-from ._add_attachment import *
 from ._create import *
 from ._delete import *
-from ._delete_attachment import *
 from ._list import *
 from ._show import *
 from ._update import *
