ef17cba14d6b6ac36d74317fe76c00188995405c
chhaynes <chhaynes@microsoft.com>
2026-01-21 18:49:48 -0500

    VMware Extension: Update to API version 2025-09-01 (#9518)

--- src/vmware/azext_vmware/aaz/latest/vmware/cloud_link/_update.py
+++ src/vmware/azext_vmware/aaz/latest/vmware/cloud_link/_update.py
@@ -19,9 +19,9 @@ class Update(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2024-09-01",
+        "version": "2025-09-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.avs/privateclouds/{}/cloudlinks/{}", "2024-09-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.avs/privateclouds/{}/cloudlinks/{}", "2025-09-01"],
         ]
     }
 
@@ -159,7 +159,7 @@ class Update(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2024-09-01",
+                    "api-version", "2025-09-01",
                     required=True,
                 ),
             }
@@ -262,7 +262,7 @@ class Update(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2024-09-01",
+                    "api-version", "2025-09-01",
                     required=True,
                 ),
             }
