7e4fd3a4af72ecda1e303033e23660913e179260
Brian Kimutai <briankimutai@microsoft.com>
2026-01-21 21:18:54 +0300

    Added test recording

--- src/nginx/azext_nginx/aaz/latest/nginx/deployment/configuration/_update.py
+++ src/nginx/azext_nginx/aaz/latest/nginx/deployment/configuration/_update.py
@@ -90,7 +90,6 @@ class Update(AAZCommand):
         _args_schema.root_file = AAZStrArg(
             options=["--root-file"],
             arg_group="Properties",
-            help="The root file of the Nginx Configuration",
             nullable=True,
         )
 
