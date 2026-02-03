e50876747d48c11d0d97ef76417dbb595367cca6
LaurenK <50049809+2003LK@users.noreply.github.com>
2026-02-03 15:32:12 -0800

    Updating API Version to 2026-01-01 (#9554)
    * Editing tests so they run in personal subscription
    
    * Updating API version
    
    * Updating HISTORY.rst
    
    * Updating release version
    
    * Addressing comments
    
    ---------
    
    Co-authored-by: Lauren Kam <laurenkam@microsoft.com>

--- src/databricks/azext_databricks/aaz/latest/databricks/workspace/vnet_peering/_delete.py
+++ src/databricks/azext_databricks/aaz/latest/databricks/workspace/vnet_peering/_delete.py
@@ -22,9 +22,9 @@ class Delete(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2025-10-01-preview",
+        "version": "2026-01-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.databricks/workspaces/{}/virtualnetworkpeerings/{}", "2025-10-01-preview"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.databricks/workspaces/{}/virtualnetworkpeerings/{}", "2026-01-01"],
         ]
     }
 
@@ -156,7 +156,7 @@ class Delete(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2025-10-01-preview",
+                    "api-version", "2026-01-01",
                     required=True,
                 ),
             }
