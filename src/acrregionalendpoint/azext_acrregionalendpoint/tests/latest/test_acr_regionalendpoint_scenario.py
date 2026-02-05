76c0b14166b382df759e0aa8e710bcfd14b14581
Zoey Li <zoeyli@microsoft.com>
2026-02-05 11:18:03 -0500

    update the test to use prod region instead of euap; add test_acr_commands test

--- src/acrregionalendpoint/azext_acrregionalendpoint/tests/latest/test_acr_regionalendpoint_scenario.py
+++ src/acrregionalendpoint/azext_acrregionalendpoint/tests/latest/test_acr_regionalendpoint_scenario.py
@@ -16,29 +16,29 @@ class AcrRegionalEndpointScenarioTest(ScenarioTest):
 
         self.kwargs.update({
             'registry_name': registry_name,
-            'rg_loc': 'centraluseuap',
+            'rg_loc': 'westus',
             'sku': 'Premium'
         })
 
         self.cmd('acr create -g {rg} -n {registry_name} --sku {sku} --location {rg_loc} --regional-endpoints enabled', checks=[
             self.check('name', registry_name),
             self.check('regionalEndpoints', 'Enabled'),
-            self.check('regionalEndpointHostNames', [f'{registry_name}.centraluseuap.geo.azurecr.io']),
+            self.check('regionalEndpointHostNames', [f'{registry_name}.westus.geo.azurecr.io']),
             self.check('provisioningState', 'Succeeded')
         ])
 
         self.cmd('acr show -g {rg} -n {registry_name}', checks=[
             self.check('regionalEndpoints', 'Enabled'),
-            self.check('regionalEndpointHostNames', [f'{registry_name}.centraluseuap.geo.azurecr.io'])
+            self.check('regionalEndpointHostNames', [f'{registry_name}.westus.geo.azurecr.io'])
         ])
 
         self.cmd('acr show-endpoints --name {registry_name} --resource-group {rg}', checks=[
             self.check('length(dataEndpoints)', 1),
             self.check('length(regionalEndpoints)', 1),
             self.check('dataEndpoints[0].endpoint', '*.blob.core.windows.net'),
-            self.check('dataEndpoints[0].region', 'centraluseuap'),
-            self.check('regionalEndpoints[0].endpoint', f'{registry_name}.centraluseuap.geo.azurecr.io'),
-            self.check('regionalEndpoints[0].region', 'centraluseuap')
+            self.check('dataEndpoints[0].region', 'westus'),
+            self.check('regionalEndpoints[0].endpoint', f'{registry_name}.westus.geo.azurecr.io'),
+            self.check('regionalEndpoints[0].region', 'westus')
         ])
 
         self.cmd('acr update -g {rg} -n {registry_name} --regional-endpoints disabled', checks=[
@@ -49,7 +49,7 @@ class AcrRegionalEndpointScenarioTest(ScenarioTest):
         self.cmd('acr show-endpoints --name {registry_name} --resource-group {rg}', checks=[
             self.check('length(dataEndpoints)', 1),
             self.check('dataEndpoints[0].endpoint', '*.blob.core.windows.net'),
-            self.check('dataEndpoints[0].region', 'centraluseuap'),
+            self.check('dataEndpoints[0].region', 'westus'),
         ])
 
         self.cmd('acr delete -g {rg} -n {registry_name} --yes')
@@ -64,7 +64,7 @@ class AcrRegionalEndpointScenarioTest(ScenarioTest):
             'sku': 'Premium'
         })
 
-        self.cmd('acr create -g {rg} -n {registry_name} --sku {sku} --location centraluseuap --regional-endpoints disabled', checks=[
+        self.cmd('acr create -g {rg} -n {registry_name} --sku {sku} --location westus --regional-endpoints disabled', checks=[
             self.check('name', registry_name),
             self.check('regionalEndpoints', 'Disabled'),
             self.check('regionalEndpointHostNames', []),
@@ -78,7 +78,7 @@ class AcrRegionalEndpointScenarioTest(ScenarioTest):
 
         self.cmd('acr update -g {rg} -n {registry_name} --regional-endpoints enabled', checks=[
             self.check('regionalEndpoints', 'Enabled'),
-            self.check('regionalEndpointHostNames', [f'{registry_name}.centraluseuap.geo.azurecr.io'])
+            self.check('regionalEndpointHostNames', [f'{registry_name}.westus.geo.azurecr.io'])
         ])
 
         self.cmd('acr delete -g {rg} -n {registry_name} --yes')
@@ -93,14 +93,14 @@ class AcrRegionalEndpointScenarioTest(ScenarioTest):
             'sku': 'Premium'
         })
 
-        self.cmd('acr create -n {registry_name} -g {rg} --sku {sku} --location centraluseuap --regional-endpoints enabled',
+        self.cmd('acr create -n {registry_name} -g {rg} --sku {sku} --location westus --regional-endpoints enabled',
                  checks=[self.check('name', registry_name),
                          self.check('regionalEndpoints', 'Enabled'),
                          self.check('provisioningState', 'Succeeded')])
 
         # using --expose-token and --endpoint at the same time is not supported
         with self.assertRaises(CLIError) as ex:
-            self.cmd('acr login -n {} --expose-token --endpoint centraluseuap'.format(registry_name))
+            self.cmd('acr login -n {} --expose-token --endpoint westus'.format(registry_name))
 
         self.cmd('acr delete -g {rg} -n {registry_name} --yes')
 
@@ -115,11 +115,11 @@ class AcrRegionalEndpointScenarioTest(ScenarioTest):
             'resource_id': '/subscriptions/dfb63c8c-7c89-4ef8-af13-75c1d873c895/resourcegroups/resourcegroupdiffsub/providers/Microsoft.ContainerRegistry/registries/sourceregistrydiffsub',
             'source_registry_name': source_registry_name,
             'registry_name': registry_name,
-            'rg_loc': 'centraluseuap',
+            'rg_loc': 'westus',
             'source_image': 'microsoft:azure-cli',
             'tag': 'repo:v1',
-            'source_registry_regional_endpoint': f'{source_registry_name}.centraluseuap.geo.azurecr.io',
-            'source_image_regional_endpoint': f'{source_registry_name}.centraluseuap.geo.azurecr.io/microsoft:azure-cli'
+            'source_registry_regional_endpoint': f'{source_registry_name}.westus.geo.azurecr.io',
+            'source_image_regional_endpoint': f'{source_registry_name}.westus.geo.azurecr.io/microsoft:azure-cli'
         })
 
         # Create source registry with regional endpoints enabled
