046ede2df08a5db9abc0de67e3036821e91c65b4
Mai Nguyen (from Dev Box) <nguyenma@microsoft.com>
2026-03-06 09:23:58 -0800

    add placeholder

--- src/acrtransfer/azext_acrtransfer/tests/latest/test_acrtransfer_scenario.py
+++ src/acrtransfer/azext_acrtransfer/tests/latest/test_acrtransfer_scenario.py
@@ -3,3 +3,10 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+from azure.cli.testsdk import ScenarioTest
+
+
+class AcrTransferScenarioTest(ScenarioTest):
+    # TODO: add tests here
+    pass
+
