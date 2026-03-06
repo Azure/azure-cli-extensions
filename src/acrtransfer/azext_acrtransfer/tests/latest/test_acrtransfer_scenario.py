d026f34780a2142c98f8401435cb9951b6bf3ee5
Mai Nguyen (from Dev Box) <nguyenma@microsoft.com>
2026-03-06 09:45:07 -0800

    add test

--- src/acrtransfer/azext_acrtransfer/tests/latest/test_acrtransfer_scenario.py
+++ src/acrtransfer/azext_acrtransfer/tests/latest/test_acrtransfer_scenario.py
@@ -3,10 +3,29 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from azure.cli.testsdk import ScenarioTest
+from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
 
 
 class AcrTransferScenarioTest(ScenarioTest):
-    # TODO: add tests here
-    pass
+    @ResourceGroupPreparer()
+    def test_acrtransfer_import_pipeline_list(self, resource_group):
+        """Test listing import pipelines"""
+        self.kwargs.update({
+            'registry_name': self.create_random_name('acr', 20),
+            'rg': resource_group,
+            'location': 'eastus',
+            'sku': 'Premium'
+        })
+        
+        self.cmd('acr create -n {registry_name} -g {rg} -l {location} --sku {sku}', checks=[
+            self.check('name', '{registry_name}'),
+            self.check('location', '{location}'),
+            self.check('sku.name', '{sku}')
+        ])
+        
+        self.cmd('acr import-pipeline list -r {registry_name} -g {rg}', checks=[
+            self.check('length(@)', 0)
+        ])
+        
+        self.cmd('acr delete -n {registry_name} -g {rg} -y')
 
