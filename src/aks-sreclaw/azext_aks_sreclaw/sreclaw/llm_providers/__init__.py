22c48e3a6862c8bdd48e88df314a4588c5ff5158
Qingchuan Hao <qingchuan.hao@microsoft.com>
2026-03-23 03:08:51 +0000

    Rename agent folder to sreclaw and update all imports

--- src/aks-sreclaw/azext_aks_sreclaw/sreclaw/llm_providers/__init__.py
+++ src/aks-sreclaw/azext_aks_sreclaw/sreclaw/llm_providers/__init__.py
@@ -5,7 +5,7 @@
 
 from typing import List, Tuple
 
-from azext_aks_sreclaw.agent.console import ERROR_COLOR, HELP_COLOR
+from azext_aks_sreclaw.sreclaw.console import ERROR_COLOR, HELP_COLOR
 from rich.console import Console
 
 from .anthropic_provider import AnthropicProvider
