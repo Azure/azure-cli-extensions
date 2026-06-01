c299bc750bcec366264605c73819c26f272a1883
Wen Huang <wenhuang@microsoft.com>
2026-06-01 22:50:51 +0000

    aks-preview: Align hosted-system BYO VNet logic with CLI

--- src/aks-preview/azext_aks_preview/custom.py
+++ src/aks-preview/azext_aks_preview/custom.py
@@ -1180,7 +1180,6 @@ def aks_create(
     # app routing istio
     enable_app_routing_istio=False,
     enable_hosted_system=False,
-    disable_hosted_system=False,
     system_node_subnet_id=None,
     node_subnet_id=None,
     control_plane_scaling_size=None,
