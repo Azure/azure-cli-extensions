d639918119a0e63ca1d0154c6c2eee3553fe878b
Daniel Steven <danielsteven@microsoft.com>
2026-06-26 11:40:27 -0500

    NetworkCloud extension updates for the 2026-07-01 GA API

--- src/networkcloud/azext_networkcloud/tests/latest/utils/__init__.py
+++ src/networkcloud/azext_networkcloud/tests/latest/utils/__init__.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#
+# --------------------------------------------------------------------------------------------
 """Utilities package for latest Network Cloud tests."""
 
 from .assert_messages import (
