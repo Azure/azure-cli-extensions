8dcb088798bb3a42809157f051de1b7206bab520
william <williamng0512@gmail.com>
2026-07-06 11:13:13 +0800

    Update code

--- src/image-gallery/azext_image_gallery/commands.py
+++ src/image-gallery/azext_image_gallery/commands.py
@@ -12,7 +12,6 @@ from ._client_factory import cf_community_gallery, cf_community_gallery_image, c
 
 
 def load_command_table(self, _):
-
     community_gallery_sdk = CliCommandType(
         operations_tmpl='azext_image_gallery.vendored_sdks.azure_mgmt_compute.operations._community_galleries_operations#CommunityGalleriesOperations.{}',
         client_factory=cf_community_gallery)
