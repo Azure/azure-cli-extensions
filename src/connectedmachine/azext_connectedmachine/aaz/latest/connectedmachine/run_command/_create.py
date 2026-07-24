23c0ae1aeb7acf58ab16c1395506e06c37d8855c
Yao Tong <yaotong@microsoft.com>
2026-07-24 17:13:30 -0400

    add

--- src/connectedmachine/azext_connectedmachine/aaz/latest/connectedmachine/run_command/_create.py
+++ src/connectedmachine/azext_connectedmachine/aaz/latest/connectedmachine/run_command/_create.py
@@ -182,11 +182,7 @@ class Create(AAZCommand):
             help="Specifies the script download location. It can be either SAS URI of an Azure storage blob with read access or public URI.",
         )
         _args_schema.script_uri_managed_identity = AAZObjectArg(
-<<<<<<< HEAD
-            options=["--script-uri-id", "--script-uri-managed-id", "--script-uri-managed-identity"],
-=======
             options=["--script-uri-id", "--script-uri-managed-identity"],
->>>>>>> de3c45b8d6da868f45044e6a318a232f936e974c
             arg_group="Source",
             help="User-assigned managed identity that has access to scriptUri in case of Azure storage blob. Use an empty object in case of system-assigned identity. Make sure the Azure storage blob exists, and managed identity has been given access to blob's container with 'Storage Blob Data Reader' role assignment. In case of user-assigned identity, make sure you add it under VM's identity. For more info on managed identity and Run Command, refer https://aka.ms/ManagedIdentity and https://aka.ms/RunCommandManaged.",
         )
