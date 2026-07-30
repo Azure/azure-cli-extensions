d6885e628d8f525411f21ed5105249bec593f520
Ekaterina Legacheva (AKVELON INC) <v-elegacheva@microsoft.com>
2026-07-30 12:18:25 -0700

    Quantum: Add `az quantum workspace user create/delete` commands
    Add commands to manage user access to an Azure Quantum workspace by creating or deleting an Azure RBAC role assignment (Quantum Workspace Data Contributor by default) at the workspace scope.

--- src/quantum/azext_quantum/_help.py
+++ src/quantum/azext_quantum/_help.py
@@ -341,6 +341,46 @@ helps['quantum workspace keys'] = """
     short-summary: Manage Azure Quantum Workspace api keys.
 """
 
+helps['quantum workspace user'] = """
+    type: group
+    short-summary: Manage users of an Azure Quantum workspace.
+"""
+
+helps['quantum workspace user create'] = """
+    type: command
+    short-summary: Grant a user, group, or service principal access to an Azure Quantum workspace.
+    long-summary: >-
+        Assigns the 'Quantum Workspace Data Contributor' role (by default) at the scope of the given
+        (or current) Azure Quantum workspace.
+    examples:
+      - name: Grant a user access to a workspace using their sign-in name.
+        text: |-
+            az quantum workspace user create -g MyResourceGroup -w MyWorkspace \\
+                --assignee user@contoso.com
+      - name: Grant a user access to a workspace using their object id.
+        text: |-
+            az quantum workspace user create -g MyResourceGroup -w MyWorkspace \\
+                --assignee-object-id 00000000-0000-0000-0000-000000000000
+      - name: Grant a group access to a workspace using its object id and principal type.
+        text: |-
+            az quantum workspace user create -g MyResourceGroup -w MyWorkspace \\
+                --assignee-object-id 00000000-0000-0000-0000-000000000000 --assignee-principal-type Group
+"""
+
+helps['quantum workspace user delete'] = """
+    type: command
+    short-summary: Remove a user, group, or service principal's access to an Azure Quantum workspace.
+    examples:
+      - name: Remove a user's access to a workspace using their sign-in name.
+        text: |-
+            az quantum workspace user delete -g MyResourceGroup -w MyWorkspace \\
+                --assignee user@contoso.com
+      - name: Remove a user's access to a workspace using their object id.
+        text: |-
+            az quantum workspace user delete -g MyResourceGroup -w MyWorkspace \\
+                --assignee-object-id 00000000-0000-0000-0000-000000000000
+"""
+
 helps['quantum workspace keys list'] = """
     type: command
     short-summary: List api keys for the given (or current) Azure Quantum workspace.
