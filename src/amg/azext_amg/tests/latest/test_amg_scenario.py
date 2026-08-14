46425c51fa308f656cc9bd53f0976a89beaeafb6
Nithin <kumbam.nithingoud@gmail.com>
2026-08-14 08:31:17 -0400

    Style: remove duplicate AmgUnitTest class from test_amg_scenario.py

--- src/amg/azext_amg/tests/latest/test_amg_scenario.py
+++ src/amg/azext_amg/tests/latest/test_amg_scenario.py
@@ -17,20 +17,6 @@ from .recording_processors import ApiKeyServiceAccountTokenReplacer
 
 TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
-
-class AmgUnitTest(unittest.TestCase):
-    def test_duration_conversion(self):
-        from azext_amg.custom import _convert_duration_to_seconds
-        self.assertEqual(_convert_duration_to_seconds("1s"), 1)
-        self.assertEqual(_convert_duration_to_seconds("1m"), 60)
-        self.assertEqual(_convert_duration_to_seconds("1h"), 3600)
-        self.assertEqual(_convert_duration_to_seconds("1d"), 86400)
-        self.assertEqual(_convert_duration_to_seconds("1w"), 604800)
-        self.assertEqual(_convert_duration_to_seconds("1M"), 2592000)
-        self.assertEqual(_convert_duration_to_seconds("1y"), 31536000)
-        self.assertEqual(_convert_duration_to_seconds("10y"), 315360000)
-
-
 class AmgScenarioTest(ScenarioTest):
     def __init__(self, method_name):
         super().__init__(method_name, recording_processors=[
