95819799069ca10b30b877c514a333922b9e50cd
Runzhen <runzhenwang@microsoft.com>
2026-08-31 22:19:15 -0700

    [AKS] az aks nodepool add dra option (#10268)

--- src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
+++ src/aks-preview/azext_aks_preview/tests/latest/test_aks_commands.py
@@ -8005,6 +8005,7 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
                 self.check(
                     "gpuProfile.nvidia.managementMode", "Managed"
                 ),
+                self.check("gpuProfile.nvidia.driverMode", "DevicePlugin"),
             ],
         )
 
@@ -18625,13 +18626,15 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             "--resource-group={resource_group} "
             "--cluster-name={name} "
             "--name={node_pool_name} "
-            "--enable-managed-gpu=true ",
+            "--enable-managed-gpu=true "
+            "--gpu-driver-mode=dra ",
             checks=[
                 self.check("provisioningState", "Succeeded"),
                 self.check("gpuProfile.driver", "Install"),
                 self.check(
                     "gpuProfile.nvidia.managementMode", "Managed"
                 ),
+                self.check("gpuProfile.nvidia.driverMode", "DRA"),
             ],
         )
 
