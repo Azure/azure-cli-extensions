9e3a39b6a4ff426882f9471711972e50f43a8832
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2026-09-23 09:22:09 -0700

    Fix setup.py, update PostEmitter.ps1

--- src/foundry-cost-control/setup.py
+++ src/foundry-cost-control/setup.py
@@ -20,11 +20,11 @@ CLASSIFIERS = [
     'Intended Audience :: System Administrators',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: 3.12',
     'Programming Language :: Python :: 3.13',
+    'Programming Language :: Python :: 3.14',
     'License :: OSI Approved :: MIT License',
 ]
 
