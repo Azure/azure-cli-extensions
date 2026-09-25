c4cc3ba3f4b5f032585820913bdcb815b530a9f5
Anusha Kowdeed <ankowdeed@microsoft.com>
2026-09-25 00:43:59 -0500

    Add Workload Manager CLI extension

---
+++ src/workload-manager/azext_workload_manager/custom.py
@@ -0,0 +1,12 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-statements
+
+from knack.log import get_logger
+
+
+logger = get_logger(__name__)
